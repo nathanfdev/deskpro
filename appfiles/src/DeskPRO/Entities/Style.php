4803d619eb6989a34cf47e0c9fc949400cf65223
Christopher Nadeau <chris@nadeau.ws>
2010-07-28 03:12:27 -0400

    - Add style entity

---
+++ appfiles/src/DeskPRO/Entities/Style.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @category Entities
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace DeskPRO\Entities;
+use Orb\Util\Strings;
+use Orb\Util\Arrays;
+
+/**
+ * Settings used by the system.
+ *
+ * @Entity
+ * @Table(name="styles")
+ */
+class Style extends Entity
+{
+	/**
+	 * The unique ID.
+	 *
+	 * @var int
+	 * @Id
+	 * @Column(name="id", type="integer")
+	 * @GeneratedValue
+	 */
+	protected $id = null;
+
+
+	/**
+	 * The parent style ID. All styles at least descened from 1, the default.
+	 *
+	 * @var int
+	 * @Id
+	 * @Column(name="parent_id", type="integer")
+	 */
+	protected $parent_id = null;
+
+
+	/**
+	 * @var Style
+	 * @OneToOne(targetEntity="Style")
+	 * @JoinColumn(name="parent_id", referencedColumnName="id")
+	 */
+	protected $parent;
+
+
+	/**
+	 * Title of the style
+	 *
+	 * @var string
+	 * @Column(name="title", type="string", length=255)
+	 */
+	protected $title;
+
+
+	/**
+	 * A note or description about the style
+	 *
+	 * @var string
+	 * @Column(name="note", type="text")
+	 */
+	protected $note;
+
+
+	/**
+	 * @var \DateTime
+	 * @Column(name="created_at",type="datetime")
+	 */
+	protected $created_at;
+
+
+	
+	/**
+	 * @PrePersist
+	 * @PreUpdate
+	 */
+	public function _setDefaultParent()
+	{
+		if (!$this->parent_id) {
+			$this->parent_id = 1;
+		}
+	}
+
+
+	/** @PrePersist */
+	public function _incCreatedAt()
+	{
+		$this->created_at = new \DateTime();
+	}
+}
