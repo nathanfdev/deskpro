ee774bda93a97b5f9990ef282946b645ef9816d2
Christopher Nadeau <chris@nadeau.ws>
2010-07-29 02:25:56 -0400

    - Add Language and Phrase entities

---
+++ appfiles/src/DeskPRO/Entities/Language.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @category Entities
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace DeskPRO\Entities;
+use Orb\Util\Strings;
+use Orb\Util\Arrays;
+
+/**
+ * Settings used by the system.
+ *
+ * @Entity
+ * @Table(name="languages")
+ */
+class Language extends Entity
+{
+	/**
+	 * The unique ID.
+	 *
+	 * @var int
+	 * @Id
+	 * @Column(name="id", type="integer")
+	 * @GeneratedValue
+	 */
+	protected $id = null;
+
+
+	/**
+	 * The parent language ID. All languages at least descened from 1, the default.
+	 *
+	 * @var int
+	 * @Id
+	 * @Column(name="parent_id", type="integer")
+	 */
+	protected $parent_id = null;
+
+
+	/**
+	 * @var Style
+	 * @OneToOne(targetEntity="Language")
+	 * @JoinColumn(name="parent_id", referencedColumnName="id")
+	 */
+	protected $parent;
+
+
+	/**
+	 * Title of the style
+	 *
+	 * @var string
+	 * @Column(name="title", type="string", length=255)
+	 */
+	protected $title;
+
+
+	/**
+	 * A note or description about the style
+	 *
+	 * @var string
+	 * @Column(name="note", type="text")
+	 */
+	protected $note;
+
+
+	/**
+	 * @var \DateTime
+	 * @Column(name="created_at",type="datetime")
+	 */
+	protected $created_at;
+
+	public function setParentId($parent_id)
+	{
+		$this->parent_id = $parent_id;
+
+		// TODO: Cache parents hierarchy later so template fetching is easier
+		$this->_parent_has_changed = true;
+	}
+
+
+	/**
+	 * @PrePersist
+	 * @PreUpdate
+	 */
+	public function _setDefaultParentId()
+	{
+		if (!$this->parent_id) {
+			$this->setParentId(1);
+		}
+	}
+
+
+	/** @PrePersist */
+	public function _incCreatedAt()
+	{
+		$this->created_at = new \DateTime();
+	}
+}
---
+++ appfiles/src/DeskPRO/Entities/Phrase.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @category Entities
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace DeskPRO\Entities;
+use Orb\Util\Strings;
+use Orb\Util\Arrays;
+
+/**
+ * Templates used in the system
+ *
+ * @Entity
+ * @Table(name="phrases")
+ */
+class Phrase extends Entity
+{
+	/**
+	 * The unique ID.
+	 *
+	 * @var int
+	 * @Id
+	 * @Column(name="id", type="integer")
+	 * @GeneratedValue
+	 */
+	protected $id = null;
+
+
+	/**
+	 * The language ID.
+	 *
+	 * @var int
+	 * @Id
+	 * @Column(name="language_id", type="integer")
+	 */
+	protected $language_id = null;
+
+
+	/**
+	 * The language this phrase belongs to
+	 *
+	 * @var Style
+	 * @OneToOne(targetEntity="Language")
+	 * @JoinColumn(name="language_id", referencedColumnName="id")
+	 */
+	protected $language;
+
+
+	/**
+	 * The name of the phrase
+	 *
+	 * @var string
+	 * @Column(name="name", type="string", length=255)
+	 */
+	protected $name = null;
+
+
+	/**
+	 * Phrases can belong to groups. The group is the string
+	 * before the first dot in the name. deskpro.profile, the group is 'deskpro'
+	 *
+	 * @var string
+	 * @Index
+	 * @Column(name="group", type="string", length=255, nullable=true, default=null)
+	 */
+	protected $group;
+
+
+	/**
+	 * @var string
+	 * @Column(name="phrase", type="text")
+	 */
+	protected $phrase;
+
+
+	/**
+	 * @var \DateTime
+	 * @Column(name="created_at",type="datetime")
+	 */
+	protected $created_at;
+
+
+	/**
+	 * @var \DateTime
+	 * @Column(name="updated_at",type="datetime")
+	 */
+	protected $updated_at;
+
+
+
+	/**
+	 * @PrePersist
+	 * @PreUpdate
+	 */
+	public function _resetGroupFromName()
+	{
+		$dotpos = strpos($this->name, '.');
+		if ($dotpos) {
+			$this->group = substr($this->name, 0, $dotpos);
+		} else {
+			$this->group = null;
+		}
+	}
+
+	/** @PrePersist */
+	public function incCreatedAt()
+	{
+		$this->created_at = $this->updated_at = new \DateTime();
+	}
+
+	/** @PreUpdate */
+	public function incUpdatedAt()
+	{
+		$this->updated_at = new \DateTime();
+	}
+}
