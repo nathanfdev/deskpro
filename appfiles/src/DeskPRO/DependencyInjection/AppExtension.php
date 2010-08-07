30b9fc2e2645375bc92a673c5a51e2f3edcccaaf
Christopher Nadeau <chris@nadeau.ws>
2010-08-06 23:27:44 -0400

    - Add an extension init the App and set the main container

---
+++ appfiles/src/DeskPRO/DependencyInjection/AppExtension.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @category DependencyInjection
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace DeskPRO\DependencyInjection;
+
+use Symfony\Components\DependencyInjection\ContainerBuilder;
+use DeskPRO\App;
+
+/**
+ * This simply initiates teh App registry and sets the main app container.
+ */
+class AppExtension extends \Symfony\Components\DependencyInjection\Extension\Extension
+{
+	public function configLoad($config, ContainerBuilder $container)
+    {
+		parent::configLoad($config, $container);
+
+		App::set('container', $container);
+    }
+
+	public function getAlias()
+    {
+        return 'app';
+    }
+}
