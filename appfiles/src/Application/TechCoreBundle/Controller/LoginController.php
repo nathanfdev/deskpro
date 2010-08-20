73ed1fb5cd0b66acf7cb0aa504e4d00a7d3365e9
Christopher Nadeau <chris@nadeau.ws>
2010-08-20 06:25:44 -0400

    - Fully working login yay

--- appfiles/src/Application/TechCoreBundle/Controller/LoginController.php
+++ appfiles/src/Application/TechCoreBundle/Controller/LoginController.php
@@ -5,8 +5,47 @@ use Symfony\Bundle\FrameworkBundle\Controller;
 
 class LoginController extends Controller
 {
-    public function indexAction()
+	public function indexAction()
     {
-        return $this->render('TechCoreBundle:Login:index:twig');
+		$tplvars = array();
+
+		if (isset($_REQUEST['process'])) {
+			$user = $this->_processLogin();
+			if ($user) {
+				$this->container->get('request')->getSession()->setAttribute('auth_userid', $user['id']);
+				return $this->redirect($this->container->getRouterService()->generate('TechCoreBundle:Main:index'));
+			} else {
+				$tplvars['invalid_login'] = true;
+			}
+		}
+
+       return $this->render('TechCoreBundle:Login:index:twig', $tplvars);
     }
+
+	protected function _processLogin()
+	{
+		$email_address = $_REQUEST['email_address'];
+		$password = $_REQUEST['password'];
+
+		$em = $this->container->get('doctrine.orm.entity_manager');
+		$q = $em->createQuery('
+			SELECT p
+			FROM DeskPRO\\Bundle\\Core\\Entity\\ProfileEmail p
+			WHERE p.email_address = ?1
+		');
+		$q->setParameter(1, $email_address);
+
+		$profile_email = $q->getSingleResult();
+
+		if (!$profile_email OR !$profile_email['profile'] OR !$profile_email['profile']['user_id']) {
+			return false;
+		}
+
+		$user = $profile_email['profile']['user'];
+		if (!$user->checkPassword($password)) {
+			return false;
+		}
+
+		return true;
+	}
 }
