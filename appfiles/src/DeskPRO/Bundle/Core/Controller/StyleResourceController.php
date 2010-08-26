be97efed9afdeba0f94dd2472162ca19e701d959
Christopher Nadeau <chris@nadeau.ws>
2010-08-26 03:10:31 -0400

    - Add new StyleResource entity for handling misc attachments for styles - Add controller to serve up static resources

---
+++ appfiles/src/DeskPRO/Bundle/Core/Controller/StyleResourceController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace DeskPRO\Bundle\Core\Entity;
+use Orb\Util\Web;
+
+/**
+ * Serve style resources
+ */
+class StyleResourceController extends \Symfony\Bundle\FrameworkBundle\Controller
+{
+	public function servAction($resource_id, $resource_filename)
+	{
+		/** @var Doctrine\ORM\EntityManager */
+		$em = $this['doctrine.orm.entity_manager'];
+
+		/** @var DeskPRO\Bundle\Core\Entity\StyleResource */
+		$res = $em->find('Core:StyleResource', $resource_id);
+		if (!$res) {
+			throw new \Symfony\Components\HttpKernel\Exception\NotFoundHttpException("There is no resource with ID $resource_id");
+		}
+
+		// Make sure the URL is correct (consistency)
+		if ($resource_filename != $res->getFilename()) {
+			$this->redirect($this->generateUrl('core_styleres_serv', array('resource_id' => $resource_id, 'resource_filename' => $res->getFilename())));
+		}
+
+		// TODO
+		// Right now mainly used for CSS so this is okay
+		// But later we'll want to handle static caching, and handling
+		// better output of large (ie images) files
+		$content_fp = fopen('php://memory', 'r+');
+		$res->writeResourceData($content_fp);
+
+		$headers = $res->getResourceHeaders();
+
+		$response = $this->createResponse($content_fp, 200, $headers);
+		fclose($content_fp);
+
+		return $response;
+	}
+}
