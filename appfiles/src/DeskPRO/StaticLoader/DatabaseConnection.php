40e595b660ea06d5b7d973f80db296799e5a117b
Christopher Nadeau <chris@nadeau.ws>
2010-09-03 00:34:40 -0400

    - Fix bug with checking for key before the args are fetched

--- appfiles/src/DeskPRO/StaticLoader/DatabaseConnection.php
+++ appfiles/src/DeskPRO/StaticLoader/DatabaseConnection.php
@@ -29,9 +29,10 @@ class DatabaseConnection
 {
 	static function getConnection()
 	{
+		$args = func_get_args();
+
 		$key = isset($args[0]['dp_from_user_config']) ? isset($args[0]['dp_from_user_config']) : 'db';
 
-		$args = func_get_args();
 		$args[0] = array_merge($args[0], \DeskPRO\Kernel\Kernel::getUserConfig($key));
 
 		return call_user_func_array(array('Doctrine\DBAL\DriverManager', 'getConnection'), $args);
