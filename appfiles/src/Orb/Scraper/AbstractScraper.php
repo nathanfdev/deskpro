ab2c481d7f801a41ee8762d82f8cc365689dce60
Christopher Nadeau <chris@nadeau.ws>
2010-09-10 02:28:17 -0400

    - Add simple resource scraper that can be implemented on any site

---
+++ appfiles/src/Orb/Scraper/AbstractScraper.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Orb
+ *
+ * @package Orb
+ * @subpackage Scraper
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace Orb\Scraper;
+
+/**
+ * A scraper is something that fetches data from some remote source. The method of fetching
+ * the data, or the format of that data, is not relevant (that'll matter for whatever
+ * system implements the system).
+ */
+abstract class AbstractScraper
+{
+	/**
+	 * Array of options
+	 * @var array
+	 */
+	protected $_options;
+
+	public function __construct(array $options = array())
+	{
+		$this->_options = $options;
+	}
+	
+
+
+	/**
+	 * Get the value of an option
+	 *
+	 * @param string $key The option to get
+	 * @param mixed $default What to return if the option doesnt exist
+	 */
+	public function getOption($key, $default = null)
+	{
+		return isset($this->_options[$key]) ? $this->_options[$key] : $default;
+	}
+
+	
+
+	/**
+	 * Check to see if an option exists
+	 * 
+	 * @param string $key
+	 * @return bool
+	 */
+	public function hasOption($key)
+	{
+		return isset($this->_options[$key]);
+	}
+
+
+	
+	/**
+	 * Get the data from the remote source.
+	 *
+	 * @param mixed $identity The ID from the remote source to fetch
+	 * @return mixed
+	 */
+	abstract function getData($identity = null);
+}
---
+++ appfiles/src/Orb/Scraper/OrbResource.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Orb
+ *
+ * @package Orb
+ * @subpackage Scraper
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace Orb\Scraper;
+
+/**
+ * This scraper fetches data from any simple Orb web resource.
+ *
+ * To create a simple orb web service, your script must implement the following protocol:
+ * - Accept POST'ed requests
+ * - There is a `consumer_key` item which should be an agreed-upon secret code to identify and authorize
+ * service users.
+ * - There is a `data` item which may be an array of any data that your resource might use (for example ID's).
+ *
+ * Your resource must return a JSON-encoded data structure that will be decoded into an array.
+ */
+class OrbResource extends AbstractScraper
+{
+	/**
+	 * HTTP client
+	 * @var \Zend\Http\Client
+	 */
+	protected $http;
+
+	/**
+	 * The following options are required:
+	 * - consumer_key: A key/password that identifies this consumer. The remote producer should
+	 * verify the key to ensure authorized access.
+	 * - service_url: The URL to the remote service
+	 */
+	public function __construct(array $options = array())
+	{
+		if (!$this->hasOption('consumer_key')) {
+			throw new \InvalidArgumentException('Missing required option `consumer_key`');
+		}
+		if (!$this->hasOption('service_url')) {
+			throw new \InvalidArgumentException('Missing required option `consumer_key`');
+		}
+
+		if ($this->hasOption('http')) {
+			$this->setHttpClient($this->getOption('http'));
+		}
+	}
+
+	
+
+	/**
+	 * Get data from the resource
+	 *
+	 * @param mixed $identity A string or array of k=>v pairs to be sent as posted 'data'
+	 * @return array
+	 */
+	public function getData($identity = null)
+	{
+		$http = $this->getHttpClient();
+		$http->setUri($this->getOption('service_url'));
+
+		$http->setParameterPost('consumer_key', $this->getOption('consumer_key'));
+
+		if ($identity) {
+			if (is_array($identity)) {
+				foreach ($identity as $k=>$v) {
+					$http->setParameterPost('data['.$k.']', $v);
+				}
+			} else {
+				$http->setParameterPost('data', $identity);
+			}
+		}
+
+		$http_result = $http->request(\Zend\Http\Client::POST);
+
+		$data = @json_decode($http_result->getBody(), true);
+		if (!$data) {
+			throw \UnexpectedValueException('Invalid JSON returned from service');
+		}
+
+		return $data;
+	}
+
+
+
+	/**
+	 * Set a custom HTTP client. If one is not set, a default client will be used automatically.
+	 *
+	 * @param \Zend\Http\Client $http
+	 */
+	public function setHttpClient(\Zend\Http\Client $http)
+	{
+		$this->http = $http;
+	}
+
+
+	
+	/**
+	 * Get the HTTP client.
+	 *
+	 * @return \Zend\Http\Client
+	 */
+	public function getHttpClient()
+	{
+		if ($this->http !== null) return $this->http;
+
+		$this->http = new \Zend\Http\Client();
+		$this->http->resetParameters();
+
+		return $this->http;
+	}
+}
