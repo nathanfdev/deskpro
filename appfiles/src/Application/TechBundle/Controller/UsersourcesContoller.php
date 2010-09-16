5230d5d1e58acc3802b1645b5c97d2f36c86aec5
Christopher Nadeau <chris@nadeau.ws>
2010-09-16 00:33:17 -0400

    - Add controller for managing user sources

---
+++ appfiles/src/Application/TechBundle/Controller/UsersourcesContoller.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage TechBundle
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace Application\TechBundle\Controller;
+
+/**
+ * Handles creating/editing of Usersources
+ */
+class UsersourcesController extends AbstractController
+{
+	############################################################################
+	# /tech/usersources                                   tech_admin_usersources
+	############################################################################
+
+	/**
+	 * Shows existing usersources
+	 */
+	public function indexAction()
+	{
+		$all_usersources = $this->em->createQuery("
+			SELECT us
+			FROM CoreEntity:Usersource us
+			ORDER BY us.display_order ASC
+		")->getResult();
+
+		if (!$all_usersources->count()) {
+			return $this->redirect($this->generateUrl('tech_admin_usersources_info', array()));
+		}
+
+		$this->tplvars['all_usersources'] = $all_usersources;
+
+		return $this->render('TechBundle:Usersources:index');
+	}
+
+
+
+	############################################################################
+	# /tech/usersources/intro                       tech_admin_usersources_intro
+	############################################################################
+
+	/**
+	 * Just shows a simple intro page
+	 */
+	public function introAction()
+	{
+		return $this->render('TechBundle:Usersources:intro');
+	}
+
+
+
+	############################################################################
+	# /tech/usersources/:usersource_id/edit          tech_admin_usersources_edit
+	############################################################################
+
+	/**
+	 * Edit a usersource
+	 */
+	public function editAction($usersource_id)
+	{
+		if ($usersource_id) {
+			$usersource = $this->getUsersourceOr404($usersource_id);
+		} else {
+			$usersource = $this->em->createEntity('CoreBundle:Usersource');
+		}
+
+		if ($this->isPostRequest()) {
+
+		}
+
+		return $this->render('TechBundle:Usersources:edit');
+	}
+
+	
+
+	############################################################################
+	# /tech/usersources/:usersource_id/info          tech_admin_usersources_info
+	############################################################################
+
+	/**
+	 * Shows info about a usersource such as user stats
+	 */
+	public function infoAction($usersource_id)
+	{
+		$usersource = $this->getUsersourceOr404($usersource_id);
+
+		// TODO
+
+		return $this->render('TechBundle:Usersources:info');
+	}
+
+
+
+	############################################################################
+
+	/**
+	 * @return Application\CoreBundle\Entity\Usersource
+	 */
+	protected function getUsersourceOr404($usersource_id)
+	{
+		try {
+			$usersource = $this->em->find('CoreBundle:Usersource', $usersource_id);
+		} catch (\Doctrine\ORM\NoResultException $e) {
+			throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException("There is no usersource with ID $usersource_id");
+		}
+
+		return $usersource;
+	}
+}
