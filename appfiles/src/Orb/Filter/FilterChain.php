7d0d30e03c4d025dc267a947527e38e7434d7fe6
Christopher Nadeau <chris@nadeau.ws>
2010-09-23 01:18:00 -0400

    - Add FilterChain class, a Filter composite

---
+++ appfiles/src/Orb/Filter/FilterChain.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Orb
+ *
+ * @package Orb
+ * @subpackage Filter
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace Orb\Filter;
+
+class FilterChain extends \Zend\Filter\AbstractFilter
+{
+	/**
+	 * An array of filters
+	 * @var array
+	 */
+	protected $filters = array();
+
+
+	
+	/**
+	 * Add a new filter to the chain
+	 *
+	 * @param \Zend\Filter\Filter $filter
+	 */
+	public function addFilter(\Zend\Filter\Filter $filter)
+	{
+		$this->filters[] = $filter;
+	}
+
+
+
+	/**
+	 * Get the filters currently set.
+	 * 
+	 * @return array
+	 */
+	public function getFilters()
+	{
+		return $this->filters;
+	}
+
+
+	
+	/**
+	 * Filter a value.
+	 *
+	 * @param mixed $value
+	 * @return mixed
+	 */
+	public function filter($value)
+	{
+		foreach ($this->filters as $filter) {
+			$value = $filter->filter($value);
+		}
+
+		return $value;
+	}
+}
