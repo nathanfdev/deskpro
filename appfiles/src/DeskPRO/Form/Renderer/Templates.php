e2e2a41d8037389f6c967c29d28fbd77928a9b70
Christopher Nadeau <chris@nadeau.ws>
2010-09-24 05:52:52 -0400

    - Add new Renderer that uses templates

---
+++ appfiles/src/DeskPRO/Form/Renderer/Templates.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage Form
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace DeskPRO\Form\Renderer;
+
+class Templates implements RendererInterface
+{
+	/**
+	 * Template namespace (aka prefix)
+	 * @var string
+	 */
+	protected $template_ns;
+
+	/**
+	 * @var DeskPRO\Templating\Engine
+	 */
+	protected $template_engine;
+
+
+
+	/**
+	 *
+	 * @param \DeskPRO\Templating\Engine $temmplate_engine
+	 * @param string $template_ns
+	 */
+	public function __construct(\DeskPRO\Templating\Engine $temmplate_engine, $template_ns)
+	{
+		$this->template_ns = $template_ns;
+		$this->template_engine = $temmplate_engine;
+	}
+
+
+	
+	/**
+	 * Render the field
+	 *
+	 * @param  Orb\Form\Field\Field $field       The field that needs rendering
+	 * @param  array                $attributes  Attributes of the field
+	 * @return string HTML
+	 */
+	public function renderField(\Orb\Form\Field\Field $field, array $attributes = array())
+	{
+		// The short classname is just the classname without the namespace,
+		// UNLESS the namespace isn't DeskPRO or Orb (meaning its custom)
+		// then its the full name
+		$short_classname = get_class($field);
+
+		if (strpos($short_classname, 'Orb\\') === 0 OR strpos($short_classname, 'DeskPRO\\') === 0) {
+			$parts = explode('\\', $short_classname);
+			$short_classname = array_pop($parts);
+		}
+
+		// So we end up with templates called...
+		// TechBundle:Whatever:Text, TechBundle:Whatever:Date
+
+		return $this->template_engine->render(
+			$this->template_ns . ':' . $short_classname,
+			array('field' => $field, 'attributes' => $attributes)
+		);
+	}
+}
