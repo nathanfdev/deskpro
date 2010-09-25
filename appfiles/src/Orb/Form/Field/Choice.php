b8cc80314515393c6c9d09d23e6473ae29cd71a1
Christopher Nadeau <chris@nadeau.ws>
2010-09-25 04:26:25 -0400

    - New choice field composite, uses checkboxes and radio fields to represent choices

--- appfiles/src/Orb/Form/Field/Choice.php
+++ appfiles/src/Orb/Form/Field/Choice.php
@@ -13,116 +13,40 @@ use Orb\Util\Strings;
 use Orb\Util\Util;
 
 /**
- * A choice field can let the user select single or multiple.
- *
- * The default renderer also allows attribute options 'type' to be 'select', 'checkbox' (multiple), and 'radio' (single)
+ * A choice field is a select box, either single selection or multiple.
+ * Choices are made up of Checkbox or Radio fields.
  */
-class Choice extends Field
+class Choice extends FieldGroup
 {
-	const VAL_OPTGROUP = '__ORB_OPTGROUP__';
-	
-	const SELMODE_SINGLE   = 'single';
+	const SELMODE_SINGLE = 'single';
 	const SELMODE_MULTIPLE = 'multiple';
-
-	protected $choices = array();
-
-	protected $selection_mode = 'single';
 
 	public function init()
 	{
-		if ($this->hasOption('selection_mode')) {
-			$this->selection_mode = $this->getOption('selection_mode');
+		$this->addAllowedFieldType('Orb\\Form\\Field\\Checkbox');
+		$this->addAllowedFieldType('Orb\\Form\\Field\\Radio');
+	}
+
+
+	public function isFieldAllowed(Field $field)
+	{
+		if (!parent::isFieldAllowed($field)) {
+			return false;
 		}
-	}
 
-	
-
-	/**
-	 * Set single or multiple selection mode. Use the SELMODE_* constants.
-	 * 
-	 * @param string $mode Mode to set
-	 */
-	public function setSelectionMode($mode)
-	{
-		$this->selection_mode = $mode;
-	}
-
-
-	
-	/**
-	 * Add a choice.
-	 *
-	 * Set a value of the constant VAL_OPTGROUP to start a new option group.
-	 *
-	 * @param string $value  The value of the option
-	 * @param string $label  The label of the option
-	 */
-	public function addChoice($value, $label, $key = null)
-	{
-		$choice = array('value' => $value, 'label' => $label, 'type' => 'choice');
-
-		if ($value == self::VAL_OPTGROUP) {
-			$choice['type'] = 'sectionstart';
-		}
-		$this->choices[] = $choice;
-	}
-
-
-
-	/**
-	 * Get an array of choices
-	 *
-	 * @param bool $flat A flat array of label=>value (also removes optgroups)?
-	 * @return array
-	 */
-	public function getChoices($flat = false)
-	{
-		$arr = $this->choices;
-
-		if ($flat) {
-			$arr = array();
-			foreach ($this->choices as $choice) {
-				if ($choice['type'] != 'choice') continue;
-				$arr[$choice['label']] = $choice['value'];
+		// Make sure its correct based on field type.
+		if ($this->getOption('selection_mode') == self::SELMODE_SINGLE) {
+			if (!($field instanceof Orb\Form\Field\Radio)) {
+				return false;
+			}
+		} else {
+			if (!($field instanceof Orb\Form\Field\Checkbox)) {
+				return false;
 			}
 		}
 
-		return $arr;
+		return true;
 	}
-
-	
-
-	/**
-	 * Check to see if a certain value is selected
-	 *
-	 * @param string $value
-	 * @return bool
-	 */
-	public function isValueSelected($value)
-	{
-		$val = $this->getFormData();
-		if (!is_array($val)) {
-			return $val == $value;
-		} else {
-			return in_array($value, $val);
-		}
-	}
-
-
-	
-	/**
-	 * Get all the values of the choices.
-	 *
-	 * @return array
-	 */
-	public function getChoiceValues()
-	{
-		$values = $this->getChoices(true);
-		$values = array_values($values);
-
-		return $values;
-	}
-
 
 	public function getDefaultAttributes()
 	{
@@ -130,12 +54,39 @@ class Choice extends Field
 
 		unset($attr['value']);
 
-		if ($this->selection_mode == self::SELMODE_MULTIPLE) {
-			$attr['multiple'] = true;
-		} else {
-			unset($attr['multiple']);
+		if ($this->getOption('selection_mode') == self::SELMODE_MULTIPLE) {
+			$attr['name'] .= '[]';
+			$attr['multiple'] = 'multiple';
+			$attr['size'] = min(4, $this->count());
 		}
 
 		return $attr;
+	}
+
+	
+
+	/**
+	 * Quickly add a choice.
+	 *
+	 * @param string $value
+	 * @param string $label
+	 */
+	public function addChoice($value, $label)
+	{
+		$name = $value;
+
+		$opts = array(
+			'name' => $name,
+			'value' => $value,
+			'label' => $label
+		);
+
+		if ($this->getOption('selection_mode') == self::SELMODE_SINGLE) {
+			$field = new \Orb\Form\Field\Radio($opts);
+		} else {
+			$field = new \Orb\Form\Field\Checkbox($opts);
+		}
+
+		$this->addField($field);
 	}
 }
