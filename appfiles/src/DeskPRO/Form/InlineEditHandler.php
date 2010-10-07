167d6948685e986710974a980f7a573bc93b352a
Christopher Nadeau <chris@nadeau.ws>
2010-10-07 08:01:41 -0400

    - Need to redo some of this helper

--- appfiles/src/DeskPRO/Form/InlineEditHandler.php
+++ appfiles/src/DeskPRO/Form/InlineEditHandler.php
@@ -25,11 +25,33 @@ class InlineEditHandler
 	protected $input_data;
 
 	/**
+	 * A flat array of names we got from input.
+	 * array(person => array(basic => array(fullname => xxx, nickname => xxx)))
+	 * becomes
+	 * array(person.basic.full_name, person.basic.nickname)
+	 * @var array
+	 */
+	protected $got_fields = array();
+
+	/**
 	 * @param array $input_data This is the 'data' item of the incoming request
 	 */
 	public function __construct(array $input_data = array())
 	{
 		$this->input_data = $input_data;
+	}
+
+	protected function _scanInputFieldNames(array $input, $key_parts = array())
+	{
+		foreach ($input as $k => $v) {
+			$key_parts[] = $k;
+			if (is_array($v)) {
+				$this->_scanInputFieldNames($v, $key_parts);
+			} else {
+				$this->got_fields[] = implode('.', $key_parts);
+			}
+			array_pop($key_parts);
+		}
 	}
 
 
@@ -41,67 +63,6 @@ class InlineEditHandler
 	 */
 	public function applyToForm(\Orb\Form\Field\FieldGroup $form)
 	{
-		$data = array();
-
-		foreach ($this->input_data as $edit) {
-			foreach ($edit as $k=>$v) {
-				$data[$k] = $v;
-			}
-		}
-
-		$form->setFormData($data);
-	}
-
-
-
-	/**
-	 * Get the ID of a form group that we'll need to map rendered values back to.
-	 * 
-	 * @param string $name
-	 * @return string
-	 */
-	public function getIdForField($name)
-	{
-		$id = null;
-
-		foreach ($this->input_data as $input_id => $data) {
-			if (isset($data[$name])) {
-				$id = $input_id;
-				break;
-			}
-		}
-
-		return $id;
-	}
-
-
-
-	/**
-	 * Get the names of fields. $base is the base name to get. So null means
-	 * all top-level (group names). If you have a group and want to see which of the
-	 * group were requested, then pass $base of the groupname and all child keys are
-	 * returned etc.
-	 *
-	 * You could use this to lazy-load only form fields that need to be loaded.
-	 *
-	 * @param string $base
-	 * @return array
-	 */
-	public function getFieldNames($base = null)
-	{
-		$names = array();
-
-		foreach ($this->input_data as $data) {
-			if ($base === null) {
-				$names = array_merge($names, array_keys($data));
-			} else {
-				$data = Arrays::getValue($base);
-				if (is_array($data)) {
-					$names = array_merge($names, array_keys($data));
-				}
-			}
-		}
-
-		return $names;
+		$form->setFormData($this->input_data);
 	}
 }
