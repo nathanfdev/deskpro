1bf1509606b2633366f3afd3e1eba0826f9f67bc
Christopher Nadeau <chris@nadeau.ws>
2010-10-07 20:48:22 -0400

    - Empty PeopleSearch controller so routes dont throw errors

---
+++ appfiles/src/Application/TechBundle/Controller/PeopleSearchController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage TechBundle
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris@nadeau.ws>
+ */
+
+namespace Application\TechBundle\Controller;
+
+use \Application\CoreBundle\Entity\Person;
+use \Application\CoreBundle\Entity\FormField;
+use \Application\CoreBundle\Entity\FormFieldAssociation;
+
+/**
+ * Handles searching for people
+ */
+class PeopleSearchController extends AbstractController
+{
+	############################################################################
+	# /tech/people                                                   tech_people
+	############################################################################
+
+	public function indexAction()
+	{
+
+	}
+}
