2f132aa0150106a1ff724b8aa469baa7ed388a87
Christopher Nadeau <chris@nadeau.ws>
2010-11-17 19:13:48 +0000

    - Add some start on the data gen

---
+++ appfiles/src/Application/DevBundle/Controller/DataGeneratorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Application\DevBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DataGeneratorController extends Controller
+{
+    public function indexAction()
+    {
+		$dbname_generated = 'test_database_' . date('Ymd_Hi');
+
+		return $this->render('DevBundle:DataGenerator:index');
+    }
+}
