7fd5d561c843bbb2f547fc447774502e08fc4bac
Christopher Nadeau <chris@nadeau.ws>
2010-11-26 19:56:13 +0000

    - Another

--- index_dev.php
+++ index_dev.php
@@ -3,6 +3,7 @@ require('./dppath.php');
 require(DP_ROOT.'/sys/Kernel.php');
 
 use \DeskPRO\Kernel\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new Kernel('dev', true);
-$kernel->handle()->send();
+$kernel->handle(new Request())->send();
