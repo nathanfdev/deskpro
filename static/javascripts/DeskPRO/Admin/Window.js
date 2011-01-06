322c01d01be42a2689327d8bff5e64fe3924dea8
Christopher Nadeau <chris@nadeau.ws>
2011-01-06 18:20:26 -0500

    - Organize and modify CSS so it's sharable between agent and admin interfaces - New admin layout mimics agent interface - Framework for admin interface: method for page-specific JS, layout options

---
+++ static/javascripts/DeskPRO/Admin/Window.js
@@ -0,0 +1,84 @@
+Orb.createNamespace('DeskPRO.Admin');
+
+/**
+ * Similar in responsibility to the Agent.Window (global object/init etc), but completely
+ * different.
+ *
+ * Within the admin interface, this is the gloabl var DeskPRO_Window.
+ *
+ * If a page defines the special function DeskPRO_Window_Init(), it will be called
+ * automatically once the page is ready.
+ */
+DeskPRO.Admin.Window = new Class({
+	
+	Implements: [Events],
+	
+	registry: {},
+	messageBroker: null,
+
+	initialize: function() {	
+		this._initBasic();
+		this._initWindowInterface();
+		
+		if (typeof window.DeskPRO_Window_Init == 'function') {
+			window.DeskPRO_Window_Init();
+		}
+	},
+	
+	
+	
+	_initBasic: function() {
+		this.messageBroker = new DeskPRO.MessageBroker();
+	},
+	
+	_initWindowInterface: function() {
+		// Set up tabs
+		$('#window_head_top ul.header-tabs li').click(function() {
+			$('#window_head_top ul.header-tabs li').removeClass('on');
+			$(this).addClass('on');
+			
+			$('#window_header_nav .group.on').removeClass('on');
+			$('#window_header_nav .group.' + $(this).data('tab-name')).addClass('on');
+		});
+	},
+	
+	
+	
+	//#################################################################
+	//# Getters
+	//#################################################################
+	
+	getMessageBroker: function() {
+		return this.messageBroker;
+	},
+	
+	//#################################################################
+	//# Global registry
+	//#################################################################
+	
+	/**
+	 * Get a value from the registry.
+	 *
+	 * @param {String} id The ID of the item
+	 * @return mixed
+	 */
+	get: function(id) {
+		if (this.registry[id] === undefined) {
+			return null;
+		}
+		
+		return this.registry[id];
+	},
+	
+	
+	
+	/**
+	 * Add or reset a value in the registry.
+	 *
+	 * @param {String} id The ID of the item
+	 * @param mixed value The value of the item
+	 */
+	set: function(id, value) {
+		this.registry[id] = value;
+	}
+});
