8f1174233e66879d8d7f8b3ef2cb4418ea1ea070
basildeskpro <basil.thoppil@deskpro.com>
2011-01-20 03:55:42 +0530

    twitter build

--- appfiles/src/Application/DeskPRO/Build/Upgrade20110119215705.php
+++ appfiles/src/Application/DeskPRO/Build/Upgrade20110119215705.php
@@ -9,7 +9,13 @@ class Upgrade20110119215705 extends UpgradeAbstract
 {
 	public function step1()
 	{
-		$this->output->write('My Upgrade Code');
+		$this->output->write('New Table for Twitter Admin');
+                try {
+			App::getDb()->exec("CREATE TABLE twitter_accounts (id INT AUTO_INCREMENT NOT NULL, access_token VARCHAR(255) NOT NULL, twitter_handle VARCHAR(255) NOT NULL, PRIMARY KEY(id)) ENGINE = InnoDB");
+		} catch (\Exception $e) {
+			$this->output->writeln("Error: {$e->getMessage()}");
+			return Upgrader::STEP_FAILED;
+		}
 		return Upgrader::STEP_DONE;
 	}
 }
