3143c02220b673a08d34b4b54ad3db917a1e38a7
basildeskpro <basil.thoppil@deskpro.com>
2011-01-21 02:49:34 +0530

    Agent Twit Cntl

--- appfiles/src/Application/AgentBundle/Controller/TwitterController.php
+++ appfiles/src/Application/AgentBundle/Controller/TwitterController.php
@@ -15,9 +15,9 @@ use \Application\DeskPRO\Entity\TicketQueue;
 use \Application\DeskPRO\Entity\Ticket;
 use \Application\DeskPRO\Entity;
 use \Application\DeskPRO\App;
-use \Application\DeskPRO\Twitter;
 use \Orb\Util\Strings;
 use \Orb\Util\Arrays;
+use Zend_Service_Twitter;
 
 /**
  * Handles ticket searches
@@ -26,21 +26,46 @@ class TwitterController extends AbstractController
 {
 	public function indexAction()
 	{
-            $config = array(
-                'callbackUrl' => 'http://basiltest.dyndns.biz/dp/DeskPRO/index_dev.php/agent/twitter/callback',
-                'siteUrl' => 'http://twitter.com/oauth',
-                'consumerKey' => 'MmuQ3021xYehoBzjjd3WFg',
-                'consumerSecret' => 'RORtnJhEUkesx7jDZCSHXonIRjLVAeQ9hIXK8MBf9o'
-            );
-            $twitter = new Twitter($config);
-
-            $twitter->requestAuth();
+            echo "god is love";
             return $this->render('AgentBundle:Twitter:list-blank.twig.html');
 	}
 
 
+        public function mensionsAction($handle){
+                $active_accounts = App::getDb()->fetchAll("
+                    SELECT id,twitter_handle,access_token
+                    FROM twitter_accounts
+                    WHERE deleted IS NULL");
+                $twitter = new Zend_Service_Twitter(array(
+                    'username' => $handle,
+                    'accessToken' => unserialize($active_accounts[0]['access_token'])
+                ));
+                $response  = $twitter->status->userTimeline();
+                //echo "<pre>";print_r($response);
+                return $this->render('AgentBundle:Twitter:tweet-list.twig.html',array(
+                    "tweets" => $response
+                ));
+                /*
+                $style = $this->em->createQuery('
+				SELECT s
+				FROM DeskPRO:Style s
+				WHERE s.id = ?1'
+			)->setParameter(1, $style_id)->getSingleResult();
+                 *
+                 *
+                 */
+        }
+        
+
 
         public function callBackAction(){
+            
+            $twitter = new Twitter($config);
+            $twitter->handleCallback();
+        }
+
+
+        public function favouritesAction(){
             $config = array(
                 'callbackUrl' => 'http://basiltest.dyndns.biz/dp/DeskPRO/index_dev.php/agent/twitter/callback',
                 'siteUrl' => 'http://twitter.com/oauth',
@@ -50,10 +75,13 @@ class TwitterController extends AbstractController
             $twitter = new Twitter($config);
             $twitter->handleCallback();
         }
-	public function queuesPaneAction()
-	{
-		$queues = App::getApi('tickets.queues')->getQueuesForPerson($this->person);
 
+        public function queuesPaneAction()
+	{
+		/*
+                $queues = App::getApi('tickets.queues')->getQueuesForPerson($this->person);
+
+                
 		$order = $this->person->getPref('agent.ui.ticket-queues-order');
 		if ($order) {
 			$queues_unordered = $queues;
@@ -70,10 +98,15 @@ class TwitterController extends AbstractController
 				}
 			}
 		}
+                */
+                $active_accounts = App::getDb()->fetchAll("
+                    SELECT id,twitter_handle
+                    FROM twitter_accounts
+                    WHERE deleted IS NULL");
 
-		return $this->render('AgentBundle:Twitter:pane-queues.twig.html', array(
-			'queues' => $queues
-		));
+                return $this->render('AgentBundle:Twitter:pane-account.twig.html',array(
+                 "active_accounts" => $active_accounts
+                ));
 	}
 
 
