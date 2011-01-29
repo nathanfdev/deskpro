6e6eeca39c9c164c5fa3da7412a81e0b618e00e3
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-01-29 00:50:37 -0500

    Try some new ui elements - Powered by - Links on the side

--- static/javascripts/DeskPRO/Agent/PageFragment/NavPane/Basic.js
+++ static/javascripts/DeskPRO/Agent/PageFragment/NavPane/Basic.js
@@ -1,9 +1,9 @@
 Orb.createNamespace('DeskPRO.Agent.PageFragment.NavPane');
 DeskPRO.Agent.PageFragment.NavPane.Basic = new Class({
 	Extends: DeskPRO.Agent.PageFragment.Basic,
-	
+
 	initPage: function(el) {
-		$('h1', el).click(function() {
+		$('h1, ul.sidebar-links > li', el).click(function() {
 			DeskPRO_Window.runPageRouteFromElement(this);
 		});
 	}
