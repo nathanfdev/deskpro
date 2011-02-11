1f39d7f7e7ad28a76c52412e082ef33feab0f204
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-02-11 17:48:19 -0500

    Add a compat class with definitions from the PHP intl extension as a temporary workaround.
    Symfony only uses Intl in a couple form components. Dev discussions say it will not be required in the future. So until then, lets just fake it.

--- appfiles/sys/Kernel.php
+++ appfiles/sys/Kernel.php
@@ -36,6 +36,8 @@ class Kernel extends \Symfony\Component\HttpKernel\Kernel
 
 	public function boot()
 	{
+		require(DP_ROOT.'/src/Application/DeskPRO/compat.php');
+
 		parent::boot();
 		App::setContainer($this->container, 'default');
 	}
