f42596124bd4be5c2b995f9bf64b9609f14f39ae
Pierre Minnieur <pm@pierre-minnieur.de>
2011-02-18 16:51:17 +0100

    [Twitter] added (empty) controllers for Admin and Agent bundle

---
+++ appfiles/src/Application/AdminBundle/Controller/TwitterController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage AdminBundle
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Pierre Minnieur <pm@pierre-minnieur.de>
+ */
+
+namespace Application\AdminBundle\Controller;
+
+/**
+ * Handles creating/editing of Twitter Accounts
+ */
+class TwitterController extends AbstractController
+{
+}
