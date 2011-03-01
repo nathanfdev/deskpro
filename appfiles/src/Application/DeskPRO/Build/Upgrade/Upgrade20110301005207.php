e24ba281cea471f07d1206c992b4f4039a7d30ad
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-02-28 20:15:19 -0500

    Update needs to drop first

--- appfiles/src/Application/DeskPRO/Build/Upgrade/Upgrade20110301005207.php
+++ appfiles/src/Application/DeskPRO/Build/Upgrade/Upgrade20110301005207.php
@@ -12,6 +12,7 @@ class Upgrade20110301005207 extends UpgradeAbstract
 		$this->output->writeln('Recreate phrase table');
 
 		try {
+			App::getDb()->exec("DROP TABLE  `phrases`");
 			App::getDb()->exec("CREATE TABLE phrases (language_id INT NOT NULL, name VARCHAR(255) NOT NULL, groupname VARCHAR(255) DEFAULT NULL, phrase LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX phrases_language_id_idx (language_id), PRIMARY KEY(language_id, name)) ENGINE = InnoDB");
 		} catch (\Exception $e) {
 			$this->output->writeln("ERROR: {$e->getMessage()}");
