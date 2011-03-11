71b4f8cd22c2c567a95aed9754692eb6fdd5ce24
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-03-11 03:28:06 -0500

    Handle resizing

--- static/javascripts/DeskPRO/Agent/Layout/WindowLayout.js
+++ static/javascripts/DeskPRO/Agent/Layout/WindowLayout.js
@@ -5,10 +5,18 @@ DeskPRO.Agent.Layout.WindowLayout = Orb.Class({
 
 	initialize: function() {
 
+		var self = this;
+
 		this.TOP_POS = 107;
 		this.PADDING = 5; // around each cell, +*2 for each edge of the window
 		this.TABSTRIP_HEIGHT = 32;
 		this.TABSTRIP_W_ALTER = 7; //2px border, 5px margin
+
+		this.winWidth = $(window).width();
+		this.winHeight = $(window).height();
+
+		var docWidth = $(document).width();
+		var docHeight = $(document).height();
 
 		$('#window_panes').css({
 			position: 'absolute',
@@ -19,19 +27,89 @@ DeskPRO.Agent.Layout.WindowLayout = Orb.Class({
 		}).addClass('has-layout').data('layout', this).show();
 
 		this.columns = $('#window_panes > tbody > tr > td');
-		var h = $(document).height();
-		h -= this.TOP_POS;
-		h -= (4*this.PADDING);
+		var h = this.calculateHeight(docHeight);
 
-		var w = $(document).width();
-		var each = Math.floor(w / this.columns.length) - (2*this.PADDING) - (this.columns.length * this.PADDING);
+		var w = docWidth;
+		var each = Math.floor(w / this.columns.length) - (2*this.PADDING) - ((this.columns.length-1) * this.PADDING);
 
-		var self = this;
 		this.columns.each(function() {
 			self.resizeColumn($(this), each, h);
 		});
 
 		this.drawDraggers();
+
+		// Handle window resizes
+		$(window).resize(function() {
+			if (self._resizeTimeout) {
+				window.clearTimeout(self._resizeTimeout);
+			}
+
+			self._resizeTimeout = self.handleResize.delay(400, self);
+		});
+	},
+
+	calculateHeight: function(h) {
+		h -= this.TOP_POS;
+		h -= (4*this.PADDING);
+
+		return h;
+	},
+
+	handleResize: function() {
+
+		var newWidth = $(window).width();
+		var newHeight = $(window).height();
+
+		var oldWidth = this.winWidth;
+		var oldHeight = this.winHeight;
+
+		this.winWidth = newWidth;
+		this.winHeight = newHeight;
+
+		var h = null;
+		if (newHeight != oldHeight) {
+			h = this.calculateHeight(newHeight);
+		}
+
+		var diffWidth = null;
+		var diffWidthEach = null;
+		var diffWidthRemain = null;
+		if (newWidth != oldWidth) {
+			diffWidth = Math.abs(newWidth - oldWidth);
+			diffWidthEach = Math.floor(diffWidth / this.columns.length);
+			diffWidthRemain = diffWidth - (diffWidthEach * this.columns.length);
+
+			if (newWidth < oldWidth) {
+				diffWidth = 0-diffWidth;
+				diffWidthEach = 0-diffWidthEach;
+				diffWidthRemain = 0-diffWidthRemain;
+			}
+		}
+
+		var self = this;
+		this.columns.each(function(i) {
+			var w = null;
+
+			if (diffWidthEach) {
+				w = $(this).width();
+				w += diffWidthEach;
+				if (i == 0 && diffWidthRemain) {
+					w += diffWidthRemain;
+				}
+			}
+
+			self.resizeColumn($(this), w, h);
+		});
+
+		// Also reposition the slider
+		//diffWidthEach
+		var col = this.columns.first();
+		var pos = col.offset();
+		$('#pane_resizer').css({
+			left: pos.left + col.outerWidth() - 3 //3= center it a bit
+		});
+
+		this.fireEvent('resized', [this]);
 	},
 
 	resizeColumn: function(col, w, h) {
