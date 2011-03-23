e8903683eb11bb8c64b8e01438eb6165f84269f6
Pierre Minnieur <pm@pierre-minnieur.de>
2011-03-23 23:36:28 +0100

    [Twitter] extend Zend_Service_Twitter, implemented retweet method

--- appfiles/src/Orb/Service/Twitter/Twitter.php
+++ appfiles/src/Orb/Service/Twitter/Twitter.php
@@ -4,7 +4,7 @@ namespace Orb\Service\Twitter;
 
 use \Application\DeskPRO\App;
 
-class Twitter
+class Twitter extends \Zend_Service_Twitter
 {
 	/**
 	 * @param \Zend_Oauth_Token_Access $accessToken
@@ -17,8 +17,23 @@ class Twitter
 			$consumer = Oauth::getConsumer();
 		}
 
-		return new \Zend_Service_Twitter(array(
+		return new self(array(
 			'accessToken' => $accessToken
 		), $consumer);
 	}
+
+    /**
+     * Retweet a specified status.
+     *
+     * @param  string $id
+     * @return Zend_Rest_Client_Result
+     * @throws Zend_Http_Client_Exception if HTTP request fails or times out
+     */
+    public function statusRetweet($id)
+    {
+        $this->_init();
+        $path = '/1/statuses/retweet/'.$this->_validInteger($id).'.xml';
+        $response = $this->_post($path);
+        return new \Zend_Rest_Client_Result($response->getBody());
+    }
 }
