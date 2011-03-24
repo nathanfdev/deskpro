8244d49cab063a07a86c07607c3f9fce0ac5b859
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-03-24 01:55:55 -0400

    Navigate to first result when pressing enter in the quicksearch

--- static/javascripts/DeskPRO/Agent/WindowElement/MainMenu/SearchBoxType.js
+++ static/javascripts/DeskPRO/Agent/WindowElement/MainMenu/SearchBoxType.js
@@ -8,5 +8,19 @@ DeskPRO.Agent.WindowElement.MainMenu.SearchBoxType = new Class({
 			var search_type = $(this).data('search-type');
 			$('#window_search_type').removeClass('all users tickets').addClass(search_type).data('search-type', search_type);
 		});
+
+		var self = this;
+		$('#search-form').submit(function(ev) {
+			ev.preventDefault();
+			self.navToFirst();
+		});
+	},
+
+	navToFirst: function() {
+		var el = $('#window_search_rusults_wrap .results-list li[data-route]:first');
+		if (el.length) {
+			DeskPRO_Window.runPageRouteFromElement(el);
+			this.closeMenu();
+		}
 	}
 });
