377a3d01fe6dd4f59bf082eb3c7f91f11c110a8e
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-03-24 21:41:37 -0400

    Drag+drop columns in ticket listings

--- static/javascripts/DeskPRO/Agent/PageFragment/ListPane/BasicTicketResults.js
+++ static/javascripts/DeskPRO/Agent/PageFragment/ListPane/BasicTicketResults.js
@@ -35,6 +35,13 @@ DeskPRO.Agent.PageFragment.ListPane.BasicTicketResults = new Class({
 		this.contentWrapper = $('.content:first', this.wrapper);
 		this.barWrapper = $('.ticket-bar:first', this.wrapper);
 
+		//dragtable.makeDraggable($('table:first', this.contentWrapper).get(0));
+		this.listColDrag = new DeskPRO.Agent.PageHelper.ListColDrag({
+			table: $('table:first', this.contentWrapper).get(0),
+			onlyRowSel: '.line-2',
+			onlyRowColOffset: 2
+		});
+
 		var center_id = Orb.getUniqueId('listpane_');
 		var south_id = Orb.getUniqueId('listpane_');
 
