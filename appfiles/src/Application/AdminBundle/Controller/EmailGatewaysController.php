d82a44f865573f7ea0a8ab9b6b4f785e19d43ec2
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-04-02 02:54:45 -0400

    Start of defining gateways in acp

---
+++ appfiles/src/Application/AdminBundle/Controller/EmailGatewaysController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Application\AdminBundle\Controller;
+
+use \Application\DeskPRO\App;
+use \Application\DeskPRO\Entity;
+use \Application\AdminBundle\Form\EditGatewayForm;
+use \Orb\Util\Arrays;
+use \Symfony\Component\Form;
+
+class EmailGatewaysController extends AbstractController
+{
+	############################################################################
+	# list
+	############################################################################
+
+	/**
+	 * Shows the main listing of gateways
+	 */
+	public function listAction()
+	{
+		$this->rememberLastPage();
+
+		$all_gateways = $this->em->createQuery("
+			SELECT g
+			FROM DeskPRO:EmailGateways g
+			ORDER BY g.name ASC
+		")->getResult();
+
+		return $this->render('AdminBundle:EmailGateways:list.html.twig', array(
+			'all_gateways' => $all_gateways
+		));
+	}
+
+	############################################################################
+	# edit
+	############################################################################
+
+	/**
+	 * Edit a gateway
+	 */
+	public function editAction($gateway_id)
+	{
+		if (!$gateway_id) {
+			$gateway = new Entity\EmailGateway();
+		} else {
+			$gateway = App::getEntityRepository('DeskPRO:EmailGateway')->find($gateway_id);
+		}
+
+		$form = EditGatewayForm::create($this->get('form.context'), 'gateway', array(gateway => $gateway));
+		$form->bind($this->get('request'), $gateway);
+
+		$is_edited = false;
+		$row_html = false;
+		if ($this->in->getBool('process')) {
+			$is_edited = true;
+			App::getOrm()->persist($gateway);
+			App::getOrm()->flush();
+
+			$row_html = $this->renderView('AdminBundle:EmailGateways:list-row.html.twig', array('gateway' => $gateway));
+
+			// Recreate form because parent_id field cant be changed, so we need to get rid of it
+			$form = EditDepartmentForm::create($this->get('form.context'), 'gateway', array('gateway' => $gateway));
+			$form->setData($gateway);
+		}
+
+		return $this->render('AdminBundle:EmailGateways:edit.html.twig', array(
+			'gateway' => $gateway,
+			'form'      => $form,
+			'is_edited' => $is_edited,
+			'row_html'  => $row_html
+		));
+	}
+}
---
+++ appfiles/src/Application/AdminBundle/Form/EditGatewayForm.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage AdminBundle
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris.nadeau@deskpro.com>
+ */
+
+namespace Application\AdminBundle\Form;
+
+use \Application\DeskPRO\App;
+use \Application\DeskPRO\Entity;
+
+use \Orb\Util\Arrays;
+
+use \Symfony\Component\Form;
+
+class EditGatewayForm extends \Symfony\Component\Form\Form
+{
+	protected function configure()
+	{
+		$this->addRequiredOption('gateway');
+
+		$this->add(new Form\TextField('title'));
+		$this->add(new Form\TextField('address'));
+	}
+}
