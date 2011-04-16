72e95981dee1d3051e92fa5fe5dcacb815fbbb0e
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-04-16 03:56:30 -0400

    Clean HTML templates

---
+++ appfiles/src/Application/DevBundle/Controller/TestEmailController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\DevBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use \Application\DeskPRO\App;
+use \Application\DeskPRO\Entity;
+use \Application\DeskPRO\Entity\IdeaCategory;
+
+use \Orb\Util\Strings;
+
+class TestEmailController extends Controller
+{
+    public function userNewTicketAction()
+    {
+		$ticket = App::getEntityRepository('DeskPRO:Ticket')->find(3);
+		$new_message = $ticket->messages[0];
+
+		return $this->render("DeskPRO:emails_user:new-ticket.html.twig", array(
+			'ticket' => $ticket,
+			'new_message' => $new_message,
+			'settings' => App::get(App::SERVICE_SETTINGS)
+		));
+    }
+}
