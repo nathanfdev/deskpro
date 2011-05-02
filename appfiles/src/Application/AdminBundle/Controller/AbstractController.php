4d499720cd5aae734f14e69d01109e4f047add17
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-05-02 14:42:08 -0400

    Admin shold use agent login controller

--- appfiles/src/Application/AdminBundle/Controller/AbstractController.php
+++ appfiles/src/Application/AdminBundle/Controller/AbstractController.php
@@ -32,7 +32,7 @@ abstract class AbstractController extends \Application\DeskPRO\Controller\Abstra
 			} else {
 				$return = $this->request->getRequestUri();
 			}
-			return $this->redirect($this->get('router')->generate('user_login', array('return' => $return)));
+			return $this->redirect($this->get('router')->generate('agent_login', array('return' => $return)));
 		}
 
 		if (!$this->_userHasPermissions()) {
