93aef216de85b497af2de2227d75c10c95f7bb16
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-05-18 02:52:57 -0400

    Add placeholder 'settings and features' page

---
+++ appfiles/src/Application/AdminBundle/Controller/TicketFeaturesController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage AdminBundle
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris.nadeau@deskpro.com>
+ */
+
+namespace Application\AdminBundle\Controller;
+
+use \Application\DeskPRO\App;
+use \Application\DeskPRO\Entity;
+
+use \Application\AdminBundle\Form\EditTicketPriorityType;
+
+/**
+ * Misc
+ */
+class TicketFeaturesController extends AbstractController
+{
+	############################################################################
+	# index
+	############################################################################
+
+	public function indexAction()
+	{
+ 		return $this->render('AdminBundle:TicketFeatures:index.html.twig', array(
+			
+		));
+	}
+}
