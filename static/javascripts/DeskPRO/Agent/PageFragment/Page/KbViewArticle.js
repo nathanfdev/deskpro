dff9762f39f391944f7ac50350cb1d05051fa52a
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-05-24 01:15:56 -0400

    Add some JS functionality for view page

--- static/javascripts/DeskPRO/Agent/PageFragment/Page/KbViewArticle.js
+++ static/javascripts/DeskPRO/Agent/PageFragment/Page/KbViewArticle.js
@@ -9,12 +9,19 @@ DeskPRO.Agent.PageFragment.Page.KbViewArticle = new Class({
 	article_id: null,
 
 	initPage: function(el) {
+
+		this.parent(el);
 		this.wrapper = el;
 
 		this.article_id = this.getMetaData('article_id');
 
-		var self = this;
+		this._initBasic();
+		this._initMenus();
+		this._initLabels();
+	},
 
+
+	_initBasic: function() {
 		var self = this;
 		$('.edit-trigger', this.wrapper).click(function() {
 			DeskPRO_Window.runPageRoute('kb_article_edit:' + BASE_URL + 'agent/kb/article/' + self.article_id);
@@ -25,5 +32,81 @@ DeskPRO.Agent.PageFragment.Page.KbViewArticle = new Class({
 			DeskPRO_Window.runPageRoute('kb_article_edit:' + BASE_URL + 'agent/kb/article/' + self.article_id + '?do_validate=1');
 			DeskPRO_Window.removePage(self);
 		});
-	}
+
+
+		// Name is editable
+		var name = $('h3.title.editable:first', el);
+		if (!name.attr('id')) {
+			name.attr('id', Orb.getUniqueId());
+		}
+
+		var editable = new DeskPRO.Form.InlineEdit({
+			baseElement: el,
+			ajax: {
+				url: BASE_URL + 'agent/kb/' + this.meta.article_id + '/ajax-save'
+			}
+		});
+	},
+
+	//#################################################################
+	//# Menus
+	//#################################################################
+
+	_initMenus: function() {
+		this.statusMenu = new DeskPRO.UI.Menu({
+			triggerElement: $('.menu-trigger.status:first', this.wrapper),
+			menuElement: $('.menu.status:first', this.wrapper)
+		});
+	},
+
+
+	//#################################################################
+	//# Labels
+	//#################################################################
+
+	labelsList: null,
+	_initLabels: function() {
+		// Tags
+		this.labelsList = $(".kb-tags ul", this.contentWrapper);
+		this.labelsTagit = this.labelsList.tagit({
+			availableTags: this.getMetaData('labelsAutocompleteUrl'),
+			enableBackspace: false,
+			fieldName: 'labels',
+			onchange: this.saveLabels.bind(this)
+		});
+	},
+
+	_saveLabelsTimeout: null,
+	saveLabels: function() {
+		if (this.changeManager.hasChanges()) {
+			// If change manager has changes, we dont save new/removed
+			// tags
+			return;
+		}
+
+		if (this._saveLabelsTimeout) {
+			window.clearTimeout(this._saveLabelsTimeout);
+		}
+
+		this._saveLabelsTimeout = this._doSaveLabels.delay(2000, this);
+	},
+
+	_doSaveLabels: function() {
+		var data = $(':input', this.labelsList).serializeArray();
+
+		$.ajax({
+			url: this.getMetaData('labelsSaveUrl'),
+			type: 'POST',
+			context: this,
+			data: data,
+			dataType: 'json',
+			success: function(data) {
+				this._handleSaveLabelsSuccess(data);
+			}
+		});
+	},
+
+	_handleSaveLabelsSuccess: function(data) {
+
+	},
 });
