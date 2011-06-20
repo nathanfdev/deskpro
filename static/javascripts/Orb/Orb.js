bca05194fc725c48b47512be20a4e976b1650b8d
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-06-20 11:58:09 -0400

    Update resize event

--- static/javascripts/Orb/Orb.js
+++ static/javascripts/Orb/Orb.js
@@ -15,12 +15,12 @@ if (window.console === undefined) {
 Orb.createNamespace = function(namespace, obj) {
 	var parts = namespace.split('.');
 	var objcheck = window;
-	
+
 	parts.forEach(function(part) {
 		if (!objcheck[part]) {
 			objcheck[part] = {};
 		}
-		
+
 		objcheck = objcheck[part];
 	});
 };
@@ -34,20 +34,20 @@ Orb.createNamespace = function(namespace, obj) {
  * @return {Object}
  */
 Orb.getNamespacedObject = function(fullname) {
-	
+
 	var obj = window;
-	
+
 	fullname_parts = fullname.split('.');
-	
+
 	var part = null;
 	while (part = fullname_parts.shift()) {
 		if (obj[part] === undefined) {
 			console.warn('Orb.getNamespacedObject(%s) is an invalid name', fullname);
 		}
-		
+
 		obj = obj[part];
 	}
-	
+
 	return obj;
 };
 
@@ -64,19 +64,19 @@ Orb.getUniqueId = function(prefix) {
 	if (!prefix) {
 		prefix = '';
 	}
-	
+
 	var id = '';
 	do {
 		id = prefix + Orb.uuid();
 	} while (document.getElementById(id));
-	
+
 	return id;
 };
 
 
 
 /**
- * Get a unique ID that can be used app-wide. This is completely different from element 
+ * Get a unique ID that can be used app-wide. This is completely different from element
  * ID's; these should be used as other JS-type identifiers.
  *
  * @return {String}
@@ -98,7 +98,7 @@ Orb.getEl = function(el) {
 	if (typeOf(el) == 'element') {
 		return el;
 	}
-	
+
 	return document.getElementById(el);
 };
 $el = function(el) { return Orb.getEl(el); };
@@ -125,15 +125,15 @@ Orb.mouseInElement = function(mouseX, mouseY, el) {
 	var pos = el.offset();
 	var width = el.outerWidth();
 	var height = el.outerHeight();
-	
+
 	if (mouseX < pos.left || mouseX > pos.left+width) {
 		return false;
 	}
-	
+
 	if (mouseY < pos.top || mouseY > pos.top+height) {
 		return false;
 	}
-	
+
 	return true;
 };
 
@@ -149,7 +149,7 @@ Orb.findHighestZindex = function(els) {
 	if (!els) {
 		els = $('body > *');
 	}
-	
+
 	var highest = 0;
 	els.each(function() {
 		var z = parseInt($(this).css('z-index'));
@@ -157,7 +157,7 @@ Orb.findHighestZindex = function(els) {
 			highest = z;
 		}
 	});
-	
+
 	return highest;
 };
 
@@ -165,7 +165,7 @@ Orb.findHighestZindex = function(els) {
 
 /**
  * Escape special HTML characters.
- * 
+ *
  * @param string
  */
 Orb.escapeHtml = function(string) {
@@ -207,7 +207,7 @@ Orb.linkUrls = function(string) {
 Orb.resourceLoader = {
 	batches: {},
 	batchesCallback: {},
-	
+
 	/**
 	 * Load a new Javascript source
 	 *
@@ -235,9 +235,9 @@ Orb.resourceLoader = {
 			url: url
 		}], callback);
 	},
-	
-	
-	
+
+
+
 	/**
 	 * Load a number of resources all at once, and be notified when they've all finished
 	 * loading.
@@ -249,21 +249,21 @@ Orb.resourceLoader = {
 	 * @param {Function} callback The function to execute when all files have been loaded
 	 */
 	loadBatch: function(resources, callback) {
-	
+
 		var batchId = Orb.uuid();
 		var head = $('head');
-		
+
 		this.batches[batchId] = [];
 		this.batchesCallback[batchId] = callback;
-		
+
 		var res = null;
 		while (res = resources.shift()) {
 			var resourceId = Orb.uuid();
-			
+
 			var fn = function() {
 				Orb.resourceLoader._resourceDoneLoading(batchId, resourceId);
 			};
-			
+
 			if (res.type == 'script') {
 				var tag = document.createElement('script');
 				tag.type = "text/javascript";
@@ -274,35 +274,35 @@ Orb.resourceLoader = {
 				tag.type = 'text/css';
 				tag.href = res.url;
 				tag.media = "screen";
-				
+
 				if (res.media != undefined) {
 					tag.media = res.media;
 				}
 			}
-			
+
 			tag.onreadystatechange= function () {
 				if (this.readyState == 'complete') fn();
 			}
 			tag.onload = fn;
-			
+
 			this.batches[batchId].push(resourceId);
 		}
-		
+
 	},
-	
+
 	_resourceDoneLoading: function(batchId, resourceId) {
 		if (this.batches[batchId] == undefined) {
 			return false;
 		}
-		
+
 		this.batches[batchId].erase(resourceId);
-		
+
 		if (!this.batches[batchId].length) {
 			var callback = this.batchesCallback[batchId];
-			
+
 			delete this.batches[batchId];
 			delete this.batchesCallback[batchId];
-			
+
 			callback();
 		}
 	}
@@ -340,7 +340,7 @@ $.fn.single_double_click = function(single_click_callback, double_click_callback
 	                clicks = 0;
 	            }, timeout);
 	        });
-	
+
 	    } else {
 	        $(this).bind("click", function(event) {
 	            clicks++;
@@ -361,103 +361,205 @@ $.fn.single_double_click = function(single_click_callback, double_click_callback
 
 
 
-// Resize watcher. Elements dont have a resize event, so the only way to know
-// when they've changed is to poll them and compare current w/h with a previously
-// recorded w/h.
-//
-// This event handler is based on: http://github.com/cowboy/jquery-resize/
+// resize event -
+// Based on http://benalman.com/code/projects/jquery-resize/examples/resize/
 (function($,window,undefined){
-	var elems = $([]);
+  '$:nomunge'; // Used by YUI compressor.
 
-	var jq_resize = $.resize = $.extend( $.resize, {} );
-	jq_resize.delay = 300;
-	
-    var timeout_id;
+  // A jQuery object containing all non-window elements to which the resize
+  // event is bound.
+  var elems = $([]),
 
-	$.event.special.resize = {
+    // Extend $.resize if it already exists, otherwise create it.
+    jq_resize = $.resize = $.extend( $.resize, {} ),
 
-		setup: function() {
-			// window has its own resize event
-			if (this.setTimeout) { return false; }
+    timeout_id,
 
-			var elem = $(this);
+    // Reused strings.
+    str_setTimeout = 'setTimeout',
+    str_resize = 'resize',
+    str_data = str_resize + '-special-event',
+    str_delay = 'delay',
+    str_throttle = 'throttleWindow';
 
-			elems = elems.add( elem );
+  // Property: jQuery.resize.delay
+  //
+  // The numeric interval (in milliseconds) at which the resize event polling
+  // loop executes. Defaults to 250.
 
-			$.data( this, 'resize-special-event', { w: elem.width(), h: elem.height() } );
+  jq_resize[ str_delay ] = 325;
 
-			if (elems.length === 1) {
-				loopy();
-			}
-		},
+  // Property: jQuery.resize.throttleWindow
+  //
+  // Throttle the native window object resize event to fire no more than once
+  // every <jQuery.resize.delay> milliseconds. Defaults to true.
+  //
+  // Because the window object has its own resize event, it doesn't need to be
+  // provided by this plugin, and its execution can be left entirely up to the
+  // browser. However, since certain browsers fire the resize event continuously
+  // while others do not, enabling this will throttle the window resize event,
+  // making event behavior consistent across all elements in all browsers.
+  //
+  // While setting this property to false will disable window object resize
+  // event throttling, please note that this property must be changed before any
+  // window object resize event callbacks are bound.
 
-		teardown: function() {
-			if (!this.setTimeout) { return false; }
+  jq_resize[ str_throttle ] = false;
 
-			var elem = $(this);
-			elems = elems.not(elem);
+  // Event: resize event
+  //
+  // Fired when an element's width or height changes. Because browsers only
+  // provide this event for the window element, for other elements a polling
+  // loop is initialized, running every <jQuery.resize.delay> milliseconds
+  // to see if elements' dimensions have changed. You may bind with either
+  // .resize( fn ) or .bind( "resize", fn ), and unbind with .unbind( "resize" ).
+  //
+  // Usage:
+  //
+  // > jQuery('selector').bind( 'resize', function(e) {
+  // >   // element's width or height has changed!
+  // >   ...
+  // > });
+  //
+  // Additional Notes:
+  //
+  // * The polling loop is not created until at least one callback is actually
+  //   bound to the 'resize' event, and this single polling loop is shared
+  //   across all elements.
+  //
+  // Double firing issue in jQuery 1.3.2:
+  //
+  // While this plugin works in jQuery 1.3.2, if an element's event callbacks
+  // are manually triggered via .trigger( 'resize' ) or .resize() those
+  // callbacks may double-fire, due to limitations in the jQuery 1.3.2 special
+  // events system. This is not an issue when using jQuery 1.4+.
+  //
+  // > // While this works in jQuery 1.4+
+  // > $(elem).css({ width: new_w, height: new_h }).resize();
+  // >
+  // > // In jQuery 1.3.2, you need to do this:
+  // > var elem = $(elem);
+  // > elem.css({ width: new_w, height: new_h });
+  // > elem.data( 'resize-special-event', { width: elem.width(), height: elem.height() } );
+  // > elem.resize();
 
-			elem.removeData('resize-special-event');
+  $.event.special[ str_resize ] = {
 
-			// If this is the last element removed, stop the polling loop.
-			if (!elems.length) {
-				clearTimeout(timeout_id);
-				elems = $([]);
-			}
-		},
+    // Called only when the first 'resize' event callback is bound per element.
+    setup: function() {
+      // Since window has its own native 'resize' event, return false so that
+      // jQuery will bind the event using DOM methods. Since only 'window'
+      // objects have a .setTimeout method, this should be a sufficient test.
+      // Unless, of course, we're throttling the 'resize' event for window.
+      if ( !jq_resize[ str_throttle ] && this[ str_setTimeout ] ) { return false; }
 
-		add: function( handleObj ) {
-			if (this['setTimeout'] ) { return false; }
+      var elem = $(this);
 
-			var old_handler;
+      // Add this element to the list of internal elements to monitor.
+      elems = elems.add( elem );
 
-			// The new_handler function is executed every time the event is triggered.
-			// This is used to update the internal element data store with the width
-			// and height when the event is triggered manually, to avoid double-firing
-			// of the event callback. See the "Double firing issue in jQuery 1.3.2"
-			// comments above for more information.
+      // Initialize data store on the element.
+      $.data( this, str_data, { w: elem.width(), h: elem.height() } );
 
-			function new_handler( e, w, h ) {
-				var elem = $(this),
-				data = $.data(this, 'resize-special-event');
+      // If this is the first element added, start the polling loop.
+      if ( elems.length === 1 ) {
+        loopy();
+      }
+    },
 
-				// If called from the polling loop, w and h will be passed in as
-				// arguments. If called manually, via .trigger( 'resize' ) or .resize(),
-				// those values will need to be computed.
-				data.w = w !== undefined ? w : elem.width();
-				data.h = h !== undefined ? h : elem.height();
+    // Called only when the last 'resize' event callback is unbound per element.
+    teardown: function() {
+      // Since window has its own native 'resize' event, return false so that
+      // jQuery will unbind the event using DOM methods. Since only 'window'
+      // objects have a .setTimeout method, this should be a sufficient test.
+      // Unless, of course, we're throttling the 'resize' event for window.
+      if ( !jq_resize[ str_throttle ] && this[ str_setTimeout ] ) { return false; }
 
-				old_handler.apply( this, arguments );
-			};
+      var elem = $(this);
 
-			old_handler = handleObj.handler;
-			handleObj.handler = new_handler;
-		}
+      // Remove this element from the list of internal elements to monitor.
+      elems = elems.not( elem );
 
-	};
+      // Remove any data stored on the element.
+      elem.removeData( str_data );
 
-	function loopy() {
-		timeout_id = window.setTimeout(function() {
+      // If this is the last element removed, stop the polling loop.
+      if ( !elems.length ) {
+        clearTimeout( timeout_id );
+      }
+    },
 
-			// Iterate over all elements to which the 'resize' event is bound.
-			elems.each(function(){
-				var elem = $(this),
-				width = elem.width(),
-				height = elem.height(),
-				data = $.data( this, 'resize-special-event' );
-				if (!data) return;
+    // Called every time a 'resize' event callback is bound per element (new in
+    // jQuery 1.4).
+    add: function( handleObj ) {
+      // Since window has its own native 'resize' event, return false so that
+      // jQuery doesn't modify the event object. Unless, of course, we're
+      // throttling the 'resize' event for window.
+      if ( !jq_resize[ str_throttle ] && this[ str_setTimeout ] ) { return false; }
 
-				// If element size has changed since the last time, update the element
-				// data store and trigger the 'resize' event.
-				if ( width !== data.w || height !== data.h ) {
-					elem.trigger('resize', [ data.w = width, data.h = height ]);
-				}
+      var old_handler;
 
-			});
+      // The new_handler function is executed every time the event is triggered.
+      // This is used to update the internal element data store with the width
+      // and height when the event is triggered manually, to avoid double-firing
+      // of the event callback. See the "Double firing issue in jQuery 1.3.2"
+      // comments above for more information.
 
-			loopy();
+      function new_handler( e, w, h ) {
+        var elem = $(this),
+          data = $.data( this, str_data );
 
-		}, jq_resize.delay);
-	};
+        // If called from the polling loop, w and h will be passed in as
+        // arguments. If called manually, via .trigger( 'resize' ) or .resize(),
+        // those values will need to be computed.
+        data.w = w !== undefined ? w : elem.width();
+        data.h = h !== undefined ? h : elem.height();
+
+        old_handler.apply( this, arguments );
+      };
+
+      // This may seem a little complicated, but it normalizes the special event
+      // .add method between jQuery 1.4/1.4.1 and 1.4.2+
+      if ( $.isFunction( handleObj ) ) {
+        // 1.4, 1.4.1
+        old_handler = handleObj;
+        return new_handler;
+      } else {
+        // 1.4.2+
+        old_handler = handleObj.handler;
+        handleObj.handler = new_handler;
+      }
+    }
+
+  };
+
+  function loopy() {
+
+    // Start the polling loop, asynchronously.
+    timeout_id = window[ str_setTimeout ](function(){
+
+      // Iterate over all elements to which the 'resize' event is bound.
+      elems.each(function(){
+        var elem = $(this),
+          width = elem.width(),
+          height = elem.height(),
+          data = $.data( this, str_data );
+
+		  console.log('check %o', elem);
+
+        // If element size has changed since the last time, update the element
+        // data store and trigger the 'resize' event.
+        if ( width !== data.w || height !== data.h ) {
+          elem.trigger( str_resize, [ data.w = width, data.h = height ] );
+        }
+
+      });
+
+      // Loop.
+      loopy();
+
+    }, jq_resize[ str_delay ] );
+
+  };
 
 })(jQuery,this);
