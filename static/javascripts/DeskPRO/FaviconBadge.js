2b5702b08da4bd5d5f8e3ea9391c50e19724c667
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-06-27 23:14:51 -0400

    Animate favicon (blink) when it's updated - Animation stops when tab is focused (supported by FF4 and Chrome), or when a mousemove is detected in the window

--- static/javascripts/DeskPRO/FaviconBadge.js
+++ static/javascripts/DeskPRO/FaviconBadge.js
@@ -14,15 +14,10 @@ DeskPRO.FaviconBadge = new Orb.Class({
 		this.badgeEl = null;
 	},
 
-	updateBadge: function(num) {
+	updateBadge: function(num, do_animate) {
 		if (!this.supported) return;
 		
-		var canvas = document.createElement('canvas');
 		var img = document.createElement('img');
-
-		canvas.height = canvas.width = 16;
-		var canvasContext = canvas.getContext('2d');
-
 		var self = this;
 
 		// We have only two digits to play with
@@ -35,25 +30,102 @@ DeskPRO.FaviconBadge = new Orb.Class({
 			return;
 		}
 
-		img.onload = function() {
-			canvasContext.drawImage(this, 0, 0);
-			canvasContext.font = '11px "helvetica", sans-serif';
+		if (self.currentCancel) self.currentCancel();
 
-			canvasContext.fillStyle = 'rgba(255, 255, 255, 0.75)';
-			canvasContext.fillRect(3, 6, 12, 10);
+		// With zero we put the old one back
+		if (num == 0) {
+			do_animate = false;
+			self.badgeEl.remove();
+		}
 
-			canvasContext.fillStyle = '#000';
-			canvasContext.fillText(num, 4, 16);
-
-			if (self.badgeEl) {
-				self.badgeEl.remove();
-			}
-
-			self.badgeEl = self.faviconEl.clone();
-			self.badgeEl.data('num', num);
-			self.badgeEl.get(0).href = canvas.toDataURL('image/png');
-			$('body').append(self.badgeEl);
-		};
 		img.src = this.faviconEl.attr('href');
+		img.onload = function() {
+			function badge1() {
+				if (self.badgeEl) {
+					self.badgeEl.remove();
+				}
+				var canvas = self.drawCanvus(img, num);
+				self.badgeEl = self.faviconEl.clone();
+				self.badgeEl.data('num', num);
+				self.badgeEl.get(0).href = canvas.toDataURL('image/png');
+				$('body').append(self.badgeEl);
+			}
+			function badge2() {
+				if (self.badgeEl) {
+					self.badgeEl.remove();
+				}
+				var canvas = self.drawCanvus(img, num, true);
+				self.badgeEl = self.faviconEl.clone();
+				self.badgeEl.data('num', num).addClass('alt');
+				self.badgeEl.get(0).href = canvas.toDataURL('image/png');
+				$('body').append(self.badgeEl);
+			};
+
+			function alternate() {
+
+				if (runs++ > 10) {
+					cancelAnimation();
+					return;
+				}
+
+				if (!self.badgeEl) {
+					badge1();
+				} else {
+					if (self.badgeEl.is('.alt')) {
+						badge1();
+					} else {
+						badge2();
+					}
+				}
+
+				timeout = window.setTimeout(function() { alternate() }, 1000);
+			};
+
+			function cancelAnimation() {
+				if (timeout) window.clearTimeout(timeout);
+				badge1();
+
+				$(window).unbind('focus', cancelAnimation);
+				$(window).unbind('mousemove', cancelAnimation);
+
+				self.currentCancel = null;
+			};
+
+			self.currentCancel = cancelAnimation;
+
+			if (do_animate) {
+
+				var timeout = null;
+				var runs = 0;
+				alternate();
+
+				$(window).bind('focus', cancelAnimation);
+				$(window).bind('mousemove', cancelAnimation);
+			} else {
+				badge1();
+			}
+		};
+	},
+
+	drawCanvus: function(img, num, alt) {
+		var canvas = document.createElement('canvas');
+		canvas.height = canvas.width = 16;
+
+		var canvasContext = canvas.getContext('2d');
+
+		canvasContext.drawImage(img, 0, 0);
+		canvasContext.font = '11px "helvetica", sans-serif';
+
+		if (alt) {
+			canvasContext.fillStyle = 'rgba(255, 255, 255, 1)';
+		} else {
+			canvasContext.fillStyle = 'rgba(255, 255, 255, 0.75)';
+		}
+		canvasContext.fillRect(3, 6, 12, 10);
+
+		canvasContext.fillStyle = '#000';
+		canvasContext.fillText(num, 4, 16);
+
+		return canvas;
 	}
 });
