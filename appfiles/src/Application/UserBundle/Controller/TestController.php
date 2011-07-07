d2b99253671a1ba10a8aee6d1f1ba417fcecae5e
Christopher Nadeau <chris@nadeau.ws>
2011-07-07 14:25:34 -0400

    Add new layout more suitable to portal design, and some example block placeholders

---
+++ appfiles/src/Application/UserBundle/Controller/TestController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage UserBundle
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris.nadeau@deskpro.com>
+ */
+
+namespace Application\UserBundle\Controller;
+
+use Application\DeskPRO\App;
+use Application\DeskPRO\Entity;
+
+use Orb\Util\Arrays;
+
+class TestController extends AbstractController
+{
+	public function indexAction()
+	{
+		return $this->render('UserBundle:Test:index.html.twig');
+	}
+}
