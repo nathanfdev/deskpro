7e9e315d6b37dfad93615d200f5052c90732d74c
Christopher Nadeau <chris@nadeau.ws>
2011-07-11 16:54:18 -0400

    Rename template

--- appfiles/src/Application/DeskPRO/PageDisplay/Item/Portal/OmniSearch.php
+++ appfiles/src/Application/DeskPRO/PageDisplay/Item/Portal/OmniSearch.php
@@ -22,7 +22,7 @@ class OmniSearch extends PortalItemAbstract
 {
 	public function getHtml()
 	{
-		$html = $this->renderView('UserBundle:Portal:omnisearch-content.html.twig', array(
+		$html = $this->renderView('UserBundle:Portal:omnisearch-topsection.html.twig', array(
 			'section' => $this->section,
 			'options' => $this->options
 		));
