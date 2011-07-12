4e5a06e98bec9a97d62dfd8ba8103baa84de9483
Christopher Nadeau <chris@nadeau.ws>
2011-07-12 00:30:45 -0400

    Some changes around the ideas view

--- static/javascripts/DeskPRO/User/ElementHandler/Ideas.js
+++ static/javascripts/DeskPRO/User/ElementHandler/Ideas.js
@@ -5,43 +5,10 @@ DeskPRO.User.ElementHandler.Ideas = new Orb.Class({
 	Extends: DeskPRO.User.ElementHandler.ElementHandlerAbstract,
 
 	init: function() {
-		this.contentList = $('.content-list:first > ul', this.el);
-		this.catSwitcher = $('select.ideas-cat-choose:first', this.el);
-		this.statusSwitcher = $('select.ideas-status-choose:first', this.el);
-
-		if (this.catSwitcher.length) {
-			this._initCatSwitcher();
-		}
-	},
-
-	_initCatSwitcher: function() {
-		var self = this;
-
-		function getChoices() {
-			return {
-				'status': self.statusSwitcher.val(),
-				'category_id': self.catSwitcher.val()
-			}
-		}
-		
-		this.catSwitcher.change(function() {
-			var c = getChoices();
-			self.loadList(c.status, c.category_id);
-		});
-		this.statusSwitcher.change(function() {
-			var c = getChoices();
-			self.loadList(c.status, c.category_id);
-		});
-	},
-
-	loadList: function(status, category_id) {
-		$.ajax({
-			url: BASE_URL + 'feedback/quick-browser/' + status + '/' + category_id + '?_partial',
-			dataType: 'html',
-			context: this,
-			success: function(html) {
-				this.contentList.empty().html(html);
-			}
+		var triggers = $('.heading-tabs li', this.el);
+		this.headerTabs = new DeskPRO.UI.SimpleTabs({
+			triggerElements: $('.heading-tabs li', this.el),
+			context: $('.tab-contents', this.el)
 		});
 	}
 });
