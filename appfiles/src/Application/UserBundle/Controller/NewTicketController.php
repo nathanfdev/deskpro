6a718d7a47cada1df488ca6583098bc9a3f97986
Christopher Nadeau <chris@nadeau.ws>
2011-07-15 05:47:51 -0400

    Ticket designer stuff on newticket

--- appfiles/src/Application/UserBundle/Controller/NewTicketController.php
+++ appfiles/src/Application/UserBundle/Controller/NewTicketController.php
@@ -80,11 +80,26 @@ class NewTicketController extends AbstractController
 			}
 		}
 
+		// Custom fields
+		$ticket_field_defs = App::getApi('custom_fields.tickets')->getEnabledFields();
+		$ticket_data_structured = App::getApi('custom_fields.util')->createDataHierarchy(array(), $ticket_field_defs);
+
+		// We use this fieldgroup so the form names are part of custom_fields array: custom_fields[field_1] etc
+		// So dont remove it even though it looks like it's not used! :-)
+		$custom_fields_form = $this->get('form.factory')->createNamedBuilder('form', 'custom_fields');
+		$custom_fields = App::getApi('custom_fields.tickets')->getFieldsDisplayArray($ticket_field_defs, $ticket_data_structured, $custom_fields_form);
+
+		$ticket_display = new \Application\DeskPRO\PageDisplay\Page\TicketPageZoneCollection('user');
+		$ticket_display->addPagesFromDb();
+		$ticket_display_js = "window.DESKPRO_TICKET_DISPLAY = " . $ticket_display->compileJs() . ";";
+
 		return $this->render('UserBundle:NewTicket:new-ticket.html.twig', array(
 			'ticket_options' => $newticket_formtype->getTicketOptions(),
 			'newticket_formtype' => $newticket_formtype,
 			'custom_fields' => $newticket_formtype->getTicketFields(),
 			'form' => $form->createView(),
+			'custom_fields' => $custom_fields,
+			'ticket_display_js' => $ticket_display_js,
 		));
     }
 
--- static/javascripts/DeskPRO/User/ElementHandler/NewTicket.js
+++ static/javascripts/DeskPRO/User/ElementHandler/NewTicket.js
@@ -5,7 +5,50 @@ DeskPRO.User.ElementHandler.NewTicket = new Orb.Class({
 	Extends: DeskPRO.User.ElementHandler.ElementHandlerAbstract,
 
 	init: function() {
+		this.depSelect = $('select.department_id', this.el);
+		this.departmentId = 0;
 
+		var self = this;
+		this.depSelect.change(function() {
+			self.setDepartment($(this).val());
+		});
+	},
+
+	setDepartment: function(department_id) {
+		this.clearAll();
+
+		if (department_id == this.departmentId) {
+			return;
+		}
+
+		this.departmentId = department_id;
+
+		if (!window.DESKPRO_TICKET_DISPLAY || !window.DESKPRO_TICKET_DISPLAY[department_id]) {
+			return;
+		}
+
+		var depItems = window.DESKPRO_TICKET_DISPLAY[department_id];
+		console.log('depItems %o', depItems);
+		
+		Array.each(depItems, function(item) {
+			var itemId = this.getItemId(item);
+			var itemEl = $('.' + itemId + ':first');
+			itemEl.show();
+		}, this);
+	},
+
+	clearAll: function() {
+		$('.ticket-display-field').hide();
+	},
+
+	getItemId: function(item) {
+
+		var itemId = item.item_type;
+		if (item.item_id) {
+			itemId += '_' + item.item_id;
+		}
+
+		return itemId;
 	}
 
 });
