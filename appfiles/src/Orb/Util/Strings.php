c54002a6c13e153e68ddd15975cb1ca9e1a35a11
Christopher Nadeau <chris@nadeau.ws>
2011-07-16 14:03:42 -0400

    Add auto-linker

--- appfiles/src/Orb/Util/Strings.php
+++ appfiles/src/Orb/Util/Strings.php
@@ -873,6 +873,36 @@ class Strings
 	}
 
 
+	/**
+	 * Turn links in text to HTML anchors
+	 * 
+	 * @param string $text
+	 * @param bool $short True to shorten off the part after the, like google.com/...
+	 * @return string
+	 */
+	public static function autoLink($text, $short = true)
+	{
+		$pattern  = '#\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))#';
+		$callback = function($matches) {
+			$url       = array_shift($matches);
+
+			$text = parse_url($url, PHP_URL_HOST) . parse_url($url, PHP_URL_PATH);
+			$text = preg_replace("/^www./", "", $text);
+
+			if ($short) {
+				$last = -(strlen(strrchr($text, "/"))) + 1;
+				if ($last < 0) {
+				   $text = substr($text, 0, $last) . "&hellip;";
+				}
+			}
+
+			return sprintf('<a href="%s">%s</a>', $url, $text);
+		};
+
+		return preg_replace_callback($pattern, $callback, $text);
+	}
+
+
 
 	/**
 	 * Like htmlentities() but encodes all characters, not just special ones.
