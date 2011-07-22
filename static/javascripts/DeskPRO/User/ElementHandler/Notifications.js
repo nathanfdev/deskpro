a3c21fafd3482017b70a40abfe9589058a661bdf
Christopher Nadeau <chris@nadeau.ws>
2011-07-22 04:27:54 -0400

    Nifty notifications slide up/down when there are too many

--- static/javascripts/DeskPRO/User/ElementHandler/Notifications.js
+++ static/javascripts/DeskPRO/User/ElementHandler/Notifications.js
@@ -5,6 +5,10 @@ DeskPRO.User.ElementHandler.Notifications = new Orb.Class({
 	Extends: DeskPRO.User.ElementHandler.ElementHandlerAbstract,
 
 	init: function() {
+
+		this.placeholder = $('#user_notifs_placeholder');
+		this.notif = $('#user_notifs');
+
 		var self = this;
 		$('.dismiss', this.el).click(function(ev) {
 			var li = $(this).parent();
@@ -20,5 +24,60 @@ DeskPRO.User.ElementHandler.Notifications = new Orb.Class({
 			ev.preventDefault();
 			$('#user_notifs').slideUp();
 		});
+
+		$('.view-all', this.el).click(function(ev){
+			ev.preventDefault();
+			ev.stopPropagation();
+			self.popOpen();
+		});
+
+		$('.close-trigger', this.el).click((function(ev){
+			ev.preventDefault();
+			ev.stopPropagation();
+			self.popClose();
+		}).bind(this));
+	},
+
+	popOpen: function() {
+		var pos = this.placeholder.offset();
+
+		this.notif.slideUp('fast', (function() {
+			this.notif.css({
+				top: pos.top + 4,
+				left: pos.left - 23,
+				width: this.placeholder.width() + 15,
+				position: 'absolute'
+			});
+			$('ul:first', this.notif).css({
+				'max-height': 400,
+				overflow: 'auto'
+			});
+			$('ul:first > li', this.notif).show();
+
+			this.notif.addClass('open');
+
+			this.notif.detach().appendTo('body').slideDown();
+		}).bind(this));
+	},
+
+	popClose: function() {
+		this.notif.slideUp('fast', (function() {
+			this.notif.css({
+				top: '',
+				left: '',
+				width: '',
+				position: 'relative'
+			});
+
+			$('ul:first', this.notif).css({
+				'max-height': '',
+				overflow: 'visible'
+			});
+			$('ul:first > li:gt(5)', this.notif).hide();
+
+			this.notif.removeClass('open');
+
+			this.notif.detach().insertAfter(this.placeholder).slideDown();
+		}).bind(this));
 	}
 });
