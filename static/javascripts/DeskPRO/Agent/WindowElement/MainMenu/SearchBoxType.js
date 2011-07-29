ce7cfa8107d31947f92e67fea19388a265249be9
Christopher Nadeau <chris@nadeau.ws>
2011-07-29 02:07:52 -0400

    Nice search box

--- static/javascripts/DeskPRO/Agent/WindowElement/MainMenu/SearchBoxType.js
+++ static/javascripts/DeskPRO/Agent/WindowElement/MainMenu/SearchBoxType.js
@@ -6,7 +6,9 @@ DeskPRO.Agent.WindowElement.MainMenu.SearchBoxType = new Class({
 	init: function () {
 		$('li[data-search-type]', this.buttonEl).click(function() {
 			var search_type = $(this).data('search-type');
+			var text = $('span', this).text();
 			$('#window_search_type').removeClass('all users tickets').addClass(search_type).data('search-type', search_type);
+			$('#window_search_type .type-text').text(text);
 		});
 
 		var self = this;
