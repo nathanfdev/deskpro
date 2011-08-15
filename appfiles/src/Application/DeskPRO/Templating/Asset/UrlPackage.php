b74c40fa20b22fc8741cfa1c3ef73307901efc39
Christopher Nadeau <chris@nadeau.ws>
2011-08-15 16:02:00 +0100

    Fix configurable static path with assethelper

---
+++ appfiles/src/Application/DeskPRO/Templating/Asset/UrlPackage.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage Templating
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris.nadeau@deskpro.com>
+ */
+
+namespace Application\DeskPRO\Templating\Asset;
+
+use Symfony\Component\Templating\Asset\UrlPackage as BaseUrlPackage;
+
+use Application\DeskPRO\App;
+
+class UrlPackage extends BaseUrlPackage
+{
+	public function __construct($baseUrls = array(), $version = null, $format = null)
+    {
+		$real = array();
+		foreach ((array)$baseUrls as $burl) {
+			if (!$burl OR $burl == 'CONFIG_HTTP' OR $burl == 'CONFIG_SSL') {
+				$type = $burl;
+				$burl = false;
+				if (!$type) {
+					$type = 'CONFIG_HTTP';
+				}
+
+				if ($type == 'CONFIG_SSL') {
+					$burl = App::getConfig('static_ssl_path');
+				}
+
+				if (!$burl) {
+					$burl = App::getConfig('static_path');
+				}
+			}
+
+			if ($burl) {
+				$real[] = $burl;
+			}
+		}
+
+        parent::__construct($real, $version, $format);
+    }
+}
