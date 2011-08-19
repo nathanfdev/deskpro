30fbd0c6a82fc7ff048a393a50e6fb40b8effa72
Christopher Nadeau <chris@nadeau.ws>
2011-08-19 14:55:25 +0100

    Load 'all' when selecting publish tab

--- static/javascripts/DeskPRO/Agent/WindowElement/Section/Publish.js
+++ static/javascripts/DeskPRO/Agent/WindowElement/Section/Publish.js
@@ -35,6 +35,11 @@ DeskPRO.Agent.WindowElement.Section.Publish = new Orb.Class({
 				} else {
 					$('#publish_outline_edit_cats').hide();
 				}
+
+				var all = $('a.all-route:first', info.tabContent);
+				if (all.length) {
+					DeskPRO_Window.runPageRouteFromElement(all);
+				}
 			}
 		});
 
