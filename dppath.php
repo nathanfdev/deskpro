3205f4d89d04d8b7307bb10f3c110c616b0f01db
Christopher Nadeau <chris@nadeau.ws>
2011-09-12 12:51:01 +0100

    Get rid of the dppath.php file and just code that constant directly into the bootstrapper files

--- dppath.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * The path to the 'appfiles' directory. If you want to move the directory,
- * you must update this path.
- */
-define('DP_ROOT', __DIR__ . '/appfiles');
--- index_dev.php
+++ index_dev.php
@@ -1,4 +1,10 @@
 <?php
-require('./dppath.php');
+/**
+ * The path to the 'appfiles' directory. If you want to move the directory,
+ * you must update this path.
+ */
+define('DP_ROOT', __DIR__ . '/appfiles');
+
+
 require(DP_ROOT . '/sys/Kernel/Boot.php');
 \DeskPRO\Kernel\Boot::bootWeb('dev', true);
