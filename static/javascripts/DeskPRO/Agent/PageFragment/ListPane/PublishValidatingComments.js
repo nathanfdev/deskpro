c27513d8162339609dd19cd7c56b7e87ed2f63d9
Christopher Nadeau <chris@nadeau.ws>
2011-10-28 15:54:59 +0100

    Changes to rte editor instances

--- static/javascripts/DeskPRO/Agent/PageFragment/ListPane/PublishValidatingComments.js
+++ static/javascripts/DeskPRO/Agent/PageFragment/ListPane/PublishValidatingComments.js
@@ -205,13 +205,11 @@ DeskPRO.Agent.PageFragment.ListPane.PublishValidatingComments = new Orb.Class({
 				theme: 'advanced',
 				plugins : "fullscreen",
 				fullscreen_new_window: true,
-				theme_advanced_buttons1: 'bold,italic,underline,|,justifyleft,justifycenter,justifyright,|,fontselect,fontsizeselect,formatselect',
-				theme_advanced_buttons2: ',bullist,numlist,|,outdent,indent,|,link,unlink,anchor,image,|,code,removeformat,fullscreen',
+				theme_advanced_buttons1: 'bold,italic,underline,|,link,unlink,anchor,image',
 				theme_advanced_buttons3: '',
 				theme_advanced_toolbar_location: 'top',
 				theme_advanced_toolbar_align: 'left',
-				theme_advanced_resizing: true,
-				theme_advanced_statusbar_location: 'bottom'
+				theme_advanced_statusbar_location: 'none'
 			});
 		}
 
--- static/javascripts/DeskPRO/Agent/PageFragment/Page/IdeaView.js
+++ static/javascripts/DeskPRO/Agent/PageFragment/Page/IdeaView.js
@@ -471,6 +471,7 @@ DeskPRO.Agent.PageFragment.Page.IdeaView = new Orb.Class({
 				theme_advanced_toolbar_location: 'top',
 				theme_advanced_toolbar_align: 'left',
 				theme_advanced_resizing: true,
+				theme_advanced_path: false,
 				theme_advanced_statusbar_location: 'bottom',
 				setup: function(ed) {
 					ed.onKeyPress.add(function() {
