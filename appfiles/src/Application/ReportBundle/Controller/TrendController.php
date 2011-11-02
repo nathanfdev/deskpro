1d8af847d40e3c73d57a63bcc81c2cd20682bd0e
Darren Rees <darren.rees@deskpro.com>
2011-11-02 15:39:35 +0000

    Edit action for Trend

--- appfiles/src/Application/ReportBundle/Controller/TrendController.php
+++ appfiles/src/Application/ReportBundle/Controller/TrendController.php
@@ -12,6 +12,7 @@
 namespace Application\ReportBundle\Controller;
 
 use Application\DeskPRO\App;
+use Application\ReportBundle\Form\EditStatType;
 
 class TrendController extends AbstractController
 {
@@ -43,8 +44,23 @@ class TrendController extends AbstractController
 	{
 		$stat = $this->getStat($stat_id);
 		
+		$form = $this->get('form.factory')->create(new EditStatType($stat->id ? false : true), $stat);
+
+		if ($this->in->getBool('process')) {
+			$form->bindRequest($this->get('request'));
+
+			if ($form->isValid()) {
+				App::getOrm()->persist($stat);
+				App::getOrm()->flush();
+
+				$this->session->setFlash('saved', $stat->title);
+				return $this->redirectRoute('trend_index');
+			}
+		}
+		
 		return $this->render('ReportBundle:Trend:edit.html.twig', array(
-			'stat' => $stat	
+			'stat' => $stat,
+			'form' => $form->createView(),
 		));
 	}
 	
@@ -55,7 +71,7 @@ class TrendController extends AbstractController
 		// TODO: clone it
 		$cloned = $stat;
 		
-		return $this->redirectRoute('trend_view', array('stat_id' => $cloned['id']));
+		return $this->redirectRoute('trend_edit', array('stat_id' => $cloned['id']));
 	}
 	
 	protected function getStat($stat_id)
---
+++ appfiles/src/Application/ReportBundle/Form/EditStatType.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage AdminBundle
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris.nadeau@deskpro.com>
+ */
+
+namespace Application\ReportBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class EditStatType extends AbstractType
+{
+	public function buildForm(FormBuilder $builder, array $options)
+	{
+		$builder->add('title', 'text');
+		$builder->add('disabled', 'checkbox', array('required' => false));
+	}
+
+	public function getName()
+	{
+		return 'trend';
+	}
+}
