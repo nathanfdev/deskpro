9f811002f04009556dc536ab4c2dad8fdd900140
Darren Rees <darren.rees@deskpro.com>
2011-11-07 09:51:48 +0000

    Adds basic worker cron for generating stats

---
+++ appfiles/src/Application/DeskPRO/WorkerProcess/Job/GenerateStats.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage WorkerProcess
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris.nadeau@deskpro.com>
+ */
+
+namespace Application\DeskPRO\WorkerProcess\Job;
+
+use Application\DeskPRO\App;
+use Application\DeskPRO\Log\Logger;
+
+/**
+ * Generate stats for the reporting system
+ */
+class GenerateStat extends AbstractJob
+{
+	const DEFAULT_INTERVAL = 3600; // 1 hour
+
+	public function run()
+	{
+		
+	}
+}
