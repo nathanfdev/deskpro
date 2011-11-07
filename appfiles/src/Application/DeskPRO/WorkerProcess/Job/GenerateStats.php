29cb5d96d2694be1826ab754768d762dbc59d411
Darren Rees <darren.rees@deskpro.com>
2011-11-07 10:11:09 +0000

    Adds basic structure for stats worker job

--- appfiles/src/Application/DeskPRO/WorkerProcess/Job/GenerateStats.php
+++ appfiles/src/Application/DeskPRO/WorkerProcess/Job/GenerateStats.php
@@ -13,16 +13,51 @@ namespace Application\DeskPRO\WorkerProcess\Job;
 
 use Application\DeskPRO\App;
 use Application\DeskPRO\Log\Logger;
+use Application\DeskPRO\Entity\Stat;
 
 /**
  * Generate stats for the reporting system
  */
-class GenerateStat extends AbstractJob
+class GenerateStats extends AbstractJob
 {
 	const DEFAULT_INTERVAL = 3600; // 1 hour
 
 	public function run()
 	{
+		$time = time();
+		
+		// Get the stats
+		$stats 		= App::getEntityRepository('DeskPRO:Stat')
+				     ->getStatsRequiringUpdate($time);
+						
+		$count_stats 	= count($stats);
+		
+		$this->processStats($stats);
+		
+		$msg = "Generate Stats ($count_stats)";
+		$this->logStatus($msg);
+	}
+	
+	/**
+	 * Process a list of stats
+	 *
+	 * @param array $stats  List of stats to process
+	 */
+	protected function processStats($stats)
+	{
+		foreach ($stats as $stat) {
+			$this->processStats($stat);
+		}
+	}
+	
+	/**
+	 * Process an individual stat
+	 *
+	 * @param Application\DeskPRO\Entity\Stat $stat	The stat to process
+	 */
+	protected function processStat($stat)
+	{
 		
 	}
+	
 }
