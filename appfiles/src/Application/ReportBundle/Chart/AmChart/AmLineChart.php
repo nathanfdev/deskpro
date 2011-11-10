3b2c66df46393a3c7ef0e8e6ef119a916e126001
Darren Rees <darren.rees@deskpro.com>
2011-11-10 12:46:58 +0000

    Adds stacked line chart, other widget updates

---
+++ appfiles/src/Application/ReportBundle/Chart/AmChart/AmLineChart.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Application\ReportBundle\Chart\AmChart;
+
+use Application\ReportBundle\Chart\Base\LineChart as BaseLineChart;
+
+class AmLineChart extends BaseLineChart
+{
+	public function __construct($stat)
+	{
+		parent::__construct($stat);
+		
+		$this->view_chart_vendor 	= 'AmChart';
+		$this->view_chart_class 	= 'Line';	
+	}
+	
+	public function getData()
+	{
+		$number_points = 10;
+		
+		$points = array();
+		$points[0] = range(0, $number_points);
+		shuffle($points[0]);
+		$points[1] = range(0, $number_points);
+		shuffle($points[1]);
+		
+		$startUnix = time() - (86400 * $number_points);
+		for ($unix = $startUnix; $unix < time(); $unix+=86400) {
+			$this->series[] = date("d-m-Y", $unix);
+		}
+		$this->graphs = array(
+			$points[0],
+			$points[1]	
+		);
+	}
+	
+	public function getFormattedData()
+	{
+		$this->getData();
+		
+		$formattedData = array();	
+		$formattedData['series'] = $this->series;
+		$formattedData['graphs'] = $this->graphs;
+		
+		return $formattedData;
+	}
+	
+	public function getSettings()
+	{
+		$settings = array();
+		
+		return $settings;
+	}
+}
