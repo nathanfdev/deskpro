4a5535be9f99dcdf6137a060f8439b0e99c70f2f
Darren Rees <darren.rees@deskpro.com>
2011-11-10 13:37:40 +0000

    Adds variation images, style changes for chart widgets

--- appfiles/src/Application/ReportBundle/Chart/AmChart/AmLineChart.php
+++ appfiles/src/Application/ReportBundle/Chart/AmChart/AmLineChart.php
@@ -16,7 +16,7 @@ class AmLineChart extends BaseLineChart
 	
 	public function getData()
 	{
-		$number_points = 10;
+		$number_points = 7;
 		
 		$points = array();
 		$points[0] = range(0, $number_points);
@@ -26,7 +26,7 @@ class AmLineChart extends BaseLineChart
 		
 		$startUnix = time() - (86400 * $number_points);
 		for ($unix = $startUnix; $unix < time(); $unix+=86400) {
-			$this->series[] = date("d-m-Y", $unix);
+			$this->series[] = date("d", $unix);
 		}
 		$this->graphs = array(
 			$points[0],
