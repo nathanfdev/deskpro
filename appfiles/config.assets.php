0817f8befb4bd9aa7b33b224bb52a2a048190dc0
Darren Rees <darren.rees@deskpro.com>
2011-11-10 18:32:16 +0000

    Work on clone overlay

--- appfiles/config.assets.php
+++ appfiles/config.assets.php
@@ -401,6 +401,7 @@ $CONFIG['report_dashboard'] = array(
 		// App related files
 		'javascripts/DeskPRO/Report/PageHandler/Dashboard.js',
 		'javascripts/DeskPRO/Report/Dashboard/Widget.js',
+		'javascripts/DeskPRO/Report/PageHandler/Trend.js',
 	),
 );
 
---
+++ static/javascripts/DeskPRO/Report/PageHandler/Trend.js
@@ -0,0 +1,57 @@
+Orb.createNamespace('DeskPRO.Report.PageHandler');
+
+/**
+ * Trend page handler
+ */
+DeskPRO.Report.PageHandler.Trend = new Orb.Class({
+	Extends: DeskPRO.Report.PageHandler.Basic,
+
+	initialize: function() {
+		
+		// UI Overlay
+		this.overlay = null;
+	},
+
+	// Init the page
+	initPage: function() {
+		var self = this;
+		
+		// Create the trend overlay
+		this.overlay = new DeskPRO.UI.Overlay({
+			contentElement: $('#overlay_wrapper')
+		});
+		$('#overlay_wrapper .close-overlay').click(function() {
+			self.overlay.close();
+		});
+		
+		$('.clone-trend').click(function() {
+			var href = $(this).attr('href');
+			$.ajax({
+				url: href,
+				type: 'GET',
+				success: function(data) {
+					self.openOverlay(data);
+				}
+			});
+			
+			return false;
+		});
+	},
+	
+	// Open the overlay loading in a template
+	openOverlay: function(overlay_content) {
+
+		$('.overlay-content').html(overlay_content);
+		this.overlay.open();
+
+	},
+
+	// Clean the overlay and close it
+	closeOverlay: function() {
+
+		$('.overlay-content').html('');
+		this.overlay.close();
+
+	},
+
+});
