2fd1309fb7bc2aa94a37661a1a448f1318f0467e
Darren Rees <darren.rees@deskpro.com>
2011-11-11 10:40:40 +0000

    Adds stat value group entity

--- appfiles/src/Application/DeskPRO/Entity/Stat.php
+++ appfiles/src/Application/DeskPRO/Entity/Stat.php
@@ -21,10 +21,14 @@ use Doctrine\ORM\Mapping as ORM_Mapping;
  */
 class Stat extends \Application\DeskPRO\Domain\DomainObject
 {
-	const FREQUENCY_HOURLY = "hourly";
-	const FREQUENCY_DAILY  = "daily";
-	const FREQUENCY_WEEKLY = "weekly";
-	
+	const FREQUENCY_DAILY   = "daily";
+	const FREQUENCY_MONTHLY = "monthly";
+	const FREQUENCY_YEARLY  = "yearly";
+
+	protected static $availableRunFrequencies = array(
+		self::FREQUENCY_DAILY, self::FREQUENCY_MONTHLY, self::FREQUENCY_YEARLY
+	);
+
 	/**
 	 * @var int
 	 * @ORM_Mapping\Id
@@ -32,7 +36,7 @@ class Stat extends \Application\DeskPRO\Domain\DomainObject
 	 * @ORM_Mapping\Column(name="id", type="integer")
 	 */
 	protected $id = null;
-	
+
 	/**
 	 * The stat title
 	 *
@@ -40,65 +44,65 @@ class Stat extends \Application\DeskPRO\Domain\DomainObject
 	 * @ORM_Mapping\Column(name="title", type="string", length=255)
 	 */
 	protected $title;
-	
+
 	/**
 	 * The stat author
 	 *
 	 * @var \Application\DeskPRO\Entity\Person
-	 * @ORM_MAPPING\OneToOne(targetEntity="Person", fetch="EAGER")
+	 * @ORM_MAPPING\ManyToOne(targetEntity="Person", fetch="EAGER")
 	 * @ORM_Mapping\JoinColumn(name="author_id", referencedColumnName="id")
 	 */
 	protected $author;
-	
+
+	/**
+	 * The stat this stat was cloned from (its parent)
+	 *
+	 * @var \Application\DeskPRO\Entity\Stat
+	 * @ORM_MAPPING\ManyToOne(targetEntity="Stat")
+	 * @ORM_MAPPING\JoinColumn(name="parent_stat_id", referencedColumnName="id")
+	 */
+	protected $parent_stat;
+
+	/**
+	 * The grouping type
+	 *
+	 * @var string
+	 * @ORM_MAPPING\Column(name="grouping_ref", type="string", length="255")
+	 */
+	protected $grouping_ref;
+
+	/**
+	 * Stat concept class. The class responsible for getting the data for this stat
+	 *
+	 * @var string
+	 * @ORM_MAPPING\Column(name="stat_concept_class", type="string", length="500")
+	 */
+	protected $stat_concept_class;
+
 	/**
 	 * Is the stat starred
 	 *
 	 * @var boolean
 	 * @ORM_MAPPING\Column(name="starred", type="boolean")
 	 */
-	protected $starred;
-	
-	/**
-	 * The stat this stat was cloned from (its parent)
-	 *
-	 * @var \Application\DeskPRO\Entity\Stat
-	 * @ORM_MAPPING\OneToOne(targetEntity="Stat")
-	 * @ORM_MAPPING\JoinColumn(name="parent_stat_id", referencedColumnName="id")
-	 */
-	protected $parent_stat;
-	
+	protected $starred = false;
+
 	/**
 	 * Is the stat disabled
 	 *
 	 * @var bool
 	 * @ORM_MAPPING\Column(name="disabled", type="boolean")
 	 */
-	protected $disabled;
-	
-	/**
-	 * The Entity Repository to run the query from
-	 *
-	 * @var string
-	 * @ORM_MAPPING\Column(name="entity_repository", type="string", length="500")
-	 */
-	protected $entity_repository;
-	
-	/**
-	 * The query method on the Entity Repository
-	 *
-	 * @var string
-	 * @ORM_MAPPING\Column(name="query_method", type="string", length="100")
-	 */
-	protected $query_method;
-	
+	protected $disabled = false;
+
 	/**
 	 * The frequency to run the stat
 	 *
 	 * @var string
 	 * @ORM_MAPPING\Column(name="run_frequency", type="string", length="10")
 	 */
-	protected $run_frequency;
-	
+	protected $run_frequency = "daily";
+
 	/**
 	 * The last run date
 	 *
@@ -106,7 +110,7 @@ class Stat extends \Application\DeskPRO\Domain\DomainObject
 	 * @ORM_MAPPING\Column(name="last_run", type="datetime", nullable=true)
 	 */
 	protected $last_run;
-	
+
 	/**
 	 * The stat creation date
 	 *
@@ -117,10 +121,9 @@ class Stat extends \Application\DeskPRO\Domain\DomainObject
 
 	public function __construct()
 	{
-		$this->disabled     = false;
 		$this->date_created = new \DateTime();
 	}
-	
+
 	/**
 	 * Get the author name. Use the associated Person if one exists, otherwise
 	 * its 'deskpro'
@@ -134,7 +137,7 @@ class Stat extends \Application\DeskPRO\Domain\DomainObject
 			return 'deskpro';
 		}
 	}
-	
+
 	/**
 	 * Get the Stat amount
 	 *
@@ -144,7 +147,7 @@ class Stat extends \Application\DeskPRO\Domain\DomainObject
 	{
 		return rand(0, 100);
 	}
-	
+
 	/**
 	 * Get the Stat variation
 	 *
@@ -154,7 +157,7 @@ class Stat extends \Application\DeskPRO\Domain\DomainObject
 	{
 		return rand(-5, 5);
 	}
-	
+
 	/**
 	 * Generate the trend points for the stat
 	 *
@@ -164,7 +167,16 @@ class Stat extends \Application\DeskPRO\Domain\DomainObject
 	{
 		$trendPoints = range(0, 10);
 		shuffle($trendPoints);
-		
+
 		return $trendPoints;
+	}
+
+	public function setRunFrequency($frequency)
+	{
+		if (false === in_array($frequency, $this->availableRunFrequencies)) {
+			throw new \Exception("Unable to set run frequency to type $frequency. Supported types are " . join(", ", $this->availableRunFrequencies));
+		}
+
+		$this->run_frequency = $frequency;
 	}
 }
---
+++ appfiles/src/Application/DeskPRO/Entity/StatValueGroup.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @category Entities
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris.nadeau@deskpro.com>
+ */
+
+namespace Application\DeskPRO\Entity;
+
+use Doctrine\ORM\Mapping as ORM_Mapping;
+
+/**
+ * Stat Value Group - The grouping data for a Stat Value
+ *
+ * @ORM_Mapping\Entity(repositoryClass="Application\DeskPRO\EntityRepository\StatValueGroup")
+ * @ORM_Mapping\Table(name="stat_value_group")
+ */
+class StatValueGroup extends \Application\DeskPRO\Domain\DomainObject
+{
+	/**
+	 * @var int
+	 * @ORM_Mapping\Id
+	 * @ORM_Mapping\generatedValue(strategy="IDENTITY")
+	 * @ORM_Mapping\Column(name="id", type="integer")
+	 */
+	protected $id = null;
+
+	/**
+	 * The Stat Value
+	 *
+	 * @var \Application\DeskPRO\Entity\StatValue
+	 * @ORM_MAPPING\ManyToOne(targetEntity="StatValue", fetch="EAGER")
+	 * @ORM_Mapping\JoinColumn(name="stat_value_id", referencedColumnName="id")
+	 */
+	protected $stat_value;
+
+	/**
+	 * The stat value
+	 *
+	 * @var int
+	 * @ORM_MAPPING\Column(name="value", type="decimal")
+	 */
+	protected $value;
+
+	/**
+	 * The unix time for the period this stat represents
+	 *
+	 * @var int
+	 * @ORM_Mapping\Column(name="stat_unix", type="integer")
+	 */
+	protected $stat_unix;
+
+	public function __construct()
+	{
+		$this->stat_unix = time();
+	}
+}
