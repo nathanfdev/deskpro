b21f991860ea2f8ed2a8d6daad1658f940b5879c
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-11-17 17:47:05 +0000

    Field options

--- static/javascripts/DeskPRO/Admin/ElementHandler/TicketEditor.js
+++ static/javascripts/DeskPRO/Admin/ElementHandler/TicketEditor.js
@@ -7,7 +7,9 @@ DeskPRO.Admin.ElementHandler.TicketEditor = new Orb.Class({
 		var self = this;
 
 		$('#department_switcher').on('change', function() {
-			var url = $(this).data('refresh-url');
+			var opt = $('option:selected', this);
+			var url = opt.data('refresh-url');
+			console.log(url);
 			if (url) {
 				window.location = url;
 			}
@@ -102,7 +104,68 @@ DeskPRO.Admin.ElementHandler.TicketEditor = new Orb.Class({
 		this.el.on('click', '.remove-field-trigger', function() {
 			var el = $(this).closest('li.form-item');
 			var sidebarEl = el.data('sidebar-item');
-			el.fadeOut('fast', function() { el.remove(); sidebarEl.show(); });
+			el.fadeOut('fast', function() {
+				el.remove();
+				sidebarEl.show();
+				if (!$('#admin_ticket_editor_items .form-item').length) {
+					$('#admin_ticket_editor_items .no-items-notice').show();
+				}
+			});
+		});
+
+		//------------------------------
+		// Display options
+		//------------------------------
+
+		this.el.on('click', '.edit-field-trigger', function() {
+			var el = $(this).closest('li.form-item');
+			var overlay = el.data('options-overlay');
+
+			if (!overlay) {
+				var overlayEl = $('.field-options-overlay', el);
+				var editor = new DeskPRO.Form.RuleBuilder($('#criteria_tpl'));
+				editor.addEvent('newRow', function(new_row) {
+					$('.remove', new_row).on('click', function() {
+						new_row.remove();
+					});
+				});
+				var to_el = $('.criteria-form .search-terms', overlayEl);
+
+				var self = this;
+
+				$('.criteria-form .add-term', this.context).data('add-count', 0).on('click', function() {
+					var basename = 'terms_all['+Orb.uuid()+']';
+					editor.addNewRow(to_el, basename);
+				});
+
+				overlay = new DeskPRO.UI.Overlay({
+					contentElement: overlayEl
+				});
+				el.data('options-overlay', overlay);
+
+				var select = $('select', el).first();
+				if (select.length) {
+					var select = select.clone();
+					select.attr('multiple', 'multiple');
+					select.css({width: '80%', maxHeight: '80px', minHeight: '30px'});
+					$('option:not([value])', select).remove();
+
+					var choiceOpts = $(DeskPRO_Window.util.getPlainTpl($('#field_options_choices_tpl')));
+					var inputRow = $('.dp-form-input', choiceOpts).append(select);
+
+					choiceOpts.appendTo($('.choices-container', overlayEl));
+
+					$('input.custom_options', choiceOpts).on('click', function() {
+						if (this.checked) {
+							inputRow.slideDown();
+						} else {
+							inputRow.slideUp();
+						}
+					});
+				}
+			}
+
+			overlay.open();
 		});
 	}
 });
