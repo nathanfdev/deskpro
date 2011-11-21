e9cc3040bdb06a7d832b2dfcdc24837db45cd298
Darren Rees <darren.rees@deskpro.com>
2011-11-21 17:25:34 +0000

    Tickets first response time query

--- appfiles/src/Application/ReportBundle/Stat/DeskPRO/TicketFirstResponseTime.php
+++ appfiles/src/Application/ReportBundle/Stat/DeskPRO/TicketFirstResponseTime.php
@@ -27,18 +27,59 @@ class TicketFirstResponseTime extends AbstractTicket
 	public function buildConceptQueries()
 	{
 		// AVG time
-		$query = new QueryBuilder();
+		// Get the total time for first response
+		$query = $this->createQuery()
+		      ->select('SUM(t.total_to_first_reply) AS first_response_total')
+		      ->from('tickets', 't')
+		      ->where("t.date_resolved IS NOT NULL")
+		      ->andWhere('UNIX_TIMESTAMP(t.date_first_agent_reply) > :date_first_agent_reply')
+		      ->setParameter(':date_first_agent_reply', $this->last_stat_date->format('U'));
+		$this->addQuery('ticket_first_response_time', $query);
 
-		$this->addQuery($query);
+		// Get the number of tickets resolved
+		$query = $this->createQuery()
+		      ->select('COUNT(t.id) as ticket_count')
+		      ->from('tickets', 't')
+		      ->where("t.date_resolved IS NOT NULL")
+		      ->andWhere('UNIX_TIMESTAMP(t.date_first_agent_reply) > :date_first_agent_reply')
+		      ->setParameter(':date_first_agent_reply', $this->last_stat_date->format('U'));
+
+		$this->addQuery('tickets_replied_to', $query);
 	}
 
 	public function processUngroupedResults($result)
 	{
+		$waitingTime 	= $result['ticket_first_response_time'][0]['first_response_total'];
+		$replied 	= $result['tickets_replied_to'][0]['ticket_count'];
+
+		return ($replied != 0) ? $waitingTime / $replied : 0;
 	}
 
 	public function processGroupedResults($results)
 	{
+		// Get the Ids and result of the replied to tickets, we use this
+		// array as a lookup based on the grouping_id
+		$resultsRepliedTo = array();
+		foreach ($results['tickets_replied_to'] as $result) {
+			$resultsRepliedTo[$result[str_replace('.', '_', $this->grouping[0])]] = $result['ticket_count'];
+		}
+
 		$processedResults = array();
+
+		foreach ($results['ticket_first_response_time'] as $result) {
+			$groupingId = $result[str_replace('.', '_', $this->grouping[0])];
+
+			// Check to see if any tickets were replied to for this grouping_id
+			$repliedCount = 0;
+			if (isset($resultsRepliedTo[$groupingId])) {
+				$repliedCount = $resultsRepliedTo[$groupingId];
+			}
+
+			$processedResults[] = array(
+				'value'       => ($repliedCount != 0) ? $result['first_response_total'] / $repliedCount : 0,
+				'grouping_id' => $result[str_replace('.', '_', $this->grouping[0])],
+			);
+		}
 
 		return $processedResults;
 	}
