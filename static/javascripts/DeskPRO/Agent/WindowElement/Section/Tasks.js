be66ddfd0234630fdf0756e877624b0e578ad1ff
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-11-29 12:42:28 +0000

    Make sure to re-select active nav when sidebar is refreshed, and if a new task is added then refresh the listing as well

--- static/javascripts/DeskPRO/Agent/WindowElement/Section/Tasks.js
+++ static/javascripts/DeskPRO/Agent/WindowElement/Section/Tasks.js
@@ -13,11 +13,45 @@ DeskPRO.Agent.WindowElement.Section.Tasks = new Orb.Class({
 	},
 
 	refresh: function() {
+		var countmap = {};
+		$('span.list-counter', this.contentEl).each(function() {
+			countmap[$(this).attr('id')] = $(this).text().trim();
+		});
+
+		var selected = $('.nav-selected', this.contentEl);
+		var selectedCountId = null;
+		if (selected) {
+			var e = $('span.list-counter', selected).first();
+			if (e.length) {
+				selectedCountId = e.attr('id');
+			}
+		}
+
 		$.ajax({
 			url: BASE_URL + 'agent/tasks/get-section-data.json',
 			context: this,
 			success: function(data) {
 				this._initSection(data);
+
+				if (selectedCountId) {
+					var countEl = $('#' + selectedCountId);
+					var newCount = countEl.text().trim();
+					var nav = countEl.closest('.is-nav-item');
+
+					// Re-select the proper nav item
+					nav.addClass('nav-selected');
+
+					// And reload the view if its changed
+					if (newCount != countmap[selectedCountId]) {
+						var routeEl;
+						if (nav.data('route')) {
+							routeEl = nav;
+						} else {
+							routeEl = $('[data-route]', nav).first();
+						}
+						DeskPRO_Window.runPageRouteFromElement(routeEl);
+					}
+				}
 			}
 		});
 	},
