ba72977bd6304cc2d3f0ae3a4b31b0175e8def29
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-11-30 13:43:05 +0000

    Minor changes to sqlite cacher to improve hit checks on smaller caches

--- appfiles/src/Orb/Doctrine/Common/Cache/SqliteCache.php
+++ appfiles/src/Orb/Doctrine/Common/Cache/SqliteCache.php
@@ -39,9 +39,50 @@ class SqliteCache extends \Doctrine\Common\Cache\AbstractCache
 	protected $_cache_name = 'doctrine_cache';
 
 	/**
+	 * Connection name which might have been created before
+	 * @var string
+	 */
+	protected $_connection_name = null;
+
+	/**
 	 * @var \Doctrine\DBAL\Connection
 	 */
 	protected $_db;
+
+	/**
+	 * No expiry mode
+	 *
+	 * @var bool
+	 */
+	protected $_no_expire = false;
+
+	/**
+	 * Fetch a full list of IDs the first time an ID containment check is made
+	 * @var bool
+	 */
+	protected $_cached_ids_mode = false;
+
+	/**
+	 * @var array
+	 */
+	protected $_cached_ids = null;
+
+	/**
+	 * Sets no expiry mode. This means all cache entries are deleted manually, no time comparions or cleanup is done.
+	 */
+	public function enableNoExpiryMode()
+	{
+		$this->_no_expire = true;
+	}
+
+	/**
+	 * This enables an ID cache so contains() doesnt result in a new query every time.
+	 * This will keep a record of all IDs with one query, and refer to that instead.
+	 */
+	public function enableCachedIdsCheckMode()
+	{
+		$this->_cached_ids_mode = true;
+	}
 
 	/**
 	 * Sets the SQLite database connection to use for the cache. This will automatically
@@ -57,48 +98,13 @@ class SqliteCache extends \Doctrine\Common\Cache\AbstractCache
 	 * @param string $filepath
 	 * @param string $cache_name
 	 * @param string|null $connection_name
-	 * @return Doctrine\DBAL\Connection
 	 */
-	public function setDbFile($filepath, $cache_name = 'doctrine_cache', $connection_name = null)
+	public function __construct($filepath, $cache_name = 'doctrine_cache', $connection_name = null)
 	{
 		$this->_dbfile = $filepath;
 		$this->_cache_name = $cache_name;
-
-		if ($connection_name !== null) {
-			if (isset(self::$named_connections[$connection_name])) {
-				$this->_db = self::$named_connections[$connection_name];
-			}
-		}
-		if (!$this->_db) {
-			$params = array(
-				'driver' => 'pdo_sqlite'
-			);
-
-			if ($filepath == 'MEMORY') {
-				$params['memory'] = true;
-			} else {
-				$params['path'] = $filepath;
-			}
-
-			$this->_db = \Doctrine\DBAL\DriverManager::getConnection($params);
-
-			if ($connection_name) {
-				self::$named_connections[$connection_name] = $this->_db;
-			}
-		}
-
-		$exists = $this->_db->fetchColumn("SELECT name FROM sqlite_master WHERE type='table' AND name='{$cache_name}'");
-		if (!$exists) {
-			$this->_db->exec("CREATE TABLE {$cache_name} (id TEXT PRIMARY KEY, data BLOB, expire INTEGER)");
-		} else {
-			if (mt_rand(1,10) <= 3) {
-				$this->_db->executeUpdate("DELETE FROM {$cache_name} WHERE expire < ? AND expire > 0", array(time()));
-			}
-		}
-
-		return $this->_db;
+		$this->_connection_name = $connection_name;
 	}
-
 
 
 	/**
@@ -108,16 +114,66 @@ class SqliteCache extends \Doctrine\Common\Cache\AbstractCache
 	 */
 	public function getDbConnection()
 	{
+		if ($this->_db !== null) {
+			return $this->_db;
+		}
+
+		if ($this->_connection_name !== null) {
+			if (isset(self::$named_connections[$this->_connection_name])) {
+				$this->_db = self::$named_connections[$this->_connection_name];
+			}
+		}
+		if (!$this->_db) {
+			$params = array(
+				'driver' => 'pdo_sqlite'
+			);
+
+			if ($this->_dbfile == 'MEMORY') {
+				$params['memory'] = true;
+			} else {
+				$params['path'] = $this->_dbfile;
+			}
+
+			$this->_db = \Doctrine\DBAL\DriverManager::getConnection($params);
+
+			if ($this->_connection_name) {
+				self::$named_connections[$this->_connection_name] = $this->_db;
+			}
+		}
+
+		$exists = $this->_db->fetchColumn("SELECT name FROM sqlite_master WHERE type='table' AND name='{$this->_cache_name}'");
+		if (!$exists) {
+			if ($this->_no_expire) {
+				$this->_db->exec("CREATE TABLE {$this->_cache_name} (id TEXT PRIMARY KEY, data BLOB, expire INTEGER)");
+			} else {
+				$this->_db->exec("CREATE TABLE {$this->_cache_name} (id TEXT PRIMARY KEY, data BLOB)");
+			}
+		} else {
+			if (!$this->_no_expire && mt_rand(1,10) <= 3) {
+				$this->_db->executeUpdate("DELETE FROM {$this->_cache_name} WHERE expire < ? AND expire != 0", array(time()));
+			}
+		}
+
 		return $this->_db;
 	}
-
 
 
 	public function getIds()
 	{
 		$keys = array();
-		foreach ($this->_db->fetchAll("SELECT id FROM {$this->_cache_name} WHERE (expire = 0 OR expire > ?)", array(time())) as $x) {
+
+		if ($this->_no_expire) {
+			$sql = "SELECT id FROM {$this->_cache_name}";
+		} else {
+			$sql = "SELECT id FROM {$this->_cache_name} WHERE (expire = 0 OR expire > ?)";
+		}
+
+		foreach ($this->getDbConnection()->fetchAll($sql, array(time())) as $x) {
 			$keys[] = $x['id'];
+		}
+
+		if ($this->_cached_ids_mode) {
+			$this->_cached_ids = array_combine($keys, array_fill(0,count($keys), true));
 		}
 
 		return $keys;
@@ -125,34 +181,67 @@ class SqliteCache extends \Doctrine\Common\Cache\AbstractCache
 
 	protected function _doFetch($id)
     {
-        $data = $this->_db->fetchColumn("SELECT data FROM {$this->_cache_name} WHERE id = ? AND (expire = 0 OR expire > ?)", array($id, time()));
+		if ($this->_no_expire) {
+			$sql = "SELECT data FROM {$this->_cache_name} WHERE id = ?";
+			$params = array($id);
+		} else {
+			$sql = "SELECT data FROM {$this->_cache_name} WHERE id = ? AND (expire = 0 OR expire > ?)";
+			$params = array($id, time());
+		}
+
+        $data = $this->getDbConnection()->fetchColumn($sql, $params);
 		$data = unserialize($data);
+
 		return $data;
     }
 
     protected function _doContains($id)
     {
-        $exists = $this->_db->fetchColumn("SELECT id FROM {$this->_cache_name} WHERE id = ? AND (expire = 0 OR expire > ?)", array($id, time()));
+		if ($this->_cached_ids_mode) {
+			if ($this->_cached_ids === null) {
+				$this->getIds();
+			}
+			return isset($this->_cached_ids[$id]);
+		}
+
+		if ($this->_no_expire) {
+			$sql = "SELECT data FROM {$this->_cache_name} WHERE id = ?";
+			$params = array($id);
+		} else {
+			$sql = "SELECT data FROM {$this->_cache_name} WHERE id = ? AND (expire = 0 OR expire > ?)";
+			$params = array($id, time());
+		}
+
+        $exists = $this->getDbConnection()->fetchColumn("SELECT id FROM {$this->_cache_name} WHERE id = ? AND (expire = 0 OR expire > ?)", array($id, time()));
 
 		return (bool)$exists;
     }
 
     protected function _doSave($id, $data, $lifeTime = 0)
     {
-		$expire = 0;
-		if ($lifeTime) {
-			$expire = time() + $lifeTime;
-		}
-
 		$data = serialize($data);
-        $this->_db->executeUpdate("INSERT OR REPLACE INTO {$this->_cache_name} (id, data, expire) VALUES (?, ?,?)", array($id, $data, $expire));
+
+		if ($this->_no_expire) {
+			$expire = 0;
+			if ($lifeTime) {
+				$expire = time() + $lifeTime;
+			}
+
+			$this->getDbConnection()->executeUpdate("INSERT OR REPLACE INTO {$this->_cache_name} (id, data, expire) VALUES (?, ?, ?)", array($id, $data, $expire));
+		} else {
+			$this->getDbConnection()->executeUpdate("INSERT OR REPLACE INTO {$this->_cache_name} (id, data) VALUES (?, ?)", array($id, $data));
+		}
 
 		return true;
     }
 
     protected function _doDelete($id)
     {
-		$this->_db->executeUpdate("DELETE FROM {$this->_cache_name} WHERE id = ?", array($id));
+		$this->getDbConnection()->executeUpdate("DELETE FROM {$this->_cache_name} WHERE id = ?", array($id));
+
+		if ($this->_cached_ids !== null) {
+			unset($this->_cached_ids[$id]);
+		}
 
 		return true;
     }
