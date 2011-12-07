c173415dd111fcdff30fec9554f4fe54c1e90051
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-12-07 09:30:27 +0000

    Fix capitalization

--- appfiles/languages/DeskPRO/agent/ideas.php
+++ appfiles/languages/DeskPRO/agent/ideas.php
@@ -14,7 +14,7 @@
 	'agent.ideas.merge_explain' => 'Merge this idea into one of the open ideas below. This description will be added as a new comment and any existing comments on this idea will be appended. Afterwards, this idea will be deleted.',
 	'agent.ideas.merge_none_open' => 'You have no other ideas open. Find an idea you want to merge with and open it in a new tab, then click on the merge button again.',
 
-	'agent.ideas.ideas_to_validate' => 'Ideas to validate',
+	'agent.ideas.ideas_to_validate' => 'Ideas to Validate',
 
 	'agent.ideas.no_labels_explain' => 'You have not labelled any ideas yet. View an idea to add labels to it.',
 );
