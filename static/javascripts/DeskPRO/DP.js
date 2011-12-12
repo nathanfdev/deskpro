a28e096e149547a3065dc8eb45c95b33fe081cd0
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-12-12 12:47:09 +0000

    Fix console proxy in chrome

--- static/javascripts/DeskPRO/DP.js
+++ static/javascripts/DeskPRO/DP.js
@@ -13,8 +13,11 @@ var DP = {
 		if (DP_DEBUG) {
 			['error', 'log', 'warn', 'info', 'debug'].each(function(v) {
 				if (window.console[v]) {
+					var fn = window.console[v];
 					DP.console[v] = function() {
-						return window.console[v].apply(window.console[v], arguments);
+						var args = Array.prototype.slice.call(arguments);
+						var ret = fn.apply(window.console, args);
+						return ret;
 					}
 				}
 			});
