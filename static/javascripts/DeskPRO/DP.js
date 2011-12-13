b4f2a067201df8884472f988fe891c13d7014706
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-12-13 09:12:50 +0000

    Fix log proxy giving errors in IE

--- static/javascripts/DeskPRO/DP.js
+++ static/javascripts/DeskPRO/DP.js
@@ -12,7 +12,7 @@ var DP = {
 	init: function() {
 		if (DP_DEBUG) {
 			['error', 'log', 'warn', 'info', 'debug'].each(function(v) {
-				if (window.console[v]) {
+				if (window.console[v] && typeof window.console[v] == 'function') {
 					var fn = window.console[v];
 					DP.console[v] = function() {
 						var args = Array.prototype.slice.call(arguments);
