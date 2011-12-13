3999ee59433730bfd5b38766ca8ca8074f9b5d1e
Christopher Nadeau <chris.nadeau@deskpro.com>
2011-12-13 10:50:45 +0000

    Fix notices in chat session starter

--- appfiles/src/Application/UserBundle/Resources/views/Chat/chat-session.js.php
+++ appfiles/src/Application/UserBundle/Resources/views/Chat/chat-session.js.php
@@ -1,17 +1,23 @@
-<?php if ($convo_messages):
-	$messages = array();
-	foreach ($convo_messages as $msg) {
-		$messages[] = $msg->getInfo();
-	}
-?>
-DpChat.setInitialMessages(<?php echo json_encode(array_reverse($messages)) ?>);
+<?php if (!empty($convo_messages) && $convo_messages): ?>
+	<?php
+		$messages = array();
+		foreach ($convo_messages as $msg) {
+			$messages[] = $msg->getInfo();
+		}
+	?>
+	DpChat.setInitialMessages(<?php echo json_encode(array_reverse($messages)) ?>);
 <?php endif ?>
-<?php if ($session->visitor->name): ?>DpChat.setFormVar('name', <?php echo json_encode($session->visitor->name) ?>);<?php endif ?>
-<?php if ($session->visitor->email): ?>DpChat.setFormVar('email', <?php echo json_encode($session->visitor->email) ?>);<?php endif ?>
-<?php if ($department_sel): ?>
-DpChat.setDepartmentSelect(<?php echo json_encode($department_sel) ?>);
+
+<?php if ($session->visitor): ?>
+	<?php if ($session->visitor->name): ?>DpChat.setFormVar('name', <?php echo json_encode($session->visitor->name) ?>);<?php endif ?>
+	<?php if ($session->visitor->email): ?>DpChat.setFormVar('email', <?php echo json_encode($session->visitor->email) ?>);<?php endif ?>
 <?php endif ?>
-<?php if ($proactive): ?>
+
+<?php if (!empty($department_sel) && $department_sel): ?>
+	DpChat.setDepartmentSelect(<?php echo json_encode($department_sel) ?>);
+<?php endif ?>
+
+<?php if (!empty($proactive) && $proactive): ?>
 	DpChat.setSessionCode('<?php echo $session['session_code'] ?>', true);
 <?php else: ?>
 	DpChat.setSessionCode('<?php echo $session['session_code'] ?>');
