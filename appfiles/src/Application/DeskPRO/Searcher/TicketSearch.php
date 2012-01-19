b613baadb545068a420ed75287d9d3c603f3e73a
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-01-19 18:08:54 +0000

    Fix php warning

--- appfiles/src/Application/DeskPRO/Searcher/TicketSearch.php
+++ appfiles/src/Application/DeskPRO/Searcher/TicketSearch.php
@@ -502,7 +502,7 @@ class TicketSearch extends SearcherAbstract
 						return $titles;
 					});
 
-					if ($choice && !in_array('0', $choice)) {
+					if ($choice && (!is_array($choice) || !in_array('0', $choice))) {
 						$choice = App::getEntityRepository('DeskPRO:Department')->getIdsInTree($choice, true);
 					}
 
