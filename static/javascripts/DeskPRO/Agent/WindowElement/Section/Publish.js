d3e0a41dbd8415d5b4160d6f2f99bc08b429ddf3
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-02-02 10:16:17 +0000

    Expand category tree automatically

--- static/javascripts/DeskPRO/Agent/WindowElement/Section/Publish.js
+++ static/javascripts/DeskPRO/Agent/WindowElement/Section/Publish.js
@@ -241,11 +241,7 @@ DeskPRO.Agent.WindowElement.Section.Publish = new Orb.Class({
 			// Perform count calcs now
 			this.recountChildCounts(listEl);
 
-			// Expand if there are less than 10 cats
-			var catcount = $('#publish_outline_'+type+'cat_list').find('li').length;
-			if (catcount <= 10) {
-				$('#publish_outline_'+type+'cat_list').find('li.has-children').addClass('sub-expanded');
-			}
+			$('#publish_outline_'+type+'cat_list').find('li.has-children').addClass('sub-expanded');
 		}, this);
 
 
