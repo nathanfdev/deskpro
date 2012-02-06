eec2bb9d420edd881f50caa05636fb1ef280d12d
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-02-06 10:36:06 +0000

    CSS tweaks, add JS logger to all events and log ones that take too long

--- appfiles/src/Application/SysBundle/Controller/MiscController.php
+++ appfiles/src/Application/SysBundle/Controller/MiscController.php
@@ -16,6 +16,7 @@ class MiscController extends \Application\DeskPRO\Controller\AbstractController
 
 		$logger = App::createNewLogger('error_log.js', null);
 		$logger->log($message, 'WARN', array(
+			'message'    => $this->in->getString('message'), // full message as the summary string is limited to 1000 chars
 			'hash'       => $hash,
 			'ip_address' => $ip_address,
 			'user_agent' => $user_agent,
--- static/javascripts/DeskPRO/ErrorLogger.js
+++ static/javascripts/DeskPRO/ErrorLogger.js
@@ -14,8 +14,9 @@ var DpErrorLog = {
 		if (window.console.error) {
 			var oldConsole = window.console.error;
 			window.console.error = function(msg) {
-				DpErrorLog.logError(jsDump.parse(arguments) + "\n" + printStackTrace().join("\n"));
-				oldConsole.apply(oldConsole, arguments);
+				var args = Array.prototype.slice.call(arguments);
+				DpErrorLog.logError(jsDump.parse(args) + "\n" + printStackTrace().join("\n"));
+				oldConsole.apply(window.console, args);
 			}
 		}
 	},
@@ -48,9 +49,24 @@ var DpErrorLog = {
 		DpErrorLog.logError(message + ' (' + script + ' on line ' + line + ')');
 
 		if (this._origHandler) {
-			this._origHandler.apply(this._origHandler, arguments);
+			var args = Array.prototype.slice.call(arguments);
+			this._origHandler.apply(this._origHandler, args);
 		}
 
 		return true;
 	}
 };
+
+
+var oldTrigger = jQuery.event.trigger;
+jQuery.event.trigger = function() {
+	var begin = new Date();
+
+	var args = Array.prototype.slice.call(arguments);
+	oldTrigger.apply(jQuery.event, args);
+
+	var time = (new Date()).getTime() - begin.getTime();
+	if (time > 150) {
+		DpErrorLog.logError("Event took "+time+"ms: " + jsDump.parse(args) + "\n" + printStackTrace().join("\n"));
+	}
+}
