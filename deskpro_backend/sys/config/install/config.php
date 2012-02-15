a099bec735a10d66163aa9a1770381f0c03997db
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-02-15 14:21:43 +0000

    Use flat annotations file for installer too

--- deskpro_backend/sys/config/install/config.php
+++ deskpro_backend/sys/config/install/config.php
@@ -30,16 +30,40 @@ $definition->setArguments(array(
 $definition->addMethodCall('setContainer', array(new Reference('service_container')));
 $container->setDefinition('doctrine.dbal.connection_factory', $definition);
 
+// deskpro.cache.annotations
+$definition = new Definition();
+$definition->setClass('Orb\\Doctrine\\Common\\Cache\\ArrayFileCache');
+$definition->setArguments(array(
+	'%kernel.cache_dir%/../annotations.php'
+));
+$definition->addMethodCall('registerShutdownCommit');
+$container->setDefinition('deskpro.cache.annotations', $definition);
+
 // annotations.file_cache_reader
 $definition = new Definition();
-$definition->setClass('Doctrine\\Common\\Annotations\\FileCacheReader');
+$definition->setClass('Doctrine\\Common\\Annotations\\CachedReader');
 $definition->setPublic(false);
 $definition->setArguments(array(
 	new Reference('annotations.reader'),
-	'%kernel.cache_dir%/../annotations',
+	new Reference('deskpro.cache.annotations'),
 	false
 ));
 $container->setDefinition('annotations.file_cache_reader', $definition);
+
+// deskpro.interface_value
+$definition = new Definition();
+$definition->setClass('Application\\DeskPRO\\InterfaceValue');
+$container->setDefinition('deskpro.interface_value', $definition);
+
+// doctrine.orm.default_query_cache
+$definition = new Definition();
+$definition->setClass('Orb\\Doctrine\\Common\\Cache\\PreloadedMysqlCache');
+$definition->setArguments(array(
+	new Reference('database_connection')
+));
+$definition->addMethodCall('setPrefix', array('dql', new Reference('deskpro.interface_value')));
+$definition->addMethodCall('preloadPrefix', array(new Reference('deskpro.interface_value')));
+$container->setDefinition('doctrine.orm.default_query_cache', $definition);
 
 ############################################################################
 # Framework Configuration
