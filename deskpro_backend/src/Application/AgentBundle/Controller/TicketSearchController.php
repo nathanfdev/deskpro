499bf2345563ad446f206730b75dff634d389603
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-02-16 17:20:11 +0000

    Fix defaults for table view, checkbox cell width

--- deskpro_backend/src/Application/AgentBundle/Controller/TicketSearchController.php
+++ deskpro_backend/src/Application/AgentBundle/Controller/TicketSearchController.php
@@ -730,7 +730,7 @@ class TicketSearchController extends AbstractController
 
 	protected function _suggestedDisplayFields(TicketSearch $searcher)
 	{
-		$display_fields = array('department', 'agent', 'agent_team');
+		$display_fields = array('subject', 'user', 'department', 'agent', 'agent_team');
 
 		$specific_fields = $searcher->getSpecificFields();
 
