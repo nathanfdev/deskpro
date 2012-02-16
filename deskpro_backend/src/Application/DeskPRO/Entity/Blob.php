10b4cc79c2f9ff9cdbca1fe39afc819b82e471b1
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-02-16 16:23:34 +0000

    Fix a bunch of attachments paths now that filename is a mandatory param

--- deskpro_backend/src/Application/DeskPRO/Entity/Blob.php
+++ deskpro_backend/src/Application/DeskPRO/Entity/Blob.php
@@ -287,7 +287,7 @@ class Blob extends \Application\DeskPRO\Domain\DomainObject
 		if (!$this->isImage()) {
 			throw new \InvalidArgumentException("You can't get a thumbnail for a non-image");
 		}
-		return App::get('router')->generate('serve_blob', array('blob_auth_id' => $this->getAuthId(), 's' => $size), $absolute);
+		return App::get('router')->generate('serve_blob', array('blob_auth_id' => $this->getAuthId(), 'filename' => $this->filename, 's' => $size), $absolute);
 	}
 
 	/**
--- deskpro_backend/src/Application/DeskPRO/Entity/Organization.php
+++ deskpro_backend/src/Application/DeskPRO/Entity/Organization.php
@@ -303,6 +303,7 @@ class Organization extends \Application\DeskPRO\Domain\DomainObject
 		if ($this->picture_blob) {
 			$url = App::get('router')->generate('serve_blob', array(
 				'blob_auth_id' => $this->picture_blob->getAuthId(),
+				'filename' => $this->picture_blob->filename,
 				's' => $size
 			), true);
 		}
