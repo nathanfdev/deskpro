8827610cb4859a63590509768d339fa82f27f945
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-02-23 15:54:22 +0000

    Use getBasePath when getting path to /file.php

--- app/src/Application/DeskPRO/HttpFoundation/Request.php
+++ app/src/Application/DeskPRO/HttpFoundation/Request.php
@@ -99,15 +99,4 @@ class Request extends \Symfony\Component\HttpFoundation\Request
 
 		return $this->url_locale;
 	}
-
-	public function getServerBaseUrl()
-	{
-		$base = parent::getBaseUrl();
-
-		if (strpos($base, 'index.php') === false && !(isset($GLOBALS['DP_CONFIG']['rewrite_urls']) && $GLOBALS['DP_CONFIG']['rewrite_urls'])) {
-			$base .= '/index.php';
-		}
-
-		return $base;
-	}
 }
