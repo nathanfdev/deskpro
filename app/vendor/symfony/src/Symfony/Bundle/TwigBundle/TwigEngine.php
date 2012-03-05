965abdc0c22f907ad433b10d94a57332bd9367ca
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-03-05 13:59:07 +0000

    Update symfony files

--- app/vendor/symfony/src/Symfony/Bundle/TwigBundle/TwigEngine.php
+++ app/vendor/symfony/src/Symfony/Bundle/TwigBundle/TwigEngine.php
@@ -7,8 +7,6 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * adding this line to change filesize
  */
 
 namespace Symfony\Bundle\TwigBundle;
