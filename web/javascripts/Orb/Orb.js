eb889a71f05ed5ba7a88383fe0ef4e735976dd68
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-03-07 10:00:20 +0000

    Add some methods for getting, setting and inserting text are the carot

--- web/javascripts/Orb/Orb.js
+++ web/javascripts/Orb/Orb.js
@@ -585,29 +585,62 @@ $.fn.single_double_click = function(single_click_callback, double_click_callback
 
 
 jQuery.fn.extend({
-insertAtCaret: function(myValue){
-  return this.each(function(i) {
-    if (document.selection) {
-      this.focus();
-      sel = document.selection.createRange();
-      sel.text = myValue;
-      this.focus();
-    }
-    else if (this.selectionStart || this.selectionStart == '0') {
-      var startPos = this.selectionStart;
-      var endPos = this.selectionEnd;
-      var scrollTop = this.scrollTop;
-      this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
-      this.focus();
-      this.selectionStart = startPos + myValue.length;
-      this.selectionEnd = startPos + myValue.length;
-      this.scrollTop = scrollTop;
-    } else {
-      this.value += myValue;
-      this.focus();
-    }
-  })
-}
+	insertAtCaret: function(myValue) {
+		return this.each(function(i) {
+			if (document.selection) {
+				this.focus();
+				sel = document.selection.createRange();
+				sel.text = myValue;
+				this.focus();
+			}
+			else if (this.selectionStart || this.selectionStart == '0') {
+				var startPos = this.selectionStart;
+				var endPos = this.selectionEnd;
+				var scrollTop = this.scrollTop;
+				this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
+				this.focus();
+				this.selectionStart = startPos + myValue.length;
+				this.selectionEnd = startPos + myValue.length;
+				this.scrollTop = scrollTop;
+			} else {
+				this.value += myValue;
+				this.focus();
+			}
+		});
+	},
+
+	setCaretPosition: function(caretPos) {
+		return $.proxy(function() {
+			if(this.createTextRange) {
+				var range = this.createTextRange();
+				range.move('character', caretPos);
+				range.select();
+			}
+			else {
+				if(this.selectionStart) {
+					this.focus();
+					this.setSelectionRange(caretPos, caretPos);
+				}
+				else {
+					this.focus();
+				}
+			}
+		}, this.get(0));
+	},
+
+	getCaretPosition: function() {
+		return $.proxy(function() {
+			if (document.selection && document.selection.createRange) {
+				var range = document.selection.createRange();
+				var bookmark = range.getBookmark();
+				return bookmark.charCodeAt(2) - 2;
+			} else if (this.setSelectionRange) {
+				return this.selectionStart;
+			} else {
+				return 0;
+			}
+		}, this.get(0));
+	}
 });
 
 
