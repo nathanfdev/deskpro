f7708f031c38a8ac9ed9e7fea63dfbe7081a8ba8
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-03-09 15:23:56 +0000

    Assign dp3 refs when importing as easy way to prevent dupes without a lookup call

--- app/src/Application/DeskPRO/Import/Importer/Step/Deskpro3/TicketsStep.php
+++ app/src/Application/DeskPRO/Import/Importer/Step/Deskpro3/TicketsStep.php
@@ -233,7 +233,7 @@ class TicketsStep extends AbstractDeskpro3Step
 			'language_id' => 1,
 			'ticket_hash' => sha1(microtime(true) . mt_rand(1000,99999)), // bogus hash
 			'date_created' => date('Y-m-d H:i:s', $ticket_info['timestamp_opened']),
-			'ref' => \Orb\Util\Strings::random(8, \Orb\Util\Strings::CHARS_KEY),
+			'ref' => $ticket_info['ref'],
 			'auth' => \Orb\Util\Strings::random(6, \Orb\Util\Strings::CHARS_KEY),
 			'urgency' => 1,
 		);
