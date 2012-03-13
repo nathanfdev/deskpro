e703806e913ec8243df6ff302857b8b3e119791c
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-03-13 18:59:50 +0000

    Add command to refill the active table

--- app/src/Application/DeskPRO/EntityRepository/Ticket.php
+++ app/src/Application/DeskPRO/EntityRepository/Ticket.php
@@ -332,19 +332,19 @@ class Ticket extends AbstractEntityRepository
 			INSERT INTO tickets_search_active (
 				`id`, `language_id`, `department_id`, `category_id`,
 				`priority_id`, `workflow_id`, `product_id`, `person_id`, `email_gateway_id`,
-				`agent_id`, `agent_team_id`, `organization_id`, `status`, `is_hold`,
+				`agent_id`, `agent_team_id`, `organization_id`, `creation_system`, `status`, `is_hold`,
 				`urgency`, `date_created`, `date_first_agent_reply`, `date_last_agent_reply`,
 				`date_last_user_reply`, `date_agent_waiting`, `date_user_waiting`, `total_user_waiting`,
 				`total_to_first_reply`
 			) SELECT
 				`id`, `language_id`, `department_id`, `category_id`,
 				`priority_id`, `workflow_id`, `product_id`, `person_id`, `email_gateway_id`,
-				`agent_id`, `agent_team_id`, `organization_id`, `status`, `is_hold`,
+				`agent_id`, `agent_team_id`, `organization_id`, `creation_system`, `status`, `is_hold`,
 				`urgency`, `date_created`, `date_first_agent_reply`, `date_last_agent_reply`,
 				`date_last_user_reply`, `date_agent_waiting`, `date_user_waiting`, `total_user_waiting`,
 				`total_to_first_reply`
 			FROM tickets
-			WHERE status IN ('awaiting_agent', 'awaiting_user')
+			WHERE status IN ('awaiting_agent', 'awaiting_user', 'resolved')
 		");
 	}
 
