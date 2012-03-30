a40f38fecf0e90dc1ba4a278bb5be666d8f0f514
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-03-30 08:57:46 +0100

    Add kernel.request event listener to handle case when portal is turned off completely

--- app/sys/config/user/config.php
+++ app/sys/config/user/config.php
@@ -19,6 +19,15 @@ $definition->setArguments(array(
 $definition->addTag('twig.extension', array());
 $container->setDefinition('twig.helpers.deskpro_user_templating', $definition);
 
+// deskpro.user.portal_off_event
+$definition = new Definition();
+$definition->setClass('Application\\UserBundle\\HttpKernel\\PortalOffEvent');
+$definition->setArguments(array(
+	new Reference('service_container')
+));
+$definition->addTag('kernel.event_listener', array('event' => 'kernel.request', 'method' => 'onKernelRequest'));
+$container->setDefinition('deskpro.user.portal_off_event', $definition);
+
 $container->loadFromExtension('framework', array(
 	'router' => array(
 		'resource' => DP_ROOT.'/sys/config/user/routing.php'
--- app/sys/config/user/config_dev.php
+++ app/sys/config/user/config_dev.php
@@ -48,3 +48,12 @@ $container->loadFromExtension('monolog', array(
 		)
 	)
 ));
+
+// deskpro.user.portal_off_event
+$definition = new Definition();
+$definition->setClass('Application\\UserBundle\\HttpKernel\\PortalOffEvent');
+$definition->setArguments(array(
+	new Reference('service_container')
+));
+$definition->addTag('kernel.event_listener', array('event' => 'kernel.request', 'method' => 'onKernelRequest'));
+$container->setDefinition('deskpro.user.portal_off_event', $definition);
