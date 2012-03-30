7ac8f009c11028eeafa6ca4cf8311c6c17be1827
Chris Padfield <chris.padfield@deskpro.com>
2012-03-30 14:00:34 +0100

    - quick tool to count words and phrases

---
+++ app/src/Application/DevBundle/Command/WordCountCommand.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @category Commands
+ * @copyright Copyright (c) 2010 DeskPRO (http://www.deskpro.com/)
+ * @license http://www.deskpro.com/license-agreement DeskPRO License
+ * @author Christopher Nadeau <chris.nadeau@deskpro.com>
+ */
+
+namespace Application\DevBundle\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+
+use Application\DeskPRO\App;
+
+use Orb\Util\Arrays;
+use Orb\Util\Strings;
+
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Routing\Route;
+
+class WordCountCommand extends \Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand
+{
+	protected function configure()
+	{
+		$this->setDefinition(array(
+		))->setName('dpdev:word-count');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+
+        $directories = array(
+            array('user', 'User Interface'),
+            array('agent', 'Agent Interface'),
+            array('admin', 'Admin Interface')
+        );
+
+        foreach ($directories AS $interface) {
+
+            $keycount = 0;
+            $wordcount = 0;
+
+            $dir = DP_ROOT . '/languages/DeskPRO/' . $interface['0'];
+
+            if ($handle = opendir($dir)) {
+                while (false !== ($filename = readdir($handle))) {
+                    if ($filename != "." && $filename != "..") {
+
+                        $words = include($dir . '/' . $filename);
+                        $keycount += count($words);
+                        foreach ($words AS $key => $var) {
+                            $wordcount += str_word_count($var);
+                        }
+                    }
+                }
+
+                closedir($handle);
+
+                echo $interface['1'] . " :: $wordcount words in $keycount phrases\n";
+            }
+        }
+	}
+}
