3a59d0c1d8fcbc7199523b301a2584cc756e8ee2
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-03-30 13:33:42 +0100

    Fix off by one z-index

--- app/sys/Resources/DoctrineMigrations/Version20120327101446.php
+++ app/sys/Resources/DoctrineMigrations/Version20120327101446.php
@@ -9,7 +9,6 @@ class Version20120327101446 extends AbstractMigration
 {
 	public function up(Schema $schema)
 	{
-		$this->addSql("DROP INDEX name_idx ON settings");
 		$this->addSql("ALTER TABLE settings DROP id, DROP groupname, DROP default_value, DROP created_at, DROP updated_at, CHANGE name name VARCHAR(255) NOT NULL, CHANGE value value VARBINARY(10000) DEFAULT NULL");
 		$this->addSql("ALTER TABLE settings ADD PRIMARY KEY (name)");
 	}
