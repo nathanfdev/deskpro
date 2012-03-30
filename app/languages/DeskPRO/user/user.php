8aad7f8b6a7c52a37160ad1eeb8183b37e4799eb
Chris Padfield <chris.padfield@deskpro.com>
2012-03-30 14:00:11 +0100

    - fix parse error in language file

--- app/languages/DeskPRO/user/user.php
+++ app/languages/DeskPRO/user/user.php
@@ -18,7 +18,7 @@
 	'user.all' => 'All',
 
 	'user.content_articles' => 'Articles',
-	'user.content_help_articles' => 'Help Articles', {# Just use above name instead? #}
+	'user.content_help_articles' => 'Help Articles', # Just use above name instead? #
 	'user.content_feedback' => 'Feedback',
 	'user.content_downloads' => 'Downloads',
 	'user.content_news' => 'News',
