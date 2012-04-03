f79c440cbefbc6a5685524848d94797c03528a02
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-04-03 19:49:56 +0100

    Only reset log when running, not during sub-commands

--- app/src/Application/DeskPRO/Command/ImportCommand.php
+++ app/src/Application/DeskPRO/Command/ImportCommand.php
@@ -111,7 +111,7 @@ class ImportCommand extends \Symfony\Bundle\FrameworkBundle\Command\ContainerAwa
 		$log_file_path = $this->getContainer()->getKernel()->getUserLogDir() . '/import.log';
 		try {
 			if (!(isset($DP_CONFIG['import']['nolog']) && $DP_CONFIG['import']['nolog'])) {
-				$wr = new \Orb\Log\Writer\Stream($log_file_path, 'w');
+				$wr = new \Orb\Log\Writer\Stream($log_file_path, $mode == 'run' ? 'w' : 'a');
 			} else {
 				$logger->disabled = true;
 			}
