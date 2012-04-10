00f8baff4e4b26d7c904b32aa510a2e0b941086a
Joey Bartlett <joey.bartlett@deskpro.com>
2012-04-10 12:01:48 +0100

    Merged ticket related language files in admin language package into single file.

--- app/languages/admin/ticketfeatures.php
+++
@@ -1,5 +0,0 @@
-<?php return array (
-  'admin.ticketfeatures.ticket_features_and_settings' => 'Ticket Features and Settings',
-  'admin.ticketfeatures.enable_ticket_feedback' => 'Enable ticket feedback?',
-  'admin.ticketfeatures.let_agents_read_ticket_feedback' => 'Let agents read ticket feedback',
-);
