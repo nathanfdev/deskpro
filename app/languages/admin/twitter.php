cbd8dd722525f5a6361b307d1e3be2d1b5f26b31
Chris Padfield <chris.padfield@deskpro.com>
2012-04-10 13:27:00 +0100

    adding files

---
+++ app/languages/admin/twitter.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'admin.twitter.error' => 'Error',
+  'admin.twitter.oauth_token' => 'OAuth Token',
+  'admin.twitter.oauth_token_secret' => 'OAuth Token Secret',
+  'admin.twitter.oauth' => 'OAuth',
+  'admin.twitter.token' => 'Token',
+  'admin.twitter.secret' => 'Secret',
+  'admin.twitter.create_new_account' => 'Create New Account',
+  'admin.twitter.you_have_no_accounts_defined' => 'You have no accounts defined.',
+);
