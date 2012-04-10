8764b81dd28dbb487655f8633b1ba059790710a6
Joey Bartlett <joey.bartlett@deskpro.com>
2012-04-10 12:49:03 +0100

    Merged agent.common into agent.

--- app/languages/user/downloads.php
+++ app/languages/user/downloads.php
@@ -1,17 +1,12 @@
 <?php return array(
+    'user.downloads.error_not_found' => 'The file you\'re looking for could not be found. It may have been deleted.',
+    'user.downloads.category' => 'Folder',
+    'user.downloads.view_all_x' => 'View downloads|View all {{count}} downloads',
+    'user.downloads.label_download' => 'Download',
 
+    'user.downloads.no_downloads' => 'There are no downloads yet.',
+    'user.downloads.no_popular' => 'There are no popular downloads yet.',
 
-	'user.downloads.error_not_found' => 'The file you\'re looking for could not be found. It may have been deleted.',
-	'user.downloads.category' => 'Folder',
-	'user.downloads.view_all_x' => 'View downloads|View all {{count}} downloads',
-	'user.downloads.label_download' => 'Download',
-
-	'user.downloads.no_downloads' => 'There are no downloads yet.',
-	'user.downloads.no_popular' => 'There are no popular downloads yet.',
-
-	'user.downloads.popular_section_name' => 'Popular Downloads',
-	'user.downloads.recent_section_name' => 'Recent Downloads',
-
-
-
+    'user.downloads.popular_section_name' => 'Popular Downloads',
+    'user.downloads.recent_section_name' => 'Recent Downloads',
 );
--- app/languages/user/news.php
+++ app/languages/user/news.php
@@ -3,9 +3,7 @@
     'user.news.view_all_x_news' => 'View all entries|View all {{count}} entries',
 
     'user.news.view_rest_of_article_link' => 'Read the rest of this news article',
-    'user.news.count_words_remaining' => '({{count}} more words)',
-
-    'user.news._comments' => '| Comments',
+    'user.news.count_words_remaining' => '({{count}} more word)|({{count}} more words)',
 
     'user.news.author_name' => 'Author: {{name}}',
 );
