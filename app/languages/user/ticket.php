a301c68d5e08001e8f55be7a6e28b65399d106bc
Joey Bartlett <joey.bartlett@deskpro.com>
2012-04-10 10:18:43 +0100

    Merged ticket and tickets language files for user bundle.

--- app/languages/user/ticket.php
+++
@@ -1,5 +0,0 @@
-<?php return array (
-'user.ticket.modify_ticket' => 'Modify Ticket',
-  'user.ticket.leave_feedback_title' => 'Feedback Title',
-  'user.ticket.leave_feedback_text_title' => 'Feedback Content',
-);
