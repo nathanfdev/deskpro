045ea0e5b92be72cbab6ea5f1e482737dadfe273
Joey Bartlett <joey.bartlett@deskpro.com>
2012-04-10 12:03:32 +0100

    Renamed billing language file in admin.

--- app/languages/admin/billing.main.php
+++
@@ -1,4 +0,0 @@
-<?php return array (
-  'admin.billing.main.deskpro_billing_interface' => 'DeskPRO Billing Interface',
-  'admin.billing.main.coming_soon' => 'Coming Soon',
-);
---
+++ app/languages/admin/billing.php
@@ -0,0 +1,4 @@
+<?php return array (
+  'admin.billing.deskpro_billing_interface' => 'DeskPRO Billing Interface',
+  'admin.billing.coming_soon' => 'Coming Soon',
+);
