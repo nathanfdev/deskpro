de68af50d132e9be08362b0c4686197a35fa5f46
Joey Bartlett <joey.bartlett@deskpro.com>
2012-04-11 14:16:24 +0100

    Repositioned user menu in admin bundle.

--- web/javascripts/DeskPRO/Admin/Window.js
+++ web/javascripts/DeskPRO/Admin/Window.js
@@ -194,7 +194,7 @@ DeskPRO.Admin.Window = new Orb.Class({
 			list.hide().detach().appendTo('body');
 
 			var left = $('#userSetting_trigger').offset().left;
-			left -= 126;
+			left -= 136;
 
 			list.css({
 				top: 41,
