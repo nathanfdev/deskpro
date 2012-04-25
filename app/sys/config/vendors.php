3ec14f956e5637dc618d9c7db9dd837eb0bdf8da
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-04-25 16:32:18 +0100

    Update Doctrine to 2.2.2

--- app/sys/config/vendors.php
+++ app/sys/config/vendors.php
@@ -12,17 +12,17 @@
 	'doctrine' => array(
 		'into' => DP_ROOT.'/vendor/doctrine',
 		'repos' => 'git://github.com/doctrine/doctrine2.git',
-		'version' => '2.2.1',
+		'version' => '2.2.2',
 	),
 	'doctrine_common' => array(
 		'into' => DP_ROOT.'/vendor/doctrine-common',
 		'repos' => 'git://github.com/doctrine/common.git',
-		'version' => '2.2.1',
+		'version' => '2.2.2',
 	),
 	'doctrine_dbal' => array(
 		'into' => DP_ROOT.'/vendor/doctrine-dbal',
 		'repos' => 'git://github.com/doctrine/dbal.git',
-		'version' => '2.2.1',
+		'version' => '2.2.2',
 	),
 	'doctrine_migrations' => array(
 		'into' => DP_ROOT.'/vendor/doctrine-migrations',
