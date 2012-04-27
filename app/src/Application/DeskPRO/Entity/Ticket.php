5c30972b22fd9caf8c72a8905d2753e2b4a7d725
Joey Bartlett <joey.bartlett@deskpro.com>
2012-04-27 13:24:37 +0100

    Quick fix for urgency setter.

--- app/src/Application/DeskPRO/Entity/Ticket.php
+++ app/src/Application/DeskPRO/Entity/Ticket.php
@@ -1481,9 +1481,12 @@ class Ticket extends \Application\DeskPRO\Domain\DomainObject
 			$this->undeleteTicket();
 		}
 
+		if($status != self::STATUS_AWAITING_AGENT) {
+			$this->setModelField('urgency', 0);
+		}
+
 		if ($this->is_hold && $status != self::STATUS_AWAITING_AGENT) {
 			$this->setModelField('is_hold', false);
-			$this->setModelField('urgency', 0);
 		}
 	}
 
