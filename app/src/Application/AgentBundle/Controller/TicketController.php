9be9cd6229a64ed93f68536c3be88f1ae761f404
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-04-30 17:40:08 +0100

    Dont allow merge between separate users

--- app/src/Application/AgentBundle/Controller/TicketController.php
+++ app/src/Application/AgentBundle/Controller/TicketController.php
@@ -1430,7 +1430,7 @@ class TicketController extends AbstractController
 		$open_tickets    = $this->em->getRepository('DeskPRO:Ticket')->getTicketsFromIds($this->in->getCleanValueArray('open_ticket_ids', 'uint', 'discard'));
 
 		$fn = function ($t) use ($ticket) {
-			if ($t['id'] == $ticket['id']) {
+			if ($t['id'] == $ticket['id'] or $t->person->getId() != $ticket->person->getId()) {
 				return false;
 			}
 			return true;
