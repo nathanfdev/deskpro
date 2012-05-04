d751ba3d55832187094e0bb550734a36729cb080
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-04 18:47:01 +0100

    Add placeholder compat for ie

--- web/javascripts/Orb/Compat.js
+++ web/javascripts/Orb/Compat.js
@@ -2,32 +2,32 @@ Orb.createNamespace('Orb.Compat.WebForms');
 
 Orb.Compat.WebForms.isPlaceholderSupported = function() {
 	this.isSupported = null;
-	
+
 	if (this.isSupported === null) {
-		this.isSupported = ('placeholder' in document.createElement(input.tagName));
+		this.isSupported = ('placeholder' in document.createElement('input'));
 	}
-	
+
 	return this.isSupported;
 };
 
 Orb.Compat.WebForms.placeholder = function(input) {
 
 	if (!input) return null;
-	
+
 	input_col = $(input);
 	if (!input_col.length) return null;
-	
+
 	input_col.each(function() {
 
 		var input = $(this);
-	
+
 		// Check if its already supported
 		if (input.placeholder && this.isPlaceholderSupported()) return;
-	
+
 		// Get the palceholder and check that its actually a value
 		var placeholder = input.attr('placeholder');
 		if (!placeholder || !placeholder.length) return;
-		
+
 		// Make sure we havent already run the placeholder compat
 		if (input.is('.has-placeholder')) return;
 
@@ -38,14 +38,14 @@ Orb.Compat.WebForms.placeholder = function(input) {
 			input.val(placeholder);
 			input.addClass('placeholder-visible');
 		}
-	
+
 		input.focus(function() {
 			if (input.is('.placeholder-visible')) {
 				input.val('');
 				input.removeClass('placeholder-visible');
 			}
 		});
-	
+
 		input.blur(function() {
 			if (input.val() === '') {
 				input.addClass('placeholder-visible');
@@ -54,7 +54,7 @@ Orb.Compat.WebForms.placeholder = function(input) {
 				input.removeClass('placeholder-visible');
 			}
 		});
-	
+
 		if (input.get(0).form) {
 			$(input.get(0).form).submit(function() {
 				if (input.is('.placeholder-visible')) {
