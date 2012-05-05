0de08de272f21dd352c47c3ba3175ce65c8efcbf
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-05 15:44:15 +0100

    Add simple config loader based on domain

---
+++ app/src/Application/DeskPRO/Cloud/CloudConfig.php
@@ -0,0 +1,52 @@
+<?php
+namespace Application\DeskPRO\Cloud;
+
+class CloudConfig
+{
+	public static function loadSiteConfig()
+	{
+		if (empty($_SERVER['HTTP_HOST'])) {
+			header('Location: ' . DP_CLOUD_MAIN_SITE);
+			exit;
+		}
+
+		try {
+
+			$pdo = new \PDO(
+				sprintf("mysql:host=%s;dbname=%s", DP_CLOUD_DATABASE_HOST, DP_CLOUD_DATABASE_NAME),
+				DP_CLOUD_DATABASE_USER,
+				DP_CLOUD_DATABASE_PASSWORD,
+				array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION)
+			);
+
+			$stmt = $pdo->prepare("SELECT * FROM cloud_sites WHERE master_domain = ? OR custom_domain = ? LIMIT 1");
+			$stmt->execute(array($_SERVER['HTTP_HOST'], $_SERVER['HTTP_HOST']));
+			$site = $stmt->fetch();
+			if (!$site) {
+				header('Location: ' . DP_CLOUD_MAIN_SITE);
+				exit;
+			}
+
+			unset($pdo);
+
+			define('DP_DATABASE_HOST',       $site['db_host']);
+			define('DP_DATABASE_USER',       $site['db_user']);
+			define('DP_DATABASE_PASSWORD',   $site['db_password']);
+			define('DP_DATABASE_NAME',       $site['db_name']);
+			define('DP_TECHNICAL_EMAIL',     'team@deskpro.com');
+
+			global $DP_CONFIG;
+			$DP_CONFIG['debug'] = array();
+			$DP_CONFIG['debug']['dev'] = true;
+			$DP_CONFIG['debug']['raw_assets'] = array('all');
+		} catch (\Exception $e) {
+			self::handleException($e);
+		}
+	}
+
+	public static function handleException(\Exception $e)
+	{
+		echo "There was a server error.<br />";
+		echo $e->getMessage();
+	}
+}
