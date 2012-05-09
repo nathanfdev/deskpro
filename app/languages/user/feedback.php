53e99792355e42c99c34ca39e5758a0cb2e18394
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-09 10:25:02 +0100

    Most votes

--- app/languages/user/feedback.php
+++ app/languages/user/feedback.php
@@ -11,7 +11,7 @@
     'user.feedback.i_voted'                   => 'I Voted',
     'user.feedback.label_explain_feedback'    => 'Explain your feedback',
     'user.feedback.label_summary_explain'     => 'A title or a brief summary of what your feedback is about',
-    'user.feedback.most_voted'                => 'Most Voted',
+    'user.feedback.most_votes'                => 'Most Votes',
     'user.feedback.newest'                    => 'Newest',
     'user.feedback.status_active'             => 'Active',
     'user.feedback.status_all'                => 'All',
