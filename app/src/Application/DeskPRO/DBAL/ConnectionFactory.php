5b1e4260195f720e9236264361cbbd6402dc85f1
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-10 10:09:15 +0100

    Better handling for invalid array data

--- app/src/Application/DeskPRO/DBAL/ConnectionFactory.php
+++ app/src/Application/DeskPRO/DBAL/ConnectionFactory.php
@@ -58,6 +58,7 @@ class ConnectionFactory extends \Symfony\Bundle\DoctrineBundle\ConnectionFactory
 		parent::__construct($typesConfig);
 		\Doctrine\DBAL\Types\Type::addType('dpblob', 'Application\\DeskPRO\\DBAL\\Types\\DpBlobType');
 		\Doctrine\DBAL\Types\Type::addType('dpblob_file', 'Application\\DeskPRO\\DBAL\\Types\\DpBlobFileType');
+		\Doctrine\DBAL\Types\Type::overrideType('array', 'Application\\DeskPRO\\DBAL\\Types\\DpArrayType');
 	}
 
 	public function setContainer(ContainerInterface $container = null)
