a22df2162ee1c125d3d0cdc71ba125a361602941
Chris Padfield <chris.padfield@deskpro.com>
2012-05-11 18:56:21 +0100

    migrate search into portal

--- app/languages/user/portal.php
+++ app/languages/user/portal.php
@@ -4,4 +4,11 @@
     'user.portal.twitter_user_link'       => '@{{name}} on twitter',
     'user.portal.twitter_view_more'       => 'View more from @{{name}} &rarr;',
     'user.portal.we_are_here_to_help'     => 'Our friendly support staff are here to help.',
+    'user.portal.find_tagged_with'    => 'Find pages tagged with',
+    'user.portal.labels_section_name' => 'Search Tags',
+    'user.portal.no_results_help'     => 'No results, try using different terms.',
+    'user.portal.x_search_results'    => '{{count}} search result|{{count}} search results',
+	'user.portal.more_labels'         => 'More Labels',
+	'user.portal.searchbox_show_type_prefix' => 'Show',
+	'user.portal.searchbox_show_label_prefox' => 'labelled with',
 );
