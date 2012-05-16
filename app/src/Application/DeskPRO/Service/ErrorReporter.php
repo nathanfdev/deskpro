91e5c61af1e9c56d02eb5c8b25ef402f73b5ba1e
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-16 22:33:33 +0100

    Fix cron notice showing up during welcome, fix url to install ping service

--- app/src/Application/DeskPRO/Service/ErrorReporter.php
+++ app/src/Application/DeskPRO/Service/ErrorReporter.php
@@ -336,7 +336,7 @@ class ErrorReporter
 		try {
 			$client = new \Zend\Http\Client(null, array('timeout' => 5, 'strictredirects' => true));
 			$client->setMethod(\Zend\Http\Request::METHOD_POST);
-			$client->setUri(\DeskPRO\Kernel\License::getLicServer() . '/api//data-submit/ping-install.json');
+			$client->setUri(\DeskPRO\Kernel\License::getLicServer() . '/api/data-submit/ping-install.json');
 			$client->getRequest()->post()->fromArray($data);
 			$r = $client->send();
 		} catch (\Exception $e) {}
