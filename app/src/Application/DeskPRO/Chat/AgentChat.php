099d0df30b518e5f15e7c8e18d9b24f0f65ab86e
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-24 19:43:58 +0100

    Fix error 305: Varname conflicts

--- app/src/Application/DeskPRO/Chat/AgentChat.php
+++ app/src/Application/DeskPRO/Chat/AgentChat.php
@@ -117,7 +117,7 @@ class AgentChat
 		});
 
 		// If any of the targets are not online, we might need to nofigy them of the message via email
-		if (!$message->is_sys) {
+		if (!$chat_message->is_sys) {
 			foreach ($conversation->participants as $part) {
 				if ($part['id'] == $this->person['id']) {
 					continue;
@@ -125,13 +125,13 @@ class AgentChat
 				$session = App::getOrm()->getRepository('DeskPRO:Session')->getSessionForPerson($part);
 
 				if ($session && $part->getPref('agent_notif.chat_message.email')) {
-					$message = App::getMailer()->createMessage();
-					$message->setTemplate('DeskPRO:emails_agent:new-agent-chat-message.html.twig', array(
-						'message' => $message
+					$email_message = App::getMailer()->createMessage();
+					$email_message->setTemplate('DeskPRO:emails_agent:new-agent-chat-message.html.twig', array(
+						'message' => $chat_message
 					));
-					$message->setToPerson($part);
-					$message->enableQueueHint();
-					App::getMailer()->send($message);
+					$email_message->setToPerson($part);
+					$email_message->enableQueueHint();
+					App::getMailer()->send($email_message);
 				}
 			}
 		}
