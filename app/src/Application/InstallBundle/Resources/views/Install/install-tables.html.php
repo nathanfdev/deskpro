47489a6a6529cb841ea345b9431fdb66236252f4
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-25 11:20:24 +0100

    Bad copypaste on 'next' button made it disbaled

--- app/src/Application/InstallBundle/Resources/views/Install/install-tables.html.php
+++ app/src/Application/InstallBundle/Resources/views/Install/install-tables.html.php
@@ -166,7 +166,7 @@ $(document).ready(function() {
 		<strong>Done!</strong> You're ready to go to the next step.
 
 		<div class="alert-actions submit-area">
-			<a class="btn disabled" tabindex="2" id="next_btn" href="<?php echo $view['router']->generate('install_checks') ?>" onclick="if (!$(this).hasClass('disabled')) { $(this).parent().addClass('clicked'); }">Go to step 6: Create your admin account</a>
+			<a class="btn" tabindex="2" id="next_btn" href="<?php echo $view['router']->generate('install_install_data') ?>" onclick="if (!$(this).hasClass('disabled')) { $(this).parent().addClass('clicked'); }">Go to step 6: Create your admin account</a>
 			<span class="next-loading"></span>
 		</div>
 	</div>
