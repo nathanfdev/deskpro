2052b5a2f5129f78cd1e63e3a45bc3a9b861c8c0
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-30 19:27:49 +0100

    Update twig to 1.8.2

--- app/sys/config/vendors.php
+++ app/sys/config/vendors.php
@@ -84,7 +84,7 @@
 	'twig' => array(
 		'into' => DP_ROOT.'/vendor/twig',
 		'repos' => 'git://github.com/fabpot/Twig.git',
-		'version' => 'v1.8.1',
+		'version' => 'v1.8.2',
 	),
 	'zend' => array(
 		'into' => DP_ROOT.'/vendor/zend',
