bea0459c1666adf610ca739efac696fa3dbce949
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-30 15:41:03 +0100

    Fix importer deleting example feedback types when imported site didnt use feedback; resulted in no cats

--- app/src/Application/DeskPRO/Import/Importer/Step/Deskpro3/FeedbackCatsStep.php
+++ app/src/Application/DeskPRO/Import/Importer/Step/Deskpro3/FeedbackCatsStep.php
@@ -51,18 +51,19 @@ class FeedbackCatsStep extends AbstractDeskpro3Step
 			return;
 		}
 
-		// If there arent any ideas besides the default, delete default data
-		$default_check = $this->getDb()->fetchColumn("SELECT id FROM feedback ORDER BY id DESC LIMIT 1");
-		$default_check2 = $this->getDb()->fetchColumn("SELECT id FROM feedback_categories ORDER BY id DESC LIMIT 1");
-		if (!$default_check || $default_check == 1 && (!$default_check2 || $default_check2 == 2)) {
-			$this->getDb()->exec("SET FOREIGN_KEY_CHECKS = 0");
-			$this->getDb()->exec("DELETE FROM feedback");
-			$this->getDb()->exec("DELETE FROM feedback_categories");
-			$this->getDb()->exec("SET FOREIGN_KEY_CHECKS = 0");
-		}
-
 		$count = $this->getOldDb()->fetchColumn("SELECT COUNT(*) FROM user_idea_categories");
 		if ($count) {
+
+			// Deleting the default example data because we're importing them
+			$default_check = $this->getDb()->fetchColumn("SELECT id FROM feedback ORDER BY id DESC LIMIT 1");
+			$default_check2 = $this->getDb()->fetchColumn("SELECT id FROM feedback_categories ORDER BY id DESC LIMIT 1");
+			if (!$default_check || $default_check == 1 && (!$default_check2 || $default_check2 == 2)) {
+				$this->getDb()->exec("SET FOREIGN_KEY_CHECKS = 0");
+				$this->getDb()->exec("DELETE FROM feedback");
+				$this->getDb()->exec("DELETE FROM feedback_categories");
+				$this->getDb()->exec("SET FOREIGN_KEY_CHECKS = 0");
+			}
+
 			$this->logMessage(sprintf("Importing %d feedback categories", $count));
 
 			$start_time = microtime(true);
