3a1e8983bbad19eba2f180f70e97044466f16287
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-30 19:13:57 +0100

    Update symfony to 2.0.15

--- app/sys/config/vendors.php
+++ app/sys/config/vendors.php
@@ -74,7 +74,7 @@
 	'symfony' => array(
 		'into' => DP_ROOT.'/vendor/symfony',
 		'repos' => 'git://github.com/symfony/symfony.git',
-		'version' => 'v2.0.14',
+		'version' => 'v2.0.15',
 	),
 	'symfony_doctrine_migrations' => array(
 		'into' => DP_ROOT.'/vendor/symfony/src/Symfony/Bundle/DoctrineMigrationsBundle',
