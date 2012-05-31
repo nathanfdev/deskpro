c0c1b469d80963d594bb92b38e98daee0c66be83
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-31 09:29:49 +0100

    Add user routing paths to reports kernel

--- app/sys/config/report/routing.php
+++ app/sys/config/report/routing.php
@@ -5,6 +5,7 @@ use Symfony\Component\Routing\Route;
 
 $collection = new RouteCollection();
 $collection->addCollection($loader->import(DP_ROOT.'/src/Application/DeskPRO/Resources/config/dp-routing.php'));
+$collection->addCollection($loader->import(DP_ROOT.'/src/Application/UserBundle/Resources/config/user-routing.php'));
 $collection->addCollection($loader->import(DP_ROOT.'/src/Application/ReportBundle/Resources/config/reports-routing.php'), '/reports');
 
 return $collection;
