284f4b915824e847a40a9bfa19137ad0bd7693e4
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-05-31 16:17:37 +0100

    Customise render of Twig's form rendering to skip check for dupe renders.
    By default form widgets won't render if you call render a second time.
    This is annoying. We re-use form objects sometimes for display reasons,
    so having this doesn't play well. If I want to render a widget twice,
    then I should be able to!

--- app/src/Application/DeskPRO/Twig/Extension/FormExtension.php
+++ app/src/Application/DeskPRO/Twig/Extension/FormExtension.php
@@ -34,6 +34,10 @@
 
 namespace Application\DeskPRO\Twig\Extension;
 
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\Exception\FormException;
+use Symfony\Component\Form\Util\FormUtil;
+
 class FormExtension extends \Symfony\Bridge\Twig\Extension\FormExtension
 {
 	public function __construct(array $resources = array())
@@ -47,4 +51,61 @@ class FormExtension extends \Symfony\Bridge\Twig\Extension\FormExtension
 
 		parent::__construct($set_resources);
 	}
+
+	protected function render(FormView $view, $section, array $variables = array())
+    {
+        $mainTemplate = in_array($section, array('widget', 'row'));
+
+        if (null === $this->template) {
+            $this->template = reset($this->resources);
+            if (!$this->template instanceof \Twig_Template) {
+                $this->template = $this->environment->loadTemplate($this->template);
+            }
+        }
+
+        $custom = '_'.$view->get('id');
+        $rendering = $custom.$section;
+        $blocks = $this->getBlocks($view);
+
+        if (isset($this->varStack[$rendering])) {
+            $typeIndex = $this->varStack[$rendering]['typeIndex'] - 1;
+            $types = $this->varStack[$rendering]['types'];
+            $this->varStack[$rendering]['variables'] = array_replace_recursive($this->varStack[$rendering]['variables'], $variables);
+        } else {
+            $types = $view->get('types');
+            $types[] = $custom;
+            $typeIndex = count($types) - 1;
+            $this->varStack[$rendering] = array (
+                'variables' => array_replace_recursive($view->all(), $variables),
+                'types'     => $types,
+            );
+        }
+
+        do {
+            $types[$typeIndex] .= '_'.$section;
+
+            if (isset($blocks[$types[$typeIndex]])) {
+
+                $this->varStack[$rendering]['typeIndex'] = $typeIndex;
+
+                // we do not call renderBlock here to avoid too many nested level calls (XDebug limits the level to 100 by default)
+                ob_start();
+                $this->template->displayBlock($types[$typeIndex], $this->varStack[$rendering]['variables'], $blocks);
+                $html = ob_get_clean();
+
+                if ($mainTemplate) {
+                    $view->setRendered();
+                }
+
+                unset($this->varStack[$rendering]);
+
+                return $html;
+            }
+        } while (--$typeIndex >= 0);
+
+        throw new FormException(sprintf(
+            'Unable to render the form as none of the following blocks exist: "%s".',
+            implode('", "', array_reverse($types))
+        ));
+    }
 }
