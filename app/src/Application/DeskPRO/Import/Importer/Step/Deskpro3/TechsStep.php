2fbe91a38acc15cacea21a80c9ce2e10b8921fcd
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-06-27 21:49:33 +0100

    Fix adding admins to 'all permissions' usergroup, the id changed to 3

--- app/src/Application/DeskPRO/Import/Importer/Step/Deskpro3/TechsStep.php
+++ app/src/Application/DeskPRO/Import/Importer/Step/Deskpro3/TechsStep.php
@@ -138,7 +138,7 @@ class TechsStep extends AbstractDeskpro3Step
 				if ($agent->can_admin) {
 					$this->getDb()->insert('person2usergroups', array(
 						'person_id' => $agent->id,
-						'usergroup_id' => 2
+						'usergroup_id' => 3
 					));
 
 				// Otherwise we'll import perms into overrides
