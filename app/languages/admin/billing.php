5c02f1f796874a4b86607e3e3678e4fed5ee6950
Chris Padfield <chris.padfield@deskpro.com>
2012-06-27 20:53:09 +0100

    move from \r\n - \n for phrases files

--- app/languages/admin/billing.php
+++ app/languages/admin/billing.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'admin.billing.coming_soon'               => 'Coming Soon',
-	'admin.billing.deskpro_billing_interface' => 'DeskPRO Billing Interface',
-);
+<?php return array(	'admin.billing.coming_soon'               => 'Coming Soon',	'admin.billing.deskpro_billing_interface' => 'DeskPRO Billing Interface',);
