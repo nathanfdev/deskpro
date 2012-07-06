4cebaf4b02d7112d6f60174b022f4a7c847ed49e
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-07-06 14:34:11 +0100

    Specify temp dir for swiftmailer

--- app/src/Application/DeskPRO/Mail/Mailer.php
+++ app/src/Application/DeskPRO/Mail/Mailer.php
@@ -63,6 +63,8 @@ class Mailer extends \Swift_Mailer implements Loggable
 
 	public function __construct(\Swift_Transport $transport, \Symfony\Bundle\FrameworkBundle\Templating\EngineInterface $templating, Logger $logger = null)
 	{
+		\Swift_Preferences::getInstance()->setTempDir(dp_get_tmp_dir() . '/swiftmailer-cache');
+
 		$this->messagesLog = new \Orb\Log\Writer\ArrayWriter();
 
 		if ($logger) {
