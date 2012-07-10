c1d80d04bbe6cbf1c93a86e475d5873072fb85f6
DeskPRO <team@deskpro.com>
2012-07-10 10:01:59 +0000

    Updated languages: Japanese
    - Japanese: Added 134 phrases

---
+++ app/languages/japanese/user/downloads.php
@@ -0,0 +1,11 @@
+<?php return array(
+	'user.downloads.button-download'                                       => 'ダウンロード',
+	'user.downloads.file_not_found'                                        => 'ファイルが見つかりませんでした',
+	'user.downloads.message_no-files'                                      => 'このフォルダは空です',
+	'user.downloads.noun'                                                  => 'ファイル',
+	'user.downloads.related_downloads'                                     => '関連ファイル',
+	'user.downloads.sidebar_description'                                   => '{{count}} のファイルをファイルブラウザで閲覧',
+	'user.downloads.title'                                                 => 'ダウンドーロ',
+	'user.downloads.title-categories'                                      => 'サブフォルダが1つあります|{{count}} サブフォルダがあります',
+	'user.downloads.title-downloads'                                       => 'ファイルが1つあります|{{count}} ファイルがあります',
+);
