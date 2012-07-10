abce99ee8602520b4f3b3719fcd0bc9b0a3d50ff
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-07-10 10:04:37 +0100

    Add other lang ID's to lang manifest, enable japanese

--- app/languages/manifest.php
+++ app/languages/manifest.php
@@ -21,6 +21,17 @@
 		'is_managed' => true
 	),
 
+	'japanese' => array(
+		'id'         => 'japanese',
+		'lang_code'  => 'jpn',
+		'title'      => 'Japanese',
+		'locale'     => 'ja',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
 	'turkish' => array(
 		'id'         => 'turkish',
 		'lang_code'  => 'tur',
@@ -30,5 +41,229 @@
 		'has_agent'  => false,
 		'has_admin'  => false,
 		'is_managed' => true
-	)
+	),
+
+	/*
+
+	'arabic' => array(
+		'id'         => 'arabic',
+		'lang_code'  => 'ara',
+		'title'      => 'Arabic',
+		'locale'     => 'ar',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'czech' => array(
+		'id'         => 'czech',
+		'lang_code'  => 'cze',
+		'title'      => 'Czech',
+		'locale'     => 'cs',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'dutch' => array(
+		'id'         => 'dutch',
+		'lang_code'  => 'dut',
+		'title'      => 'Dutch',
+		'locale'     => 'nl',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'finnish' => array(
+		'id'         => 'finnish',
+		'lang_code'  => 'fin',
+		'title'      => 'Finnish',
+		'locale'     => 'fi',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'french' => array(
+		'id'         => 'french',
+		'lang_code'  => 'fre',
+		'title'      => 'French',
+		'locale'     => 'fr',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'german' => array(
+		'id'         => 'german',
+		'lang_code'  => 'ger',
+		'title'      => 'German',
+		'locale'     => 'de',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'greek' => array(
+		'id'         => 'greek',
+		'lang_code'  => 'gre',
+		'title'      => 'Greek',
+		'locale'     => 'el',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'hebrew' => array(
+		'id'         => 'hebrew',
+		'lang_code'  => 'heb',
+		'title'      => 'Hebrew',
+		'locale'     => 'he',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'hindi' => array(
+		'id'         => 'hindi',
+		'lang_code'  => 'hin',
+		'title'      => 'Hindi',
+		'locale'     => 'hi',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'hungarian' => array(
+		'id'         => 'hungarian',
+		'lang_code'  => 'hun',
+		'title'      => 'Hungarian',
+		'locale'     => 'hu',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'korean' => array(
+		'id'         => 'korean',
+		'lang_code'  => 'kor',
+		'title'      => 'Korean',
+		'locale'     => 'ko',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'mandarin' => array(
+		'id'         => 'mandarin',
+		'lang_code'  => 'cmn',
+		'title'      => 'Mandarin',
+		'locale'     => 'cmn',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'norwegian' => array(
+		'id'         => 'norwegian',
+		'lang_code'  => 'nor',
+		'title'      => 'Norwegian',
+		'locale'     => 'no',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'polish' => array(
+		'id'         => 'polish',
+		'lang_code'  => 'pol',
+		'title'      => 'Polish',
+		'locale'     => 'pl',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'portuguese' => array(
+		'id'         => 'portuguese',
+		'lang_code'  => 'por',
+		'title'      => 'Portuguese',
+		'locale'     => 'pt',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'romanian' => array(
+		'id'         => 'romanian',
+		'lang_code'  => 'rum',
+		'title'      => 'Romanian',
+		'locale'     => 'ro',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'russian' => array(
+		'id'         => 'russian',
+		'lang_code'  => 'rus',
+		'title'      => 'Russian',
+		'locale'     => 'ru',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'spanish' => array(
+		'id'         => 'spanish',
+		'lang_code'  => 'spa',
+		'title'      => 'Spanish',
+		'locale'     => 'es',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'swedish' => array(
+		'id'         => 'swedish',
+		'lang_code'  => 'swe',
+		'title'      => 'Swedish',
+		'locale'     => 'sv',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	'ukrainian' => array(
+		'id'         => 'ukrainian',
+		'lang_code'  => 'ukr',
+		'title'      => 'Ukrainian',
+		'locale'     => 'uk',
+		'has_user'   => true,
+		'has_agent'  => false,
+		'has_admin'  => false,
+		'is_managed' => true
+	),
+
+	*/
 );
