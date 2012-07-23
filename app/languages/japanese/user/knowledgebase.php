a9e3b15a29fb5a04948f5164f5b6a19b0994a8e9
DeskPRO <team@deskpro.com>
2012-07-23 13:35:41 +0000

    Updated languages: Japanese
    - Japanese: Added 298 phrases

---
+++ app/languages/japanese/user/knowledgebase.php
@@ -0,0 +1,13 @@
+<?php return array(
+	'user.knowledgebase.article_not_found'                                 => 'この記事は見つかりませんでした。',
+	'user.knowledgebase.category_no-articles'                              => 'このカテゴリには記事がありません。',
+	'user.knowledgebase.knowledgebase'                                     => 'ナレッジベース',
+	'user.knowledgebase.link_view-all'                                     => '1記事を表示|全{{count}}記事を表示',
+	'user.knowledgebase.related_articles'                                  => '関連記事',
+	'user.knowledgebase.sidebar_description'                               => 'ナレッジデータベースにはあなたの質問の回答の参考となる、当社のサポートチームの質問が1つあります。|ナレッジデータベースにはあなたの質問の回答の参考となる、当社のサポートチームの質問が{{count}}あります。',
+	'user.knowledgebase.title'                                             => 'ナレッジベース',
+	'user.knowledgebase.title-articles'                                    => '記事が1つあります。|記事が{{count}}あります。',
+	'user.knowledgebase.title-articles-paged'                              => 'あなたの記事全{{total}}を{{first}} から {{last}}の順に表示します。',
+	'user.knowledgebase.title-categories'                                  => 'サブカテゴリが1つあります。|サブカテゴリが{{count}}あります。',
+	'user.knowledgebase.type_article'                                      => '記事',
+);
