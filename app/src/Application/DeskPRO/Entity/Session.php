663875dbf5fb558a71409375addcacd877361b45
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-07-24 14:16:18 +0100

    Dont include util there. Its included as part as the production compiled single file and will result in duplicates sometimes.

--- app/src/Application/DeskPRO/Entity/Session.php
+++ app/src/Application/DeskPRO/Entity/Session.php
@@ -238,7 +238,6 @@ class Session extends \Application\DeskPRO\Domain\DomainObject
 
 		list ($session_id, ) = explode('-', $sess_code, 2);
 
-		require_once(DP_ROOT.'/src/Orb/Util/Util.php');
 		$session_id = Util::baseDecode($session_id, Util::BASE36_ALPHABET);
 
 		return $session_id;
