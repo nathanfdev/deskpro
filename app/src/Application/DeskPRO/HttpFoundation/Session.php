b1a8144eee245b957062b6deb3f3fc8e98a62c0d
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-08-02 20:09:48 +0100

    Fix agent status cathing requests that arent real loads

--- app/src/Application/DeskPRO/HttpFoundation/Session.php
+++ app/src/Application/DeskPRO/HttpFoundation/Session.php
@@ -148,7 +148,7 @@ class Session extends \Symfony\Component\HttpFoundation\Session implements \Arra
 
 		$this->visitor = $vis;
 
-        if($this->getPerson() && $this->getPerson()->is_agent && !preg_match('#^/agent/(client-messages/|poller|.*/new)#', $path)) {
+        if($this->getPerson() && $this->getPerson()->is_agent && !preg_match('#^/agent/(client-messages/|poller|.*/new)#', $path) && !preg_match('#\.json(\?.*?)?$#', $path)) {
             $agent = $this->getPerson();
             $date_active = new \DateTime();
             list($hour, $minute) = explode(':', $date_active->format('H:i'));
