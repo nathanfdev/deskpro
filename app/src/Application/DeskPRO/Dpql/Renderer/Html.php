42aa410b656e7cdd0c5c05ece04c77c2cc6137c0
Mike Sullivan <mike@regexia.com>
2012-08-09 10:41:59 +0100

    Support for row spans in grouping for improved readability.

--- app/src/Application/DeskPRO/Dpql/Renderer/Html.php
+++ app/src/Application/DeskPRO/Dpql/Renderer/Html.php
@@ -33,7 +33,7 @@ class Html
 		foreach ($this->_results AS $key => $row) {
 			$splitId = array();
 			foreach ($splitColumns AS $column) {
-				$splitId[] = $this->renderCell($row, $column);
+				$splitId[] = $this->_renderCell($row, $column);
 			}
 			$splitResults[implode(' / ', $splitId)][$key] = $row;
 		}
@@ -73,24 +73,73 @@ class Html
 		return '<tr>' . implode("\n\t", $columnHtml) . '</tr>';
 	}
 
+	protected $_rowSpans = array();
+	protected $_rowGroupHit = array();
+
 	public function renderBody(array $rows)
 	{
+		$this->_analyzeRows($rows);
+
 		$rowsHtml = array();
 		foreach ($rows AS $row) {
-			$rowsHtml[] = $this->renderRow($row);
+			$rowsHtml[] = $this->_renderRow($row);
 		}
 
 		return implode("\n", $rowsHtml);
 	}
 
-	public function renderRow(array $row)
+	protected function _analyzeRows(array $rows)
 	{
-		$cells = array();
-		foreach ($this->_handler->getGroupYColumns() AS $column) {
-			$cells[] = '<td>' . $this->renderCell($row, $column) . '</td>';
+		$this->_rowSpans = array();
+		$this->_rowGroupHit = array();
+
+		$groupColumns = $this->_handler->getGroupYColumns();
+		if ($groupColumns) {
+			$rowSpans = array();
+
+			foreach ($rows AS $row) {
+				$groupParts = array();
+				foreach ($groupColumns AS $column) {
+					$groupParts[] = $this->_renderCell($row, $column);
+					$groupPath = $this->_getGroupPathKey($groupParts);
+
+					if (isset($rowSpans[$groupPath])) {
+						$rowSpans[$groupPath]++;
+					} else {
+						$rowSpans[$groupPath] = 1;
+					}
+				}
+			}
+
+			$this->_rowSpans = $rowSpans;
 		}
+	}
+
+	protected function _renderRow(array $row)
+	{
+		$groupParts = array();
+		$cells = array();
+
+		foreach ($this->_handler->getGroupYColumns() AS $column) {
+			$rendered =  $this->_renderCell($row, $column);
+			$groupParts[] = $rendered;
+
+			$groupPath = $this->_getGroupPathKey($groupParts);
+
+			if (empty($this->_rowGroupHit[$groupPath])) {
+				$this->_rowGroupHit[$groupPath] = true;
+
+				$rowSpan = ($this->_rowSpans[$groupPath] > 1
+					? ' rowspan="' . $this->_rowSpans[$groupPath] . '"'
+					: ''
+				);
+
+				$cells[] = "<td$rowSpan>$rendered</td>";
+			}
+		}
+
 		foreach ($this->_handler->getSelectColumns() AS $column) {
-			$cells[] = '<td>' . $this->renderCell($row, $column) . '</td>';
+			$cells[] = '<td>' . $this->_renderCell($row, $column) . '</td>';
 		}
 
 		if ($cells) {
@@ -100,7 +149,7 @@ class Html
 		}
 	}
 
-	public function renderCell(array $row, array $column)
+	protected function _renderCell(array $row, array $column)
 	{
 		$renderer = $column['render'];
 
@@ -109,5 +158,10 @@ class Html
 		} else {
 			return $renderer;
 		}
+	}
+
+	protected function _getGroupPathKey(array $groupParts)
+	{
+		return implode('|', $groupParts);
 	}
 }
