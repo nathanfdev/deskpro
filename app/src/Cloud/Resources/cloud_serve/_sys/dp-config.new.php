17a7c2366e9ed5ae0aa1c79070959a9931f9a198
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-08-13 21:30:22 +0100

    Tweak save path

--- app/src/Cloud/Resources/cloud_serve/_sys/dp-config.new.php
+++ app/src/Cloud/Resources/cloud_serve/_sys/dp-config.new.php
@@ -3,6 +3,9 @@
  * Config file loaded by DeskPRO
  */
 
+define('DP_MA_SERVER', 'http://www.deskpro.com/members');
+define('DP_SAVEMAIL_AUTH', 'XXXX');
+
 $DP_CONFIG = array();
 $DP_CONFIG['debug'] = array();
 $DP_CONFIG['dir_data'] = DPC_SITE_DATADIR;
