18250f53830865961ab50587bb3e1337592d4dae
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-08-21 13:18:08 +0100

    Fix plain select boxes being revealed sometimes when switching between departments with custom field layouts

--- web/javascripts/DeskPRO/Agent/PageFragment/Page/NewTicket.js
+++ web/javascripts/DeskPRO/Agent/PageFragment/Page/NewTicket.js
@@ -156,7 +156,7 @@ DeskPRO.Agent.PageFragment.Page.NewTicket = new Orb.Class({
 						var classname = f.field_type;
 					}
 
-					$('.' + classname, self.wrapper).not('.error-message').show();
+					$('.ticket-field.' + classname, self.wrapper).not('.error-message').show();
 				});
 			});
 
