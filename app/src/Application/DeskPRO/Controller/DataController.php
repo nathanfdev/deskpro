cda6e970cea1c2caa8b5fc545a8471c8e431cb87
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-08-22 18:51:06 +0100

    Fix missing js phrase'

--- app/src/Application/DeskPRO/Controller/DataController.php
+++ app/src/Application/DeskPRO/Controller/DataController.php
@@ -66,6 +66,7 @@ class DataController extends AbstractController
 		$js_phrases['agent.general.check_off']   = $tr->getPhraseText('agent.general.check_off');
 
 		$js_phrases["agent.general.reltime_less_second"]    = $tr->getPhraseText("agent.general.reltime_less_second");
+		$js_phrases["agent.general.reltime_less_minute"]    = $tr->getPhraseText("agent.general.reltime_less_minute");
 		$js_phrases["agent.general.reltimeago_less_second"] = $tr->getPhraseText("agent.general.reltime_less_second");
 		$js_phrases["agent.general.reltimeago_less_minute"] = $tr->getPhraseText("agent.general.reltimeago_less_minute");
 
