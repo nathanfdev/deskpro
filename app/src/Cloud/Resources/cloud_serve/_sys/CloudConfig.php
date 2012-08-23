73e88dce48066ef7347d23274b936b085e9c1860
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-08-23 16:50:59 +0100

    Dont load offline sites

--- app/src/Cloud/Resources/cloud_serve/_sys/CloudConfig.php
+++ app/src/Cloud/Resources/cloud_serve/_sys/CloudConfig.php
@@ -316,7 +316,7 @@ class CloudConfig
 				cloud_accounts.id AS account_id, cloud_accounts.agents, cloud_accounts.is_demo, UNIX_TIMESTAMP(cloud_accounts.date_demo_expire) AS demo_expire_at
 			FROM cloud_sites
 			LEFT JOIN cloud_accounts ON cloud_accounts.cloud_site_id = cloud_sites.id
-			WHERE cloud_sites.id = ? AND cloud_sites.build_number > 0
+			WHERE cloud_sites.id = ? AND cloud_sites.build_number > 0 AND cloud_accounts.is_offline = 0
 			LIMIT 1
 		");
 		$stmt->execute(array($id));
@@ -339,7 +339,7 @@ class CloudConfig
 				cloud_accounts.id AS account_id, cloud_accounts.agents, cloud_accounts.is_demo, UNIX_TIMESTAMP(cloud_accounts.date_demo_expire) AS demo_expire_at
 			FROM cloud_sites
 			LEFT JOIN cloud_accounts ON cloud_accounts.cloud_site_id = cloud_sites.id
-			WHERE (cloud_sites.master_domain = ? OR cloud_sites.custom_domain = ?) AND cloud_sites.build_number > 0
+			WHERE (cloud_sites.master_domain = ? OR cloud_sites.custom_domain = ?) AND cloud_sites.build_number > 0 AND cloud_accounts.is_offline = 0
 			LIMIT 1
 		");
 		$stmt->execute(array($domain, $domain));
