5aa7f1019c6acedbc459c821e89806d8db749dfe
Mike Sullivan <mike@regexia.com>
2012-08-31 15:15:48 +0100

    Expose person custom fields to the ticket "API" for widgets.

--- app/src/Application/AgentBundle/Controller/TicketController.php
+++ app/src/Application/AgentBundle/Controller/TicketController.php
@@ -187,12 +187,20 @@ class TicketController extends AbstractController
 		foreach (array('id', 'subject', 'ref', 'status') AS $key) {
 			$ticket_api[$key] = $ticket->$key;
 		}
+		foreach ($custom_fields AS $field) {
+			$ticket_api['custom'][$field['id']] = array(
+				'id' => $field['id'],
+				'title' => $field['title'],
+				'value' => isset($field['value']['value']) ? $field['value']['value'] : false
+			);
+		}
 		foreach (array('id', 'name', 'first_name', 'last_name') AS $key) {
 			$ticket_api['person'][$key] = $ticket->person->$key;
 		}
 		$ticket_api['person']['email'] = $ticket->person->getPrimaryEmailAddress();
-		foreach ($custom_fields AS $field) {
-			$ticket_api['custom'][$field['id']] = array(
+		$person_custom_fields = $this->container->getSystemService('person_fields_manager')->getDisplayArrayForObject($ticket->person);
+		foreach ($person_custom_fields AS $field) {
+			$ticket_api['person']['custom'][$field['id']] = array(
 				'id' => $field['id'],
 				'title' => $field['title'],
 				'value' => isset($field['value']['value']) ? $field['value']['value'] : false
