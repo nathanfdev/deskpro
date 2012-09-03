9ad65f764da672d912ef920e5a863b0fc65ec082
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-09-03 13:14:04 +0100

    Fix double-counting some user sessions

--- app/src/Application/AdminBundle/Controller/MainController.php
+++ app/src/Application/AdminBundle/Controller/MainController.php
@@ -54,7 +54,7 @@ class MainController extends AbstractController
 
 		$count_online_users = $this->db->fetchColumn("
 			SELECT COUNT(*) FROM sessions
-			WHERE date_last > ? AND is_helpdesk = 1
+			WHERE date_last > ? AND is_helpdesk = 1 GROUP BY visitor_id
 		", array(date('Y-m-d H:i:s', time() - App::getSetting('core.sessions_lifetime'))));
 		$count_online_users -= max(0, count($online_agents));
 
