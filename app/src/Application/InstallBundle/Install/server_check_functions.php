405ec61c255bcc5b249fb2837dc83aeba6a6f396
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-09-03 18:58:04 +0100

    Fix warning in some cases where PDO::getAvailableDrivers doesnt return array

--- app/src/Application/InstallBundle/Install/server_check_functions.php
+++ app/src/Application/InstallBundle/Install/server_check_functions.php
@@ -115,7 +115,20 @@ function deskpro_install_check_pdo()
 
 function deskpro_install_check_pdo_mysql()
 {
-	return (deskpro_install_check_pdo() && in_array('mysql', PDO::getAvailableDrivers()));
+	if (!deskpro_install_check_pdo()) {
+		return false;
+	}
+
+	$drivers = PDO::getAvailableDrivers();
+	if (!$drivers) {
+		return false;
+	}
+
+	if (!in_array('mysql', $drivers)) {
+		return false;
+	}
+
+	return true;
 }
 
 function deskpro_install_check_image_manip()
