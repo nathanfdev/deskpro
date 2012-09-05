5967bae3fb2e8f3c205ca12c066642104916d235
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-09-05 13:06:24 +0100

    Get path to PHP as used in the build scripts from the usual config.php

--- app/bin/build/php-path.php
+++ app/bin/build/php-path.php
@@ -1,14 +1,19 @@
 <?php
 
-/**
- * You can edit this line to set the path to the PHP CLI if you know it.
- */
 $php_path = null;
 
+if (isset($DP_CONFIG['php_path']) && $DP_CONFIG['php_path']) {
+	$php_path = $DP_CONFIG['php_path'];
+} else {
+	$config_file = __DIR__.'/../../config.php';
+	if (file_exists($config_file)) {
+		require_once($config_file);
+	}
 
-########################################################################################################################
-
-
+	if (isset($DP_CONFIG['php_path']) && $DP_CONFIG['php_path']) {
+		$php_path = $DP_CONFIG['php_path'];
+	}
+}
 
 if (!$php_path) {
 	$php_paths = array('/usr/bin/php', '/usr/local/bin/php', '/usr/bin/php5', '/usr/local/bin/php5', 'C:\\php\\php.exe', 'C:\\php5\\php.exe');
@@ -43,13 +48,13 @@ if (!$php_path) {
 	if ($php_path) {
 		exec("$php_path -v", $out);
 		if (!isset($out[0]) || strpos($out[0], 'PHP 5.') === false) {
-			echo "Detected possible PHP at $php_path, but it seems invalid. Edit /appfiles/bin/build/php-path.php with the specific path to the PHP CLI.\n";
+			echo "Detected possible PHP at $php_path, but it seems invalid. Edit /config.php with the specific path to the PHP CLI.\n";
 			exit(1);
 		}
 	}
 
 	if (!$php_path) {
-		echo "We could not detect where your PHP CLI is. Edit /appfiles/bin/build/php-path.php with the specific path to the PHP CLI.\n";
+		echo "We could not detect where your PHP CLI is. Edit /config.php with the specific path to the PHP CLI.\n";
 		exit(1);
 	}
 }
