9139d1bcc36ca3551dc963bd4ea5c264475c8bcc
DeskPRO <team@deskpro.com>
2012-09-12 18:01:06 +0000

    Updated languages: German
    - German: Changed 1 phrase

--- app/languages/german/user/lang.php
+++ app/languages/german/user/lang.php
@@ -1,3 +1,3 @@
 <?php return array(
-	'user.lang.lang_title'                                                 => 'Englisch',
+	'user.lang.lang_title'                                                 => 'Deutsch',
 );
