9b4b57d5f95b9be58b51a1491b31cb02a04d8b4d
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-09-17 13:26:02 +0100

    Dont report pop3 to us

--- app/sys/Kernel/KernelErrorHandler.php
+++ app/sys/Kernel/KernelErrorHandler.php
@@ -433,6 +433,10 @@ class KernelErrorHandler
 			'time_to_error'  => defined('DP_START_TIME') ? sprintf("%0.4f", microtime(true) - DP_START_TIME) : 0
 		);
 
+		if ($exception instanceof \Zend\Mail\Protocol\Exception\RuntimeException) {
+			$errinfo['no_send_error'] = true;
+		}
+
 		return $errinfo;
 	}
 
