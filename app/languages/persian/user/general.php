06277dfb99d5285af1430775408b9c7bb99a3323
DeskPRO <team@deskpro.com>
2012-10-02 18:12:40 +0000

    Updated languages: Persian
    - Persian: Added 332 phrases

---
+++ app/languages/persian/user/general.php
@@ -0,0 +1,5 @@
+<?php return array(
+	'user.general.add'                                                     => 'اضافه کردن',
+	'user.general.attachments'                                             => 'فایل پیوست',
+	'user.general.comments_add-comment'                                    => 'اضافه کردن نظر',
+);
---
+++ app/languages/persian/user/tickets.php
@@ -0,0 +1,8 @@
+<?php return array(
+	'user.tickets.dont_add_me_to_ticket'                                   => 'نه ممنون',
+	'user.tickets.download_pdf'                                            => 'دانلود PDF',
+	'user.tickets.edit'                                                    => 'ویرایش',
+	'user.tickets.fields_category'                                         => 'دسته بندی',
+	'user.tickets.fields_product'                                          => 'محصول',
+	'user.tickets.from_x_at_y'                                             => 'از {{name}} به {{date}}',
+);
