89db58b0cc3a39e7b7be8ea3b93f34c4e5bf86cb
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-10-02 10:13:39 +0100

    Make disabled blocks look disabled in portal editor

--- app/src/Application/DeskPRO/PageDisplay/Page/PortalPage.php
+++ app/src/Application/DeskPRO/PageDisplay/Page/PortalPage.php
@@ -346,7 +346,7 @@ class PortalPage extends BasicPage implements PersonContextInterface
 			if ($block_html) {
 				$pid = $item->getOption('pid');
 				$type = strtolower(\Orb\Util\Util::getBaseClassname($item));
-				$block_html = "<div class=\"dp-p dp-{$type} dp-pid-{$pid}" . ($item->getOption('is_enabled') ? '' : ' dp-p-disabled') . "\" data-dp-pid=\"{$pid}\">{$block_html}</div>";
+				$block_html = "<div class=\"dp-p dp-{$type} dp-pid-{$pid}" . ($item->getOption('is_enabled') ? '' : ' dp-p-disabled disabled') . "\" data-dp-pid=\"{$pid}\">{$block_html}</div>";
 				$html[] = $block_html;
 			}
 		}
