9d56ba312d91cde8c9ec37ca5c9b30a155980c67
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-10-03 09:55:22 +0100

    Fix icon misplacement in person tab in chrome

--- web/javascripts/DeskPRO/Agent/PageFragment/Page/Person.js
+++ web/javascripts/DeskPRO/Agent/PageFragment/Page/Person.js
@@ -28,14 +28,14 @@ DeskPRO.Agent.PageFragment.Page.Person = new Orb.Class({
 
 		if (this.tabBtn) {
 			if (this.getMetaData('personPicIcon')) {
-				this.tabBtn.find('a').css('background-image', 'url(' + this.getMetaData('personPicIcon') + ')').css('background-position', '2px 50%');
+				this.tabBtn.find('a').css('background-image', 'url("' + this.getMetaData('personPicIcon') + '")').css('background-position', '2px 50%');
 			} else if (this.getMetaData('personGravatarIcon')) {
 				var defaultIcon = ASSETS_BASE_URL_FULL + 'images/agent/tabs/tabtype-person.png';
 				var url = this.getMetaData('personGravatarIcon');
 				url = Orb.appendQueryData(url, 'd', defaultIcon);
 
 				var a = this.tabBtn.find('a');
-				a.css('background-image', 'url(\'' + url + ')\'').css('background-position', '2px 50%');
+				a.css('background-image', 'url("' + url + '")').css('background-position', '2px 50%');
 			}
 		}
 
