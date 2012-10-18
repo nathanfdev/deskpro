737e9ba41194ae2aa986ffc8b61040ea68efed8b
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-10-18 15:01:07 +0100

    Log result of cli check file in importer

--- app/src/Application/DeskPRO/Command/ImportCommand.php
+++ app/src/Application/DeskPRO/Command/ImportCommand.php
@@ -417,12 +417,16 @@ class ImportCommand extends \Symfony\Bundle\FrameworkBundle\Command\ContainerAwa
 				$cli_check = file_get_contents($cli_check_file);
 				$cli_check = @unserialize($cli_check);
 
+				$logger->log("cli check file: $cli_check_file: " . print_r($cli_check,1), Logger::INFO);
+
 				if (is_array($cli_check) && isset($cli_check['checks'])) {
 					$okay = true;
 					if (in_array('fatal', $cli_check['checks'])) {
 						$okay = false;
 					}
 				}
+			} else {
+				$logger->log("cli check file does not exist: $cli_check_file", Logger::ERR);
 			}
 
 			// Means they are different: Import command (the wrapper) works fine or else we would have quit already,
