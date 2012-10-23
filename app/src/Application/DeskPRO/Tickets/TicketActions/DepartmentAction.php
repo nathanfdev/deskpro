2630d0984ee901412c171328c5786e2e417ec5a7
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-10-23 15:07:29 +0100

    Remove useless use line which also fixes the magic 4096 bug

--- app/src/Application/DeskPRO/Tickets/TicketActions/DepartmentAction.php
+++ app/src/Application/DeskPRO/Tickets/TicketActions/DepartmentAction.php
@@ -36,7 +36,6 @@ namespace Application\DeskPRO\Tickets\TicketActions;
 
 use Application\DeskPRO\App;
 use Application\DeskPRO\Tickets\TicketActions\ActionInterface;
-use Application\DeskPRO\People\PersonContextInterface;
 use Application\DeskPRO\Entity\Ticket;
 use Application\DeskPRO\Entity\Person;
 
