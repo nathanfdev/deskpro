b1be523c3066b94138522c2f691c4919ae39632e
DeskPRO <team@deskpro.com>
2012-11-01 16:19:41 +0000

    Updated languages: Polish, Russian
    - Polish: Added 265 phrases
    - Russian: Added 553 phrases

---
+++ app/languages/polish/user/news.php
@@ -0,0 +1,10 @@
+<?php return array(
+	'user.news.all-categories'                                             => 'Wszystkie Kategorie',
+	'user.news.category'                                                   => 'Kategoria',
+	'user.news.latest_news'                                                => 'Ostatnie Wiadomości',
+	'user.news.news_not_found'                                             => 'Ten post nie został znaleziony',
+	'user.news.posted_date'                                                => 'Opublikowane: {{date}}',
+	'user.news.related_news'                                               => 'Powiązane Wiadomości',
+	'user.news.title'                                                      => 'Wiadomości',
+	'user.news.type_news'                                                  => 'Wiadomości',
+);
