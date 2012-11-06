692595947af369a8af36a7d5f2236354cce84ee3
Mike Sullivan <mike@regexia.com>
2012-11-06 10:43:52 +0000

    Give more details and guidance when some database connection errors happen.

--- app/src/Application/InstallBundle/Install/server_check_phpold_stub.php
+++ app/src/Application/InstallBundle/Install/server_check_phpold_stub.php
@@ -12,6 +12,21 @@ function deskpro_handle_boot_db_exception($e)
 			$error_info = str_replace(DP_DATABASE_USER, '...', $error_info);
 			$error_info = str_replace(DP_DATABASE_PASSWORD, '...', $error_info);
 
+			switch($e->getCode()) {
+				case 1049:
+					$error_info .= '<hr />The database could not be found. Please ensure the correct database is listed in config.php.';
+					break;
+
+				case 1044:
+				case 1045:
+					$error_info .= '<hr />Please ensure the correct database details are listed in config.php. If you are sure they are correct, you may not have sufficient permissions to access the database.';
+					break;
+
+				case 2002:
+					$error_info .= '<hr />Could not connect to the database server. Please ensure the correct database details are listed in config.php. If you are sure they are correct, your database server may not be configured to accept connections from this server.';
+					break;
+			}
+
 			$error_info .= "<hr />More information may be available in in data/logs/error.log";
 		}
 
