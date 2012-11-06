b140946e5499d9d30875d9c384cb38948b566335
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-11-06 13:32:53 +0000

    Fix custom phrases link

--- app/src/Application/AdminBundle/Controller/LanguagesController.php
+++ app/src/Application/AdminBundle/Controller/LanguagesController.php
@@ -380,10 +380,14 @@ class LanguagesController extends AbstractController
 		if ($group == 'CUSTOM') {
 			$set = array();
 			foreach ($vars['lang_phrases']['custom'] as $phrase) {
-				$set[$phrase->name] = isset($vars['master_phrases'][$phrase->name]) ? $vars['master_phrases'][$phrase->name] : null;
+				if (isset($vars['master_phrases'][$phrase['name']])) {
+					$set[$phrase['name']] = $vars['master_phrases'][$phrase['name']];
+				} else {
+					$set[$phrase['name']] = null;
+				}
 			}
 
-			$vars['master_phrases'] = $set;
+			$vars['master_phrases'] = array_merge($vars['master_phrases'], $set);
 		}
 
 		return $this->render('AdminBundle:Languages:lang-phrases.html.twig', $vars);
