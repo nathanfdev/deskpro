4cbcc6982ac39c89d34e0b1b941e4287ab3136bc
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-11-14 11:25:59 +0000

    Add new default agent perm group for all non-destructive permissions

--- app/languages/default/agent/defaults.php
+++ app/languages/default/agent/defaults.php
@@ -28,6 +28,8 @@
 	'agent.defaults.team_support_managers'                               => 'Support Managers',
 	'agent.defaults.usergroup_agent_all_perms'                           => 'All Permissions',
 	'agent.defaults.usergroup_agent_all_perms_note'                      => 'Agent has full permissions',
+	'agent.defaults.usergroup_agent_all_non_destructive'                 => 'All Non-Destructive Permissions',
+	'agent.defaults.usergroup_agent_all_non_destructive_note'            => 'Agent has all permissions except those that would allow them to make destructive changes such as deleting records.',
 	'agent.defaults.usergroup_everyone'                                  => 'Everyone',
 	'agent.defaults.usergroup_everyone_note'                             => 'Every user including both guests and registered members.',
 	'agent.defaults.usergroup_registered'                                => 'Registered',
--- app/src/Application/InstallBundle/Data/data.php
+++ app/src/Application/InstallBundle/Data/data.php
@@ -1259,6 +1259,100 @@ $AGENTGROUP_ALL['is_agent_group'] = true;
 $em->persist($AGENTGROUP_ALL);
 $em->flush();
 
+##BEGIN:usergroups.agent_all_nondestructive##
+$AGENTGROUP_ALL_ND = new \Application\DeskPRO\Entity\Usergroup();
+$AGENTGROUP_ALL_ND['title'] = $translate->phrase('agent.defaults.usergroup_agent_all_non_destructive');
+$AGENTGROUP_ALL_ND['note'] = $translate->phrase('agent.defaults.usergroup_agent_all_non_destructive_note');
+$AGENTGROUP_ALL_ND['is_agent_group'] = true;
+$em->persist($AGENTGROUP_ALL_ND);
+$em->flush();
+
+// Permissions for ND group
+$ugid = $AGENTGROUP_ALL_ND->getId();
+$em->getConnection()->executeUpdate("
+	INSERT INTO `permissions` (`usergroup_id`, `person_id`, `value`, `name`)
+	VALUES
+		($ugid, NULL, '1', 'agent_tickets.use'),
+		($ugid, NULL, '1', 'agent_tickets.create'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_closed'),
+		($ugid, NULL, '1', 'agent_tickets.reply_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_department_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_fields_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_agent_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_team_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_self_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_cc_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_merge_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_labels_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_notes_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_hold_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_awaiting_user_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_awaiting_agent_own'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_resolved_own'),
+		($ugid, NULL, '1', 'agent_tickets.reply_to_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_department_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_fields_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_agent_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_team_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_self_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_cc_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_merge_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_labels_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_notes_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_hold_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_awaiting_user_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_awaiting_agent_followed'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_resolved_followed'),
+		($ugid, NULL, '1', 'agent_tickets.view_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.reply_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_department_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_fields_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_agent_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_team_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_self_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_merge_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_labels_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_notes_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_hold_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_awaiting_user_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_awaiting_agent_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_resolved_unassigned'),
+		($ugid, NULL, '1', 'agent_tickets.view_others'),
+		($ugid, NULL, '1', 'agent_tickets.reply_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_department_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_fields_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_agent_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_team_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_assign_self_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_merge_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_labels_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_notes_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_hold_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_awaiting_user_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_awaiting_agent_others'),
+		($ugid, NULL, '1', 'agent_tickets.modify_set_resolved_others'),
+		($ugid, NULL, '1', 'agent_people.create'),
+		($ugid, NULL, '1', 'agent_people.edit'),
+		($ugid, NULL, '1', 'agent_people.validate'),
+		($ugid, NULL, '1', 'agent_people.manage_emails'),
+		($ugid, NULL, '1', 'agent_people.reset_password'),
+		($ugid, NULL, '1', 'agent_people.notes'),
+		($ugid, NULL, '1', 'agent_people.disable'),
+		($ugid, NULL, '1', 'agent_org.create'),
+		($ugid, NULL, '1', 'agent_org.edit'),
+		($ugid, NULL, '1', 'agent_chat.use'),
+		($ugid, NULL, '1', 'agent_chat.view_unassigned'),
+		($ugid, NULL, '1', 'agent_chat.view_others'),
+		($ugid, NULL, '1', 'agent_publish.create'),
+		($ugid, NULL, '1', 'agent_publish.edit'),
+		($ugid, NULL, '1', 'agent_publish.validate'),
+		($ugid, NULL, '1', 'agent_general.signature'),
+		($ugid, NULL, '1', 'agent_general.signature_rte')
+");
 
 ################################################################################
 # Default base stats
