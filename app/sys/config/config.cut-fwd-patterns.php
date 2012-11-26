f340c7cee46ea134a93bbadb0ff18619bcf6451e
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-11-26 15:42:14 +0000

    Add missing Outlook pattern for fwd message marker

--- app/sys/config/config.cut-fwd-patterns.php
+++ app/sys/config/config.cut-fwd-patterns.php
@@ -7,5 +7,7 @@
 	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 	'standard_1' => '#-{3,15}\s*Forward(ed)?( Message)?\s*-{3,15}#i',
-	'standard_2' => '#^\s*Forward(ed)?( Message):\s*$#im'
+	'standard_2' => '#^\s*Forward(ed)?( Message):\s*$#im',
+
+	'outlook_1' => '#^\-+Original Message\-+\s*$#im',
 );
