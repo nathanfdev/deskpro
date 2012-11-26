eff66fa5c1a4f58fc348a5fd4f092540d5d324a0
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-11-26 09:29:58 +0000

    // Would help if it was the right path

--- app/sys/serve_abstract.php
+++ app/sys/serve_abstract.php
@@ -72,7 +72,7 @@ abstract class LoaderAbstract
 		date_default_timezone_set('UTC');
 		ini_set('default_charset', 'UTF-8');
 
-		require_once DP_ROOT.'/app/src/Orb/Util/Strings.php';
+		require_once DP_ROOT.'/src/Orb/Util/Strings.php';
 		\Orb\Util\Strings::setPhpUtf8Dir(DP_ROOT.'/vendor/php-utf8');
 
 		#------------------------------
