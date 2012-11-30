7eb7c330a5422d3e817667f5240a2cde1f967f33
Mike Sullivan <mike.sullivan@deskpro.com>
2012-11-30 18:20:25 +0000

    When display order is tied for custom fields, display in alphabetical order.

--- app/src/Application/DeskPRO/EntityRepository/CustomDefAbstract.php
+++ app/src/Application/DeskPRO/EntityRepository/CustomDefAbstract.php
@@ -52,7 +52,7 @@ class CustomDefAbstract extends AbstractEntityRepository
 		$q = $this->_em->createQuery("
 			SELECT f
 			FROM {$this->_entityName} f INDEX BY f.id
-			ORDER BY f.display_order ASC
+			ORDER BY f.display_order ASC, f.title
 		");
 		$q->useResultCache(true, null, static::getCacheId('getfields'));
 
@@ -65,7 +65,7 @@ class CustomDefAbstract extends AbstractEntityRepository
 			SELECT f
 			FROM {$this->_entityName} f INDEX BY f.id
 			WHERE f.is_enabled = true
-			ORDER BY f.display_order ASC
+			ORDER BY f.display_order ASC, f.title
 		");
 		$q->useResultCache(true, null, static::getCacheId('getenabledfields'));
 
@@ -78,7 +78,7 @@ class CustomDefAbstract extends AbstractEntityRepository
 			SELECT f
 			FROM {$this->_entityName} f INDEX BY f.id
 			WHERE f.is_enabled = true AND f.is_agent_field = false
-			ORDER BY f.display_order ASC
+			ORDER BY f.display_order ASC, f.title
 		");
 		$q->useResultCache(true, null, static::getCacheId('getenabledfields'));
 
@@ -94,7 +94,7 @@ class CustomDefAbstract extends AbstractEntityRepository
 			SELECT f
 			FROM {$this->_entityName} f INDEX BY f.id
 			WHERE f.parent IS NULL
-			ORDER BY f.display_order ASC
+			ORDER BY f.display_order ASC, f.title
 		");
 
 		$q->useResultCache(true, null, static::getCacheId('gettopfields'));
@@ -108,7 +108,7 @@ class CustomDefAbstract extends AbstractEntityRepository
 			SELECT f
 			FROM {$this->_entityName} f INDEX BY f.id
 			WHERE f.parent IS NULL AND f.is_enabled = true
-			ORDER BY f.display_order ASC
+			ORDER BY f.display_order ASC, f.title
 		");
 
 		$q->useResultCache(true, null, static::getCacheId('gettopfields'));
