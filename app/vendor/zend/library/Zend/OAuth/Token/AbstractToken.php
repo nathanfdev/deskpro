4dcc73ee7e786697bbe6e348b6b32e3823ce1699
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-12-07 09:38:26 +0000

    Quick patch to zend oauth to fix notice with invalid/failed responses

--- app/vendor/zend/library/Zend/OAuth/Token/AbstractToken.php
+++ app/vendor/zend/library/Zend/OAuth/Token/AbstractToken.php
@@ -47,14 +47,14 @@ abstract class AbstractToken implements OAuthToken
 
     /**
      * Token parameters
-     * 
+     *
      * @var array
      */
     protected $_params = array();
 
     /**
      * OAuth response object
-     * 
+     *
      * @var \Zend\Http\Response
      */
     protected $_response = null;
@@ -267,15 +267,17 @@ abstract class AbstractToken implements OAuthToken
         $parts = explode('&', $body);
         foreach ($parts as $kvpair) {
             $pair = explode('=', $kvpair);
-            $params[rawurldecode($pair[0])] = rawurldecode($pair[1]);
+			if (count($pair) == 2) {
+				$params[rawurldecode($pair[0])] = rawurldecode($pair[1]);
+			}
         }
         return $params;
     }
-    
+
     /**
      * Limit serialisation stored data to the parameters
      */
-    public function __sleep() 
+    public function __sleep()
     {
         return array('_params');
     }
@@ -283,7 +285,7 @@ abstract class AbstractToken implements OAuthToken
     /**
      * After serialisation, re-instantiate a HTTP utility class for use
      */
-    public function __wakeup() 
+    public function __wakeup()
     {
         if ($this->_httpUtility === null) {
             $this->_httpUtility = new HTTPUtility;
