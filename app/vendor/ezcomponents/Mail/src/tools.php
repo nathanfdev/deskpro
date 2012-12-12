7fd06e822d22da94feaab40b911bdfcd5ce14c43
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-12-12 14:16:12 +0000

    Small patch to handle malformed encoded fields (RFC 2047 style)

--- app/vendor/ezcomponents/Mail/src/tools.php
+++ app/vendor/ezcomponents/Mail/src/tools.php
@@ -597,7 +597,15 @@ class ezcMailTools
 
         // Try it as latin 1 string
         $text = preg_replace( '/=\?([^?]+)\?/', '=?iso-8859-1?', $origtext );
-        $text = iconv_mime_decode( $text, 0, $charset );
+        $text = @iconv_mime_decode( $text, 0, $charset );
+
+		if (!$text) {
+			$text = @iconv_mime_decode( $origtext, \ICONV_MIME_DECODE_CONTINUE_ON_ERROR, $charset );
+
+			if (!$text) {
+				return $origtext;
+			}
+		}
 
         return $text;
     }
@@ -750,7 +758,7 @@ class ezcMailTools
      * $contentIdArray = array( 'consoletools-table.png@1421450' => 'http://localhost/consoletools-table.jpg' );
      * $text = "<html> Embedded image: <img src='cid:consoletools-table.png@1421450'/> </html>";
      * $htmlBody = ezcMailTools::replaceContentIdRefs( $text, $contentIdArray );
-     * // $htmlBody is now: 
+     * // $htmlBody is now:
      * // <html> Embedded image: <img src='http://localhost/consoletools-table.jpg'/> </html>
      * ?>
      * </code>
