9acada342e6a49664a18d0c27039b606ca3c5072
Christopher Nadeau <chris.nadeau@deskpro.com>
2012-12-17 17:13:36 +0000

    FIx loading of custom phrasenames

--- app/src/Application/DeskPRO/Translate/Loader/DbLoader.php
+++ app/src/Application/DeskPRO/Translate/Loader/DbLoader.php
@@ -97,9 +97,9 @@ class DbLoader implements LoaderInterface
 		if (DP_INTERFACE == 'admin') {
 			$group_like = '1';
 		} elseif (DP_INTERFACE == 'agent') {
-			$group_like = 'groupname LIKE "agent.%" OR groupname LIKE "user.%" OR groupname LIKE "obj_%"';
+			$group_like = 'groupname LIKE "agent.%" OR groupname LIKE "user.%" OR groupname LIKE "obj_%" OR groupname = "custom"';
 		} else {
-			$group_like = 'groupname LIKE "user.%" OR groupname LIKE "obj_%"';
+			$group_like = 'groupname LIKE "user.%" OR groupname LIKE "obj_%" OR groupname = "custom"';
 		}
 
 		$q = $this->dbconn->query("
