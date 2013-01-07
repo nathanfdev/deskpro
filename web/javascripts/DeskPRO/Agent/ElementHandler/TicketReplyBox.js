c1f84009bb9e8c832ea2a5762640a5bb82ab2ba0
Mike Sullivan <mike.sullivan@deskpro.com>
2013-01-07 12:33:01 +0000

    Allow ticket snippet inserts to be undone.

--- web/javascripts/DeskPRO/Agent/ElementHandler/TicketReplyBox.js
+++ web/javascripts/DeskPRO/Agent/ElementHandler/TicketReplyBox.js
@@ -335,6 +335,7 @@ DeskPRO.Agent.ElementHandler.TicketReplyBox = new Orb.Class({
 
 				if (isWysiwyg && textarea.data('redactor')) {
 					textarea.data('redactor').restoreSelection();
+					textarea.data('redactor').setBuffer();
 					textarea.data('redactor').insertHtml(info.snippetHtml);
 				} else {
 					self.page.insertTextInReply(info.snippet);
