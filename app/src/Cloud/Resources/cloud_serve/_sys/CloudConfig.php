efe798bc37bae5fe6ba012668045d82343780ef1
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-01-08 17:44:42 +0000

    // Set copyfree constant

--- app/src/Cloud/Resources/cloud_serve/_sys/CloudConfig.php
+++ app/src/Cloud/Resources/cloud_serve/_sys/CloudConfig.php
@@ -162,6 +162,7 @@ class CloudConfig
 		define('DP_TECHNICAL_EMAIL',     'team@deskpro.com');
 		define('DPC_BILL_DATE',          $siteinfo['next_bill_at']);
 		define('DPC_BILL_OVERDUE',       $siteinfo['next_bill_at'] && $siteinfo['next_bill_at'] < time());
+		define('DPC_COPYFREE',           $siteinfo['is_copyfree'] ? true : false);
 
 		if (!is_dir(DPC_SITE_DATADIR)) {
 			mkdir(DPC_SITE_DATADIR, 0777, true);
@@ -329,7 +330,7 @@ class CloudConfig
 		$stmt = self::getDb()->prepare("
 			SELECT
 				cloud_sites.*,
-				cloud_accounts.id AS account_id, cloud_accounts.agents, cloud_accounts.is_demo, UNIX_TIMESTAMP(cloud_accounts.date_demo_expire) AS demo_expire_at, UNIX_TIMESTAMP(cloud_accounts.date_next_bill) AS next_bill_at
+				cloud_accounts.id AS account_id, cloud_accounts.agents, cloud_accounts.is_demo, UNIX_TIMESTAMP(cloud_accounts.date_demo_expire) AS demo_expire_at, UNIX_TIMESTAMP(cloud_accounts.date_next_bill) AS next_bill_at, cloud_accounts.is_copyfree
 			FROM cloud_sites
 			LEFT JOIN cloud_accounts ON cloud_accounts.cloud_site_id = cloud_sites.id
 			WHERE cloud_sites.id = ? AND cloud_sites.build_number > 0 AND cloud_accounts.is_offline = 0
@@ -358,7 +359,7 @@ class CloudConfig
 		$stmt = self::getDb()->prepare("
 			SELECT
 				cloud_sites.*,
-				cloud_accounts.id AS account_id, cloud_accounts.agents, cloud_accounts.is_demo, UNIX_TIMESTAMP(cloud_accounts.date_demo_expire) AS demo_expire_at, UNIX_TIMESTAMP(cloud_accounts.date_next_bill) AS next_bill_at
+				cloud_accounts.id AS account_id, cloud_accounts.agents, cloud_accounts.is_demo, UNIX_TIMESTAMP(cloud_accounts.date_demo_expire) AS demo_expire_at, UNIX_TIMESTAMP(cloud_accounts.date_next_bill) AS next_bill_at, cloud_accounts.is_copyfree
 			FROM cloud_sites
 			LEFT JOIN cloud_accounts ON cloud_accounts.cloud_site_id = cloud_sites.id
 			LEFT JOIN cloud_site_domains ON (cloud_site_domains.cloud_site_id = cloud_sites.id)
