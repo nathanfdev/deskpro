6ef99967a4d2ac6cde1df2300878aefaf4e6e411
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-01-15 14:39:09 +0000

    Add cloud-specific api bundle, simple calls to reset user passwords

--- app/src/Cloud/ApiBundle/CloudApiBundle.php
+++ app/src/Cloud/ApiBundle/CloudApiBundle.php
@@ -26,48 +26,22 @@
 \**************************************************************************/
 
 /**
- * DeskPRO
- *
- * @package DeskPRO
- */
+* DeskPRO
+*
+* @package DeskPRO
+*/
 
-namespace DeskPRO\Kernel;
+namespace Cloud\ApiBundle;
 
-use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
-use Symfony\Component\Config\ConfigCache;
-use Symfony\Component\HttpKernel\Debug\ErrorHandler;
-use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
-
-use Application\DeskPRO\App;
-
-class ApiKernel extends AbstractKernel
+class CloudApiBundle extends \Symfony\Component\HttpKernel\Bundle\Bundle
 {
-	protected function registerAdditionalBundles()
+	public function getNamespace()
 	{
-		$bundles = array(
-			new \Application\ApiBundle\ApiBundle(),
-			new \Application\AgentBundle\AgentBundle(),
-		);
-
-		if (defined('DPC_IS_CLOUD')) {
-			$bundles[] = new \Cloud\ApiBundle\CloudApiBundle();
-		}
-
-		return $bundles;
+		return __NAMESPACE__;
 	}
 
-	public function registerContainerConfiguration(LoaderInterface $loader)
+	public function getPath()
 	{
-		if (defined('DPC_IS_CLOUD')) {
-			$loader->load(DP_ROOT.'/sys/config-cloud/api/config_'.$this->getEnvironment().'.php');
-		} else {
-			$loader->load(DP_ROOT.'/sys/config/admin/api'.$this->getEnvironment().'.php');
-		}
+		return __DIR__;
 	}
 }
