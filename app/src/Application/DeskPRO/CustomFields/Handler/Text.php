0448610cd33a7e02e2559f73463231cffc911dc4
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-01-22 09:56:32 +0000

    // Make sure to use regex var that mightve been fixed

--- app/src/Application/DeskPRO/CustomFields/Handler/Text.php
+++ app/src/Application/DeskPRO/CustomFields/Handler/Text.php
@@ -109,7 +109,7 @@ class Text extends HandlerAbstract
 
 		if ($options['regex']) {
 			$regex = Strings::getInputRegexPattern($options['regex']);
-			if ($regex && !preg_match($options['regex'], $data)) {
+			if ($regex && !preg_match($regex, $data)) {
 				return $this->makeErrorArray(array('regex_fail'));
 			}
 		}
