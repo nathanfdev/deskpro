0a747b84f2f64e10a6298e871342f5bbe383bde4
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-01-24 12:10:14 +0000

    Standalone check-req command should attempt to set mem limit or it may falsely report error

--- app/bin/check-req.php
+++ app/bin/check-req.php
@@ -1,5 +1,15 @@
 <?php
 define('DP_ROOT', realpath(dirname(__FILE__) . '/../'));
+
+@ini_set('memory_limit', -1);
+@ini_set('memory_limit', 268435456);
+@set_time_limit(0);
+
+// Normalise env
+setlocale(LC_CTYPE, 'C');
+date_default_timezone_set('UTC');
+ini_set('default_charset', 'UTF-8');
+
 require_once DP_ROOT . '/src/Application/InstallBundle/Install/server_check_functions.php';
 
 $fatal = array();
