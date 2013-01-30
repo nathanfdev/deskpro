9640f57961ac6d4f318b31cf1502925d10983d9f
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-01-30 16:43:56 +0000

    // Another network error

--- app/sys/Kernel/KernelErrorHandler.php
+++ app/sys/Kernel/KernelErrorHandler.php
@@ -654,7 +654,7 @@ class KernelErrorHandler
 		}
 
 		// Socket/network errors
-		if (strpos($errstr, 'stream_socket_enable_crypto():') !== false || strpos($errstr, 'SSL: Broken pipe') !== false || strpos($errstr, 'errno=32 Broken pipe')) {
+		if (strpos($errstr, 'stream_socket_enable_crypto():') !== false || strpos($errstr, 'SSL: Broken pipe') !== false || strpos($errstr, 'errno=32 Broken pipe') || strpos($errstr, 'SSL: An established connection was aborted') !== false) {
 			$no_send_error = true;
 		}
 
