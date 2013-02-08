1995a2947e58ba4150c3944ed6570e94598d66cf
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-02-08 17:16:54 +0000

    Better label management from admin interface when many labels are defined

--- web/javascripts/DeskPRO/Admin/ElementHandler/LabelsPage.js
+++ web/javascripts/DeskPRO/Admin/ElementHandler/LabelsPage.js
@@ -69,18 +69,15 @@ DeskPRO.Admin.ElementHandler.LabelsPage = new Orb.Class({
 		});
 
 		var renameUrl = this.el.data('rename-url');
-		$('#labels_list').on('click', '.rename-trigger', function() {
-			var newLabel = prompt('Enter a new label to rename to');
-			if (newLabel) {
-				newLabel = newLabel.trim();
-			}
-
-			if (!newLabel || !newLabel.length) {
-				return;
-			}
-
+		$('#labels_list').on('change', 'input.label-text', function() {
 			var row = $(this).closest('li.label-row');
 			var label = row.data('label');
+			var newLabel = $.trim($(this).val());
+
+			if (!newLabel || label == newLabel) {
+				$(this).val(label);
+				return;
+			}
 
 			$.ajax({
 				url: renameUrl,
@@ -88,17 +85,9 @@ DeskPRO.Admin.ElementHandler.LabelsPage = new Orb.Class({
 				dataType: 'json',
 				data: { old_label: label, new_label: newLabel },
 				success: function(data) {
-
-					row.remove();
-
-					var existing = $('#labels_list li[data-label="' + label + '"]');
-					existing.remove();
-
-					var row = $(data.row_html).hide();
-					row.prependTo($('#labels_list')).fadeIn('fast');
-
-					DeskPRO_Window.util.showSavePuff($('> div', row));
-					self.updateDefCounts(data.def_counts);
+					row.find('input.label-text').val(newLabel);
+					row.data('label', newLabel).attr('data-label', newLabel);
+					DeskPRO_Window.util.showSavePuff(row.find('.count'));
 				}
 			});
 		});
