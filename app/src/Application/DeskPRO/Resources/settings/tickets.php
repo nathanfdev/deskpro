b01646ebe43a664a0c5a434548e982fdd72242aa
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-02-11 09:39:31 +0000

    The setting to automatically change status to awaiting agent after reassignment is now off by default

--- app/src/Application/DeskPRO/Resources/settings/tickets.php
+++ app/src/Application/DeskPRO/Resources/settings/tickets.php
@@ -47,7 +47,7 @@
 	'core_tickets.reply_assign_assigned' => false,
 	'core_tickets.reply_assignteam_unassigned' => false,
 	'core_tickets.reply_assignteam_assigned' => false,
-	'core_tickets.reassign_auto_change_status' => true,
+	'core_tickets.reassign_auto_change_status' => false,
 	'core_tickets.resolve_auto_close_tab' => false,
 	'core_tickets.new_status' => 'awaiting_user',
 	'core_tickets.new_assign' => 'assign',
