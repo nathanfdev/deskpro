c730801f8cfa38d7f629b6cda0d9153c40487bef
DeskPRO <team@deskpro.com>
2013-02-11 11:38:27 +0000

    Updated languages: Hungarian
    - Hungarian: Added 588 phrases

---
+++ app/languages/hungarian/user/knowledgebase.php
@@ -0,0 +1,14 @@
+<?php return array(
+	'user.knowledgebase.article-viewed'                                    => 'Ezt a bejegyzést 1 alkalommal olvasták|Ezt a bejegyzést {{count}} alkalommal olvasták',
+	'user.knowledgebase.article_not_found'                                 => 'A bejegyzés nem található.',
+	'user.knowledgebase.category_no-articles'                              => 'Ez a kategória nem tartalmaz bejegyzéseket.',
+	'user.knowledgebase.knowledgebase'                                     => 'Tudásbázis',
+	'user.knowledgebase.link_view-all'                                     => 'A bejegyzés megtekintése|Az összes ({{count}} db) bejegyzés megtekintése',
+	'user.knowledgebase.related_articles'                                  => 'Kapcsolódó bejegyzések',
+	'user.knowledgebase.sidebar_description'                               => 'A tudásbázisunk jelenleg egy bejegyzést tartalmaz, amely segíthet Önnek.|A tudásbázisunk jelenleg {{count}} bejegyzést tartalmaz, amely segíthet Önnek.',
+	'user.knowledgebase.title'                                             => 'Tudásbázis',
+	'user.knowledgebase.title-articles'                                    => '1 bejegyzés|{{count}} bejegyzés',
+	'user.knowledgebase.title-articles-paged'                              => 'Bejegyzések megjelenítése: {{first}} - {{last}} / {{total}}',
+	'user.knowledgebase.title-categories'                                  => '1 alkategória|{{count}} alkategória',
+	'user.knowledgebase.type_article'                                      => 'Bejegyzés',
+);
