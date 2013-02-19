82f362c0f656341e92072ebb59b6e77f88744deb
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-02-19 18:21:08 +0000

    Fix body_full in same way body was fixed

--- app/src/Application/DeskPRO/EmailGateway/TicketGatewayProcessor.php
+++ app/src/Application/DeskPRO/EmailGateway/TicketGatewayProcessor.php
@@ -767,13 +767,14 @@ class TicketGatewayProcessor extends AbstractGatewayProcessor
 			$email_info['body_full'] = $inline_images2->processTokens($email_info['body_full']);
 
 			// The basic cleaner cleans out outlook type stuff like empty <p>'s that cause whitespace
+			$email_info['body'] = $this->cleaner->clean($email_info['body'], 'html_email_preclean');
 			$email_info['body'] = $this->cleaner->clean($email_info['body'], 'html_email_basicclean');
 			$email_info['body'] = $this->cleaner->clean($email_info['body'], 'html_email');
+			$email_info['body'] = $this->cleaner->clean($email_info['body'], 'html_email_postclean');
 		}
 
 		if ($email_info['body_is_html']) {
 			$email_info['body'] = $this->trimHtmlWhitespace($email_info['body']);
-			$email_info['body_full'] = $this->trimHtmlWhitespace($email_info['body_full']);
 		}
 
 		$email_info['body'] = $this->replaceInlineAttachTokens($email_info['body'], $inline_images);
