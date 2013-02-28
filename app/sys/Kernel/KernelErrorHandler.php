442e31354c13352de9580d620bbbb12d66d3534c
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-02-28 11:02:30 +0000

    In error log, log how long the script was running

--- app/sys/Kernel/KernelErrorHandler.php
+++ app/sys/Kernel/KernelErrorHandler.php
@@ -315,7 +315,7 @@ class KernelErrorHandler
 			$line = sprintf("DeskPRO Exception: %s:%s (%s line %s): %s", $errinfo['exception_type'], $e->getCode(), $errinfo['errfile'], $errinfo['errline'], $e->getMessage());
 			$str[] = sprintf("Exception: %s %s\n", $e->getCode(), $e->getMessage());
 			$str[] = sprintf("\tType: %s\n", $errinfo['exception_type']);
-			$str[] = sprintf("\tDate: %s\n", date('Y-m-d H:i:s'));
+			$str[] = sprintf("\tDate: %s (Running time to error: %s)\n", date('Y-m-d H:i:s'), $errinfo['time_to_error']);
 			$str[] = sprintf("\tBuild: %s\n", defined('DP_BUILD_NUM') ? DP_BUILD_NUM : defined('DP_BUILD_TIME') ? DP_BUILD_TIME : '0');
 			if (!empty($errinfo['url'])) {
 				$str[] = sprintf("\tURL: %s\n", $errinfo['url']);
@@ -326,7 +326,7 @@ class KernelErrorHandler
 			$line = sprintf("DeskPRO Error: %s (%s line %s): %s", $errinfo['errname'], $errinfo['errfile'], $errinfo['errline'], $errinfo['errstr']);
 			$str[] = sprintf("Error: %s\n", $errinfo['errstr']);
 			$str[] = sprintf("\tType: %s\n", $errinfo['errname']);
-			$str[] = sprintf("\tDate: %s\n", date('Y-m-d H:i:s'));
+			$str[] = sprintf("\tDate: %s (Running time to error: %s)\n", date('Y-m-d H:i:s'), $errinfo['time_to_error']);
 			$str[] = sprintf("\tBuild: %s\n", defined('DP_BUILD_NUM') ? DP_BUILD_NUM : defined('DP_BUILD_TIME') ? DP_BUILD_TIME : '0');
 			if (!empty($errinfo['url'])) {
 				$str[] = sprintf("\tURL: %s\n", $errinfo['url']);
