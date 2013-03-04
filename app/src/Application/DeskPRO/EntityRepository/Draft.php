f1d26e8872a7921c94ae9aa2491c21bda4c46042
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-03-04 18:55:56 +0000

    Fix dupe drafts

--- app/src/Application/DeskPRO/EntityRepository/Draft.php
+++ app/src/Application/DeskPRO/EntityRepository/Draft.php
@@ -129,6 +129,10 @@ class Draft extends AbstractEntityRepository
 		$draft->person = $person;
 
 		$this->getEntityManager()->persist($draft);
+
+		$this->getEntityManager()->getConnection()->executeUpdate("
+			DELETE FROM drafts WHERE content_type = ? AND content_id = ? AND person_id =?
+		", array($content_type, $content_id, $person->getId()));
 		$this->getEntityManager()->flush();
 
 		return $draft;
