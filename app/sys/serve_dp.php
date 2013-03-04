403ffe0601b1d59477c093efe355df7b281dafbf
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-03-04 12:10:15 +0000

    Fix getting IP address when using "trust proxy data" - HTTP_X_FORWARDED_FOR can be an array of IPs so we need to fetch the first (ie first) IP in the chain

--- app/sys/serve_dp.php
+++ app/sys/serve_dp.php
@@ -143,7 +143,7 @@ class DpLoader extends LoaderAbstract
 			$user_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 			$user_ip = explode(',', $user_ip);
 			if (isset($user_ip[0])) {
-				$user_ip = $user_ip;
+				$user_ip = $user_ip[0];
 			} else {
 				$user_ip = $_SERVER['REMOTE_ADDR'];
 			}
