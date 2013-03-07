254c8998945a2ae64abc588c062d140ee14c3559
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-03-07 13:24:51 +0000

    Strip out <meta> tags in HTML email that set Content-Type - Email body is converted into UTF-8 as part of decoding process, having a mis-matching meta tag with a different charset will mess up DOMDocument on Windows

--- app/src/Orb/Input/Cleaner/CleanerPlugin/HtmlPurifier.php
+++ app/src/Orb/Input/Cleaner/CleanerPlugin/HtmlPurifier.php
@@ -111,6 +111,11 @@ class HtmlPurifier implements CleanerPlugin
 			$value = str_replace('</head>', '</head><body>', $value);
 			$value = str_replace('</html>', '</body></html>', $value);
 
+			// The message has been converted to UTF8 by now, and is HTML,
+			// so discard the meta content-type tag that can interfere with
+			// DOMDocument (esp on windows)
+			$value = preg_replace('#<meta.*?Content-Type.*?>#', '', $value);
+
 			// Remove <a name="_MailEndCompose">. HTMLPurifier will clean up the </a> automatically
 			$value = str_replace('<a name="_MailEndCompose">', '', $value);
 
