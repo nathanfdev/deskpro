edfd009d2795172e563cf147fa329aa007f3c878
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-03-19 16:06:46 +0000

    Fix test bootstrapper

--- app/sys/tests/bootstrap.php
+++ app/sys/tests/bootstrap.php
@@ -1,3 +1,4 @@
 <?php
 define('DP_ROOT', realpath(__DIR__.'/../../'));
-require(DP_ROOT.'/sys/autoload.php');
+define('DP_WEB_ROOT', realpath(__DIR__.'/../../web'));
+require(DP_ROOT.'/sys/bootstrap-dev.php');
