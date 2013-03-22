b602a0c097c892f09644664da5f883811db14ecc
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-03-22 09:50:47 +0000

    Fix basepath setting

--- app/src/Application/DeskPRO/DependencyInjection/SystemServices/BlobStorageService.php
+++ app/src/Application/DeskPRO/DependencyInjection/SystemServices/BlobStorageService.php
@@ -74,6 +74,7 @@ class BlobStorageService
 			$adapter = new AmazonS3Storage(array(
 				's3_client' => $client,
 				'bucket'    => $container->getSetting('core.filestorage_s3_bucket'),
+				'base_path' => $container->getSetting('core.filestorage_s3_basepath')
 			));
 			$adapter->setLogger($logger);
 
