096fd95e33a87b691bde68b3446a91251deba3bf
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-03-28 14:35:27 +0000

    [FIX] Image thumbnails being made square

--- app/sys/serve_file.php
+++ app/sys/serve_file.php
@@ -1026,26 +1026,48 @@ class FilestorageLoader extends LoaderAbstract
 		// Only shrink if it doesn't fit inside the box.
 		if (max($width, $height) > $size || $is_fit) {
 
-			$width  = $size;
-			$height = $size;
+			if ($is_fit) {
+				$width  = $size;
+				$height = $size;
 
-			$size      = new \Imagine\Image\Box($width, $height);
-			$mode      = \Imagine\Image\ImageInterface::THUMBNAIL_INSET;
-			$resizeimg = $image->thumbnail($size, $mode);
-			$sizeR     = $resizeimg->getSize();
-			$widthR    = $sizeR->getWidth();
-			$heightR   = $sizeR->getHeight();
+				$size      = new \Imagine\Image\Box($width, $height);
+				$mode      = \Imagine\Image\ImageInterface::THUMBNAIL_INSET;
+				$resizeimg = $image->thumbnail($size, $mode);
+				$sizeR     = $resizeimg->getSize();
+				$widthR    = $sizeR->getWidth();
+				$heightR   = $sizeR->getHeight();
 
-			$preserve  = $container->getImagine()->create($size);
-			$startX = $startY = 0;
-			if ( $widthR < $width ) {
-				$startX = ( $width - $widthR ) / 2;
+				$preserve  = $container->getImagine()->create($size);
+				$startX = $startY = 0;
+				if ( $widthR < $width ) {
+					$startX = ( $width - $widthR ) / 2;
+				}
+				if ( $heightR < $height ) {
+					$startY = ( $height - $heightR ) / 2;
+				}
+				$preserve->paste($resizeimg, new \Imagine\Image\Point($startX, $startY));
+				$image = $preserve;
+			} else {
+				$size_w = $size_h = $size;
+
+				if ($height > $width) {
+					$size_w = round($size_w * ($width / $height));
+				}
+				elseif ($width > $height) {
+					$size_h = round($size_h * ($height / $width));
+				}
+
+				if ($size_w == 0) {
+					$size_w = 1;
+				}
+
+				if ($size_h == 0) {
+					$size_h = 1;
+				}
+
+				$box = new \Imagine\Image\Box($size_w, $size_h);
+				$image->resize($box);
 			}
-			if ( $heightR < $height ) {
-				$startY = ( $height - $heightR ) / 2;
-			}
-			$preserve->paste($resizeimg, new \Imagine\Image\Point($startX, $startY));
-			$image = $preserve;
 		}
 
 		$file = $image->get($blob->getImageType());
