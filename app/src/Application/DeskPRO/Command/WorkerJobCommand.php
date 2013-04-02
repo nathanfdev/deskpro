c8d9e8990fb755412a368547d97e116be3551333
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-04-02 09:18:37 +0100

    [FIX] Possible PHP warning when scanning log files ("Offset is greater than the length of haystack string")

--- app/src/Application/DeskPRO/Command/WorkerJobCommand.php
+++ app/src/Application/DeskPRO/Command/WorkerJobCommand.php
@@ -185,7 +185,7 @@ class WorkerJobCommand extends \Symfony\Bundle\FrameworkBundle\Command\Container
 				$last_timestamp = null;
 				$x = 0;
 				while ($x++ < 100) {
-					$line_start = strrpos($log, "\n[", min($last_pos-1000, 0));
+					$line_start = strrpos($log, "\n[", max($last_pos-1000, 0));
 					$last_timestamp = Strings::extractRegexMatch('#\[((.*?)-(.*?)-(.*?) (.*?))\]#', substr($log, $line_start, 100));
 
 					if ($last_timestamp) {
