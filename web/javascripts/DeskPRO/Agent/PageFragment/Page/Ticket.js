e01ca91580e01c27c284285600542cec4447c33e
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-04-18 14:05:34 +0100

    Dont show forward message for agent replies

--- web/javascripts/DeskPRO/Agent/PageFragment/Page/Ticket.js
+++ web/javascripts/DeskPRO/Agent/PageFragment/Page/Ticket.js
@@ -1355,6 +1355,12 @@ DeskPRO.Agent.PageFragment.Page.Ticket = new Orb.Class({
 					menuElement.find('li.delete-link').hide();
 				}
 
+				if (message.hasClass('agent-message')) {
+					menuElement.find('li.forward-message').hide();
+				} else {
+					menuElement.find('li.forward-message').show();
+				}
+
 				if (message.hasClass('with-attach')) {
 					menuElement.find('li.delete-attachments-link').show();
 				} else {
