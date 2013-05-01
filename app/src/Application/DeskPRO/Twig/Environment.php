b31b7d9fca7bbd11f1a2bcad9bdf7173cfbb8cfc
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-05-01 12:09:53 +0100

    Fix errors during build

--- app/src/Application/DeskPRO/Twig/Environment.php
+++ app/src/Application/DeskPRO/Twig/Environment.php
@@ -135,8 +135,11 @@ class Environment extends \Twig_Environment
 							if ($e) {
 								$prev = $e;
 							}
-							$e = new \Exception("IMPORTANT: Could not write twig template file for template $name. You should re-download the DeskPRO source files. Contact support@deskpro.com for assistance.", 0, $prev);
-							KernelErrorHandler::logException($e, false, 'twig_write_failed');
+
+							if (defined('DP_BUILD_NUM') && !defined('DP_BUILDING')) {
+								$e = new \Exception("IMPORTANT: Could not write twig template file for template $name. You should re-download the DeskPRO source files. Contact support@deskpro.com for assistance.", 0, $prev);
+								KernelErrorHandler::logException($e, false, 'twig_write_failed');
+							}
 
 							$source = $this->compileSource($this->loader->getSource($name), $name);
 							eval('?>'.$source);
