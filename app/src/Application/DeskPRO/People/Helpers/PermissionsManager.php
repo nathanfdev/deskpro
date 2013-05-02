4a1eff9d536600aa994c14a35916dc9eebc7ac19
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-05-02 10:18:34 +0100

    md5 that key

--- app/src/Application/DeskPRO/People/Helpers/PermissionsManager.php
+++ app/src/Application/DeskPRO/People/Helpers/PermissionsManager.php
@@ -148,7 +148,7 @@ class PermissionsManager implements \Orb\Helper\ShortCallableInterface
 		$this->usergroups_key = PermissionCache::generateUsergroupSetKey($this->usergroup_ids);
 
 		if ($this->person->is_agent) {
-			$this->usergroups_key .= '.agent-' . $this->person->id;
+			$this->usergroups_key = md5($this->usergroups_key . 'agent-' . $this->person->id);
 		}
 
 		\DpShutdown::add(array($this, 'flushCache'));
