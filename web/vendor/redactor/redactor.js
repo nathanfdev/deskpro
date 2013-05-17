9a2a565666eb70561e3d2436466fb96b1e62f0cb
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-05-17 10:58:36 +0100

    [FIX] Pressing 'enter' in RTE when the current line contains &, < or > characters could reset cursor position

--- web/vendor/redactor/redactor.js
+++ web/vendor/redactor/redactor.js
@@ -4192,7 +4192,8 @@ var RLANG = {
 									.replace(url1, '$1<a href="' + protocol + '$2">$2</a>$3')
 									.replace(url2, '$1<a href="$2">$2</a>$5');
 
-						if (newHtml != html) {
+						if (newHtml != html && newHtml != html.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')) {
+							var nodeNode = $(newHtml);
 							$(n).after(newHtml).remove();
 						}
 					}
