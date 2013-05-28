47c2d31686ef1996ab7cf0c22f44239da2d5752c
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-05-28 09:44:18 +0100

    Disable queueing of error report emails

--- app/sys/Kernel/KernelErrorHandler.php
+++ app/sys/Kernel/KernelErrorHandler.php
@@ -438,6 +438,7 @@ class KernelErrorHandler
 					$message = App::getMailer()->createMessage();
 					$message->setTo(DP_TECHNICAL_EMAIL);
 					$message->setSubject($line);
+					$message->disableQueueHint();
 
 					$email_str = nl2br(htmlspecialchars($email_str, \ENT_QUOTES, 'UTF-8'));
 					$message->setBody($email_str, 'text/html');
