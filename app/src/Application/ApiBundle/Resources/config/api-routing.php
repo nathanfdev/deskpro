62323a086998cdb4b0aec122110845411887ceff
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-06-05 19:42:50 +0100

    API for text snippets

--- app/src/Application/ApiBundle/Resources/config/api-routing.php
+++ app/src/Application/ApiBundle/Resources/config/api-routing.php
@@ -378,61 +378,61 @@ $collection->add('api_tickets_sla_organizations', new Route(
 ));
 
 ################################################################################
-# Ticket Snippets
+# Text Snippets
 ################################################################################
 
-$collection->add('api_ticketsnippets_list', new Route(
-	'/ticket-snippets',
-	array('_controller' => 'ApiBundle:TicketSnippet:getSnippets'),
+$collection->add('api_textsnippets_list', new Route(
+	'/text-snippets/{typename}',
+	array('_controller' => 'ApiBundle:TextSnippets:filterSnippets'),
 	array('_method' => 'GET'),
 	array()
 ));
 
-$collection->add('api_ticketsnippets_new', new Route(
-	'/ticket-snippets',
-	array('_controller' => 'ApiBundle:TicketSnippet:editSnippet', 'snippet_id' => '0'),
+$collection->add('api_textsnippets_new', new Route(
+	'/text-snippets/{typename}',
+	array('_controller' => 'ApiBundle:TextSnippets:saveSnippet', 'snippet_id' => '0'),
 	array('_method' => 'POST'),
 	array()
 ));
 
-$collection->add('api_ticketsnippets_edit', new Route(
-	'/ticket-snippets/{snippet_id}',
-	array('_controller' => 'ApiBundle:TicketSnippet:editSnippet'),
+$collection->add('api_textsnippets_edit', new Route(
+	'/text-snippets/{typename}/{snippet_id}',
+	array('_controller' => 'ApiBundle:TextSnippets:saveSnippet'),
 	array('_method' => 'POST', 'snippet_id' => '\d+'),
 	array()
 ));
 
-$collection->add('api_ticketsnippets_get', new Route(
-	'/ticket-snippets/{snippet_id}',
-	array('_controller' => 'ApiBundle:TicketSnippet:getSnippet'),
+$collection->add('api_textsnippets_get', new Route(
+	'/text-snippets/{typename}/{snippet_id}',
+	array('_controller' => 'ApiBundle:TextSnippets:getSnippet'),
 	array('_method' => 'GET', 'snippet_id' => '\d+'),
 	array()
 ));
 
-$collection->add('api_ticketsnippets_cats_list', new Route(
-	'/ticket-snippets/categories',
-	array('_controller' => 'ApiBundle:TicketSnippet:getCategories'),
+$collection->add('api_textsnippets_cats_list', new Route(
+	'/text-snippets/{typename}/categories',
+	array('_controller' => 'ApiBundle:TextSnippets:listCategories'),
 	array('_method' => 'GET'),
 	array()
 ));
 
-$collection->add('api_ticketsnippets_cats_new', new Route(
-	'/ticket-snippets/categories',
-	array('_controller' => 'ApiBundle:TicketSnippet:editCategory', 'category_id' => '0'),
+$collection->add('api_textsnippets_cats_new', new Route(
+	'/text-snippets/{typename}/categories',
+	array('_controller' => 'ApiBundle:TextSnippets:saveCategory', 'category_id' => '0'),
 	array('_method' => 'POST'),
 	array()
 ));
 
-$collection->add('api_ticketsnippets_cats_edit', new Route(
-	'/ticket-snippets/categories/{category_id}',
-	array('_controller' => 'ApiBundle:TicketSnippet:editCategory'),
+$collection->add('api_textsnippets_cats_edit', new Route(
+	'/text-snippets/{typename}/categories/{category_id}',
+	array('_controller' => 'ApiBundle:TextSnippets:saveCategory'),
 	array('_method' => 'POST', 'category_id' => '\d+'),
 	array()
 ));
 
-$collection->add('api_ticketsnippets_cats_get', new Route(
-	'/ticket-snippets/categories/{category_id}',
-	array('_controller' => 'ApiBundle:TicketSnippet:category_id'),
+$collection->add('api_textsnippets_cats_get', new Route(
+	'/text-snippets/{typename}/categories/{category_id}',
+	array('_controller' => 'ApiBundle:TextSnippets:getCategory'),
 	array('_method' => 'GET', 'category_id' => '\d+'),
 	array()
 ));
