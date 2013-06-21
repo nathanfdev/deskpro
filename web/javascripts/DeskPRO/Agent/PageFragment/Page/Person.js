8a5ac89e21c4923eea0207f0a435f0e724f448d1
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-06-21 16:29:08 +0100

    Couple fixes for IE

--- web/javascripts/DeskPRO/Agent/PageFragment/Page/Person.js
+++ web/javascripts/DeskPRO/Agent/PageFragment/Page/Person.js
@@ -36,43 +36,47 @@ DeskPRO.Agent.PageFragment.Page.Person = new Orb.Class({
 		var lastClip = null;
 		var lastLi = null;
 
-		var flashEnabled = !!(navigator.mimeTypes["application/x-shockwave-flash"] || window.ActiveXObject && new ActiveXObject('ShockwaveFlash.ShockwaveFlash'));
-		if (flashEnabled) {
-			// Set timeout to have it exec in global scope,
-			// so errors (eg flash has crashed) can be ignored and dont break the rest of this init
-			window.setTimeout(function() {
-				self.wrapper.find('.copy-btn').each(function() {
-					var btnEl = this;
-					var btn = $(this);
+		try {
+			var flashEnabled = !!(navigator.mimeTypes["application/x-shockwave-flash"] || window.ActiveXObject && new ActiveXObject('ShockwaveFlash.ShockwaveFlash'));
+			if (flashEnabled) {
+				// Set timeout to have it exec in global scope,
+				// so errors (eg flash has crashed) can be ignored and dont break the rest of this init
+				window.setTimeout(function() {
+					self.wrapper.find('.copy-btn').each(function() {
+						var btnEl = this;
+						var btn = $(this);
 
-					try {
-						var clip = new ZeroClipboard(this, {
-							btnEl: this
-						});
-						clip.on('mouseover', function(client, args) {
-							$(client.options.btnEl).addClass('over');
-						});
-						clip.on('mouseout', function(client, args) {
-							$(client.options.btnEl).removeClass('over');
-						});
-						clip.on('complete', function(client, args) {
+						try {
+							var clip = new ZeroClipboard(this, {
+								btnEl: this
+							});
+							clip.on('mouseover', function(client, args) {
+								$(client.options.btnEl).addClass('over');
+							});
+							clip.on('mouseout', function(client, args) {
+								$(client.options.btnEl).removeClass('over');
+							});
+							clip.on('complete', function(client, args) {
 
-						});
+							});
 
-						self.addEvent('destroy', function() {
-							try {
-								clip.unglue(btnEl);
-							} catch (e) {}
-						});
-						self.addEvent('activate', function() {
-							try {
-								clip.reposition();
-							} catch (e) {}
-						});
-					} catch (e) {}
-				});
-			}, 100);
-		} else {
+							self.addEvent('destroy', function() {
+								try {
+									clip.unglue(btnEl);
+								} catch (e) {}
+							});
+							self.addEvent('activate', function() {
+								try {
+									clip.reposition();
+								} catch (e) {}
+							});
+						} catch (e) {}
+					});
+				}, 100);
+			} else {
+				this.wrapper.find('.copy-btn').remove();
+			}
+		} catch (e) {
 			this.wrapper.find('.copy-btn').remove();
 		}
 
--- web/javascripts/DeskPRO/Agent/PageFragment/Page/Ticket.js
+++ web/javascripts/DeskPRO/Agent/PageFragment/Page/Ticket.js
@@ -39,44 +39,48 @@ DeskPRO.Agent.PageFragment.Page.Ticket = new Orb.Class({
 		this.getEl('replybox_wrap').data('page', this);
 		this.hasReplyFocused = false;
 
-		var flashEnabled = !!(navigator.mimeTypes["application/x-shockwave-flash"] || window.ActiveXObject && new ActiveXObject('ShockwaveFlash.ShockwaveFlash'));
-		if (flashEnabled) {
-			// Set timeout to have it exec in global scope,
-			// so errors (eg flash has crashed) can be ignored and dont break the rest of this init
-			window.setTimeout(function() {
-				self.wrapper.find('.copy-btn').each(function() {
-					var btnEl = this;
-					var btn = $(this);
+		try {
+			var flashEnabled = !!(navigator.mimeTypes["application/x-shockwave-flash"] || window.ActiveXObject && new ActiveXObject('ShockwaveFlash.ShockwaveFlash'));
+			if (flashEnabled) {
+				// Set timeout to have it exec in global scope,
+				// so errors (eg flash has crashed) can be ignored and dont break the rest of this init
+				window.setTimeout(function() {
+					self.wrapper.find('.copy-btn').each(function() {
+						var btnEl = this;
+						var btn = $(this);
 
-					try {
-						var clip = new ZeroClipboard(this, {
-							btnEl: this,
-							savePuffEl: self.getEl('idref_switch')
-						});
-						clip.on('mouseover', function(client, args) {
-							$(client.options.btnEl).addClass('over');
-						});
-						clip.on('mouseout', function(client, args) {
-							$(client.options.btnEl).removeClass('over');
-						});
-						clip.on('complete', function(client, args) {
-							DeskPRO_Window.util.showSavePuff($(this).closest('.id-number'));
-						});
+						try {
+							var clip = new ZeroClipboard(this, {
+								btnEl: this,
+								savePuffEl: self.getEl('idref_switch')
+							});
+							clip.on('mouseover', function(client, args) {
+								$(client.options.btnEl).addClass('over');
+							});
+							clip.on('mouseout', function(client, args) {
+								$(client.options.btnEl).removeClass('over');
+							});
+							clip.on('complete', function(client, args) {
+								DeskPRO_Window.util.showSavePuff($(this).closest('.id-number'));
+							});
 
-						self.addEvent('destroy', function() {
-							try {
-								clip.unglue(btnEl);
-							} catch (e) {}
-						});
-						self.addEvent('activate', function() {
-							try {
-								clip.reposition();
-							} catch (e) {}
-						});
-					} catch (e) {}
-				});
-			}, 100);
-		} else {
+							self.addEvent('destroy', function() {
+								try {
+									clip.unglue(btnEl);
+								} catch (e) {}
+							});
+							self.addEvent('activate', function() {
+								try {
+									clip.reposition();
+								} catch (e) {}
+							});
+						} catch (e) {}
+					});
+				}, 100);
+			} else {
+				this.wrapper.find('.copy-btn').remove();
+			}
+		} catch (e) {
 			this.wrapper.find('.copy-btn').remove();
 		}
 
