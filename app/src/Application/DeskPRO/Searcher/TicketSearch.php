9e77f6c3c049c46d508d27dca47aa4e284269b5f
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-06-24 09:26:04 +0100

    Isset check before is_array check to prevent notice about undefined index

--- app/src/Application/DeskPRO/Searcher/TicketSearch.php
+++ app/src/Application/DeskPRO/Searcher/TicketSearch.php
@@ -1563,7 +1563,7 @@ class TicketSearch extends SearcherAbstract
 
 						break;
 					case self::TERM_ORGANIZATION:
-						if (is_array($choice['organization_ids']) && isset($choice['organization_ids'])) {
+						if (isset($choice['organization_ids']) && is_array($choice['organization_ids'])) {
 							$choice = $choice['organization_ids'];
 						}
 						if (!is_array($choice)) {
