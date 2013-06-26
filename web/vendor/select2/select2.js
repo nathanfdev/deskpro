57199339037d4f8edba65fa6af7b207e6de3cd9f
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-06-26 15:44:38 +0100

    [FIX] Weird phantom option added to labels list (and some other multi-select select2's)

--- web/vendor/select2/select2.js
+++ web/vendor/select2/select2.js
@@ -2635,6 +2635,9 @@ the specific language governing permissions and limitations under the Apache Lic
         },
 
         addSelectedChoice: function (data) {
+			if (!data || !data.id || !data.text || (data.id === "" && data.text === "")) {
+				return;
+			}
             var enableChoice = !data.locked,
                 enabledItem = $(
                     "<li class='select2-search-choice'>" +
