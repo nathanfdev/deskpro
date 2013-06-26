d4839689f45354be6e562f63343f3eefb8d9c519
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-06-26 11:52:36 +0100

    [FIX] Entries into log_items could potentially cause errors if long logs went over max_packet_size

--- app/src/Application/DeskPRO/Log/Writer/LogItemEntity.php
+++ app/src/Application/DeskPRO/Log/Writer/LogItemEntity.php
@@ -46,15 +46,33 @@ class LogItemEntity extends \Orb\Log\Writer\AbstractWriter
 	public function _write(\Orb\Log\LogItem $log_item)
 	{
 		try {
+
+			$message = $log_item->getMessage();
+			$message_len = strlen($message);
+
+			$data = $log_item->getExtra() ? serialize($log_item->getExtra()) : null;
+			$data_len = $data ? strlen($data) : 0;
+
+			$max_size = App::getDb()->getMaxPacketSize();
+			if (($message_len + $data_len) * 2 >= $max_size) {
+				$message = substr($message, 0, ($max_size - 50) /2);
+				$message_len = strlen($message);
+
+				if (($message_len + $data_len) * 2 >= $max_size) {
+					$data = null;
+					$data_len = 0;
+				}
+			}
+
 			App::getDb()->insert('log_items', array(
 				'log_name'         => $log_item->getLogName(),
 				'session_name'     => $log_item->getSessionName(),
-				'message'          => $log_item->getMessage(),
+				'message'          => $message,
 				'priority'         => $log_item->getPriority(),
 				'priority_name'    => $log_item->getPriorityName(),
 				'date_created'     => $log_item->getDatetime()->format('Y-m-d H:i:s'),
 				'flag'             => $log_item->getFlag() ?: null,
-				'data'             => $log_item->getExtra() ? serialize($log_item->getExtra()) : null,
+				'data'             => $data,
 			));
 		} catch (\Exception $e) {
 			KernelErrorHandler::logException($e, false);
