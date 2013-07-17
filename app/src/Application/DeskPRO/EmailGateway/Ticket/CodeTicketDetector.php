3c4c71e71b8cfb07cd1c8d0c076dcc1b74b46d15
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-07-17 12:41:41 +0100

    [FIX] Possible PHP notices in some email processing to do with detecting TAC codes

--- app/src/Application/DeskPRO/EmailGateway/Ticket/CodeTicketDetector.php
+++ app/src/Application/DeskPRO/EmailGateway/Ticket/CodeTicketDetector.php
@@ -120,13 +120,13 @@ class CodeTicketDetector implements TicketDetectorInterface, Loggable
 			$m = null;
 			if (preg_match_all('#(P?)TAC\-([A-Za-z0-9]+)\.#', $body, $m, \PREG_SET_ORDER)) {
 				foreach ($m as $match) {
-					if ($m[1]) {
-						$this->getLogger()->logDebug("[CodeTicketDetector] Found PTAC in body-headers: " . $m[2]);
+					if ($match[1]) {
+						$this->getLogger()->logDebug("[CodeTicketDetector] Found PTAC in body-headers: " . $match[2]);
 					} else {
-						$this->getLogger()->logDebug("[CodeTicketDetector] Found TAC in body-headers: " . $m[2]);
+						$this->getLogger()->logDebug("[CodeTicketDetector] Found TAC in body-headers: " . $match[2]);
 					}
 
-					$search_text[] = '(#' . $m[2] . ')';
+					$search_text[] = '(#' . $match[2] . ')';
 				}
 			}
 		}
