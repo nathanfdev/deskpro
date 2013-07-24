847fa96c5f82a606bb5769f0cf66198d38908eaf
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-07-24 16:31:33 +0100

    [IMPROVE] New flat style in the crm pane in the agent interface [IMPROVE] New flat style in the publish pane in the agent interface [IMPROVE] New flat style in the chat pane in the agent interface [IMPROVE] New flat style in the feedback pane in the agent interface [IMPROVE] New flat style in the tasks pane in the agent interface

--- web/javascripts/DeskPRO/Agent/WindowElement/Section/Publish.js
+++ web/javascripts/DeskPRO/Agent/WindowElement/Section/Publish.js
@@ -142,349 +142,37 @@ DeskPRO.Agent.WindowElement.Section.Publish = new Orb.Class({
 
 		var self = this;
 
-		$('section.group-section header', this.contentEl).click(function() {
-			var section = $(this).parent();
-			if (section.is('.open')) {
-				section.removeClass('open');
-				$('> article', section).slideUp('fast', function() {
-					self.updateUi();
-				});
+		this.contentEl.find('.pane-section').filter('.dp-collapsible').each(function() {
+			var section = $(this);
+			var header  = section.find('> header');
+			var article = section.find('> article');
+
+			header.find('.dp-collapsible-toggle-btn').on('click', function(ev) {
+				Orb.cancelEvent(ev);
+				section.toggleClass('dp-collapsible-open');
+				self.updateUi();
+			});
+		});
+
+		this.contentEl.find('.dp-toggle-icon').on('click', function(ev) {
+			Orb.cancelEvent(ev);
+			var $me    = $(this);
+			var $li    = $me.closest('li');
+			var $groupList = $li.find('> .nav-list-small');
+
+			if ($me.hasClass('icon-caret-right')) {
+				$me.removeClass('icon-caret-right');
+				$me.addClass('icon-caret-down');
+				$groupList.show();
 			} else {
-				section.addClass('open');
-				$('> article', section).slideDown('fast', function() {
-					self.updateUi();
-				});
+				$me.addClass('icon-caret-right');
+				$me.removeClass('icon-caret-down');
+				$groupList.hide();
 			}
 		});
 
 		this._initGlossary();
-
-		var types = ['articles','downloads','news'];
-		this.catEditors = {};
-
-		var makeOrderData = function(orders) {
-			var orderData = [];
-
-			Array.each(orders, function(id) {
-				orderData.push({
-					name: 'orders[]',
-					value: id
-				});
-			});
-
-			return orderData;
-		};
-
-		var makeStructureData = function(structure, name) {
-			var structureData = [];
-
-			if (!name) {
-				name = 'structure';
-			}
-
-			Object.each(structure, function(parent_id, id) {
-				structureData.push({
-					name: name + '[' + id + ']',
-					value: parent_id
-				});
-			});
-
-			return structureData;
-		};
-
-		var makeTitleData = function(titles) {
-			var titleData = [];
-
-			Object.each(titles, function(title, id) {
-				titleData.push({
-					name: 'titles[' + id + ']',
-					value: title
-				});
-			});
-
-			return titleData;
-		};
-
-		Array.each(types, function(type) {
-
-			var listEl = $('#publish_outline_'+type+'cat_list');
-			listEl.parent().css('position', 'relative');
-
-			var catTreeLoading = function(turn_on) {
-				if (turn_on) {
-					listEl.parent().addClass('mark-loading');
-				} else {
-					listEl.parent().removeClass('mark-loading');
-				}
-			};
-
-			var updateNewOverlay = function() {
-				if (type == 'articles' && DeskPRO_Window.newArticleLoader) {
-					DeskPRO_Window.newArticleLoader.clear();
-				} else if (type == 'downloads' && DeskPRO_Window.newDownloadLoader) {
-					DeskPRO_Window.newDownloadLoader.clear();
-				} else if (type == 'news' && DeskPRO_Window.newNewsLoader) {
-					DeskPRO_Window.newNewsLoader.clear();
-				}
-			};
-
-			var ed = new DeskPRO.UI.CatListEditor({
-				listEl: listEl,
-				itemSelector: 'li:not(.all)',
-				newItemTplSelector: '#publish_outline_cat_list_newitem',
-				editorBaseId: 'publish_',
-				onReordered: function() {
-					$.ajax({
-						url: BASE_URL + 'agent/publish/categories/'+type+'/update-orders',
-						data: makeOrderData(ed.getOrder()),
-						type: 'POST'
-					});
-				},
-				onRestructured: function() {
-
-					catTreeLoading(1);
-
-					// This timeout is here because by the time we get notiifed from droppable,
-					// the dom elements arent actually in place yet,
-					// so if we want a valid structure from getStructure we need to delay for a
-					// few ms so the dom update is run first
-					window.setTimeout(function() {
-						// Hide/show delete icons
-						$('.dp-cat-li', listEl).each(function() {
-							var show = true;
-							$('.list-counter', this).each(function() {
-								if (parseInt($(this).text().trim()) > 0) {
-									show = false;
-									return false;
-								}
-							});
-
-							if (show) {
-								$('.delete-cat', this).removeClass('undeletable');
-							} else {
-								$('.delete-cat', this).addClass('undeletable');
-							}
-						});
-
-						// Recounts
-						self.recountChildCounts(listEl);
-
-						var postData = makeStructureData(ed.getStructure());
-						postData.append(makeStructureData(ed.pristineStructure, 'structure_check'));
-
-						$('.dp-cat-li', listEl).each(function() {
-							postData.push({
-								name: 'orders[]',
-								value: $(this).data('category-id')
-							});
-						});
-
-						$.ajax({
-							url: BASE_URL + 'agent/publish/categories/'+type+'/update-structure',
-							data: postData,
-							dataType: 'json',
-							type: 'POST',
-							error: function() {
-								self.reload();
-							},
-							success: function(result) {
-								if (result.error) {
-									self.reload();
-									DeskPRO_Window.showAlert('Could not update category structure because someone else updated it before you. The section will now refresh and you can try again.');
-									return;
-								}
-
-								catTreeLoading(0);
-
-								// Reset tree checker
-								ed.pristineStructure = ed.getStructure();
-								self.updateUi();
-							}
-						});
-					}, 200); //end timeout
-				},
-				onCatUpdated: function(categoryId, newTitle, newUgs) {
-
-					var postData = [];
-					postData.push({
-						name: 'title',
-						value: newTitle
-					});
-
-					Array.each(newUgs, function(id) {
-						postData.push({
-							name: 'usergroup_ids[]',
-							value: id
-						});
-					});
-
-					$.ajax({
-						url: BASE_URL + 'agent/publish/categories/'+type+'/update/' + categoryId,
-						data: postData,
-						type: 'POST'
-					});
-				},
-				onNewAdded: function(li, input) {
-					// Saving having on blur, which might have happened by clicking trashcan
-					if (li.is('.being-deleted')) {
-						return;
-					}
-					var title = input.val().trim();
-					self.updateUi();
-
-					catTreeLoading(1);
-					$.ajax({
-						url: BASE_URL + 'agent/publish/categories/'+type+'/add-category',
-						data: { title: title },
-						type: 'POST',
-						dataType: 'json',
-						success: function(info) {
-
-							catTreeLoading(0);
-
-							li.data('category-id', info.id);
-							li.data('usergroup-ids', '1');
-							$('.is-nav-item', li).data('route', 'listpane:' + info.url).attr('data-route', 'listpane:' + info.url);;
-							$('.list-counter', li).attr('id', type + '_cat_count_' + info.id);
-
-							// The new overlays need to be reloaded if a new cat was added
-							updateNewOverlay();
-
-							// Reset tree checker
-							ed.pristineStructure = ed.getStructure();
-							self.updateUi();
-						}
-					});
-				}
-			});
-
-			$('#publish_outline_'+type+'cat_editmode').on('click', function() {
-				var ul = $(this).parent().parent();
-				ul.toggleClass('edit-mode');
-
-				if (ul.hasClass('edit-mode')) {
-					ed.enableEditMode();
-					self.updateUi();
-				} else {
-					ed.disableEditMode();
-					self.updateUi();
-				}
-			});
-
-			$('#publish_outline_'+type+'cat_edittiles').on('click', function() {
-				if (ed.isTitleEditing()) {
-					ed.endEditTitles();
-				} else {
-					ed.showEditTitles();
-				}
-			});
-
-			$('#publish_outline_'+type+'cat_addcat').on('click', function() {
-				var ul = $(this).parent().parent();
-				ul.toggleClass('edit-mode');
-				ed.addNew();
-			});
-
-			$('#publish_outline_'+type+'_add').on('click', function() {
-				var name = $(this).data('newloader-name');
-				if (!name || !DeskPRO_Window[name]) {
-					return;
-				}
-
-				DeskPRO_Window[name].toggle();
-			});
-
-			$('#publish_outline_'+type+'cat_list').on('click', '.edit-cat', function(ev) {
-				var li = $(this).parent().parent();
-				ed.showEditor(li);
-			});
-
-			$('#publish_outline_'+type+'cat_list').on('click', '.delete-cat', function(ev) {
-
-				var i = 0;
-				var li = $(this);
-				while (!li.is('li')) {
-					if (i++ > 5) return;
-					li = li.parent();
-				}
-
-				self.updateUi();
-				var fn = function() {
-					catTreeLoading(1);
-					$.ajax({
-						url: BASE_URL + 'agent/publish/categories/'+type+'/delete-category',
-						data: { category_id: li.data('category-id') },
-						type: 'POST',
-						dataType: 'json',
-						error: function() {
-							catTreeLoading(0);
-							li.show();
-						},
-						success: function(info) {
-
-							catTreeLoading(0);
-
-							// It could've been removed by now
-							if (!li || !li.closest('html').length) {
-								li = null;
-							}
-
-							if (info.error) {
-								if (info.error_code == 'not_empty') {
-									DeskPRO_Window.showAlert('The category is not empty. You cannot delete categories that contain articles or other categories.');
-								}
-
-								if (li) {
-									li.show();
-								}
-								self.reload();
-								return;
-							}
-
-							if (li) {
-								li.remove();
-							}
-							updateNewOverlay();
-
-							// Reset tree checker
-							ed.pristineStructure = ed.getStructure();
-						}
-					});
-				};
-
-				li.addClass('being-deleted');
-
-				if (li.data('category-id')) {
-					li.fadeOut('fast', fn);
-				} else {
-					li.fadeOut('fast');
-				}
-			});
-
-			// Perform count calcs now
-			this.recountChildCounts(listEl);
-
-			$('#publish_outline_'+type+'cat_list').find('li.has-children').addClass('sub-expanded');
-		}, this);
-
-
 		this.recountBadge();
-
-		if (this.expanded_ids.length) {
-			this.contentEl.find('section.group-section').removeClass('open').find('> article').hide();
-			Array.each(this.expanded_ids, function(id) {
-				$('#' + id).addClass('open').find('> article').show();
-			});
-			this.expanded_ids = [];
-		}
-		if (this.expanded_cats.length) {
-			this.contentEl.find('li.sub-expanded').removeClass('sub-expanded');
-			Array.each(this.expanded_cats, function(id) {
-				$('#' + id).addClass('sub-expanded');
-			});
-
-			this.expanded_cats = [];
-		}
 
 		this.fireEvent('sectionInit');
 		this.updateUi();
