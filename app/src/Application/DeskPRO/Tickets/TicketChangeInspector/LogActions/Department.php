fe0b9124cd7ae2985f84303a322be66310a057e9
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-08-14 19:16:36 +0100

    [FIX] Department name in ticket logs did not include the parent name

--- app/src/Application/DeskPRO/Tickets/TicketChangeInspector/LogActions/Department.php
+++ app/src/Application/DeskPRO/Tickets/TicketChangeInspector/LogActions/Department.php
@@ -68,9 +68,9 @@ class Department extends AbstractLogAction
 			'id_after'  => $this->new_dep['id'] ?: null,
 
 			'old_department_id'    => $this->old_dep ? $this->old_dep['id'] : 0,
-			'old_department_title' => $this->old_dep ? $this->old_dep['title'] : '',
+			'old_department_title' => $this->old_dep ? $this->old_dep->getFullTitle() : '',
 			'new_department_id'    => $this->new_dep ? $this->new_dep['id'] : 0,
-			'new_department_title' => $this->new_dep ? $this->new_dep['title'] : '',
+			'new_department_title' => $this->new_dep ? $this->new_dep->getFullTitle() : '',
 		);
 	}
 
