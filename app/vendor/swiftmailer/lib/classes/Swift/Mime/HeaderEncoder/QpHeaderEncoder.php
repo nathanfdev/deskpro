4ecceeeb34b365d9f1ffd6c6e2c2caf5d56a6871
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-08-15 17:12:11 +0100

    Use a static map rather than generating it. Possible PHP bug can cause range(0x61, 0x7A) to fail. - Not sure if there's an official swiftmailer patch, github was down at the time I found this

--- app/vendor/swiftmailer/lib/classes/Swift/Mime/HeaderEncoder/QpHeaderEncoder.php
+++ app/vendor/swiftmailer/lib/classes/Swift/Mime/HeaderEncoder/QpHeaderEncoder.php
@@ -29,10 +29,8 @@ class Swift_Mime_HeaderEncoder_QpHeaderEncoder extends Swift_Encoder_QpEncoder i
 
     protected function initSafeMap()
     {
-        foreach (array_merge(
-            range(0x61, 0x7A), range(0x41, 0x5A),
-            range(0x30, 0x39), array(0x20, 0x21, 0x2A, 0x2B, 0x2D, 0x2F)
-        ) as $byte) {
+		$map = array(32, 33, 42, 43, 45, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122);
+        foreach ($map as $byte) {
             $this->_safeMap[$byte] = chr($byte);
         }
     }
