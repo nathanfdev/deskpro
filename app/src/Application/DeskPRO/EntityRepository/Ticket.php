25c973cef23188d880ca863a463a85475720d6ba
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-08-30 15:07:46 +0100

    [IMPROVE] In user profile, sort tickets by status/urgency and then date

--- app/src/Application/DeskPRO/EntityRepository/Ticket.php
+++ app/src/Application/DeskPRO/EntityRepository/Ticket.php
@@ -270,7 +270,7 @@ class Ticket extends AbstractEntityRepository
 				SELECT id
 				FROM tickets
 				WHERE id IN (" . implode(',', $ids) . ")
-				ORDER BY FIELD(tickets.status, 'awaiting_agent', 'awaiting_user', 'resolved', 'closed', 'hidden') ASC, tickets.urgency DESC
+				ORDER BY FIELD(tickets.status, 'awaiting_agent', 'awaiting_user', 'resolved', 'closed', 'hidden') ASC, tickets.urgency DESC, tickets.id ASC
 			");
 		} else {
 			sort($ids, \SORT_NUMERIC);
