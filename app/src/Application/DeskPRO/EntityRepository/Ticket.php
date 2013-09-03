fecf3d0efb1f078daec62dfdf0ed63e4b75ac16c
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-09-03 11:29:16 +0100

    Fix final ticket list order clause in user profile, newer should be first

--- app/src/Application/DeskPRO/EntityRepository/Ticket.php
+++ app/src/Application/DeskPRO/EntityRepository/Ticket.php
@@ -270,7 +270,7 @@ class Ticket extends AbstractEntityRepository
 				SELECT id
 				FROM tickets
 				WHERE id IN (" . implode(',', $ids) . ")
-				ORDER BY FIELD(tickets.status, 'awaiting_agent', 'awaiting_user', 'resolved', 'closed', 'hidden') ASC, tickets.urgency DESC, tickets.id ASC
+				ORDER BY FIELD(tickets.status, 'awaiting_agent', 'awaiting_user', 'resolved', 'closed', 'hidden') ASC, tickets.urgency DESC, tickets.id DESC
 			");
 		} else {
 			sort($ids, \SORT_NUMERIC);
