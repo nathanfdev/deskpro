afece723d8732a7a6569849830335854d3d6ac67
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-09-17 08:50:07 +0100

    Fix notice about bad phrase

--- app/src/Application/DeskPRO/Searcher/TicketSearch.php
+++ app/src/Application/DeskPRO/Searcher/TicketSearch.php
@@ -1592,7 +1592,9 @@ class TicketSearch extends SearcherAbstract
 							if (strpos($c, '.') !== false) {
 								list ($status, $hstatus) = explode('.', $c, 2);
 								$hidden_status[] = $hstatus;
-								$choice_str[] = $tr->phrase('agent.tickets.hidden_status_' . $hstatus);
+								if ($status == 'hidden' && $tr->hasPhrase('agent.tickets.hidden_status_' . $hstatus)) {
+									$choice_str[] = $tr->phrase('agent.tickets.hidden_status_' . $hstatus);
+								}
 								$this->enableArchiveSearch();
 							} else {
 								$show_status[] = $show_status;
