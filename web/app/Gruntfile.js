359c8ca19ebbac050a5153aadcc40046d2519bc1
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-09-26 11:21:39 +0100

    Get Grunt working

--- web/app/Gruntfile.js
+++ web/app/Gruntfile.js
@@ -1,13 +1,52 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  });
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.loadNpmTasks('grunt-recess');
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-  grunt.registerTask('default', ['uglify']);
+		recess: {
+			options: {
+				compile: true
+			},
+			admin_style: {
+				src: ['Admin/Resources/style/admin-style.less'],
+				dest: 'Admin/build/css/admin-style.css'
+			}
+		},
 
+		coffee: {
+			options: {
+				sourceMap: true
+			},
+			admin_js: {
+				files: [{
+					expand: true,
+					flatten: false,
+					cwd: 'Admin/',
+					src: ['**/*.coffee'],
+					dest: 'Admin/build/js',
+					ext: '.js'
+				}]
+			}
+		},
+
+		watch: {
+			admin_recess: {
+				files: 'Admin/Resources/style/*.less',
+				tasks: ['recess']
+			},
+			admin_js: {
+				files: 'Admin/**/*.coffee',
+				tasks: ['coffee']
+			}
+		}
+	});
+
+	grunt.registerTask('build', ['coffee', 'recess']);
+	grunt.registerTask('default', ['coffee', 'recess']);
 };
