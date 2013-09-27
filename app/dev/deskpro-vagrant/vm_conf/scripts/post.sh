2eb57d577fb51fb0a020a064c104f7ab99933644
Christopher Nadeau <chris.nadeau@deskpro.com>
2013-09-27 09:38:40 +0100

    Fix install of node/npm that will be needed for build tools on new adminui. - chris-lea/node.js package now contains nodejs, nodejs-dev and npm all in one and specifying nodejs-dev and npm will come from ubuntu's repos which cause conflicts which makes the whole thing fail
    - Update zip to not include sub-directory

--- app/dev/deskpro-vagrant/vm_conf/scripts/post.sh
+++ app/dev/deskpro-vagrant/vm_conf/scripts/post.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install --yes python-software-properties
 sudo add-apt-repository --yes ppa:chris-lea/node.js
 sudo apt-get update
-sudo apt-get install --yes nodejs nodejs-dev npm
+sudo apt-get install --yes nodejs
 
 npm install -g grunt-cli
 npm install -g bower
