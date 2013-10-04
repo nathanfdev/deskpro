1388e1381a1487f4439ca7e5e3e0cb5462075b38
Christopher Nadeau <chris@nadeau.ws>
2013-10-04 13:44:54 +0100

    travis-ci script

--- app/tests/travis-ci/before_script.sh
+++ app/tests/travis-ci/before_script.sh
@@ -40,8 +40,8 @@ sudo apt-get install -y apache2
 sudo a2enmod actions
 sudo a2enmod rewrite
 echo "export PATH=/home/vagrant/.phpenv/bin:$PATH" | sudo tee -a /etc/apache2/envvars > /dev/null
-echo cat app/tests/travis-ci/apache-php-config.txt | sudo tee /etc/apache2/conf.d/phpconfig > /dev/null
-echo cat app/tests/travis-ci/apache-vhost-config.txt | sed -e "s,PATH,`pwd`,g" | sudo tee /etc/apache2/sites-available/default > /dev/null
+cat app/tests/travis-ci/apache-php-config.txt | sudo tee /etc/apache2/conf.d/phpconfig > /dev/null
+cat app/tests/travis-ci/apache-vhost-config.txt | sed -e "s,PATH,`pwd`,g" | sudo tee /etc/apache2/sites-available/default > /dev/null
 sudo echo "Listen 8888" >> /etc/apache2/ports.conf
 sudo service apache2 restart
 
