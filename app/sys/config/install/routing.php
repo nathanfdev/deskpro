056f53b74f8e0308bd56111a7ae7b833e4353de8
Christopher Nadeau <chris@nadeau.ws>
2013-10-04 15:42:02 +0100

    Fix installer routing - remove running_tests.trigger file

--- app/sys/config/install/routing.php
+++ app/sys/config/install/routing.php
@@ -3,6 +3,8 @@
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
+$collection = new RouteCollection();
+
 $col = $loader->import(DP_ROOT.'/src/Application/InstallBundle/Resources/config/install-routing.php');
 $col->addPrefix('/install');
 $collection->addCollection($col);
