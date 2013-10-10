07f0c8a59107c46b17b87cc96a558fe9445414ed
Christopher Nadeau <chris@nadeau.ws>
2013-10-10 13:15:55 +0100

    - Fix npm-install needing sudo - Fix setting file perms on index.html files in data/ that would cause git to think they were modified (filemode) - Fix a few /deskpro-cache permission errors after running cmd-line install as root - Install from master rather than develop, since upgrade scripts may need to run after that

--- app/dev/deskpro-vagrant/vm_local/init/install_deskpro.sh
+++ app/dev/deskpro-vagrant/vm_local/init/install_deskpro.sh
@@ -7,15 +7,12 @@ cp /vm_local/init/conf/deskpro_config.testing.php config.testing.php
 
 php cmd.php dp:install --verbose
 
-# New adminui currently only branch that uses grunt
-git checkout feature/adminui
-cd /deskpro/www/web/app
-npm install --save-dev
-
-# Then after that, pop them back into the default develop branch
-cd /deskpro/www
-git checkout develop
-
 chown --silent -R vagrant:vagrant /deskpro/www
+chown --silent -R vagrant:vagrant /deskpro-cache
 chmod --silent -R 0777 /deskpro/www/data
 chmod --silent -R 0777 /deskpro/www/app/sys/cache
+chmod --silent -R 0777 /deskpro-cache
+
+# Correct filemode on index.html files or else git will complain
+find /deskpro/www/data/ -name 'index.html' -exec chmod 0644 {} +
+chmod 0644 /deskpro/www/data/.htaccess
