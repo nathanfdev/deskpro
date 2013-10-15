375d4e20b832577d0801a9fb7aaeddcb210e04a6
Christopher Nadeau <chris@nadeau.ws>
2013-10-15 16:18:01 +0100

    Ignore travis.yml file in checksum checker

--- app/src/Application/DeskPRO/Distribution/ChecksumChecker.php
+++ app/src/Application/DeskPRO/Distribution/ChecksumChecker.php
@@ -57,6 +57,7 @@ class ChecksumChecker extends \Orb\File\ChecksumChecker
 			->exclude('sys/cache/dev')
 			->exclude('.settings')
 			->exclude('.idea')
+			->exclude('.travis.yml')
 			->exclude('data')
 			->exclude('.feedback');
 	}
