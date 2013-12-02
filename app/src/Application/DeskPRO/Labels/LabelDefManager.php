3420a2acdf316c2886b88f6214b1684cdf74e4c0
Egor Smirnov <egorsmir@gmail.com>
2013-12-02 17:12:53 +0400

    Get rid of LabelChat entity / entity repository and switched it with LabelChatConversation

--- app/src/Application/DeskPRO/Labels/LabelDefManager.php
+++ app/src/Application/DeskPRO/Labels/LabelDefManager.php
@@ -54,7 +54,7 @@ class LabelDefManager
 		'deals'                => array('table' => 'labels_blobs',              'entity' => 'DeskPRO:LabelDeal'),
 		'downloads'            => array('table' => 'labels_downloads',          'entity' => 'DeskPRO:LabelDownload'),
 		'feedback'             => array('table' => 'labels_feedback',           'entity' => 'DeskPRO:LabelFeedback'),
-		'chat'                 => array('table' => 'labels_chat_conversations', 'entity' => 'DeskPRO:LabelChat'),
+		'chat'                 => array('table' => 'labels_chat_conversations', 'entity' => 'DeskPRO:LabelChatConversation'),
 		'news'                 => array('table' => 'labels_news',               'entity' => 'DeskPRO:LabelNews'),
 		'organizations'        => array('table' => 'labels_organizations',      'entity' => 'DeskPRO:LabelOrganization'),
 		'people'               => array('table' => 'labels_people',             'entity' => 'DeskPRO:LabelPeople'),
