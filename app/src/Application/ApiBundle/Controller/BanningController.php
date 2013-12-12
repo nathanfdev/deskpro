0472108e4c07587ffd83257a140c723202b92ac0
Egor Smirnov <egorsmir@gmail.com>
2013-12-12 16:30:58 +0400

    CRM -> Banning -> IP Bans -> Update - server-side part

--- app/src/Application/ApiBundle/Controller/BanningController.php
+++ app/src/Application/ApiBundle/Controller/BanningController.php
@@ -35,6 +35,9 @@
 namespace Application\ApiBundle\Controller;
 
 use Application\DeskPRO\Exception\ValidationException;
+use Application\DeskPRO\Banning\IpBanEdit;
+use Application\DeskPRO\Banning\Form\Type\IpBanType;
+use Application\DeskPRO\Banning\Form\Type\IpBanPropsType;
 
 use Orb\Util\Arrays;
 
@@ -60,6 +63,80 @@ class BanningController extends AbstractController
 					 'ip_bans'    => $ip_bans->getAll(),
 					 'email_bans' => array(),
 				 )
+			)
+		);
+	}
+
+	###################################################################################################################
+	# get IP
+	####################################################################################################################
+
+	public function getIpAction($id)
+	{
+		/**
+		 * @var \Application\DeskPRO\Banning\IpBans $ip_bans
+		 */
+
+		$ip_bans = $this->container->getSystemService('ip_bans');
+		$ip_ban  = $ip_bans->getById($id);
+
+		if (!$ip_ban) {
+
+			throw $this->createNotFoundException();
+		}
+
+		return $this->createApiResponse(
+			array(
+				 'ip_ban' => $this->getApiData($ip_ban)
+			)
+		);
+	}
+
+	####################################################################################################################
+	# save IP
+	####################################################################################################################
+
+	public function saveIpAction($id)
+	{
+		/**
+		 * @var \Application\DeskPRO\Banning\IpBans $ip_bans
+		 */
+
+		$ip_bans = $this->container->getSystemService('ip_bans');
+
+		if ($id) {
+
+			$ip_ban = $ip_bans->getById($id);
+
+			if (!$ip_ban) {
+
+				throw $this->createNotFoundException();
+			}
+		} else {
+
+			$ip_ban = $ip_bans->createNew();
+		}
+
+		$postData = $this->in->getAll('post');
+
+		$ip_ban_edit = new IpBanEdit($ip_ban);
+
+		$form = $this->createForm(new IpBanType(), $ip_ban_edit, array('cascade_validation' => true));
+		$form->submit($this->deleteExtraDataFromRequest($form, $postData, 'ip_ban'), true);
+
+		if ($form->isValid()) {
+
+			$ip_ban_edit->save($this->em);
+
+		} else {
+
+			throw ValidationException::create($this->getFormValidationErrorsString($form));
+		}
+
+		return $this->createApiResponse(
+			array(
+				 'success'   => true,
+				 'banned_ip' => $ip_ban->banned_ip
 			)
 		);
 	}
