59b556d61d3e40496c1326520da1a918f9eba8ac
Egor Smirnov <egorsmir@gmail.com>
2013-12-20 08:50:08 +0400

    CRM -> Usergroups -> Update Permissions - client-side part

--- web/app/Admin/UserGroups/UserGroupEditFormMapper.coffee
+++ web/app/Admin/UserGroups/UserGroupEditFormMapper.coffee
@@ -19,7 +19,11 @@ define [
 			form.note = model.user_group.note
 			form.is_enabled = model.user_group.is_enabled
 
-			form.permissions = []
+			form.permissions = {}
+
+			for permission in model.user_group.permissions
+				key = permission.name.replace('.', '_')
+				if Util.isBlank(permission.value) then form.permissions[key] = false else form.permissions[key] = true
 
 			return form
 
@@ -44,7 +48,18 @@ define [
 			postData.id = formModel.id
 			postData.title = formModel.title
 			postData.note = formModel.note
+			postData.is_enabled = formModel.is_enabled
 
 			postData.permissions = []
+
+			console.log formModel.permissions
+
+			for own permission, value of formModel.permissions
+				postData.permissions.push({
+					usergroup_id: formModel.id,
+					name: permission.replace('_', '.'),
+					value: (if value then 1 else 0),
+					person_id: null,
+				})
 
 			return postData
--- web/app/Admin/build/js/UserGroups/UserGroupEditFormMapper.js
+++ web/app/Admin/build/js/UserGroups/UserGroupEditFormMapper.js
@@ -1,4 +1,6 @@
 (function() {
+  var __hasProp = {}.hasOwnProperty;
+
   define(['DeskPRO/Util/Util'], function(Util) {
     var UserGroupEditFormMapper;
     return UserGroupEditFormMapper = (function() {
@@ -11,13 +13,23 @@
 
 
       UserGroupEditFormMapper.prototype.getFormFromModel = function(model) {
-        var form;
+        var form, key, permission, _i, _len, _ref;
         form = {};
         form.id = model.user_group.id;
         form.title = model.user_group.title;
         form.note = model.user_group.note;
         form.is_enabled = model.user_group.is_enabled;
-        form.permissions = [];
+        form.permissions = {};
+        _ref = model.user_group.permissions;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          permission = _ref[_i];
+          key = permission.name.replace('.', '_');
+          if (Util.isBlank(permission.value)) {
+            form.permissions[key] = false;
+          } else {
+            form.permissions[key] = true;
+          }
+        }
         return form;
       };
 
@@ -38,12 +50,25 @@
 
 
       UserGroupEditFormMapper.prototype.getPostDataFromForm = function(formModel) {
-        var postData;
+        var permission, postData, value, _ref;
         postData = {};
         postData.id = formModel.id;
         postData.title = formModel.title;
         postData.note = formModel.note;
+        postData.is_enabled = formModel.is_enabled;
         postData.permissions = [];
+        console.log(formModel.permissions);
+        _ref = formModel.permissions;
+        for (permission in _ref) {
+          if (!__hasProp.call(_ref, permission)) continue;
+          value = _ref[permission];
+          postData.permissions.push({
+            usergroup_id: formModel.id,
+            name: permission.replace('_', '.'),
+            value: (value ? 1 : 0),
+            person_id: null
+          });
+        }
         return postData;
       };
 
