2a7850bba1651d6d8289dac44916cea322c17b97
Christopher Nadeau <chris@nadeau.ws>
2014-01-08 16:18:30 +0000

    [FIX] Missing escalation phrase for 'total user waiting time' type

--- app/languages/default/admin/tickets.php
+++ app/languages/default/admin/tickets.php
@@ -174,5 +174,6 @@
 	'admin.tickets.x_since_time_open'                       => '&gt; {{time}} since ticket was opened',
 	'admin.tickets.x_since_time_resolved'                   => '&gt; {{time}} since status was set to resolved',
 	'admin.tickets.x_since_time_user_waiting'               => 'User waiting &gt; {{time}}',
+	'admin.tickets.x_since_time_total_user_waiting'         => 'Total user waiting &gt; {{time}}',
 	'admin.tickets.x_since_total_user_waiting'              => 'User has been waiting &gt; {{time}}',
 );
