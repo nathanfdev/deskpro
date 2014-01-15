471ee4ed8993652f407c8e66fc65cd4b32537d8c
Christopher Nadeau <chris@nadeau.ws>
2014-01-15 12:29:32 +0000

    [FIX] Error generating download URLs for downlaods with special characters in the title

--- app/src/Application/DeskPRO/Entity/Download.php
+++ app/src/Application/DeskPRO/Entity/Download.php
@@ -176,6 +176,19 @@ class Download extends ContentAbstract
 
 
 	/**
+	 * @return string
+	 */
+	public function getFilenameSafe()
+	{
+		$filename_safe = Strings::utf8_accents_to_ascii($this->getFileName());
+		$filename_safe = preg_replace('#[^a-zA-Z0-9\-_\.]#', '-', $filename_safe);
+		$filename_safe = preg_replace('#\-{2,}#', '-', $filename_safe);
+
+		return $filename_safe;
+	}
+
+
+	/**
 	 * @return int|string
 	 */
 	public function getFileSize()
