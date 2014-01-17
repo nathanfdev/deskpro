c095914e253c0909cd843a829e6f1f28bf5bf020
Christopher Nadeau <chris@nadeau.ws>
2014-01-17 09:05:46 +0000

    - Fix cases where $abc.on() was used with a selector - Also improve case of switching back to a mouse from using touch, mouse clicks didn't register

--- web/javascripts/DeskPRO/Agent/TouchClicker.js
+++ web/javascripts/DeskPRO/Agent/TouchClicker.js
@@ -5,20 +5,45 @@ if (window.jQuery && ('ontouchstart' in window || 'msmaxtouchpoints' in window.n
 
 	var replaceEventName = function (event) {
 		if (event.slice(0, 5) == 'click') {
-			return event.replace('click', 'touchend');
+			// We need to attach events to both click and touch
+			// Because the user might have both a touch *and* mouse
+			return event.replace('click', 'click touchend');
 		}
 		return event;
 	}
 
 	// Change event type and re-apply .on() method
 	jQuery.fn.on = function() {
-		var oldFn = arguments[1];
+		var fnKey = 1, oldFn, hasClicked = false;
+
+		// on(eventName, fn)
+		// or on(eventName, selector, fn)
+		// So we need to detect those
+		if (typeof arguments[2] == 'function') {
+			fnKey = 2;
+		}
+		oldFn = arguments[fnKey];
 		arguments[0] = replaceEventName(arguments[0]);
-		arguments[1] = function() {
+
+		// We need to override handling of touch events a bit
+		// - We want to cancel clicking when the user has scrolled by dragging
+		// so the original 'down' event doesnt fire after touch has ended
+		// - We want to cancel double-clicking that can sometimes happen when click and touch
+		// both fire
+		arguments[fnKey] = function() {
 			if (window.DP_SCROLL_CANCEL_TOUCH) {
 				window.DP_SCROLL_CANCEL_TOUCH = false;
+				hasClicked = false;
 				return;
 			}
+			if (hasClicked) {
+				hasClicked = false;
+				return;
+			}
+			hasClicked = true;
+			window.setTimeout(function() {
+				hasClicked = false;
+			}, 80);
 			return oldFn.apply(this, arguments);
 		};
 
