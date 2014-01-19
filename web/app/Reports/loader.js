2e511fd760eb846001f61ed7159b9185247ab0d6
Egor Smirnov <egorsmir@gmail.com>
2014-01-19 22:12:58 +0400

    Reports loader.js - a lot of changes

--- web/app/Reports/loader.js
+++ web/app/Reports/loader.js
@@ -2,36 +2,61 @@ requirejs.config({
 	baseUrl: DP_ASSET_URL,
 	urlArgs: "bust=" + (new Date()).getTime(),
     paths: {
-		angular:                DP_ASSET_URL+'/app/bower_components/angular/angular',
-		angularBootstrap:       DP_ASSET_URL+'/app/bower_components/angular-bootstrap/ui-bootstrap-tpls.min',
-		angularSanitize:        DP_ASSET_URL+'/app/bower_components/angular-sanitize/angular-sanitize',
-		angularSelect2:         DP_ASSET_URL+'/app/bower_components/angular-ui-select2/src/select2',
-		angularUiRouter:        DP_ASSET_URL+'/app/bower_components/angular-ui-router/release/angular-ui-router',
-		angularUiSortable:      DP_ASSET_URL+'/app/bower_components/angular-ui-sortable/src/sortable',
+		angular:                         DP_ASSET_URL+'/app/bower_components/angular/angular',
+		angularAnimate:                  DP_ASSET_URL+'/app/bower_components/angular-animate/angular-animate.min',
+		angularBootstrap:                DP_ASSET_URL+'/app/bower_components/angular-bootstrap/ui-bootstrap-tpls.min',
+		angularSanitize:                 DP_ASSET_URL+'/app/bower_components/angular-sanitize/angular-sanitize',
+		angularSelect2:                  DP_ASSET_URL+'/app/bower_components/angular-ui-select2/src/select2',
+		angularUiRouter:                 DP_ASSET_URL+'/app/bower_components/angular-ui-router/release/angular-ui-router',
+		angularUiSortable:               DP_ASSET_URL+'/app/bower_components/angular-ui-sortable/src/sortable',
+		angularMoment:                   DP_ASSET_URL+'/app/bower_components/angular-moment/angular-moment.min',
+		angularFileUpload:               DP_ASSET_URL+'/app/bower_components/blueimp-file-upload/js/jquery.fileupload-angular',
+		angularSlider:                   DP_ASSET_URL+'/app/bower_components/angular-slider/angular-slider.min',
 
-		moment:                 DP_ASSET_URL+'/app/bower_components/momentjs/min/moment-with-langs.min',
-		stacktrace:             DP_ASSET_URL+'/app/bower_components/stacktrace/stacktrace',
+		moment:                          DP_ASSET_URL+'/app/bower_components/momentjs/min/moment-with-langs.min',
+		stacktrace:                      DP_ASSET_URL+'/app/bower_components/stacktrace/stacktrace',
 
-		jquery:                 DP_ASSET_URL+'/app/bower_components/jquery/jquery',
-		jqueryUi:               DP_ASSET_URL+'/app/bower_components/jquery-ui/ui/jquery-ui',
-		underscore:             DP_ASSET_URL+'/app/bower_components/underscore/underscore-min',
+		jquery:                          DP_ASSET_URL+'/app/bower_components/jquery/jquery',
+		jqueryUi:                        DP_ASSET_URL+'/app/bower_components/jquery-ui/ui/jquery-ui',
+		underscore:                      DP_ASSET_URL+'/app/bower_components/underscore/underscore-min',
 
-		bootstrapModal:         DP_ASSET_URL+'/app/bower_components/bootstrap/js/modal',
-		bootstrapTooltip:       DP_ASSET_URL+'/app/bower_components/bootstrap/js/tooltip',
-		select2:                DP_ASSET_URL+'/app/bower_components/select2/select2.min',
-		toastr:                 DP_ASSET_URL+'/app/bower_components/toastr/toastr.min',
+		bootstrapModal:                  DP_ASSET_URL+'/app/bower_components/bootstrap/js/modal',
+		bootstrapTooltip:                DP_ASSET_URL+'/app/bower_components/bootstrap/js/tooltip',
+		select2:                         DP_ASSET_URL+'/app/bower_components/select2/select2.min',
+		toastr:                          DP_ASSET_URL+'/app/bower_components/toastr/toastr.min',
 
-		Admin:                  DP_ASSET_URL+'/app/Admin/build/js',
-		Reports:                DP_ASSET_URL+'/app/Reports/build/js',
-		ReportsRouting:         DP_ASSET_URL+'/app/Reports/Resources/config/routing'
+		DeskPRO:                         DP_ASSET_URL+'/app/DeskPRO/build/js',
+		Admin:                           DP_ASSET_URL+'/app/Admin/build/js',
+		Reports:                         DP_ASSET_URL+'/app/Reports/build/js',
+		ReportsRouting:                  DP_ASSET_URL+'/app/Reports/Resources/config/routing',
+
+		'jquery.ui.widget':              DP_ASSET_URL+'/app/bower_components/blueimp-file-upload/js/vendor/jquery.ui.widget',
+
+		'jquery.fileupload':             DP_ASSET_URL+'/app/bower_components/blueimp-file-upload/js/jquery.fileupload',
+		'jquery.fileupload-process':     DP_ASSET_URL+'/app/bower_components/blueimp-file-upload/js/jquery.fileupload-process',
+		'jquery.fileupload-image':       DP_ASSET_URL+'/app/bower_components/blueimp-file-upload/js/jquery.fileupload-image',
+		'jquery.fileupload-audio':       DP_ASSET_URL+'/app/bower_components/blueimp-file-upload/js/jquery.fileupload-audio',
+		'jquery.fileupload-video':       DP_ASSET_URL+'/app/bower_components/blueimp-file-upload/js/jquery.fileupload-video',
+		'jquery.fileupload-validate':    DP_ASSET_URL+'/app/bower_components/blueimp-file-upload/js/jquery.fileupload-validate',
+
+		'load-image':                    DP_ASSET_URL+'/app/bower_components/blueimp-load-image/js/load-image',
+		'load-image-meta':               DP_ASSET_URL+'/app/bower_components/blueimp-load-image/js/load-image-meta',
+		'load-image-ios':                DP_ASSET_URL+'/app/bower_components/blueimp-load-image/js/load-image-ios',
+		'load-image-exif':               DP_ASSET_URL+'/app/bower_components/blueimp-load-image/js/load-image-exif',
+
+		'canvas-to-blob':                DP_ASSET_URL+'/app/bower_components/blueimp-canvas-to-blob/js/canvas-to-blob.min'
 	},
 	shim: {
 		'angular':              {'exports' : 'angular'},
+		'angularAnimate':       ['angular'],
 		'angularBootstrap':     ['angular'],
 		'angularSanitize':      ['angular'],
 		'angularSelect2':       ['angular'],
 		'angularUiRouter':      ['angular'],
 		'angularUiSortable':    ['angular'],
+		'angularMoment':        ['angular'],
+		'angularFileUpload':    ['jquery'],
+		angularSlider:          ['angular'],
 
 		'jqueryUi':             ['jquery'],
 
@@ -51,19 +76,30 @@ window.name = "NG_DEFER_BOOTSTRAP!";
 
 requirejs([
 	'angular',
+	'angularAnimate',
+	'angularSanitize',
 	'angularBootstrap',
 	'angularSelect2',
 	'angularUiRouter',
 	'angularUiSortable',
+	'angularMoment',
+	'angularFileUpload',
+	'angularSlider',
+
+	'moment',
 
 	'jquery',
 	'jqueryUi',
 	'underscore',
+	'stacktrace',
 
 	'bootstrapTooltip',
 
 	'select2',
 	'toastr',
+
+	'DeskPRO/OptionBuilder/Module',
+	'DeskPRO/CategoryBuilder/Module',
 
 	'Reports/App/App'
 ], function(angular) {
