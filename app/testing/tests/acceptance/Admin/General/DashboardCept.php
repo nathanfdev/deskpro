f9d8e02ed38d21607ec5a96e02062185afdfb853
Christopher Nadeau <chris@nadeau.ws>
2014-01-30 12:05:17 +0000

    Fix test that was failing the build

--- app/testing/tests/acceptance/Admin/General/DashboardCept.php
+++ app/testing/tests/acceptance/Admin/General/DashboardCept.php
@@ -5,4 +5,4 @@ $I = new WebGuy($scenario);
 $I->wantToTest("See the admin dashboard");
 $I->enableDatabaseSet('FreshDb');
 $I->openAdminInterface('admin@example.com');
-$I->see('DeskPRO News', 'h3');
+$I->see('DeskPRO Updates', 'h3');
