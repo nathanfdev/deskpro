a38a02c31e5c4940b8ac05c073ef73124e878d48
Egor Smirnov <egorsmir@gmail.com>
2014-01-30 23:57:49 +0400

    Reports -> Builder -> help block directive is now working properly

--- web/app/Reports/build/js/App/SetupDirectives.js
+++ web/app/Reports/build/js/App/SetupDirectives.js
@@ -1,8 +1,9 @@
 (function() {
-  define(['DeskPRO/Directive/DpTimeWithUnit', 'DeskPRO/Directive/DpStateMark'], function(DeskPRO_Directive_DpTimeWithUnit, DeskPRO_Directive_DpStateMark) {
+  define(['DeskPRO/Directive/DpTimeWithUnit', 'DeskPRO/Directive/DpStateMark', 'Admin/Main/Directive/DpHelpPage'], function(DeskPRO_Directive_DpTimeWithUnit, DeskPRO_Directive_DpStateMark, Admin_Main_Directive_DpHelpPage) {
     return function(Module) {
       Module.directive('dpTimeWithUnit', DeskPRO_Directive_DpTimeWithUnit);
-      return Module.directive('dpStateMark', DeskPRO_Directive_DpStateMark);
+      Module.directive('dpStateMark', DeskPRO_Directive_DpStateMark);
+      return Module.directive('dpHelpPage', Admin_Main_Directive_DpHelpPage);
     };
   });
 
