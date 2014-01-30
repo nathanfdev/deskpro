fe78ff683fd266ce5f830e086c7a7f889b62d1a1
Egor Smirnov <egorsmir@gmail.com>
2014-01-30 06:46:31 +0400

    Reports Builder - changing name / id of route

--- web/app/Reports/Resources/config/routing.js
+++ web/app/Reports/Resources/config/routing.js
@@ -18,8 +18,8 @@ define(function() {
 	//##################################################################################################################
 
 	routes.push({
-		id: 'report_builder',
-		url: '/report_builder',
+		id: 'builder',
+		url: '/builder',
 		templateName: 'Builder/list.html',
 		controller: 'Reports_Builder_Ctrl_List'
 	});
