146050c8765490c0813f06e53467b897dec5fc73
Egor Smirnov <egorsmir@gmail.com>
2014-02-02 18:52:03 +0400

    Reports -> Builder -> adding of dpSubNav directive

--- web/app/Reports/App/SetupDirectives.coffee
+++ web/app/Reports/App/SetupDirectives.coffee
@@ -2,12 +2,15 @@ define [
 	'DeskPRO/Directive/DpTimeWithUnit',
 	'DeskPRO/Directive/DpStateMark',
 	'DeskPRO/Directive/DpHelpPage',
+	'Admin/Main/Directive/DpNavSubnav',
 ], (
 	DeskPRO_Directive_DpTimeWithUnit,
 	DeskPRO_Directive_DpStateMark,
 	DeskPRO_Directive_DpHelpPage,
+	Admin_Main_Directive_DpNavSubnav,
 ) ->
 	return (Module) ->
 		Module.directive('dpTimeWithUnit',                 DeskPRO_Directive_DpTimeWithUnit)
 		Module.directive('dpStateMark',                    DeskPRO_Directive_DpStateMark)
 		Module.directive('dpHelpPage',                     DeskPRO_Directive_DpHelpPage)
+		Module.directive('dpNavSubnav',                    Admin_Main_Directive_DpNavSubnav)
--- web/app/Reports/build/js/App/SetupDirectives.js
+++ web/app/Reports/build/js/App/SetupDirectives.js
@@ -1,9 +1,10 @@
 (function() {
-  define(['DeskPRO/Directive/DpTimeWithUnit', 'DeskPRO/Directive/DpStateMark', 'DeskPRO/Directive/DpHelpPage'], function(DeskPRO_Directive_DpTimeWithUnit, DeskPRO_Directive_DpStateMark, DeskPRO_Directive_DpHelpPage) {
+  define(['DeskPRO/Directive/DpTimeWithUnit', 'DeskPRO/Directive/DpStateMark', 'DeskPRO/Directive/DpHelpPage', 'Admin/Main/Directive/DpNavSubnav'], function(DeskPRO_Directive_DpTimeWithUnit, DeskPRO_Directive_DpStateMark, DeskPRO_Directive_DpHelpPage, Admin_Main_Directive_DpNavSubnav) {
     return function(Module) {
       Module.directive('dpTimeWithUnit', DeskPRO_Directive_DpTimeWithUnit);
       Module.directive('dpStateMark', DeskPRO_Directive_DpStateMark);
-      return Module.directive('dpHelpPage', DeskPRO_Directive_DpHelpPage);
+      Module.directive('dpHelpPage', DeskPRO_Directive_DpHelpPage);
+      return Module.directive('dpNavSubnav', Admin_Main_Directive_DpNavSubnav);
     };
   });
 
