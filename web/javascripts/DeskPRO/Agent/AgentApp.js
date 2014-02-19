da230646057c099659c97d9aee51d876f08d175c
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-02-19 13:44:04 +0000

    Remove some dead code

--- web/javascripts/DeskPRO/Agent/AgentApp.js
+++ web/javascripts/DeskPRO/Agent/AgentApp.js
@@ -3,35 +3,6 @@ Orb.createNamespace('DeskPRO.Agent');
 DeskPRO.Agent.AgentAppFactory = function() {
 	var AgentApp = angular.module('AgentApp', ['ngAnimate', 'pasvaz.bindonce']);
 
-	//-------------------------------------------------------------------------
-	// dpAppAssetInterceptor
-	//-------------------------------------------------------------------------
-
-	// The asset interceptor re-writes the path to app assets (mainly for templates)
-	// For example, in source, apps would reference a template file like:
-	// <div ng-include="com.deskpro.apps.test/html/some-template.html"></div>
-	// But that file obviously doesn't exist. We use the interceptor to rewrite it
-	// to the real file.php/xxx/some-template.html file.
-
-	/*
-	AgentApp.factory('dpAppAssetInterceptor', [function() {
-		return  {
-			request: function(config) {
-				var assetPath = window.AppPlatform.getAssetPath(config.url);
-				if (assetPath) {
-					console.log("[dpAppAssetInterceptor] %s -> %s", config.url, assetPath);
-					config.url = assetPath;
-					config.dpIsAppAsset = true;
-				} else {
-					config.url = config.url.replace(/DP_URL\//g, window.BASE_URL.replace(/\/+$/, '')+'/')
-				}
-
-				return config;
-			}
-		};
-	}]);
-	*/
-
 	AgentApp.filter('formatTimestampAgo', function() {
 		return function(ts) {
 			var m;
@@ -189,8 +160,8 @@ DeskPRO.Agent.AgentAppFactory = function() {
 		}
 	}]);
 
-	AgentApp.config(['$httpProvider', function($httpProvider) {
-		//$httpProvider.interceptors.push('dpAppAssetInterceptor');
+	AgentApp.config(['$locationProvider', function($locationProvider) {
+		$locationProvider.html5Mode(true).hashPrefix('');
 	}]);
 
 	return AgentApp;
