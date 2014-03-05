df8425a09ed5ad031bd288939b94ce8c9117dafa
Abhinav Kumar <abhinavkumar940@gmail.com>
2014-03-05 16:55:16 +0530

    Debugging code removal

--- web/javascripts/DeskPRO/Agent/PageFragment/Page/PersonHelper/UploadVcard.js
+++ web/javascripts/DeskPRO/Agent/PageFragment/Page/PersonHelper/UploadVcard.js
@@ -15,8 +15,6 @@ DeskPRO.Agent.PageFragment.Page.PersonHelper.UploadVcard = new Orb.Class({
 		this.setOptions(options);
 		this.page = page;
 
-		this.page.getEl('change_user_picture').on('click', this.open.bind(this));
-
 		this.page.addEvent('destroy', this.destroy, this);
 	},
 
@@ -135,7 +133,7 @@ DeskPRO.Agent.PageFragment.Page.PersonHelper.UploadVcard = new Orb.Class({
                         }
 		});
 
-		//this.close();
+		this.close();
 	},
 
 	open: function() {
