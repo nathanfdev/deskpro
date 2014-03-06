9c2438e077e3c7f10ef31c0005dacfa0a68f6b54
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-03-06 18:39:12 +0000

    Add LDAP app

---
+++ app/apps/deskpro_us_ldap/native/native_config.php
@@ -0,0 +1,8 @@
+<?php return array(
+	'install' => array(
+		'handler' => 'deskpro_us_ldap\\InstallerHandler'
+	),
+	'api' => array(
+		'package_request_handler' => 'deskpro_us_ldap\\RequestHandler\\PackageRequestHandler'
+	)
+);
