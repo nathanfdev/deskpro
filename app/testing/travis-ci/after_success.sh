557b2772e7d8c287b0bcc344e91f901a0d23a460
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-03-11 13:03:29 +0000

    Fix a couple of paths

--- app/testing/travis-ci/after_success.sh
+++ app/testing/travis-ci/after_success.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/../
 
-./bin/coveralls --config ../coveralls.yml --verbose --exclude-no-stmt
+./bin/coveralls --config=coveralls.yml --verbose --exclude-no-stmt
