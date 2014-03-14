4f585464e301ba612ca016d37a1249d3b2f03879
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-03-14 18:52:00 +0000

    Add trigger action and test for email account

---
+++ app/testing/tests/unit/DeskPRO/Tickets/Triggers/Terms/CheckEmailAccountTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace DpUnitTests\DeskPRO\Tickets\Triggers\Terms;
+
+require_once 'AbstractTicketEntityCheckTest.php';
+
+class CheckEmailAccountTest extends AbstractTicketEntityCheckTest
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function getCheckClass()
+	{
+		return 'Application\\DeskPRO\\Tickets\\Triggers\\Terms\\CheckEmailAccount';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function getCheckClassOptionKey()
+	{
+		return 'email_account_ids';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getEntityClass()
+	{
+		return 'Application\\DeskPRO\\Entity\\EmailAccount';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getTicketPropertyName()
+	{
+		return 'email_account';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function createEntityObject($id)
+	{
+		$ent_class = $this->getEntityClass();
+
+		$object = new $ent_class('tickets');
+		$object->id = $id;
+
+		return $object;
+	}
+}
