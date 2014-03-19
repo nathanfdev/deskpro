cace5ef839a071788bab8c919b2215962de8902f
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-03-19 12:48:36 +0000

    Update agent api to send new welcome guide

--- app/src/Application/ApiBundle/Controller/AgentsController.php
+++ app/src/Application/ApiBundle/Controller/AgentsController.php
@@ -270,6 +270,8 @@ class AgentsController extends AbstractController implements ProtectedController
 			$message = $this->container->getMailer()->createMessage();
 			$message->setToPerson($agent);
 			$message->setTemplate('DeskPRO:emails_agent:agent-welcome.html.twig', array('agent' => $agent));
+			$attach = \Swift_Attachment::fromPath(DP_ROOT.'/src/Application/AgentBundle/Resources/assets/agent-quickstart/en_US.pdf', 'application/pdf');
+			$attach->setFilename('Getting Started with DeskPRO.pdf');
 			$this->container->getMailer()->send($message);
 		}
 
