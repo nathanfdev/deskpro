4fb63f6a43531d22a09c9a3d66959a8b622d3f23
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-04-03 16:31:05 +0100

    [FIX] Newlines entered into multi-line (textarea) custom fields did not render as newlines

--- app/src/Application/DeskPRO/CustomFields/Handler/Textarea.php
+++ app/src/Application/DeskPRO/CustomFields/Handler/Textarea.php
@@ -36,12 +36,22 @@ namespace Application\DeskPRO\CustomFields\Handler;
 
 use Application\DeskPRO\Entity;
 use Application\DeskPRO\App;
+use Orb\Util\Util;
 
 /**
  * Handles the text field
  */
 class Textarea extends Text
 {
+	public function getRenderTemplateVars($context = 'html')
+	{
+		if ($context == 'html') {
+			return array('nl2br' => true);
+		}
+
+		return array();
+	}
+
 	public function getFormField($data = null)
 	{
 		$setData = null;
