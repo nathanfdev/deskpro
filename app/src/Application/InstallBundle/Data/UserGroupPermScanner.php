fd1961751075f09b65b8e32ee5b764026b0e656c
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-04-09 13:23:38 +0100

    Fix installer not initialising user perms

--- app/src/Application/InstallBundle/Data/UserGroupPermScanner.php
+++ app/src/Application/InstallBundle/Data/UserGroupPermScanner.php
@@ -58,9 +58,7 @@ class UserGroupPermScanner
 		foreach (GroupsDbLoader::$prefix_map as $real_name => $coll_name) {
 			$obj = $perms->$coll_name;
 			foreach ($obj->getNames() as $prop) {
-				if ($obj->$prop) {
-					$set_perms[] = $real_name . '.' . $prop;
-				}
+				$set_perms[] = $real_name . '.' . $prop;
 			}
 		}
 
