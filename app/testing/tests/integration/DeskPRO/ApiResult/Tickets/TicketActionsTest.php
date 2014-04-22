420f300d0d14202b5578338c2df22d91cafcabe3
Abhinav Kumar <abhinavkumar940@gmail.com>
2014-04-22 14:27:02 +0530

    API Test Case - TicketActions - Claim

---
+++ app/testing/tests/integration/DeskPRO/ApiResult/Tickets/TicketActionsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DpUnitTests\DeskPRO\ApiResult\Tickets;
+
+use DpUnitTests\DeskPRO\ApiResult\AbstractApiResultTest;
+
+require_once __DIR__ . '/../AbstractApiResultTest.php';
+
+class TicketActionsTest extends AbstractApiResultTest
+{
+	public function testCanClaimTicket()
+	{
+		$testTicketId = 1;
+		
+		$result = $this->getApi()->tickets->findById($testTicketId);
+		
+		$data = $result->getData();
+		
+		$this->assertEmpty($data['ticket']['agent']);
+		
+		$result = $this->getApi()->tickets->assignToMe($testTicketId);
+		
+		$this->assertEquals('200', $result->getResponseCode());
+		
+		$result = $this->getApi()->tickets->findById($testTicketId);
+		
+		$data = $result->getData();
+		
+		$this->assertNotEmpty($data['ticket']['agent']);
+		
+		$builder = $this->getApi()->tickets->createBuilder();
+		
+		$builder->setId($testTicketId)->assignToAgent(0);
+		
+		$this->getApi()->tickets->save($builder);
+	}
+}
