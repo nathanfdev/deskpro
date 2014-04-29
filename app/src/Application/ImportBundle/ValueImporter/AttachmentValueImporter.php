6da669318f4babff7d2e5c9115286bbba2719782
Abhinav Kumar <abhinavkumar940@gmail.com>
2014-04-29 12:58:08 +0530

    AttachmentValueImporter

--- app/src/Application/ImportBundle/ValueImporter/AttachmentValueImporter.php
+++ app/src/Application/ImportBundle/ValueImporter/AttachmentValueImporter.php
@@ -29,42 +29,56 @@
  * @package Importer
  */
 
-namespace Application\ImportBundle\Value;
+namespace Application\ImportBundle\ValueImporter;
 
-class AttachmentValue
+use Application\ImportBundle\Value\AttachmentValue;
+use Application\ImportBundle\Exception\BadDataException;
+use Application\ImportBundle\Exception\DuplicateValueException;
+use Orb\Validator\StringEmail;
+
+class AttachmentValueImporter extends AbstractValueImporter
 {
 	/**
-	 * @var int
+	 * 
+	 * @param \Application\ImportBundle\Value\FeedbackValue $aval
+	 * @return boolean
+	 * @throws \InvalidArgumentException
+	 * @throws BadDataException
+	 * @throws DuplicateValueException
 	 */
-	public $oid;
+	public function importValue($aval)
+	{
+		if (!($aval instanceof AttachmentValue)) {
+			throw new \InvalidArgumentException("This importer can only import Attachments");
+		}
+		
+		$log_id = "Attachment :: " . $aval->oid . " ";
+		
+		if (!$aval->blob_data &&
+			!$aval->blob_path &&
+			!$aval->blob_url) {
+			throw new BadDataException(sprintf("[%s] Invalid Attachment: The attachement must have one of 'blob_data', 'blob_path' or 'blob_url'", $log_id));
+		}
+		
+		if ($aval->blob_data) {
+			$blob_data = base64_decode($aval->blob_data);
+		} elseif ($aval->blob_path) {
+			if (!is_readable($aval->blob_path)) {
+				throw new BadDataException(sprintf('[%s] Invalid blob path %s', $log_id, $aval->blob_path));
+			}
+			
+			$blob_data = file_get_contents($aval->blob_path);
+		} elseif ($aval->blob_url) {
+			if (!is_readable($aval->blob_url)) {
+				throw new BadDataException(sprintf('[%s] Invalid blob url %s', $log_id, $aval->blob_url));
+			}
+			$blob_data = file_get_contents($aval->blob_url);
+		}
+			
+		$blobStarage = $this->getContainer()->getBlobStorage();
 
-	/**
-	 * @var string
-	 */
-	public $blob_data;
-
-	/**
-	 * @var string
-	 */
-	public $blob_url;
-
-	/**
-	 * @var string
-	 */
-	public $blob_path;
-
-	/**
-	 * @var string
-	 */
-	public $file_name;
-
-	/**
-	 * @var string
-	 */
-	public $content_type;
-
-	/**
-	 * @var bool
-	 */
-	public $is_inline = false;
+		$blob = $blobStarage->createBlobRecordFromString($blob_data, $aval->file_name, $aval->content_type);
+		
+		return $blob;
+	}
 }
