2339fef8d1ed750a62c4658670e2fcca5ded745b
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-05-13 13:02:29 +0100

    Fix creating/editing filters from agent interface

--- app/src/Application/DeskPRO/CustomFields/FieldDisplayArray.php
+++ app/src/Application/DeskPRO/CustomFields/FieldDisplayArray.php
@@ -155,7 +155,7 @@ class FieldDisplayArray implements \ArrayAccess
 
 					$field_group = $this->field_group;
 					if (!$field_group) {
-						$field_group = App::get('form.factory')->createNamedBuilder('form', 'custom_fields');
+						$field_group = App::get('form.factory')->createNamedBuilder('custom_fields', 'form');
 					}
 
 					$f = $handler->getFormField($this->data['value']);
