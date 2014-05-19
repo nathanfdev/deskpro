4b2efeba3818542abb38de69b70cdc6923586a80
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-05-19 12:55:55 +0100

    Correct error/rejected status in counts and add upgrade to correct old ones

--- app/src/Application/InstallBundle/Upgrade/Build/2014/04/Build1396876031.php
+++ app/src/Application/InstallBundle/Upgrade/Build/2014/04/Build1396876031.php
@@ -45,5 +45,8 @@ class Build1396876031 extends AbstractBuild
 		$db->exec("UPDATE sendmail_queue SET status = 'complete' WHERE has_sent = 1");
 		$db->exec("UPDATE sendmail_queue SET status = 'error' WHERE has_sent = 0 AND date_next_attempt IS NULL");
 		$db->exec("UPDATE sendmail_queue SET status = 'pending' WHERE has_sent = 0 AND date_next_attempt IS NOT NULL");
+
+		$this->out("Correct email_sources.status");
+		$db->exec("UPDATE email_sources SET status = 'rejected' WHERE status = 'error' AND error_code NOT IN ('server_error', 'timeout')");
 	}
 }
