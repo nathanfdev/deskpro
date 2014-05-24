46cc10af328f6258f15cfd447116916b947823ee
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-05-24 10:22:55 +0100

    Fix misplaced parenthesis when password policy settings are enabled

--- app/src/Application/DeskPRO/HttpFoundation/Session.php
+++ app/src/Application/DeskPRO/HttpFoundation/Session.php
@@ -105,7 +105,7 @@ class Session extends \Symfony\Component\HttpFoundation\Session\Session implemen
 							SELECT person_id, auth
 							FROM sessions
 							WHERE id = ? AND date_last > ? AND date_last_page > ?
-						", array($sid, date('Y-m-d H:i:s', time() - App::getSetting('core.sessions_lifetime'), time() - App::getSetting('core.sessions_lifetime'))));
+						", array($sid, date('Y-m-d H:i:s', time() - App::getSetting('core.sessions_lifetime')), date(time() - App::getSetting('core.sessions_lifetime'))));
 					} else {
 						$agent_session = App::getDb()->fetchAssoc("
 							SELECT person_id, auth
