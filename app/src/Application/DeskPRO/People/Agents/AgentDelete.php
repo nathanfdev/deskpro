537aa0678770a44736d50db4817613a6cc0ab2d9
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-05-29 11:29:29 +0100

    [FIX] Deleting agent to a user would not delete filters and some other agent-related associations

--- app/src/Application/DeskPRO/People/Agents/AgentDelete.php
+++ app/src/Application/DeskPRO/People/Agents/AgentDelete.php
@@ -116,6 +116,24 @@ class AgentDelete
 				WHERE agent_id = ?
 			", array($this->agent->id));
 
+			// Filters
+			$this->db->executeUpdate("
+				DELETE FROM ticket_filters
+				WHERE person_id = ?
+			", array($this->agent->id));
+
+			// Subscriptions
+			$this->db->executeUpdate("
+				DELETE FROM ticket_filter_subscriptions
+				WHERE person_id = ?
+			", array($this->agent->id));
+
+			// Agent team
+			$this->db->delete('agent_team_members', array('person_id' => $this->agent->getId()));
+
+			// Permission overrides
+			$this->db->delete('permissions', array('person_id' => $this->agent->getId()));
+
 			$this->db->commit();
 		} catch (\Exception $e) {
 			$this->db->rollback();
