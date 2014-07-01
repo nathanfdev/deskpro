5022153c49840b6bc1e2bd1bef6a073704dc4172
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-07-01 10:32:12 +0100

    Fix handling of old terms not being array

--- app/src/Application/DeskPRO/Tickets/Triggers/Terms/CheckEmailAccount.php
+++ app/src/Application/DeskPRO/Tickets/Triggers/Terms/CheckEmailAccount.php
@@ -65,6 +65,11 @@ class CheckEmailAccount extends AbstractTriggerTerm
 
 		$account_ids = $options['email_account_ids'];
 
+		// Legacy handling where may be a single integer instead of array
+		if (!is_array($account_ids)) {
+			$account_ids = array($account_ids);
+		}
+
 		return $this->isEntityMatch($ticket, $context, 'email_account', 'id', $account_ids);
 	}
 }
