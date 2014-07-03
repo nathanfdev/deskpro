784e9aef20c7bfe2df6c886e801670275405b139
n3b <neb802@gmail.com>
2014-07-04 00:32:58 +0400

    tab titles

--- app/languages/default/adm/api_keys.php
+++ app/languages/default/adm/api_keys.php
@@ -1,6 +1,6 @@
 <?php return array(
-	'adm.api_keys.keys_title'                  => 'API Keys',
-	'adm.api_keys.logs_title'                  => 'API Logs',
+	'adm.api_keys.keys_title'                  => 'API Key Info',
+	'adm.api_keys.logs_title'                  => 'API Key Logs',
 
 	'adm.api_keys.request'                     => 'Request',
 	'adm.api_keys.path'                        => 'Path',
