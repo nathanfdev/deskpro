6b50ab8b688c04d52bd8db880729915a2f190002
n3b <neb802@gmail.com>
2014-07-04 15:11:17 +0400

    debug info for travis

--- app/testing/travis-ci/before_script.sh
+++ app/testing/travis-ci/before_script.sh
@@ -48,4 +48,28 @@ cat app/testing/travis-ci/apache-php-config.txt | sudo tee /etc/apache2/conf.d/p
 cat app/testing/travis-ci/apache-vhost-config.txt | sed -e "s,PATH,`pwd`,g" | sudo tee /etc/apache2/sites-available/default > /dev/null
 echo "Listen 8888" | sudo tee -a /etc/apache2/ports.conf
 sudo service apache2 restart
-sudo cat /var/log/apache2/error.log
+
+echo "Starting xvfb"
+export DISPLAY=:99
+/usr/bin/Xvfb :99 -ac -screen 0 1280x800x8 > /var/log/Xvfb.log 2>&1 &
+echo "--> Done"
+
+echo "Starting firefox"
+firefox > /var/log/firefox.log 2>&1 &
+echo "--> Done"
+
+echo "Downloading Selenium"
+wget -O /tmp/selenium-server-standalone-2.38.0.jar http://selenium.googlecode.com/files/selenium-server-standalone-2.38.0.jar
+echo "--> Done"
+
+echo "Starting Selenium Hub"
+java -jar /tmp/selenium-server-standalone-2.38.0.jar -role hub > /var/log/selenium-hub.log 2>&1 &
+echo "."
+sleep 3
+echo "--> Done"
+
+echo "Starting Selenium Node"
+java -jar /tmp/selenium-server-standalone-2.38.0.jar -role node -hub http://localhost:4444/grid/register > /var/log/selenium-node.log 2>&1 &
+echo "."
+sleep 3
+echo "--> Done"
