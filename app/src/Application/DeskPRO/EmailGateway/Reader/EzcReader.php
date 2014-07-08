abb2254d3535e5cce6a92d16d2aa0f36c914d11c
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-07-08 11:12:48 +0100

    [FIX] Decoding email with multi-part bodies would only ever include the first part.
    MIME can be nested, and the "body" part might contain multiple sections. This is most
    common (as far as I've seen) in Apple Mail where forwarding a message. The message
    is forwarded as a separate part (but not an attachment, so it is meant to be inline).

--- app/src/Application/DeskPRO/EmailGateway/Reader/EzcReader.php
+++ app/src/Application/DeskPRO/EmailGateway/Reader/EzcReader.php
@@ -386,6 +386,8 @@ class EzcReader extends AbstractReader
 
 	protected function _getBodyHtml()
 	{
+		$raw_parts = array();
+
 		foreach ($this->mail->fetchParts(array('ezcMailText')) as $part) {
 			if ($part->subType == 'html') {
 				$originalCharset = $part->originalCharset;
@@ -400,20 +402,68 @@ class EzcReader extends AbstractReader
 				$body->body_utf8 = Strings::convertToUtf8(Strings::standardEol($part->text), $originalCharset);
 				$body->original_charset = $part->originalCharset;
 
-				return $body;
+				$raw_parts[] = $body;
 			}
 		}
 
-		// Default to a blank body
-		$body = new Item\BodyHtml();
-		$body->body = '';
-		$body->body_utf8 = '';
-		$body->original_charset = 'UTF-8';
-		return $body;
+		if ($raw_parts) {
+
+			$all_same = true;
+			$charset = null;
+
+			$all_utf = '';
+			$all_raw = '';
+
+			foreach ($raw_parts as $p) {
+
+				$all_utf .= $p->body_utf8;
+				$all_raw .= $p->body;
+
+				if (!$charset) {
+					$charset = $p->original_charset;
+				} else if ($charset != $p->original_charset) {
+					$all_same = false;
+				}
+			}
+
+			// All charsets were the same,
+			// so we can have an accurate computed body with
+			// accurate original_charset
+			if ($all_same) {
+				$body = new Item\BodyHtml();
+				$body->body = $all_raw;
+				$body->body_utf8 = $all_utf;
+				$body->original_charset = $charset;
+
+			// Charsets differ, so we need
+			// to construct based on the utf8-only body
+			} else {
+				$body = new Item\BodyHtml();
+				$body->body = $all_utf;
+				$body->body_utf8 = $all_utf;
+				$body->original_charset = 'UTF-8';
+			}
+
+			$body->raw_parts = $raw_parts;
+
+			return $body;
+
+		} else {
+			// Default to a blank body
+			$body = new Item\BodyHtml();
+			$body->body = '';
+			$body->body_utf8 = '';
+			$body->original_charset = 'UTF-8';
+			$body->raw_parts = array(clone $body);
+
+			return $body;
+		}
 	}
 
 	protected function _getBodyText()
 	{
+		$raw_parts = array();
+
 		foreach ($this->mail->fetchParts(array('ezcMailText')) as $part) {
 			if ($part->subType == 'plain') {
 				$originalCharset = $part->originalCharset;
@@ -428,15 +478,60 @@ class EzcReader extends AbstractReader
 				$body->body_utf8 = Strings::convertToUtf8($part->text, $originalCharset);
 				$body->original_charset = $originalCharset;
 
-				return $body;
+				$raw_parts[] = $body;
 			}
 		}
 
-		// Default to a blank body
-		$body = new Item\BodyText();
-		$body->body = '';
-		$body->body_utf8 = '';
-		$body->original_charset = 'UTF-8';
-		return $body;
+		if ($raw_parts) {
+
+			$all_same = true;
+			$charset = null;
+
+			$all_utf = '';
+			$all_raw = '';
+
+			foreach ($raw_parts as $p) {
+
+				$all_utf .= $p->body_utf8;
+				$all_raw .= $p->body;
+
+				if (!$charset) {
+					$charset = $p->original_charset;
+				} else if ($charset != $p->original_charset) {
+					$all_same = false;
+				}
+			}
+
+			// All charsets were the same,
+			// so we can have an accurate computed body with
+			// accurate original_charset
+			if ($all_same) {
+				$body = new Item\BodyText();
+				$body->body = $all_raw;
+				$body->body_utf8 = $all_utf;
+				$body->original_charset = $charset;
+
+				// Charsets differ, so we need
+				// to construct based on the utf8-only body
+			} else {
+				$body = new Item\BodyText();
+				$body->body = $all_utf;
+				$body->body_utf8 = $all_utf;
+				$body->original_charset = 'UTF-8';
+			}
+
+			$body->raw_parts = $raw_parts;
+
+			return $body;
+
+		} else {
+			// Default to a blank body
+			$body = new Item\BodyText();
+			$body->body = '';
+			$body->body_utf8 = '';
+			$body->original_charset = 'UTF-8';
+			$body->raw_parts = array(clone $body);
+			return $body;
+		}
 	}
 }
