913592447ece469e952fd1e78693db4dfef43c9c
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-07-14 18:05:29 +0100

    Fix SSL cert errors

--- app/src/Cloud/AdminBundle/Controller/SettingsController.php
+++ app/src/Cloud/AdminBundle/Controller/SettingsController.php
@@ -105,7 +105,7 @@ class SettingsController extends BaseSettingsController
 				$url = DP_MA_SERVER . '/cloud/call/'.DPC_SITE_ID.'/'. $tmpdata->getCode();
 
 				try {
-					$client = new \Zend\Http\Client(null, array('timeout' => 10));
+					$client = new \Zend\Http\Client(null, array('timeout' => 15, 'sslverifypeer' => false));
 					$client->setMethod(\Zend\Http\Request::METHOD_GET);
 					$client->setUri($url);
 					$r = $client->send();
