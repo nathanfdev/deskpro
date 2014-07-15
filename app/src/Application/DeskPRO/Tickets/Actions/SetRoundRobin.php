fdfc68665b48ff38d651adc248faa6bfabdd39f1
n3b <neb802@gmail.com>
2014-07-15 17:02:44 +0400

    injected trigger id into executor context

--- app/src/Application/DeskPRO/Tickets/Actions/SetRoundRobin.php
+++ app/src/Application/DeskPRO/Tickets/Actions/SetRoundRobin.php
@@ -110,8 +110,8 @@ class SetRoundRobin extends AbstractContainerAwareAction implements ActionInterf
 			$this->getRep()->updateNextAgent($rr);
 			$ticket->agent = $agent;
 
-			// todo inject trigger id
-			$entry = new LogRoundRobin($rr['id'], $agent['id'], $ticket['id'], 0);
+			$triggerId = $context->getVars()->get('trigger_id', 0);
+			$entry = new LogRoundRobin($rr['id'], $agent['id'], $ticket['id'], $triggerId);
 			$context->getLogger()->info($entry);
 
 		} catch (\RuntimeException $e) {
