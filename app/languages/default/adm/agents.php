85fba2dbd3afe24fb1e1e414a465c942cfd2aa40
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-07-23 19:15:38 +0100

    Add template file to distro, tweak phrase a bit

--- app/languages/default/adm/agents.php
+++ app/languages/default/adm/agents.php
@@ -69,12 +69,12 @@
 	'adm.agents.import_textarea_title'                            => 'Copy/Paste a list of Email Addresses',
 	'adm.agents.import_textarea_description'                      => 'Paste a list of addresses below separated by a comma, semicolon, space or line return',
 	'adm.agents.import_file_title'                                => 'Upload a spreadsheet',
-	'adm.agents.import_file_description'                          => 'Download this excel spreadsheet and fill it with your own data. When you are ready, upload it and your agents will be created',
-	'adm.agents.import_file_dd'                                   => 'Drag & Drop Completed Spreadsheet here',
+	'adm.agents.import_file_description'                          => 'Click the link below to download a special CSV spreadsheet. Fill in the spreadsheet with the contact details of your agents, then upload it here.',
+	'adm.agents.import_file_dd'                                   => 'Drag & Drop completed spreadsheet here',
 	'adm.agents.import_btn_next'                                  => 'Next step',
 	'adm.agents.import_btn_more'                                  => 'Invite More Agents?',
 	'adm.agents.import_results'                                   => '{{count}} Agent have been invited to DeskPRO|{{count}} Agents have been invited to DeskPRO',
 	'adm.agents.import_results_failed'                            => '{{count}} Agent Invitation Failed|{{count}} Agent Invitations Failed',
-	'adm.agents.import_kb'                                        => 'Read more about DeskPRO Bulk Add Agents Spreadsheet',
+	'adm.agents.import_kb'                                        => 'Download Spreadsheet Template',
 
 );
