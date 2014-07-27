b55b9624fb97b90d7dd7b6502060a351b5a54cae
Chris Tickner <chris.tickner@deskpro.com>
2014-07-27 19:21:10 -0300

    [SMS] implement the TwilioSmsProvider

--- app/src/Orb/Sms/Provider/TwilioSmsProvider.php
+++ app/src/Orb/Sms/Provider/TwilioSmsProvider.php
@@ -7,6 +7,12 @@ use Orb\Sms\SmsResult;
 
 class TwilioSmsProvider implements SmsProviderInterface
 {
+	protected $twilio;
+
+	public function __construct($sid, $auth_token)
+	{
+		$this->twilio = new \Services_Twilio($sid, $auth_token);
+	}
 
 	/**
 	 * @param string $fromPhoneNumber phone number to send to, provider should be able to handle any format
@@ -18,7 +24,44 @@ class TwilioSmsProvider implements SmsProviderInterface
 	 */
 	public function sendMessage($fromPhoneNumber, $toPhoneNumber, $textMessage)
 	{
-		return new SmsResult(SmsResult::SMS_FAIL, $fromPhoneNumber, $toPhoneNumber, $textMessage, $this, array());
+		try {
+			$message = $this->twilio->account->messages->sendMessage(
+				$fromPhoneNumber,
+				$toPhoneNumber,
+				$textMessage
+			);
+		} catch (\Services_Twilio_RestException $e) {
+			$result = new SmsResult(
+				SmsResult::SMS_FAIL, $fromPhoneNumber, $toPhoneNumber, $textMessage, $this, array(
+					'status'        => $e->getCode(),
+					'message'       => $e->getMessage(),
+					'twilio_status' => $e->getStatus(),
+					'twilio_info'   => $e->getInfo()
+				)
+			);
+
+			return $result;
+		} catch (\Exception $e) {
+			$result = new SmsResult(
+				SmsResult::SMS_FAIL, $fromPhoneNumber, $toPhoneNumber, $textMessage, $this, array(
+					'status'  => $e->getCode(),
+					'message' => $e->getMessage()
+				)
+			);
+
+			return $result;
+		}
+
+		// we successfully sent a valid SMS to Twilio
+		$result = new SmsResult(
+			SmsResult::SMS_SENT, $fromPhoneNumber, $toPhoneNumber, $textMessage, $this, array(
+				'sid'             => $message->sid, // this can later be used to find the status of the sms
+				'num_segments'    => $message->num_segments,
+				'provider_status' => $message->status
+			)
+		);
+
+		return $result;
 	}
 
 	/**
