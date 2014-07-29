d4a954730511e2e6f05cf75dcf1bfa4f5f303bf7
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-07-29 15:28:40 +0100

    Remove useless param passing

--- app/src/Application/DeskPRO/BlobStorage/DeskproBlobStorage.php
+++ app/src/Application/DeskPRO/BlobStorage/DeskproBlobStorage.php
@@ -260,7 +260,7 @@ class DeskproBlobStorage implements Loggable
 		}
 
 		$this->em->persist($blob_entity);
-		$this->em->flush($blob_entity);
+		$this->em->flush();
 
 		// We need the ID first to generate a proper unique filename/auth
 		$batch = (int)(($blob_entity->id-1) / 1000) + 1;
@@ -331,7 +331,7 @@ class DeskproBlobStorage implements Loggable
 		}
 
 		$this->em->persist($blob_entity);
-		$this->em->flush($blob_entity);
+		$this->em->flush();
 
 		$this->logger->logDebug("[DeskproBlobStorage] (saveBlobRecordFromFile) Save success");
 
@@ -402,7 +402,7 @@ class DeskproBlobStorage implements Loggable
 		}
 
 		$this->em->persist($blob_entity);
-		$this->em->flush($blob_entity);
+		$this->em->flush();
 
 		// We need the ID first to generate a proper unique filename/auth
 		$batch = (int)(($blob_entity->id-1) / 1000) + 1;
@@ -473,7 +473,7 @@ class DeskproBlobStorage implements Loggable
 		}
 
 		$this->em->persist($blob_entity);
-		$this->em->flush($blob_entity);
+		$this->em->flush();
 
 		$this->logger->logDebug("[DeskproBlobStorage] (saveBlobRecordFromString) Save success");
 
@@ -764,7 +764,7 @@ class DeskproBlobStorage implements Loggable
 		}
 
 		$this->em->persist($blob_entity);
-		$this->em->flush($blob_entity);
+		$this->em->flush();
 
 		// Delete the old one
 		$this->deleteBlob($old_blob, $old_adapter_id);
