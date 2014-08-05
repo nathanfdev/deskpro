2fbff9a6302214ab786553d6c2d807bff596c925
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-08-05 09:50:29 +0100

    Correct note on build script

--- app/src/Application/InstallBundle/Upgrade/Build/2014/07/Build1405429499.php
+++ app/src/Application/InstallBundle/Upgrade/Build/2014/07/Build1405429499.php
@@ -38,7 +38,7 @@ class Build1405429499 extends AbstractBuild
 {
 	public function run()
 	{
-		$this->out("My Upgrade Class");
+		$this->out("Install round robin tables");
 		$this->execMutateSql("CREATE TABLE log_round_robin (id INT AUTO_INCREMENT NOT NULL, timestamp int(11) unsigned not null, round_robin_id int(11) unsigned not null, agent_id int(11) unsigned not null, ticket_id int(11) unsigned not null, trigger_id int(11) unsigned not null, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci");
 		$this->execMutateSql("CREATE TABLE round_robin (id INT AUTO_INCREMENT NOT NULL, next_agent_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_A56034E1C0E3DE5 (next_agent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci");
 		$this->execMutateSql("CREATE TABLE round_robin_agents (agent_id INT NOT NULL, robin_id INT NOT NULL, sort INT NOT NULL, INDEX IDX_B51E3A8F3414710B (agent_id), INDEX IDX_B51E3A8F654E52F7 (robin_id), PRIMARY KEY(agent_id, robin_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci");
