bd384c5c91aba57650591547b70865ccd677d433
Chris Tickner <chris.tickner@deskpro.com>
2014-08-06 18:07:33 -0300

    [Channels] fix language

--- app/languages/default/admin/channels.php
+++ app/languages/default/admin/channels.php
@@ -1,6 +1,6 @@
 <?php return array(
 	'admin.channels.sms_x_accounts'       => '{{count}} SMS Account|{{count}} SMS Accounts',
 	'admin.channels.sms_account_saved'   => 'SMS Account Saved',
-	'admin.channels.sms_no_accounts_yet' => 'No Accounts Yet',
+	'admin.channels.sms_no_accounts_yet' => 'You have not created any SMS accounts yet.',
 	'admin.channels.sms_new_account' => 'New SMS Account'
 );
