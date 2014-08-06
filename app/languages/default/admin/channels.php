b54d1a1f20587b1d475ca9c1d309db6f2203f900
Chris Tickner <chris.tickner@deskpro.com>
2014-08-06 17:42:56 -0300

    [Channels] create list view for sms (with languages)

---
+++ app/languages/default/admin/channels.php
@@ -0,0 +1,6 @@
+<?php return array(
+	'admin.channels.sms_x_accounts'       => '{{count}} SMS Account|{{count}} SMS Accounts',
+	'admin.channels.sms_account_saved'   => 'SMS Account Saved',
+	'admin.channels.sms_no_accounts_yet' => 'No Accounts Yet',
+	'admin.channels.sms_new_account' => 'New SMS Account'
+);
