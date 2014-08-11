5ee4f05683ea0cc36b18db3e3902c43536466d5b
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-08-11 19:10:36 +0100

    [FIX] Editing templates removing phrase tags that it shouldnt

--- app/src/Application/DeskPRO/Templating/Templates/TemplateSet.php
+++ app/src/Application/DeskPRO/Templating/Templates/TemplateSet.php
@@ -275,8 +275,8 @@ class TemplateSet
 	public function resolvePhraseTags($code, Translate $tr)
 	{
 		$code = preg_replace_callback('#\{\{\s*phrase\((\"|\')([a-zA-Z0-9_\-\.]+)\\1\)\s*\}\}#', function($m) use ($tr) {
-			$phrase = $tr->phrase($m[2]);
-			if ($phrase) {
+			$phrase = $tr->getPhraseText($m[2], null, true);
+			if ($phrase && strpos('|', $phrase) === false) {
 				return $phrase;
 			} else {
 				return $m[0];
