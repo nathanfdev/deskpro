35d086903653d15b7dccea1cf541c50f62d25a24
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-08-14 10:15:48 +0100

    Fix loading of font-awesome, and fix duplicate loading of FA3 and FA4 font files

--- app/sys/config/config.assets.php
+++ app/sys/config/config.assets.php
@@ -701,7 +701,6 @@ $CONFIG['agent_vendors_css'] = array(
 		'vendor/jquery/colorbox/colorbox.css',
 		'vendor/select2/select2.css',
 		'vendor/redactor/redactor.css',
-		'app/bower_components/font-awesome/css/font-awesome.min.css',
 	)
 );
 
