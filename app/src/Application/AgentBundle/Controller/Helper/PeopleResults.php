54d573517c26dbaa982ec8e69d34d87f5bf536ef
n3b <neb802@gmail.com>
2014-08-14 13:21:20 +0400

    revert test const

--- app/src/Application/AgentBundle/Controller/Helper/PeopleResults.php
+++ app/src/Application/AgentBundle/Controller/Helper/PeopleResults.php
@@ -46,7 +46,7 @@ use Orb\Util\Arrays;
  */
 class PeopleResults
 {
-	const PER_PAGE_DEFAULT = 2;
+	const PER_PAGE_DEFAULT = 50;
 
 	/**
 	 * @var \Application\AgentBundle\Controller\AbstractController
