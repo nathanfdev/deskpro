9a008fb6ded373662bc4a62f33562c08d4d97cda
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-08-18 19:07:15 +0100

    Fix label search and escape terms to prevent parser errors

--- app/src/Application/DeskPRO/NewSearch/Repository/AbstractRepository.php
+++ app/src/Application/DeskPRO/NewSearch/Repository/AbstractRepository.php
@@ -64,21 +64,20 @@ abstract class AbstractRepository extends Repository
     {
 		$l = Strings::extractRegexMatch('#^\[(.*?)\]$#', $q);
 		if ($l && $this instanceof WithLabelsInterface) {
-			$queryString = new QueryString($l);
+			$queryString = new QueryString(ElasticaUtil::escapeTerm($l));
 			$queryString->setFields(array('labels'));
 			$queryString->setDefaultOperator('AND');
 			$query = new Query(
 				array(
 					'query' => array(
 						'filtered' => array(
-							'query'  => $queryString,
+							'query'  => $queryString->toArray(),
 							'filter' => $this->getFilters(),
 						)
 					)
 				)
 			);
 		} else {
-			if ($l) $q = $l;
 			$query = new Query(
 				array(
 					'query' => array(
@@ -102,7 +101,7 @@ abstract class AbstractRepository extends Repository
      */
     protected function getQueryString($q)
     {
-        $queryString = new QueryString($q);
+        $queryString = new QueryString(ElasticaUtil::escapeTerm($q));
 		$queryString->setFields($this->getQueryFields());
         $queryString->setDefaultOperator('AND');
 
