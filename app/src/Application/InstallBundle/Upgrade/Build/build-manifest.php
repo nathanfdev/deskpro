2f91a6587a915e935ac06574babccb40133906d1
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-08-19 11:52:24 +0100

    Build files for previous commit

--- app/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
+++ app/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
@@ -1484,4 +1484,9 @@
 		'classname' => 'Application\InstallBundle\Upgrade\Build\deskpro/www/app/src/Application/InstallBundle/Upgrade/Build/2014/08/Build1408010946'
 	),
 
+	1408444531 => array(
+		'file'      => '/src/Application/InstallBundle/Upgrade/Build/2014/08/Build1408444531.php',
+		'classname' => 'Application\InstallBundle\Upgrade\Build\deskpro/www/app/src/Application/InstallBundle/Upgrade/Build/2014/08/Build1408444531'
+	),
+
 );
