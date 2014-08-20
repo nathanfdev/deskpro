eb0fc0cfab038bab89dd8a37f7b25f2da413b944
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-08-20 19:27:41 +0100

    [IMPROVE] Agent app proxy script allows passing of custom headers

--- app/src/Application/AgentBundle/Controller/MiscController.php
+++ app/src/Application/AgentBundle/Controller/MiscController.php
@@ -315,26 +315,35 @@ JS;
 
 	public function proxyAction()
 	{
-		$url = $this->in->getString('url');
+		$url = $this->request->headers->get('X-DeskPRO-Proxy-Url');
+		if ($url) {
+			$used_req_url = false;
+		} else {
+			$url = $this->in->getString('url');
+			$used_req_url = true;
+		}
+
 		$urlinfo = @parse_url($url);
 		if (!$url OR !$urlinfo OR empty($urlinfo['scheme']) OR !preg_match('#^https?#', $urlinfo['scheme'])) {
 			return $this->createResponse('Bad url', 400);
 		}
 
 		$originalMethod = $this->request->getMethod();
-		$method = $originalMethod;
-		if ($originalMethod == 'GET' || $originalMethod == 'POST') {
-			$newMethod = $this->in->getString('method');
-			if ($newMethod) {
-				$method = $newMethod;
-			}
+		$method = $this->request->headers->get('X-DeskPRO-Proxy-Method');
+		if (!$method) {
+			$method = $originalMethod;
+		}
 
+		if ($originalMethod == 'GET' || $originalMethod == 'POST') {
 			if ($originalMethod == 'GET') {
 				$passData = $_GET;
 			} else {
 				$passData = $_POST;
 			}
-			unset($passData['url'], $passData['method']);
+
+			if ($used_req_url) {
+				unset($passData['url']);
+			}
 		} else {
 			$passData = file_get_contents('php://input');
 		}
@@ -400,8 +409,25 @@ JS;
 			}
 		}
 
+		$headers = array();
 		if (!empty($_SERVER['CONTENT_TYPE'])) {
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: ' . $_SERVER['CONTENT_TYPE']));
+			$headers[] = 'Content-Type: ' . $_SERVER['CONTENT_TYPE'];
+		}
+
+		// Proxy custom headers
+		foreach ($this->request->headers->all() as $name => $value) {
+			$realname = Strings::extractRegexMatch('#^X\-DeskPRO\-Proxy\-Header\-(.*?)$#i', $name);
+			if ($realname) {
+				foreach ($value as $v) {
+					$headers[] = "$realname: " . $v;
+				}
+			}
+		}
+
+		dp_log($headers);
+
+		if ($headers) {
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 		}
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_HEADER, false);
