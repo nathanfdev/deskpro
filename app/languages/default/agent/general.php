e4af9626bcb65b2085a7f0e9e56e82d01adabfd3
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-08-26 13:48:56 +0100

    Tweak UI around assign/unassign quick-links

--- app/languages/default/agent/general.php
+++ app/languages/default/agent/general.php
@@ -741,5 +741,5 @@
 	'agent.general.your_email_address'                      => 'Your Email Address',
 	'agent.general.your_teams'                              => 'Your Teams',
 	'agent.general.new'                                     => 'New',
-	'agent.general.unassign'                                => 'Unasign',
+	'agent.general.unassign'                                => 'Unassign',
 );
