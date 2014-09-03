f684cc1f2855615b887363d5a9a26fcfbc32dd94
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-09-03 19:34:28 +0100

    https url

--- app/sys/config/config.news.php
+++ app/sys/config/config.news.php
@@ -2,6 +2,6 @@
 	459 => array(
 		'id'    => 459,
 		'title' => 'Agent Interface Improved',
-		'url'   => 'http://support.deskpro.com/news/view/459-agent-interface-improved',
+		'url'   => 'https://support.deskpro.com/news/view/459-agent-interface-improved',
 	)
 );
