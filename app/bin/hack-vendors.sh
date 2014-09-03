234f1bddaa097c666ce498dea7adb5b107344ff1
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-09-03 11:30:09 +0100

    [FIX] Possible issues with some LDAP/ActiveDirectory user sources to do with updated 3rd-party library

--- app/bin/hack-vendors.sh
+++ app/bin/hack-vendors.sh
@@ -5,3 +5,7 @@ DIR_VENDOR=$DIR_ROOT/app/vendor
 
 sed -i -e 's/&offsetGet/offsetGet/g' $DIR_VENDOR/zendframework/zendframework/library/Zend/Stdlib/ArrayObject.php
 rm -f $DIR_VENDOR/zendframework/zendframework/library/Zend/Stdlib/ArrayObject.php-e
+
+cp $DIR_VENDOR/zendframework/zendframework/library/Zend/Ldap/Node.php $DIR_VENDOR/zendframework/zendframework/library/Zend/Ldap/Node.php.orig
+cat $DIR_VENDOR/zendframework/zendframework/library/Zend/Ldap/Node.php.orig | php -r 'echo preg_replace('\''/if \(\!Dn\:\:isChildOf\(\$this\-\>_getDn\(\), \$ldap\-\>getBaseDn\(\)\)\) \{.*?\}\s*/s'\'', "/* DESKPRO EDIT: Removed isChildOf check */\n\n        ", stream_get_contents(STDIN));' > $DIR_VENDOR/zendframework/zendframework/library/Zend/Ldap/Node.php
+rm $DIR_VENDOR/zendframework/zendframework/library/Zend/Ldap/Node.php.orig
