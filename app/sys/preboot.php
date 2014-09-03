b39d674f85fa83d2f072e17845b73f3a8fe90a0a
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-09-03 11:50:46 +0100

    Fix display_errors being turned on on cloud

--- app/sys/preboot.php
+++ app/sys/preboot.php
@@ -93,7 +93,7 @@ if (!DP_REAL_ERROR_LOG) {
 
 // If DeskPRO is not installed yet, always force-on display_errors
 // so problems during an install process are not missed
-if (!file_exists(dp_get_data_dir().'/is_installed.dat')) {
+if (!file_exists(dp_get_data_dir().'/is_installed.dat') && !defined('DPC_IS_CLOUD')) {
 	@ini_set('display_errors', "1");
 }
 
