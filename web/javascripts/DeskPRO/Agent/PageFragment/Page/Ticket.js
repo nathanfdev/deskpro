6088a59a56f3636be90b86ba781c64bdf87d1863
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-09-08 16:12:48 +0100

    [FIX] Agent: Removing a CC would remove the 'add cc' input box [FIX] Agent: Incorrect error message if you tried to remove a CC but did not have permission to do so

--- web/javascripts/DeskPRO/Agent/PageFragment/Page/Ticket.js
+++ web/javascripts/DeskPRO/Agent/PageFragment/Page/Ticket.js
@@ -678,7 +678,7 @@ DeskPRO.Agent.PageFragment.Page.Ticket = new Orb.Class({
 			});
 		}
 
-		this.getEl('cc_list_btn').on('click', function(ev) {
+		this.getEl('cc_list_btn2').on('click', function(ev) {
 			ev.preventDefault();
 
 			if (self.getEl('cc_list').hasClass('cc-open')) {
@@ -686,12 +686,21 @@ DeskPRO.Agent.PageFragment.Page.Ticket = new Orb.Class({
 					self.getEl('cc_list').hide().removeClass('cc-open');
 				} else {
 					self.getEl('cc_list').find('.addrow').toggle();
+					self.getEl('cc_list').find('.addrow').find('input[type="text"]').focus();
 				}
 			} else {
 				self.getEl('cc_list').show().addClass('cc-open');
 				self.getEl('cc_list').find('.addrow').show();
 				self.getEl('cc_list').find('.addrow').find('input[type="text"]').focus();
 			}
+		});
+
+		this.getEl('cc_list_btn').on('click', function(ev) {
+			ev.preventDefault();
+
+			self.getEl('cc_list').show().addClass('cc-open');
+			self.getEl('cc_list').find('.addrow').show();
+			self.getEl('cc_list').find('.addrow').find('input[type="text"]').focus();
 		});
 
 		var logsWrap = this.getEl('logs_wrap');
@@ -899,7 +908,7 @@ DeskPRO.Agent.PageFragment.Page.Ticket = new Orb.Class({
 					}
 				}
 
-				this.fireEvent('load_messge_page', [page]);
+				this.fireEvent('load', [data]);
 			}
 		});
 	},
