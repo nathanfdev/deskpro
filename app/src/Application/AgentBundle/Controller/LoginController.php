8a100e1d4eecb69f0f951d49a64533afed79da5d
Chris Tickner <chris.tickner@deskpro.com>
2014-09-09 16:18:49 -0400

    [auth] update agent login form with new authManager

--- app/src/Application/AgentBundle/Controller/LoginController.php
+++ app/src/Application/AgentBundle/Controller/LoginController.php
@@ -132,8 +132,6 @@ class LoginController extends \Application\UserBundle\Controller\LoginController
 
 		return $this->render('AgentBundle:Login:index.html.twig', array(
 			'return'                   => $return,
-			'display_input_form'       => $this->auth_manager->hasFormLoginCapability(),
-			'login_button_usersources' => $this->auth_manager->getLoginButtonUsersources(),
 			'route_prefix'             => $this->route_prefix,
 			'logo_blob'                => $logo_blob,
 			'has_logged_out'           => $has_logged_out,
