5a238217db6d3c5f153c37a0268412b23f763060
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-09-10 16:08:35 +0100

    Upgrade script to set status on tickets that should have been validated

--- app/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
+++ app/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
@@ -1501,7 +1501,12 @@
 
 	1410276758 => array(
 		'file'      => '/src/Application/InstallBundle/Upgrade/Build/2014/09/Build1410276758.php',
-		'classname' => 'Application\InstallBundle\Upgrade\Build\deskpro/www/app/src/Application/InstallBundle/Upgrade/Build/2014/09/Build1410276758'
+		'classname' => 'Application\InstallBundle\Upgrade\Build\Build1410276758'
+	),
+
+	1410361094 => array(
+		'file'      => '/src/Application/InstallBundle/Upgrade/Build/2014/09/Build1410361094.php',
+		'classname' => 'Application\InstallBundle\Upgrade\Build\deskpro/www/app/src/Application/InstallBundle/Upgrade/Build/2014/09/Build1410361094'
 	),
 
 );
