45495a992dfc6ec3c32e034cf2d13716c36eeae9
Chris Tickner <chris.tickner@deskpro.com>
2014-09-17 00:54:07 -0400

    [Auth] update magento

--- app/apps/deskpro_magento/native/InstallerHandler.php
+++ app/apps/deskpro_magento/native/InstallerHandler.php
@@ -34,68 +34,30 @@
 
 namespace deskpro_magento;
 
-use Application\DeskPRO\App\Native\InstallerHandler\InstallerContext;
-use Application\DeskPRO\App\Native\InstallerHandler\AbstractInstallerHandler;
+use Application\DeskPRO\App\Native\InstallerHandler\AbstractUsersourceInstallerHandler;
+use Application\DeskPRO\Entity\AppInstance;
+use Application\DeskPRO\Entity\Usersource;
+use Application\DeskPRO\ORM\EntityManager;
 
-class InstallerHandler extends AbstractInstallerHandler
+class InstallerHandler extends AbstractUsersourceInstallerHandler
 {
 	/**
 	 * {@inheritDoc}
 	 */
-	public function install(InstallerContext $context)
+	protected function applyAppToUsersource(AppInstance $app, Usersource $us, EntityManager $em)
 	{
-		$context->getDb()->insert('usersources', array(
-			'app_id'            => $context->getApp()->id,
-			'title'             => $context->getApp()->title,
-			'source_type'       => 'app',
-			'lost_password_url' => $context->getApp()->getSetting('lost_pwd_url') ?: '',
-			'options'           => json_encode(array(
-				'url'      => $context->getApp()->getSetting('url'),
-				'api_user' => $context->getApp()->getSetting('api_user'),
-				'api_key'  => $context->getApp()->getSetting('api_key'),
-				'sso_js'   => $context->getApp()->getSetting('enable_sso') ? true : false,
-			)),
-			'is_enabled'        => $context->getApp()->getSetting('enable_usersource') ? '1' : '0',
-			'source_type'       => 'deskpro_magento\\Usersource\\Adapter\\Magento'
-		));
-	}
+		$us->options           = array(
+			'url'      => $app->getSetting('url'),
+			'api_user' => $app->getSetting('api_user'),
+			'api_key'  => $app->getSetting('api_key'),
+			'sso_js'   => $app->getSetting('enable_sso') ? true : false,
+		);
+		$us->lost_password_url = $app->getSetting('lost_pwd_url') ? : '';
+		$us->is_enabled        = $app->getSetting('enable_usersource') ? 1 : 0;
+		$us->source_type       = 'deskpro_magento\\Usersource\\Adapter\\Magento';
 
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function uninstall(InstallerContext $context)
-	{
-		$context->getDb()->delete('usersources', array('app_id' => $context->getApp()->id));
-	}
-
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function updateSettings(InstallerContext $context)
-	{
-		$context->getDb()->update('usersources', array(
-			'title'             => $context->getApp()->title,
-			'source_type'       => 'app',
-			'lost_password_url' => $context->getApp()->getSetting('lost_pwd_url') ?: '',
-			'options'           => json_encode(array(
-				'url'      => $context->getApp()->getSetting('url'),
-				'api_user' => $context->getApp()->getSetting('api_user'),
-				'api_key'  => $context->getApp()->getSetting('api_key'),
-				'sso_js'   => $context->getApp()->getSetting('enable_sso') ? true : false,
-			)),
-			'is_enabled'        => $context->getApp()->getSetting('enable_usersource') ? '1' : '0',
-			'source_type'       => 'deskpro_magento\\Usersource\\Adapter\\Magento'
-		), array('app_id' => $context->getApp()->id));
-	}
-
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function updatePackage(InstallerContext $context)
-	{
-		// Nothing
+		$em->persist($app);
+		$em->persist($us);
+		$em->flush();
 	}
 }
