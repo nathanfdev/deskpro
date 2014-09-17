06c39a641d949e0972906647b5a5a150836dd4cf
Chris Tickner <chris.tickner@deskpro.com>
2014-09-17 00:24:33 -0400

    [Auth] update facebook

--- app/apps/deskpro_us_facebook/native/InstallerHandler.php
+++ app/apps/deskpro_us_facebook/native/InstallerHandler.php
@@ -34,64 +34,54 @@
 
 namespace deskpro_us_facebook;
 
-use Application\DeskPRO\App\Native\InstallerHandler\InstallerContext;
 use Application\DeskPRO\App\Native\InstallerHandler\AbstractInstallerHandler;
+use Application\DeskPRO\App\Native\InstallerHandler\InstallerContext;
+use Application\DeskPRO\Entity\AppInstance;
+use Application\DeskPRO\Entity\Usersource;
+use Doctrine\ORM\EntityManager;
 
 class InstallerHandler extends AbstractInstallerHandler
 {
+	protected function appToUsersource(AppInstance $app, Usersource $us, EntityManager $em)
+	{
+		$us->options           = array(
+			'app_key'    => $app->getSetting('app_key'),
+			'app_secret' => $app->getSetting('app_secret'),
+		);
+		$us->lost_password_url = $app->getSetting('lost_pwd_url') ? : '';
+		$us->title             = $app->title;
+		$us->is_enabled        = $app->getSetting('enable_usersource') ? 1 : 0;
+		$us->source_type       = 'Application\\DeskPRO\\Usersource\\Adapter\\Facebook';
+
+		$em->persist($app);
+		$em->persist($us);
+		$em->flush();
+	}
+
 	/**
 	 * {@inheritDoc}
 	 */
 	public function install(InstallerContext $context)
 	{
-		$context->getDb()->insert('usersources', array(
-				'app_id'            => $context->getApp()->id,
-				'title'             => $context->getApp()->title,
-				'type'              => $context->getUsersourceType(),
-				'lost_password_url' => $context->getApp()->getSetting('lost_pwd_url') ? : '',
-				'options'           => json_encode(
-					array(
-						'app_key'    => $context->getApp()->getSetting('app_key'),
-						'app_secret' => $context->getApp()->getSetting('app_secret'),
-					)
-				),
-				'source_type'       => 'Application\\DeskPRO\\Usersource\\Adapter\\Facebook',
-				'is_enabled'        => $context->getApp()->getSetting('enable_usersource') ? 1 : 0,
-			)
-		);
+		$this->appToUsersource($context->getApp(), $context->getUsersource(), $context->getEm());
 	}
-
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function uninstall(InstallerContext $context)
-	{
-		$context->getDb()->delete('usersources', array('app_id' => $context->getApp()->id));
-	}
-
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public function updateSettings(InstallerContext $context)
 	{
-		$context->getDb()->update('usersources', array(
-				'title'             => $context->getApp()->title,
-				'lost_password_url' => $context->getApp()->getSetting('lost_pwd_url'),
-				'options'           => json_encode(
-					array(
-						'app_key'    => $context->getApp()->getSetting('app_key'),
-						'app_secret' => $context->getApp()->getSetting('app_secret'),
-					)
-				),
-				'source_type'       => 'Application\\DeskPRO\\Usersource\\Adapter\\Facebook',
-				'is_enabled' => $context->getApp()->getSetting('enable_usersource') ? 1 : 0,
-			),
-			array('app_id' => $context->getApp()->id)
-		);
+		$this->appToUsersource($context->getApp(), $context->getUsersource(), $context->getEm());
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
+	public function uninstall(InstallerContext $context)
+	{
+		$context->getEm()->remove($context->getUsersource());
+		$context->getEm()->flush();
+	}
 
 	/**
 	 * {@inheritDoc}
