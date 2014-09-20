baf7a59f228e9eb55ee5f006df3bf713258ca82b
n3b <neb802@gmail.com>
2014-09-21 00:24:21 +0400

    fix lang exception

--- app/src/Application/DeskPRO/Translate/Loader/DbLoader.php
+++ app/src/Application/DeskPRO/Translate/Loader/DbLoader.php
@@ -89,7 +89,7 @@ class DbLoader implements LoaderInterface
 		if ($language) {
 			$specific_lang_ids[] = $language->getId();
 		}
-		$langs = array_merge($langs, implode(',', $specific_lang_ids));
+		$langs = array_merge($langs, $specific_lang_ids);
 		$langs = array_unique($langs, \SORT_STRING);
 
 		$lang_in = implode(',', $langs);
