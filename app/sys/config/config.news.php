65fc24031209fdc6d676ffef86239d69b03d79d2
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-09-23 18:27:39 +0100

    [FIX] Agent: 'new interface' news showing to new agents (and new installs)

--- app/sys/config/config.news.php
+++ app/sys/config/config.news.php
@@ -3,5 +3,6 @@
 		'id'    => 459,
 		'title' => 'Agent Interface Improved',
 		'url'   => 'https://support.deskpro.com/news/view/459-agent-interface-improved',
+		'date'  => '2014-08-20 12:00:00'
 	)
 );
