a5eca74f96e2f925bc0c2c94e468027779df99ea
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-09-29 11:04:57 +0100

    [FIX] Missing escalation data from ticket debug file

--- app/src/Application/DeskPRO/Debug/Data/TicketTriggerData.php
+++ app/src/Application/DeskPRO/Debug/Data/TicketTriggerData.php
@@ -56,6 +56,21 @@ class TicketTriggerData implements DataInterface
 		$data = App::getDb()->fetchAll("SELECT * FROM slas ORDER BY id ASC");
 		$ret['slas'] = $data;
 
+		$data = App::getDb()->fetchAll("SELECT * FROM ticket_escalations ORDER BY id ASC");
+		foreach ($data as &$d) {
+			if (!empty($d['terms'])) {
+				$d['terms'] = @json_decode($d['terms'], true);
+			}
+			if (!empty($d['terms_any'])) {
+				$d['terms_any'] = @json_decode($d['terms_any'], true);
+			}
+			if (!empty($d['actions'])) {
+				$d['actions'] = @json_decode($d['actions'], true);
+			}
+		}
+		unset($d);
+		$ret['escalations'] = $data;
+
 		return $ret;
 	}
 }
