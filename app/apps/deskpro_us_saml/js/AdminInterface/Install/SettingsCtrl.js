262453f6e91e83b25c9bdabda6b210098cf661dc
Chris Tickner <chris.tickner@deskpro.com>
2014-09-29 19:06:56 -0400

    [SAML] SAML app cert fingerpint is now optional

--- app/apps/deskpro_us_saml/js/AdminInterface/Install/SettingsCtrl.js
+++ app/apps/deskpro_us_saml/js/AdminInterface/Install/SettingsCtrl.js
@@ -11,7 +11,7 @@ define(['DeskPRO/Util/Strings', 'DeskPRO/Util/Util'], function(Strings, Util) {
 		//# Form validation / errors
 		//##############################################################################################################
 
-		['cert_fingerprint', 'sso_url'].forEach(function (field) {
+		['sso_url'].forEach(function (field) {
 			$scope.$watch('setting_values.' + field, function () {
 				if (touched[field] || touched.always || $scope.errors[field]) {
 					updateFormErrors();
