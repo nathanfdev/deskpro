b5e357e90a7b987f80c95c80a0bd5055a288d3c7
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-10-06 12:30:43 +0100

    Rerun vendors

--- app/bin/hack-vendors.sh
+++ app/bin/hack-vendors.sh
@@ -10,4 +10,4 @@ cp $DIR_VENDOR/zendframework/zendframework/library/Zend/Ldap/Node.php $DIR_VENDO
 cat $DIR_VENDOR/zendframework/zendframework/library/Zend/Ldap/Node.php.orig | php -r 'echo preg_replace('\''/if \(\!Dn\:\:isChildOf\(\$this\-\>_getDn\(\), \$ldap\-\>getBaseDn\(\)\)\) \{.*?\}\s*/s'\'', "/* DESKPRO EDIT: Removed isChildOf check */\n\n        ", stream_get_contents(STDIN));' > $DIR_VENDOR/zendframework/zendframework/library/Zend/Ldap/Node.php
 rm $DIR_VENDOR/zendframework/zendframework/library/Zend/Ldap/Node.php.orig
 
-php DIR_ROOT/app/bin/build/build-vendors-mutate.php
+php $DIR_ROOT/app/bin/build/build-vendors-mutate.php
--- app/vendor/friendsofsymfony/elastica-bundle/Index/AliasProcessor.php
+++ app/vendor/friendsofsymfony/elastica-bundle/Index/AliasProcessor.php
@@ -11,10 +11,12 @@
 
 namespace FOS\ElasticaBundle\Index;
 
+use Elastica\Client;
 use Elastica\Exception\ExceptionInterface;
+use Elastica\Request;
 use FOS\ElasticaBundle\Configuration\IndexConfig;
-use FOS\ElasticaBundle\Elastica\Client;
 use FOS\ElasticaBundle\Elastica\Index;
+use FOS\ElasticaBundle\Exception\AliasIsIndexException;
 
 class AliasProcessor
 {
@@ -33,11 +35,15 @@ class AliasProcessor
      * Switches an index to become the new target for an alias. Only applies for
      * indexes that are set to use aliases.
      *
+     * $force will delete an index encountered where an alias is expected.
+     *
      * @param IndexConfig $indexConfig
      * @param Index $index
+     * @param bool $force
+     * @throws AliasIsIndexException
      * @throws \RuntimeException
      */
-    public function switchIndexAlias(IndexConfig $indexConfig, Index $index)
+    public function switchIndexAlias(IndexConfig $indexConfig, Index $index, $force = false)
     {
         $client = $index->getClient();
 
@@ -45,7 +51,16 @@ class AliasProcessor
         $oldIndexName = false;
         $newIndexName = $index->getName();
 
-        $aliasedIndexes = $this->getAliasedIndexes($client, $aliasName);
+        try {
+            $aliasedIndexes = $this->getAliasedIndexes($client, $aliasName);
+        } catch(AliasIsIndexException $e) {
+            if (!$force) {
+                throw $e;
+            }
+
+            $this->deleteIndex($client, $aliasName);
+            $aliasedIndexes = array();
+        }
 
         if (count($aliasedIndexes) > 1) {
             throw new \RuntimeException(
@@ -59,7 +74,7 @@ class AliasProcessor
         }
 
         $aliasUpdateRequest = array('actions' => array());
-        if (count($aliasedIndexes) == 1) {
+        if (count($aliasedIndexes) === 1) {
             // if the alias is set - add an action to remove it
             $oldIndexName = $aliasedIndexes[0];
             $aliasUpdateRequest['actions'][] = array(
@@ -116,8 +131,10 @@ class AliasProcessor
     /**
      * Returns array of indexes which are mapped to given alias
      *
+     * @param Client $client
      * @param string $aliasName Alias name
      * @return array
+     * @throws AliasIsIndexException
      */
     private function getAliasedIndexes(Client $client, $aliasName)
     {
@@ -125,6 +142,13 @@ class AliasProcessor
         $aliasedIndexes = array();
 
         foreach ($aliasesInfo as $indexName => $indexInfo) {
+            if ($indexName === $aliasName) {
+                throw new AliasIsIndexException($indexName);
+            }
+            if (!isset($indexInfo['aliases'])) {
+                continue;
+            }
+
             $aliases = array_keys($indexInfo['aliases']);
             if (in_array($aliasName, $aliases)) {
                 $aliasedIndexes[] = $indexName;
@@ -132,5 +156,17 @@ class AliasProcessor
         }
 
         return $aliasedIndexes;
+    }
+
+    /**
+     * Delete an index
+     *
+     * @param Client $client
+     * @param string $indexName Index name to delete
+     */
+    private function deleteIndex(Client $client, $indexName)
+    {
+        $path = sprintf("%s", $indexName);
+        $client->request($path, Request::DELETE);
     }
 }
