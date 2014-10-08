1eeca95bc658889165a3f928df5cc97b8f95ac42
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-10-08 16:52:59 +0100

    Improve confusing ui around creating org on new person screen

--- app/languages/default/agent/general.php
+++ app/languages/default/agent/general.php
@@ -247,6 +247,7 @@
 	'agent.general.enter_a_message'                         => 'Enter a message',
 	'agent.general.enter_a_title'                           => 'Enter a title',
 	'agent.general.enter_organization_name'                 => 'Enter an organization name...',
+	'agent.general.find_or_create_org'                      => 'Find or create an organization',
 	'agent.general.enter_search_terms'                      => 'Enter search terms',
 	'agent.general.error'                                   => 'Error',
 	'agent.general.error_invalid_phone_number'              => 'The phone number you entered is not in a valid format',
