a0a5c1cdcc8414a4cfdbb0cdc789bc1eca616ab3
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-10-10 16:06:39 +0100

    Rebuild js

--- web/app/build/Admin/js/CustomFields/User/Ctrl/Edit.js
+++ web/app/build/Admin/js/CustomFields/User/Ctrl/Edit.js
@@ -3,21 +3,21 @@
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   define(['Admin/CustomFields/Base/Ctrl/Edit'], function(Admin_CustomFields_Base_Ctrl_Edit) {
-    var Admin_CustomFields_Chat_Ctrl_Edit;
-    Admin_CustomFields_Chat_Ctrl_Edit = (function(_super) {
-      __extends(Admin_CustomFields_Chat_Ctrl_Edit, _super);
+    var Admin_CustomFields_User_Ctrl_Edit;
+    Admin_CustomFields_User_Ctrl_Edit = (function(_super) {
+      __extends(Admin_CustomFields_User_Ctrl_Edit, _super);
 
-      function Admin_CustomFields_Chat_Ctrl_Edit() {
-        return Admin_CustomFields_Chat_Ctrl_Edit.__super__.constructor.apply(this, arguments);
+      function Admin_CustomFields_User_Ctrl_Edit() {
+        return Admin_CustomFields_User_Ctrl_Edit.__super__.constructor.apply(this, arguments);
       }
 
-      Admin_CustomFields_Chat_Ctrl_Edit.CTRL_ID = 'Admin_CustomFields_User_Ctrl_Edit';
+      Admin_CustomFields_User_Ctrl_Edit.CTRL_ID = 'Admin_CustomFields_User_Ctrl_Edit';
 
-      Admin_CustomFields_Chat_Ctrl_Edit.CTRL_AS = 'EditCtrl';
+      Admin_CustomFields_User_Ctrl_Edit.CTRL_AS = 'EditCtrl';
 
-      Admin_CustomFields_Chat_Ctrl_Edit.DEPS = [];
+      Admin_CustomFields_User_Ctrl_Edit.DEPS = [];
 
-      Admin_CustomFields_Chat_Ctrl_Edit.prototype.initialLoadExtra = function() {
+      Admin_CustomFields_User_Ctrl_Edit.prototype.initialLoadExtra = function() {
         var promise;
         promise = this.Api.sendGet('/apps?tags=usersources').then((function(_this) {
           return function(result) {
@@ -29,18 +29,18 @@
         return promise;
       };
 
-      Admin_CustomFields_Chat_Ctrl_Edit.prototype.getDataService = function() {
+      Admin_CustomFields_User_Ctrl_Edit.prototype.getDataService = function() {
         return this.DataService.get('UserFields');
       };
 
-      Admin_CustomFields_Chat_Ctrl_Edit.prototype.getBaseRouteName = function() {
+      Admin_CustomFields_User_Ctrl_Edit.prototype.getBaseRouteName = function() {
         return "crm.user_fields";
       };
 
-      return Admin_CustomFields_Chat_Ctrl_Edit;
+      return Admin_CustomFields_User_Ctrl_Edit;
 
     })(Admin_CustomFields_Base_Ctrl_Edit);
-    return Admin_CustomFields_Chat_Ctrl_Edit.EXPORT_CTRL();
+    return Admin_CustomFields_User_Ctrl_Edit.EXPORT_CTRL();
   });
 
 }).call(this);
