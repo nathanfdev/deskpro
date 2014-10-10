9a233e822592b01a83054753f6f0bb149ab11a8b
n3b <neb802@gmail.com>
2014-10-10 06:33:38 +0400

    upgrade script

--- app/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
+++ app/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
@@ -1531,7 +1531,12 @@
 
 	1411643955 => array(
 		'file'      => '/src/Application/InstallBundle/Upgrade/Build/2014/09/Build1411643955.php',
-		'classname' => 'Application\InstallBundle\Upgrade\Build\deskpro/www/app/src/Application/InstallBundle/Upgrade/Build/2014/09/Build1411643955'
+		'classname' => 'Application\InstallBundle\Upgrade\Build\Build1411643955'
+	),
+
+	1412908313 => array(
+		'file'      => '/src/Application/InstallBundle/Upgrade/Build/2014/10/Build1412908313.php',
+		'classname' => 'Application\InstallBundle\Upgrade\Build\Users/n3b/projects/deskpro/app/src/Application/InstallBundle/Upgrade/Build/2014/10/Build1412908313'
 	),
 
 );
