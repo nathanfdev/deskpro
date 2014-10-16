c8ffc5b2be7040ae7ec1b63abed5bfb853627354
Chris Tickner <chris.tickner@deskpro.com>
2014-10-16 00:08:28 -0400

    [Settings] create the SettingsBag

---
+++ app/src/Application/DeskPRO/NewSettings/SettingsBag.php
@@ -0,0 +1,137 @@
+<?php
+/**************************************************************************\
+| DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+| a British company located in London, England.                            |
+|                                                                          |
+| All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+|                                                                          |
+| The license agreement under which this software is released              |
+| can be found at http://www.deskpro.com/license                           |
+|                                                                          |
+| By using this software, you acknowledge having read the license          |
+| and agree to be bound thereby.                                           |
+|                                                                          |
+| Please note that DeskPRO is not free software. We release the full       |
+| source code for our software because we trust our users to pay us for    |
+| the huge investment in time and energy that has gone into both creating  |
+| this software and supporting our customers. By providing the source code |
+| we preserve our customers' ability to modify, audit and learn from our   |
+| work. We have been developing DeskPRO since 2001, please help us make it |
+| another decade.                                                          |
+|                                                                          |
+| Like the work you see? Think you could make it better? We are always     |
+| looking for great developers to join us: http://www.deskpro.com/jobs/    |
+|                                                                          |
+| ~ Thanks, Everyone at Team DeskPRO                                       |
+\**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage NewSettings
+ */
+
+namespace Application\DeskPRO\NewSettings;
+
+/**
+ * The SettingsBag acts like an immutable array, and also offers an API with methods like has('key') and get('key', 'default').
+ *
+ */
+class SettingsBag implements \ArrayAccess, \IteratorAggregate, \Countable, \Serializable
+{
+	/**
+	 * @var array
+	 */
+	private $settings;
+
+
+	public function __construct(array $settings = array())
+	{
+		$this->settings = $settings;
+	}
+
+	public function has($key)
+	{
+		return array_key_exists($key, $this->settings);
+	}
+
+	public function get($key, $default = null)
+	{
+		return $this->has($key) ? $this->settings[$key] : $default;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getIterator()
+	{
+		return new \ArrayIterator($this->settings);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function offsetExists($offset)
+	{
+		return $this->has($offset);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function offsetGet($offset)
+	{
+		return $this->get($offset);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function offsetSet($offset, $value)
+	{
+		throw new \LogicException('cannot set a setting in this way. instead, change the underlying source of the setting and get a fresh settings bag by forcing a reload of settings on the settings resolver');
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function offsetUnset($offset)
+	{
+		throw new \LogicException(
+			'cannot unset a setting in this way. instead, remove from the underlying source of the setting and get a fresh settings bag by forcing a reload of settings on the settings resolver'
+		);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function serialize()
+	{
+		return serialize($this->settings);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function unserialize($serialized)
+	{
+		return unserialize($serialized);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function count()
+	{
+		return count($this->settings);
+	}
+}
+ 
---
+++ app/testing/tests/unit/DeskPRO/NewSettings/SettingsBagTest.php
@@ -0,0 +1,79 @@
+<?php
+/**************************************************************************\
+| DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+| a British company located in London, England.                            |
+|                                                                          |
+| All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+|                                                                          |
+| The license agreement under which this software is released              |
+| can be found at http://www.deskpro.com/license                           |
+|                                                                          |
+| By using this software, you acknowledge having read the license          |
+| and agree to be bound thereby.                                           |
+|                                                                          |
+| Please note that DeskPRO is not free software. We release the full       |
+| source code for our software because we trust our users to pay us for    |
+| the huge investment in time and energy that has gone into both creating  |
+| this software and supporting our customers. By providing the source code |
+| we preserve our customers' ability to modify, audit and learn from our   |
+| work. We have been developing DeskPRO since 2001, please help us make it |
+| another decade.                                                          |
+|                                                                          |
+| Like the work you see? Think you could make it better? We are always     |
+| looking for great developers to join us: http://www.deskpro.com/jobs/    |
+|                                                                          |
+| ~ Thanks, Everyone at Team DeskPRO                                       |
+\**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage NewSettings
+ */
+
+namespace DpUnitTests\DeskPRO\NewSettings;
+
+
+use Application\DeskPRO\NewSettings\SettingsBag;
+
+class SettingsBagTest extends \DpUnitTestCase
+{
+	public function testIsLikeAnArrayObject()
+	{
+		$bag = new SettingsBag();
+
+		$this->assertInstanceOf('\ArrayAccess', $bag);
+		$this->assertInstanceOf('\IteratorAggregate', $bag);
+		$this->assertInstanceOf('\Countable', $bag);
+		$this->assertInstanceOf('\Serializable', $bag);
+	}
+
+	public function testEverythingAtOnceBecauseThisIsTrivial()
+	{
+		$bag = new SettingsBag(array(
+			'key' => 'value',
+			'setting' => 2,
+			'extra_setting' => 0.9
+		));
+
+		$this->assertEquals('value', $bag->get('key'));
+		$this->assertEquals(2, $bag->get('setting'));
+		$this->assertEquals(0.9, $bag->get('extra_setting'));
+
+		$this->assertEquals(null, $bag->get('does_not_exist'));
+		$this->assertEquals('default_works', $bag->get('does_not_exist', 'default_works'));
+
+		$this->assertEquals('value', $bag['key']);
+		$this->assertEquals(2, $bag['setting']);
+		$this->assertEquals(0.9, $bag['extra_setting']);
+
+		$this->assertSame(3, $bag->count());
+		$this->assertSame(3, count($bag));
+
+		$this->assertTrue($bag->has('key'));
+		$this->assertFalse($bag->has('non_existant-key'));
+
+	}
+}
+ 
