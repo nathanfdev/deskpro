b644ece0b53da7b2c4f3fc81d51a55a01450b3de
Chris Tickner <chris.tickner@deskpro.com>
2014-10-15 23:36:27 -0400

    [Settings] create SettingsResolver and getGlobalSettings

---
+++ app/testing/tests/unit/DeskPRO/NewSettings/SettingsResolverTest.php
@@ -0,0 +1,62 @@
+<?php
+/**************************************************************************\
+| DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+| a British company located in London, England.                            |
+|                                                                          |
+| All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+|                                                                          |
+| The license agreement under which this software is released              |
+| can be found at http://www.deskpro.com/license                           |
+|                                                                          |
+| By using this software, you acknowledge having read the license          |
+| and agree to be bound thereby.                                           |
+|                                                                          |
+| Please note that DeskPRO is not free software. We release the full       |
+| source code for our software because we trust our users to pay us for    |
+| the huge investment in time and energy that has gone into both creating  |
+| this software and supporting our customers. By providing the source code |
+| we preserve our customers' ability to modify, audit and learn from our   |
+| work. We have been developing DeskPRO since 2001, please help us make it |
+| another decade.                                                          |
+|                                                                          |
+| Like the work you see? Think you could make it better? We are always     |
+| looking for great developers to join us: http://www.deskpro.com/jobs/    |
+|                                                                          |
+| ~ Thanks, Everyone at Team DeskPRO                                       |
+\**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage NewSettings
+ */
+
+namespace DpUnitTests\DeskPRO\NewSettings;
+
+
+use Application\DeskPRO\NewSettings\SettingsResolver;
+
+class SettingsResolverTest extends \DpUnitTestCase
+{
+	public function testConstructedWithLoaders()
+	{
+		$mock1 = \Mockery::mock('Application\DeskPRO\NewSettings\SettingsLoaderInterface');
+		$mock2 = \Mockery::mock('Application\DeskPRO\NewSettings\SettingsLoaderInterface');
+
+		$loaders = array($mock1, $mock2);
+
+		$resolver = new SettingsResolver($loaders);
+
+		$this->assertSame($loaders, $resolver->getLoaders(), 'stored with the same order');
+	}
+
+
+	public function testGlobalSettingsIsASettingsBag()
+	{
+		$resolver = new SettingsResolver(array());
+
+		$this->assertInstanceOf('Application\DeskPRO\NewSettings\SettingsBag', $resolver->getGlobalSettings());
+	}
+}
+ 
