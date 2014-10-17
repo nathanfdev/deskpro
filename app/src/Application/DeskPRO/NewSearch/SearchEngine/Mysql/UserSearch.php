b1a5a28982527b7973c58ec7ef44459dcd63084e
n3b <neb802@gmail.com>
2014-10-17 21:24:32 +0400

    copy UserSearch source from develop

--- app/src/Application/DeskPRO/NewSearch/SearchEngine/Mysql/UserSearch.php
+++ app/src/Application/DeskPRO/NewSearch/SearchEngine/Mysql/UserSearch.php
@@ -29,10 +29,8 @@ namespace Application\DeskPRO\NewSearch\SearchEngine\Mysql;
 
 use Application\DeskPRO\DBAL\Connection;
 use Application\DeskPRO\NewSearch\SearchEngine\Result\ResultSet;
-use Application\DeskPRO\NewSearch\SearchEngine\SearchContext;
 use Application\DeskPRO\NewSearch\SearchEngine\SearchContextInterface;
 use Application\DeskPRO\NewSearch\SearchEngine\UserSearchInterface;
-use Doctrine\DBAL\Query\QueryBuilder;
 use Elastica\Filter;
 use Elastica\Query;
 use Orb\Util\Arrays;
@@ -101,37 +99,53 @@ class UserSearch implements UserSearchInterface
 			return new ResultSet();
 		}
 
-		$qb = $this->db->createQueryBuilder()
-			->from('content_search', 'cs')
-			->where('cs.object_type IN (:types)')
-			->setParameter(':types', $types, Connection::PARAM_STR_ARRAY);
-
-		foreach ($query_words as $k => $w) {
+		$params = array();
+		$likes = array();
+		foreach ($query_words as $w) {
 			if (strlen($w) <= 2) {
 				continue;
 			}
 
-			$key = ':where' . $k;
-			$val = '%' . str_replace(array('%', '_', '\\'), array('\\%', '\\_', '\\\\'), $w) . '%';
-			$qb
-				->andWhere('cs.content LIKE ' . $key)
-				->setParameter($key, $val);
+			$likes[] = "content_search.content LIKE ?";
+			$params[] = '%' . str_replace(array('%', '_', '\\'), array('\\%', '\\_', '\\\\'), $w) . '%';
 		}
-
-		// the first parameter is :types
-		if (count($qb->getParameters()) > 1) {
+		if ($likes) {
+			$where = "
+				content_search.object_type IN ($limit_types)
+				AND (" . implode(' OR ', $likes) . ")
+			";
 
 			if (!$ignore_perms) {
-				$this->buildPermsQuery($context, $qb, $types);
+				$perm_join  = $context_params['join'];
+				$perm_where = $context_params['where'];
+				if (!$perm_where) {
+					$perm_where = '1';
+				}
+			} else {
+				$perm_join = '';
+				$perm_where = '1';
 			}
 
-			$per_page && $qb->setMaxResults($per_page);
-			$total = $qb->select('COUNT(*)')->execute()->fetchColumn();
+			$count_query = "
+				SELECT COUNT(*)
+				FROM content_search
+				$perm_join
+				WHERE $perm_where AND $where
+				LIMIT $per_page
+			";
 
-			$page > 0 && $per_page && $qb->setFirstResult(($page - 1) * $per_page);
-			$qb->orderBy('cs.object_id', 'DESC');
-			$results = $qb->select('cs.object_type, cs.object_id')->execute()->fetchAll();
+			$start = ($page - 1) * $per_page;
+			$select_query = "
+				SELECT content_search.object_type, content_search.object_id
+				FROM content_search
+				$perm_join
+				WHERE $perm_where AND $where
+				ORDER BY content_search.object_id DESC
+				LIMIT $start, $per_page
+			";
 
+			$total = $this->db->fetchColumn($count_query, $params);
+			$results  = $this->db->fetchAll($select_query, $params);
 		} else {
 			$total       = 0;
 			$results     = array();
@@ -153,34 +167,6 @@ class UserSearch implements UserSearchInterface
 		$objects = $this->transformer->transform($results);
 
 		return new ResultSet($objects, $total);
-	}
-
-	/**
-	 * add permissions part to query
-	 * @param SearchContext $context
-	 * @param QueryBuilder $qb
-	 * @param array $types
-	 */
-	protected function buildPermsQuery(SearchContext $context, QueryBuilder $qb, array $types)
-	{
-		$qb->join('cs', 'content_search_attribute', 'csa', '
-			csa.object_type = cs.object_type
-			AND csa.object_id = cs.object_id
-			AND csa.attribute_id LIKE "category_id%"
-		');
-
-		$joinWhere = array();
-		foreach ($types as $type) {
-			$method = 'get' . ucfirst($type) . 'CategoryIds';
-
-			if (!is_callable(array($context, $method))) continue;
-			if (!$allowed = $context->$method()) continue;
-
-			$joinWhere[] = "(csa.object_type = '$type' AND csa.content IN (:allowed_$type))";
-			$qb->setParameter('allowed_' . $type, $allowed, Connection::PARAM_INT_ARRAY);
-		}
-
-		$qb->andWhere('(' . implode(' OR ', $joinWhere) . ')');
 	}
 
 	private function getTicketResults(SearchContextInterface $context, array $query_words)
