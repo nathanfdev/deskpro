951a808049e12717d9b8de53cf5fd6f803a5b796
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-10-27 17:41:50 +0000

    Prevent errors when bad URL in a template is used

--- app/sys/config/config.php
+++ app/sys/config/config.php
@@ -38,6 +38,7 @@ $container->setParameter('templating.engine.twig.class', 'Application\\DeskPRO\\
 $container->setParameter('debug.templating.engine.twig.class', 'Application\\DeskPRO\\Twig\\TwigEngine');
 $container->setParameter('twig.cache_warmer.class', 'Application\\DeskPRO\\Twig\\CacheWarmer\\TemplateCacheCacheWarmer');
 $container->setParameter('twig.extension.trans.class', 'Application\\DeskPRO\\Twig\\Extension\\TranslationExtension');
+$container->setParameter('twig.extension.routing.class', 'Application\\DeskPRO\\Twig\\Extension\\RoutingExtension');
 $container->setParameter('templating.engine.delegating.class', 'Application\\DeskPRO\\Templating\\Engine');
 $container->setParameter('form.type_extension.csrf.enabled', false);
 
