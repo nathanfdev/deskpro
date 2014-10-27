83292848c57152289f232f9138606cd2d32cc7b1
ImmRanneft <registerden@ranneft.ru>
2014-10-28 01:42:12 +0300

    ticket escalations, macroses, layouts and fields controller commented

--- app/src/Application/ApiBundle/Controller/TicketLayoutsController.php
+++ app/src/Application/ApiBundle/Controller/TicketLayoutsController.php
@@ -210,14 +210,14 @@ class TicketLayoutsController extends AbstractController implements ProtectedCon
      *				description="Layout user",
      *				paramType="query",
      *				required=false,
-     *				type="array",
+     *				type="string",
      *			),
      *          @SWG\Parameter(
      *				name="layout[agent]",
      *				description="Layout agent",
      *				paramType="query",
      *				required=false,
-     *				type="array",
+     *				type="string",
      *			),
      *      )
      *  )
