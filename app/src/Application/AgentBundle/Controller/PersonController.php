e7019107f54eaf562e7a45a1e1259a96bb8041df
n3b <neb802@gmail.com>
2014-10-27 17:14:21 +0300

    added rendered value for custom definitions

--- app/src/Application/AgentBundle/Controller/PersonController.php
+++ app/src/Application/AgentBundle/Controller/PersonController.php
@@ -761,19 +761,19 @@ class PersonController extends AbstractController
 
 		// specific user custom fields definitions
 		$manager = $this->container->getCustomFieldManager();
-		$form = $manager->createDefinitionsFormForContext($person);
+		$custom_fields_definitions = $manager->createDefinitionsFormForContext($person);
 		// fix: jquery removes empty arrays from post request
-		if (!$request->request->has($form->getName())) {
-			$request->request->set($form->getName(), array());
+		if (!$request->request->has($custom_fields_definitions->getName())) {
+			$request->request->set($custom_fields_definitions->getName(), array());
 		}
-		if (!$form->handleRequest($request)->isValid()) {
+		if (!$custom_fields_definitions->handleRequest($request)->isValid()) {
 			return $this->createJsonResponse(array(
 				'error' => true,
-				'invalid_custom_fields' => $form->getErrors(true, true)->current(),
+				'invalid_custom_fields' => $custom_fields_definitions->getErrors(true, true)->current(),
 			));
 
 		}
-		$manager->flush($form);
+		$manager->flush($custom_fields_definitions);
 
 
 
@@ -797,6 +797,7 @@ class PersonController extends AbstractController
 				'timezone_options' => $timezone_options,
 				'person' => $person,
 				'custom_fields' => $custom_fields,
+				'custom_fields_definitions' => $custom_fields_definitions->createView(),
 			))
 		));
 	}
