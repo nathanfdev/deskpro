fd8a8329cc674cfc8fa112a1d4f90510b1c730dc
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-10-27 11:25:13 +0000

    Fix name conflict

--- app/src/Application/DeskPRO/Markdown.php
+++ app/src/Application/DeskPRO/Markdown.php
@@ -34,7 +34,7 @@
 
 namespace Application\DeskPRO;
 use Orb\Util\Strings;
-use Orb\Util\Util;
+use Orb\Util\Util as OrbUtil;
 
 /**
  * Transform markdown-formatted text into html
@@ -67,7 +67,7 @@ class Markdown extends \Markdown_Parser
 		$m = null;
 		if (preg_match_all('#!\[attach(.*?)\]#', $text, $m)) {
 			foreach ($m[0] as $match) {
-				$token = ":attach-token-" . md5(microtime() . Util::requestUniqueId()) . ":";
+				$token = ":attach-token-" . md5(microtime() . OrbUtil::requestUniqueId()) . ":";
 				$this->attach_tokens[$token] = $match;
 			}
 		}
