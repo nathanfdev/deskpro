2ac5258f45b3a73f9dcb4d8384fc2bc29b633e7c
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-11-05 09:20:05 +0000

    Installer broken due to entity listeners on people

--- app/src/Application/DeskPRO/Entity/Person.php
+++ app/src/Application/DeskPRO/Entity/Person.php
@@ -2746,12 +2746,14 @@ class Person extends DomainObject implements HighlightableModelInterface
 		$metadata->addLifecycleCallback('_savePersonLogs', 'postPersist');
 		$metadata->addLifecycleCallback('_savePersonLogs', 'postUpdate');
 
-		foreach (array(Events::prePersist, Events::postPersist, Events::preUpdate, Events::postUpdate) as $event) {
-			$metadata->addEntityListener(
-				$event,
-				'Application\DeskPRO\Entity\EventListener\PersonChangeLogListener',
-				'on' . ucfirst($event)
-			);
+		if (defined('DP_INTERFACE') && DP_INTERFACE != 'install') {
+			foreach (array(Events::prePersist, Events::postPersist, Events::preUpdate, Events::postUpdate) as $event) {
+				$metadata->addEntityListener(
+					$event,
+					'Application\DeskPRO\Entity\EventListener\PersonChangeLogListener',
+					'on' . ucfirst($event)
+				);
+			}
 		}
 
 		$metadata->mapField(array( 'fieldName' => 'id', 'type' => 'integer', 'precision' => 0, 'scale' => 0, 'nullable' => false, 'columnName' => 'id', 'id' => true, ));
