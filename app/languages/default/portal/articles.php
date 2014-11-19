2ba6192a3a140fb050320a1e2868e19180b4b104
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-11-19 13:26:09 +0000

    Add section title to kb homepage

---
+++ app/languages/default/portal/articles.php
@@ -0,0 +1,4 @@
+<?php return array(
+    'portal.articles.section-title' => 'Knowledgebase',
+    'portal.articles.section-desc'  => 'Our knowledgebase contains articles written by our support team to help you answer your questions.',
+);
