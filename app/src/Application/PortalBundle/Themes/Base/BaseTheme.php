34855a74d21aa59949f82ffb7887b55990424668
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-11-19 17:53:19 +0000

    Add a few sidebar templates

--- app/src/Application/PortalBundle/Themes/Base/BaseTheme.php
+++ app/src/Application/PortalBundle/Themes/Base/BaseTheme.php
@@ -64,6 +64,7 @@ class BaseTheme extends AbstractTheme
 
             new Tag('news_posts',               'Theme:News:list',                  array('style' => 'posts')),
             new Tag('news_posts_list',          'Theme:News:list',                  array('style' => 'small')),
+            new Tag('news_date_list',           'Theme:Dev:render',                 array('tpl' => 'Theme:News:news_date_list.html.twig')),
 
             new Tag('news_cats_list',           'Theme:News:cats',                  array('style' => 'small')),
             new Tag('news_cats_dropdown',       'Theme:News:cats',                  array('style' => 'dropdown')),
@@ -76,6 +77,9 @@ class BaseTheme extends AbstractTheme
 
             new Tag('feedback_items',           'Theme:Feedback:list',              array('style' => 'items')),
             new Tag('feedback_list_small',      'Theme:Feedback:list',              array('style' => 'small')),
+
+            new Tag('statisfaction_stats_list', 'Theme:Dev:render',                 array('tpl' => 'Theme:Portal:satisfaction_stats_list.html.twig')),
+            new Tag('agents_online_list',       'Theme:Dev:render',                 array('tpl' => 'Theme:Portal:agents_online_list.html.twig')),
         );
     }
 
