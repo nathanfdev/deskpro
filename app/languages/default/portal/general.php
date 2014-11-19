afdb20b899e3530f97aa0f3762cfefc16a7ba64c
Christopher Nadeau <chris.nadeau@deskpro.com>
2014-11-19 14:14:09 +0000

    Update css, add view article page

--- app/languages/default/portal/general.php
+++ app/languages/default/portal/general.php
@@ -1,3 +1,13 @@
 <?php return array(
     'portal.general.label-sort' => 'Sort',
+    'portal.general.comments-count' => '1 Comment|{{count}} Comments',
+    'portal.general.comments-title' => 'Comments (1)|Comments ({{count}})',
+    'portal.general.rate-page' => 'Rate the quality of this page',
+    'portal.general.rate-page-positive' => 'This page was helpful',
+    'portal.general.rate-page-negative' => 'This page was not helpful',
+    'portal.general.btn-subscribe' => 'Subscribe',
+    'portal.general.btn-email' => 'Email',
+    'portal.general.btn-print' => 'Print',
+    'portal.general.btn-download-pdf' => 'Download PDF',
+    'portal.general.related-pages' => 'Related Pages',
 );
