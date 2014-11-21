c311e66ec594c0b26d216e560ca877b28480c33d
Chris Tickner <chris.tickner@deskpro.com>
2014-11-21 17:27:06 -0500

    [Auth] fixes typos

--- app/src/Application/AuthBundle/Permissions/PermissionsBag.php
+++ app/src/Application/AuthBundle/Permissions/PermissionsBag.php
@@ -29,21 +29,22 @@
  * DeskPRO
  *
  * @package    DeskPRO
- * @subpackage NewSettings
+ * @subpackage Permissions
  */
 
 namespace Application\AuthBundle\Permissions;
 
 /**
- * The SettingsBag acts like an immutable array, and also offers an API with methods like has('key') and get('key', 'default').
+ * The PermissionsBag acts like an immutable array, and also offers an API with methods like has('key') and get('key', 'default').
  *
+ * Using the hasPermission(name) method returns a bool if the bag has that permission and it is turned on, or not
  */
 class PermissionsBag implements \ArrayAccess, \IteratorAggregate, \Countable, \Serializable
 {
     /**
      * @var array
      */
-    private $settings;
+    private $permissions;
 
 
     public function __construct(array $settings = array())
@@ -64,22 +65,22 @@ class PermissionsBag implements \ArrayAccess, \IteratorAggregate, \Countable, \S
 
     public function toArray()
     {
-        return $this->settings;
+        return $this->permissions;
     }
 
-    public function setArray(array $settings)
+    public function setArray(array $permissions)
     {
-        $this->settings = $settings;
+        $this->permissions = $permissions;
     }
 
     public function has($key)
     {
-        return array_key_exists($key, $this->settings);
+        return array_key_exists($key, $this->permissions);
     }
 
     public function get($key, $default = null)
     {
-        return $this->has($key) ? $this->settings[$key] : $default;
+        return $this->has($key) ? $this->permissions[$key] : $default;
     }
 
     /**
@@ -87,7 +88,7 @@ class PermissionsBag implements \ArrayAccess, \IteratorAggregate, \Countable, \S
      */
     public function getIterator()
     {
-        return new \ArrayIterator($this->settings);
+        return new \ArrayIterator($this->permissions);
     }
 
 
@@ -114,7 +115,7 @@ class PermissionsBag implements \ArrayAccess, \IteratorAggregate, \Countable, \S
      */
     public function offsetSet($offset, $value)
     {
-        throw new \LogicException('cannot set a setting in this way. instead, change the underlying source of the setting and get a fresh settings bag by forcing a reload of settings on the settings resolver');
+        throw new \LogicException('cannot set a permission in this way. instead, change the underlying entities in the permission system. this is just a dumb bag of data.');
     }
 
 
@@ -124,7 +125,7 @@ class PermissionsBag implements \ArrayAccess, \IteratorAggregate, \Countable, \S
     public function offsetUnset($offset)
     {
         throw new \LogicException(
-            'cannot unset a setting in this way. instead, remove from the underlying source of the setting and get a fresh settings bag by forcing a reload of settings on the settings resolver'
+            'cannot set a permission in this way. instead, change the underlying entities in the permission system. this is just a dumb bag of data.'
         );
     }
 
@@ -134,7 +135,7 @@ class PermissionsBag implements \ArrayAccess, \IteratorAggregate, \Countable, \S
      */
     public function serialize()
     {
-        return serialize($this->settings);
+        return serialize($this->permissions);
     }
 
 
@@ -152,41 +153,6 @@ class PermissionsBag implements \ArrayAccess, \IteratorAggregate, \Countable, \S
      */
     public function count()
     {
-        return count($this->settings);
-    }
-
-
-    /**
-     * Seperating setting names by dots "." is popular. We can use grouping to subset a settings bag and get the
-     * result array of settings that fit inside that subset.
-     *
-     * eg. new SettingsBag(array('core.register' => 1));
-     *
-     * This method with the input $group = 'core' will return:
-     *
-     * array('register' => 1)
-     *
-     * Set the "short" flag to false to not cut off the group part of the setting name, getting:
-     *
-     * array('core.register' => 1)
-     *
-     * @param  string $group the group prefix
-     * @param  bool   $short true to cut the group name out of the result array keys
-     * @return array
-     */
-    public function getGroup($group, $short = true)
-    {
-        $ret = array();
-
-        $group_dot = $group.'.';
-        $len = strlen($group_dot);
-
-        foreach ($this->settings as $k => $v) {
-            if (substr($k, 0, $len) === $group_dot) {
-                $ret[$short ? substr($k, $len) : $k] = $v;
-            }
-        }
-
-        return $ret;
+        return count($this->permissions);
     }
 }
