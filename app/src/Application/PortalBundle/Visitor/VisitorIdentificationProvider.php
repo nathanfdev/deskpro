a2d72546f600109594ba8e0f08cd831f51a9f70b
Chris Tickner <chris.tickner@deskpro.com>
2014-12-09 22:42:20 -0500

    [Portal] update visitor ID algorithm

--- app/src/Application/PortalBundle/Visitor/VisitorIdentificationProvider.php
+++ app/src/Application/PortalBundle/Visitor/VisitorIdentificationProvider.php
@@ -60,7 +60,18 @@ class VisitorIdentificationProvider
 
     public static function generateRandomIdentifier()
     {
-        return uniqid(time().'-');
+        // take from a comment at: http://php.net/manual/en/function.com-create-guid.php
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid =
+            ''
+            . substr($charid, 0, 8) . $hyphen
+            . substr($charid, 8, 4) . $hyphen
+            . substr($charid, 12, 4) . $hyphen
+            . substr($charid, 16, 4) . $hyphen
+            . substr($charid, 20, 12)
+            ;
+        return $uuid;
     }
 
     public function getVisitorIdentifier()
