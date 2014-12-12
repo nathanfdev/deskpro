1ee2b4644d54d89caa8f8dc0681bfd9d486c0951
Chris Tickner <chris.tickner@deskpro.com>
2014-12-12 03:29:13 -0500

    [Portal] add ProfileVoter

---
+++ app/src/Application/AuthBundle/Voter/Portal/ProfileVoter.php
@@ -0,0 +1,61 @@
+<?php
+/**************************************************************************\
+| DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+| a British company located in London, England.                            |
+|                                                                          |
+| All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+|                                                                          |
+| The license agreement under which this software is released              |
+| can be found at http://www.deskpro.com/license                           |
+|                                                                          |
+| By using this software, you acknowledge having read the license          |
+| and agree to be bound thereby.                                           |
+|                                                                          |
+| Please note that DeskPRO is not free software. We release the full       |
+| source code for our software because we trust our users to pay us for    |
+| the huge investment in time and energy that has gone into both creating  |
+| this software and supporting our customers. By providing the source code |
+| we preserve our customers' ability to modify, audit and learn from our   |
+| work. We have been developing DeskPRO since 2001, please help us make it |
+| another decade.                                                          |
+|                                                                          |
+| Like the work you see? Think you could make it better? We are always     |
+| looking for great developers to join us: http://www.deskpro.com/jobs/    |
+|                                                                          |
+| ~ Thanks, Everyone at Team DeskPRO                                       |
+\**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage
+ */
+
+namespace Application\AuthBundle\Voter\Portal;
+
+
+
+use Application\AuthBundle\Voter\AbstractVoter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ProfileVoter extends AbstractVoter
+{
+    const EDIT_PROFILE = 'EDIT_PROFILE';
+
+    protected function getSupportedClasses()
+    {
+        return array('Application\\DeskPRO\\Entity\\Person');
+    }
+
+    protected function getSupportedAttributes()
+    {
+        return array(self::EDIT_PROFILE);
+    }
+
+    protected function isGranted($attribute, $object, $user = null)
+    {
+        // any person that is logged in can edit their own profile
+        return $this->isLoggedIn($user) && $object->getId() === $user->getId();
+    }
+}
