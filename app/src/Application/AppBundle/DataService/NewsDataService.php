456aab436eb460fdcc94be9d80bb5f363f84bff7
Chris Tickner <chris.tickner@deskpro.com>
2014-12-17 19:44:28 -0500

    [Portal] news data service

--- app/src/Application/AppBundle/DataService/NewsDataService.php
+++ app/src/Application/AppBundle/DataService/NewsDataService.php
@@ -40,6 +40,7 @@ use Application\DeskPRO\Entity\ArticleCategory;
 use Application\DeskPRO\Entity\DownloadCategory;
 use Application\DeskPRO\Entity\NewsCategory;
 use Application\DeskPRO\Entity\Person;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManager;
 use Pagerfanta\Adapter\DoctrineCollectionAdapter;
 use Pagerfanta\Pagerfanta;
@@ -62,14 +63,83 @@ class NewsDataService
      * @param $max_per_page
      * @return Pagerfanta
      */
-    public function getNewsPager(NewsCategory $category, $page, $max_per_page)
+    public function getNewsPager(NewsCategory $category = null, $page, $max_per_page)
     {
+        // TODO: optimize this query
+        if ($category) {
+            $dls = $category->articles;
+        } else {
+            $dls = new ArrayCollection($this->getNewsRepo()->findAll());
+        }
         // TODO: make sure this collection adapter gets a collection that is EXTRA_LAZY!
-        $pager = new Pagerfanta(new DoctrineCollectionAdapter($category->news));
+        $pager = new Pagerfanta(new DoctrineCollectionAdapter($dls));
         $pager->setMaxPerPage($max_per_page);
         $pager->setCurrentPage($page);
 
         return $pager;
+    }
+
+    /**
+     * Takes null, a category ID, or a NewsCategory and returns an iterable collection of NewsCategories
+     *
+     * Null means ruturn the roots.
+     *
+     * TODO: this is using the doctrine proxy as a method of finding children of the category. Might be able to improve that.
+     *
+     * @param int|null|NewsCategory $category
+     * @return NewsCategory[]
+     * @throws \InvalidArgumentException
+     */
+    public function getCategoryChildren($category)
+    {
+        if (!$category) { // get root categories
+            return $this->getNewsCategoriesRepo()->findBy(array('parent' => null));
+        }
+
+        if (!$category instanceof NewsCategory) { // if not already category, try to make it one
+            if (!$category = $this->getCategory($category)) {
+                throw new \InvalidArgumentException(sprintf('could not convert "%s" into a download category'));
+            }
+        }
+
+        return $category->children;
+    }
+
+    /**
+     * Get a category based on arbirtary input
+     *
+     * TODO: optimize the heck out of any possible inputs here (if it helps: cache in an array at least, cache long term if desired, should normalize cache key on lowest common denominator "id")
+     *
+     * @param int|null|NewsCategory $category
+     * @return NewsCategory|null
+     */
+    public function getCategory($category)
+    {
+        if (!$category) { // we need some input
+            return null;
+        }
+
+        if ($category instanceof NewsCategory) { // already have what you seek
+            return $category;
+        }
+
+        return $this->getNewsCategoriesRepo()->find($category);
+    }
+
+    /**
+     * @return \Application\DeskPRO\EntityRepository\News
+     */
+    public function getNewsRepo()
+    {
+        return $this->em->getRepository('DeskPRO:News');
+    }
+
+    /**
+     * @return \Application\DeskPRO\EntityRepository\NewsCategory
+     */
+    public function getNewsCategoriesRepo()
+    {
+        return $this->em->getRepository('DeskPRO:NewsCategory');
     }
 }
  
