92ab75ee6b912004bb0022b650e7539c27f96202
Chris Tickner <chris.tickner@deskpro.com>
2015-01-07 13:55:43 -0500

    [Portal] enchnacement to TagRequestConverter (preserve other query params)

--- app/src/Application/PortalBundle/Request/TagRequestConverter.php
+++ app/src/Application/PortalBundle/Request/TagRequestConverter.php
@@ -72,7 +72,9 @@ class TagRequestConverter implements ParamConverterInterface
         $tag = $this->brand_stack->getActive()->getTheme()->resolveTag($tag_name);
 
         $current_request = $request;
-        $query = array('tag_options' => array_merge($tag->getDefaultOptions(), $current_request->query->get('tag_options', array())));
+        $tag_options = array_merge($tag->getDefaultOptions(), $current_request->query->get('tag_options', array()));
+        $query = array_merge($current_request->query->all(), array('tag_options' => $tag_options));
+
         $attrs = array_merge($current_request->attributes->all(), array('_tag_name' => $tag_name));
 
         $tag_request = new TagRequest($query, array(), $attrs);
