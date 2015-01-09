1075cd8e6b7f94d7b833f7f43edf0872a08f30c9
Chris Tickner <chris.tickner@gmail.com>
2015-01-09 05:12:30 -0500

    [Portal] profile pictures

--- app/src/Application/FormBundle/Form/Type/PersonEditProfileType.php
+++ app/src/Application/FormBundle/Form/Type/PersonEditProfileType.php
@@ -35,12 +35,15 @@
 namespace Application\FormBundle\Form\Type;
 
 
+use Application\DeskPRO\BlobStorage\DeskproBlobStorage;
+use Application\DeskPRO\ORM\EntityManager;
 use Application\FormBundle\Form\FormFieldManager;
 use Application\LanguageBundle\Language\LanguageManager;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class PersonEditProfileType extends AbstractType
@@ -55,10 +58,21 @@ class PersonEditProfileType extends AbstractType
      */
     private $language_manager;
 
-    public function __construct(FormFieldManager $field_manager, LanguageManager $language_manager)
+    /**
+     * @var DeskproBlobStorage
+     */
+    private $blob_storage;
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    public function __construct(FormFieldManager $field_manager, LanguageManager $language_manager, DeskproBlobStorage $blob_storage, EntityManager $em)
     {
         $this->field_manager = $field_manager;
         $this->language_manager = $language_manager;
+        $this->blob_storage = $blob_storage;
+        $this->em = $em;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -74,16 +88,59 @@ class PersonEditProfileType extends AbstractType
             ));
         }
 
+        $blob_storage = $this->blob_storage;
+        $em = $this->em;
+        $builder->addEventListener(FormEvents::SUBMIT, function(FormEvent $event) use ($blob_storage, $em) {
+            /** @var \Application\DeskPRO\Entity\Person $person */
+            $person = $event->getData();
+            $form = $event->getForm();
+
+            if ($form->has('upload_picture')) {
+                $file = $form->get('upload_picture')->getData();
+                if ($file instanceof File && $file->getRealPath()) {
+                    $blob = $blob_storage->createBlobRecordFromFile(
+                        $file->getRealPath(),
+                        $file->getClientOriginalName(),
+                        $file->getClientMimeType()
+                    );
+
+                    $person->setPictureBlob($blob);
+                    $em->persist($blob);
+                    $form->remove('upload_picture');
+                    $form->add('delete_picture', 'checkbox', array('required' => false, 'mapped' => false));
+                }
+            }
+
+            if ($form->has('delete_picture')) {
+                if ($form->get('delete_picture')->getData()) {
+                    $blob_storage->deleteBlobRecord($person->picture_blob);
+                    $person->setPictureBlob(null);
+                }
+            }
+        });
+
 
         $field_manager = $this->field_manager;
         $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($field_manager) {
+
+            /** @var \Application\DeskPRO\Entity\Person $person */
+            $person = $event->getData();
+            $form = $event->getForm();
+
+            if ($person->picture_blob) {
+                $form->add('delete_picture', 'checkbox', array('required' => false, 'mapped' => false));
+            } else {
+                $form->add('upload_picture', 'file', array('required' => false, 'mapped' => false));
+            }
+
+
             foreach ($field_manager->getAvailablePersonFields() as $field_def) {
                 if (!$field_def->is_enabled) {
                     return false;
                 }
 
                 $id = $field_def->getId();
-                $event->getForm()->add(
+                $form->add(
                     $id,
                     'deskpro_custom_data_person',
                     array(
