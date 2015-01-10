610721c55258f95fa0efb442a7e6dd6fc2a9fb89
Chris Tickner <chris.tickner@gmail.com>
2015-01-10 18:37:43 -0500

    [Portal] create ratings helper

---
+++ app/src/Application/PortalBundle/Helper/PortalRatingsHelper.php
@@ -0,0 +1,153 @@
+<?php
+/**************************************************************************\
+| DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+| a British company located in London, England.                            |
+|                                                                          |
+| All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+|                                                                          |
+| The license agreement under which this software is released              |
+| can be found at http://www.deskpro.com/license                           |
+|                                                                          |
+| By using this software, you acknowledge having read the license          |
+| and agree to be bound thereby.                                           |
+|                                                                          |
+| Please note that DeskPRO is not free software. We release the full       |
+| source code for our software because we trust our users to pay us for    |
+| the huge investment in time and energy that has gone into both creating  |
+| this software and supporting our customers. By providing the source code |
+| we preserve our customers' ability to modify, audit and learn from our   |
+| work. We have been developing DeskPRO since 2001, please help us make it |
+| another decade.                                                          |
+|                                                                          |
+| Like the work you see? Think you could make it better? We are always     |
+| looking for great developers to join us: http://www.deskpro.com/jobs/    |
+|                                                                          |
+| ~ Thanks, Everyone at Team DeskPRO                                       |
+\**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage
+ */
+
+namespace Application\PortalBundle\Helper;
+
+
+use Application\DeskPRO\Entity\ContentAbstract;
+use Application\DeskPRO\Entity\Rating;
+use Application\DeskPRO\Entity\Person;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class PortalRatingsHelper
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function rateContentUp(ContentAbstract $content, $visitor_id, Person $person = null)
+    {
+        $content_rating = $this->getPersistedOrNewRating($content, $visitor_id, $person);
+
+        $content_rating->rateUp();
+
+        $this->em->flush($content_rating);
+
+        return $content_rating;
+    }
+
+    public function rateContentDown(ContentAbstract $content, $visitor_id, Person $person = null)
+    {
+        $content_rating = $this->getPersistedOrNewRating($content, $visitor_id, $person);
+
+        $content_rating->rateDown();
+
+        $this->em->flush($content_rating);
+
+        return $content_rating;
+    }
+
+    public function getPersistedOrNewRating(ContentAbstract $content, $visitor_id, $person)
+    {
+        if ($person && $content_rating = $this->findPersonRating($content, $person)) {
+            return $content_rating;
+        }
+
+        if (!$person && $content_rating = $this->findVisitorRating($content, $visitor_id)) {
+            return $content_rating;
+        }
+
+        $content_rating = new Rating();
+        $content_rating->setContentObject($content);
+        $content_rating->setPerson($person);
+        $content_rating->setVisitorId($visitor_id);
+        $content->addRating($content_rating);
+
+        $this->em->persist($content_rating);
+
+        return $content_rating;
+    }
+
+    public function findPersonRating(ContentAbstract $content, Person $person = null)
+    {
+        if (!$person) {
+            return null;
+        }
+
+        $res = $this->em->createQuery("
+                SELECT r
+                FROM DeskPRO:Rating r
+                WHERE
+                    r.object_type = ?1 AND r.object_id = ?2
+                    AND (r.person = ?3)
+            ")
+            ->setParameter(1, $content->getContentType())
+            ->setParameter(2, $content->getId())
+            ->setParameter(3, $person)
+            ->execute();
+
+        if ($res and count($res)) {
+            return $res[0];
+        }
+
+        return null;
+    }
+
+    public function findVisitorRating(ContentAbstract $content, $visitor_id)
+    {
+        if (!$visitor_id) {
+            return null;
+        }
+
+        $res = $this->em->createQuery("
+                SELECT r
+                FROM DeskPRO:Rating r
+                WHERE
+                    r.object_type = ?1 AND r.object_id = ?2
+                    AND (r.visitor_id = ?3)
+            ")
+            ->setParameter(1, $content->getContentType())
+            ->setParameter(2, $content->getId())
+            ->setParameter(3, $visitor_id)
+            ->execute();
+
+        if ($res and count($res)) {
+            return $res[0];
+        }
+
+        return null;
+    }
+
+    public function getPersonRating(ContentAbstract $content, Person $person = null)
+    {
+        return $this->findPersonRating($content, $person);
+    }
+}
