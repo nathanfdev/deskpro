8c120c39acf265c10eacd948e80a4d8473a11d69
Chris Tickner <chris.tickner@gmail.com>
2015-01-12 16:52:37 -0500

    [Portal] add temp routing to DpKernel in user-routing.php

--- app/src/Application/UserBundle/Resources/config/user-routing.php
+++ app/src/Application/UserBundle/Resources/config/user-routing.php
@@ -7,6 +7,64 @@ use Application\DeskPRO\Routing\RouteCollection;
 
 $collection = new RouteCollection();
 
+
+
+
+
+
+
+
+// for email templates and the like in the DpKernel, we need a temporary place for the new routes. They are REPLICATED
+// here, from the new portal's .yml files.
+
+$collection->create('portal_index', array(
+    'path' => '/'
+));
+$collection->create('portal_news_view', array(
+    'path' => '/news/posts/{slug}'
+));
+$collection->create('portal_news_unsubscribe_all', array(
+    'path' => '/news/posts/subscriptions/unsubscribe'
+));
+$collection->create('portal_downloads_view', array(
+    'path' => '/downloads/files/{slug}'
+));
+$collection->create('portal_downloads_unsubscribe_all', array(
+    'path' => '/downloads/files/subscriptions/unsubscribe'
+));
+$collection->create('portal_kb_view', array(
+    'path' => '/kb/articles/{slug}'
+));
+$collection->create('portal_kb_unsubscribe_all', array(
+    'path' => '/kb/articles/subscriptions/unsubscribe'
+));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $collection->create('user', array(
     'path'        => '/',
     'controller'  => 'UserBundle:Portal:portal',
