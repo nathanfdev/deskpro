aa4d8d7e46c41a37ee7381313477d311ce402c4c
ImmortaL <registerden@ranneft.ru>
2015-01-16 01:59:12 +0300

    Updatd build file

--- app/src/Application/InstallBundle/Upgrade/Build/2015/01/Build1420543838.php
+++ app/src/Application/InstallBundle/Upgrade/Build/2015/01/Build1420543838.php
@@ -34,7 +34,7 @@
 
 namespace Application\InstallBundle\Upgrade\Build;
 
-class Build1417766996 extends AbstractBuild
+class Build1420543838 extends AbstractBuild
 {
     public function run()
     {
