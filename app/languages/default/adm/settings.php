cbb35f9564cda1dd63d55b282d99cf7cbb564616
hendesk <ben.henley@deskpro.com>
2015-01-30 17:35:01 +0000

    Fixed placeholder text for login limit setting

--- app/languages/default/adm/settings.php
+++ app/languages/default/adm/settings.php
@@ -2,6 +2,6 @@
 
     'adm.settings.rate_limit_title'                        => 'Login attempt rate limiting',
     'adm.settings.rate_limit_enabled_title'                => 'Enable login attempt rate limiting',
-    'adm.settings.rate_limit_help'                         => 'Help block',
+    'adm.settings.rate_limit_help'                         => 'Lock accounts after a number of failed login attempts within a short period of time to reduce the risk from automated password guessing attacks.',
 
 );
