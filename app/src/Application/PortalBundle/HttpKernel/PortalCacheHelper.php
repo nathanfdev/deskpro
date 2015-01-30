2ac2a882bd484406939b0d516c28b1f878480306
Chris Tickner <chris.tickner@gmail.com>
2015-01-30 18:57:42 -0500

    [Portal] implement foshttpcache's user_context

---
+++ app/src/Application/PortalBundle/HttpKernel/PortalCacheHelper.php
@@ -0,0 +1,69 @@
+<?php
+/**************************************************************************\
+| DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+| a British company located in London, England.                            |
+|                                                                          |
+| All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+|                                                                          |
+| The license agreement under which this software is released              |
+| can be found at http://www.deskpro.com/license                           |
+|                                                                          |
+| By using this software, you acknowledge having read the license          |
+| and agree to be bound thereby.                                           |
+|                                                                          |
+| Please note that DeskPRO is not free software. We release the full       |
+| source code for our software because we trust our users to pay us for    |
+| the huge investment in time and energy that has gone into both creating  |
+| this software and supporting our customers. By providing the source code |
+| we preserve our customers' ability to modify, audit and learn from our   |
+| work. We have been developing DeskPRO since 2001, please help us make it |
+| another decade.                                                          |
+|                                                                          |
+| Like the work you see? Think you could make it better? We are always     |
+| looking for great developers to join us: http://www.deskpro.com/jobs/    |
+|                                                                          |
+| ~ Thanks, Everyone at Team DeskPRO                                       |
+\**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage
+ */
+
+namespace Application\PortalBundle\HttpKernel;
+
+
+use Application\DeskPRO\PortalBundle\HttpKernel\PortalHttpCache;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class PortalCacheHelper
+{
+    /**
+     * @var RequestStack
+     */
+    private $request_stack;
+
+    public function __construct(RequestStack $request_stack)
+    {
+        $this->request_stack = $request_stack;
+    }
+
+    /**
+     * Is the current request in the request stack a guest request?
+     *
+     * @return bool true if in a guest request
+     */
+    public function isGuestRequest()
+    {
+        $current_request = $this->request_stack->getCurrentRequest();
+        if ($current_request->headers->has(PortalHttpCache::USER_CONTEXT_HASH_HEADER)) {
+            $user_hash = $current_request->headers->get(PortalHttpCache::USER_CONTEXT_HASH_HEADER);
+
+            return in_array($user_hash, array(PortalHttpCache::ANON_HASH, PortalHttpCache::GUEST_HASH));
+        }
+
+        return true; // if no user context header (should not be possible), mark as a guest
+    }
+}
---
+++ app/src/Application/PortalBundle/HttpKernel/PortalUserHashContextProvider.php
@@ -0,0 +1,93 @@
+<?php
+/**************************************************************************\
+| DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+| a British company located in London, England.                            |
+|                                                                          |
+| All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+|                                                                          |
+| The license agreement under which this software is released              |
+| can be found at http://www.deskpro.com/license                           |
+|                                                                          |
+| By using this software, you acknowledge having read the license          |
+| and agree to be bound thereby.                                           |
+|                                                                          |
+| Please note that DeskPRO is not free software. We release the full       |
+| source code for our software because we trust our users to pay us for    |
+| the huge investment in time and energy that has gone into both creating  |
+| this software and supporting our customers. By providing the source code |
+| we preserve our customers' ability to modify, audit and learn from our   |
+| work. We have been developing DeskPRO since 2001, please help us make it |
+| another decade.                                                          |
+|                                                                          |
+| Like the work you see? Think you could make it better? We are always     |
+| looking for great developers to join us: http://www.deskpro.com/jobs/    |
+|                                                                          |
+| ~ Thanks, Everyone at Team DeskPRO                                       |
+\**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage
+ */
+
+namespace Application\PortalBundle\HttpKernel;
+
+use Application\AuthBundle\Permissions\Portal\PortalPermissionsManager;
+use Application\DeskPRO\Entity\Person;
+use FOS\HttpCache\UserContext\ContextProviderInterface;
+use FOS\HttpCache\UserContext\UserContext;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+
+class PortalUserHashContextProvider implements ContextProviderInterface
+{
+    /**
+     * @var TokenStorage
+     */
+    private $token_storage;
+
+    /**
+     * @var PortalPermissionsManager
+     */
+    private $permissions_manager;
+
+    /**
+     * @var AuthorizationChecker
+     */
+    private $auth_checker;
+
+    public function __construct(TokenStorage $token_storage, AuthorizationChecker $auth_checker, PortalPermissionsManager $permissions_manager)
+    {
+        $this->token_storage = $token_storage;
+        $this->permissions_manager = $permissions_manager;
+        $this->auth_checker = $auth_checker;
+    }
+
+    /**
+     * This function is called before generating the hash of a UserContext.
+     *
+     * This allow to add a parameter on UserContext or set the whole array of parameters
+     *
+     * @param UserContext $context
+     */
+    public function updateUserContext(UserContext $context)
+    {
+        $cache_key = null;
+        if ($token = $this->token_storage->getToken()) {
+            $person = $token->getUser();
+            if ($person instanceof Person && $this->auth_checker->isGranted('ROLE_USER')) {
+                $cache_key = $this->permissions_manager->getCacheKeyForPerson($person);
+            }
+        }
+
+        if (!$cache_key) {
+            // not a ROLE_USER, or no Person is token
+            // NOTE: this will get hashed to the x-user-context-hash of self::GUEST_HASH
+            $cache_key = 'guest';
+        }
+
+        $context->addParameter('usergroup_cache_key', $cache_key);
+    }
+}
