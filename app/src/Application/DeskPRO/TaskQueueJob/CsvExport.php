73a03a65ac1f873e66da160bbeeae4475f3f8fe7
n3b <neb802@gmail.com>
2015-02-04 17:29:47 +0800

    contact data and custom fields for export

--- app/src/Application/DeskPRO/TaskQueueJob/CsvExport.php
+++ app/src/Application/DeskPRO/TaskQueueJob/CsvExport.php
@@ -35,7 +35,9 @@
 namespace Application\DeskPRO\TaskQueueJob;
 
 use Application\DeskPRO\App;
+use Application\DeskPRO\Entity\CustomDefPerson;
 use Application\DeskPRO\Entity\Person;
+use Application\DeskPRO\Entity\PersonContactData;
 use Application\DeskPRO\Entity\TmpData;
 
 class CsvExport extends AbstractJob
@@ -51,6 +53,12 @@ class CsvExport extends AbstractJob
             'file' => null,
             'offset' => 0,
             'limit' => 100,
+            'headers' => array(
+                'ID', 'Name', 'Title', 'Primary Email', 'Additional Emails', 'Organization', 'Org Position',
+                'Date Created', 'Date Last Login', 'Usergroup IDs', 'Timezone', 'Labels',
+            ),
+            'contact_headers' => array(),
+            'custom_fields_headers' => array(),
         );
     }
 
@@ -68,9 +76,13 @@ class CsvExport extends AbstractJob
             $file = dp_get_tmp_dir() . '/export/DP-export-' . date('Ymd-His') . '.csv';
         }
 
+        $this->getContactDataHeaders();
+        $this->getCustomFieldsHeaders();
         $fp = fopen($file, 'a');
 
         if (!$this->_data['file']) {
+
+            //select max(cnt) from (select count(person_id) as cnt from people_contact_data group by person_id) as counts
             /**
              *  ID
              *  Name (all three fields)
@@ -81,30 +93,29 @@ class CsvExport extends AbstractJob
              *  Org Position
              *  Dates (date_created, date_last_login)
              *  Usergroup (IDs, comma-separated)
-             *  Contact Information (flatten to one per column, eg "address", "phone", "msn" etc)
-             *  Custom Fields (Flattened to 1 per column)
              *  Timezone
              *  Labels (comma separated)
+             *
+             *  Contact Information (flatten to one per column, eg "address", "phone", "msn" etc)
+             *  Custom Fields (Flattened to 1 per column)
              */
-            fputcsv($fp, array(
-                'ID', 'Name', 'Title', 'Primary Email', 'Additional Emails', 'Organization', 'Org Position',
-                'Date Created', 'Date Last Login', 'Usergroup IDs', 'Contact Information', 'Custom Fields',
-                'Timezone', 'Labels',
-            ), $delimeter, $enclosure);
+            $headers = array_merge($this->_data['headers'], $this->_data['contact_headers'], $this->_data['custom_fields_headers']);
+            fputcsv($fp, $headers, $delimeter, $enclosure);
             $this->_data['file'] = $file;
         }
 
         /** @var \Application\DeskPRO\EntityRepository\Person $rep */
         $rep = App::getOrm()->getRepository('DeskPRO:Person');
 
-        while (microtime(true) - $start_time < $max_time) {
+        while (microtime(true) - $start_time < $max_time * 10000) {
 
             if (!$batch = $rep->findBy(array(), array(), $this->_data['limit'], $this->_data['offset'])) {
                 break;
             }
 
             foreach ($batch as $person) {
-                if (microtime(true) - $start_time >= $max_time) break;
+
+                if (microtime(true) >= 10000 * $max_time + $start_time) break;
                 /** @var $person Person */
 
                 $row = array(
@@ -118,11 +129,12 @@ class CsvExport extends AbstractJob
                     $person['date_created'] ? $person['date_created']->format('Y-m-d H:i:s') : '',
                     $person['date_last_login'] ? $person['date_last_login']->format('Y-m-d H:i:s') : '',
                     implode(',', $person->getUsergroupIds()),
-                    'contact data',
-                    'custom fields',
                     $person->getTimezone(),
                     implode(',', $person->getLabelManager()->getLabelsArray()),
                 );
+
+                $this->fillWithContactData($person, $row);
+                $this->fillCustomFieldsValues($person, $row);
 
                 fputcsv($fp, $row, $delimeter, $enclosure);
                 $this->_data['offset']++;
@@ -154,6 +166,89 @@ class CsvExport extends AbstractJob
             return self::TASK_COMPLETED;
         } else {
             return self::TASK_CONTINUING;
+        }
+    }
+
+    /**
+     * @return mixed
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    protected function getContactDataHeaders()
+    {
+        if ($this->_data['contact_headers']) {
+            return $this->_data['contact_headers'];
+        }
+
+        $ch = array(
+            'address', 'facebook', 'fax', 'instant_message', 'linked_in', 'mobile', 'phone', 'skype', 'twitter', 'website',
+        );
+
+        foreach ($ch as $type) {
+            $q = 'select max(cnt) from (select count(person_id) as cnt from people_contact_data where contact_type = :type group by person_id) as counts';
+            $res = App::getOrm()->getConnection()->executeQuery($q, array('type' => $type))->fetchColumn();
+            if (!$res) continue;
+
+            for ($i = 1; $i <= (int) $res; $i++) {
+                $this->_data['contact_headers'][] = $type;
+            }
+        }
+
+        return $this->_data['contact_headers'];
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getCustomFieldsHeaders()
+    {
+        if ($this->_data['custom_fields_headers']) {
+            return $this->_data['custom_fields_headers'];
+        }
+
+        $field_manager = App::$container->getPersonFieldManager();
+        foreach ($field_manager->getFields() as $def) {
+            /** @var $def CustomDefPerson */
+            $this->_data['custom_fields_headers'][] = $def->getTitle();
+        }
+
+        return $this->_data['custom_fields_headers'];
+    }
+
+    /**
+     * @param Person $person
+     * @param $row
+     */
+    protected function fillWithContactData(Person $person, &$row)
+    {
+        $types = array();
+        foreach ($person->getContactData() as $cd) {
+            /** @var $cd PersonContactData */
+            $data = ('phone' === $cd['contact_type'] || 'mobile' === $cd['contact_type'])
+                ? $cd['field_1'] . $cd['field_2']
+                :$cd->getSearchString();
+
+            $types[$cd['contact_type']][] = $data;
+        }
+
+        foreach ($this->_data['contact_headers'] as $type) {
+            $val = isset($types[$type]) ? array_shift($types[$type]) : false;
+            $row[] = (string) $val;
+        }
+    }
+
+    /**
+     * @param Person $person
+     * @param $row
+     */
+    protected function fillCustomFieldsValues(Person $person, &$row)
+    {
+        $field_manager = App::$container->getPersonFieldManager();
+        $data = $field_manager->getFieldDataForObject($person);
+        foreach ($field_manager->getFields() as $def) {
+            /** @var $def CustomDefPerson */
+            $row[] = isset($data[$def['id']])
+                ? trim($def->getHandler()->renderText($data[$def['id']]))
+                : '';
         }
     }
 }
