7548c8518d16732c0b4d7dd31d8676ef23985e4d
Chris Tickner <chris.tickner@gmail.com>
2015-02-06 11:55:08 -0500

    [Portal] create an "ArbitraryHasher" helper

---
+++ app/src/Application/AppBundle/Helper/ArbitratyHasher.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Application\AppBundle\Helper;
+use Application\DeskPRO\Domain\DomainObject;
+
+/**
+ * This helper generates hashes for data. It can accept an arbitrary set of data, and return the same hash of that data every time it is run.
+ *
+ * Order of the input array is not relevenat.
+ */
+class ArbitratyHasher
+{
+    /**
+     * Will always return a unique string hash of the $input, where $input can be a scalar, an object, or any \Traversable
+     * or array of scalars/objects. The objects must be serializable, else a spl_object_hash is used.
+     *
+     * IMPORTANT: DomainObject instances are treated in a unique way, in that only the ID of the entity is used in the hash.
+     *
+     * This is meant to be used to hash $input's for a single request. The idea is to use this hash as a cache key that
+     * is only in the memory of the current process. This hash should not be trusted for cache's that persist over more than
+     * a single request.
+     *
+     * @param mixed $input
+     * @return string
+     */
+    public function generateHash($input)
+    {
+        $inputs = $this->collectInputs($input);
+
+        if (is_array($inputs)) {
+            sort($inputs);
+        }
+
+        $v=  md5(json_encode($inputs));
+
+        return $v;
+    }
+
+    protected function collectInputs($input)
+    {
+        if (is_scalar($input)) {
+            return $input;
+        }
+
+        if ($input instanceof DomainObject) {
+            return $input->getId();
+        }
+
+        if (is_object($input)) {
+            try {
+                 return serialize($input);
+            } catch (\Exception $e) {
+                  return spl_object_hash($input);
+            }
+
+        }
+
+        if (is_array($input) || $input instanceof \Traversable) {
+            $value = array();
+
+            foreach ($input as $key => $val) {
+                $value[$key] = $this->collectInputs($val);
+            }
+
+            sort($value);
+
+            return $value;
+        }
+
+        return $input;
+    }
+}
---
+++ app/testing/spec/spec/Application/AppBundle/Helper/ArbitratyHasherSpec.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace spec\Application\AppBundle\Helper;
+
+use Application\DeskPRO\Entity\Article;
+use Application\DeskPRO\Entity\BrandSetting;
+use Application\DeskPRO\Entity\News;
+use Application\PortalBundle\Helper\PortalRatingsHelper;
+use Application\PortalBundle\Model\TicketFilter;
+use MyProject\Proxies\__CG__\OtherProject\Proxies\__CG__\stdClass;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ArbitratyHasherSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Application\AppBundle\Helper\ArbitratyHasher');
+    }
+
+    function it_can_hash_a_single_scalar()
+    {
+        $hash = $this->generateHash('hi')->shouldBeString();
+
+        $hash = $this->generateHash('hi');
+        $this->generateHash('hi')->shouldReturn($hash);
+
+        $hash = $this->generateHash(2);
+        $this->generateHash(2)->shouldReturn($hash);
+
+        $hash = $this->generateHash(4.56);
+        $this->generateHash(4.56)->shouldReturn($hash);
+    }
+
+    function it_can_hash_an_array_of_scalars()
+    {
+        $standard_input = array('hi there', 9, 4.78);
+        $different_input = array('hi there!', 9, 4.78);
+        $reorganized_standard_input = array(9, 'hi there', 4.78);
+
+        $hash = $this->generateHash($standard_input)->shouldBeString();
+
+        $hash = $this->generateHash($standard_input);
+        $next_hash = $this->generateHash($standard_input);
+        $hash->shouldEqual($next_hash);
+
+        $hash = $this->generateHash($standard_input);
+        $next_hash = $this->generateHash($reorganized_standard_input);
+        $hash->shouldEqual($next_hash);
+
+        $hash = $this->generateHash($standard_input);
+        $next_hash = $this->generateHash($different_input);
+        $hash->shouldNotEqual($next_hash);
+    }
+
+    function it_can_hash_an_entity(News $news1, News $news2, Article $article)
+    {
+        $news1->getId()->willReturn(1);
+        $news2->getId()->willReturn(3);
+        $article->getId()->willReturn(1);
+
+        $hash = $this->generateHash($news1)->shouldBeString();
+
+        $hash = $this->generateHash($news1);
+        $next_hash = $this->generateHash($news1);
+        $hash->shouldEqual($next_hash);
+
+        $hash = $this->generateHash($article);
+        $next_hash = $this->generateHash($article);
+        $hash->shouldEqual($next_hash);
+
+        $hash = $this->generateHash($news1);
+        $next_hash = $this->generateHash($news2);
+        $hash->shouldNotEqual($next_hash);
+
+        $hash = $this->generateHash($article);
+        $next_hash = $this->generateHash($news2);
+        $hash->shouldNotEqual($next_hash);
+    }
+
+    function it_can_hash_plain_objects_too()
+    {
+        $object1 = new \stdClass();
+        $object2 = new stdClassChild();
+        $object3 = new \stdClass();
+        $object3->foo = 'baz';
+
+        $hash = $this->generateHash($object1)->shouldBeString();
+
+        $hash = $this->generateHash($object1);
+        $next_hash = $this->generateHash($object1);
+        $hash->shouldEqual($next_hash);
+
+        $hash = $this->generateHash($object1);
+        $next_hash = $this->generateHash($object2);
+        $hash->shouldNotEqual($next_hash);
+
+        $hash = $this->generateHash($object1);
+        $next_hash = $this->generateHash($object3);
+        $hash->shouldNotEqual($next_hash);
+    }
+
+    function it_can_hash_a_mixed_array(News $news1, News $news2, Article $article, TicketFilter $filter)
+    {
+        $object1 = new \stdClass();
+
+        $news1->getId()->willReturn(1);
+        $news2->getId()->willReturn(3);
+        $article->getId()->willReturn(1);
+
+        $standard_array = array($object1, $news1, $news2, $article, $filter, 'hi there');
+        $standard_reorganized_array = array($news1, 'hi there', $object1, $filter, $news2, $article);
+        $different_array = array($object1, $news1, $news2, $article, $filter, 'chris tickner');
+
+        $hash = $this->generateHash($standard_array)->shouldBeString();
+        $hash = $this->generateHash($different_array)->shouldBeString();
+
+        $hash = $this->generateHash($standard_array);
+        $next_hash = $this->generateHash($standard_array);
+        $hash->shouldEqual($next_hash);
+
+        $hash = $this->generateHash($standard_array);
+        $next_hash = $this->generateHash($standard_reorganized_array);
+        $hash->shouldEqual($next_hash);
+
+        $hash = $this->generateHash($standard_array);
+        $next_hash = $this->generateHash($different_array);
+        $hash->shouldNotEqual($next_hash);
+    }
+}
+
+class StdClassChild extends \StdClass
+{
+}
