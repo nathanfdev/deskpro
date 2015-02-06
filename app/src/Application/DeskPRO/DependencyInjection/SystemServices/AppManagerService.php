db971e93082956c0957ade067fa1ebf6fe7edff6
Chris Tickner <chris.tickner@gmail.com>
2015-02-06 17:44:39 -0500

    [Portal] comment out two queries in AppManagerService

--- app/src/Application/DeskPRO/DependencyInjection/SystemServices/AppManagerService.php
+++ app/src/Application/DeskPRO/DependencyInjection/SystemServices/AppManagerService.php
@@ -46,19 +46,20 @@ class AppManagerService
         $em = $container->getEm();
 
         if (!defined('DP_BUILDING')) {
-            $packages = $em->createQuery("
-                SELECT package, asset
-                FROM DeskPRO:AppPackage package
-                LEFT JOIN package.assets asset
-                ORDER BY package.title
-            ")->execute();
-
-            $apps = $em->createQuery("
-                SELECT app, package, asset
-                FROM DeskPRO:AppInstance app
-                LEFT JOIN app.package package
-                LEFT JOIN package.assets asset
-            ")->execute();
+            $packages = array();
+            //$packages = $em->createQuery("
+            //    SELECT package, asset
+            //    FROM DeskPRO:AppPackage package
+            //    LEFT JOIN package.assets asset
+            //    ORDER BY package.title
+            //")->execute();
+            $apps = array();
+            //$apps = $em->createQuery("
+            //    SELECT app, package, asset
+            //    FROM DeskPRO:AppInstance app
+            //    LEFT JOIN app.package package
+            //    LEFT JOIN package.assets asset
+            //")->execute();
 
             if (count($apps)) {
                 $names = array_map(function ($a) {
