43d3a926dd3544d272d2aab5817483150c1607ea
Chris Tickner <chris.tickner@gmail.com>
2015-02-06 11:56:34 -0500

    [Portal] create an AbstractDataService with optimization utilities

---
+++ app/src/Application/AppBundle/DataService/AbstractDataService.php
@@ -0,0 +1,71 @@
+<?php
+/**************************************************************************\
+| DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+| a British company located in London, England.                            |
+|                                                                          |
+| All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+|                                                                          |
+| The license agreement under which this software is released              |
+| can be found at http://www.deskpro.com/license                           |
+|                                                                          |
+| By using this software, you acknowledge having read the license          |
+| and agree to be bound thereby.                                           |
+|                                                                          |
+| Please note that DeskPRO is not free software. We release the full       |
+| source code for our software because we trust our users to pay us for    |
+| the huge investment in time and energy that has gone into both creating  |
+| this software and supporting our customers. By providing the source code |
+| we preserve our customers' ability to modify, audit and learn from our   |
+| work. We have been developing DeskPRO since 2001, please help us make it |
+| another decade.                                                          |
+|                                                                          |
+| Like the work you see? Think you could make it better? We are always     |
+| looking for great developers to join us: http://www.deskpro.com/jobs/    |
+|                                                                          |
+| ~ Thanks, Everyone at Team DeskPRO                                       |
+\**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ * @subpackage
+ */
+
+namespace Application\AppBundle\DataService;
+
+use Application\AppBundle\Helper\ArbitratyHasher;
+use Application\DeskPRO\Cache\ConvenientCache;
+
+/**
+ * Provides a simple interface to cache various data requests so that data requests through the DataServices' apis are
+ * only computed ONCE in any given http request.
+ *
+ * Multiple calls to the same method with the same params are retrieved from memory (an array).
+ */
+class AbstractDataService 
+{
+
+    /**
+     * @var ArbitratyHasher|null
+     */
+    protected $hash_generator;
+    /**
+     * @var ConvenientCache
+     */
+    protected $cache;
+
+    protected function generateAndCache($params, $callable)
+    {
+        return $this->cache->get($this->generateHash($params), $callable);
+    }
+
+    protected function generateHash($input)
+    {
+        if (null === $this->hash_generator) {
+            $this->hash_generator = new ArbitratyHasher();
+        }
+
+        return $this->hash_generator->generateHash($input);
+    }
+}
