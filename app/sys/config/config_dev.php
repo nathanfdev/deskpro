0a3ccce1ef141412ef424913afa5e89560414282
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-02-10 09:25:26 +0000

    Fixes to install kernel to make some services available that are used during initial account create

--- app/sys/config/config_dev.php
+++ app/sys/config/config_dev.php
@@ -5,12 +5,3 @@ $container->setParameter('kernel.debug', true);
 $container->loadFromExtension('twig', array(
     'debug' => true
 ));
-
-$container->loadFromExtension('monolog', array(
-    'handlers' => array(
-        'main' => array(
-            'type' => 'stream',
-            'path' => '%kernel.logs_dir%/deskpro.log'
-        )
-    )
-));
