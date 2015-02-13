0064c39e379ea0073308e2252ee04ef769c83c5f
Chris Tickner <chris.tickner@gmail.com>
2015-02-12 22:52:29 -0500

    [Tests] ensure security headers on every response

--- app/tests/spec/Application/AppBundle/EventListener/SecurityHeadersResponseListenerSpec.php
+++ app/tests/spec/Application/AppBundle/EventListener/SecurityHeadersResponseListenerSpec.php
@@ -4,11 +4,46 @@ namespace spec\Application\AppBundle\EventListener;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
+/**
+ * @mixin \Application\AppBundle\EventListener\SecurityHeadersResponseListener
+ */
 class SecurityHeadersResponseListenerSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_is_an_event_subscriber()
     {
-        $this->shouldHaveType('Application\AppBundle\EventListener\SecurityHeadersResponseListener');
+        $this->shouldHaveType('Symfony\Component\EventDispatcher\EventSubscriberInterface');
+    }
+
+    function it_listens_to_kernel_response_events_with_64_priority()
+    {
+        $this->getSubscribedEvents()->shouldBe(
+            array(
+                KernelEvents::RESPONSE => array('onResponse', 64)
+            )
+        );
+    }
+
+    function it_adds_security_headers_to_every_response(
+        FilterResponseEvent $event,
+        Response $response,
+        HeaderBag $headers
+    )
+    {
+        $response->headers = $headers;
+        $event->getResponse()->willReturn($response);
+
+        $headers->add(
+            array(
+                'X-Content-Type-Options' => 'nosniff',
+                'X-FRAME-OPTIONS' => 'DENY'
+            )
+        )->shouldBeCalled();
+
+        $this->onResponse($event);
     }
 }
