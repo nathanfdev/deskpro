3a75f735d5f069105009691a78ca9a8d5b81f03a
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-03-02 10:22:20 +0000

    - Hack libphonenumber to add a few mb_* functions it uses - Improve build script for phone number migrate

--- app/vendor-src/libphonenumber/src/libphonenumber/PhoneNumberUtil.php
+++ app/vendor-src/libphonenumber/src/libphonenumber/PhoneNumberUtil.php
@@ -3201,3 +3201,33 @@ class PhoneNumberUtil
 }
 
 /* EOF */
+
+/*
+ * This file is appended to /app/vendor-src/libphonenumber/src/libphonenumber/PhoneNumberUtil.php
+ * to make libphonenumber work when mbstring is not installed.
+ *
+ * See hack-vendors.sh
+ */
+
+if (!function_exists('mb_strlen')) {
+    function mb_strlen($s)
+    {
+        return strlen($s);
+    }
+}
+
+if (!function_exists('mb_substr')) {
+    function mb_substr($s, $start, $len = null)
+    {
+        return substr($s, $start, $len);
+    }
+}
+
+if (!function_exists('mb_strtoupper')) {
+    function mb_strtoupper($s)
+    {
+        return strtoupper($s);
+    }
+}
+
+// DESKPRO_MODIFIED
