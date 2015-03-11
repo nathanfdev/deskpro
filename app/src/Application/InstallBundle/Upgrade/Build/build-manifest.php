8b36b1ac624130214a61f740ebf8dd851368a3c8
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-03-11 09:21:20 +0000

    [FIX] Errors to do with "Argument 1 passed to CustomDataPerson::setField() ..." [FIX] Agent: Delete attachments window would not show 'x' button on attachments with very long filenames [FIX] Admin: Language IDs listed in languages section were names, not IDs [FIX] Agent: Pagination on organizations was broken

--- app/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
+++ app/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
@@ -1736,7 +1736,7 @@
 
 	1425653819 => array(
 		'file'      => '/src/Application/InstallBundle/Upgrade/Build/2015/03/Build1425653819.php',
-		'classname' => 'Application\InstallBundle\Upgrade\Build\deskpro/www/app/src/Application/InstallBundle/Upgrade/Build/2015/03/Build1425653819'
+		'classname' => 'Application\InstallBundle\Upgrade\Build\Build1425653819'
 	),
 
 );
