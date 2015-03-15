b61347a5276f671f2189e1537cd335a7a00aa6db
Chris Tickner <chris.tickner@gmail.com>
2015-03-15 18:14:13 -0400

    create the compositeterm

---
+++ app/tests/spec/DeskPRO/Bundle/AppBundle/TermEngine/Term/CompositeTermSpec.php
@@ -0,0 +1,74 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at https://www.deskpro.com/eula/                            |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace spec\DeskPRO\Bundle\AppBundle\TermEngine\Term;
+
+use DeskPRO\Bundle\AppBundle\TermEngine\TermInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use DeskPRO\Bundle\AppBundle\TermEngine\Term\CompositeTerm;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @mixin \DeskPRO\Bundle\AppBundle\TermEngine\Term\CompositeTerm
+ */
+class CompositeTermSpec extends ObjectBehavior
+{
+    function it_is_a_collection_of_terms(TermInterface $term1, TermInterface $term2)
+    {
+        $this->getTerms()->shouldBe(array());
+        $this->addTerm($term1);
+        $this->getTerms()->shouldBe(array($term1));
+        $this->addTerm($term2);
+        $this->getTerms()->shouldBe(array($term1, $term2));
+    }
+
+    public function it_defaults_to_or_op()
+    {
+        $this->getOp()->shouldReturn(TermInterface::OP_OR);
+    }
+
+    public function it_lets_you_change_the_op()
+    {
+        $this->setOp(TermInterface::OP_AND);
+
+        $this->getOp()->shouldReturn(TermInterface::OP_AND);
+    }
+
+    function it_sets_up_its_own_settings_resolver(OptionsResolver $options_resolver)
+    {
+        // does not actually take options at the moment. the "op" is the main property here.
+
+        $this->setDefaultOptions($options_resolver);
+    }
+}
