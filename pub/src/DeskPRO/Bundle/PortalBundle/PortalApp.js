5262875fcecfe2eedd97a2d02d7a1a8491e1472d
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-03-20 18:29:05 +0000

    Start setting up frameowkr

--- pub/src/DeskPRO/Bundle/PortalBundle/PortalApp.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/PortalApp.js
@@ -1,6 +1,12 @@
 import reflux from "reflux";
+import Container from "DeskPRO/Component/DependencyInjection/Container";
+import $ from "jquery";
 
 export default class PortalApp {
+  constructor() {
+    this.container = new Container();
+    this.container.register('$', { constant: $, alias: ['jquery', 'jQuery'] });
+  }
   run() {
     console.log("RUN");
   }
---
+++ pub/src/DeskPRO/Component/Http/Http.js
@@ -0,0 +1,5 @@
+export default class Http {
+  constructor(jQuery) {
+    this.jQuery = jQuery;
+  }
+}
