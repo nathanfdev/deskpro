81d2684aa6c78f4ed99feec86c60cdb6a6a9afc6
Chris Tickner <chris.tickner@gmail.com>
2015-03-24 16:25:38 -0400

    fix broken install by re-enabling slow appmanager service db calls

--- app/src/Application/DeskPRO/DependencyInjection/SystemServices/AppManagerService.php
+++ app/src/Application/DeskPRO/DependencyInjection/SystemServices/AppManagerService.php
@@ -1,29 +1,29 @@
 <?php
 /**************************************************************************\
-| DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
-| a British company located in London, England.                            |
-|                                                                          |
-| All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
-|                                                                          |
-| The license agreement under which this software is released              |
-| can be found at https://www.deskpro.com/eula/                            |
-|                                                                          |
-| By using this software, you acknowledge having read the license          |
-| and agree to be bound thereby.                                           |
-|                                                                          |
-| Please note that DeskPRO is not free software. We release the full       |
-| source code for our software because we trust our users to pay us for    |
-| the huge investment in time and energy that has gone into both creating  |
-| this software and supporting our customers. By providing the source code |
-| we preserve our customers' ability to modify, audit and learn from our   |
-| work. We have been developing DeskPRO since 2001, please help us make it |
-| another decade.                                                          |
-|                                                                          |
-| Like the work you see? Think you could make it better? We are always     |
-| looking for great developers to join us: http://www.deskpro.com/jobs/    |
-|                                                                          |
-| ~ Thanks, Everyone at Team DeskPRO                                       |
-\**************************************************************************/
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at https://www.deskpro.com/eula/                            |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
 
 /**
  * DeskPRO.
@@ -46,19 +46,21 @@ class AppManagerService
 
         if (!defined('DP_BUILDING')) {
             $packages = array();
-            //$packages = $em->createQuery("
-            //    SELECT package, asset
-            //    FROM DeskPRO:AppPackage package
-            //    LEFT JOIN package.assets asset
-            //    ORDER BY package.title
-            //")->execute();
+            // MUST be refactored, the query below should not be here (performance reasons)
+            $packages = $em->createQuery("
+                SELECT package, asset
+                FROM DeskPRO:AppPackage package
+                LEFT JOIN package.assets asset
+                ORDER BY package.title
+            ")->execute();
             $apps = array();
-            //$apps = $em->createQuery("
-            //    SELECT app, package, asset
-            //    FROM DeskPRO:AppInstance app
-            //    LEFT JOIN app.package package
-            //    LEFT JOIN package.assets asset
-            //")->execute();
+            // MUST be refactored, the query below should not be here (performance reasons)
+            $apps = $em->createQuery("
+                SELECT app, package, asset
+                FROM DeskPRO:AppInstance app
+                LEFT JOIN app.package package
+                LEFT JOIN package.assets asset
+            ")->execute();
 
             if (count($apps)) {
                 $names = array_map(function ($a) {
@@ -84,8 +86,8 @@ class AppManagerService
         } else {
             // this is constructed in the portal system (for app usersources) during portal cache warm up
             // cannot make db queries while building - nor is this necessary for building
-            $packages    = array();
-            $apps        = array();
+            $packages = array();
+            $apps = array();
             $usersources = array();
         }
 
@@ -102,7 +104,7 @@ class AppManagerService
         $app_service_container = new AppServiceContainer($container);
 
         $app_paths = array(
-            'default' => DP_ROOT.'/apps',
+            'default' => DP_ROOT . '/apps',
         );
 
         if (dp_get_config('app_paths')) {
