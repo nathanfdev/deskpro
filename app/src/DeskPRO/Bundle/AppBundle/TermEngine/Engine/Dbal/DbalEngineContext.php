b8adfa5420b4cd0b474756b4e49024fb5866dc6e
Chris Tickner <chris.tickner@gmail.com>
2015-03-25 15:41:59 -0400

    creates the DbalEngineContext

---
+++ app/src/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Dbal/DbalEngineContext.php
@@ -0,0 +1,139 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at https://www.deskpro.com/eula/                            |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal;
+
+use Application\DeskPRO\Entity\Person;
+
+class DbalEngineContext
+{
+    /**
+     * @var Person
+     */
+    protected $agent;
+
+    /**
+     * @var int|null
+     */
+    protected $page;
+
+    /**
+     * @var int|null
+     */
+    protected $per_page;
+
+    /**
+     * @var array
+     */
+    protected $group_by;
+
+    /**
+     * @var array
+     */
+    protected $order_by;
+
+    public function __construct(Person $person)
+    {
+        $this->agent = $person;
+        $this->group_by = array();
+        $this->order_by = array();
+    }
+
+    /**
+     * @return Person
+     */
+    public function getAgent()
+    {
+        return $this->agent;
+    }
+
+    /**
+     * @param Person $agent
+     */
+    public function setAgent(Person $agent)
+    {
+        $this->agent = $agent;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * @param int|null $page
+     */
+    public function setPage($page)
+    {
+        $this->page = $page;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPerPage()
+    {
+        return $this->per_page;
+    }
+
+    /**
+     * @param int|null $per_page
+     */
+    public function setPerPage($per_page)
+    {
+        $this->per_page = $per_page;
+    }
+
+    public function addGroupBy($group)
+    {
+        $this->group_by[] = $group;
+    }
+
+    public function getGroupBy()
+    {
+        return $this->group_by;
+    }
+
+    public function addOrderBy($name, $dir)
+    {
+        $this->order_by[$name] = strtoupper($dir);
+    }
+
+    public function getOrderBy()
+    {
+        return $this->order_by;
+    }
+}
---
+++ app/tests/spec/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Dbal/DbalEngineContextSpec.php
@@ -0,0 +1,124 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at https://www.deskpro.com/eula/                            |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace spec\DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal;
+
+use Application\DeskPRO\Entity\Person;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\DbalEngineContext;
+
+/**
+ * @mixin \DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\DbalEngineContext
+ */
+class DbalEngineContextSpec extends ObjectBehavior
+{
+    function let(
+        Person $person
+    )
+    {
+        $this->beConstructedWith($person);
+    }
+
+    function it_can_be_constructed_with_an_agent(
+        Person $agent
+    )
+    {
+        $this->beConstructedWith($agent);
+
+        $this->getAgent()->shouldBe($agent);
+    }
+
+    function it_lets_you_change_agent(
+        Person $agent1,
+        Person $agent2
+    )
+    {
+        $this->beConstructedWith($agent1);
+
+        $this->setAgent($agent2);
+
+        $this->getAgent()->shouldBe($agent2);
+    }
+
+    function it_lets_you_control_page_settings()
+    {
+        $this->setPage(5);
+        $this->setPerPage(10);
+
+        $this->getPage()->shouldBe(5);
+        $this->getPerPage()->shouldBe(10);
+    }
+
+    function it_has_default_page_settings_of_null_to_signal_no_limit()
+    {
+        $this->getPage()->shouldBe(null);
+        $this->getPerPage()->shouldBe(null);
+    }
+
+    function it_has_no_group_bys_by_default()
+    {
+        $this->getGroupBy()->shouldReturn(array());
+    }
+
+    function it_allows_adding_group_bys_and_normalizes_the_direction_case()
+    {
+        $this->addGroupBy('date');
+        $this->addGroupBy('name');
+
+        $this->getGroupBy()->shouldBe(
+            array(
+                'date',
+                'name'
+            )
+        );
+    }
+
+    function it_has_no_order_bys_by_default()
+    {
+        $this->getOrderBy()->shouldReturn(array());
+    }
+
+    function it_allows_adding_order_bys_and_normalizes_the_direction_case()
+    {
+        $this->addOrderBy('date', 'asC');
+        $this->addOrderBy('name', 'deSc');
+
+        $this->getOrderBy()->shouldBe(
+            array(
+                'date' => 'ASC',
+                'name' => 'DESC'
+            )
+        );
+    }
+}
