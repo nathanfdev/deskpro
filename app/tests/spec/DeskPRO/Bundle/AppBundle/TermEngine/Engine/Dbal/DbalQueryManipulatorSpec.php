e625a68e5ed318818d95785ae6da9f6148681f44
Chris Tickner <chris.tickner@gmail.com>
2015-03-26 10:28:56 -0400

    spec and create the DbalQueryManipulator (bare bones for now)

---
+++ app/tests/spec/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Dbal/DbalQueryManipulatorSpec.php
@@ -0,0 +1,96 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at https://www.deskpro.com/eula/                            |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace spec\DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal;
+
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\DbalEngineContext;
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQuery;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\DbalQueryManipulator;
+
+/**
+ * @mixin \DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\DbalQueryManipulator
+ */
+class DbalQueryManipulatorSpec extends ObjectBehavior
+{
+    function it_will_manipulate_agent_permissions(
+        DbalCompiledQuery $query,
+        DbalEngineContext $engine_context
+    )
+    {
+        $manipulated = $this->ensureAgentPermissions($query, $engine_context);
+
+        $manipulated->shouldBeAnInstanceOf('DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQuery');
+    }
+
+    function it_will_manipulate_pagination(
+        DbalCompiledQuery $query,
+        DbalEngineContext $engine_context
+    )
+    {
+        $manipulated = $this->alterPagination($query, $engine_context);
+
+        $manipulated->shouldBeAnInstanceOf('DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQuery');
+    }
+
+    function it_will_set_the_correct_sort_order(
+        DbalCompiledQuery $query,
+        DbalEngineContext $engine_context
+    )
+    {
+        $manipulated = $this->alterSortOrder($query, $engine_context);
+
+        $manipulated->shouldBeAnInstanceOf('DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQuery');
+    }
+
+    function it_will_use_proper_grouping(
+        DbalCompiledQuery $query,
+        DbalEngineContext $engine_context
+    )
+    {
+        $manipulated = $this->alterGrouping($query, $engine_context);
+
+        $manipulated->shouldBeAnInstanceOf('DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQuery');
+    }
+
+    function it_will_resolve_query_parameters(
+        DbalCompiledQuery $query,
+        DbalEngineContext $engine_context
+    )
+    {
+        $manipulated = $this->resolveParameters($query, $engine_context);
+
+        $manipulated->shouldBeAnInstanceOf('DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQuery');
+    }
+}
