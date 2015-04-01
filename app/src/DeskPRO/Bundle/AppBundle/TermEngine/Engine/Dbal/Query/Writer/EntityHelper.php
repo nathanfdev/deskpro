17764e87b7c9e7326487ef4d7e53504062165fc8
Chris Tickner <chris.tickner@gmail.com>
2015-04-01 17:31:18 -0400

    add entity helper for the writer to use

---
+++ app/src/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Dbal/Query/Writer/EntityHelper.php
@@ -0,0 +1,101 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at https://www.deskpro.com/eula/                            |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\Writer;
+
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQuery;
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQueryWriter;
+use DeskPRO\Bundle\AppBundle\TermEngine\Expression\TermEngineExpression;
+use DeskPRO\Bundle\AppBundle\TermEngine\TermInterface;
+
+class EntityHelper
+{
+    public static function write(DbalCompiledQueryWriter $query_writer, $field_name, $op, array $ids)
+    {
+        // the "count" function won't catch arrays that are all null
+        $all_null = true;
+        foreach ($ids as $val) {
+            if (null !== $val) {
+                $all_null = false;
+            }
+        }
+
+        if (!count($ids) || $all_null) {
+            $ids = array(0);
+        }
+
+        // filter ids
+        $filtered_ids = array();
+        foreach ($ids as $id) {
+            if (null === $id) {
+                $filtered_ids[] = 0;
+            } elseif ($id instanceof TermEngineExpression) {
+                $filtered_ids[] = $id;
+            } else {
+                $filtered_ids[] = (int)$id;
+            }
+        }
+
+        // determine what we will assert
+        $assert_ids = array();
+        $assert_null = false;
+        foreach ($filtered_ids as $id) {
+            if (0 === $id) {
+                $assert_null = true;
+            } else {
+                $assert_ids[] = $id;
+            }
+        }
+
+        $where = '';
+        if (count($assert_ids) > 0) {
+            $ids_isser = $op === TermInterface::OP_NOT ? 'NOT IN' : 'IN';
+            $ids_param = $query_writer->addParameter('ids', $assert_ids);
+
+            $where .= sprintf('ticket.agent_id %s (:%s)', $ids_isser, $ids_param);
+        }
+
+        if ($assert_null) {
+            $null_isser = $op === TermInterface::OP_NOT ? 'IS NOT NULL' : 'IS NULL';
+            $and_or = $op === TermInterface::OP_NOT ? 'AND' : 'OR';
+
+            $where .= sprintf(
+                '%sticket.agent_id %s',
+                strlen($where) > 0 ? ' ' . $and_or . ' ' : '',
+                $null_isser
+            );
+        }
+
+        return $where;
+    }
+}
---
+++ app/tests/spec/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Dbal/Query/Writer/EntityHelperSpec.php
@@ -0,0 +1,122 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at https://www.deskpro.com/eula/                            |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace spec\DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\Writer;
+
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQuery;
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQueryWriter;
+use DeskPRO\Bundle\AppBundle\TermEngine\TermInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\Writer\EntityHelper;
+
+/**
+ * @mixin \DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\Writer\EntityHelper
+ */
+class EntityHelperSpec extends ObjectBehavior
+{
+    function it_handles_the_simple_is_case(
+        DbalCompiledQueryWriter $query_writer
+    )
+    {
+        $query_writer->addParameter('ids', array(1))->willReturn('ids_0');
+
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_IS, array(1))
+            ->shouldReturn('ticket.agent_id IN (:ids_0)');
+    }
+
+    function it_handles_the_simple_NOT_case(
+        DbalCompiledQueryWriter $query_writer
+    )
+    {
+        $query_writer->addParameter('ids', array(1))->willReturn('ids_0');
+
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_NOT, array(1))
+            ->shouldReturn('ticket.agent_id NOT IN (:ids_0)');
+    }
+
+    function it_handles_the_is_null_case(
+        DbalCompiledQueryWriter $query_writer
+    )
+    {
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_IS, array(0))
+            ->shouldReturn('ticket.agent_id IS NULL');
+
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_IS, array())
+            ->shouldReturn('ticket.agent_id IS NULL');
+
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_IS, array(null))
+            ->shouldReturn('ticket.agent_id IS NULL');
+
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_IS, array(null, null, null))
+            ->shouldReturn('ticket.agent_id IS NULL');
+    }
+
+    function it_handles_the_NOT_null_case(
+        DbalCompiledQueryWriter $query_writer
+    )
+    {
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_NOT, array(0))
+            ->shouldReturn('ticket.agent_id IS NOT NULL');
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_NOT, array())
+            ->shouldReturn('ticket.agent_id IS NOT NULL');
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_NOT, array(null, null))
+            ->shouldReturn('ticket.agent_id IS NOT NULL');
+    }
+
+    function it_handles_the_full_is_case(
+        DbalCompiledQueryWriter $query_writer
+    )
+    {
+        $query_writer->addParameter('ids', array(3))->willReturn('ids_0');
+
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_IS, array(0, 3))
+            ->shouldReturn('ticket.agent_id IN (:ids_0) OR ticket.agent_id IS NULL');
+
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_IS, array(null, 3))
+            ->shouldReturn('ticket.agent_id IN (:ids_0) OR ticket.agent_id IS NULL');
+    }
+
+    function it_handles_the_full_NOT_case(
+        DbalCompiledQueryWriter $query_writer
+    )
+    {
+        $query_writer->addParameter('ids', array(3))->willReturn('ids_0');
+
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_NOT, array(0, 3))
+            ->shouldReturn('ticket.agent_id NOT IN (:ids_0) AND ticket.agent_id IS NOT NULL');
+
+        $this->write($query_writer, 'ticket.agent_id', TermInterface::OP_NOT, array(null, 3))
+            ->shouldReturn('ticket.agent_id NOT IN (:ids_0) AND ticket.agent_id IS NOT NULL');
+    }
+}
