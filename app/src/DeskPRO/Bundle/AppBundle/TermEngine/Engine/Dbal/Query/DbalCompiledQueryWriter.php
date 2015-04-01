2a7ff78d9751d7620b40f8aef772c1145fdaeee1
Chris Tickner <chris.tickner@gmail.com>
2015-04-01 16:42:48 -0400

    create the DbalCompiledQueryWriter

---
+++ app/src/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Dbal/Query/DbalCompiledQueryWriter.php
@@ -0,0 +1,119 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at https://www.deskpro.com/eula/                            |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query;
+
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQuery;
+
+class DbalCompiledQueryWriter
+{
+    /**
+     * @var DbalCompiledQuery
+     */
+    private $query;
+
+    public function __construct(DbalCompiledQuery $query)
+    {
+        $this->query = $query;
+    }
+
+    /**
+     * @return DbalCompiledQuery
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * ONLY the main compiler should use this. It will replace the existing WHERE string
+     * in the query with a new one.
+     *
+     * @param $new_where_string
+     */
+    public function replaceWhere($new_where_string)
+    {
+        $this->query->setWherePart($new_where_string);
+    }
+
+    /**
+     * Set a parameter, but the $name_prefix is just a prefix. The actual parameter
+     * name will be returned to you.
+     *
+     * @param $name_prefix
+     * @param $value
+     * @return string the parameter name
+     */
+    public function addParameter($name_prefix, $value)
+    {
+        return $this->query->addParameter($name_prefix, $value);
+    }
+
+    /**
+     * This will do nothing if the table is already joined, else it will join the
+     * table with the given ON
+     *
+     * @param $table_name
+     * @param string $on
+     */
+    public function addJoin($table_name, $on)
+    {
+        $this->query->addJoin($table_name, $on);
+    }
+
+    /**
+     * You can use {alias} in the $on param as a placeholder for the real join alias.
+     *
+     * It returns the real join alias so you can reference it.
+     *
+     * @param $table_name
+     * @param string $on
+     * @param string $type
+     * @return string the join alias
+     */
+    public function addUniqueJoin($table_name, $on, $type = 'LEFT')
+    {
+        return $this->query->addUniqueJoin($table_name, $on, $type);
+    }
+
+    /**
+     * What table are we selecting from?
+     *
+     * @param $table
+     * @param $alias
+     */
+    public function setFrom($table, $alias)
+    {
+        $this->query->setFrom($table, $alias);
+    }
+}
---
+++ app/tests/spec/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Dbal/Query/DbalCompiledQueryWriterSpec.php
@@ -0,0 +1,104 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at https://www.deskpro.com/eula/                            |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace spec\DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query;
+
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQuery;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQueryWriter;
+
+/**
+ * @mixin \DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalCompiledQueryWriter
+ */
+class DbalCompiledQueryWriterSpec extends ObjectBehavior
+{
+    function let(
+        DbalCompiledQuery $query
+    )
+    {
+        $this->beConstructedWith($query);
+    }
+
+    function it_is_a_wrapper_around_a_dbal_compiled_query(
+        DbalCompiledQuery $query
+    )
+    {
+        $this->getQuery()->shouldReturn($query);
+    }
+
+    function it_writes_where(
+        DbalCompiledQuery $query
+    )
+    {
+        $query->setWherePart('ticket.id = 3')->shouldBeCalled();
+
+        $this->replaceWhere('ticket.id = 3');
+    }
+
+    function it_writes_parameters(
+        DbalCompiledQuery $query
+    )
+    {
+        $query->addParameter('name_prefix', 'value')->shouldBeCalled();
+
+        $this->addParameter('name_prefix', 'value');
+    }
+
+    function it_writes_joins(
+        DbalCompiledQuery $query
+    )
+    {
+        $query->addJoin('table', 'alias')->shouldBeCalled();
+
+        $this->addJoin('table', 'alias');
+    }
+
+    function it_writes_unique_joins(
+        DbalCompiledQuery $query
+    )
+    {
+        $query->addUniqueJoin('table', 'on', 'type')->shouldBeCalled();
+
+        $this->addUniqueJoin('table', 'on', 'type');
+    }
+
+    function it_lets_you_write_from(
+        DbalCompiledQuery $query
+    )
+    {
+        $query->setFrom('table', 'alias')->shouldBeCalled();
+
+        $this->setFrom('table', 'alias');
+    }
+}
