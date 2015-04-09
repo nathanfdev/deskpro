0bf12920223727338a4d16a124c9a3b1afddb554
Chris Tickner <chris.tickner@gmail.com>
2015-04-09 12:32:19 -0400

    the "PhpCheck" that term compilers make

---
+++ app/src/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Php/Dumper/PhpCheck.php
@@ -0,0 +1,71 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/  |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2014, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at https://www.deskpro.com/eula/                            |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace DeskPRO\Bundle\AppBundle\TermEngine\Engine\Php\Dumper;
+
+class PhpCheck
+{
+    /**
+     * This is a PHP string that will be executed. You must set the $check variable
+     * to true or false, with no return statements.
+     *
+     * @var string|null
+     */
+    protected $check_code;
+
+    public function __construct($check_code = null)
+    {
+        $this->check_code = $check_code;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCheckCode()
+    {
+        return $this->check_code;
+    }
+
+    /**
+     * @param mixed $check_code
+     */
+    public function setCheckCode($check_code)
+    {
+        $this->check_code = $check_code;
+    }
+
+    public function __toString()
+    {
+        return trim($this->check_code);
+    }
+}
