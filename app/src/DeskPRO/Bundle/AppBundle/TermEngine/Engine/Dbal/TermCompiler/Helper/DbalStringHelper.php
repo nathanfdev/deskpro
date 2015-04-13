95c945a9cd9b68171ea93ab6d30260445573d1ad
n3b <neb802@gmail.com>
2015-04-13 14:20:57 +0800

    undefined?

--- app/src/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Dbal/TermCompiler/Helper/DbalStringHelper.php
+++ app/src/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Dbal/TermCompiler/Helper/DbalStringHelper.php
@@ -35,7 +35,6 @@ namespace DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\TermCompiler\Helper;
 
 use DeskPRO\Bundle\AppBundle\TermEngine\TermCompilerHelperInterface;
 use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\Query\DbalQueryPart;
-use DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\TermCompiler\DbalHelperInterface;
 use DeskPRO\Bundle\AppBundle\TermEngine\TermInterface;
 
 class DbalStringHelper implements TermCompilerHelperInterface
