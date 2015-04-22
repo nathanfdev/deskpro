4ea13566badf069c7ee70579f62c657e1c870a12
Chris Tickner <chris.tickner@gmail.com>
2015-04-22 19:37:59 -0400

    fix /api_tokens endpoint

--- app/src/DeskPRO/Bundle/ApiBundle/Controller/Authentication/ApiTokensController.php
+++ app/src/DeskPRO/Bundle/ApiBundle/Controller/Authentication/ApiTokensController.php
@@ -1,29 +1,29 @@
 <?php
 /**************************************************************************\
-| DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
-| a British company located in London, England.                            |
-|                                                                          |
-| All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
-|                                                                          |
-| The license agreement under which this software is released              |
-| can be found at http://www.deskpro.com/license                           |
-|                                                                          |
-| By using this software, you acknowledge having read the license          |
-| and agree to be bound thereby.                                           |
-|                                                                          |
-| Please note that DeskPRO is not free software. We release the full       |
-| source code for our software because we trust our users to pay us for    |
-| the huge investment in time and energy that has gone into both creating  |
-| this software and supporting our customers. By providing the source code |
-| we preserve our customers' ability to modify, audit and learn from our   |
-| work. We have been developing DeskPRO since 2001, please help us make it |
-| another decade.                                                          |
-|                                                                          |
-| Like the work you see? Think you could make it better? We are always     |
-| looking for great developers to join us: http://www.deskpro.com/jobs/    |
-|                                                                          |
-| ~ Thanks, Everyone at Team DeskPRO                                       |
-\**************************************************************************/
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at http://www.deskpro.com/license                           |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
 
 /**
  * DeskPRO
@@ -67,9 +67,9 @@ class ApiTokensController extends BaseController
     public function newTokenAction(Request $request)
     {
         $form = $this->createFormBuilder(
-                array('email' => null, 'password' => null),
-                array('csrf_protection' => false)
-            )
+            array('email' => null, 'password' => null),
+            array('csrf_protection' => false)
+        )
             ->add('email', 'email', array('constraints' => new NotNull()))
             ->add('password', 'password', array('constraints' => new NotNull()))
             ->getForm();
@@ -114,7 +114,7 @@ class ApiTokensController extends BaseController
         return View::create(
             $this->createRepresentation(
                 array(
-                    'token' => $api_token->token
+                    'token' => $api_token->id . ':' . $api_token->token
                 )
             ),
             201
