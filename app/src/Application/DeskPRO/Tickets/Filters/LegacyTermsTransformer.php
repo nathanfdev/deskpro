6a26641addbae5b272eb8b494a733a8d9e32a887
n3b <neb802@gmail.com>
2015-04-24 03:01:54 +0800

    fix fatal in filters

--- app/src/Application/DeskPRO/Tickets/Filters/LegacyTermsTransformer.php
+++ app/src/Application/DeskPRO/Tickets/Filters/LegacyTermsTransformer.php
@@ -824,7 +824,11 @@ class LegacyTermsTransformer
                 $new_opts = $options;
                 $new_opts['field_id'] = $type_id;
                 $new_opts['value'] = isset($options['custom_fields']["field_{$type_id}"]) ? $options['custom_fields']["field_{$type_id}"] : null;
-                $parts = explode('|', $new_opts['value']);
+
+                $parts = is_string($new_opts['value'])
+                    ? explode('|', $new_opts['value'])
+                    : array();
+
                 if ($field = array_shift($parts)) {
                     foreach ($parts as $k => $part) {
                         if ('date_relative' === $field) {
