af4e05b098d2e1d0d11696b2af665ef036eed545
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-04-30 17:02:21 +0100

    Some more cleanup of ticket view

--- pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/PortalPage.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/PortalPage.js
@@ -1,10 +1,12 @@
 import PageWidget from "DeskPRO/Component/PageWidget/PageWidget";
 import NewTicketForm from "DeskPRO/Bundle/PortalBundle/PageWidget/NewTicketForm";
+import TicketView from "DeskPRO/Bundle/PortalBundle/PageWidget/TicketView";
 import $ from "jquery";
 
 export default class PortalPage extends PageWidget {
   init() {
     this.addWidgetDef(NewTicketForm, "#new_ticket_page");
+    this.addWidgetDef(TicketView, "#ticket_view_page");
     return new Promise((resolve) => {
       $(document).ready(resolve);
     });
---
+++ pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/TicketView.js
@@ -0,0 +1,20 @@
+import _ from "lodash";
+import $ from "jquery";
+import PageWidget from "DeskPRO/Component/PageWidget/PageWidget";
+
+//######################################################################################################################
+//# Page widget
+//######################################################################################################################
+
+export default class NewTicketForm extends PageWidget {
+  init() {
+
+  }
+
+  renderWidget() {
+    $('.button-reply').on('click', (ev) => {
+      ev.preventDefault();
+      $('.reply-form').slideToggle();
+    });
+  }
+}
