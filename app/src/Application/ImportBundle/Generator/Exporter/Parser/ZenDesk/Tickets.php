8883bd5ec7ebccc694c78b998f01f24b4f5a2b57
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-06-17 22:14:44 +0300

    zd skip ticket replies without author id

--- app/src/Application/ImportBundle/Generator/Exporter/Parser/ZenDesk/Tickets.php
+++ app/src/Application/ImportBundle/Generator/Exporter/Parser/ZenDesk/Tickets.php
@@ -266,10 +266,10 @@ final class Tickets extends AbstractParser
         $comments = new Entity\Collection();
 
         foreach ($ticket['comments'] as $comment) {
-            if (empty($comment['author_id'])) {
-                $this->logError(sprintf('No comment author'));
+            if ( ! $comment['author_id']) {
                 continue;
             }
+
             $author_email = $this->tickets_people->getPersonEmail($comment['author_id']);
             if ( ! $author_email) {
                 $this->logError(sprintf('Unable to get comment author #%d', $ticket['author_id']));
