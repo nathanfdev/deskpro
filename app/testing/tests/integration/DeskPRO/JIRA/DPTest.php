25248be6c8a485a840ea2e172147b8e68de7b206
n3b <neb802@gmail.com>
2015-06-26 05:25:12 +0800

    travis testing

--- app/testing/tests/integration/DeskPRO/JIRA/DPTest.php
+++ app/testing/tests/integration/DeskPRO/JIRA/DPTest.php
@@ -82,7 +82,7 @@ class DPTest extends \DpIntegrationTestCase
 
         $issues = $service->issues($ticket['id']);
 
-        throw new \Exception(print_r($issues));
+        throw new \Exception(print_r($issues, 1));
 
         /** @var JiraIssue $issue */
         $issue = $em->getRepository('DeskPRO:JiraIssue')->findOneBy(array());
