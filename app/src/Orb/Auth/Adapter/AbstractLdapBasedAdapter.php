2fe94c3a3ffc7081f1470292f888cf93ef715e7e
Chris Tickner <chris.tickner@gmail.com>
2015-06-30 18:15:30 -0400

    [Usersource] fix the php < 5.3.9 abstract function over an interface issue in AD/LDAP

--- app/src/Orb/Auth/Adapter/AbstractLdapBasedAdapter.php
+++ app/src/Orb/Auth/Adapter/AbstractLdapBasedAdapter.php
@@ -35,17 +35,13 @@ namespace Orb\Auth\Adapter;
 
 
 use Application\DeskPRO\Ldap\LdapPagedSearcher;
+use Orb\Log\Loggable;
 use Orb\Log\Logger;
 use Zend\Ldap\Ldap;
 
-abstract class AbstractLdapBasedAdapter
+abstract class AbstractLdapBasedAdapter implements Loggable
 {
     protected $options;
-
-    /**
-     * @return \Psr\Log\LoggerInterface
-     */
-    abstract function getLogger();
 
     /**
      * @return \Zend\Authentication\Adapter\Ldap
