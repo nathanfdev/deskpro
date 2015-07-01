4ce203009b9dc4be9e097e49f0254847f1c91a5f
n3b <neb802@gmail.com>
2015-07-02 02:21:58 +0800

    fix typo

--- app/src/Application/DeskPRO/Entity/PersonActivity.php
+++ app/src/Application/DeskPRO/Entity/PersonActivity.php
@@ -103,17 +103,57 @@ class PersonActivity extends \Application\DeskPRO\Domain\DomainObject
     {
         $metadata->setInheritanceType(ClassMetadataInfo::INHERITANCE_TYPE_NONE);
         $metadata->customRepositoryClassName = 'Application\DeskPRO\EntityRepository\PersonActivity';
-        $metadata->setPrimaryTable(array(
-            'name' => 'person_activity',
-            'indexes' => array(
-                'date_created_idx' => array('columns' => array('date_created'))
+        $metadata->setPrimaryTable(
+            array(
+                'name' => 'person_activity',
+                'indexes' => array(
+                    'date_created_idx' => array('columns' => array('date_created'))
+                )
             )
-        ));
+        );
         $metadata->setChangeTrackingPolicy(ClassMetadataInfo::CHANGETRACKING_NOTIFY);
-        $metadata->mapField(array( 'fieldName' => 'id', 'type' => 'integer', 'precision' => 0, 'scale' => 0, 'nullable' => false, 'columnName' => 'id', 'id' => true, ));
-        $metadata->mapField(array( 'fieldName' => 'action_type', 'type' => 'string', 'length' => 255, 'precision' => 0, 'scale' => 0, 'nullable' => false, 'columnName' => 'action_type', ));
-        $metadata->mapField(array( 'fieldName' => 'details', 'type' => 'array', 'precision' => 0, 'scale' => 0, 'nullable' => false, 'columnName' => 'details', ));
-        $metadata->mapField(array( 'fieldName' => 'date_created', 'type' => 'datetime', 'precision' => 0, 'scale' => 0, 'nullable' => false, 'columnName' => 'date_created', ));
+        $metadata->mapField(
+            array(
+                'fieldName' => 'id',
+                'type' => 'integer',
+                'precision' => 0,
+                'scale' => 0,
+                'nullable' => false,
+                'columnName' => 'id',
+                'id' => true,
+            )
+        );
+        $metadata->mapField(
+            array(
+                'fieldName' => 'action_type',
+                'type' => 'string',
+                'length' => 255,
+                'precision' => 0,
+                'scale' => 0,
+                'nullable' => false,
+                'columnName' => 'action_type',
+            )
+        );
+        $metadata->mapField(
+            array(
+                'fieldName' => 'details',
+                'type' => 'array',
+                'precision' => 0,
+                'scale' => 0,
+                'nullable' => false,
+                'columnName' => 'details',
+            )
+        );
+        $metadata->mapField(
+            array(
+                'fieldName' => 'date_created',
+                'type' => 'datetime',
+                'precision' => 0,
+                'scale' => 0,
+                'nullable' => false,
+                'columnName' => 'date_created',
+            )
+        );
         $metadata->setIdGeneratorType(ClassMetadataInfo::GENERATOR_TYPE_IDENTITY);
         $metadata->mapManyToOne(
             array(
