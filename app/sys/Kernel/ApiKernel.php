35742b8f5a4d2617df1e324696a55f94b48a86a2
Chris Tickner <chris.tickner@gmail.com>
2015-07-08 15:12:40 -0400

    [Errors] make extra sure we handle exceptions peoperly in new kernels

--- app/sys/Kernel/ApiKernel.php
+++ app/sys/Kernel/ApiKernel.php
@@ -34,11 +34,13 @@
 namespace DeskPRO\Kernel;
 
 use Application\DeskPRO\App;
+use Doctrine\DBAL\DBALException;
 use Symfony\Component\Config\ConfigCache;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Kernel;
 
 class ApiKernel extends Kernel
@@ -86,6 +88,18 @@ class ApiKernel extends Kernel
         return $bundles;
     }
 
+    public function handle(\Symfony\Component\HttpFoundation\Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
+    {
+        try {
+            return parent::handle($request, $type, $catch);
+        } catch (\Exception $e) {
+            // we catch the DBALExceptions in KernelBooter, so don't handle that here.
+            if (!$e instanceof DBALException) {
+                // the http kernel will handle exception inside in most cases. this is a "just in case" catch.
+                KernelErrorHandler::handleException($e);
+            }
+        }
+    }
 
     /**
      * {@inheritDoc}
--- app/sys/Kernel/PortalKernel.php
+++ app/sys/Kernel/PortalKernel.php
@@ -32,11 +32,13 @@
 namespace DeskPRO\Kernel;
 
 use Application\DeskPRO\App;
+use Doctrine\DBAL\DBALException;
 use Symfony\Component\Config\ConfigCache;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Kernel;
 
 class PortalKernel extends Kernel
@@ -83,6 +85,23 @@ class PortalKernel extends Kernel
         }
 
         return $bundles;
+    }
+
+    public function handle(
+        \Symfony\Component\HttpFoundation\Request $request,
+        $type = HttpKernelInterface::MASTER_REQUEST,
+        $catch = true
+    )
+    {
+        try {
+            return parent::handle($request, $type, $catch);
+        } catch (\Exception $e) {
+            // we catch the DBALExceptions in KernelBooter, so don't handle that here.
+            if (!$e instanceof DBALException) {
+                // the http kernel will handle exception inside in most cases. this is a "just in case" catch.
+                KernelErrorHandler::handleException($e);
+            }
+        }
     }
 
     /**
