1536283011f459a09fe6fad7ac12d04a8c828506
Chris Tickner <chris.tickner@gmail.com>
2015-07-08 20:40:31 -0400

    [Tests] remove old "testing" dir

--- app/testing/tests/integration/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__.'/../../src/init.php';
-
-\Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespaces(array('Sensio\\Bundle\\FrameworkExtraBundle' => DP_ROOT.'/vendor/sensio/framework-extra-bundle/'));
