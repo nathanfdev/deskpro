dd35ce5288773059fde3f98a93ce61f85e0e33fc
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-07-15 14:41:53 +0100

    [FIX] Agent: Some ticket counts in filter list might be wrong for a few seconds until the browser did a background-refresh

--- app/src/Application/DeskPRO/Searcher/TicketSearch.php
+++ app/src/Application/DeskPRO/Searcher/TicketSearch.php
@@ -654,8 +654,8 @@ class TicketSearch extends SearcherAbstract
             $table = 'tickets_search_active';
         }
 
-        $sql = "SELECT COUNT(tickets.id) AS count FROM $table AS tickets ";
-        $sql2 = "SELECT COUNT(part_perm.id) AS count FROM tickets_participants AS part_perm LEFT JOIN $table AS tickets ON (tickets.id = part_perm.ticket_id) ";
+        $sql = "SELECT tickets.id AS ticket_id FROM $table AS tickets ";
+        $sql2 = "SELECT tickets.id AS ticket_id FROM tickets_participants AS part_perm LEFT JOIN $table AS tickets ON (tickets.id = part_perm.ticket_id) ";
 
         #------------------------------
         # Standard for permissions
@@ -789,12 +789,12 @@ class TicketSearch extends SearcherAbstract
             $sql2 .= " AND part_perm.person_id = {$this->person->getId()} ";
         }
 
-        $sql .= " ";
-        $sql2 .= " ";
+        $sql .= " LIMIT 10000 ";
+        $sql2 .= " LIMIT 10000 ";
 
         if ($with_part_union) {
             $count_sql = "
-                SELECT SUM(count)
+                SELECT COUNT(DISTINCT ticket_id)
                 FROM (
                     $sql
                     UNION
