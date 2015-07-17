04e1a368fdfa9b1e8cd59b178273ae15a99ae150
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-07-17 18:37:22 +0300

    inline people csv

---
+++ app/testing/tests/integration/DeskPRO/Import/CsvInlineCustomDataTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace DpIntegrationTests\DeskPRO\Import;
+
+use Application\ImportBundle\Command\ExportCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Class CsvInlineCustomDataTest
+ * @package DpIntegrationTests\DeskPRO\Import
+ *
+ * @group importer
+ */
+class CsvInlineCustomDataTest extends \DpIntegrationTestCase
+{
+    /**
+     * @var string
+     */
+    private $input_path;
+
+    /**
+     * @var string
+     */
+    private $output_path;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function runBefore()
+    {
+        $this->input_path  = DP_ROOT . '/src/Application/ImportBundle/Resources/docs/data_example/csv_inline_custom_data';
+        $this->output_path = dp_get_data_dir() . '/import/csv/export';
+
+        $this->helper->amInPath($this->output_path);
+        $this->helper->cleanDir($this->output_path);
+    }
+
+    public function testExport()
+    {
+        $application = new Application($this->helper->getSymfonyContainer()->getKernel());
+        $application->add(new ExportCommand());
+
+        $command = $application->find('dp:export:run');
+        $command_tester = new CommandTester($command);
+        $command_tester->execute(array(
+            'command'       => $command->getName(),
+            'script'        => 'csv',
+            '--input-path'  => $this->input_path,
+            '--output-path' => $this->output_path,
+            '--batch'       => true,
+        ));
+    }
+}
