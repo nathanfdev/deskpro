479545921a036cc56602f8a0330285f006ccf33b
Rob Lowcock <rob.lowcock@deskpro.com>
2015-07-21 09:47:37 +0100

    Basic GET API endpoints for agent teams and departments

---
+++ app/src/DeskPRO/Bundle/ApiBundle/Controller/People/DepartmentsController.php
@@ -0,0 +1,282 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at http://www.deskpro.com/license                           |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace DeskPRO\Bundle\ApiBundle\Controller\People;
+
+use Application\DeskPRO\Entity\Department;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use DeskPRO\Bundle\ApiBundle\Controller\BaseController;
+use DeskPRO\Bundle\ApiBundle\Error\ApiErrors;
+use DeskPRO\Bundle\ApiBundle\Error\Exception\InvalidFormException;
+use DeskPRO\Bundle\ApiBundle\Exception\WrappedApiErrorException;
+use DeskPRO\Bundle\AppBundle\TermEngine\Exception\TermTypeDoesNotExistException;
+use FOS\RestBundle\Controller\Annotations\Put;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\View\View;
+use Pagerfanta\Adapter\ArrayAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Delete;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class DepartmentsController extends BaseController implements ClassResourceInterface
+{
+    /**
+     * @ApiDoc(
+     *      description="get a list of departments",
+     *      parameters={
+     *          {
+     *              "name"="page",
+     *              "requirement"="\d+",
+     *              "description"="the page you are requesting",
+     *              "dataType"="integer",
+     *              "required"=false
+     *          },
+     *          {
+     *              "name"="count",
+     *              "requirement"="\d+",
+     *              "description"="results per page",
+     *              "dataType"="integer",
+     *              "required"=false
+     *          }
+     *      },
+     *      statusCodes={
+     *          200="Success"
+     *      }
+     * )
+     * @Get("/departments", name="api_departments")
+     * @param Request $request
+     * @return View
+     */
+    public function cgetAction(Request $request)
+    {
+        $departments = $this->getDoctrine()->getManager()->getRepository('DeskPRO:Department')->findAll();
+
+        $page = $request->query->get('page', 1);
+        $count = $request->query->get('count', 10);
+
+        $pager = new Pagerfanta(new ArrayAdapter($departments));
+        $pager->setMaxPerPage($count);
+        $pager->setCurrentPage($page);
+
+        return View::create(
+            $this->createRepresentation($pager),
+            Response::HTTP_OK
+        );
+    }
+
+    /**
+     * @ApiDoc(
+     *      description="get a department",
+     *      requirements={
+     *          {
+     *              "name"="id",
+     *              "requirement"="\d+",
+     *              "description"="the id of the department",
+     *              "dataType"="integer"
+     *          }
+     *      },
+     *      statusCodes={
+     *          200="Success",
+     *          404="Not Found"
+     *      },
+     *      output="Application\DeskPRO\Entity\Department"
+     * )
+     * @Get("/departments/{id}", name="api_departments_get")
+     * @param int $id
+     * @return View
+     */
+    public function getAction($id)
+    {
+        $department = $this->getDepartment($id);
+
+        if (empty($department)) {
+            throw $this->createNotFoundException();
+        }
+
+        return View::create(
+            $this->createRepresentation($department),
+            Response::HTTP_OK
+        );
+    }
+
+    /**
+     * @ApiDoc(
+     *      description="create a new department",
+     *      input={"class"="department", "name"=""},
+     *      statusCodes={
+     *          201="Created",
+     *          400="Bad Request"
+     *      },
+     *      output="Application\DeskPRO\Entity\Department"
+     * )
+     * @Post("/departments", name="api_departments_post")
+     * @param Request $request
+     * @throws WrappedApiErrorException
+     * @throws InvalidFormException
+     * @return View
+     */
+    public function postAction(Request $request)
+    {
+        $department = new Department($this->getUser());
+        return $this->handleFormSubmission($request, $department);
+    }
+
+    /**
+     * @APIDoc(
+     *      description="update a department",
+     *      requirements={
+     *          {
+     *              "name"="id",
+     *              "requirement"="\d+",
+     *              "description"="the id of the department",
+     *              "dataType"="integer"
+     *          }
+     *      },
+     *      input={"class"="department", "name"=""},
+     *      statusCodes={
+     *          204="Updated",
+     *          400="Bad Request",
+     *          404="Not Found"
+     *      }
+     * )
+     * @Put("/departments/{id}", name="api_departments_put")
+     * @param Request $request
+     * @param $id
+     * @throws WrappedApiErrorException
+     * @return View
+     */
+    public function putAction(Request $request, $id)
+    {
+        $department = $this->getDepartment($id);
+
+        return $this->handleFormSubmission($request, $department);
+    }
+
+    /**
+     * @APIDoc(
+     *      description="delete a department",
+     *      requirements={
+     *          {
+     *              "name"="id",
+     *              "requirement"="\d+",
+     *              "description"="the id of the department",
+     *              "dataType"="integer"
+     *          }
+     *      },
+     *      statusCodes={
+     *          200="Success",
+     *          404="Not Found"
+     *      }
+     * )
+     * @Delete("/departments/{id}", name="api_departments_delete")
+     * @param $id
+     * @return View
+     */
+    public function deleteAction($id)
+    {
+        $department = $this->getDepartment($id);
+        $this->getDoctrine()->getManager()->remove($department);
+        $this->getDoctrine()->getManager()->flush();
+
+        return View::create(
+            array(),
+            Response::HTTP_OK
+        );
+    }
+
+    /**
+     * @param int $id
+     * @return Department
+     */
+    protected function getDepartment($id)
+    {
+        $id = (int) $id;
+        $department = $this->getDoctrine()->getManager()->getRepository('DeskPRO:Department')->find($id);
+
+        if (!$department) {
+            throw $this->createNotFoundException();
+        }
+
+        return $department;
+    }
+
+    /**
+     * Will be abstracted for use by other controllers
+     * @param Request $request
+     * @param Department $department
+     * @return View
+     * @throws WrappedApiErrorException
+     */
+    protected function handleFormSubmission(Request $request, Department $department)
+    {
+        $status = $department->getId() ? Response::HTTP_NO_CONTENT : Response::HTTP_CREATED;
+
+        /** @var Form $form */
+        $form = $this->get('form.factory')->createNamedBuilder(null, 'department', $department)->getForm();
+
+        $submitted = $request->request->all();
+
+        try {
+            $form->submit($submitted, $request->getMethod() !== 'PUT');
+        } catch (TermTypeDoesNotExistException $e) {
+            throw new WrappedApiErrorException(
+                new BadRequestHttpException(ApiErrors::TERM_TYPE_DOES_NOT_EXIST),
+                array(
+                    'type' => $e->getMessage()
+                )
+            );
+        }
+
+        if ($form->isValid()) {
+            $this->getDoctrine()->getManager()->persist($department);
+            $this->getDoctrine()->getManager()->flush();
+
+            $location = $this->generateUrl('api_departments_get', array('id' => $department->getId()));
+
+            return View::create(
+                $this->createRepresentation($department),
+                $status,
+                array(
+                    'Location' => $location,
+                )
+            );
+        }
+
+        throw new InvalidFormException($form);
+    }
+}
---
+++ app/src/DeskPRO/Bundle/ApiBundle/Controller/People/TeamsController.php
@@ -0,0 +1,282 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at http://www.deskpro.com/license                           |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace DeskPRO\Bundle\ApiBundle\Controller\People;
+
+use Application\DeskPRO\Entity\AgentTeam;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use DeskPRO\Bundle\ApiBundle\Controller\BaseController;
+use DeskPRO\Bundle\ApiBundle\Error\ApiErrors;
+use DeskPRO\Bundle\ApiBundle\Error\Exception\InvalidFormException;
+use DeskPRO\Bundle\ApiBundle\Exception\WrappedApiErrorException;
+use DeskPRO\Bundle\AppBundle\TermEngine\Exception\TermTypeDoesNotExistException;
+use FOS\RestBundle\Controller\Annotations\Put;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\View\View;
+use Pagerfanta\Adapter\ArrayAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Delete;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class TeamsController extends BaseController implements ClassResourceInterface
+{
+    /**
+     * @ApiDoc(
+     *      description="get a list of teams",
+     *      parameters={
+     *          {
+     *              "name"="page",
+     *              "requirement"="\d+",
+     *              "description"="the page you are requesting",
+     *              "dataType"="integer",
+     *              "required"=false
+     *          },
+     *          {
+     *              "name"="count",
+     *              "requirement"="\d+",
+     *              "description"="results per page",
+     *              "dataType"="integer",
+     *              "required"=false
+     *          }
+     *      },
+     *      statusCodes={
+     *          200="Success"
+     *      }
+     * )
+     * @Get("/teams", name="api_teams")
+     * @param Request $request
+     * @return View
+     */
+    public function cgetAction(Request $request)
+    {
+        $teams = $this->getDoctrine()->getManager()->getRepository('DeskPRO:AgentTeam')->findAll();
+
+        $page = $request->query->get('page', 1);
+        $count = $request->query->get('count', 10);
+
+        $pager = new Pagerfanta(new ArrayAdapter($teams));
+        $pager->setMaxPerPage($count);
+        $pager->setCurrentPage($page);
+
+        return View::create(
+            $this->createRepresentation($pager),
+            Response::HTTP_OK
+        );
+    }
+
+    /**
+     * @ApiDoc(
+     *      description="get a team",
+     *      requirements={
+     *          {
+     *              "name"="id",
+     *              "requirement"="\d+",
+     *              "description"="the id of the team",
+     *              "dataType"="integer"
+     *          }
+     *      },
+     *      statusCodes={
+     *          200="Success",
+     *          404="Not Found"
+     *      },
+     *      output="Application\DeskPRO\Entity\AgentTeam"
+     * )
+     * @Get("/teams/{id}", name="api_teams_get")
+     * @param int $id
+     * @return View
+     */
+    public function getAction($id)
+    {
+        $team = $this->getAgentTeam($id);
+
+        if (empty($team)) {
+            throw $this->createNotFoundException();
+        }
+
+        return View::create(
+            $this->createRepresentation($team),
+            Response::HTTP_OK
+        );
+    }
+
+    /**
+     * @ApiDoc(
+     *      description="create a new team",
+     *      input={"class"="team", "name"=""},
+     *      statusCodes={
+     *          201="Created",
+     *          400="Bad Request"
+     *      },
+     *      output="Application\DeskPRO\Entity\AgentTeam"
+     * )
+     * @Post("/teams", name="api_teams_post")
+     * @param Request $request
+     * @throws WrappedApiErrorException
+     * @throws InvalidFormException
+     * @return View
+     */
+    public function postAction(Request $request)
+    {
+        $team = new AgentTeam($this->getUser());
+        return $this->handleFormSubmission($request, $team);
+    }
+
+    /**
+     * @APIDoc(
+     *      description="update a team",
+     *      requirements={
+     *          {
+     *              "name"="id",
+     *              "requirement"="\d+",
+     *              "description"="the id of the team",
+     *              "dataType"="integer"
+     *          }
+     *      },
+     *      input={"class"="team", "name"=""},
+     *      statusCodes={
+     *          204="Updated",
+     *          400="Bad Request",
+     *          404="Not Found"
+     *      }
+     * )
+     * @Put("/teams/{id}", name="api_teams_put")
+     * @param Request $request
+     * @param $id
+     * @throws WrappedApiErrorException
+     * @return View
+     */
+    public function putAction(Request $request, $id)
+    {
+        $team = $this->getAgentTeam($id);
+
+        return $this->handleFormSubmission($request, $team);
+    }
+
+    /**
+     * @APIDoc(
+     *      description="delete a team",
+     *      requirements={
+     *          {
+     *              "name"="id",
+     *              "requirement"="\d+",
+     *              "description"="the id of the team",
+     *              "dataType"="integer"
+     *          }
+     *      },
+     *      statusCodes={
+     *          200="Success",
+     *          404="Not Found"
+     *      }
+     * )
+     * @Delete("/teams/{id}", name="api_teams_delete")
+     * @param $id
+     * @return View
+     */
+    public function deleteAction($id)
+    {
+        $team = $this->getAgentTeam($id);
+        $this->getDoctrine()->getManager()->remove($team);
+        $this->getDoctrine()->getManager()->flush();
+
+        return View::create(
+            array(),
+            Response::HTTP_OK
+        );
+    }
+
+    /**
+     * @param int $id
+     * @return AgentTeam
+     */
+    protected function getAgentTeam($id)
+    {
+        $id = (int) $id;
+        $team = $this->getDoctrine()->getManager()->getRepository('DeskPRO:AgentTeam')->find($id);
+
+        if (!$team) {
+            throw $this->createNotFoundException();
+        }
+
+        return $team;
+    }
+
+    /**
+     * Will be abstracted for use by other controllers
+     * @param Request $request
+     * @param AgentTeam $team
+     * @return View
+     * @throws WrappedApiErrorException
+     */
+    protected function handleFormSubmission(Request $request, AgentTeam $team)
+    {
+        $status = $team->getId() ? Response::HTTP_NO_CONTENT : Response::HTTP_CREATED;
+
+        /** @var Form $form */
+        $form = $this->get('form.factory')->createNamedBuilder(null, 'team', $team)->getForm();
+
+        $submitted = $request->request->all();
+
+        try {
+            $form->submit($submitted, $request->getMethod() !== 'PUT');
+        } catch (TermTypeDoesNotExistException $e) {
+            throw new WrappedApiErrorException(
+                new BadRequestHttpException(ApiErrors::TERM_TYPE_DOES_NOT_EXIST),
+                array(
+                    'type' => $e->getMessage()
+                )
+            );
+        }
+
+        if ($form->isValid()) {
+            $this->getDoctrine()->getManager()->persist($team);
+            $this->getDoctrine()->getManager()->flush();
+
+            $location = $this->generateUrl('api_teams_get', array('id' => $team->getId()));
+
+            return View::create(
+                $this->createRepresentation($team),
+                $status,
+                array(
+                    'Location' => $location,
+                )
+            );
+        }
+
+        throw new InvalidFormException($form);
+    }
+}
