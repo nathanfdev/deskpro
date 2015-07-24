802db3c4eade1bc286ed0bb71b730e5824beb7b4
Guillaume Pasquet <guillaume.pasquet@deskpro.com>
2015-07-24 15:26:49 +0100

    Ported task actions to Ampliflux.

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Actions/TaskListActions.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Actions/TaskListActions.js
@@ -1,119 +1,112 @@
-import { createAction } from "redux-actions";
-
+import { createAction } from "Ampliflux/actions";
 import DpApi from "DeskPRO/Bundle/AgentBundle/Services/DpApi";
-import ActionTypes from "./ActionTypes";
 
-export const setLoadedTasks = createAction(ActionTypes.TASKS_LOAD_TASKS);
-export const setLoadedProjects = createAction(ActionTypes.TASKS_LOAD_PROJECTS);
-export const setLoadedMyTasks = createAction(ActionTypes.TASKS_LOAD_MY_TASKS);
-export const setLoadedTeamTasks = createAction(ActionTypes.TASKS_LOAD_TEAM_TASKS);
-export const setLoadedDepartmentTasks = createAction(ActionTypes.TASKS_LOAD_DEPARTMENT_TASKS);
-export const setLoadedDelegatedTasks = createAction(ActionTypes.TASKS_LOAD_DELEGATED_TASKS);
-export const setLoadedUnassignedTasks = createAction(ActionTypes.TASKS_LOAD_UNASSIGNED_TASKS);
-export const setLoadedAgents = createAction(ActionTypes.TASKS_LOAD_AGENTS);
-export const setLoadedLabels = createAction(ActionTypes.TASKS_LOAD_LABELS);
-export const setLoadedTeams = createAction(ActionTypes.TASKS_LOAD_TEAMS);
-export const setLoadedDepartments = createAction(ActionTypes.TASKS_LOAD_DEPARTMENTS);
-export const setCreatedProject = createAction(ActionTypes.TASKS_POST_PROJECT);
-export const failedProject = createAction(ActionTypes.TASKS_POST_PROJECT_FAIL);
+export const loadTasks = createAction(
+  "TASKS_LOAD_TASKS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/tasks').then(
+      (value) => trigger(value.getData())
+    );
+  }
+);
 
-// TODO
+export const loadProjects = createAction(
+  "TASKS_LOAD_PROJECTS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/projects').then(
+      (value) => trigger(value.getData())
+    );
+  }
+);
 
-export const loadTasks = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/tasks').then((value) => {
-            dispatch(setLoadedTasks(value.getData()));
-        });
-    }
-};
+export const loadMyTasks = createAction(
+  "TASKS_LOAD_MY_TASKS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/tasks?assigned=me').then(
+      (value) => trigger(value.getData())
+    );
+  }
+);
 
-export const loadProjects = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/projects').then((value) => {
-            dispatch(setLoadedProjects(value.getData()));
-        });
-    }
-};
+export const loadTeamTasks = createAction(
+  "TASKS_LOAD_TEAM_TASKS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/tasks?assigned_team=me').then(
+      (value) => trigger(value.getData())
+    );
+  }
+);
 
-export const loadMyTasks = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/tasks?assigned=me').then((value) => {
-            dispatch(setLoadedMyTasks(value.getData()));
-        });
-    }
-};
+export const loadDepartmentTasks = createAction(
+  "TASKS_LOAD_DEPARTMENT_TASKS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/tasks?assigned_department=me').then(
+      (value) => trigger(value.getData())
+    );
+  }
+);
 
-export const loadTeamTasks = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/tasks?assigned_team=me').then((value) => {
-            dispatch(setLoadedTeamTasks(value.getData()));
-        });
-    }
-};
+export const loadDelegatedTasks = createAction(
+  "TASKS_LOAD_DELEGATED_TASKS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/tasks?assigned=not_me&creator=me').then(
+      (value) => trigger(value.getData())
+    );
+  }
+);
 
-export const loadDepartmentTasks = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/tasks?assigned_department=me').then((value) => {
-            dispatch(setLoadedDepartmentTasks(value.getData()));
-        });
-    }
-};
+export const loadUnassignedTasks = createAction(
+  "TASKS_LOAD_UNASSIGNED_TASKS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/tasks?assigned=null&assigned_team=null&assigned_department=null').then(
+      (trigger) => trigger(value.getData())
+    );
+  }
+);
 
-export const loadDelegatedTasks = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/tasks?assigned=not_me&creator=me').then((value) => {
-            dispatch(setLoadedDelegatedTasks(value.getData()));
-        });
-    }
-};
+export const loadAgents = createAction(
+  "TASKS_LOAD_AGENTS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/people?is_agent=1&not_me=1').then(
+      (value) => trigger(value.getData())
+    );
+  }
+);
 
-export const loadUnassignedTasks = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/tasks?assigned=null&assigned_team=null&assigned_department=null').then((value) => {
-            dispatch(setLoadedUnassignedTasks(value.getData()));
-        });
-    }
-};
+export const loadLabels = createAction(
+  "TASKS_LOAD_LABELS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/task_labels').then(
+      (value) => trigger(value.getData())
+    );
+  }
+);
 
-export const loadAgents = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/people?is_agent=1&not_me=1').then((value) => {
-            dispatch(setLoadedAgents(value.getData()));
-        });
-    }
-};
+export const loadTeams = createAction(
+  "TASKS_LOAD_TEAMS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/teams').then(
+      (value) => trigger(value.getData())
+    );
+  }
+);
 
-export const loadLabels = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/task_labels').then((value) => {
-            dispatch(setLoadedLabels(value.getData()));
-        });
-    }
-};
+export const loadDepartments = createAction(
+  "TASKS_LOAD_DEPARTMENTS",
+  (trigger) => {
+    DpApi.sendGet('DP_API/departments').then(
+      (value) => (value.getData())
+    );
+  }
+);
 
-export const loadTeams = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/teams').then((value) => {
-            dispatch(setLoadedTeams(value.getData()));
-        });
-    }
-};
-
-export const loadDepartments = () => {
-    return dispatch => {
-        DpApi.sendGet('DP_API/departments').then((value) => {
-            dispatch(setLoadedDepartments(value.getData()));
-        });
-    }
-};
-
-export const createProject = (data) => {
-    return dispatch => {
-        DpApi.sendPost('DP_API/projects', data).then((value) => {
-            dispatch(setCreatedProject(value.getData()));
-        },
-        (value) => {
-            dispatch(failedProject(value.errors));
-        });
-    }
-}
+export const failedProject = createAction("TASKS_POST_PROJECT_FAIL");
+export const createProject = createAction(
+  "TASKS_POST_PROJECT",
+  (trigger, data) => {
+    DpApi.sendPost('DP_API/projects', data).then(
+      (value) => trigger(value.getData()),
+      (value) => trigger(value.errors, failedProject.actionType)
+    );
+  }
+);
