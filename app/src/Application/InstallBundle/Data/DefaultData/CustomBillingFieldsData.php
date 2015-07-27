0d2a6436af15760bcfc2c6a16fac684ba0bb81f0
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-07-27 15:43:49 +0100

    Fix billing custom data

--- app/src/Application/InstallBundle/Data/DefaultData/CustomBillingFieldsData.php
+++ app/src/Application/InstallBundle/Data/DefaultData/CustomBillingFieldsData.php
@@ -38,8 +38,6 @@ class CustomBillingFieldsData extends AbstractDefaultData
 {
     public function runInstall()
     {
-        $this->getDb()->executeUpdate("DELETE FROM custom_def_billing");
-        $this->getDb()->executeUpdate("DELETE FROM custom_data_billing");
         $this->getDb()->executeQuery("
             INSERT INTO `custom_def_billing`
 				(`parent_id`, `app_id`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `handler_class`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `default_value`, `is_agent_field`)
@@ -48,13 +46,18 @@ class CustomBillingFieldsData extends AbstractDefaultData
 		");
     }
 
+    public function runInstallViaUpgrade()
+    {
+        // nothing because the upgrader installs the comment field as part of the upgrade step
+    }
+
     public function runReset()
     {
-        $this->runInstall();
+        // nothing
     }
 
     public function runSync()
     {
-        $this->runInstall();
+        // nothing
     }
 }
