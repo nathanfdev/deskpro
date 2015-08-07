7e67b5b8c5754de89c196f1701d43824b63c691b
Sergey <laa513@gmail.com>
2015-08-07 18:01:29 +0300

    Revert "[debug] expanded reducers"
    This reverts commit 4d77ba531e010c13ab57ec4e74b8dbf1f6728370.

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Chat/Reducers/ChatConversationsNavFrame.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Chat/Reducers/ChatConversationsNavFrame.js
@@ -12,20 +12,10 @@ export default class ChatConversationsNavFrame extends Reducer {
 
   registerHandlers() {
     this
-      .r(types.CHAT_LOAD_MY_CONVERSATIONS_COUNTS, this.myChatsCountsLoaded)
-      .r(types.CHAT_LOAD_ALL_CONVERSATIONS_COUNTS, this.allChatsCountsLoaded)
+      .r(types.CHAT_LOAD_MY_CONVERSATIONS_COUNTS, this.setPayload('myChats'))
+      .r(types.CHAT_LOAD_ALL_CONVERSATIONS_COUNTS, this.setPayload('allChats'))
       .r(types.CHAT_LOAD_AGENT_NAME, this.agentNameLoaded)
     ;
-  }
-
-  myChatsCountsLoaded(state, {payload}) {
-    console.log("--myChatsCountsLoaded", state);
-    return {...state, myChats: payload};
-  }
-
-  allChatsCountsLoaded(state, {payload}) {
-    console.log("--allChatsCountsLoaded", state);
-    return {...state, allChats: payload};
   }
 
   agentNameLoaded(state, action) {
