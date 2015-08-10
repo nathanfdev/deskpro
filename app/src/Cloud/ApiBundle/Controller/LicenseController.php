a5407f2e5558445f2e613d03c96c996450090701
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-08-10 16:04:51 +0100

    Add special custom authcode (for use with fake UK cloud)

--- app/src/Cloud/ApiBundle/Controller/LicenseController.php
+++ app/src/Cloud/ApiBundle/Controller/LicenseController.php
@@ -114,17 +114,23 @@ class LicenseController extends BaseLicenseController
         $this->em->persist($tmpdata);
         $this->em->flush();
 
-        $url = DP_MA_SERVER_SECURE . '/cloud/start/'.DPC_SITE_ID.'/'. $tmpdata->getCode();
+        $code = $tmpdata->getCode();
+
+        if ($custom_code = $this->settings->get('custom_cloud_billing_authcode')) {
+            $code = "XX-" . $custom_code;
+        }
+
+        $url = DP_MA_SERVER_SECURE . '/cloud/start/'.DPC_SITE_ID.'/'. $code;
         if (defined('DP_CLOUD_LIC_URL')) {
             $url = str_replace(
                 array('{SITE_ID}', '{SITE_AUTH}'),
-                array(DPC_SITE_ID, $tmpdata->getCode()),
+                array(DPC_SITE_ID, $code),
                 DP_CLOUD_LIC_URL
             );
         }
 
         return $this->createJsonResponse(array(
-            'code'   => $tmpdata->getCode(),
+            'code'   => $code,
             'ma_url' => $url
         ));
     }
