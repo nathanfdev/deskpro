98c6885986f11bdaa6531756ef92475a8fa8ae03
Sergey <laa513@gmail.com>
2015-08-13 12:08:02 +0300

    API /people/counts?is_agent=1&is_deleted=0 endpoint

---
+++ app/src/DeskPRO/Bundle/AppBundle/DataService/People/PeopleCountCriteria.php
@@ -0,0 +1,98 @@
+<?php
+/**************************************************************************\
+| DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+| a British company located in London, England.                            |
+|                                                                          |
+| All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+|                                                                          |
+| The license agreement under which this software is released              |
+| can be found at http://www.deskpro.com/license                           |
+|                                                                          |
+| By using this software, you acknowledge having read the license          |
+| and agree to be bound thereby.                                           |
+|                                                                          |
+| Please note that DeskPRO is not free software. We release the full       |
+| source code for our software because we trust our users to pay us for    |
+| the huge investment in time and energy that has gone into both creating  |
+| this software and supporting our customers. By providing the source code |
+| we preserve our customers' ability to modify, audit and learn from our   |
+| work. We have been developing DeskPRO since 2001, please help us make it |
+| another decade.                                                          |
+|                                                                          |
+| Like the work you see? Think you could make it better? We are always     |
+| looking for great developers to join us: http://www.deskpro.com/jobs/    |
+|                                                                          |
+| ~ Thanks, Everyone at Team DeskPRO                                       |
+\**************************************************************************/
+
+/**
+ * DeskPRO.
+ */
+
+namespace DeskPRO\Bundle\AppBundle\DataService\People;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Class PeopleCountCriteria
+ */
+class PeopleCountCriteria
+{
+    /**
+     * @var array
+     */
+    protected $filters = [];
+
+    /**
+     * ChatCountCriteria constructor.
+     *
+     * @param array $filters
+     */
+    protected function __construct(array $filters)
+    {
+        $this->filters = $filters;
+    }
+
+
+    /**
+     * @param array $params
+     * @param OptionsResolver $resolver
+     * @return PeopleCountCriteria
+     */
+    public static function fromParameters(array $params, OptionsResolver $resolver)
+    {
+        self::configureResolver($resolver);
+        $filters = $resolver->resolve($params);
+
+        return new self($filters);
+    }
+
+    /**
+     * @param QueryBuilder $qb
+     */
+    public function applyFilters(QueryBuilder $qb)
+    {
+        $alias = $qb->getRootAliases()[0];
+
+        foreach ($this->filters as $field => $value) {
+            switch ($field) {
+                case 'is_agent':
+                case 'is_deleted':
+                    $qb->andWhere($qb->expr()->eq("$alias.$field", ":$field"));
+                    $qb->setParameter($field, $value);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    private static function configureResolver(OptionsResolver $resolver)
+    {
+        $resolver->setDefined(['is_agent', 'is_deleted']);
+        $resolver->setAllowedValues('is_agent', ['0', '1']);
+        $resolver->setAllowedValues('is_deleted', ['0', '1']);
+    }
+}
