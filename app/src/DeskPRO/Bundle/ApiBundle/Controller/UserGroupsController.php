4489464b501ea5810c7d59ec6532269b7f53cd40
Sergey <laa513@gmail.com>
2015-08-13 15:39:47 +0300

    Removed redundant group_by from User Group counts request

--- app/src/DeskPRO/Bundle/ApiBundle/Controller/UserGroupsController.php
+++ app/src/DeskPRO/Bundle/ApiBundle/Controller/UserGroupsController.php
@@ -34,9 +34,7 @@
 
 namespace DeskPRO\Bundle\ApiBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\Annotations\Get;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
@@ -92,15 +90,10 @@ class UserGroupsController extends BaseController
      * )
      * @Get("/user_groups/counts", name="api_user_group_count_users")
      */
-    public function countUsersInGroups(Request $request)
+    public function countUsersInGroups()
     {
         /** @var \DeskPRO\Bundle\AppBundle\DataService\UserGroups\UserGroupsDataService $service */
         $service = $this->get('data.user_groups');
-
-        if ($request->get('group_by') !== 'user_groups') {
-            throw new BadRequestHttpException(
-                'You must provide group_by query parameter. Allowed values: user_groups.');
-        }
 
         return View::create(
             $this->createRepresentation($service->countPeopleInUserGroups()),
--- app/tests/features/api/user_groups/user_group_counts.feature
+++ app/tests/features/api/user_groups/user_group_counts.feature
@@ -8,7 +8,7 @@ Feature: /user_group/counts endpoint
     And my request is authenticated
 
   Scenario: I get counts of users for all user groups
-    When I send a GET request to "/api/v2/user_groups/counts?group_by=user_groups"
+    When I send a GET request to "/api/v2/user_groups/counts"
     Then the response should be in JSON
     And the response status code should be 200
     And the JSON node "data.count" should be equal to 4
