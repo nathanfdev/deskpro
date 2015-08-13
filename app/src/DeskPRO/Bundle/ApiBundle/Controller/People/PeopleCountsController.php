0c408d72c0a160d66cbd1a29be2f4347814ec232
Sergey <laa513@gmail.com>
2015-08-13 12:08:02 +0300

    API /people/counts?is_agent=1&is_deleted=0 endpoint

---
+++ app/src/DeskPRO/Bundle/ApiBundle/Controller/People/PeopleCountsController.php
@@ -0,0 +1,97 @@
+<?php
+/**************************************************************************\
+ * | DeskPRO (r) has been developed by DeskPRO Ltd. http://www.deskpro.com/   |
+ * | a British company located in London, England.                            |
+ * |                                                                          |
+ * | All source code and content Copyright (c) 2012, DeskPRO Ltd.             |
+ * |                                                                          |
+ * | The license agreement under which this software is released              |
+ * | can be found at http://www.deskpro.com/license                           |
+ * |                                                                          |
+ * | By using this software, you acknowledge having read the license          |
+ * | and agree to be bound thereby.                                           |
+ * |                                                                          |
+ * | Please note that DeskPRO is not free software. We release the full       |
+ * | source code for our software because we trust our users to pay us for    |
+ * | the huge investment in time and energy that has gone into both creating  |
+ * | this software and supporting our customers. By providing the source code |
+ * | we preserve our customers' ability to modify, audit and learn from our   |
+ * | work. We have been developing DeskPRO since 2001, please help us make it |
+ * | another decade.                                                          |
+ * |                                                                          |
+ * | Like the work you see? Think you could make it better? We are always     |
+ * | looking for great developers to join us: http://www.deskpro.com/jobs/    |
+ * |                                                                          |
+ * | ~ Thanks, Everyone at Team DeskPRO                                       |
+ * \**************************************************************************/
+
+/**
+ * DeskPRO
+ *
+ * @package DeskPRO
+ */
+
+namespace DeskPRO\Bundle\ApiBundle\Controller\People;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\Exception\InvalidArgumentException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Controller\Annotations\Get;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use DeskPRO\Bundle\ApiBundle\Controller\BaseController;
+use DeskPRO\Bundle\AppBundle\DataService\People\PeopleCountCriteria;
+
+class PeopleCountsController extends BaseController implements ClassResourceInterface
+{
+    /**
+     * @ApiDoc(
+     *      description="Get people count",
+     *      parameters={
+     *          {
+     *              "name"="is_agent",
+     *              "requirement"="0|1",
+     *              "description"="Agents filter",
+     *              "dataType"="integer",
+     *              "required"=false
+     *          },
+     *          {
+     *              "name"="is_deleted",
+     *              "requirement"="0|1",
+     *              "description"="Soft-deleted filter",
+     *              "dataType"="integer",
+     *              "required"=false
+     *          }
+     *      },
+     *      statusCodes={
+     *          200="Success",
+     *          400="Bad Request",
+     *          404="Not Found"
+     *      },
+     *      output="DeskPRO\Bundle\AppBundle\CountBadge\Count"
+     * )
+     * @Get("/people/counts", name="api_people_counts")
+     */
+    public function countAction(Request $request)
+    {
+        /** @var \DeskPRO\Bundle\AppBundle\DataService\People\PeopleCountsDataService $dataService */
+        $dataService = $this->get('data.people_counts');
+
+        $params = $request->query->all();
+        try {
+            $criteria = PeopleCountCriteria::fromParameters($params, new OptionsResolver());
+        } catch (InvalidArgumentException $e) {
+            throw new BadRequestHttpException($e->getMessage());
+        }
+
+        $count = $dataService->countPeople($criteria);
+
+        return View::create(
+            $this->createRepresentation($count),
+            Response::HTTP_OK
+        );
+    }
+}
