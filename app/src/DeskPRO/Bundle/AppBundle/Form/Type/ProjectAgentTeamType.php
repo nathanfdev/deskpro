64c27a19eba2a57d88ed291179eaa3124c879d5f
Rob Lowcock <rob.lowcock@deskpro.com>
2015-08-14 18:05:48 +0100

    Started work on refactoring endpoints for attaching agents, teams and departments to tasks

--- app/src/DeskPRO/Bundle/AppBundle/Form/Type/ProjectAgentTeamType.php
+++ app/src/DeskPRO/Bundle/AppBundle/Form/Type/ProjectAgentTeamType.php
@@ -3,13 +3,13 @@
 namespace DeskPRO\Bundle\AppBundle\Form\Type;
 
 use Application\DeskPRO\ORM\EntityManager;
-use DeskPRO\Bundle\AppBundle\Form\DataTransformer\PersonProjectMemberTransformer;
+use DeskPRO\Bundle\AppBundle\Form\DataTransformer\AgentTeamProjectMemberTransformer;
 use DeskPRO\Bundle\AppBundle\Form\EventListener\ReplaceNotSubmittedValuesWithDefaultsListener;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class PersonType extends AbstractType
+class ProjectAgentTeamType extends AbstractType
 {
     private $entityManager;
 
@@ -20,7 +20,7 @@ class PersonType extends AbstractType
 
     public function getName()
     {
-        return 'person';
+        return 'project_agent_team';
     }
 
     public function getParent()
@@ -30,14 +30,14 @@ class PersonType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->addModelTransformer(new PersonProjectMemberTransformer($this->entityManager, $options['project']));
-        $builder->addViewTransformer(new PersonProjectMemberTransformer($this->entityManager, $options['project']));
+        $builder->addModelTransformer(new AgentTeamProjectMemberTransformer($this->entityManager, $options['project']));
+        $builder->addViewTransformer(new AgentTeamProjectMemberTransformer($this->entityManager, $options['project']));
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Application\DeskPRO\Entity\Person',
+            'data_class' => 'Application\DeskPRO\Entity\AgentTeam',
             'project' => null,
         ));
     }
--- app/src/DeskPRO/Bundle/AppBundle/Form/Type/ProjectPersonType.php
+++ app/src/DeskPRO/Bundle/AppBundle/Form/Type/ProjectPersonType.php
@@ -3,13 +3,13 @@
 namespace DeskPRO\Bundle\AppBundle\Form\Type;
 
 use Application\DeskPRO\ORM\EntityManager;
-use DeskPRO\Bundle\AppBundle\Form\DataTransformer\AgentTeamProjectMemberTransformer;
+use DeskPRO\Bundle\AppBundle\Form\DataTransformer\PersonProjectMemberTransformer;
 use DeskPRO\Bundle\AppBundle\Form\EventListener\ReplaceNotSubmittedValuesWithDefaultsListener;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class AgentTeamType extends AbstractType
+class ProjectPersonType extends AbstractType
 {
     private $entityManager;
 
@@ -20,7 +20,7 @@ class AgentTeamType extends AbstractType
 
     public function getName()
     {
-        return 'agent_team';
+        return 'project_person';
     }
 
     public function getParent()
@@ -30,14 +30,14 @@ class AgentTeamType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->addModelTransformer(new AgentTeamProjectMemberTransformer($this->entityManager, $options['project']));
-        $builder->addViewTransformer(new AgentTeamProjectMemberTransformer($this->entityManager, $options['project']));
+        $builder->addModelTransformer(new PersonProjectMemberTransformer($this->entityManager, $options['project']));
+        $builder->addViewTransformer(new PersonProjectMemberTransformer($this->entityManager, $options['project']));
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Application\DeskPRO\Entity\AgentTeam',
+            'data_class' => 'Application\DeskPRO\Entity\Person',
             'project' => null,
         ));
     }
