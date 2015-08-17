d2e369552296993f06b4163b89b51583b3d834d2
Sergey <laa513@gmail.com>
2015-08-17 17:02:28 +0300

    LabelsDictionary componnt

---
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/NavFrame/labels.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export class LabelsDictionary extends React.Component {
+  render() {
+    const grouped = this.groupByFirstLetter(this.props.labels);
+
+    let groupKey = 0, labelKey = 0;
+
+    return (
+      <section className="sidebar-list sidebar-list-labels tasks-nav-labels">
+        <div className="sidebar-label-list sidebar-list">
+          {grouped.map(group => {
+            return (
+              <div key={groupKey++}>
+                <span className="labelCharacter">{group.letter}</span>
+                <ul>
+                  {group.labels.map(label => <li key={labelKey++}><a href="#" className="item-label">{label}</a></li>)}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+
+  groupByFirstLetter(labels) {
+    const dictionary = {};
+
+    for (let i = 0, label, letter; i < labels.length; i++) {
+      label = labels[i];
+      letter = label[0].toUpperCase();
+      if (!dictionary.hasOwnProperty(letter)) {
+        dictionary[letter] = [];
+      }
+
+      dictionary[letter].push(label);
+    }
+
+    const letters = Object.keys(dictionary);
+    const grouped = [];
+    for (let i = 0; i < letters.length; i++) {
+      grouped.push({letter: letters[i], labels: dictionary[letters[i]]});
+    }
+
+    return grouped;
+  }
+}
