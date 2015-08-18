9ed3f3a92410ba217a0baebf280be50f212b5bc7
Chris Tickner <chris.tickner@gmail.com>
2015-08-18 14:39:06 -0400

    [JS] remove DeskPRO/Component/DependencyWidget/Container and related code

--- pub/src/DeskPRO/Bundle/PortalBundle/PortalApp.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/PortalApp.js
@@ -1,7 +1,5 @@
 require("babel/polyfill");
 
-import Container from "DeskPRO/Component/DependencyInjection/Container";
-import App from "DeskPRO/Component/DependencyInjection/AppContainer";
 import Http from "DeskPRO/Component/Http/Http";
 import UrlCorrector from "DeskPRO/Bundle/AppBundle/Http/UrlCorrector";
 import PortalPage from "DeskPRO/Bundle/PortalBundle/PageWidget/PortalPage";
@@ -10,24 +8,11 @@ import $ from "jquery";
 export default class PortalApp {
   constructor() {
     window.$ = $;
-    window.App = App;
     window.PortalApp = this;
-
-    this.container = new Container();
-    App.setContainer(this.container);
-
-    this.container.registerFactory('http', function() {
-      let http = new Http(j$.ajax);
-      http.enableJsonPayloads();
-
-      http.addInterceptor(new UrlCorrector(window.DP_BASE_URL));
-
-      return http;
-    });
   }
 
   run() {
-    let page = new PortalPage(this.container);
+    let page = new PortalPage();
     page.renderWhenReady();
   }
 }
--- pub/src/DeskPRO/Component/React/ContainerComponent.js
+++ pub/src/DeskPRO/Component/React/ContainerComponent.js
@@ -1,14 +1,11 @@
 import Component from "./Component";
-import App from "DeskPRO/Component/DependencyInjection/AppContainer";
 
 export default class ConatinerComponent extends Component {
   constructor(props) {
-    this.container = App.getContainer();
 
     let classInit = this.init;
     classInit.$inject = this.init$inject();
     this.init = () => {
-      this.container.invoke(classInit, this);
     };
 
     super(props);
