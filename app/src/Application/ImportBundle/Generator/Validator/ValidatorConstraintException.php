d5497b36571316989cc221a68a1be049cd51a7b2
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-08-20 08:30:32 +0300

    importer validator type hinting

--- app/src/Application/ImportBundle/Generator/Validator/ValidatorConstraintException.php
+++ app/src/Application/ImportBundle/Generator/Validator/ValidatorConstraintException.php
@@ -28,7 +28,7 @@
 namespace Application\ImportBundle\Generator\Validator;
 
 use Application\ImportBundle\Entity;
-use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
  * Exporter validator exception
@@ -44,17 +44,17 @@ final class ValidatorConstraintException extends \Exception implements Validator
     private $entity;
 
     /**
-     * @var ConstraintViolationList
+     * @var ConstraintViolationListInterface
      */
     private $errors;
 
     /**
      * Constructor
      *
-     * @param Entity\EntityInterface  $entity
-     * @param ConstraintViolationList $errors
+     * @param Entity\EntityInterface           $entity
+     * @param ConstraintViolationListInterface $errors
      */
-    public function __construct(Entity\EntityInterface $entity, ConstraintViolationList $errors)
+    public function __construct(Entity\EntityInterface $entity, ConstraintViolationListInterface $errors)
     {
         $this->entity = $entity;
         $this->errors = $errors;
@@ -73,7 +73,7 @@ final class ValidatorConstraintException extends \Exception implements Validator
     /**
      * Returns a collection of the errors
      *
-     * @return ConstraintViolationList
+     * @return ConstraintViolationListInterface
      */
     public function getErrors()
     {
