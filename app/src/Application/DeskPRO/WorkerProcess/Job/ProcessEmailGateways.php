6c9372aa5ce7da123d75d336e4953729d0690228
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-09-02 10:06:24 +0100

    Increase gateway time limit a bit

--- app/src/Application/DeskPRO/WorkerProcess/Job/ProcessEmailGateways.php
+++ app/src/Application/DeskPRO/WorkerProcess/Job/ProcessEmailGateways.php
@@ -129,11 +129,11 @@ class ProcessEmailGateways extends AbstractJob
             }
 
             $runner->setAccounts(array($account));
-            $runner->execute(180);
+            $runner->execute(300);
 
         } else {
             $runner->loadAccountsFromDb(false);
-            $runner->execute(180);
+            $runner->execute(300);
         }
 
         // The PHP time limit would've been set above while processing messages,
