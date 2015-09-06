70fcfb91c3f70f6c480f4931d26b326dcad5df21
Chris Tickner <chris.tickner@gmail.com>
2015-09-06 10:22:53 -0400

    [Temp] disable elastica in search engine for build

--- app/src/Application/DeskPRO/DependencyInjection/SystemServices/SearchEngineService.php
+++ app/src/Application/DeskPRO/DependencyInjection/SystemServices/SearchEngineService.php
@@ -40,17 +40,17 @@ class SearchEngineService
 {
     public static function create(DeskproContainer $container)
     {
-        if ($container->getSetting('elastica.enabled')) {
-            $user_search = new SearchEngine\Elastic\UserSearch(
-                $container->get('fos_elastica.index.deskpro'),
-                new SearchEngine\Elastic\ElasticaResultsTransformer($container->getEm())
-            );
-        } else {
+        //if ($container->getSetting('elastica.enabled')) {
+        //    $user_search = new SearchEngine\Elastic\UserSearch(
+        //        $container->get('fos_elastica.index.deskpro'),
+        //        new SearchEngine\Elastic\ElasticaResultsTransformer($container->getEm())
+        //    );
+        //} else {
             $user_search = new SearchEngine\Mysql\UserSearch(
                 $container->getDbRead('search.searcher.content'),
                 new SearchEngine\Mysql\MysqlResultsTransformer($container->getEm())
             );
-        }
+        //}
 
         $se = new SearchEngine\SearchEngine($user_search);
 
