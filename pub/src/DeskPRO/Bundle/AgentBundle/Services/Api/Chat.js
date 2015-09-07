17d09722e19afb00f442d641bd1c19dedaeac405
Sergey <laa513@gmail.com>
2015-09-07 17:27:34 +0300

    Sample tests with ugly relative ../../../../ paths

--- pub/src/DeskPRO/Bundle/AgentBundle/Services/Api/Chat.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Services/Api/Chat.js
@@ -15,5 +15,5 @@ export function loadCounts(groupBy, agent) {
  * @return Promise
  */
 export function load(filters) {
-  return DpApi.sendGet('DP_API/user_chats?' + compileParams(filters));
+  return DpApi.sendGet('DP_API/user_chats' + (filters ? '?' + compileParams(filters) : ''));
 }
