59d2c0e1e202cec115e9da547020b52c753b4aeb
Sergey <laa513@gmail.com>
2015-09-07 19:47:57 +0300

    NavFrame component test and minor fix

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/NavFrame/frame.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/NavFrame/frame.js
@@ -1,26 +1,27 @@
 import React from 'react';
 
 export class NavFrame extends React.Component {
-    render() {
-        let outer, inner = this.props.children;
-        if (this.props.children.length) {
-            this.props.children.forEach((child) => {
-                if (child.props.part === 'outer') {
-                    outer = child;
-                } else if (child.props.part === 'inner') {
-                    inner = child;
-                }
-            });
+  render() {
+    let outer, inner = this.props.children;
+
+    if ((this.props.children instanceof Array) && this.props.children.length) {
+      this.props.children.forEach((child) => {
+        if (child.props.part === 'outer') {
+          outer = child;
+        } else if (child.props.part === 'inner') {
+          inner = child;
         }
+      });
+    }
 
-        return (
-            <div>
+    return (
+      <div>
 
-                {outer}
+        {outer}
 
-                <section className="task-nav-frame dp-nav-frame">
-                    <div className="sidebar-wrapper" id="sidebar-wrapper">
-                        <a className="collapse-button" href="#"><i className="fa fa-angle-right"></i></a>
+        <section className="task-nav-frame dp-nav-frame">
+          <div className="sidebar-wrapper" id="sidebar-wrapper">
+            <a className="collapse-button" href="#"><i className="fa fa-angle-right"></i></a>
             <span className="collapse-controls">
               <span className="disc"></span>
               <span className="disc"></span>
@@ -28,35 +29,35 @@ export class NavFrame extends React.Component {
               <span className="disc"></span>
               <span className="disc"></span>
             </span>
-                        <aside className="sidebar has-tabs" id="sidebar">
+            <aside className="sidebar has-tabs" id="sidebar">
 
-                            {inner}
+              {inner}
 
-                        </aside>
-                    </div>
-                </section>
-            </div>
-        );
-    }
+            </aside>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export class NavFrameHeader extends React.Component {
-    render() {
-        const iconClass = 'fa ' + this.props.icon;
+  render() {
+    const iconClass = 'fa ' + this.props.icon;
 
-        return (
-            <div className="sidebar-title">
-              <span className="sidebar-type-icon">
-                <i className={iconClass}></i>
-                <span className="help">
-                  <i className="fa fa-question"></i>
-                </span>
-              </span>
+    return (
+      <div className="sidebar-title">
+        <span className="sidebar-type-icon">
+          <i className={iconClass}></i>
+          <span className="help">
+            <i className="fa fa-question"></i>
+          </span>
+        </span>
 
-                <h1>{this.props.children}</h1>
-                <hr />
-                <a href="#" className="slider-control"></a>
-            </div>
-        );
-    }
+        <h1>{this.props.children}</h1>
+        <hr />
+        <a href="#" className="slider-control"></a>
+      </div>
+    );
+  }
 }
