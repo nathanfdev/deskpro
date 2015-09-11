4e7757491092f8ecba5d8b535c4c0150a251d8f4
Sergey <laa513@gmail.com>
2015-09-11 14:35:06 +0300

    Extracted fixPaths() method in preprocessor

--- pub/jest-preprocessor.js
+++ pub/jest-preprocessor.js
@@ -3,9 +3,16 @@ var path = require('path');
 
 module.exports = {
   process: function(src, filename) {
-    var result = src;
+    var result = this.fixPaths(src, filename);
+    result = babelJest.process(result, filename);
+
+    return result;
+  },
+
+  fixPaths: function(result, filename) {
 
     // Fix absolute paths to relative for source imports and tests dontMock and require
+
     var index = filename.indexOf(path.sep + 'pub' + path.sep + 'src');
     if (index > -1) {
       var relative = filename.substr(index);
@@ -15,25 +22,21 @@ module.exports = {
 
       // DeskPRO/... paths ---------------------------------------------------------------------------------------------
 
-      // sources imports
       result = result.replace(
         /^import(.*)[\"\']DeskPRO(.*)[\"\'](.*)$/gm,
         "import$1'" + margin + "DeskPRO$2'$3"
       );
-
-      // tests dontMock
       result = result.replace(
         /^jest\.dontMock\([\"\']DeskPRO(.*)[\"\']\)(.*)$/gm,
         "jest.dontMock('" + margin + "DeskPRO$1')$2"
       );
-
-      // tests require
       result = result.replace(
         /^(.+)([\s=])require\([\"\']DeskPRO(.*)[\"\']\)(.*)$/gm,
         "$1$2require('" + margin + "DeskPRO$3')$4"
       );
 
       // Ampliflux/... paths -------------------------------------------------------------------------------------------
+
       result = result.replace(
         /^import(.*)[\"\']Ampliflux(.*)[\"\'](.*)$/gm,
         "import$1'" + margin + "DeskPRO/Component/Ampliflux$2'$3"
@@ -41,14 +44,11 @@ module.exports = {
 
       // Helpers/... paths ----------------------------------------------------------------------------------------
 
-      // tests require
       result = result.replace(
         /^(.+)([\s=])require\([\"\']Helpers(.*)[\"\']\)(.*)$/gm,
         "$1$2require('" + margin + "tests/Helpers$3')$4"
       );
     }
-
-    result = babelJest.process(result, filename);
 
     return result;
   }
