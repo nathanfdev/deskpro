01ba56079cb00b4c1272de543c2d93a51ef713ee
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-09-12 10:23:39 +0300

    importer feedback label writer refactoring

--- app/src/Application/DeskPRO/Entity/Feedback.php
+++ app/src/Application/DeskPRO/Entity/Feedback.php
@@ -380,19 +380,32 @@ class Feedback extends ContentAbstract implements HighlightableModelInterface
     public function resetLabels()
     {
         foreach ($this->labels as $data) {
-            App::getOrm()->remove($data);
+            $this->labels->removeElement($data);
         }
 
-        $this->labels->clear();
         $this->_onPropertyChanged('labels', null, $this->labels);
+        return $this;
+    }
+
+    /**
+     * @param LabelFeedback $label
+     *
+     * @return $this
+     */
+    public function addLabel(LabelFeedback $label)
+    {
+        $label['feedback'] = $this;
+        $this->labels->add($label);
 
         return $this;
     }
 
-    public function addLabel($label)
+    /**
+     * @return \Application\DeskPRO\Entity\LabelFeedback[]
+     */
+    public function getLabels()
     {
-        $label['feedback'] = $this;
-        $this->labels->add($label);
+        return $this->labels;
     }
 
 
