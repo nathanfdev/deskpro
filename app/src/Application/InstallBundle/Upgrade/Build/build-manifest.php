3e1906b65e36409e63b3080faae4626128b5a09d
Chris Tickner <chris.tickner@gmail.com>
2015-09-16 15:15:49 -0400

    rebuild build manifest

--- app/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
+++ app/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
@@ -1861,7 +1861,7 @@
 
 	1442418182 => array(
 		'file'      => '/src/Application/InstallBundle/Upgrade/Build/2015/09/Build1442418182.php',
-		'classname' => 'Application\InstallBundle\Upgrade\Build\Users/tickner/src/deskpro/old-portal/app/src/Application/InstallBundle/Upgrade/Build/2015/09/Build1442418182'
+		'classname' => 'Application\InstallBundle\Upgrade\Build\Build1442418182'
 	),
 
 );
