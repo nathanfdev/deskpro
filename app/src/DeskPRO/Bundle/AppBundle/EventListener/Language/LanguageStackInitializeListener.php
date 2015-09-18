2b334d0e604933d6d38048b969c373d35f19098d
Chris Tickner <chris.tickner@gmail.com>
2015-09-18 17:39:37 -0400

    [Portal] lang: negotiate accept-header, use person->language, fix lang in tags

--- app/src/DeskPRO/Bundle/AppBundle/EventListener/Language/LanguageStackInitializeListener.php
+++ app/src/DeskPRO/Bundle/AppBundle/EventListener/Language/LanguageStackInitializeListener.php
@@ -32,9 +32,13 @@
 namespace DeskPRO\Bundle\AppBundle\EventListener\Language;
 
 use Application\DeskPRO\Entity\Language;
+use Application\DeskPRO\Entity\Person;
 use DeskPRO\Bundle\AppBundle\Helper\IsProxyRequestHelper;
 use DeskPRO\Bundle\AppBundle\Language\LanguageManager;
 use DeskPRO\Bundle\PortalBundle\Routing\UrlMatcher;
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManager;
+use Negotiation\LanguageNegotiator;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -52,11 +56,21 @@ class LanguageStackInitializeListener implements EventSubscriberInterface
      * @var LoggerInterface
      */
     private $logger;
+    /**
+     * @var EntityManager
+     */
+    private $connection;
 
-    public function __construct(LanguageManager $language_manager, LoggerInterface $logger)
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    public function __construct(LanguageManager $language_manager, LoggerInterface $logger, EntityManager $em)
     {
         $this->language_manager = $language_manager;
         $this->logger           = $logger;
+        $this->em = $em;
     }
 
     public static function getSubscribedEvents()
@@ -79,8 +93,10 @@ class LanguageStackInitializeListener implements EventSubscriberInterface
 
         if (
             ($lang = $this->detectFromRequestPath($request))
-            || ($lang = $this->detectFromRequestCookies($request))
             || ($lang = $this->detectFromEsiQuery($request))
+            || ($lang = $this->detectFromPersonIfLoggedIn($request))
+            || ($lang = $this->detectFromRequestCookies($request))
+            || ($lang = $this->detectFromRequestHeaders($request))
         ) {
             $this->logger->info(
                 sprintf('detected "%s" and initializing language stack with it',
@@ -128,7 +144,7 @@ class LanguageStackInitializeListener implements EventSubscriberInterface
         return;
     }
 
-    private function detectFromEsiQuery(Request $request)
+    protected function detectFromEsiQuery(Request $request)
     {
         if (IsProxyRequestHelper::check($request)) {
             if ($lang_code = $request->query->get('lang_url_code')) {
@@ -136,6 +152,74 @@ class LanguageStackInitializeListener implements EventSubscriberInterface
 
                 return $this->language_manager->getLanguage($lang_code);
             }
+        }
+
+        return;
+    }
+
+    protected function detectFromPersonIfLoggedIn(Request $request)
+    {
+        // this is a request listener that runs before the security firewall
+        // the only way to detect language this early (needed for routing)
+        // is to manually fetch the sess_data and find the person_id
+        if ($session_id = $request->cookies->get('dpsid', null)) {
+            $query = $this->em->getConnection()->executeQuery(
+                'SELECT * FROM sess_data WHERE sess_id = :sess_id',
+                array(
+                    'sess_id' => $session_id
+                ),
+                array(
+                    'sess_id' => \PDO::PARAM_STR
+                )
+            );
+
+            if (!$sess_row = $query->fetch()) {
+                return;
+            }
+
+            if (!isset($sess_row['person_id'])) {
+                return;
+            }
+
+            if (!$person_id = $sess_row['person_id']) {
+                return;
+            }
+
+            if (!$person = $this->em->getRepository('DeskPRO:Person')->find($person_id)) {
+                return;
+            }
+
+            /** @var Person $person */
+            return $person->language;
+        }
+    }
+
+    protected function detectFromRequestHeaders(Request $request)
+    {
+        $negotiator = new LanguageNegotiator();
+
+        // get the language codes of all portal langs enabled
+        $langs = $this->language_manager->getEnabledLanguages();
+        $lang_codes = array_map(function(Language $lang) {
+            return $lang->getTwoLetterLanguageCode();
+        }, $langs);
+
+        // negotiate between our list of supported langs vs. the Accept-Language header
+        $header_lang = $negotiator->getBest(
+            $request->headers->get('Accept-Language'),
+            $lang_codes
+        );
+
+        // make sure the negotiated language was a language that was in the request headers
+        // it is possible that a language was negotiated that didn't exist in the request
+        // (e.g. if the request header had all langs that we do not support).
+        // it's better to move on and eventually use portal-defined default lang than
+        // to use a lang they didn't request.
+        $header_lang = $header_lang->getValue();
+        if (in_array($header_lang, $request->getLanguages())) {
+            try {
+                return $this->language_manager->getLanguage($header_lang);
+            } catch (\Exception $e) {}
         }
 
         return;
