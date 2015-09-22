73daa9745a345a946626bcf76bc15cdc4d2d25d3
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-09-22 18:31:30 +0100

    Remove debug line

--- pub/src/DeskPRO/Component/Ampliflux/middleware/loggerMiddleware.js
+++ pub/src/DeskPRO/Component/Ampliflux/middleware/loggerMiddleware.js
@@ -23,7 +23,6 @@ function jsValue(val) {
 
 export default function loggerMiddleware({ getState }) {
   return next => action => {
-    return next(action);
     if (!window.DP_ENABLE_ACTION_LOGGER) {
       return next(action);
     }
