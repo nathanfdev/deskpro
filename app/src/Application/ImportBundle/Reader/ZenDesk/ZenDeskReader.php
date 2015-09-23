9af1f767a93f9bf8cd635e7677ba8e33db026a7c
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-09-23 19:25:25 +0300

    zd reader check config error messages

--- app/src/Application/ImportBundle/Reader/ZenDesk/ZenDeskReader.php
+++ app/src/Application/ImportBundle/Reader/ZenDesk/ZenDeskReader.php
@@ -67,15 +67,8 @@ class ZenDeskReader extends AbstractReader implements ZenDeskReaderInterface
      */
     public function checkConfig()
     {
-        try {
-            $this->getSettings();
-            return true;
-
-        } catch (\Exception $e) {
-
-        }
-
-        return false;
+        $this->getSettings();
+        return true;
     }
 
     /**
