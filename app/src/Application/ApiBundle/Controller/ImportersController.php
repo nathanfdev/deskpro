ebb3546cdbe38b9120f183c961f53af0c1b2b669
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-09-23 09:45:46 +0300

    enabled osticket importer

--- app/src/Application/ApiBundle/Controller/ImportersController.php
+++ app/src/Application/ApiBundle/Controller/ImportersController.php
@@ -79,12 +79,6 @@ class ImportersController extends AbstractController implements ProtectedControl
         $ret = array();
 
         foreach ($importers as $importer) {
-            // TODO: remove when these are ready
-            if (
-                $importer['name'] === 'importers.osticket'
-            ) {
-                continue;
-            }
             $ret[] = array(
                 'id'          => str_replace('importers.', '', $importer['name']),
                 'title'       => $importer->getData('title'),
