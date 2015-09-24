ea49f1111d6fe9916cef7a3ef62f355c9a931daa
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-09-24 19:08:28 +0300

    importer json export custom def

--- app/src/Application/ImportBundle/Generator/Exporter/Parser/Json/PeopleCustomDef.php
+++ app/src/Application/ImportBundle/Generator/Exporter/Parser/Json/PeopleCustomDef.php
@@ -28,6 +28,8 @@
 namespace Application\ImportBundle\Generator\Exporter\Parser\Json;
 
 use Application\ImportBundle\Entity;
+use Application\ImportBundle\Generator\Exporter\Formatter\Transformer\TransformerConfiguration;
+use Application\ImportBundle\Generator\Exporter\Formatter\Transformer\TransformerInterface;
 use Application\ImportBundle\Generator\Exporter\Parser\ExportCollectionConfig;
 use Application\ImportBundle\Reader\Json\JsonReaderInterface;
 
@@ -78,6 +80,35 @@ final class PeopleCustomDef extends AbstractParser
      */
     protected function exportCustomDef(array $data)
     {
-        return new Entity\TicketCustomDef();
+        $formatted = $this->formatter->format($data, array(
+            'oid'           => TransformerInterface::TYPE_STRING,
+            'destination'   => TransformerConfiguration::create(TransformerInterface::TYPE_DESTINATION, array(
+                'prefix' => 'person_custom_def_',
+                'ref'    => 'oid',
+            )),
+            'sys_name'      => TransformerInterface::TYPE_STRING,
+            'parent_id'     => TransformerInterface::TYPE_STRING,
+            'title'         => TransformerInterface::TYPE_STRING,
+            'description'   => TransformerInterface::TYPE_STRING,
+            'handler_class' => TransformerInterface::TYPE_STRING,
+            'is_enabled'    => TransformerInterface::TYPE_BOOLEAN,
+            'options'       => TransformerInterface::TYPE_ARRAY,
+        ));
+
+        $entity = new Entity\PersonCustomDef();
+        $entity
+            ->setRawData($data)
+            ->setOid($formatted['oid'])
+            ->setDestination($formatted['destination'])
+            ->setSysName($formatted['sys_name'])
+            ->setParentId($formatted['parent_id'])
+            ->setTitle($formatted['title'])
+            ->setDestination($formatted['description'])
+            ->setHandlerClass($formatted['handler_class'])
+            ->setAsEnabled($formatted['is_enabled'])
+            ->setOptions($formatted['options'])
+        ;
+
+        return $entity;
     }
 }
