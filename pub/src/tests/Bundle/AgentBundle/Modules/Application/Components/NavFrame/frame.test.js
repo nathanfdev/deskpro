927185d9f8a6f44da7a5a808fce52da2d735cbb8
Sergey <laa513@gmail.com>
2015-09-29 14:37:43 +0300

    Fixed deprecated React API

--- pub/src/tests/Bundle/AgentBundle/Modules/Application/Components/NavFrame/frame.test.js
+++ pub/src/tests/Bundle/AgentBundle/Modules/Application/Components/NavFrame/frame.test.js
@@ -2,6 +2,7 @@ jest.dontMock('DeskPRO/Bundle/AgentBundle/Modules/Application/Components/NavFram
 
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 describe('UI: NavFrame component', () => {
   const frame = require('DeskPRO/Bundle/AgentBundle/Modules/Application/Components/NavFrame/frame.js');
@@ -10,7 +11,7 @@ describe('UI: NavFrame component', () => {
     const NavFrame = frame.NavFrame;
 
     it('should accept "inner" and "outer" parts HTML', () => {
-      const html = React.renderToStaticMarkup(
+      const html = renderToStaticMarkup(
         <NavFrame>
           <div part="inner"><i>Inner part content</i></div>
           <div part="outer"><p>Outer part content</p></div>
@@ -23,7 +24,7 @@ describe('UI: NavFrame component', () => {
     it("should render its' children as inner part if parts are omitted", () => {
       const component = TestUtils.renderIntoDocument(<NavFrame>Content</NavFrame>);
       const inner = TestUtils.findRenderedDOMComponentWithTag(component, 'aside');
-      expect(inner.getDOMNode().textContent).toEqual('Content');
+      expect(inner.textContent).toEqual('Content');
     });
   });
 
@@ -33,7 +34,7 @@ describe('UI: NavFrame component', () => {
     it("should render its' content inside h1 tag", () => {
       const component = TestUtils.renderIntoDocument(<NavFrameHeader>Header text</NavFrameHeader>);
       const h1 = TestUtils.findRenderedDOMComponentWithTag(component, 'h1');
-      expect(h1.getDOMNode().textContent).toEqual('Header text');
+      expect(h1.textContent).toEqual('Header text');
     });
 
     it('should render an icon passed via the "icon" property', () => {
