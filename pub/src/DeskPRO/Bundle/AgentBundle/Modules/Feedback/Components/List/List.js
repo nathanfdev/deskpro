7bb317206b207ed95a2b34a406802af27fc22c87
Alex Yakutskiy <a.yakutskiy@gmail.com>
2015-09-29 09:37:09 +0300

    Mass action checkbox

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Feedback/Components/List/List.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Feedback/Components/List/List.js
@@ -15,23 +15,12 @@ export class List extends Component {
     currentViewMode: PropTypes.object.isRequired,
   };
 
-
-  render() {
-    return (
-      <ListFrame>
-        <FeedbackListControlBar />
-        {this.contentChoice()}
-      </ListFrame>
-    );
-  }
-
   contentChoice() {
     const {currentContent} = this.props;
     if (currentContent === 'comments') {
       return this.renderComments();
-    } else {
-      return this.renderFeedback();
     }
+    return this.renderFeedback();
   }
 
   renderFeedback() {
@@ -42,11 +31,9 @@ export class List extends Component {
         <FeedbackList elements={feedback} people={people} feedbackTypes={feedbackTypes}/>
       );
     }
-    else {
-      return (
-        <FeedbackTable elements={feedback} people={people}/>
-      );
-    }
+    return (
+      <FeedbackTable elements={feedback} people={people}/>
+    );
   }
 
   renderComments() {
@@ -57,10 +44,18 @@ export class List extends Component {
         <FeedbackCommentList elements={comments}/>
       );
     }
-    else {
-      return (
-        <FeedbackCommentTable elements={comments}/>
-      );
-    }
+    return (
+      <FeedbackCommentTable elements={comments}/>
+    );
   }
+
+  render() {
+    return (
+      <ListFrame>
+        <FeedbackListControlBar count={this.props.feedback.length} />
+        {this.contentChoice()}
+      </ListFrame>
+    );
+  }
+
 }
