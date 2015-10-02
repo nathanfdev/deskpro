afb1a7fbe637ce958b68f5356b4c7f0a71eb7669
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-02 17:01:58 +0300

    headers buttons html from ui-design repo

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Header.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Header.js
@@ -16,6 +16,24 @@ export class Header extends React.Component {
     return (<header className="dp-window-header top-bar">
       <a href="https://www.deskpro.com/" className="logo"></a>
       <HeaderWidget/>
+
+      <div className="user-options">
+        <a href="#" className="notification-button">
+          <span className="title"><i className="fa fa-dollar"></i></span>
+        </a>
+
+        <a href="#" className="notification-button">
+          <span className="notification-count">23</span>
+          <span className="title"><i className="fa fa-cog"></i> Admin</span>
+        </a>
+
+        <a href="#" className="user-options-button">
+          <span className="user-photo" style={{backgroundImage: 'url(' + user.picture_url + ')'}}></span>
+          Settings
+          <i className="fa fa-angle-down"></i>
+        </a>
+      </div>
+
     </header>);
   }
 }
