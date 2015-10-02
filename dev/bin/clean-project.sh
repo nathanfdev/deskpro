bcfcc2fb991b09bbbc4e31e5405d085f3ccbc7f1
Chris Tickner <chris.tickner@gmail.com>
2015-10-02 12:26:28 -0400

    remove http_cache dir contents in clean-project.sh

--- dev/bin/clean-project.sh
+++ dev/bin/clean-project.sh
@@ -11,6 +11,7 @@ rm -rf $DIR_ROOT/pub/node_modules
 rm -rf $DIR_ROOT/pub/build
 
 rm -rf ../../app/sys/cache/*/
+rm -rf $DIR_ROOT/data/http_cache/*/
 
 rm -f $DIR_ROOT/app/src/Application/InstallBundle/Data/schema.php
 rm -f $DIR_ROOT/pub/src/DeskPRO/Bundle/AgentBundle/AgentApp_Reducers.js
