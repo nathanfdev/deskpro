181afc5ef36edd327d3706bfbd12644097b4cff4
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-03 12:36:09 +0300

    load user reducer code style

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Reducers/user.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Reducers/user.js
@@ -2,13 +2,13 @@ import ActionTypes from '../Actions/ActionTypes';
 import { handleActions } from 'redux-actions';
 
 const initialState = {
-	id: null
+  id: null
 };
 
 const r = handleActions({
-	[ActionTypes.APP_SET_USER]: (state, action) => action.payload
+  [ActionTypes.APP_SET_USER]: (state, action) => action.payload
 }, initialState);
 
 export default (state, action = {type: null}) => {
-	return r(state, action);
-}
+  return r(state, action);
+};
