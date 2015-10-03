5b5af10122ba775f6380ccdda51f39cd28d67af0
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-03 12:37:35 +0300

    index code style

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Reducers/index.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Reducers/index.js
@@ -1,4 +1,5 @@
 import dp_window from './dp_window';
 import routing from './routing';
 import user from './user';
-export default {dp_window,routing,user,};
+
+export default {dp_window, routing, user};
