3869a7098f072ad61ade55b08a448605fc91e797
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-04 15:05:51 +0300

    code style

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/DpApp.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/DpApp.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
 import { connect } from 'react-redux';
 
-import { Header } from "./Header";
-import { AppSwitcher } from "./AppSwitcher";
-import TabFrame from "./TabFrame";
+import { Header } from './Header';
+import { AppSwitcher } from './AppSwitcher';
+import TabFrame from './TabFrame';
 
-import TicketsApp from "DeskPRO/Bundle/AgentBundle/Modules/Tickets/Components/TicketsApp";
-import TasksApp from "DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/TasksApp";
-import {FeedbackApp} from "DeskPRO/Bundle/AgentBundle/Modules/Feedback/Components/FeedbackApp";
+import TicketsApp from 'DeskPRO/Bundle/AgentBundle/Modules/Tickets/Components/TicketsApp';
+import TasksApp from 'DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/TasksApp';
+import {FeedbackApp} from 'DeskPRO/Bundle/AgentBundle/Modules/Feedback/Components/FeedbackApp';
 
-import { routingStarted } from "../Actions/AppActions";
+import { routingStarted } from '../Actions/AppActions';
 
 @connect(state => ({
   ...state,
