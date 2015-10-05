a57d2ea56819f3b083163dc8fcc79d1f4669bb53
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-05 22:02:03 +0300

    workspace dropdown sidebar change mode btns

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Workspace/ChangeModeButton.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Workspace/ChangeModeButton.js
@@ -4,23 +4,22 @@ export class ChangeModeButton extends React.Component {
 
   static propTypes = {
     type: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
     activeType: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
+    children: PropTypes.object.isRequired
   };
 
   render() {
-    const {type, activeType, onChange} = this.props;
+    const {type, title, activeType, onChange, children} = this.props;
     const classNames = 'workspace-state-a ' + (type === activeType ? 'active' : '');
 
     return (
       <div className={classNames} onClick={onChange.bind(this, type)}>
         <div className="workspace-state-screen">
-          <span className="active-workspace-mark"><i className="fa fa-check"></i></span>
-          <span className="workspace-state-item state-sidebar state-sidebar-hover active"><i className="fa fa-asterisk"></i></span>
-          <span className="workspace-state-item left-column"></span>
-          <span className="workspace-state-item right-column"></span>
+          {children}
         </div>
-        <span className="workspace-state-title">{type}</span>
+        <span className="workspace-state-title">{title}</span>
       </div>
     );
   }
--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Workspace/SidebarMode.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Workspace/SidebarMode.js
@@ -24,13 +24,24 @@ export class SidebarMode extends React.Component {
         </div>
 
         <div className="dpw-workspace-state">
-          <ChangeModeButton type={'Static'}
+          <ChangeModeButton type={'static'}
+                            title={'Static Mode'}
                             activeType={currentMode}
-                            onChange={this.changeMode} />
+                            onChange={this.changeMode}>
+            <span className="workspace-state-item state-sidebar active"></span>
+            <span className="workspace-state-item left-column"></span>
+            <span className="workspace-state-item right-column"></span>
+          </ChangeModeButton>
 
-          <ChangeModeButton type={'Hover'}
+          <ChangeModeButton type={'hover'}
+                            title={'Hover Mode'}
                             activeType={currentMode}
-                            onChange={this.changeMode} />
+                            onChange={this.changeMode}>
+            <span className="active-workspace-mark"><i className="fa fa-check"></i></span>
+            <span className="workspace-state-item state-sidebar state-sidebar-hover active"><i className="fa fa-asterisk"></i></span>
+            <span className="workspace-state-item left-column"></span>
+            <span className="workspace-state-item right-column"></span>
+          </ChangeModeButton>
         </div>
       </div>);
   }
