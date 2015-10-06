fb3e361e72c5c6377fae3fb095f57acdf1c6772f
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-06 17:36:49 +0300

    change align

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Workspace/ColumnMode.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Workspace/ColumnMode.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { ChangeModeButton } from './ChangeModeButton';
 
 export class ColumnMode extends React.Component {
@@ -16,51 +17,50 @@ export class ColumnMode extends React.Component {
     return (
       <div className="dpw-workspace-type-container">
 
-      <div className="dpw-workspace-type-header">
-        <span className="title">Column Mode</span>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-      </div>
+        <div className="dpw-workspace-type-header">
+          <span className="title">Column Mode</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+        </div>
 
-      <div className="dpw-workspace-state">
-        <ChangeModeButton type={'column'}
-                          title={'Column Mode'}
-                          activeType={currentMode}
-                          onChange={onChangeMode}>
+        <div className="dpw-workspace-state">
+          <ChangeModeButton type={'column'}
+                            title={'Column Mode'}
+                            activeType={currentMode}
+                            onChange={onChangeMode}>
 
-          <span className="workspace-state-item state-sidebar"></span>
-          <span className="workspace-state-item left-column active"></span>
-          <span className="workspace-state-item right-column active"></span>
-        </ChangeModeButton>
+            <span className="workspace-state-item state-sidebar"></span>
+            <span className="workspace-state-item left-column active"></span>
+            <span className="workspace-state-item right-column active"></span>
+          </ChangeModeButton>
 
-        <ChangeModeButton type={'focus'}
-                          title={'Focus Mode'}
-                          activeType={currentMode}
-                          onChange={onChangeMode}>
+          <ChangeModeButton type={'focus'}
+                            title={'Focus Mode'}
+                            activeType={currentMode}
+                            onChange={onChangeMode}>
 
-          <span className="workspace-state-item state-sidebar"></span>
-          <span className="workspace-state-item full-width-column active"></span>
-        </ChangeModeButton>
-      </div>
+            <span className="workspace-state-item state-sidebar"></span>
+            <span className="workspace-state-item full-width-column active"></span>
+          </ChangeModeButton>
+        </div>
 
-      <div className="dpw-workspace-state dpw-workspace-slider-container">
-        <div className="">
-          <h2>Column Dimensions <a href="#" onClick={onChangeDimensions.bind(this, 0)}>Reset</a></h2>
-          <div className="dpw-workspace-slider">
-            <div className="dpw-workspace-slider-count-container">
-              <span className="dpw-workspace-slider-count">{state.columnDimensions}%</span>
-            </div>
+        <div className="dpw-workspace-state dpw-workspace-slider-container">
+          <div className="">
+            <h2>Column Dimensions <a href="#" onClick={onChangeDimensions.bind(this, 0)}>Reset</a></h2>
+            <div className="dpw-workspace-slider">
+              <div className="dpw-workspace-slider-count-container">
+                <span className="dpw-workspace-slider-count">{state.columnDimensions}%</span>
+              </div>
 
-            <div className="dpw-workspace-slider-slide-container">
-                <span className="dpw-workspace-slider-slide">
-                  <span className="slider-blocked-left"></span>
-                  <span className="slider-blocked-right"></span>
-                  <span className="slider-button" style={{left: state.columnDimensions}}></span>
-                </span>
+              <div className="dpw-workspace-slider-slide-container">
+                  <span className="dpw-workspace-slider-slide" ref="slider">
+                    <span className="slider-blocked-left"></span>
+                    <span className="slider-blocked-right"></span>
+                    <span className="slider-button" style={{left: state.columnDimensions}}></span>
+                  </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
 
     </div>);
   }
