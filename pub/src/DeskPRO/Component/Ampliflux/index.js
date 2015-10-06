5955b51e65af2ef567caf666575b64b81e2cf249
Sergey <laa513@gmail.com>
2015-10-06 16:58:31 +0300

    Ampliflux @pureRender - optimizes components by defining shouldComponentUpdate() method

--- pub/src/DeskPRO/Component/Ampliflux/index.js
+++ pub/src/DeskPRO/Component/Ampliflux/index.js
@@ -1,9 +1,11 @@
 import createAction from './actions/createAction';
 import createReducer from './reducers/createReducer';
 import combineReducerHierarchy from './reducers/combineReducerHierarchy';
+import { pureRender } from './common/components/pureRenderDecorator';
 
 export default {
   createAction,
   createReducer,
-  combineReducerHierarchy
+  combineReducerHierarchy,
+  pureRender
 };
