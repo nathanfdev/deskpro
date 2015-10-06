830ad6b2bb20ed1581dcd989b59d21525bd546da
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-06 22:12:27 +0300

    crm collapse/expand nav fix

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/CRM/Components/Nav/Nav.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/CRM/Components/Nav/Nav.js
@@ -10,39 +10,41 @@ export class Nav extends React.Component {
 
     return (
       <NavFrame dispatch={dispatch.bind(this)} dpWindow={dpWindow}>
-        <NavFrameHeader icon="fa-users" dispatch={dispatch.bind(this)}>CRM</NavFrameHeader>
-            <SectionHeader>People</SectionHeader>
-            <TabsPane>
-              <Tab title="Groups">
-                <ul>
-                  <ListItem count={users.total} label="Everyone" />
-                  {users.groups.map(item =>
-                      <ListItem key={itemKey++} count={item.count} label={groupNames[item.group]} />)}
-                </ul>
-              </Tab>
-              <Tab title="Filters">Filters tab content</Tab>
-              <Tab title="Labels">
-                <LabelsDictionary labels={labels.person} />
-              </Tab>
-            </TabsPane>
-            <SectionHeader>Organizations</SectionHeader>
-            <TabsPane>
-              <Tab title="All">
-                <ul>
-                  <ListItem count={organizations.total} label="All Organizations" />
-                </ul>
-              </Tab>
-              <Tab title="Labels">
-                <LabelsDictionary labels={labels.organization} />
-              </Tab>
-            </TabsPane>
-
-            <SectionHeader>Agents</SectionHeader>
+        <NavFrameHeader icon="fa-users" dispatch={dispatch.bind(this)}>
+          CRM
+        </NavFrameHeader>
+        <SectionHeader>People</SectionHeader>
+        <TabsPane>
+          <Tab title="Groups">
             <ul>
-              <ListItem count={agents.total} label="All Agents" />
-              {agents.teams.map(item =>
-                  <ListItem key={itemKey++} count={item.count} label={teamNames[item.group]} />)}
+              <ListItem count={users.total} label="Everyone" />
+              {users.groups.map(item =>
+                  <ListItem key={itemKey++} count={item.count} label={groupNames[item.group]} />)}
             </ul>
+          </Tab>
+          <Tab title="Filters">Filters tab content</Tab>
+          <Tab title="Labels">
+            <LabelsDictionary labels={labels.person} />
+          </Tab>
+        </TabsPane>
+        <SectionHeader>Organizations</SectionHeader>
+        <TabsPane>
+          <Tab title="All">
+            <ul>
+              <ListItem count={organizations.total} label="All Organizations" />
+            </ul>
+          </Tab>
+          <Tab title="Labels">
+            <LabelsDictionary labels={labels.organization} />
+          </Tab>
+        </TabsPane>
+
+        <SectionHeader>Agents</SectionHeader>
+        <ul>
+          <ListItem count={agents.total} label="All Agents" />
+          {agents.teams.map(item =>
+              <ListItem key={itemKey++} count={item.count} label={teamNames[item.group]} />)}
+        </ul>
       </NavFrame>
     );
   }
