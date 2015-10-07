660926a3d53514f50a6ce64bd727439a6d704051
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-07 18:13:55 +0300

    nav collapse/expand fix for crm, chat, feedback sections

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/CRM/Components/CrmApp.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/CRM/Components/CrmApp.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { AppContainer } from 'DeskPRO/Component/AppContainer';
 import { NavContainer } from './Nav/NavContainer';
 import { ListContainer } from './List/ListContainer';
 
+@connect(state => ({
+  dpWindow: state.Application.dpWindow
+}))
 export class CrmApp extends React.Component {
   render() {
     return (
-      <AppContainer thisAppId="crm">
+      <AppContainer thisAppId="crm" {...this.props}>
         <NavContainer />
       </AppContainer>
     );
--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Chat/Components/ChatApp.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Chat/Components/ChatApp.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { AppContainer } from 'DeskPRO/Component/AppContainer';
 import { NavContainer } from './Nav/NavContainer';
 import { ListContainer } from './List/ListContainer';
 
+@connect(state => ({
+  dpWindow: state.Application.dpWindow
+}))
 export class ChatApp extends React.Component {
   render() {
     return (
-      <AppContainer thisAppId="chat">
+      <AppContainer thisAppId="chat" {...this.props}>
         <NavContainer />
       </AppContainer>
     );
