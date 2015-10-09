520196a601f364578e75063ccca9779a343c5ae2
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-09 19:19:56 +0300

    initial login actions/reducers

---
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Login/Actions/loginActions.js
@@ -0,0 +1,5 @@
+import { createAction } from 'Ampliflux';
+
+export const login = createAction('LOGIN');
+export const logout = createAction('LOGOUT');
+
---
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Login/Reducers/login.js
@@ -0,0 +1,4 @@
+import * as actions from '../Actions/AppActions';
+import { createReducer } from 'Ampliflux';
+
+const initialState = {};
