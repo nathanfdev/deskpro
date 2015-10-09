a170c5e81a12730f4a23ac0b4aca89207585c635
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-09 10:16:41 +0300

    app switcher html, styles, fonts

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/AppSwitcher.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/AppSwitcher.js
@@ -49,14 +49,60 @@ export class AppSwitcher extends React.Component {
       <nav onMouseEnter={this.hoverSwitcher.bind(this)}
         onMouseLeave={this.cancelSwitcher.bind(this)}
         className={classesNames.join(' ')}>
-        <div className="app-bar">
-          <ul>
-            {this.renderAppIcon('tickets', 'Tickets', 'fa-envelope-o')}
-            {this.renderAppIcon('crm', 'CRM', 'fa-users')}
-            {this.renderAppIcon('chat', 'Chat', 'fa-comments-o')}
-            {this.renderAppIcon('feedback', 'Feedback', 'fa-thumbs-up')}
-            {this.renderAppIcon('publish', 'Publish', 'fa-edit')}
-            {this.renderAppIcon('tasks', 'Tasks', 'fa-check-square-o')}
+
+
+        <div className="dpw-app-bar dpw-app-bar-state-1">
+          <ul className="app-list">
+            <li>
+              <a href="#" className="dpw-app-bar-item-1 active">
+                <span className="dpw-app-bar-notification">33</span>
+                <div className="dpw-app-bar-icon">
+                  <div className="icon icon-dp-streamline-mail-2"></div>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="dpw-app-bar-item-2">
+                <div className="dpw-app-bar-icon">
+                  <div className="icon icon-dp-streamline-bubble-conversation-4"></div>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="dpw-app-bar-item-3">
+                <span className="dpw-app-bar-notification">33</span>
+                <div className="dpw-app-bar-icon">
+                  <div className="icon icon-dp-streamline-connection-2"></div>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="dpw-app-bar-item-4">
+                <div className="dpw-app-bar-icon">
+                  <div className="icon icon-dp-streamline-hand-like-2"></div>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="dpw-app-bar-item-5">
+                <div className="dpw-app-bar-icon">
+                  <div className="icon icon-dp-streamline-edit-1"></div>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="dpw-app-bar-item-6">
+                <div className="dpw-app-bar-icon">
+                  <div className="icon icon-dp-streamline-check-circle-2"></div>
+                </div>
+              </a>
+            </li>
+
           </ul>
         </div>
       </nav>
