56ff02ee5d7d1599e1d727d08cc865a3bb410901
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-10 13:07:00 +0300

    login form banners

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Login/Components/LoginForm.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Login/Components/LoginForm.js
@@ -63,15 +63,15 @@ export class LoginForm extends React.Component {
 
           <div className="right-panel">
             <div className="dpw-login-side">
-              <a href="#" className="dpw-main-banner" style={{backgroundImage: 'url(../img/samples/sample-banner.png)'}}></a>
+              <a href="#" className="dpw-main-banner sample-banner"></a>
 
               <div className="dpw-login-side-banners">
                 <div className="left">
-                  <a href="#" className="dpw-login-side-small-banner" style={{backgroundImage: 'url(../img/loading/banner-quickstart-guide.png)'}}></a>
+                  <a href="#" className="dpw-login-side-small-banner quick-start"></a>
                 </div>
 
                 <div className="right">
-                  <a href="#" className="dpw-login-side-small-banner" style={{backgroundImage: 'url(../img/loading/banner-mobile-apps.png)'}}></a>
+                  <a href="#" className="dpw-login-side-small-banner mobile-apps"></a>
                 </div>
 
               </div>
