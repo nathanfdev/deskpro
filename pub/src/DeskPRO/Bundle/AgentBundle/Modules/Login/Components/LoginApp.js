03a35bea7b83e9852a6482419dbb1ac9d3f4fade
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-10 15:24:46 +0300

    login app cleanup

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Login/Components/LoginApp.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Login/Components/LoginApp.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { AppContainer } from 'DeskPRO/Component/AppContainer';
 import { LoginForm } from './LoginForm';
 
-@connect(state => ({
-  dpWindow: state.Application.dpWindow
-}))
 export class LoginApp extends React.Component {
   render() {
     return (
