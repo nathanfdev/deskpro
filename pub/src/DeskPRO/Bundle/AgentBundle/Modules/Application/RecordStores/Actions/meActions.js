9cbeaaecf3c9c2d30b28975f7abe00c34dd7f1e4
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-12 19:59:10 +0300

    small change

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/RecordStores/Actions/meActions.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/RecordStores/Actions/meActions.js
@@ -1,5 +1,5 @@
 import { createAction } from 'Ampliflux';
-import { createRecordsRequest } from 'Ampliflux/common/record-store/actions'
+import { createRecordsRequest } from 'Ampliflux/common/record-store/actions';
 import { loadPeople } from 'DeskPRO/Bundle/AgentBundle/Modules/CRM/RecordStores/Actions/peopleActions';
 import DpApi from 'DeskPRO/Bundle/AgentBundle/Services/DpApi';
 
