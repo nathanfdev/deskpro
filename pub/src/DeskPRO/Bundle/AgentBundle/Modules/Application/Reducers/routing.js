e4d64524c6072b68723941dce6086465ec57bdb1
Sergey <laa513@gmail.com>
2015-10-13 19:43:50 +0300

    Skip unnecessary hash state updates

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Reducers/routing.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Reducers/routing.js
@@ -2,15 +2,35 @@ import * as actions from '../Actions/routingActions';
 import { createReducer } from 'Ampliflux';
 import { stateToString, stateFromString } from '../Service/hash';
 import Immutable from 'immutable';
+import shallowEqual from 'fbjs/lib/shallowEqual';
 
 const initialState = {
-  hash: null
+  hash: {}
 };
 
 export default createReducer(initialState, {
-  [actions.hashChanged]: (state, payload) => state.merge({
-    hash: payload ? stateFromString(payload.substring(1)) : null
-  }),
+  [actions.hashChanged]: (state, payload) => {
+    const newHashState = stateFromString(payload.substring(1));
+
+    // When we update window.location.hash from updateHashState() action reducer and
+    // window.onhashchange() event is fired, we don't actually need to update state
+    // as state and hash are already in sync.
+    //
+    // So to prevent unnecessary re-rendering of components using hash state, we skip
+    // updating when state and hash are already synchronized.
+    //
+    // @todo Test this all to work correctly when hash state is more extensively used
+    //
+    if (shallowEqual(state.get('hash').toJS(), newHashState.toJS())) {
+      console.error('Skipping update!');
+      return state;
+    } else {
+      console.error('Updating');
+      return state.merge({
+        hash: payload ? newHashState : null
+      });
+    }
+  },
   [actions.updateHashState]: (state, {component, option, value}) => {
     let next = state;
 
