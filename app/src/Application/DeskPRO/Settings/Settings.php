21e6b1d9802819264f5be3edd88624d7ae1a4763
Christopher Nadeau <chris@nadeau.ws>
2015-10-13 17:08:29 +0100

    Add cron job for health-check of email jobs

--- app/src/Application/DeskPRO/Settings/Settings.php
+++ app/src/Application/DeskPRO/Settings/Settings.php
@@ -294,12 +294,10 @@ class Settings implements \ArrayAccess, \IteratorAggregate, \Countable
                 }
 
                 $this->db->executeUpdate('
-                    INSERT INTO settings
+                    REPLACE INTO settings
                         (name, value)
                     VALUES
                         (?, ?)
-                    ON DUPLICATE KEY UPDATE
-                        value = VALUES(value)
                 ', array($setting, $value));
             } else {
                 $this->db->delete('settings', array('name' => $setting));
