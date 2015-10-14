acf593a6c740999cd6d75a383041438d79a593ba
Sergey <laa513@gmail.com>
2015-10-14 19:04:40 +0300

    Load feedback list depending on the selected nav list item stored in URL

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Feedback/Components/Nav/FeedbackListItem.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Feedback/Components/Nav/FeedbackListItem.js
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 import { ListItemStatefulContainer } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/NavFrame/index';
 import * as actions from '../../Actions/FeedbackListActions';
 
-@connect(state => ({}))
+@connect(state => ({
+  activeItemId: state.Application.routing.getIn(['hash', 'nav', 'active'])}
+))
 export class FeedbackListItem extends Component {
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
+    activeItemId: PropTypes.string,
 
     // Label is also used as "itemId" of underlying ListItemStatefulContainer, so labels must be unique
     label: PropTypes.string.isRequired,
@@ -15,11 +18,18 @@ export class FeedbackListItem extends Component {
     listOptions: PropTypes.object.isRequired
   };
 
+  constructor(props) {
+    super(props);
+
+    // @todo urlSanitize() helper replacing \s and reserved characters
+    this.itemId = props.label.replace(/\s/g, '_');
+  }
+
   render() {
     const props = {
       groupId: 'nav',
       onClick: this.loadList(this.props.listOptions),
-      itemId: this.props.label.replace(/\s/g, '_'), // @todo urlSanitize() helper replacing \s and reserved characters
+      itemId: this.itemId,
       label: this.props.label,
       count: this.props.count,
       children: this.props.children
@@ -28,6 +38,12 @@ export class FeedbackListItem extends Component {
     return (
       <ListItemStatefulContainer {...props} />
     );
+  }
+
+  componentDidMount() {
+    if (this.props.activeItemId === this.itemId) {
+      this.props.dispatch(actions.loadFeedbackList(this.props.listOptions));
+    }
   }
 
   loadList(options) {
