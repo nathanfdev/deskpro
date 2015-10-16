15b8ecabfc158399de463bdafd05ecc9636c1e4b
Christopher Nadeau <chris.nadeau@deskpro.com>
2015-10-16 16:58:03 +0100

    Allow using --api-key with v2 api

--- app/src/Application/DeskPRO/Command/DevTestApiCommand.php
+++ app/src/Application/DeskPRO/Command/DevTestApiCommand.php
@@ -160,6 +160,10 @@ class DevTestApiCommand extends \Symfony\Bundle\FrameworkBundle\Command\Containe
             }
 
             $api_key = $key->getKeyString();
+
+            if ($v2) {
+                $api_key = 'key ' . $api_key;
+            }
         }
 
         #------------------------------
@@ -169,7 +173,7 @@ class DevTestApiCommand extends \Symfony\Bundle\FrameworkBundle\Command\Containe
         $headers = array();
 
         if ($v2) {
-            $headers['Authorization'] = 'key '.$api_key;
+            $headers['Authorization'] = $api_key;
         } else {
             $headers['X-DeskPRO-API-Key'] = $api_key;
         }
