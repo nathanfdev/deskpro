49680f5f7379d5fb1b04905019129a0957a09f74
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-19 15:51:05 +0300

    login form no default values

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Login/Actions/loginActions.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Login/Actions/loginActions.js
@@ -31,8 +31,8 @@ export const login = createAction(
   'LOGIN_SUBMIT_FORM',
   params => dispatch => DpApi.sendPost('DP_API/get_session', params)
     .success(() => {
-      dispatch(emailChange('admin@example.com'));
-      dispatch(passwordChange('password'));
+      dispatch(emailChange(null));
+      dispatch(passwordChange(null));
       dispatch(loadMe());
       dispatch(setHasAuth(true));
     })
