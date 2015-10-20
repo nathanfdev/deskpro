a5f4f3e9a3d2a3f4db5ab665ab78c96afd428a41
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-21 00:47:34 +0300

    split form to render fields methods

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/Form/ProfileForm.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/Form/ProfileForm.js
@@ -82,6 +82,49 @@ export class ProfileForm extends React.Component {
     event.preventDefault();
   };
 
+  renderNameAvatarField() {
+    return (
+      <FieldWrapper label="Your name">
+        <Avatar />
+        <Name value={this.state.name} onChange={this.onChangeName} />
+      </FieldWrapper>
+    );
+  }
+
+  renderOverrideDefaultNameField() {
+    return (
+      <div className="bucket short">
+        <label className="simple-label">
+          <input type="checkbox"
+                 checked={this.state.overrideDefaultName}
+                 onChange={this.onToggleOverrideDefaultName} />
+
+          Override default name?
+        </label>
+        <span className="small">(will be displayed to users instead of your real name.)</span>
+      </div>
+    );
+  }
+
+  renderEmailField() {
+    return (
+      <FieldWrapper label="Your email">
+        <Email emails={this.state.emails}
+               primary={this.state.primaryEmail}
+               onChangeEmails={this.onChangeEmails}
+               onChangePrimary={this.onChangePrimaryEmail} />
+      </FieldWrapper>
+    );
+  }
+
+  renderPhoneField() {
+    return (
+      <FieldWrapper label="Phone #">
+        <Phone value={this.state.phone} onChange={this.onChangePhone} />
+      </FieldWrapper>
+    );
+  }
+
   renderLanguageField() {
     const { languages } = this.props;
     if (languages.size < 2) {
@@ -95,55 +138,44 @@ export class ProfileForm extends React.Component {
     );
   }
 
+  renderTimezoneField() {
+    return (
+      <FieldWrapper label="Time Zone">
+        <div className="bucket-column">
+          <a href="#" className="select">Europe/London (0 GMT) <i className="fa fa-caret-down"></i></a>
+        </div>
+      </FieldWrapper>
+    );
+  }
+
+  renderPasswordField() {
+    return (
+      <FieldWrapper label="Password">
+        <Password
+          value={this.state.password}
+          confirmValue={this.state.confirmPassword}
+          onChangeValue={this.onChangePassword}
+          onChangeConfirmValue={this.onChangeConfirmPassword} />
+      </FieldWrapper>
+    );
+  }
+
   render() {
     return (
       <form className="popup-form-default">
-        <FieldWrapper label="Your name">
-          <Avatar />
-          <Name value={this.state.name} onChange={this.onChangeName} />
-        </FieldWrapper>
-
-        <div className="bucket short">
-          <label className="simple-label">
-            <input type="checkbox"
-                   checked={this.state.overrideDefaultName}
-                   onChange={this.onToggleOverrideDefaultName} />
-
-            Override default name?
-          </label>
-          <span className="small">(will be displayed to users instead of your real name.)</span>
-        </div>
-
-        <FieldWrapper label="Your email">
-          <Email emails={this.state.emails}
-                 primary={this.state.primaryEmail}
-                 onChangeEmails={this.onChangeEmails}
-                 onChangePrimary={this.onChangePrimaryEmail} />
-        </FieldWrapper>
-
-        <FieldWrapper label="Phone #">
-          <Phone value={this.state.phone} onChange={this.onChangePhone} />
-        </FieldWrapper>
+        {this.renderNameAvatarField()}
+        {this.renderOverrideDefaultNameField()}
+        {this.renderEmailField()}
+        {this.renderPhoneField()}
 
         <hr />
 
         {this.renderLanguageField()}
-
-        <FieldWrapper label="Time Zone">
-          <div className="bucket-column">
-            <a href="#" className="select">Europe/London (0 GMT) <i className="fa fa-caret-down"></i></a>
-          </div>
-        </FieldWrapper>
+        {this.renderTimezoneField()}
 
         <hr />
 
-        <FieldWrapper label="Password">
-          <Password
-            value={this.state.password}
-            confirmValue={this.state.confirmPassword}
-            onChangeValue={this.onChangePassword}
-            onChangeConfirmValue={this.onChangeConfirmPassword} />
-        </FieldWrapper>
+        {this.renderPasswordField()}
 
         <input type="submit" onClick={this.submitForm} />
       </form>
