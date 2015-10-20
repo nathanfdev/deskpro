e6054eccfe1d95512effd2d06f6f0e8ac63ba649
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-20 19:54:42 +0300

    profile initial avatar component

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/Form/ProfileForm.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/Form/ProfileForm.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { FieldWrapper } from './Fields/FieldWrapper';
 import { Name } from './Fields/Name';
+import { Avatar } from './Fields/Avatar';
 import { Email } from './Fields/Email';
 import { Phone } from './Fields/Phone';
 import { Password } from './Fields/Password';
@@ -83,19 +84,7 @@ export class ProfileForm extends React.Component {
     return (
       <form className="popup-form-default">
         <FieldWrapper label="Your name">
-          <div className="bucket-column-last">
-            <a href="#" className="button button-secondary user-avatar"><span style={{backgroundImage: 'url(./img/avatar2.png)'}}></span>Manage Avatar</a>
-
-            <div className="avatar-crop" id="avatar-crop" style={{display: 'none'}}>
-              <p>Click &amp; drag to crop your avatar</p>
-              <div className="cropper-bucket">
-                <img src="./img/avatar-sample.png" alt="Avatar Sample" />
-              </div>
-              <a href="#" className="crop">Crop &amp; Save Avatar</a>
-              <a href="#" className="cancel">Or cancel &amp; discard your changes</a>
-            </div>
-          </div>
-
+          <Avatar />
           <Name value={this.state.name} onChange={this.onChangeName} />
         </FieldWrapper>
 
