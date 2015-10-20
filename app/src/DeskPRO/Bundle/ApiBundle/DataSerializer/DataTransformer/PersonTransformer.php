e5234effb69c6ff0d9057edf22556c72dc325ee2
Sergey <laa513@gmail.com>
2015-10-20 20:48:08 +0300

    Removed default_url and default_url_pattern props and AvatarTransformer and its' hack as they no longer needed (w/o default_* props can use Avatar model serialization as it is)

--- app/src/DeskPRO/Bundle/ApiBundle/DataSerializer/DataTransformer/PersonTransformer.php
+++ app/src/DeskPRO/Bundle/ApiBundle/DataSerializer/DataTransformer/PersonTransformer.php
@@ -31,30 +31,21 @@
  */
 namespace DeskPRO\Bundle\ApiBundle\DataSerializer\DataTransformer;
 
-use DeskPRO\Bundle\ApiBundle\DataSerializer\DataSerializer;
-use DeskPRO\Bundle\ApiBundle\DataSerializer\DataTransformer;
 use DeskPRO\Bundle\ApiBundle\DataSerializer\DataTransformerRequest;
 use DeskPRO\Bundle\AppBundle\Content\AvatarResolver;
 
 class PersonTransformer extends AbstractDataSerializerTransformer
 {
     /**
-     * @var DataTransformer
-     */
-    private $serializer;
-
-    /**
      * @var AvatarResolver
      */
     private $avatar_resolver;
 
     /**
-     * @param DataTransformer $transformer
-     * @param AvatarResolver  $avatar_resolver
+     * @param AvatarResolver $avatar_resolver
      */
-    public function __construct(DataSerializer $serializer, AvatarResolver $avatar_resolver)
+    public function __construct(AvatarResolver $avatar_resolver)
     {
-        $this->serializer      = $serializer;
         $this->avatar_resolver = $avatar_resolver;
     }
 
@@ -133,12 +124,7 @@ class PersonTransformer extends AbstractDataSerializerTransformer
         #------------------------------
         # Avatar
         #------------------------------
-
-        $ava      = $this->avatar_resolver->getAvatarModel($person);
-        $ava_data = $this->serializer->serialize($ava);
-
-        // TODO whats the proper way to do this?
-        $ret['avatar'] = $ava_data['data'];
+        $ret['avatar'] = $this->avatar_resolver->getAvatarModel($person);
 
         return $ret;
     }
