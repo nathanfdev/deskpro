a7170c4cfb6cd3f1550e38b3d710089284653511
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-20 11:06:33 +0300

    preferences password button shows two fields

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/Form/Password.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/Form/Password.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 export class Password extends React.Component {
+
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    };
+  }
+
+  open = () => {
+    this.setState({
+      expanded: true
+    });
+  };
+
+  renderButton() {
+    return (
+      <a href="#" className="button button-secondary button-short" onClick={this.open}>Change Password</a>
+    );
+  }
+
+  renderFields() {
+    return (
+      <div>
+        <p>
+          <input type="text" placeholder="Password" />
+        </p>
+        <p>
+          <input type="text" placeholder="Confirm password" />
+        </p>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="bucket-column">
-        <a href="#" className="button button-secondary button-short">Change Password</a>
+        {this.state.expanded ? this.renderFields() : this.renderButton()}
       </div>
     );
   }
