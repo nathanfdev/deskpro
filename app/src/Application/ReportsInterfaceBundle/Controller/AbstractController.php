c8fb9d5c717ff125b24b5b7e2533ebf8119ca185
Christopher Nadeau <chris@nadeau.ws>
2015-10-20 10:31:30 +0100

    Add quick way to show maint message on reports for when its down

--- app/src/Application/ReportsInterfaceBundle/Controller/AbstractController.php
+++ app/src/Application/ReportsInterfaceBundle/Controller/AbstractController.php
@@ -65,6 +65,10 @@ abstract class AbstractController extends \Application\DeskPRO\Controller\Abstra
      */
     public function preAction($action, $arguments = null)
     {
+        if (dp_get_config('disabled_reports_message')) {
+            return $this->createResponse(dp_get_config('disabled_reports_message'));
+        }
+
         if (!$this->_userHasPermissions()) {
             if ($this->request->isXmlHttpRequest()) {
                 $data = array('error' => 'session_expired');
