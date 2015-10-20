94dd56247eca479c0e2fbcb74dd60760fa87b50a
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-20 17:02:23 +0300

    profile tab pass dispatch prop

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/Content.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/Content.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { ProfileForm } from './Form/ProfileForm';
 
 export class Content extends React.Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired
+  };
+
   render() {
     return (
       <div>
-        <ProfileForm />
+        <ProfileForm dispatch={this.props.dispatch} />
       </div>
     );
   }
--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/ContentContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/ContentContainer.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Content } from './Content';
 
 @connect()
 export class ContentContainer extends React.Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired
+  };
+
   render() {
     return (
       <div>
-        <Content />
+        <Content dispatch={this.props.dispatch} />
       </div>
     );
   }
