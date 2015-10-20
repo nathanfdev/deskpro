32946e2e7f98fe9ad146607744919b635b5fb480
n3b <neb802@gmail.com>
2015-10-20 23:49:46 +0800

    [FIX] unchecked Choice custom field now saved correctly

--- app/src/Application/AgentBundle/Form/Model/NewTicket.php
+++ app/src/Application/AgentBundle/Form/Model/NewTicket.php
@@ -505,9 +505,7 @@ class NewTicket
                 }
             }
         }
-        if (!empty($post_custom_person_fields)) {
-            $manager->saveFormToObject($post_custom_person_fields, $ticket->person);
-        }
+        $manager->saveFormToObject($post_custom_person_fields, $ticket->person);
 
         if ($ticket->person->organization) {
             $manager                = App::$container->getOrgFieldManager();
@@ -520,9 +518,8 @@ class NewTicket
                     }
                 }
             }
-            if (!empty($post_custom_org_fields)) {
-                $manager->saveFormToObject($post_custom_org_fields, $ticket->person->organization);
-            }
+
+            $manager->saveFormToObject($post_custom_org_fields, $ticket->person->organization);
         }
 
         foreach ($add_cc_people as $add_cc_person) {
