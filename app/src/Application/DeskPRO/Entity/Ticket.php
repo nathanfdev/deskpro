67a78ae9b2c77b73faf9eec7440f63d400b8e22d
Christopher Nadeau <chris@nadeau.ws>
2015-10-23 17:25:58 +0100

    Prevent error when trying to render a field that doesnt exist

--- app/src/Application/DeskPRO/Entity/Ticket.php
+++ app/src/Application/DeskPRO/Entity/Ticket.php
@@ -1667,6 +1667,9 @@ class Ticket extends DomainObject implements HighlightableModelInterface
     public function renderCustomField($field_id, $context = 'html')
     {
         $f_def = App::getEntityRepository('DeskPRO:CustomDefTicket')->find($field_id);
+        if (!$f_def) {
+            return '';
+        }
 
         $data_structured = App::getApi('custom_fields.util')->createDataHierarchy($this->custom_data, array($f_def));
 
