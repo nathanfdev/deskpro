4e0c289e1661e0f50f3ac160c47743723878af30
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-26 17:10:59 +0300

    initial dropzone component

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/Form/Fields/Avatar.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Profile/Form/Fields/Avatar.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Cropper from 'react-cropper';
+import DropzoneComponent from 'react-dropzone-component';
 
 export class Avatar extends React.Component {
 
   render() {
+    const djsConfig = {
+      addRemoveLinks: true,
+      params: {
+        myParameter: "I'm a parameter!"
+      }
+    };
+
+    const componentConfig = {
+      iconFiletypes: ['.jpg', '.png', '.gif'],
+      showFiletypeIcon: true,
+      postUrl: '/uploadHandler'
+    };
+
     return (
       <div>
         <Cropper
@@ -15,29 +28,11 @@ export class Avatar extends React.Component {
           aspectRatio={16 / 9}
           guides={false}
           crop={this._crop} />
+
+        <DropzoneComponent config={componentConfig}
+                           eventHandlers={[]}
+                           djsConfig={djsConfig} />
         </div>
-    );
-  }
-}
-
-export class FileUpload extends React.Component {
-
-  handleFile = (e) => {
-    var reader = new FileReader();
-    var file = e.target.files[0];
-
-    if (!file) return;
-
-    reader.onload = function(img) {
-      ReactDOM.findDOMNode(this.refs.in).value = '';
-      this.props.handleFileChange(img.target.result);
-    }.bind(this);
-    reader.readAsDataURL(file);
-  };
-
-  render() {
-    return (
-      <input ref="in" type="file" accept="image/*" onChange={this.handleFile} />
     );
   }
 }
