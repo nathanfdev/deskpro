ada042dfbd67426c105b17f2f69f054ca6a5831c
Sergey <laa513@gmail.com>
2015-10-27 19:04:54 +0300

    ES6 react-onclickout

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/NavFrame/list-grouping-control.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/NavFrame/list-grouping-control.js
@@ -1,34 +1,54 @@
-import React from 'react';
-
+import React, { PropTypes } from 'react';
+import ClickOutHandler from 'react-onclickout';
 
 export class ListGroupingControl extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    visible: PropTypes.bool.isRequired,
+    onChange: PropTypes.func.isRequired,
+    options: PropTypes.array.isRequired,
+    onClose: PropTypes.func
+  };
+
+  componentDidMount() {
+    this.onClickOutWorked = false;
+  }
+
+  onClickOut = () => {
+    if (this.onClickOutWorked && this.props.onClose) {
+      this.props.onClose();
+    }
+    this.onClickOutWorked = true;
+  }
 
   render() {
-    const {title, onChange, options, visible} = this.props;
+    const { title, onChange, options, visible } = this.props;
     const className = visible ? 'sidebar-hover show' : 'sidebar-hover hide';
 
     return (
-      <section className={className}>
-        <div className="sidebar-hover-content">
-          <div className="sidebar-hover-header">
-            <i className="fa fa-tag"></i>
-            <span>&nbsp;</span>
-            <span>{title}</span>
+      <ClickOutHandler onClickOut={this.onClickOut}>
+        <section className={className}>
+          <div className="sidebar-hover-content">
+            <div className="sidebar-hover-header">
+              <i className="fa fa-tag"></i>
+              <span>&nbsp;</span>
+              <span>{title}</span>
+            </div>
+            <form>
+              <p>
+                <label>Grouping Options:</label>
+                <select onChange={onChange}>
+                  {options.map(option => <option key={option.value} value={option.value}>{option.label}</option>)}
+                </select>
+              </p>
+            </form>
           </div>
-          <form>
-            <p>
-              <label>Grouping Options:</label>
-              <select onChange={onChange}>
-                {options.map(option => <option key={option.value} value={option.value}>{option.label}</option>)}
-              </select>
-            </p>
-          </form>
-        </div>
-      </section>
+        </section>
+      </ClickOutHandler>
     );
   }
 
   shouldComponentUpdate(nextProps) {
-    return nextProps.visible !== this.props.visible;
+    return (nextProps.visible !== this.props.visible) || (nextProps.title !== this.props.title);
   }
 }
