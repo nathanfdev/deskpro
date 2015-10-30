431f3f614dfe981fed866fe6d79e8353eca2c195
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-30 13:48:16 +0300

    signature pane template

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Signature/Content.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Preferences/Tabs/Signature/Content.js
@@ -3,8 +3,20 @@ import React from 'react';
 export class Content extends React.Component {
   render() {
     return (
-      <div>
-        Signature content
+      <div className="user-signature-settings">
+        <h1>Signature</h1>
+
+        <section className="single-signature" id="single-signature">
+          <p>This signature will be appended automatically when you send ticket replies. Any reply drafts will be cleared when updating your signature.</p>
+
+          <div className="signature">
+            <form>
+              <div className="textarea-tagalong"></div>
+              <textarea placeholder="Your Signature"></textarea>
+              <input type="submit" value="Save Signature" />
+            </form>
+          </div>
+        </section>
       </div>
     );
   }
