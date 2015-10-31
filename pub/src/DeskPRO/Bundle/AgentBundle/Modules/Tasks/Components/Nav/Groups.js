8742dbc1221c78573c9038ce492bad7f8cd2c680
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-31 11:53:08 +0300

    tasks groups menu item

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Groups.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Groups.js
@@ -2,55 +2,32 @@ import React from 'react';
 import Immutable from 'immutable';
 
 export class Groups extends React.Component {
-  render() {
+
+  renderItem(name, count, route) {
     const taskList = Immutable.fromJS({});
     const filterTasks = () => {};
 
     return (
+      <li>
+        <div className="list-counter-bucket">
+          <a className="list-counter" href="#">{taskList.get(count, 0)}</a>
+        </div>
+        <a href="#" className="item" onClick={filterTasks.bind(this, route)}>{name}</a>
+      </li>
+    );
+  }
+
+  render() {
+    return (
       <section className="sidebar-list tasks-nav-groups">
         <div className="list-sidebar-title">Tasks</div>
         <ul>
-          <li>
-            <div className="list-counter-bucket">
-              <a className="list-counter" href="#">{taskList.get('myTaskCount', 0)}</a>
-            </div>
-            <a href="#" className="item" onClick={filterTasks.bind(this, {agents: ['me']})}>My Tasks</a>
-          </li>
-
-          <li>
-            <div className="list-counter-bucket">
-              <a href="#" className="list-counter">{taskList.get('teamTaskCount', 0)}</a>
-            </div>
-            <a href="#" className="item" onClick={filterTasks.bind(this, {teams: ['me']})}>My Team Tasks</a>
-          </li>
-
-          <li>
-            <div className="list-counter-bucket">
-              <a href="#" className="list-counter">{taskList.get('deptTaskCount', 0)}</a>
-            </div>
-            <a href="#" className="item" onClick={filterTasks.bind(this, {departments: ['me']})}>My Department Tasks</a>
-          </li>
-
-          <li>
-            <div className="list-counter-bucket">
-              <a href="#" className="list-counter">{taskList.get('delegatedTaskCount', 0)}</a>
-            </div>
-            <a href="#" className="item" onClick={filterTasks.bind(this, {agents: ['not_me'], creator: 'me'})}>Delegated Tasks</a>
-          </li>
-
-          <li>
-            <div className="list-counter-bucket">
-              <a href="#" className="list-counter">{taskList.get('unassignedTaskCount', 0)}</a>
-            </div>
-            <a href="#" className="item" onClick={filterTasks.bind(this, {agents: ['null'], teams: ['null'], departments: ['null']})}>Unassigned Tasks</a>
-          </li>
-
-          <li>
-            <div className="list-counter-bucket">
-              <a href="#" className="list-counter">{taskList.get('taskCount', 0)}</a>
-            </div>
-            <a href="#" className="item" onClick={filterTasks.bind(this, {done: 'all'})}>All Tasks</a>
-          </li>
+          {this.renderItem('My Tasks', 'myTaskCount', {agents: ['me']})}
+          {this.renderItem('My Department Tasks', 'teamTaskCount', {teams: ['me']})}
+          {this.renderItem('My Department Tasks', 'deptTaskCount', {departments: ['me']})}
+          {this.renderItem('Delegated Tasks', 'delegatedTaskCount', {agents: ['not_me'], creator: 'me'})}
+          {this.renderItem('Unassigned Tasks', 'unassignedTaskCount', {agents: ['null'], teams: ['null'], departments: ['null']})}
+          {this.renderItem('All Tasks', 'taskCount', {done: 'all'})}
         </ul>
       </section>
     );
