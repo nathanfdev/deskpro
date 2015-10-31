4dc819028eabf68ab9894a6f7a199b755709d169
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-31 15:02:47 +0300

    initial task form component

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ListFrameContainer } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/index';
 import ListFrameContents from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrameContents';
 import { Controls } from './ControlBar/Controls';
-import Formsy from 'formsy-react';
+import { TaskForm } from './TaskForm';
 
 export class List extends React.Component {
 
@@ -11,10 +11,7 @@ export class List extends React.Component {
       <ListFrameContainer>
         <Controls />
         <ListFrameContents>
-          <Formsy.Form onSubmit={()=>{}}>
-            <input name="title" type="text"/>
-            <button type="submit" value="Save" className="button">Add</button>
-          </Formsy.Form>
+          <TaskForm />
         </ListFrameContents>
       </ListFrameContainer>
     );
