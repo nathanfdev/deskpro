3325e9531bf5ce84282a078e080f5f7550c98dd5
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-10-31 11:26:55 +0300

    tasks nav frame header

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ListContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ListContainer.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { List } from './List';
 
+@connect()
 export class ListContainer extends React.Component {
   render() {
     return (
--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Nav.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Nav.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { NavFrame, NavFrameHeader, SectionHeader }
+  from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/NavFrame/index';
 
 export class Nav extends React.Component {
+
+  static propTypes = {
+    dispatch: PropTypes.object.isRequired,
+    dpWindow: PropTypes.object.isRequired
+  };
+
   render() {
+    const { dispatch, dpWindow } = this.props;
+
     return (
-      <div />
+      <NavFrame dispatch={dispatch.bind(this)} dpWindow={dpWindow}>
+        <NavFrameHeader icon="icon-dp-streamline-check-circle-2">
+          Tasks
+        </NavFrameHeader>
+      </NavFrame>
     );
   }
 }
