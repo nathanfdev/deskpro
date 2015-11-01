da68585ecd09835d55c457684b611688015e8a48
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-01 15:16:38 +0300

    project form container load lists

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/ProjectForm/ProjectFormContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/ProjectForm/ProjectFormContainer.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { ProjectForm } from './ProjectForm';
+import { agentsSelector } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Selectors/agentsSelectors';
+import { agentTeamsSelector } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Selectors/agentTeamsSelectors';
+import { allDepartmentsSelector } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Selectors/departmentsSelectors';
+import { loadAllAgents } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Actions/agentsActions';
+import { loadAllAgentTeams } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Actions/agentTeamsActions';
+import { loadAllDepartments } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Actions/departmentsActions';
 
-@connect()
+@connect(state => ({
+  agents: agentsSelector(state),
+  agentTeams: agentTeamsSelector(state),
+  departments: allDepartmentsSelector(state)
+}))
 export class ProjectFormContainer extends React.Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    agents: PropTypes.object.isRequired,
+    agentTeams: PropTypes.object.isRequired,
+    departments: PropTypes.object.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+
+    props.dispatch(loadAllAgents());
+    props.dispatch(loadAllAgentTeams());
+    props.dispatch(loadAllDepartments());
+  }
 
   render() {
     return (
