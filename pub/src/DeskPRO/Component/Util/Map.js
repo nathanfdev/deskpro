2848d8481f7ad5a91d50a4cc7f37f2ffbdfef8db
Sergey <laa513@gmail.com>
2015-11-02 15:53:25 +0300

    Fixed immutable obj type checking

--- pub/src/DeskPRO/Component/Util/Map.js
+++ pub/src/DeskPRO/Component/Util/Map.js
@@ -15,8 +15,8 @@ import warning from 'warning';
 export function mapKeyedFromArray(arrayVal, keyProp) {
   const isDeepKey = isArray(keyProp);
 
-  // Already a Map
-  if (Immutable.Map.isMap(arrayVal)) {
+  // Already an Immutable object
+  if (Immutable.Iterable.isIterable(arrayVal)) {
     return Immutable.Map().withMutations(map => {
       arrayVal.forEach(v => {
         const k = isDeepKey ? v.getIn(keyProp) : v.get(keyProp);
