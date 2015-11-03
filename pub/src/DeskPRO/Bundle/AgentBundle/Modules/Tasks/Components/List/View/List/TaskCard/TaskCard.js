2351f65df19365eecabbd8312a84e39cf63611e4
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-03 18:33:37 +0300

    render, collapse/expand details

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/List/TaskCard/TaskCard.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/List/TaskCard/TaskCard.js
@@ -34,9 +34,48 @@ export class TaskCard extends React.Component {
     });
   };
 
+  renderDetails() {
+    return (
+      <CardLine>
+        <div>
+            <span className="overdue dpwd--card-line-item">
+              <i className="fa fa-calendar-o"/> Due: N/A
+              <input type="text" name="due-date" className="due-date-field" disabled="disabled"/>
+            </span>
+
+            <span>
+              <span className="dpw--card-disc"/>
+              <span className="dpwd--card-line-item">
+                <i className="fa fa-book"/> Some project
+              </span>
+            </span>
+
+            <span>
+              <span className="dpw--card-disc"/>
+              <span className="dpwd--card-line-item">
+                <i className="fa fa-link"/> <a href="#">Ticket title</a>
+              </span>
+            </span>
+        </div>
+
+        <div>
+            <span className="dpwd--card-line-item">
+              0 <i className="fa fa-comment"/>
+            </span>
+
+            <span className="dpwd--card-line-item">
+              <div>
+                <span className="dpw--card-disc"/> 1/3 <i className="fa fa-folder-open"/>
+              </div>
+            </span>
+        </div>
+      </CardLine>
+    );
+  }
+
   render() {
     return (
-      <Card minimized={true} type="task">
+      <Card minimized={!this.state.isDone} type="task">
         <MarkDone isDone={this.state.isDone}
                   onToggle={this.onToggleDone} />
         <CardLine>
@@ -58,40 +97,7 @@ export class TaskCard extends React.Component {
           </div>
         </CardLine>
 
-        <CardLine>
-          <div>
-            <span className="overdue dpwd--card-line-item">
-              <i className="fa fa-calendar-o"/> Due: N/A
-              <input type="text" name="due-date" className="due-date-field" disabled="disabled"/>
-            </span>
-
-            <span>
-              <span className="dpw--card-disc"/>
-              <span className="dpwd--card-line-item">
-                <i className="fa fa-book"/> Some project
-              </span>
-            </span>
-
-            <span>
-              <span className="dpw--card-disc"/>
-              <span className="dpwd--card-line-item">
-                <i className="fa fa-link"/> <a href="#">Ticket title</a>
-              </span>
-            </span>
-          </div>
-
-          <div>
-            <span className="dpwd--card-line-item">
-              0 <i className="fa fa-comment"/>
-            </span>
-
-            <span className="dpwd--card-line-item">
-              <div>
-                <span className="dpw--card-disc"/> 1/3 <i className="fa fa-folder-open"/>
-              </div>
-            </span>
-          </div>
-        </CardLine>
+        {(this.state.expanded || !this.state.isDone) && this.renderDetails()}
       </Card>
     );
   }
