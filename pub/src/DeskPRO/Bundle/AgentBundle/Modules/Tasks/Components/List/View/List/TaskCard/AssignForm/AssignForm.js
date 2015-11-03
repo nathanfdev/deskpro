964cb8749382b0e782548e394f7fb38ec244422f
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-03 21:25:42 +0300

    task assign form fields

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/List/TaskCard/AssignForm/AssignForm.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/List/TaskCard/AssignForm/AssignForm.js
@@ -3,11 +3,9 @@ import {
   Header,
   Popup,
   FieldGroup,
-  FullField,
   FloatField,
   CollectionField,
   QuickFilter,
-  ShowOnlySelected,
   Unassign,
   AgentsList,
   AgentTeamsList,
@@ -25,10 +23,69 @@ export class AssignForm extends React.Component {
     departments: PropTypes.object.isRequired
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quickFilter: '',
+      agents: [],
+      agentTeams: [],
+      departments: [],
+      errors: {},
+      submit: false
+    };
+  }
+
   render() {
+    const { agents, agentTeams, departments } = this.props;
+
     return (
       <Popup>
-        <Header>Assign to task</Header>
+        <Header>Assign to Task</Header>
+
+        <form>
+          <div className="dpw--popup-content">
+            <FieldGroup>
+              <FloatField align="left">
+                <QuickFilter value={this.state.quickFilter}
+                             onChange={this.onChangeQuickFilter} />
+              </FloatField>
+
+              <FloatField align="right">
+                <Unassign onClick={this.onUnassignAll} />
+              </FloatField>
+            </FieldGroup>
+
+            <FieldGroup>
+              <CollectionField>
+                <div part="title">
+                  Agent <a href="#" onClick={this.onAssignSelf}>Assign to me</a>
+                </div>
+                <AgentsList values={agents}
+                            selected={this.state.agents}
+                            showOnlySelected={this.state.showOnlySelected}
+                            filter={this.state.quickFilter}
+                            onChange={this.onChangeAgents} />
+              </CollectionField>
+
+              <CollectionField title="Team">
+                <AgentTeamsList values={agentTeams}
+                                selected={this.state.agentTeams}
+                                showOnlySelected={this.state.showOnlySelected}
+                                filter={this.state.quickFilter}
+                                onChange={this.onChangeAgentTeams} />
+              </CollectionField>
+
+              <CollectionField title="Department">
+                <DepartmentsList values={departments}
+                                 selected={this.state.departments}
+                                 showOnlySelected={this.state.showOnlySelected}
+                                 filter={this.state.quickFilter}
+                                 onChange={this.onChangeDepartments} />
+              </CollectionField>
+            </FieldGroup>
+          </div>
+        </form>
       </Popup>
     );
   }
