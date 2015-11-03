6b9ef2c94664b7939e6f20fd9b7fe17f15c8fafe
Dennis Schipper <dennis@designfordetails.com>
2015-11-03 12:30:03 +0000

    removing the whole {} (used to add a blod style) broke the buidler

--- pub/src/DeskPRO/Bundle/PortalBundle/React/Feedback/Controls/StatusCategory.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/React/Feedback/Controls/StatusCategory.js
@@ -11,7 +11,7 @@ export default class StatusCategory extends React.Component {
     return (
       <div className="cat-checkbox-title">
         <input type="checkbox" checked={this.props.isActive} onChange={this.clicked.bind(this)}/>
-        <a style={this.props.isActive ? : {}} onClick={this.clicked.bind(this)}>
+        <a style={this.props.isActive ? {} : {}} onClick={this.clicked.bind(this)}>
           {this.props.cat.title}
         </a>
       </div>
