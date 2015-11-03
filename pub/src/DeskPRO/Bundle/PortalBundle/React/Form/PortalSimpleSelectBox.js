55abcbce6ee0a508e3115e4386d3d5430c2b6cd4
Chris Tickner <chris.tickner@gmail.com>
2015-11-03 16:44:01 -0500

    [Portal] fix select box dropdown width mis-match

--- pub/src/DeskPRO/Bundle/PortalBundle/React/Form/PortalSimpleSelectBox.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/React/Form/PortalSimpleSelectBox.js
@@ -126,7 +126,7 @@ export default class PortalSimpleSelectBox extends React.Component {
     }
 
     return (
-      <ul className={"level-" + this.state.level} onClick={this.dropdownClickHandler.bind(this)}>
+      <ul onClick={this.dropdownClickHandler.bind(this)}>
         {this.state.options.map((option) => {
             return (
               <SelectOption onClickOption={this.onClickOption.bind(this)}
