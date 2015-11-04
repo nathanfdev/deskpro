556ea39d3c09492f35da91241112ac80ab8c960b
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-04 20:47:58 +0300

    task list frame files struct

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
@@ -3,7 +3,7 @@ import { ListFrameContainer } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Co
 import ListFrameContents from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrameContents';
 import { Controls } from './ControlBar/Controls';
 import { ListGroup } from './ListGroup';
-import { TaskCard } from './View/List/TaskCard/TaskCard';
+import { TaskCard } from './TaskCard/TaskCard';
 import Immutable from 'immutable';
 
 export class List extends React.Component {
