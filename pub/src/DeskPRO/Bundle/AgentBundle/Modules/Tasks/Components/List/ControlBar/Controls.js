42519c9d70b766d9c3703c400d01d83cb7c288e3
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-04 23:17:15 +0300

    spited task control bar by dirs

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/Controls.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/Controls.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ListFrameMenu } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrameMenu';
-import { MassActionCheckboxContainer } from './MassActionCheckboxContainer';
-import { OrderByContainer } from './OrderByContainer';
-import { FilterByContainer } from './FilterByContainer';
-import { ViewModeSwitcherContainer } from './ViewModeSwitcherContainer';
+import { MassActionCheckboxContainer } from './MassAction/MassActionCheckboxContainer';
+import { OrderByContainer } from './OrderBy/OrderByContainer';
+import { FilterByContainer } from './FilterBy/FilterByContainer';
+import { ViewModeSwitcherContainer } from './ViewMode/ViewModeSwitcherContainer';
 
 export class Controls extends React.Component {
 
