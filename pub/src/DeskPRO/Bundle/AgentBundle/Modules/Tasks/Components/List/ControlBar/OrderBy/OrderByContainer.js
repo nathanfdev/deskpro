92093c243478445f9f949088f26fb54300850bb1
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-05 00:37:25 +0300

    tasks initial order by dropdown

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/OrderBy/OrderByContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/OrderBy/OrderByContainer.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Detached from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Positioned/Detached';
+import { ClickOut } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ClickOut';
 import { OrderBy } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/index';
+import { OrderByDropdown } from './OrderByDropdown';
 
 @connect()
 export class OrderByContainer extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownOpened: false
+    };
+  }
+
+  onOpenDropdown = () => {
+    this.setState({
+      dropdownOpened: true
+    });
+  };
+
+  onCloseDropDown = () => {
+    this.setState({
+      dropdownOpened: false
+    });
+  };
+
   render() {
     return (
-      <OrderBy {...this.props} />
+      <OrderBy ref="button"
+               toggleDropdown={this.onOpenDropdown}>
+
+        <Detached isOpen={this.state.dropdownOpened}
+                  positionAt="left bottom"
+                  positionTarget={this.refs.button}>
+
+          <ClickOut onClickOut={this.onCloseDropDown}>
+            <OrderByDropdown />
+          </ClickOut>
+        </Detached>
+      </OrderBy>
     );
   }
 }
---
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/OrderBy/OrderByDropdown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class OrderByDropdown extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Order by dropdown
+      </div>
+    );
+  }
+}
