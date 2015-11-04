7d4a3cc755ff36f8c81782ad595ba2f795b52365
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-04 20:40:25 +0300

    removed old new task form

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { ListFrameContainer } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/index';
 import ListFrameContents from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrameContents';
 import { Controls } from './ControlBar/Controls';
-import { TaskForm } from './TaskForm';
 import { ListGroup } from './ListGroup';
 import { TaskCard } from './View/List/TaskCard/TaskCard';
 import Immutable from 'immutable';
@@ -14,8 +13,6 @@ export class List extends React.Component {
       <ListFrameContainer>
         <Controls />
         <ListFrameContents>
-          <TaskForm />
-
           <ListGroup title="Overdue">
             <TaskCard task={Immutable.fromJS({date_due: '2015-01-01 11:15'})} />
             <TaskCard task={Immutable.fromJS({date_due: '2016-01-01'})} />
