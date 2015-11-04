bf378d8ab6bec29cbc560aa990dd0cd1b554f0b4
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-04 22:41:49 +0300

    tasks initial control buttons

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/Controls.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/Controls.js
@@ -1,41 +1,23 @@
 import React from 'react';
 import { ListFrameMenu } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrameMenu';
+import { OrderBy, FilterBy, ViewModeSwitcher } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/index';
+import { MassActionCheckbox } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/ControlBar';
 
 export class Controls extends React.Component {
 
   render() {
     return (
-      <ListFrameMenu ref="ticketControlBar">
+      <ListFrameMenu>
+        <MassActionCheckbox />
+        <OrderBy />
         <li>
-          <a href="#" ref="orderButton" className="dpwd-navigation-dropdown-top-row-button">
-            <span className="dpwd-navigation-dropdown-top-row-button-text dpwd-navigation-dropdown-top-row-button-text-grey">Order by:</span>
-            <span className="dpwd-navigation-dropdown-top-row-button-text">&nbsp;
-              <span className="focus"></span>
-              <span className="down">
-                <i className="fa fa-caret-down"/></span>
-            </span>
-          </a>
+          <hr/>
         </li>
+        <FilterBy />
         <li>
-          <a href="#" className="dpwd-navigation-dropdown-top-row-button">
-            <span className="dpwd-navigation-dropdown-top-row-button-text dpwd-navigation-dropdown-top-row-button-text-grey">Filter by:</span>
-            <span className="dpwd-navigation-dropdown-top-row-button-text">&nbsp;
-              <span className="focus">12</span>
-              <span className="down"> Completed <i className="fa fa-caret-down"/></span>
-            </span>
-          </a>
+          <hr/>
         </li>
-        <li>
-          <a href="#" className="dpwd-navigation-dropdown-top-row-button">
-            <span className="dpwd-navigation-dropdown-top-row-button-text dpwd-navigation-dropdown-top-row-button-text-grey">View:</span>
-            <span className="dpwd-navigation-dropdown-top-row-button-text">&nbsp;
-              <span className="multi task-list-view-switcher">
-                <span className="multi-down"> <i className="fa fa-caret-down"/></span>
-              </span>
-            </span>
-          </a>
-
-        </li>
+        <ViewModeSwitcher />
       </ListFrameMenu>
     );
   }
