5bbd11dfac2b906020ac674a4ebe17769fd721f3
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-05 00:43:57 +0300

    tasks initial view mode dropdown

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/ViewMode/ViewModeSwitcherContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/ViewMode/ViewModeSwitcherContainer.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Detached from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Positioned/Detached';
+import { ClickOut } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ClickOut';
 import { ViewModeSwitcher } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/index';
+import { ViewModeDropdown } from './ViewModeDropdown';
 
 @connect()
 export class ViewModeSwitcherContainer extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownOpened: false
+    };
+  }
+
+  onOpenDropdown = () => {
+    this.setState({
+      dropdownOpened: true
+    });
+  };
+
+  onCloseDropDown = () => {
+    this.setState({
+      dropdownOpened: false
+    });
+  };
+
   render() {
     return (
-      <ViewModeSwitcher {...this.props} />
+      <ViewModeSwitcher ref="button"
+               toggleDropdown={this.onOpenDropdown}>
+
+        <Detached isOpen={this.state.dropdownOpened}
+                  positionAt="left bottom"
+                  positionTarget={this.refs.button}>
+
+          <ClickOut onClickOut={this.onCloseDropDown}>
+            <ViewModeDropdown />
+          </ClickOut>
+        </Detached>
+      </ViewModeSwitcher>
     );
   }
 }
