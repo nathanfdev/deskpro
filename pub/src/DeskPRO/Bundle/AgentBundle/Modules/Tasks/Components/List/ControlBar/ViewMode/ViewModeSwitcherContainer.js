08bb7e4442473cd218eebfa85b6e7d75c84df3a6
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-05 21:46:11 +0300

    view mode options

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/ViewMode/ViewModeSwitcherContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/ViewMode/ViewModeSwitcherContainer.js
@@ -5,6 +5,13 @@ import { ClickOut } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/C
 import { ViewModeSwitcher } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/index';
 import { ViewModeDropdown } from './ViewModeDropdown';
 
+const viewModeOptions = [
+  { field: 'list', label: 'Card View', icon: 'list' },
+  { field: 'table', label: 'Table View', icon: 'table' },
+  { field: 'kanban', label: 'Kanban', icon: 'sticky-note-o' },
+  { field: 'calendar', label: 'Calendar', icon: 'calendar' }
+];
+
 @connect()
 export class ViewModeSwitcherContainer extends React.Component {
 
@@ -29,6 +36,8 @@ export class ViewModeSwitcherContainer extends React.Component {
   };
 
   render() {
+    const currentViewModeOption = viewModeOptions[0];
+
     return (
       <ViewModeSwitcher ref="button"
                toggleDropdown={this.onOpenDropdown}>
@@ -38,7 +47,8 @@ export class ViewModeSwitcherContainer extends React.Component {
                   positionTarget={this.refs.button}>
 
           <ClickOut onClickOut={this.onCloseDropDown}>
-            <ViewModeDropdown />
+            <ViewModeDropdown currentViewModeOption={currentViewModeOption}
+                              viewModeOptions={viewModeOptions} />
           </ClickOut>
         </Detached>
       </ViewModeSwitcher>
