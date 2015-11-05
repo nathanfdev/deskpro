66d51a8c6e48aa44fc78aaa23d1b62f02260eabb
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-05 08:59:21 +0300

    initial task view mode dropdown

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/ViewMode/ViewModeDropdown.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/ViewMode/ViewModeDropdown.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Menu from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Menu/Menu';
+import Item from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Menu/Item';
 
 export class ViewModeDropdown extends React.Component {
 
   render() {
     return (
-      <div>
-        View mode dropdown
-      </div>
+      <Menu>
+        <Item label="Card View"
+              icon="list"/>
+        <Item label="Table View"
+              icon="table"/>
+        <Item label="Kanban"
+              icon="sticky-note-o"/>
+        <Item label="Calendar"
+              icon="calendar"/>
+      </Menu>
     );
   }
 }
