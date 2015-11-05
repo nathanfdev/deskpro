0e785dbd1badf7d957f058a52ce53e058f16d902
Sergey <laa513@gmail.com>
2015-11-05 14:51:56 +0300

    Removed no longer needed loadAll* actions from new-agent

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/TaskCard/AssignForm/AssignFormContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/TaskCard/AssignForm/AssignFormContainer.js
@@ -5,9 +5,6 @@ import { meSelector } from 'DeskPRO/Bundle/AgentBundle/Modules/Application/Recor
 import { agentsSelector } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Selectors/agentsSelectors';
 import { agentTeamsSelector } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Selectors/agentTeamsSelectors';
 import { allDepartmentsSelector } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Selectors/departmentsSelectors';
-import { loadAllAgents } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Actions/agentsActions';
-import { loadAllAgentTeams } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Actions/agentTeamsActions';
-import { loadAllDepartments } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/RecordStores/Actions/departmentsActions';
 
 @connect(state => ({
   me: meSelector(state),
@@ -16,15 +13,6 @@ import { loadAllDepartments } from 'DeskPRO/Bundle/AgentBundle/Modules/Agent/Rec
   departments: allDepartmentsSelector(state)
 }))
 export class AssignFormContainer extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    props.dispatch(loadAllAgents());
-    props.dispatch(loadAllAgentTeams());
-    props.dispatch(loadAllDepartments());
-  }
-
   render() {
     return (
       <AssignForm {...this.props} />
