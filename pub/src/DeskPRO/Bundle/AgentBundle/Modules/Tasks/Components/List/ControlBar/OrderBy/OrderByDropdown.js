eb9b25e2e877cc028d904dc8287586a7e844ff84
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-05 08:55:21 +0300

    initial order by dropdown

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/OrderBy/OrderByDropdown.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ControlBar/OrderBy/OrderByDropdown.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import Menu from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Menu/Menu';
+import Item from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Menu/Item';
+import MenuFooter from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Menu/MenuFooter';
+import MenuFooterOptions from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Menu/MenuFooterOptions';
 
 export class OrderByDropdown extends React.Component {
 
   render() {
     return (
-      <div>
-        Order by dropdown
-      </div>
+      <Menu>
+        <Item label="List"
+              icon="list"/>
+        <Item label="Project"
+              icon="briefcase"/>
+        <Item label="Due Date"
+              icon="calendar"/>
+        <Item label="Done Date"
+              icon="calendar"/>
+        <Item label="Created Date"
+              icon="calendar"/>
+        <Item label="Assignee"
+              icon="user"/>
+        <MenuFooter>
+          <MenuFooterOptions>
+            Sort
+          </MenuFooterOptions>
+        </MenuFooter>
+      </Menu>
     );
   }
 }
