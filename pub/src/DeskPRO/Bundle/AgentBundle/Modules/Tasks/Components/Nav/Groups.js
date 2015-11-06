e4e0663e75ab71243f391b1392b010140e7b6986
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-06 14:32:17 +0300

    task nav groups routing hash

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Groups.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Groups.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
-import { Section, SectionHeader, ListItem } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/NavFrame/index';
+import { Section, SectionHeader } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/NavFrame/index';
+import { ListItemContainer } from './ListItemContainer';
 
 export class Groups extends React.Component {
 
@@ -14,12 +15,24 @@ export class Groups extends React.Component {
       <Section>
         <SectionHeader>Tasks</SectionHeader>
         <ul>
-          <ListItem count={groups.get('myTasksCount')} label="My Tasks" />
-          <ListItem count={groups.get('teamTasksCount')} label="My Team Tasks" />
-          <ListItem count={groups.get('deptTasksCount')} label="My Department Tasks" />
-          <ListItem count={groups.get('delegatedTasksCount')} label="Delegated Tasks" />
-          <ListItem count={groups.get('unassignedTasksCount')} label="Unassigned Tasks" />
-          <ListItem count={groups.get('allTasksCount')} label="All Tasks" />
+          <ListItemContainer count={groups.get('myTasksCount')}
+                             label="My Tasks"
+                             listOptions={{navItem: {custom_category: 1}}} />
+          <ListItemContainer count={groups.get('teamTasksCount')}
+                             label="My Team Tasks"
+                             listOptions={{navItem: {custom_category: 2}}} />
+          <ListItemContainer count={groups.get('deptTasksCount')}
+                             label="My Department Tasks"
+                             listOptions={{navItem: {custom_category: 3}}} />
+          <ListItemContainer count={groups.get('delegatedTasksCount')}
+                             label="Delegated Tasks"
+                             listOptions={{navItem: {custom_category: 4}}} />
+          <ListItemContainer count={groups.get('unassignedTasksCount')}
+                             label="Unassigned Tasks"
+                             listOptions={{navItem: {custom_category: 5}}} />
+          <ListItemContainer count={groups.get('allTasksCount')}
+                             label="All Tasks"
+                             listOptions={{navItem: {custom_category: 6}}} />
         </ul>
       </Section>
     );
---
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/ListItemContainer.js
@@ -0,0 +1,47 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { hashStateSelectorFactory } from 'DeskPRO/Bundle/AgentBundle/Modules/Application/Selectors/routing';
+import { urlSanitize } from 'DeskPRO/Bundle/AgentBundle/Modules/Application/Service/routing';
+import { ListItemStatefulContainer } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/NavFrame/index';
+
+@connect(state => ({
+  activeItemId: hashStateSelectorFactory(['nav', 'active'])(state)
+}))
+export class ListItemContainer extends React.Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    activeItemId: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    isComments: PropTypes.bool,
+    count: PropTypes.number.isRequired,
+    children: PropTypes.node,
+    listOptions: PropTypes.object.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.itemId = urlSanitize(props.label);
+  }
+
+  loadList = event => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.log(this.props.listOptions);
+  };
+
+  render() {
+    const props = this.props;
+    const newProps = {
+      ...props,
+      groupId: 'nav',
+      onClick: this.loadList,
+      itemId: this.itemId
+    };
+
+    return (
+      <ListItemStatefulContainer {...newProps} />
+    );
+  }
+}
