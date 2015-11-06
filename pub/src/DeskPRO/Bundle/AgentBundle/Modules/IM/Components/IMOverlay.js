0d0fa7469890b9c11292fec45a6d884908ec0297
Den <denis@ranneft.ru>
2015-11-06 13:47:45 +0300

    use clickout

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/IM/Components/IMOverlay.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/IM/Components/IMOverlay.js
@@ -3,6 +3,8 @@ import { AgentsList } from './Agents/AgentsList';
 import { TeamsList } from './Teams/TeamsList';
 import { DepartmentsList } from './Departments/DepartmentsList';
 import * as actions from '../Actions/chatsActions';
+import * as ui from '../Actions/uiActions';
+import { ClickOut } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ClickOut';
 
 export class IMOverlay extends React.Component {
   static propTypes = {
@@ -13,24 +15,30 @@ export class IMOverlay extends React.Component {
     this.props.dispatch(actions.startChat(id, type));
   };
 
+  handleOnClickOut = () => {
+    this.props.dispatch(ui.toggleOverlay());
+  };
+
   render() {
     return (
-      <div className="dropdown im-dropdown" id="im-dropdown">
-        <header className="dropdown-header">Agent Instant Messages</header>
-        <div className="wrapper">
-          <AgentsList />
-          <div className="bucket right">
-            <a href="#" onClick={this.startChat.bind(null, '0', 'everyone')}className="broadcast-to-all"><i className="fa fa-bullhorn"></i> Broadcast to Everyone</a>
+      <ClickOut onClickOut={this.handleOnClickOut}>
+        <div className="dropdown im-dropdown" id="im-dropdown">
+          <header className="dropdown-header">Agent Instant Messages</header>
+          <div className="wrapper">
+            <AgentsList />
+            <div className="bucket right">
+              <a href="#" onClick={this.startChat.bind(null, '0', 'everyone')}className="broadcast-to-all"><i className="fa fa-bullhorn"></i> Broadcast to Everyone</a>
 
-            <div className="im-list-wrapper">
-              <h2>Teams</h2>
-              <TeamsList />
-              <h2>Departments</h2>
-              <DepartmentsList />
+              <div className="im-list-wrapper">
+                <h2>Teams</h2>
+                <TeamsList />
+                <h2>Departments</h2>
+                <DepartmentsList />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </ClickOut>
     );
   }
 }
