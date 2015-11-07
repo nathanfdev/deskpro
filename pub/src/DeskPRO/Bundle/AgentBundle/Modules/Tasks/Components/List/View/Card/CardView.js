1f08c43d73e2d91f71dec4943a07b95df953ffe7
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-07 14:50:02 +0300

    tasks card view removed fake items

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Card/CardView.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Card/CardView.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import { ListGroup } from './ListGroup';
 import { TaskCard } from './TaskCard/TaskCard';
-import Immutable from 'immutable';
 
 export class CardView extends React.Component {
 
@@ -14,10 +13,6 @@ export class CardView extends React.Component {
       <div>
         <ListGroup title="Overdue">
           {this.props.tasks.map((task, index) => <TaskCard task={task} key={index} />)}
-        </ListGroup>
-        <ListGroup title="Other">
-          <TaskCard task={Immutable.fromJS({date_due: '2015-11-04 20:00'})} />
-          <TaskCard task={Immutable.fromJS({})} />
         </ListGroup>
       </div>
     );
