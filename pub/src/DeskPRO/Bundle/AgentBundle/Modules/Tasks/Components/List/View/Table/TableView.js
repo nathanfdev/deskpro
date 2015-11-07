4a6ad257f7b67d01d96e0a12d649149c5802537e
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-07 14:16:48 +0300

    tasks table view initial task card, tasks group components

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Table/TableView.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Table/TableView.js
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react';
 import { Header } from './Header/Header';
+import { ListGroup } from './ListGroup';
+import { TaskCard } from './TaskCard';
 
 export class TableView extends React.Component {
 
@@ -13,6 +15,14 @@ export class TableView extends React.Component {
         <table cellSpacing="0" className="condensed-task-list">
           <Header currentOrder="project"
                   currentDirection="desc" />
+
+          <ListGroup title="Overdue">
+            <TaskCard />
+            <TaskCard />
+            <TaskCard />
+            <TaskCard />
+            <TaskCard />
+          </ListGroup>
         </table>
       </div>
     );
---
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Table/TaskCard.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+
+export class TaskCard extends React.Component {
+
+  render() {
+    return (
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    );
+  }
+}
