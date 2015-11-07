9179e6bae693ecafe6388e88ce800774e397b946
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-07 11:09:45 +0300

    task card view

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
@@ -1,10 +1,7 @@
 import React, { PropTypes } from 'react';
 import { ListFrameContainer } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/index';
-import ListFrameContents from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrameContents';
 import { Controls } from './ControlBar/Controls';
-import { ListGroup } from './ListGroup';
-import { TaskCard } from './TaskCard/TaskCard';
-import Immutable from 'immutable';
+import { CardView } from './View/Card/CardView';
 import Loader from 'react-loader';
 
 export class List extends React.Component {
@@ -22,15 +19,7 @@ export class List extends React.Component {
         <Controls />
 
         <Loader loaded={loaded}>
-          <ListFrameContents>
-            <ListGroup title="Overdue">
-              {tasks.map((task, index) => <TaskCard task={task} key={index} />)}
-            </ListGroup>
-            <ListGroup title="Other">
-              <TaskCard task={Immutable.fromJS({date_due: '2015-11-04 20:00'})} />
-              <TaskCard task={Immutable.fromJS({})} />
-            </ListGroup>
-          </ListFrameContents>
+          <CardView tasks={tasks} />
         </Loader>
       </ListFrameContainer>
     );
---
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Card/CardView.js
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react';
+import ListFrameContents from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrameContents';
+import { ListGroup } from './ListGroup';
+import { TaskCard } from './TaskCard/TaskCard';
+import Immutable from 'immutable';
+
+export class CardView extends React.Component {
+
+  static propTypes = {
+    tasks: PropTypes.array.isRequired
+  };
+
+  render() {
+    return (
+      <ListFrameContents>
+        <ListGroup title="Overdue">
+          {this.props.tasks.map((task, index) => <TaskCard task={task} key={index} />)}
+        </ListGroup>
+        <ListGroup title="Other">
+          <TaskCard task={Immutable.fromJS({date_due: '2015-11-04 20:00'})} />
+          <TaskCard task={Immutable.fromJS({})} />
+        </ListGroup>
+      </ListFrameContents>
+    );
+  }
+}
