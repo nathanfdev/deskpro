2ab849db3e434eddca77c896a69ce4ec26de1e62
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-07 10:23:31 +0300

    tasks load real tasks

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { ListFrameContainer } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/index';
 import ListFrameContents from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrameContents';
 import { Controls } from './ControlBar/Controls';
 import { ListGroup } from './ListGroup';
 import { TaskCard } from './TaskCard/TaskCard';
 import Immutable from 'immutable';
+import Loader from 'react-loader';
 
 export class List extends React.Component {
 
+  static propTypes = {
+    tasks: PropTypes.array.isRequired,
+    loaded: PropTypes.bool.isRequired
+  };
+
   render() {
+    const { tasks, loaded } = this.props;
+
     return (
       <ListFrameContainer>
         <Controls />
-        <ListFrameContents>
-          <ListGroup title="Overdue">
-            <TaskCard task={Immutable.fromJS({date_due: '2015-01-01 11:15'})} />
-            <TaskCard task={Immutable.fromJS({date_due: '2016-01-01'})} />
-          </ListGroup>
-          <ListGroup title="Other">
-            <TaskCard task={Immutable.fromJS({date_due: '2015-11-04 20:00'})} />
-            <TaskCard task={Immutable.fromJS({})} />
-          </ListGroup>
-        </ListFrameContents>
+
+        <Loader loaded={loaded}>
+          <ListFrameContents>
+            <ListGroup title="Overdue">
+              {tasks.map((task, index) => <TaskCard task={task} key={index} />)}
+            </ListGroup>
+            <ListGroup title="Other">
+              <TaskCard task={Immutable.fromJS({date_due: '2015-11-04 20:00'})} />
+              <TaskCard task={Immutable.fromJS({})} />
+            </ListGroup>
+          </ListFrameContents>
+        </Loader>
       </ListFrameContainer>
     );
   }
--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ListContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ListContainer.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { List } from './List';
 
-@connect()
+@connect(state => ({
+  tasks: state.Tasks.tasks.get('elements'),
+  loaded: state.Tasks.tasks.getIn(['async', 'done'])
+}))
 export class ListContainer extends React.Component {
 
   render() {
