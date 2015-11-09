4372fec8a1f52d1302e5157cef0212047d3af640
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-09 15:11:49 +0300

    calendar task card template

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Calendar/TaskCard/TaskCard.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Calendar/TaskCard/TaskCard.js
@@ -22,6 +22,43 @@ export class TaskCard extends BaseTaskCard {
               <h1>{task.get('title')}</h1>
             </div>
           </div>
+
+          <div className="dpw--card-line-right">
+            <div className="dpwd--card-assigned">
+              <div className="dpw--avatar-face">assigneeAvatar</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="dpw--card-line">
+          <div className="dpw--card-line-left">
+            <span className="overdue dpwd--card-line-item">
+              <i className="fa fa-calendar-o" /> Due: N/A
+            </span>
+
+            <span>
+              <span className="dpw--card-disc" />
+              <span className="dpwd--card-line-item">
+                <i className="fa fa-book" /> Project title
+              </span>
+            </span>
+
+            <span>
+              <span className="dpw--card-disc" />
+              <span className="dpwd--card-line-item">
+                <i className="fa fa-link" /> <a href="#">Ticket title</a>
+              </span>
+            </span>
+          </div>
+
+          <div className="dpw--card-line-right">
+            <span className="dpwd--card-line-item">
+              1 <i className="fa fa-comment" />
+            </span>
+            <span className="dpwd--card-line-item">
+              <div><span className="dpw--card-disc" /> 1/3 <i className="fa fa-folder-open"/></div>
+            </span>
+          </div>
         </div>
       </Card>
     );
