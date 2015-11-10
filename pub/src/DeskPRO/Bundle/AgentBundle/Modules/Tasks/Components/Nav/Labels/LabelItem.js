0ca47b87da44edea85124b5bf7a6d73ce1dc8138
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-10 13:51:32 +0300

    task nav label on click func warn fix

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Labels/LabelItem.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Labels/LabelItem.js
@@ -6,11 +6,11 @@ export class LabelItem extends React.Component {
   static propTypes = {
     active: PropTypes.bool,
     label: PropTypes.object.isRequired,
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func
   };
 
   render() {
-    const { label, active, onClick } = this.props;
+    const { label, active, onClick = () => {} } = this.props;
     const name = label.get('label');
     const char = name && name.substr(0, 1).toUpperCase();
 
