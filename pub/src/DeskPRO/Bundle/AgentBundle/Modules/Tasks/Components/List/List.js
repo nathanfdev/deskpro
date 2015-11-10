a3afee1f82d9b574d9b3a249c4ecdc8cb58788d5
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-10 13:16:05 +0300

    tasks list check filter fix

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/List.js
@@ -40,7 +40,7 @@ export class List extends React.Component {
     return (
       <ListFrameContainer>
         <ControlBarContainer />
-        {listParams &&
+        {listParams.get('filter') &&
           <Loader loaded={loaded}
                   color="green"
                   opacity={0}
