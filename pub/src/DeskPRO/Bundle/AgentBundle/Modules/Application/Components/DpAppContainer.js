b4423bcfab7e2eaf0c5c5ab2bda855425fd90bae
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-10 11:04:30 +0300

    dp app container dispatch while render fix

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/DpAppContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/DpAppContainer.js
@@ -49,6 +49,15 @@ export class DpAppContainer extends React.Component {
     });
   }
 
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+    // dispatch hashChanged() when hash is changed to bind it to the redux state
+    window.onhashchange = () => dispatch(hashChanged(window.location.hash));
+    // dispatch hashChanged() to track the initial hash value
+    dispatch(hashChanged(window.location.hash));
+  }
+
   workOutBasePath() {
     const baseEnd = DP_BASE_URL.indexOf('/', DP_BASE_URL.indexOf('://') + 3);
 
@@ -56,14 +65,7 @@ export class DpAppContainer extends React.Component {
   }
 
   render() {
-    const { dispatch, history } = this.props;
-
-    // dispatch hashChanged() when hash is changed to bind it to the redux state
-    window.onhashchange = () => dispatch(hashChanged(window.location.hash));
-
-    // dispatch hashChanged() to track the initial hash value
-    dispatch(hashChanged(window.location.hash));
-
+    const { history } = this.props;
     const basePath = this.workOutBasePath();
     const defaultPath = `${basePath}/tasks`;
 
