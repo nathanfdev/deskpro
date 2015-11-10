67e9db0f736dcee9ffccc5d2fd21ef2e85adcde6
n3b <neb802@gmail.com>
2015-11-10 22:44:27 +0800

    backup for cli mode

--- app/src/Application/DeskPRO/Command/InstallCommand.php
+++ app/src/Application/DeskPRO/Command/InstallCommand.php
@@ -35,6 +35,7 @@ namespace Application\DeskPRO\Command;
 
 use Application\DeskPRO\App;
 use Application\DeskPRO\Entity;
+use Application\DeskPRO\JobQueue\Processor\Reset\SettingsProcessor;
 use Application\DeskPRO\Monolog\Handler\OrbLoggerAdapterHandler;
 use Application\InstallBundle\Data\DefaultDataProcessor;
 use Doctrine\DBAL\DBALException;
@@ -291,6 +292,8 @@ class InstallCommand extends \Symfony\Bundle\FrameworkBundle\Command\ContainerAw
             'name'  => 'core.install_via_cmd',
             'value' => 1,
         ));
+
+        SettingsProcessor::saveBaseSettings($this->getContainer()->getDb());
     }
 
     private function createDatabase()
