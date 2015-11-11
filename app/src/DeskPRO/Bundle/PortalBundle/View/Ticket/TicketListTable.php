392f54c43ddfef85e924b6883aae04be403c04ed
Chris Tickner <chris.tickner@gmail.com>
2015-11-11 14:36:10 -0500

    organization tickets should default to also show the "user" col

--- app/src/DeskPRO/Bundle/PortalBundle/View/Ticket/TicketListTable.php
+++ app/src/DeskPRO/Bundle/PortalBundle/View/Ticket/TicketListTable.php
@@ -150,7 +150,18 @@ class TicketListTable
 
     public function getDefaultColumnsIds()
     {
-        // if the request does not specify columns to show in the table initially, we use these columns
+        // get the initial columns to show by default
+
+        if ($this->ticket_type === TicketFilter::TYPE_ORGANIZATION) {
+            return [
+                TicketColumn::TYPE_SUBJECT,
+                TicketColumn::TYPE_DEPARTMENT,
+                TicketColumn::TYPE_USER,
+                TicketColumn::TYPE_DATE_CREATED,
+                TicketColumn::TYPE_DATE_ACTIVITY,
+            ];
+        }
+
         return [
             TicketColumn::TYPE_SUBJECT,
             TicketColumn::TYPE_DEPARTMENT,
