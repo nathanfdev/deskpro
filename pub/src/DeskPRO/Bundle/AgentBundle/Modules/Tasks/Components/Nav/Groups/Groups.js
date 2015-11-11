c81bc67853b152f8289ab7fecaccb7f837dc440e
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-11 21:36:37 +0300

    task nav moved groups options to container

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Groups/Groups.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Groups/Groups.js
@@ -3,34 +3,26 @@ import { Section, SectionHeader, ListItem } from 'DeskPRO/Bundle/AgentBundle/Mod
 import { ListItemContainer } from '../ListItemContainer';
 import jQuery from 'jquery';
 
-const groupOptions = {
-  my: {filter: {agents: ['me']}, label: 'My Tasks'},
-  team: {filter: {teams: ['me']}, label: 'My Team Tasks'},
-  department: {filter: {departments: ['me']}, label: 'My Department Tasks'},
-  delegated: {filter: {agents: ['not_me'], creator: 'me'}, label: 'My Delegated Tasks'},
-  unassigned: {filter: {agents: ['null'], teams: ['null'], departments: ['null']}, label: 'Unassigned Tasks'},
-  all: {filter: {done: 'all'}, label: 'All Tasks'}
-};
-
 export class Groups extends React.Component {
 
   static propTypes = {
-    groups: PropTypes.object.isRequired
+    groupsOptions: PropTypes.object.isRequired,
+    groupsCount: PropTypes.object.isRequired
   };
 
   render() {
-    const { groups } = this.props;
+    const { groupsOptions, groupsCount } = this.props;
 
     return (
       <Section>
         <SectionHeader>Tasks</SectionHeader>
         <ul>
-          {jQuery.map(groupOptions, (params, type) =>
+          {jQuery.map(groupsOptions, (params, type) =>
             <ListItemContainer key={type}
                                urlHash={params.label}
                                listOptions={params.filter}>
 
-              <ListItem count={groups.get(type)}
+              <ListItem count={groupsCount.get(type)}
                         label={params.label} />
             </ListItemContainer>
           )}
--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Groups/GroupsContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Groups/GroupsContainer.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Groups } from './Groups';
 
+const groupsOptions = {
+  my: {filter: {agents: ['me']}, label: 'My Tasks'},
+  team: {filter: {teams: ['me']}, label: 'My Team Tasks'},
+  department: {filter: {departments: ['me']}, label: 'My Department Tasks'},
+  delegated: {filter: {agents: ['not_me'], creator: 'me'}, label: 'My Delegated Tasks'},
+  unassigned: {filter: {agents: ['null'], teams: ['null'], departments: ['null']}, label: 'Unassigned Tasks'},
+  all: {filter: {done: 'all'}, label: 'All Tasks'}
+};
+
 @connect(state => ({
-  groups: state.Tasks.nav.get('groups')
+  groupsCount: state.Tasks.nav.get('groups')
 }))
 export class GroupsContainer extends React.Component {
 
   render() {
-    return <Groups {...this.props} />;
+    return <Groups {...this.props} groupsOptions={groupsOptions} />;
   }
 }
