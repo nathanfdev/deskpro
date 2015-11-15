f43581f849894e1cf3e598925126b64ecd082ac4
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-15 20:00:31 +0300

    task list group container

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ListGroupContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ListGroupContainer.js
@@ -3,31 +3,24 @@ import { connect } from 'react-redux';
 import { currentSortSelector } from '../../Selectors/list';
 import moment from 'moment';
 
-const dateGroups = {
-  past: {
-    hour: {title: 'This Hour', value: moment().startOf('hour')},
-    day: {title: 'Today', value: moment().startOf('day')},
-    tomorrow: {title: 'Yesterday', value: moment().startOf('day').subtract(1, 'd')},
-    week: {title: 'This Week', value: moment().startOf('week')},
-    lastWeek: {title: 'Last Week', value: moment().startOf('week').subtract(7, 'd')},
-    month: {title: 'This Month', value: moment().startOf('month')},
-    lastMonth: {title: 'Last Month', value: moment().startOf('month').subtract(1, 'M')},
-    year: {title: 'This Year', value: moment().startOf('year')},
-    forever: {title: 'Older', value: false}
-  },
-  future: {
-    overdue: {title: 'Overdue', value: false},
-    hour: { title: 'This Hour', value: moment().endOf('hour')},
-    day: {title: 'Today', value: moment().endOf('day')},
-    tomorrow: {title: 'Tomorrow', value: moment().endOf('day').add(1, 'd')},
-    week: {title: 'This Week', value: moment().endOf('week')},
-    nextWeek: {title: 'Next Week', value: moment().endOf('week').add(7, 'd')},
-    month: {title: 'This Month', value: moment().endOf('month')},
-    nextMonth: {title: 'Next Month', value: moment().endOf('month').add(1, 'M')},
-    year: {title: 'This Year', value: moment().endOf('year')},
-    forever: {title: 'Other', value: false}
-  }
-};
+const dateGroups = [
+  {title: 'This Hour', match: date => moment().isSame(date, 'hour')},
+  {title: 'Yesterday', match: date => moment().subtract(1, 'day').isSame(date, 'day')},
+  {title: 'Today', match: date => moment().isSame(date, 'day')},
+  {title: 'Tomorrow', match: date => moment().add(1, 'day').isSame(date, 'day')},
+
+  {title: 'Last Week', match: date => moment().subtract(1, 'week').isSame(date, 'week')},
+  {title: 'This Week', match: date => moment().isSame(date, 'week')},
+  {title: 'Next Week', match: date => moment().add('week').isSame(date, 'week')},
+
+  {title: 'Last Month', match: date => moment().subtract(1, 'month').isSame(date, 'month')},
+  {title: 'This Month', match: date => moment().isSame(date, 'month')},
+  {title: 'Next Month', match: date => moment().add(1, 'month').isSame(date, 'month')},
+
+  {title: 'Older', match: date => moment().isBefore(date, 'year')},
+  {title: 'This Year', match: date => moment().isSame(date, 'year')},
+  {title: 'Other', match: date => moment().isAfter(date, 'year')}
+];
 
 @connect(state => ({
   sort: currentSortSelector(state)
@@ -38,14 +31,55 @@ export class ListGroupContainer extends React.Component {
     tasks: PropTypes.object
   };
 
+  getGroupKey(task) {
+    const dateFilter = date => {
+      const matched = dateGroups.filter(group => group.match(date)).shift();
+      return matched ? matched.title : 'Other';
+    };
+
+    switch (this.props.sort) {
+      case 'project':
+        return task.get('project') || 'None';
+      case 'date_due':
+        return dateFilter(task.get('date_due'));
+      case 'date_done':
+        return dateFilter(task.get('date_done'));
+      case 'date_created':
+        return dateFilter(task.get('date_created'));
+      case 'assignee':
+        if (task.get('agents').size) {
+          return task.get('agents').first();
+        } else if (task.get('teams').size) {
+          return task.get('teams').first();
+        } else if (task.get('departments').size) {
+          task.get('departments').first();
+        }
+
+        return 'None';
+      case 'list':
+      default:
+        return task.get('list') || 'None';
+    }
+  }
+
   render() {
+    const { tasks } = this.props;
     const child = this.props.children;
     const childProps = child.props;
     const taskGroups = {};
 
+    tasks.forEach(task => {
+      const groupKey = this.getGroupKey(task);
+      if (!taskGroups[groupKey]) {
+        taskGroups[groupKey] = [];
+      }
+
+      taskGroups[groupKey].push(task);
+    });
+
     return React.cloneElement(child, {
       ...childProps,
-      tasks: this.props.tasks,
+      tasks: tasks,
       taskGroups: taskGroups
     });
   }
