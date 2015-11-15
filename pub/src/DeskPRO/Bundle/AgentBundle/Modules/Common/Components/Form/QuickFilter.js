840a816f588658c2a1946fc5d8e3d313a119c299
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-15 11:44:23 +0300

    quick filter cleanup

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Form/QuickFilter.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Form/QuickFilter.js
@@ -1,6 +1,7 @@
-import React, {Component, PropTypes} from 'react';
+import React from 'react';
 
-export class QuickFilter extends Component {
+export class QuickFilter extends React.Component {
+
   render() {
     return (
       <div className="dpw-quick-filter">
