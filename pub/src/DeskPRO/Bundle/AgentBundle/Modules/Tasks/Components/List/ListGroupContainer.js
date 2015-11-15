6488aa1f0ab5ee2430aa32ff08283649070b36b9
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-15 16:03:19 +0300

    task date groups

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ListGroupContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/ListGroupContainer.js
@@ -1,7 +1,37 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { currentSortSelector } from '../../Selectors/list';
+import moment from 'moment';
 
-@connect()
+const dateGroups = {
+  past: {
+    hour: {title: 'This Hour', value: moment().startOf('hour')},
+    day: {title: 'Today', value: moment().startOf('day')},
+    tomorrow: {title: 'Yesterday', value: moment().startOf('day').subtract(1, 'd')},
+    week: {title: 'This Week', value: moment().startOf('week')},
+    lastWeek: {title: 'Last Week', value: moment().startOf('week').subtract(7, 'd')},
+    month: {title: 'This Month', value: moment().startOf('month')},
+    lastMonth: {title: 'Last Month', value: moment().startOf('month').subtract(1, 'M')},
+    year: {title: 'This Year', value: moment().startOf('year')},
+    forever: {title: 'Older', value: false}
+  },
+  future: {
+    overdue: {title: 'Overdue', value: false},
+    hour: { title: 'This Hour', value: moment().endOf('hour')},
+    day: {title: 'Today', value: moment().endOf('day')},
+    tomorrow: {title: 'Tomorrow', value: moment().endOf('day').add(1, 'd')},
+    week: {title: 'This Week', value: moment().endOf('week')},
+    nextWeek: {title: 'Next Week', value: moment().endOf('week').add(7, 'd')},
+    month: {title: 'This Month', value: moment().endOf('month')},
+    nextMonth: {title: 'Next Month', value: moment().endOf('month').add(1, 'M')},
+    year: {title: 'This Year', value: moment().endOf('year')},
+    forever: {title: 'Other', value: false}
+  }
+};
+
+@connect(state => ({
+  sort: currentSortSelector(state)
+}))
 export class ListGroupContainer extends React.Component {
 
   static propTypes = {
@@ -11,10 +41,12 @@ export class ListGroupContainer extends React.Component {
   render() {
     const child = this.props.children;
     const childProps = child.props;
+    const taskGroups = {};
 
     return React.cloneElement(child, {
       ...childProps,
-      tasks: this.props.tasks
+      tasks: this.props.tasks,
+      taskGroups: taskGroups
     });
   }
 }
