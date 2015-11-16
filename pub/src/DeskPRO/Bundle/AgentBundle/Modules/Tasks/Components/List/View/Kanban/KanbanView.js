05ec8faa36c49868fb0b4a07aed9ea4372a5986b
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-16 14:39:06 +0300

    kanban checkbox

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Kanban/KanbanView.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Kanban/KanbanView.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { ListGroup } from './ListGroup';
-import { TaskCard } from './TaskCard/TaskCard';
+import { TaskCard } from './TaskCard';
+import { TaskCardContainer } from '../TaskCardContainer';
 
 export class KanbanView extends React.Component {
 
@@ -15,7 +16,11 @@ export class KanbanView extends React.Component {
       <div className="kanban kanban-columns">
         {taskGroups.map(taskGroup =>
           <ListGroup title={taskGroup.get('title')}>
-            {taskGroup.get('elements').map((task, index) => <TaskCard task={task} key={index} />)}
+            {taskGroup.get('elements').map((task, index) =>
+              <TaskCardContainer task={task} key={index}>
+                <TaskCard />
+              </TaskCardContainer>
+            )}
           </ListGroup>
         )}
       </div>
