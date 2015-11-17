1d35b8acdfd1929f81930a2f911bea7b1faa91e9
Sergey <laa513@gmail.com>
2015-11-17 21:08:42 +0300

    Base CRUD controller for nested resources such as /tickets/42/participants

---
+++ app/src/DeskPRO/Bundle/ApiBundle/Controller/CrudSubController.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+ * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * a British company located in London, England.
+ *
+ * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ *
+ * The license agreement under which this software is released
+ * can be found at https://www.deskpro.com/eula/
+ *
+ * By using this software, you acknowledge having read the license
+ * and agree to be bound thereby.
+ *
+ * Please note that DeskPRO is not free software. We release the full
+ * source code for our software because we trust our users to pay us for
+ * the huge investment in time and energy that has gone into both creating
+ * this software and supporting our customers. By providing the source code
+ * we preserve our customers' ability to modify, audit and learn from our
+ * work. We have been developing DeskPRO since 2001, please help us make it
+ * another decade.
+ *
+ * Like the work you see? Think you could make it better? We are always
+ * looking for great developers to join us: http://www.deskpro.com/jobs/
+ *
+ * ~ Thanks, Everyone at Team DeskPRO
+ */
+
+/**
+ * DeskPRO.
+ */
+namespace DeskPRO\Bundle\ApiBundle\Controller;
+
+use Doctrine\ORM\QueryBuilder;
+use FOS\RestBundle\Controller\Annotations\Get;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class CrudSubController.
+ *
+ * Base REST CRUD controller for nested resources such as /people/1/labels. All children must declare parent
+ * entity id URL param as {parentId}
+ */
+class CrudSubController extends CrudController
+{
+    public static $parentProperty;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function applyListFilters(QueryBuilder $qb, $alias, Request $request)
+    {
+        $prop = static::$parentProperty;
+        $qb
+            ->andWhere("$alias.$prop = :parentId")
+            ->setParameter('parentId', $this->findParentOr404()->getId());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function instantiateEntity(Request $request)
+    {
+        $entity             = new static::$entity();
+        $reflectionProperty = new \ReflectionProperty(static::$entity, static::$parentProperty);
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($entity, $this->findParentOr404());
+
+        return $entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function findEntity($id)
+    {
+        $parent = $this->findParentOr404();
+
+        $entity             = $this->findOr404(static::$entity, $id);
+        $reflectionProperty = new \ReflectionProperty(static::$entity, static::$parentProperty);
+        $reflectionProperty->setAccessible(true);
+        $entityParent = $reflectionProperty->getValue($entity);
+
+        if ($parent !== $entityParent) {
+            throw $this->createNotFoundException('Requested resources does not belong to the specified parent');
+        }
+
+        return $entity;
+    }
+
+    /**
+     * @return object
+     */
+    private function findParentOr404()
+    {
+        $request     = $this->container->get('request_stack')->getCurrentRequest();
+        $parentId    = $request->get('parentId');
+        $parentClass = $this->getManager()
+                            ->getClassMetadata(trim(static::$entity, '\\'))
+                            ->getAssociationMapping(static::$parentProperty)['targetEntity'];
+
+        return $this->findOr404($parentClass, $parentId);
+    }
+}
