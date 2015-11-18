8d5aa9e8750f023e0db9f5d374eb7762da024483
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-18 16:20:26 +0300

    custom drag layer little cleanup

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/CustomCardDragLayer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/CustomCardDragLayer.js
@@ -1,33 +1,6 @@
 import React, { PropTypes } from 'react';
 import { DragLayer } from 'react-dnd';
 
-const layerStyles = {
-  position: 'fixed',
-  pointerEvents: 'none',
-  zIndex: 10000,
-  left: 0,
-  top: 0,
-  width: '100%',
-  height: '100%'
-};
-
-function getItemStyles(props) {
-  const { currentOffset } = props;
-  if (!currentOffset) {
-    return {
-      display: 'none'
-    };
-  }
-
-  let { x, y } = currentOffset;
-
-  const transform = `translate(${x}px, ${y}px)`;
-  return {
-    transform: transform,
-    WebkitTransform: transform
-  };
-}
-
 @DragLayer(monitor => ({
   currentOffset: monitor.getSourceClientOffset(),
   isDragging: monitor.isDragging()
@@ -49,16 +22,39 @@ export class CustomCardDragLayer extends React.Component {
     children: PropTypes.any
   };
 
+  getItemStyles() {
+    const { currentOffset } = this.props;
+    if (!currentOffset) {
+      return {
+        display: 'none'
+      };
+    }
+
+    const transform = `translate(${currentOffset.x}px, ${currentOffset.y}px)`;
+    return {
+      transform: transform,
+      WebkitTransform: transform
+    };
+  }
+
   render() {
     const { isDragging, children } = this.props;
-
     if (!isDragging) {
       return null;
     }
 
     return (
-      <div style={layerStyles}>
-        <div style={getItemStyles(this.props)}>
+      <div style={{
+        position: 'fixed',
+        pointerEvents: 'none',
+        zIndex: 10000,
+        left: 0,
+        top: 0,
+        width: '100%',
+        height: '100%'
+      }}>
+
+        <div style={this.getItemStyles()}>
           {children}
         </div>
       </div>
