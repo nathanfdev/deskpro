81f2726d9c858102f823e9d810c522bec444fc2b
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-21 18:50:27 +0300

    open/close new task form

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Card/ListGroup.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/View/Card/ListGroup.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { DropTarget } from 'react-dnd';
 import * as constants from 'DeskPRO/Bundle/AgentBundle/Constants/Constants';
 import { groupTargetSpec, targetCollect } from '../../TaskCard/TaskCardContainer';
+import { ClickOut } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ClickOut';
 import { CardGroupDivider } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/View/Card';
 import classNames from 'classnames';
 
@@ -15,6 +16,43 @@ export class ListGroup extends React.Component {
     connectDropTarget: PropTypes.func.isRequired
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newTaskExpanded: false
+    };
+  }
+
+  onOpenNewTaskForm = event => {
+    event.preventDefault();
+    this.setState({
+      newTaskExpanded: true
+    });
+  };
+
+  onCloseNewTaskForm = () => {
+    this.setState({
+      newTaskExpanded: false
+    });
+  };
+
+  renderAddTaskButton() {
+    return (
+      <div>
+        [ <a href="#" onClick={this.onOpenNewTaskForm}>Add task</a> ]
+      </div>
+    );
+  }
+
+  renderNewTaskForm() {
+    return (
+      <ClickOut onClickOut={this.onCloseNewTaskForm}>
+        <div>New task form</div>
+      </ClickOut>
+    );
+  }
+
   render() {
     const { title, children, isOver, connectDropTarget } = this.props;
 
@@ -23,6 +61,8 @@ export class ListGroup extends React.Component {
         <CardGroupDivider title={title} />
 
         {children}
+
+        {this.state.newTaskExpanded ? this.renderNewTaskForm() : this.renderAddTaskButton()}
       </div>
     );
   }
