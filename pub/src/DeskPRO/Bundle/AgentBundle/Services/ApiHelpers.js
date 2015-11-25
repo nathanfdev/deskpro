cbbb172f070919a3c2f6f79c2a9d47eab869e760
Sergey <laa513@gmail.com>
2015-11-25 23:30:14 +0300

    compileParams() encode object values

--- pub/src/DeskPRO/Bundle/AgentBundle/Services/ApiHelpers.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Services/ApiHelpers.js
@@ -9,6 +9,10 @@ export function compileParams(params = {}) {
   for (const key of Object.keys(params)) {
     if (Object.prototype.toString.call(params[key]) === '[object Array]') {
       params[key].forEach(item => compiled.push(key + '[]=' + String(item).replace(/\s/g, '%20')));
+    } else if (params[key] && typeof params[key] === 'object') {
+      Object.keys(params[key]).forEach(
+        subKey => compiled.push(`${key}[${subKey}]=` + String(params[key][subKey]).replace(/\s/g, '%20'))
+      );
     } else if (params[key] !== undefined) {
       const str = String(params[key]);
       if (str !== 'null') {
