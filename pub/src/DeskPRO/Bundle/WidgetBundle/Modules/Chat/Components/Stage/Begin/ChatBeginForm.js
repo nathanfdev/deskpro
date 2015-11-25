7fb392c15596814baea649da7c48be98c98df848
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-25 18:41:14 +0300

    initial chat begin form

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Begin/ChatBeginForm.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Begin/ChatBeginForm.js
@@ -2,9 +2,43 @@ import React from 'react';
 
 export class ChatBeginForm extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: ''
+    };
+  }
+
+  onChangeName = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+
+  onChangeEmail = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    console.log('submit form');
+  };
+
   render() {
     return (
-      <div>Begin form mode</div>
+      <div>
+        Begin form mode
+
+        <form onSubmit={this.onSubmit}>
+          <input type="text" name={this.state.name} onChange={this.onChangeName} />
+          <input type="text" name={this.state.email} onChange={this.onChangeEmail} />
+
+          <input type="submit" />
+        </form>
+      </div>
     );
   }
 }
