7c08a1616db64323c2dd9d30fed7d4f5a38ca517
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-26 12:35:04 +0300

    frame render contents refactoring

--- pub/src/DeskPRO/Component/Ampliflux/common/components/Frame.js
+++ pub/src/DeskPRO/Component/Ampliflux/common/components/Frame.js
@@ -6,7 +6,6 @@ export default class Frame extends React.Component {
 
   static propTypes = {
     style: PropTypes.object,
-    head: PropTypes.node,
     isVisible: PropTypes.bool,
     positionMode: PropTypes.string,
     children: PropTypes.any
@@ -18,8 +17,7 @@ export default class Frame extends React.Component {
       dimensions: {
         width: 0,
         height: 0
-      },
-      isRendered: false
+      }
     };
   }
 
@@ -46,7 +44,7 @@ export default class Frame extends React.Component {
   getFrameStyles() {
     const { style = {}, isVisible, positionMode } = this.props;
 
-    let position = {};
+    let position;
     switch (positionMode) {
       case 'bottom.left':
         position = {left: 0, bottom: 0};
@@ -73,18 +71,13 @@ export default class Frame extends React.Component {
 
   autoFrameDimensions() {
     const doc = this.getContentDocument();
-    if (!doc) {
-      return false;
-    }
-
     const $container = jQuery(doc.body.firstChild);
     const width = $container.width();
     const height = $container.height();
 
     const dimensions = this.state.dimensions;
     if (dimensions.width === width && dimensions.height === height) {
-      // same dimensions, no need to update
-      return false;
+      return;
     }
 
     this.setState({
@@ -93,53 +86,23 @@ export default class Frame extends React.Component {
         height: height
       }
     });
-
-    return true;
   }
 
   renderFrameContents() {
-    if (this.state.isRendered) {
-      return;
-    }
-
     const doc = this.getContentDocument();
-    if (!doc) {
-      return;
-    }
 
-    if (doc && doc.readyState === 'complete') {
-      const { head, children } = this.props;
-      const contents = React.createElement('div', undefined, head, children);
+    if (doc.readyState === 'complete') {
+      const $head = jQuery(doc.head);
+      const $body = jQuery(doc.body);
 
-      const container = doc.createElement('div');
-      container.id = 'react_frame_container';
-      doc.body.appendChild(container);
+      const $styles = jQuery(document).find('style').clone();
+      const $container = jQuery('<div/>', {id: 'react_frame_container'});
 
-      // This is copying CSS from the current page
-      // into the iframe
-      // TODO this need some xbrowser testing
-      let css = [];
-      const styles = document.getElementsByTagName('style');
-      for (let i = 0; i < styles.length; i++) {
-        css.push(styles[i].innerHTML);
-      }
+      $head.html($styles);
+      $body.html($container);
 
-      css = css.join(' ');
-
-      const styleTag = doc.createElement('style');
-      styleTag.type = 'text/css';
-      if (styleTag.styleSheet) {
-        styleTag.styleSheet.cssText = css;
-      } else {
-        styleTag.appendChild(doc.createTextNode(css));
-      }
-
-      doc.body.appendChild(styleTag);
-
-      ReactDOM.render(contents, container);
-      this.setState({
-        isRendered: true
-      });
+      const contents = React.createElement('div', undefined, this.props.children);
+      ReactDOM.render(contents, doc.body.firstChild);
     } else {
       setTimeout(this.renderFrameContents, 0);
     }
