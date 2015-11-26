ce1665fe6a476def50740ee0a116b860f3fdb105
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-26 12:37:25 +0300

    widget styles

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/ChatApp.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/ChatApp.js
@@ -11,7 +11,7 @@ export class ChatApp extends React.Component {
     const { children } = this.props;
 
     return (
-      <div>
+      <div className="dpdesignportal mobile">
         Chat:
 
         <Link to="/chat/begin/simple">/chat/begin/simple</Link><br />
