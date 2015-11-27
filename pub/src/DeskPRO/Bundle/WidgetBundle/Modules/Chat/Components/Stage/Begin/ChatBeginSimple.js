4b3fbe04bf649cdd963b851ef75a2b39beb3d3a3
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-27 10:21:13 +0300

    begin simple chat send ajax request on load

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Begin/ChatBeginSimple.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Begin/ChatBeginSimple.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { WaitingPreview } from '../WaitingPreview';
 
 export class ChatBeginSimple extends React.Component {
+
+  static propTypes = {
+    onSubmit: PropTypes.func
+  };
+
+  componentDidMount() {
+    this.props.onSubmit();
+  }
 
   render() {
     return <WaitingPreview />;
