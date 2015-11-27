aeb6ddae2f267720dbf3bf9739f217602b0015a1
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-27 12:59:23 +0300

    copied ticket form as chat form

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Begin/ChatBeginForm.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Begin/ChatBeginForm.js
@@ -22,6 +22,42 @@ export class ChatBeginForm extends React.Component {
 
           <input type="submit" />
         </form>
+
+        <div className="dpdesignportal-content dpdesignportal-open-new-ticket">
+          <div className="header">
+            <h1>Open a new ticket</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</p>
+          </div>
+
+          <form className="dpdesignportal-form">
+            <h2>Open new ticket</h2>
+
+            <div>
+              <label>Your Details</label>
+              <input type="text" placeholder="First name, Last name" />
+            </div>
+
+            <div>
+              <label>Pick the department you want to contact:</label>
+              <span className="dpdesignportal-form-item-dropdown">Tech Support<i className="fa fa-caret-down"></i></span>
+            </div>
+
+            <div>
+              <label>Software version:</label>
+              <span className="dpdesignportal-form-item-dropdown">v2.1<i className="fa fa-caret-down"></i></span>
+            </div>
+
+            <div>
+              <label>Message:</label>
+              <textarea></textarea>
+            </div>
+
+            <div className="button-label">
+              <input type="submit" value="Open a ticket" className="dpdesignportal-button dpdesignportal-button-wide" />
+            </div>
+          </form>
+
+        </div>
       </div>
     );
   }
