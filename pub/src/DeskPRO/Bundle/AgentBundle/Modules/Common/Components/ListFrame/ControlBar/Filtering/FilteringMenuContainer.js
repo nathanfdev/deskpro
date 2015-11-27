e785a6ef9e98e5cf4bff8d94851ffe6cbb247a44
Alex Kosachev <alex.kosachev@deskpro.com>
2015-11-27 09:06:11 +0700

    Select filter markup and styles

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/ControlBar/Filtering/FilteringMenuContainer.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/ControlBar/Filtering/FilteringMenuContainer.js
@@ -282,7 +282,7 @@ export class FilteringMenu extends Component {
     return this.renderLabelsFilterInfo(selected);
   }
 
-  renderSelectFilter({ label, icon, param, multiple, options }, index) {
+  renderSelectFilter({ label, icon, param, multiple, quickFilter, options }, index) {
     const { dispatch, setParamsAction, stateValue } = this.props;
     const params = [param];
     options.map(option=> {
@@ -345,7 +345,7 @@ export class FilteringMenu extends Component {
         >
         {this.renderSelectFilterInfo(options, filterValue)}
         <Menu>
-          <ChoiceMenu title={label}>
+          <ChoiceMenu title={label} quickFilter={quickFilter}>
             <ul>
               {options.map((option, index2) =>
                   <ChoiceMenuOption
