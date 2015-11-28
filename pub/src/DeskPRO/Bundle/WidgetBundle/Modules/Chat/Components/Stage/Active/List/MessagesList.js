10b1f928be955febee7f56f240233e3a9c5bb36b
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-28 20:30:19 +0300

    widget chat initial messages scrollarea

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/List/MessagesList.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/List/MessagesList.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { AgentMessage } from './Message/AgentMessage';
 import { UserMessage } from './Message/UserMessage';
 import { TypingMessage } from './Message/TypingMessage';
+import ScrollArea from 'react-scrollbar';
 import Immutable from 'immutable';
 
 export class MessagesList extends React.Component {
 
   render() {
     return (
-      <div className="dpdesignportal-content">
-        <AgentMessage />
-        <UserMessage />
-        <TypingMessage user={Immutable.fromJS({name: 'Noelle'})} />
+      <div className="dpdesignportal-content" speed={0.8} style={{height: 300, overflowY: 'hidden'}}>
+        <ScrollArea vertical horizontal={false}>
+          <AgentMessage />
+          <AgentMessage />
+          <AgentMessage />
+          <AgentMessage />
+          <AgentMessage />
+          <AgentMessage />
+          <UserMessage />
+          <TypingMessage user={Immutable.fromJS({name: 'Noelle'})} />
+        </ScrollArea>
       </div>
     );
   }
