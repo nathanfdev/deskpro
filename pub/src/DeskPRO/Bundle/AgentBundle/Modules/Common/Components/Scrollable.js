95580384a1a1a096b03fe77028762b0fc05405e9
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-28 20:06:43 +0300

    scrollable method order warn fix

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Scrollable.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Scrollable.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 
 // NOTE
 // - <Scrollable> can only be used when the parent
-// element is positioned and has dimentions. Scrollable will fill the parent
+// element is positioned and has dimensions. Scrollable will fill the parent
 // entirely.
 // - So you must set width/height and at least position:relative.
 
@@ -17,10 +17,6 @@ export class Scrollable extends React.Component {
     vertical: PropTypes.bool,
     className: PropTypes.string
   };
-
-  render() {
-    return this.platformSupportsScrollbars() ? this.renderWithScrollbars() : this.renderWithoutScrollbars();
-  }
 
   // @todo how can we determine if current platform supports scrollbars?
   platformSupportsScrollbars() {
@@ -40,12 +36,11 @@ export class Scrollable extends React.Component {
           'as-vertical': vertical || both
         }
       )}>
-        <ScrollArea
-          className="dpscrollarea"
-          contentClassName="dpscrollarea"
-          horizontal={horizontal || both}
-          vertical={vertical || both}
-        >
+        <ScrollArea className="dpscrollarea"
+                    contentClassName="dpscrollarea"
+                    horizontal={horizontal || both}
+                    vertical={vertical || both}>
+
           {children}
         </ScrollArea>
       </div>
@@ -68,5 +63,9 @@ export class Scrollable extends React.Component {
         {children}
       </div>
     );
+  }
+
+  render() {
+    return this.platformSupportsScrollbars() ? this.renderWithScrollbars() : this.renderWithoutScrollbars();
   }
 }
