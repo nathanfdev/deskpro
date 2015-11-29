c95aa044bbff3cded7b5d9c8d5ae977457563a5d
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-29 07:39:54 +0300

    message list scrollarea

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/List/MessagesList.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/List/MessagesList.js
@@ -9,18 +9,16 @@ export class MessagesList extends React.Component {
 
   render() {
     return (
-      <div className="dpdesignportal-content" speed={0.8} style={{height: 300, overflowY: 'hidden'}}>
-        <ScrollArea vertical horizontal={false}>
-          <AgentMessage />
-          <AgentMessage />
-          <AgentMessage />
-          <AgentMessage />
-          <AgentMessage />
-          <AgentMessage />
-          <UserMessage />
-          <TypingMessage user={Immutable.fromJS({name: 'Noelle'})} />
-        </ScrollArea>
-      </div>
+      <ScrollArea className="dpdesignportal-content" vertical>
+        <AgentMessage />
+        <AgentMessage />
+        <AgentMessage />
+        <AgentMessage />
+        <AgentMessage />
+        <AgentMessage />
+        <UserMessage />
+        <TypingMessage user={Immutable.fromJS({name: 'Noelle'})} />
+      </ScrollArea>
     );
   }
 }
