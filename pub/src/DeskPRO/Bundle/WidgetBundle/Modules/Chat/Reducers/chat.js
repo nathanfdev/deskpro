b0ce34521fac31ae3dc91d22378a7dd8a95f6916
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-29 13:27:17 +0300

    removed chat default messages

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Reducers/chat.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Reducers/chat.js
@@ -4,28 +4,7 @@ import { async, mergeFullPayload } from 'Ampliflux/reducers/handlers';
 
 const initialState = {
   chat: {
-    messages: [
-      {
-        type: 'agent',
-        message: 'I have already written a view to show the current order status of every order that was touched today. I based it off of the view that exists in the system that is based off of the audit table.'
-      },
-      {
-        type: 'user',
-        message: 'I have already written a view to show the current order status of every order that was touched today. I based it off of the view that exists in the system that is based off of the audit table.'
-      },
-      {
-        type: 'user',
-        message: 'I have already written a view to show the current order status of every order that was touched today. I based it off of the view that exists in the system that is based off of the audit table.'
-      },
-      {
-        type: 'agent',
-        message: 'I have already written a view to show the current order status of every order that was touched today. I based it off of the view that exists in the system that is based off of the audit table.'
-      },
-      {
-        type: 'user',
-        message: 'I have already written a view to show the current order status of every order that was touched today. I based it off of the view that exists in the system that is based off of the audit table.'
-      }
-    ]
+    messages: []
   }
 };
 
