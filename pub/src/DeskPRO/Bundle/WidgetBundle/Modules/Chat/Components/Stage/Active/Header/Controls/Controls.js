63e1b73e750d0310d5482b08b4df7e71a539f303
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-30 17:43:54 +0300

    widget chat controls pane depends on chat status active/done

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Header/Controls/Controls.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Header/Controls/Controls.js
@@ -8,36 +8,42 @@ export class Controls extends React.Component {
     ended: PropTypes.bool
   };
 
-  render() {
+  renderActive() {
     return (
-      <div>
-        <ControlsPane>
-          <ControlItem>
-            <i className="fa fa-angle-double-left"></i>Assets
-          </ControlItem>
-          <ControlItem className="dpdesignportal-chat-header-control-mute">
-            <i className="fa fa-volume-up"></i>Mute
-          </ControlItem>
-          <ControlItem>
-            <span className="dpdesignportal-checkbox-container">
-              <span className="dpdesignportal-checkbox"><i className="fa fa-check"></i></span>
-              Chat Transcript <i className="fa fa-exclamation-circle"></i>
-            </span>
-          </ControlItem>
-          <ControlItem>
-            End Chat <i className="fa fa-power-off"></i>
-          </ControlItem>
-        </ControlsPane>
-
-        <ControlsPane>
-          <ControlItem>
-            <i className="fa fa-angle-double-left"></i>Assets
-          </ControlItem>
-          <ControlItem>
-            Reopen Chat <i className="fa fa-commenting-o"></i>
-          </ControlItem>
-        </ControlsPane>
-      </div>
+      <ControlsPane>
+        <ControlItem>
+          <i className="fa fa-angle-double-left"></i>Assets
+        </ControlItem>
+        <ControlItem className="dpdesignportal-chat-header-control-mute">
+          <i className="fa fa-volume-up"></i>Mute
+        </ControlItem>
+        <ControlItem>
+          <span className="dpdesignportal-checkbox-container">
+            <span className="dpdesignportal-checkbox"><i className="fa fa-check"></i></span>
+            Chat Transcript <i className="fa fa-exclamation-circle"></i>
+          </span>
+        </ControlItem>
+        <ControlItem>
+          End Chat <i className="fa fa-power-off"></i>
+        </ControlItem>
+      </ControlsPane>
     );
+  }
+
+  renderDone() {
+    return (
+      <ControlsPane>
+        <ControlItem>
+          <i className="fa fa-angle-double-left"></i>Assets
+        </ControlItem>
+        <ControlItem>
+          Reopen Chat <i className="fa fa-commenting-o"></i>
+        </ControlItem>
+      </ControlsPane>
+    );
+  }
+
+  render() {
+    return this.props.ended ? this.renderDone() : this.renderActive();
   }
 }
