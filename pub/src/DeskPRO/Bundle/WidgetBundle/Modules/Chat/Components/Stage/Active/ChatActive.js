8351a7bf459fe41bb0c48171725b760d0e224bb2
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-11-30 14:33:43 +0300

    widget end chat popup template

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/ChatActive.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/ChatActive.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header } from './Header';
 import { MessagesListContainer } from './List/MessagesListContainer';
 import { ReplyFormContainer } from './Reply/ReplyFormContainer';
+import { EndChatConfirm } from './EndChatConfirm';
 
 export class ChatActive extends React.Component {
 
@@ -11,6 +12,8 @@ export class ChatActive extends React.Component {
         <Header />
         <MessagesListContainer />
         <ReplyFormContainer />
+
+        <EndChatConfirm />
       </div>
     );
   }
