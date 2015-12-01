9316ea4ab3f1db8dca23a4d31ae46fd8f817dfaa
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-01 19:07:50 +0300

    widget footer link

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/WidgetFooter.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/WidgetFooter.js
@@ -5,7 +5,9 @@ export class WidgetFooter extends React.Component {
   render() {
     return (
       <div className="dpdesignportal-powered-by-deskpro">
-        <a href="#"><hr/> Support powered by <span className="dpdesignportal-deskpro-mark-logo"></span> <hr/></a>
+        <a href="http://deskpro.com/" target="_blank">
+          <hr/> Support powered by <span className="dpdesignportal-deskpro-mark-logo"></span> <hr/>
+        </a>
       </div>
     );
   }
