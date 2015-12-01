aa5a4236460c62dd59fa0c5c95b8bb67f77fa935
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-01 13:49:53 +0300

    dp app loading small refactoring

--- pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/DpAppLoading.js
+++ pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/DpAppLoading.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export class DpAppLoading extends React.Component {
+
   render() {
-    return (<div className="dp-window-loading">
-      LOADING
-    </div>);
+    return (
+      <div className="dp-window-loading">
+        LOADING
+      </div>
+    );
   }
 }
