93dfa9fcd16b0007e109083106837067405c1225
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-02 14:34:59 +0300

    widget rate agent form removed unused fields

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Feedback/RateAgentForm.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Feedback/RateAgentForm.js
@@ -24,27 +24,6 @@ export class RateAgentForm extends React.Component {
               <textarea placeholder="Enter your message here"></textarea>
             </label>
 
-            <div className="label">
-              <span className="dpdesignportal-form-item-label-title">How lorel ipsum is the lorel ipsum</span>
-              <div className="dpdesignportal-radio-group dpdesignportal-radio-group-rating">
-                <a className="dpdesignportal-radio dpdesignportal-radio-active rate-1" href="#"><span></span></a>
-                <a className="dpdesignportal-radio rate-2" href="#"><span></span></a>
-                <a className="dpdesignportal-radio rate-3" href="#"><span></span></a>
-                <a className="dpdesignportal-radio rate-4" href="#"><span></span></a>
-                <a className="dpdesignportal-radio rate-5" href="#"><span></span></a>
-                <a className="dpdesignportal-radio rate-6" href="#"><span></span></a>
-                <a className="dpdesignportal-radio rate-7" href="#"><span></span></a>
-                <a className="dpdesignportal-radio rate-8" href="#"><span></span></a>
-                <a className="dpdesignportal-radio rate-9" href="#"><span></span></a>
-                <a className="dpdesignportal-radio rate-10" href="#"><span></span></a>
-              </div>
-            </div>
-
-            <div className="label">
-              <span className="dpdesignportal-form-item-label-title">How lorel ipsum is the lorel ipsum</span>
-              <span className="dpdesignportal-form-item-dropdown">Option<i className="fa fa-caret-down"></i></span>
-            </div>
-
             <div className="label button-label">
                 <span className="dpdesignportal-checkbox-container">
                   <span className="dpdesignportal-checkbox"><i className="fa fa-check"></i></span>
