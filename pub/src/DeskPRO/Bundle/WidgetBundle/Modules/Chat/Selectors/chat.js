c712a5e397ae6b3597cef2b95085948f5ac93518
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-03 16:49:19 +0300

    widget chat last message selector fix

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Selectors/chat.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Selectors/chat.js
@@ -36,5 +36,5 @@ export const messagesSelector = createSelector(
 
 export const lastMessageIdSelector = createSelector(
   messagesSelector,
-  messages => messages && messages.size ? messages.max((a, b) => a.get('id') - b.get('id')).first() : null
+  messages => messages && messages.size ? messages.max((a, b) => a.get('id') - b.get('id')).get('id') : null
 );
