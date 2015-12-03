6dbc6d9ce30f8684db013dea5f182d7f01ae7bac
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-03 19:49:43 +0300

    widget chat header no avatar icon, real agent data

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Disconnect/AgentDisconnectedContainer.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Disconnect/AgentDisconnectedContainer.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { agentNameSelector, agentAvatarSelector } from '../../../../Selectors/chat';
 import { AgentDisconnected } from './AgentDisconnected';
 
-@connect()
+@connect(state => ({
+  agentName: agentNameSelector(state),
+  agentAvatar: agentAvatarSelector(state)
+}))
 export class AgentDisconnectedContainer extends React.Component {
 
   render() {
