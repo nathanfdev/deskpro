fe88d11bf51ca0b7f7db30590db26b0b15f41a0c
Sergey <laa513@gmail.com>
2015-12-04 01:36:07 +0300

    Test runner cache warm up hack

--- app/tests/phpunit/DpTest/Application/EmailBundle/Templating/EngineTest.php
+++ app/tests/phpunit/DpTest/Application/EmailBundle/Templating/EngineTest.php
@@ -41,7 +41,7 @@ class EngineTest extends PortalTestCase
 {
     public function testEngineRendersBasicEmailBundleTemplate()
     {
-        $this->installDataSet('fresh', true);
+        $this->installDataSet('fresh');
         $this->assertNotEmpty(
             $this->getTemplating()->render('EmailBundle:Portal:reset-password.html.twig', array()),
             'EmailBundle templates load and can be rendered'
@@ -50,7 +50,7 @@ class EngineTest extends PortalTestCase
 
     public function testEngineRendersBasicDeskproEmailTemplate()
     {
-        $this->installDataSet('fresh', true);
+        $this->installDataSet('fresh');
         $this->assertNotEmpty(
             $this->getTemplating()->render('DeskPRO:emails_agent:agent-welcome-usersource.html.twig', array()),
             'DeskPRO email templates load and can be rendered'
