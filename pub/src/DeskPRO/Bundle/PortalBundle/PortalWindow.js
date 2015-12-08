da7b4abfd2d52ca7fd3ef6a7828c9d6e449c74f7
Chris Tickner <chris.tickner@gmail.com>
2015-12-08 18:30:26 -0500

    portal: hide links to tickets/feedback/chat in dropdown if no permissions for them

--- pub/src/DeskPRO/Bundle/PortalBundle/PortalWindow.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/PortalWindow.js
@@ -4,7 +4,10 @@ const portal_window = {
   web_url: window.DESKPRO_WEB_URL,
   lang: window.DESKPRO_LANG,
   is_multi_lang: window.DESKPRO_MULTI_LANG,
-  enabled_langs: window.DESKPRO_ENABLED_LANGS
+  enabled_langs: window.DESKPRO_ENABLED_LANGS,
+  can_use_tickets: window.DESKPRO_CAN_USE_TICKETS,
+  can_use_feedback: window.DESKPRO_CAN_USE_FEEDBACK,
+  can_use_chat: window.DESKPRO_CAN_USE_CHAT
 };
 
 console.log('PortalWindow: %o', portal_window);
