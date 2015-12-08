2e0d2d1d649b771f5e8433ce0ae0bc7c210cce73
Chris Tickner <chris.tickner@gmail.com>
2015-12-08 18:19:26 -0500

    portal: only send ticket_ref to thank you page if the user is able to login (for the ticket link)

--- app/src/DeskPRO/Bundle/PortalBundle/Controller/NewTicketController.php
+++ app/src/DeskPRO/Bundle/PortalBundle/Controller/NewTicketController.php
@@ -233,7 +233,7 @@ class NewTicketController extends AbstractController
         }
 
         $params = [
-            'ticket_ref'        => $person->isConfirmed() ? $this->get('ticket.public_id_resolver')->findId($ticket) : null,
+            'ticket_ref'        => $person->isUser() ? $this->get('ticket.public_id_resolver')->findId($ticket) : null,
             'create_pw_link'    => $create_pw_link,
             'is_confirmed_user' => $person->isConfirmed(),
         ];
