cade291ced067480b17085ce87bd4b739a77d3b3
Christopher Nadeau <chris@nadeau.ws>
2015-12-10 11:46:08 +0000

    Fix error to do with possible null object

--- app/src/Application/DeskPRO/Tickets/TicketDisplay.php
+++ app/src/Application/DeskPRO/Tickets/TicketDisplay.php
@@ -244,6 +244,10 @@ class TicketDisplay implements PersonContextInterface
 
     public function getMessageAttachments($message, $include_inline = false)
     {
+        if (!$message) {
+            return;
+        }
+
         $id              = $message->getId();
         $messagetoattach = $this->getMessagesToAttachments($include_inline);
 
