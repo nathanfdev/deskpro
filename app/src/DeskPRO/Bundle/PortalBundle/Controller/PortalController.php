3fd0cfacde3d792f3a2cb821384a0b8f69a6370d
Chris Tickner <chris.tickner@gmail.com>
2015-12-09 19:49:39 -0500

    portal: dont start a session in controllers without checking if one exists

--- app/src/DeskPRO/Bundle/PortalBundle/Controller/PortalController.php
+++ app/src/DeskPRO/Bundle/PortalBundle/Controller/PortalController.php
@@ -124,7 +124,7 @@ class PortalController extends AbstractController
         }
 
         $captcha_form  = null;
-        $last_username = $this->getSession()->get('last_username');
+        $last_username = $request->hasPreviousSession() ? $this->getSession()->get('last_username') : null;
         $abuse_check   = new LoginAbuseCheck($last_username, $request->getClientIp());
         $abuse_check->markAsCheckOnly();
         $this->getAntiAbuseService()->check($abuse_check);
