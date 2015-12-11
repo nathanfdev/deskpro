2dff0034c1b0df8235a5d4e9f86830dacbb99ee8
Chris Tickner <chris.tickner@gmail.com>
2015-12-11 12:56:37 -0500

    portal: re-enable debug info on ajax requests

--- app/sys/KernelBooter.php
+++ app/sys/KernelBooter.php
@@ -337,7 +337,7 @@ class KernelBooter
                     if ($kernel instanceof PortalHttpCache
                         && strpos($request->getPathInfo(), '/_wdt') === false
                         && strpos($request->getPathInfo(), '/_profile') === false
-                        && !$request->isXmlHttpRequest()
+                        //&& !$request->isXmlHttpRequest()
                     ) {
                         $pretty_log       = HttpCacheDebugPrinter::debugPortalCacheKernel($kernel);
                         $response_content = $response->getContent();
