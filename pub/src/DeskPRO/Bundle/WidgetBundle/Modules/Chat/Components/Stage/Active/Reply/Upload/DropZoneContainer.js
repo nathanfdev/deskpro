2a69d839fb46d870dbcfdeb6a1b3ec491c3305c3
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-11 16:27:41 +0300

    widget chat disable upload listeners on unmount uploader

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Reply/Upload/DropZoneContainer.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Reply/Upload/DropZoneContainer.js
@@ -25,18 +25,23 @@ export class DropZoneContainer extends React.Component {
     $(document).on('dragover', this.onDragStarted);
     $(parent.window.document).on('dragover', this.onDragStarted);
 
-    $(document).bind('drop dragover', e => {
-      e.preventDefault();
-    });
-
-    $(parent.window.document).bind('drop dragover', e => {
-      e.preventDefault();
-    });
+    $(document).on('drop dragover', this.onDefaultDrop);
+    $(parent.window.document).on('drop dragover', this.onDefaultDrop);
   }
 
   componentWillUnmount() {
     $('#fileupload').fileupload('destroy');
+
+    $(document).off('dragover', this.onDragStarted);
+    $(parent.window.document).off('dragover', this.onDragStarted);
+
+    $(document).off('drop dragover', this.onDefaultDrop);
+    $(parent.window.document).off('drop dragover', this.onDefaultDrop);
   }
+
+  onDefaultDrop = e => {
+    e.preventDefault();
+  };
 
   onDragStarted = () => {
     if (!this.timeout) {
