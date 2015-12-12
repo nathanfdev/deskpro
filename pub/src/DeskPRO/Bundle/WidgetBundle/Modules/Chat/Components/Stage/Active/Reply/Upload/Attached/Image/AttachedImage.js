8d2fc9d157539ecf76857df3f0c0a020bce91e09
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-12 21:58:55 +0300

    widget chat initial attached image

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Reply/Upload/Attached/Image/AttachedImage.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Reply/Upload/Attached/Image/AttachedImage.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 export class AttachedImage extends React.Component {
 
+  static propTypes = {
+    attachment: PropTypes.object
+  };
+
   render() {
-    return <div />;
+    const { attachment } = this.props;
+
+    return (
+      <li>
+        <div className="dpdesignportal-chat-form-attached-image">
+          <div className="dpdesignportal-chat-form-attached-image-remove">
+            <i className="fa fa-times"></i>
+          </div>
+          <div className="dpdesignportal-chat-form-attached-image-thumb"
+               style={{backgroundImage: `url(${attachment.get('download_url')})`}} />
+        </div>
+      </li>
+    );
   }
 }
--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Reply/Upload/Attached/Image/AttachedImages.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Reply/Upload/Attached/Image/AttachedImages.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import Immutable from 'immutable';
+import { AttachedImage } from './AttachedImage';
 
 export class AttachedImages extends React.Component {
 
@@ -25,24 +26,9 @@ export class AttachedImages extends React.Component {
         </div>
         <div className="dpdesignportal-chat-form-attached-image-list">
           <ul>
-            <li>
-              <div className="dpdesignportal-chat-form-attached-image">
-                <div className="dpdesignportal-chat-form-attached-image-remove"><i className="fa fa-times"></i></div>
-                <div className="dpdesignportal-chat-form-attached-image-thumb" style={{backgroundImage: `url(${lastImage.get('download_url')})`}}></div>
-              </div>
-            </li>
-            <li>
-              <div className="dpdesignportal-chat-form-attached-image">
-                <div className="dpdesignportal-chat-form-attached-image-remove"><i className="fa fa-times"></i></div>
-                <div className="dpdesignportal-chat-form-attached-image-thumb" style={{backgroundImage: `url(${lastImage.get('download_url')})`}}></div>
-              </div>
-            </li>
-            <li>
-              <div className="dpdesignportal-chat-form-attached-image">
-                <div className="dpdesignportal-chat-form-attached-image-remove"><i className="fa fa-times"></i></div>
-                <div className="dpdesignportal-chat-form-attached-image-thumb" style={{backgroundImage: `url(${lastImage.get('download_url')})`}}></div>
-              </div>
-            </li>
+            <AttachedImage attachment={lastImage} />
+            <AttachedImage attachment={lastImage} />
+            <AttachedImage attachment={lastImage} />
           </ul>
         </div>
       </div>
