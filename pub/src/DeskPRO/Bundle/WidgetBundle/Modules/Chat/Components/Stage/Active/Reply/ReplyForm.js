f44de1d42217b482e409a991f27a1034725177b4
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-15 17:45:24 +0300

    widget chat hide screen share button

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Reply/ReplyForm.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Reply/ReplyForm.js
@@ -128,9 +128,13 @@ export class ReplyForm extends React.Component {
               <i className="fa fa-upload"></i> Upload file
               <input ref="fileUpload" className="file" type="file" name="files[]" multiple />
             </span>
-            <a href="#" className="dpdesignportal-chat-form-button" onClick={this.onScreenShare}>
-              <i className="fa fa-camera"></i> Screen Share
-            </a>
+
+            {false /* disabled for now */ &&
+              <a href="#" className="dpdesignportal-chat-form-button" onClick={this.onScreenShare}>
+                <i className="fa fa-camera"></i> Screen Share
+              </a>
+            }
+
             <EmotionButton
               buttonClassName="img"
               context={[parent.document, window.widgetFrame.document]}
