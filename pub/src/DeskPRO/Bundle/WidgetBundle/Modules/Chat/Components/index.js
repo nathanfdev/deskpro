f93f290892f2fa3c8af0e77cad0f42e2383eb940
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-17 23:05:13 +0300

    reverted chat router

---
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/index.js
@@ -0,0 +1,19 @@
+import { ChatApp } from './ChatApp';
+import { ChatBeginContainer } from './Stage/Begin/ChatBeginContainer';
+import { ChatBeginSimple } from './Stage/Begin/ChatBeginSimple';
+import { ChatBeginConversation } from './Stage/Begin/Conversation/ChatBeginConversation';
+import { ChatBeginForm } from './Stage/Begin/Form/ChatBeginForm';
+import { ChatPollingContainer } from './Stage/ChatPollingContainer';
+import { ChatWaiting } from './Stage/Waiting/ChatWaiting';
+import { ChatActive } from './Stage/Active/ChatActive';
+
+export {
+  ChatApp,
+  ChatBeginContainer,
+  ChatBeginSimple,
+  ChatBeginConversation,
+  ChatBeginForm,
+  ChatPollingContainer,
+  ChatWaiting,
+  ChatActive
+};
--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Widget/Components/WidgetApp.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Widget/Components/WidgetApp.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Router, Redirect } from 'react-router';
+import { Router, Route, Redirect } from 'react-router';
 import { WidgetFrameContainer } from './WidgetFrameContainer';
 import { Widget } from './Parts/Widget';
 import { WidgetHeaderContainer } from './Parts/Header/WidgetHeaderContainer';
 import { WidgetBody } from './Parts/WidgetBody';
 import { WidgetFooter } from './Parts/WidgetFooter';
-import { ChatRouter } from '../../Chat/Components/ChatRouter';
+import {
+  ChatApp,
+  ChatBeginContainer,
+  ChatBeginSimple,
+  ChatBeginConversation,
+  ChatBeginForm,
+  ChatPollingContainer,
+  ChatActive,
+  ChatWaiting
+} from '../../Chat/Components/index';
 import history from '../../../Services/history';
 
 export class WidgetApp extends React.Component {
@@ -22,7 +31,17 @@ export class WidgetApp extends React.Component {
           <WidgetBody>
             <Router history={history}>
               <Redirect from="/" to="chat"/>
-              <ChatRouter />
+              <Route path="chat" component={ChatApp}>
+                <Route path="begin" component={ChatBeginContainer}>
+                  <Route name="chat_begin_simple" path="simple" component={ChatBeginSimple} />
+                  <Route name="chat_begin_conversation" path="conversation" component={ChatBeginConversation} />
+                  <Route name="chat_begin_form" path="form" component={ChatBeginForm} />
+                </Route>
+                <Route component={ChatPollingContainer}>
+                  <Route name="chat_waiting" path="waiting" component={ChatWaiting} />
+                  <Route name="chat_active" path="active" component={ChatActive} />
+                </Route>
+              </Route>
             </Router>
           </WidgetBody>
           <WidgetFooter />
