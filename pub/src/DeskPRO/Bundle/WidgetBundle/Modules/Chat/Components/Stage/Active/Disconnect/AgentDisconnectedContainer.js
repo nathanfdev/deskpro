912269a3bc78f7431e7dd46b73b36339fe216330
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-17 17:31:37 +0300

    widget chat agent disconnected container check for timeout message

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Disconnect/AgentDisconnectedContainer.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Disconnect/AgentDisconnectedContainer.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { agentNameSelector, agentAvatarSelector } from '../../../../Selectors/chat';
 import { AgentDisconnected } from './AgentDisconnected';
 import { FindAnotherAgent } from './FindAnotherAgent';
 import { WaitingLoader } from './WaitingLoader';
+import {
+  agentNameSelector,
+  agentAvatarSelector,
+  messagesSelector,
+  lastMessageIdSelector
+} from '../../../../Selectors/chat';
 
 @connect(state => ({
   agentName: agentNameSelector(state),
-  agentAvatar: agentAvatarSelector(state)
+  agentAvatar: agentAvatarSelector(state),
+  messages: messagesSelector(state),
+  lastMessageId: lastMessageIdSelector(state)
 }))
 export class AgentDisconnectedContainer extends React.Component {
+
+  static propTypes = {
+    lastMessageId: PropTypes.number,
+    messages: PropTypes.object
+  };
 
   constructor(props) {
     super(props);
     this.state = {
-      started: false
+      shown: false,
+      started: false,
+      lastMessageId: null
     };
+  }
+
+  componentDidUpdate() {
+    this.checkForAgentTimeoutMessage();
   }
 
   onStart = () => {
@@ -24,7 +42,33 @@ export class AgentDisconnectedContainer extends React.Component {
     });
   };
 
+  checkForAgentTimeoutMessage() {
+    const { messages, lastMessageId } = this.props;
+    if (lastMessageId !== this.state.lastMessageId) {
+      const agentTimeoutMessages = messages.filter(message => {
+        return message.get('id') > this.state.lastMessageId
+            && message.get('is_sys')
+            && message.get('content').indexOf('message_agent-timeout') !== -1;
+      });
+
+      if (agentTimeoutMessages.size > 0) {
+        this.setState({
+          shown: true,
+          lastMessageId: lastMessageId
+        });
+      } else {
+        this.setState({
+          lastMessageId: lastMessageId
+        });
+      }
+    }
+  }
+
   render() {
+    if (!this.state.shown) {
+      return null;
+    }
+
     return (
       <AgentDisconnected {...this.props}>
         {this.state.started
