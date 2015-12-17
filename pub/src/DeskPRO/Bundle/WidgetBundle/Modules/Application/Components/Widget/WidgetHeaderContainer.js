659c4222beae5e9e7a34f550b2bd381af4f0d385
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-17 13:41:52 +0300

    widget chat hide header menu button

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/WidgetHeaderContainer.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/WidgetHeaderContainer.js
@@ -23,9 +23,11 @@ export class WidgetHeaderContainer extends React.Component {
   render() {
     return (
       <div className="dpdesignportal-header">
-        <a href="#" className="dpdesignportal-header-controls left" onClick={this.onOpenMenu}>
-          <i className="fa fa-navicon"></i>
-        </a>
+        {false /* disabled for now */ &&
+          <a href="#" className="dpdesignportal-header-controls left" onClick={this.onOpenMenu}>
+            <i className="fa fa-navicon"></i>
+          </a>
+        }
 
         <a href="#" className="dpdesignportal-header-controls dpdesignportal-mobile-nav-control right" onClick={this.onClose}>
           <span className="dpdesignportal-control-hide"><i className="fa fa-times"></i></span>
