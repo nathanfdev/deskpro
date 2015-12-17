00e72bb267e97089a0e401cedb4e26257e38ccd7
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-17 15:15:51 +0300

    widget header, sample logo

---
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/WidgetHeader.js
@@ -0,0 +1,45 @@
+import React, { PropTypes } from 'react';
+import sampleLogo from '../../../../Resources/img/sample-logo.png';
+
+export class WidgetHeader extends React.Component {
+
+  static propTypes = {
+    onOpenMenu: PropTypes.func,
+    onClose: PropTypes.func,
+    companyName: PropTypes.string,
+    companyLogo: PropTypes.string
+  };
+
+  onOpenMenu = event => {
+    event.preventDefault();
+    this.props.onOpenMenu();
+  };
+
+  onClose = event => {
+    event.preventDefault();
+    this.props.onClose();
+  };
+
+  render() {
+    const { companyName, companyLogo } = this.props;
+
+    return (
+      <div className="dpdesignportal-header">
+        {false /* disabled for now */ &&
+        <a href="#" className="dpdesignportal-header-controls left" onClick={this.onOpenMenu}>
+          <i className="fa fa-navicon"></i>
+        </a>
+        }
+
+        <a href="#" className="dpdesignportal-header-controls dpdesignportal-mobile-nav-control right" onClick={this.onClose}>
+          <span className="dpdesignportal-control-hide"><i className="fa fa-times"></i></span>
+        </a>
+
+        <div className="dpdesignportal-header-mark">
+          <img src={companyLogo || sampleLogo} className="dpdesignportal-logo" />
+          <h1>{companyName}</h1>
+        </div>
+      </div>
+    );
+  }
+}
--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/WidgetHeaderContainer.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/WidgetHeaderContainer.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { closeWidget } from '../../Actions/dpWindowActions';
 import { companyNameSelector, companyLogoSelector } from '../../Selectors/dpWindow';
+import { WidgetHeader } from './WidgetHeader';
 
 @connect(state => ({
   companyName: companyNameSelector(state),
@@ -10,41 +11,18 @@ import { companyNameSelector, companyLogoSelector } from '../../Selectors/dpWind
 export class WidgetHeaderContainer extends React.Component {
 
   static propTypes = {
-    dispatch: PropTypes.func,
-    companyName: PropTypes.string,
-    companyLogo: PropTypes.string
+    dispatch: PropTypes.func
   };
 
-  onOpenMenu = event => {
-    event.preventDefault();
+  onOpenMenu = () => {
     console.log('onOpenMenu');
   };
 
-  onClose = event => {
-    event.preventDefault();
+  onClose = () => {
     this.props.dispatch(closeWidget());
   };
 
   render() {
-    const { companyName, companyLogo } = this.props;
-
-    return (
-      <div className="dpdesignportal-header">
-        {false /* disabled for now */ &&
-          <a href="#" className="dpdesignportal-header-controls left" onClick={this.onOpenMenu}>
-            <i className="fa fa-navicon"></i>
-          </a>
-        }
-
-        <a href="#" className="dpdesignportal-header-controls dpdesignportal-mobile-nav-control right" onClick={this.onClose}>
-          <span className="dpdesignportal-control-hide"><i className="fa fa-times"></i></span>
-        </a>
-
-        <div className="dpdesignportal-header-mark">
-          <img src={companyLogo} className="dpdesignportal-logo sample-logo" />
-          <h1>{companyName}</h1>
-        </div>
-      </div>
-    );
+    return <WidgetHeader {...this.props} />;
   }
 }
