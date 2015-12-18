f1980005ec57cb80898aecfa73a2dedac939283e
n3b <neb802@gmail.com>
2015-12-18 13:57:09 +0800

    [FIX] usersearch js error

--- web/javascripts/DeskPRO/Agent/ElementHandler/PersonSearchBox.js
+++ web/javascripts/DeskPRO/Agent/ElementHandler/PersonSearchBox.js
@@ -301,7 +301,7 @@ DeskPRO.Agent.ElementHandler.PersonSearchBox = new Orb.Class({
 					this.resultsList.append(row);
 				}, this);
 
-				if (data.length && this.getEl('userselect').is(':visible')) {
+				if (data.length && this.el.find('input.select-user:first').is(':visible')) {
 					this.open();
 				}
 
