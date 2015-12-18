72f295f77c07f5faa5fe44d9f71eb5aedd2e5440
n3b <neb802@gmail.com>
2015-12-18 14:54:53 +0800

    [FIX] usersearch by ticket ID and Ref

--- app/src/Application/DeskPRO/NewSearch/SearchEngine/Mysql/UserSearch.php
+++ app/src/Application/DeskPRO/NewSearch/SearchEngine/Mysql/UserSearch.php
@@ -237,11 +237,17 @@ class UserSearch implements UserSearchInterface
 
         $search_places = array();
         $search_params = array();
+        $search_types  = array();
 
         foreach ($query_words as $w) {
             if (strlen($w) <= 2) {
                 continue;
             }
+
+            $search_places[] = 'tickets.id = '.(int) $w;
+
+            $search_places[] = 'tickets.ref = ?';
+            $search_params[] = $w;
 
             $search_places[] = 'tickets_messages.message LIKE ?';
             $search_params[] = '%'.str_replace(array('%', '_', '\\'), array('\\%', '\\_', '\\\\'), $w).'%';
@@ -261,20 +267,19 @@ class UserSearch implements UserSearchInterface
             $params = array(
                 $context->getPerson()->getId(),
                 $context->getPerson()->getId(),
+                $context->getPerson()->getId(),
                 $context->getPerson()->organization->getId(),
             );
 
             $params = array_merge($params, $search_params);
 
             $ticket_ids = $this->db->fetchAllCol("
-                SELECT tickets.id
+                SELECT DISTINCT(tickets.id)
                 FROM tickets
                 LEFT JOIN tickets_participants ON (tickets_participants.ticket_id = tickets.id)
                 LEFT JOIN tickets_messages ON (tickets_messages.ticket_id = tickets.id AND tickets_messages.is_agent_note = 0)
                 WHERE
-                    (tickets.person_id = ?
-                    OR tickets_participants.person_id = ?
-                    OR tickets.organization_id = ?)
+                    (tickets.person_id = ? OR tickets_participants.person_id = ? OR tickets.agent_id = ? OR tickets.organization_id = ?)
                     AND (tickets.date_last_agent_reply IS NOT NULL OR tickets.date_last_user_reply IS NOT NULL)
                     AND ($search_places)
                 ORDER BY tickets.date_status DESC, tickets.date_created DESC
@@ -284,18 +289,18 @@ class UserSearch implements UserSearchInterface
             $params = array(
                 $context->getPerson()->getId(),
                 $context->getPerson()->getId(),
+                $context->getPerson()->getId(),
             );
 
             $params = array_merge($params, $search_params);
 
             $ticket_ids = $this->db->fetchAllCol("
-                SELECT tickets.id
+                SELECT DISTINCT(tickets.id)
                 FROM tickets
                 LEFT JOIN tickets_participants ON (tickets_participants.ticket_id = tickets.id)
                 LEFT JOIN tickets_messages ON (tickets_messages.ticket_id = tickets.id AND tickets_messages.is_agent_note = 0)
                 WHERE
-                    (tickets.person_id = ?
-                    OR tickets_participants.person_id = ?)
+                    (tickets.person_id = ? OR tickets_participants.person_id = ? OR tickets.agent_id = ?)
                     AND (tickets.date_last_agent_reply IS NOT NULL OR tickets.date_last_user_reply IS NOT NULL)
                     AND ($search_places)
                 ORDER BY tickets.date_status DESC, tickets.date_created DESC
