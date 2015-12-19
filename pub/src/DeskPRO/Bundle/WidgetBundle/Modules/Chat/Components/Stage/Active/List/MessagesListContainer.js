7af9e7bb5d4ba74e94039ec2251e535ee61bca17
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-19 17:15:33 +0300

    widget chat message list use outer height to calc scrollarea size

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/List/MessagesListContainer.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/List/MessagesListContainer.js
@@ -30,10 +30,10 @@ export class MessagesListContainer extends React.Component {
     const { widgetHeight } = this.props;
     const node = ReactDOM.findDOMNode(this);
 
-    let height = widgetHeight - 240; // header height
+    let height = widgetHeight - 230; // header height
     $(node).parent().children().each((i, child) => {
       if (child !== node) {
-        height = height - $(child).height();
+        height = height - $(child).outerHeight();
       }
     });
 
