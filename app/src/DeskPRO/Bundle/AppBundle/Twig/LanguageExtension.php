6dc69ed6899f25c9ecfb2176f62f20e5edece034
Chris Tickner <chris.tickner@gmail.com>
2015-12-20 22:07:03 -0500

    portal: add "object_phrase" twig function

--- app/src/DeskPRO/Bundle/AppBundle/Twig/LanguageExtension.php
+++ app/src/DeskPRO/Bundle/AppBundle/Twig/LanguageExtension.php
@@ -29,7 +29,6 @@
 /**
  * DeskPRO.
  */
-
 namespace DeskPRO\Bundle\AppBundle\Twig;
 
 use DeskPRO\Bundle\AppBundle\Language\LanguageManager;
@@ -64,6 +63,13 @@ class LanguageExtension extends \Twig_Extension
                     'needs_environment' => true,
                 )
             ),
+            new \Twig_SimpleFunction(
+                'object_phrase',
+                array($this, 'getObjectPhrase'),
+                array(
+                    'is_safe' => array('all'),
+                )
+            ),
         );
     }
 
@@ -92,7 +98,18 @@ class LanguageExtension extends \Twig_Extension
 
         $vars['_context'] = $context;
 
-        return $this->language_manager->getTranslator()->phrase($phrase_name, $vars);
+        return $this->language_manager->phrase($phrase_name, $vars);
+    }
+
+    /**
+     * @param object $object
+     * @param bool   $property
+     *
+     * @return string
+     */
+    public function getObjectPhrase($object, $property = false)
+    {
+        return $this->language_manager->objectPhrase($object, $property);
     }
 
     /**
