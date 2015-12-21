7bb7b682ecec0d614eddb34696a95f389b8d0b55
Chris Tickner <chris.tickner@gmail.com>
2015-12-21 02:16:30 -0500

    portal: if theme repo cache is fresh, use it

--- app/src/DeskPRO/Bundle/PortalBundle/Theme/ThemeRepository.php
+++ app/src/DeskPRO/Bundle/PortalBundle/Theme/ThemeRepository.php
@@ -110,7 +110,7 @@ class ThemeRepository
         }
 
         // if fresh, we are done, return the stored array and retain it for easy access
-        if (file_exists($this->config_cache) && !$this->config_cache->isFresh()) {
+        if (file_exists($this->config_cache) && $this->config_cache->isFresh()) {
             $this->logger->debug('theme repository: unserializing from file cache');
 
             return $this->theme_map = require $this->config_cache;
