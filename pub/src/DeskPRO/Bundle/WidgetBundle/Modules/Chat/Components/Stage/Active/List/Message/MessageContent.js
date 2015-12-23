8e254583cec0f552bfe12768e4481d14e07c6187
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-23 18:12:54 +0300

    widget message content links _target=blank

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/List/Message/MessageContent.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/List/Message/MessageContent.js
@@ -1,12 +1,21 @@
 import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { AttachmentLink } from './Assets/AttachmentLink';
 import { replaceSmileCodes } from 'DeskPRO/Component/Rte/Emotions';
+import $ from 'jquery';
 
 export class MessageContent extends React.Component {
 
   static propTypes = {
     message: PropTypes.object
   };
+
+  componentDidMount() {
+    const contentNode = ReactDOM.findDOMNode(this);
+    $('a', contentNode).each((i, linkNode) => {
+      $(linkNode).attr('target', '_blank');
+    });
+  }
 
   render() {
     const { message } = this.props;
