4a3be7fc3716c457d7ae48ffb152501ddca2adbc
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-24 17:58:04 +0300

    widget trigger resize on re calc dimensions

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/WidgetFrameContainer.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/WidgetFrameContainer.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect, Provider } from 'react-redux';
+import { windowResize } from '../../Actions/dpWindowActions';
 import { widgetOpenedSelector } from '../../Selectors/dpWindow';
 import Frame from 'Ampliflux/common/components/Frame';
 import store from '../../../../Services/store';
@@ -25,6 +26,7 @@ export class WidgetFrameContainer extends React.Component {
 
   triggerResize() {
     this.refs.frame.autoFrameDimensions();
+    this.props.dispatch(windowResize());
   }
 
   render() {
