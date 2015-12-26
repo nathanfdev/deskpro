00f1d8edcef2c7f5bd168938cbbb874587309a3c
ImmortaL <denis@ranneft.ru>
2015-12-26 23:55:32 +0300

    something I forgot

---
+++ app/src/DeskPRO/Bundle/AppBundle/Notification/Strategy/DefferedStrategy.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * a British company located in London, England.
+ *
+ * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ *
+ * The license agreement under which this software is released
+ * can be found at https://www.deskpro.com/eula/
+ *
+ * By using this software, you acknowledge having read the license
+ * and agree to be bound thereby.
+ *
+ * Please note that DeskPRO is not free software. We release the full
+ * source code for our software because we trust our users to pay us for
+ * the huge investment in time and energy that has gone into both creating
+ * this software and supporting our customers. By providing the source code
+ * we preserve our customers' ability to modify, audit and learn from our
+ * work. We have been developing DeskPRO since 2001, please help us make it
+ * another decade.
+ *
+ * Like the work you see? Think you could make it better? We are always
+ * looking for great developers to join us: http://www.deskpro.com/jobs/
+ *
+ * ~ Thanks, Everyone at Team DeskPRO
+ */
+
+namespace DeskPRO\Bundle\AppBundle\Notification\Strategy;
+
+use DeskPRO\Bundle\AppBundle\Notification\Event\SystemEventInterface;
+use DeskPRO\Bundle\AppBundle\Notification\Persistance\PersistanceAdapterInterface;
+
+/**
+ * Class DefferedStrategy
+ * The cloud strategy is simple: just persist SystemEvent, and then CloudService started in cron-job should
+ * calculate messages and targets, then it should deliver.
+ */
+class DefferedStrategy extends AbstractStrategy
+{
+    /**
+     * @param SystemEventInterface $event
+     */
+    public function handleSystemEvent(SystemEventInterface $event)
+    {
+        $this->persistEvent($event);
+    }
+
+    /**
+     * @param PersistanceAdapterInterface $persistance_adapter
+     *
+     * @return DefferedStrategy
+     */
+    public function setPersistanceAdapter(PersistanceAdapterInterface $persistance_adapter)
+    {
+        $this->persistance_adapter = $persistance_adapter;
+    }
+
+    /**
+     * @param $event
+     */
+    protected function persistEvent($event)
+    {
+        $this->persistance_adapter->persist($event);
+    }
+}
---
+++ pub/src/DeskPRO/Bundle/AppBundle/Pusher.js
@@ -0,0 +1,71 @@
+/**
+ * wrapper for pusher-app client
+ */
+import Pusher from 'pusher-js';
+
+export default class PusherClient {
+  constructor(me) {
+    this.me = me;
+    Pusher.log = function(message) {
+      if (window.console && window.console.log) {
+        window.console.log(message);
+      }
+    };
+
+    this.client = new Pusher('eaa00fb39fddc251d116', {
+      encrypted: true, authEndpoint: '/api/v2/pusher/auth', authTransport: 'rest'
+    });
+  }
+
+  bind(channelName, eventName, dispatcher, action) {
+    const that = this;
+
+    Pusher.authorizers.rest = (socketId, callback) => {
+      let xhr;
+
+      if (Pusher.XHR) {
+        xhr = new Pusher.XHR();
+      } else {
+        xhr = (window.XMLHttpRequest ? new window.XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP'));
+      }
+
+      xhr.open('POST', that.client.config.authEndpoint, true);
+
+      // add request headers
+      xhr.setRequestHeader('Content-Type', 'application/json');
+
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            let data = false;
+            let parsed = false;
+
+            try {
+              data = JSON.parse(xhr.responseText);
+              parsed = true;
+            } catch (e) {
+              callback(true, 'JSON returned from webapp was invalid, yet status code was 200. Data was: ' + xhr.responseText);
+            }
+
+            if (parsed) { // prevents double execution.
+              callback(false, data);
+            }
+          } else {
+            Pusher.warn("Couldn't get auth info from your webapp", xhr.status);
+            callback(true, xhr.status);
+          }
+        }
+      };
+
+      xhr.send(JSON.stringify({socket_id: socketId, channel_name: channelName, user_id: that.me.get('id')}));
+      return xhr;
+    };
+
+    const channel = this.client.subscribe(channelName);
+    channel.bind(eventName, (data) => {
+      if (data.target === that.me.get('id')) {
+        dispatcher(action([data]));
+      }
+    });
+  }
+}
