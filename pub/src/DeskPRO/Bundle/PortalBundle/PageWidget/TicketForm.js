91d23f79532623ddd03744182765ca785f628ba6
Sergey Yastrebov <serg.yastrebov@gmail.com>
2015-12-29 16:42:45 +0300

    ticket form cleanup, eslint fixes

--- pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/TicketForm.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/TicketForm.js
@@ -1,22 +1,20 @@
-import _ from "lodash";
-import React from "react";
-import ReactDOM from "react-dom"
-import PortalApp from "DeskPRO/Bundle/PortalBundle/PortalApp";
-import PageWidget from "DeskPRO/Component/PageWidget/PageWidget";
-import NewTicketSuggestions from "DeskPRO/Bundle/PortalBundle/React/NewTicketSuggestions";
-import DynamicForm from "DeskPRO/Bundle/AppBundle/Form/DynamicForm.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PortalApp from 'DeskPRO/Bundle/PortalBundle/PortalApp';
+import PageWidget from 'DeskPRO/Component/PageWidget/PageWidget';
+import NewTicketSuggestions from 'DeskPRO/Bundle/PortalBundle/React/NewTicketSuggestions';
+import DynamicForm from 'DeskPRO/Bundle/AppBundle/Form/DynamicForm.js';
+import _ from 'lodash';
+import $ from 'jquery';
 
-//######################################################################################################################
-//# Ticket value reader
-//######################################################################################################################
-
+// Ticket value reader
 class TicketValueReader {
   constructor($formEl) {
     this.$formEl = $formEl;
   }
 
   _parseIntSelect(f) {
-    return parseInt(f.val() || 0) || 0;
+    return parseInt(f.val() || 0, 10) || 0;
   }
 
   getDepartmentId() {
@@ -36,7 +34,7 @@ class TicketValueReader {
   }
 
   getOrganizationId() {
-    return this._parseIntSelect($('#ticket_user_organization'));;
+    return this._parseIntSelect($('#ticket_user_organization'));
   }
 
   getWorkflowId() {
@@ -44,69 +42,66 @@ class TicketValueReader {
   }
 }
 
-
-//######################################################################################################################
-//# Page widget
-//######################################################################################################################
-
+// Page widget
 export default class TicketForm extends PageWidget {
   renderWidget() {
-    let $formEl       = this.$element.find('.dp_ticket_form');
-    let formName     = $formEl.find('form').attr('name');
-    let $tplEl        = this.$element.find('.js_form_tpl');
-    let ticketReader  = new TicketValueReader($formEl);
-    let allFormFields = $([]).add($formEl.find('select')).add($tplEl.find('select'));
+    const $formEl = this.$element.find('.dp_ticket_form');
+    const formName = $formEl.find('form').attr('name');
+    const $tplEl = this.$element.find('.js_form_tpl');
+    const ticketReader = new TicketValueReader($formEl);
+    const allFormFields = $([]).add($formEl.find('select')).add($tplEl.find('select'));
+
     let updateHitter;
     let updateLastDepId;
     let setDisplayedFields;
 
     updateLastDepId = () => {
-      let dep_id = ticketReader.getDepartmentId();
+      const depId = ticketReader.getDepartmentId();
+      const $ldp = $formEl.find("[data-field='last_department_id']");
 
-      let $ldp = $formEl.find("[data-field='last_department_id']");
       if ($ldp.length) {
-        if($ldp.find('input').length) {
-          $ldp.find('input').val(dep_id);
+        if ($ldp.find('input').length) {
+          $ldp.find('input').val(depId);
         } else {
-          $ldp.val(dep_id); // on first page load this is the case
+          $ldp.val(depId); // on first page load this is the case
         }
       } else {
-        $formEl.find('form').append('<input data-field="last_department_id" type="hidden" name="' + formName + '[last_department_id]" value="' + dep_id + '" />');
+        $formEl.find('form').append('<input data-field="last_department_id" type="hidden" name="' + formName + '[last_department_id]" value="' + depId + '" />');
       }
     };
 
     setDisplayedFields = (e) => {
-      const displayed_fields = e.inst.currentFields.filter((field) => {
-          return field != 'displayed_fields'; // don't include this special field in the list
+      const displayedFields = e.inst.currentFields.filter((field) => {
+        return field !== 'displayed_fields'; // don't include this special field in the list
       }).join(',');
-      let $df = $formEl.find("[data-field='displayed_fields']").find('input[type="hidden"]');
-      console.log('[TicketForm] [setDisplayedFields] setting displayed_fields to: ', displayed_fields);
-      $df.val(displayed_fields)
+      const $df = $formEl.find("[data-field='displayed_fields']").find('input[type="hidden"]');
+      console.log('[TicketForm] [setDisplayedFields] setting displayed_fields to: ', displayedFields);
+      $df.val(displayedFields);
     };
 
     this.dynForm = new DynamicForm({
       formEl: $formEl,
-      tplEl:  $tplEl,
+      tplEl: $tplEl,
       alwaysFields: ['department', 'user_name_and_email', 'user_email', 'subject', 'message', 'submit', 'last_department_id', 'displayed_fields'],
       onInit: () => {
         updateLastDepId();
 
         // only render ticket deflection if a .dpx-with-ticket-deflection is present on the form
         if ($formEl.hasClass('dpx-with-ticket-deflection')) {
-          let $subject = $('#ticket_subject');
-          let $rElement = $('<div class="dp-react-widget"></div>').insertAfter($subject);
+          const $subject = $('#ticket_subject');
+          const $rElement = $('<div class="dp-react-widget"></div>').insertAfter($subject);
           ReactDOM.render(React.createElement(NewTicketSuggestions, {input: $subject}), $rElement.get(0));
         }
       },
-      fieldFilter: (fields, currentFields, dynForm) => {
+      fieldFilter: fields => {
         if (!window.DESKPRO_TICKET_DISPLAY) {
-          console.error("DESKPRO_TICKET_DISPLAY is not defined");
+          console.error('DESKPRO_TICKET_DISPLAY is not defined');
           return fields;
         }
 
-        let layout = window.DESKPRO_TICKET_DISPLAY.getLayout(ticketReader.getDepartmentId());
-        let newFields = _.map(layout.getMatchingFields(ticketReader), (v) => {
-          let id = v.id;
+        const layout = window.DESKPRO_TICKET_DISPLAY.getLayout(ticketReader.getDepartmentId());
+        const newFields = _.map(layout.getMatchingFields(ticketReader), (v) => {
+          const id = v.id;
           switch (id) {
             case 'subject': return 'subject';
             case 'attach': return ['attachments', 'more_attachments'];
