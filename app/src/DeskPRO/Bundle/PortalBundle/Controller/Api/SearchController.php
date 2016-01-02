3e73e40f58cb4ea20bb74b1faedf41bbe91b2287
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-02 13:01:18 +0300

    portal api similar response

--- app/src/DeskPRO/Bundle/PortalBundle/Controller/Api/SearchController.php
+++ app/src/DeskPRO/Bundle/PortalBundle/Controller/Api/SearchController.php
@@ -34,6 +34,7 @@ namespace DeskPRO\Bundle\PortalBundle\Controller\Api;
 use FOS\RestBundle\View\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class SearchController.
@@ -44,10 +45,16 @@ class SearchController extends AbstractApiController
      * @Route("/portal/api/search/similar/{content_type}", name="portal_api_search_similar", defaults={"content_type":null})
      * @Method({"GET"})
      *
+     * @param Request $request
+     * @param string  $content_type
+     *
      * @return View
      */
-    public function similarTicketsAction()
+    public function similarTicketsAction(Request $request, $content_type = null)
     {
-        return $this->redirectToRoute('portal_search_similar');
+        $path  = ['content_type' => $content_type];
+        $query = ['content' => $request->get('content', '')];
+
+        return $this->forward('PortalBundle:Search:similarTo', $path, $query);
     }
 }
