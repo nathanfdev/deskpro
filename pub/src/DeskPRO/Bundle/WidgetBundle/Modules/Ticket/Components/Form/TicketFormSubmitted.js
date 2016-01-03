c87e71d280631d0fcecad25d8d11acd99394a673
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-03 16:16:49 +0300

    widget ticket thank you stage styles

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Ticket/Components/Form/TicketFormSubmitted.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Ticket/Components/Form/TicketFormSubmitted.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import PortalPhrases from 'DeskPRO/Bundle/PortalBundle/PortalPhrases';
 
 export class TicketFormSubmitted extends React.Component {
 
   render() {
     return (
-      <div>
-        Thank You!
+      <div className="ticket-success-blurb">
+        <div className="icon-mark"></div>
+        <h1>{PortalPhrases.get('portal.tickets.thank_you')}</h1>
+        <p>{PortalPhrases.get('portal.tickets.thank_you_desc')}</p>
       </div>
     );
   }
