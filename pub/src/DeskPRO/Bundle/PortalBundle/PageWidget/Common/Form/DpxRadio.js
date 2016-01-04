f7be2869d078929192a9ac3143178070b3a6755a
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-04 17:16:17 +0300

    radio on click

--- pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/Common/Form/DpxRadio.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/Common/Form/DpxRadio.js
@@ -9,9 +9,12 @@ export default class DpxRadio extends PageWidget {
 
     // This prevents the focus border when just clicking on an item,
     // but leaves it there if you focus it via kbd
-    $el.on('mousedown', () => $el.addClass('no-focus-border'));
-    $el.on('blur', () => $el.removeClass('no-focus-border'));
+    $el.on('mousedown', () => {
+      $el.addClass('no-focus-border');
+      $radio.prop('checked', true);
+    });
 
+    $el.on('blur', () => $el.removeClass('no-focus-border'));
     $el.on('keydown', (ev) => {
       if (ev.which === 32) {
         ev.preventDefault();
