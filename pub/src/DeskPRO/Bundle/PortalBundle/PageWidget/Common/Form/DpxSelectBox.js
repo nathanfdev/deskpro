e99d67c22b7ee9895ab1ce14ab73bfa8020a84d6
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-04 15:02:27 +0300

    widget ticket form context

--- pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/Common/Form/DpxSelectBox.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/Common/Form/DpxSelectBox.js
@@ -2,10 +2,11 @@ import $ from 'jquery';
 import PageWidget from 'DeskPRO/Component/PageWidget/PageWidget';
 import * as PortalSelectBox from 'DeskPRO/Bundle/PortalBundle/React/Form/PortalSelectBox';
 
-export default class DpxLevelSelect extends PageWidget {
+export default class DpxMultipleSelectBox extends PageWidget {
+
   renderWidget() {
     this.$element.hide();
     this.$rElement = $('<div class="dp-react-widget as-dpui"></div>').insertAfter(this.$element);
-    this.actionStore = PortalSelectBox.createComponent(this.$element, this.$rElement);
+    this.actionStore = PortalSelectBox.createComponent(this.$element, this.$rElement, null, this.options);
   }
 }
