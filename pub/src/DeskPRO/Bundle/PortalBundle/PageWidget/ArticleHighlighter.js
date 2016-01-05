6ed0bbb81480e299eb4806d5332c379ec6ee51e7
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-05 12:05:39 +0300

    article highlighter cleanup

--- pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/ArticleHighlighter.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/ArticleHighlighter.js
@@ -1,36 +1,22 @@
-import _ from "lodash";
-import $ from "jquery";
-import PageWidget from "DeskPRO/Component/PageWidget/PageWidget";
-import WordHighlighter from "DeskPRO/Bundle/PortalBundle/PageWidget/Common/WordHighlighter";
-
-//######################################################################################################################
-//# Page widget
-//######################################################################################################################
+import $ from 'jquery';
+import PageWidget from 'DeskPRO/Component/PageWidget/PageWidget';
+import WordHighlighter from 'DeskPRO/Bundle/PortalBundle/PageWidget/Common/WordHighlighter';
 
 export default class ArticleHighlighter extends PageWidget {
   renderWidget() {
     const $article = this.$element;
     const highlighter = new WordHighlighter;
     const glossary = window.DP_ARTICLE_GLOSSARY;
-    const hightlight_words = highlighter.highlight(
-        $article[0],
-        glossary.words,
-        false,
-        true
-    );
+    const hightlightWords = highlighter.highlight($article[0], glossary.words, false, true);
 
-    $(hightlight_words).each(function() {
-      const $el = $(this);
+    $(hightlightWords).each((i, node) => {
+      const $el = $(node);
       const word = $el.data('word');
       const def = glossary.defs[word];
       const tooltip = $(`<div class="glossary-tooltip" style="display: none;"><h1>${word}</h1><hr><p>${def}</p></div>`);
-      $el.append(tooltip);
 
-      $el.hover(function() {
-        tooltip.show();
-      }, function() {
-        tooltip.hide();
-      })
+      $el.append(tooltip);
+      $el.hover(() => tooltip.show(), () => tooltip.hide());
     });
   }
 }
