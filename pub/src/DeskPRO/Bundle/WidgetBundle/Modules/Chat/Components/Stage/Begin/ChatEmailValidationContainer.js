d467089dd3b1887a0206bbf0a6cec1530b395afa
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-11 17:41:26 +0300

    widget email validation form spinner

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Begin/ChatEmailValidationContainer.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Begin/ChatEmailValidationContainer.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { validateEmail } from '../../../Actions/chatActions';
 import { chatIdSelector } from '../../../Selectors/chat';
+import { FieldErrors } from 'DeskPRO/Component/Form/FormErrors';
 import $ from 'jquery';
 
 @connect(state => ({
@@ -19,26 +20,54 @@ export class ChatEmailValidationContainer extends React.Component {
     super(props);
     this.state = {
       code: '',
+      submit: false,
       errors: null
     };
   }
 
   componentDidMount() {
+    this.mounted = true;
+
     const inputNode = ReactDOM.findDOMNode(this.refs.input);
     $(inputNode).focus();
   }
 
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
   onChangeCode = event => {
     this.setState({
-      code: event.target.value
+      code: event.target.value,
+      errors: null
     });
   };
 
   onSubmit = event => {
     event.preventDefault();
-    const { chatId, dispatch } = this.props;
+    this.setState({
+      submit: true
+    });
 
-    dispatch(validateEmail(chatId, {code: this.state.code}));
+    const { chatId, dispatch } = this.props;
+    const promise = dispatch(validateEmail(chatId, {code: this.state.code}));
+    promise.then(
+      () => {
+        if (this.mounted) {
+          this.setState({
+            submit: false
+          });
+        }
+      },
+      result => {
+        if (this.mounted) {
+          this.setState({
+            submit: false,
+            errors: result.getData()
+          });
+        }
+      }
+    );
   };
 
   render() {
@@ -51,11 +80,16 @@ export class ChatEmailValidationContainer extends React.Component {
           <p>Check your email then enter the code below</p>
         </span>
 
-        <form>
+        <form onSubmit={this.onSubmit}>
           <input type="text" ref="input" onChange={this.onChangeCode} value={this.state.code} />
-          <a href="#" className="email-code-submit" onClick={this.onSubmit}>
-            Start Chat <i className="fa fa-chevron-right"></i>
-          </a>
+          <FieldErrors errors={this.state.errors} field="code" />
+
+          {this.state.submit
+            ? <div className="spinner"><i/></div>
+            : <a href="#" className="email-code-submit" onClick={this.onSubmit}>
+                Start Chat <i className="fa fa-chevron-right"></i>
+              </a>
+          }
         </form>
       </div>
     );
