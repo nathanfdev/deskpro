2d2f3db0d84c46cf7ba283d5d4976f94ace20270
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-12 21:21:29 +0300

    chat convo transformer fix

--- app/src/DeskPRO/Bundle/AppBundle/DataSerializer/DataTransformer/ChatConversationTransformer.php
+++ app/src/DeskPRO/Bundle/AppBundle/DataSerializer/DataTransformer/ChatConversationTransformer.php
@@ -79,9 +79,22 @@ class ChatConversationTransformer extends AbstractDataSerializerTransformer
     public function getCustomProperties(DataTransformerRequest $transformation_request)
     {
         /** @var ChatConversation $data */
-        $data = $transformation_request->getDataToBeTransformed();
+        $data       = $transformation_request->getDataToBeTransformed();
+        $person     = $data->getPerson();
+        $agent      = $data->getAgent();
+        $department = $data->getDepartment();
 
         return [
+            'conversation_id'     => $data->getId(),
+            'author_id'           => $person ? $person->getId() : 0,
+            'author_name'         => $person ? $person->getDisplayName() : $data->getPersonName(),
+            'author_email'        => $person ? $person->getPrimaryEmailAddress() : $data->getPersonEmail(),
+            'author_avatar'       => $person ? $this->avatar_resolver->getAvatarModel($person) : null,
+            'author_type'         => $person && $person->isAgent() ? 'agent' : 'user',
+            'agent_id'            => $agent ? $agent->getId() : 0,
+            'agent_name'          => $agent ? $agent->getDisplayName() : '',
+            'agent_avatar'        => $agent ? $this->avatar_resolver->getAvatarModel($agent) : null,
+            'department_name'     => $department ? $department->getFullTitle() : '',
             'need_validate_email' => $data->getEmailValidationCode() && !$data->getEmailValidated(),
         ];
     }
