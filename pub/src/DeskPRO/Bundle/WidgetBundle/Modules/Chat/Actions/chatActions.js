d3007651626676988047a026be6647d2a3a5e171
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-13 19:59:15 +0300

    chat messages uses people store

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Actions/chatActions.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Actions/chatActions.js
@@ -14,7 +14,7 @@ import {
   chatInfoSelector,
   chatLoadedSelector,
   isEndedSelector,
-  authorAvatarSelector,
+  authorIdSelector,
   messageIdsSelector,
   attachmentsSelector,
   canReopenSelector,
@@ -361,7 +361,7 @@ export const sendChatMessage = createAction(
     }
 
     const state = getState();
-    const authorAvatar = authorAvatarSelector(state);
+    const authorId = authorIdSelector(state);
     const tmpId = generate({
       length: 20,
       charset: 'alphabetic'
@@ -373,8 +373,7 @@ export const sendChatMessage = createAction(
         tmp_id: tmpId,
         content: params.message,
         is_html: true,
-        author_avatar: authorAvatar,
-        author_type: 'user',
+        author: authorId,
         date_created: moment().format()
       }));
     }
@@ -387,8 +386,7 @@ export const sendChatMessage = createAction(
         tmp_id: tmpId,
         content: null,
         is_html: true,
-        author_avatar: authorAvatar,
-        author_type: 'user',
+        author: authorId,
         date_created: moment().format(),
         metadata: {
           type: 'file',
