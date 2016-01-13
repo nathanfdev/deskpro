2c22239585b79c1f4a21493fcf12a4e1d27df3a9
ImmortaL <denis@ranneft.ru>
2016-01-13 21:06:43 +0300

    added different handlers spec

---
+++ app/tests/spec/DeskPRO/Bundle/AppBundle/Notification/Delivery/Handler/DbDeliveryHandlerSpec.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * a British company located in London, England.
+ *
+ * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ *
+ * The license agreement under which this software is released
+ * can be found at https://www.deskpro.com/eula/
+ *
+ * By using this software, you acknowledge having read the license
+ * and agree to be bound thereby.
+ *
+ * Please note that DeskPRO is not free software. We release the full
+ * source code for our software because we trust our users to pay us for
+ * the huge investment in time and energy that has gone into both creating
+ * this software and supporting our customers. By providing the source code
+ * we preserve our customers' ability to modify, audit and learn from our
+ * work. We have been developing DeskPRO since 2001, please help us make it
+ * another decade.
+ *
+ * Like the work you see? Think you could make it better? We are always
+ * looking for great developers to join us: http://www.deskpro.com/jobs/
+ *
+ * ~ Thanks, Everyone at Team DeskPRO
+ */
+
+namespace spec\DeskPRO\Bundle\AppBundle\Notification\Delivery\Handler;
+
+use Application\DeskPRO\ORM\EntityManager;
+use DeskPRO\Bundle\AppBundle\Notification\Delivery\Handler\DbDeliveryHandler;
+use DeskPRO\Bundle\AppBundle\Notification\Message\ActionAlert;
+use PhpSpec\ObjectBehavior;
+
+/**
+ * @mixin DbDeliveryHandler.
+ */
+class DbDeliveryHandlerSpec extends ObjectBehavior
+{
+    const TYPE = 'notification.delivery.handler.db';
+
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    public function let(EntityManager $em)
+    {
+        $this->beConstructedWith($em);
+    }
+
+    public function it_can_deliver_message_through_db(ActionAlert $actionAlert, EntityManager $em)
+    {
+        $actionAlert->getId()->willReturn('asdf-asdf-asdf-adsf-');
+        $actionAlert->getTarget()->willReturn(1);
+        $actionAlert->getType()->willReturn('test.action.alert.type');
+        $actionAlert->getData()->willReturn(['data' => []]);
+        $actionAlert->getDate()->willReturn(date('Y-m-d H:i:s'));
+
+        $actionAlert->getId()->shouldBeCalled();
+        $actionAlert->getTarget()->shouldBeCalled();
+        $actionAlert->getType()->shouldBeCalled();
+        $actionAlert->getData()->shouldBeCalled();
+        $actionAlert->getDate()->shouldBeCalled();
+
+        $this->deliver($actionAlert);
+    }
+}
