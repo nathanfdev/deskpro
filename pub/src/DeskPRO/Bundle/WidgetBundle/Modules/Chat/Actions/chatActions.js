d10d1032928ab787035d3932a43a3a77c065485f
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-13 15:03:03 +0300

    widget chat load chat info optimization

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Actions/chatActions.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Actions/chatActions.js
@@ -193,11 +193,7 @@ export const loadChatInfo = createAction(
     const state = getState();
     const queryParams = compileParams(addSessionCode(state));
 
-    return new Promise(resolve => {
-      DpApi
-        .sendGet(`DP_API/chats/${chatId}/polling?${queryParams}`, {...ajaxOptions})
-        .success(response => resolve(response.chat_info && response.chat_info.data));
-    });
+    return DpApi.sendGet(`DP_API/chats/${chatId}/polling?${queryParams}`, {...ajaxOptions});
   }
 );
 
