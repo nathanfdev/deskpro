2dc97a7e50d67aefd721fe5c4e47e57bc7620fb0
Sergey <laa513@gmail.com>
2016-01-18 18:04:58 +0300

    Turned "I am authenticated as" to set token instead of filling the login form, "I login with credentials" still uses the form to test login form works

--- app/tests/features/bootstrap/Portal/AuthContext.php
+++ app/tests/features/bootstrap/Portal/AuthContext.php
@@ -33,10 +33,9 @@ namespace DpBehat\Portal;
 
 use Application\DeskPRO\Entity\Organization;
 use Application\DeskPRO\Entity\Person;
-use Behat\Behat\Hook\Scope\BeforeScenarioScope;
-use DpBehat\LanguageContext;
 use DpTestSrc\TestBundle\UserDetailsRepo;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class AuthContext extends BasePortalContext
 {
@@ -54,11 +53,6 @@ class AuthContext extends BasePortalContext
     private $me;
 
     /**
-     * @var LanguageContext
-     */
-    private $lang_context;
-
-    /**
      * @param UserDetailsRepo $user_details
      * @param TokenStorage    $token_storage
      */
@@ -67,17 +61,6 @@ class AuthContext extends BasePortalContext
         $this->user_details  = $user_details;
         $this->token_storage = $token_storage;
         $this->me            = null;
-    }
-
-    /**
-     * @BeforeScenario
-     *
-     * @param BeforeScenarioScope $scope
-     */
-    public function gatherContexts(BeforeScenarioScope $scope)
-    {
-        $environment        = $scope->getEnvironment();
-        $this->lang_context = $environment->getContext('DpBehat\LanguageContext');
     }
 
     /**
@@ -98,12 +81,13 @@ class AuthContext extends BasePortalContext
      */
     public function iLoginWithCredentials($who)
     {
-        $this->lang_context->setTheActiveLanguage('default');
+        $this->getSession()->visit('/login');
 
-        $this->getPage('Login')->login(
-            $this->user_details->getEmail($who),
-            $this->user_details->getPass($who)
-        );
+        /** @var \Behat\Mink\Element\DocumentElement $page */
+        $page = $this->getSession()->getPage();
+        $page->fillField('username', $this->user_details->getEmail($who));
+        $page->fillField('password', $this->user_details->getPass($who));
+        $page->pressButton('Login');
 
         $this->me = $this->user_details->getWho($who);
     }
@@ -188,7 +172,10 @@ class AuthContext extends BasePortalContext
      */
     public function iAmAuthenticatedAsUser($who)
     {
-        $this->iLoginWithCredentials($who);
+        $user = $this->user_details->getWho($who);
+        $this->token_storage->setToken(
+            new UsernamePasswordToken($who, null, 'main', $user->getRoles())
+        );
     }
 
     /**
