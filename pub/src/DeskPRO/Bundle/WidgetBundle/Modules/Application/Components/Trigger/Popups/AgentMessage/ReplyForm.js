f6d842428b4fafbaa04c5d74af14df4305721209
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-19 18:42:33 +0300

    widget popup reply form colors customization

--- pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Trigger/Popups/AgentMessage/ReplyForm.js
+++ pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Trigger/Popups/AgentMessage/ReplyForm.js
@@ -3,14 +3,23 @@ import React, { PropTypes } from 'react';
 export class ReplyForm extends React.Component {
 
   static propTypes = {
+    backgroundColor: PropTypes.string,
+    textColor: PropTypes.string,
     onClick: PropTypes.func
   };
 
   render() {
+    const { backgroundColor, textColor } = this.props;
+
     return (
       <form onClick={this.props.onClick}>
         <input type="text" placeholder="Reply" />
-        <button><i className="fa fa-angle-double-right"></i></button>
+        <button style={{
+          backgroundColor: backgroundColor,
+          color: textColor
+        }}>
+          <i className="fa fa-angle-double-right" />
+        </button>
       </form>
     );
   }
