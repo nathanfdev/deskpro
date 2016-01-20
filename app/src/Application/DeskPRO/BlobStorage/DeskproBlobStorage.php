e8c1d77b1f06dda50bef4a093b0a3e9466ab4c64
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-01-20 16:13:07 +0000

    Correct incorrect ->find() call

--- app/src/Application/DeskPRO/BlobStorage/DeskproBlobStorage.php
+++ app/src/Application/DeskPRO/BlobStorage/DeskproBlobStorage.php
@@ -411,8 +411,8 @@ class DeskproBlobStorage implements Loggable
      */
     public function createBlobRecordFromFile($source_path, $filename, $content_type, array $props = null)
     {
-        $blob = $this->createBlobRowFromFile($source_path, $filename, $content_type, $props);
-        $blob = $this->em->find('DeskPRO:Blob', $blob['id']);
+        $blob_info = $this->createBlobRowFromFile($source_path, $filename, $content_type, $props);
+        $blob      = $this->em->find('DeskPRO:Blob', $blob_info['id']);
 
         return $blob;
     }
@@ -557,8 +557,8 @@ class DeskproBlobStorage implements Loggable
      */
     public function createBlobRecordFromString($source_data, $filename, $content_type, array $props = null)
     {
-        $blob_id = $this->createBlobRowFromString($source_data, $filename, $content_type, $props);
-        $blob    = $this->em->find('DeskPRO:Blob', $blob_id);
+        $blob_info = $this->createBlobRowFromString($source_data, $filename, $content_type, $props);
+        $blob      = $this->em->find('DeskPRO:Blob', $blob_info['id']);
 
         return $blob;
     }
