fe55cbe74b89b8f8581be005327a30c272cfa866
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-20 16:52:26 +0300

    omni search eslint warn fixes

--- pub/src/DeskPRO/Bundle/PortalBundle/React/OmniSearch/OmniSearch.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/React/OmniSearch/OmniSearch.js
@@ -1,24 +1,27 @@
-import React from "react";
-import _ from "lodash";
-import $ from "jquery";
-import moment from "moment";
-import PortalHttp from "DeskPRO/Bundle/PortalBundle/Http/PortalHttp";
-import PortalUrlGenerator from "DeskPRO/Bundle/PortalBundle/Http/PortalUrlGenerator";
-import OmniSearchResultSection from "DeskPRO/Bundle/PortalBundle/React/OmniSearch/OmniSearchResultSection";
-import Pagination from "DeskPRO/Bundle/PortalBundle/React/Pagination";
+import React from 'react';
+import PortalHttp from 'DeskPRO/Bundle/PortalBundle/Http/PortalHttp';
+import PortalUrlGenerator from 'DeskPRO/Bundle/PortalBundle/Http/PortalUrlGenerator';
+import OmniSearchResultSection from 'DeskPRO/Bundle/PortalBundle/React/OmniSearch/OmniSearchResultSection';
+import _ from 'lodash';
+import $ from 'jquery';
+import moment from 'moment';
 
 class SearchType extends React.Component {
+
   toggle() {
     this.props.toggleType(this.props.type);
   }
   render() {
     return (
-      <li><a className="omnisearch-type" onClick={this.toggle.bind(this)}>{this.props.active ? <i className="fa fa-check"></i> : null} {this.props.name}</a></li>
+      <li><a className="omnisearch-type" onClick={this.toggle.bind(this)}>
+        {this.props.active ? <i className="fa fa-check"></i> : null} {this.props.name}</a>
+      </li>
     );
   }
 }
 
 export default class OmniSearch extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -38,16 +41,18 @@ export default class OmniSearch extends React.Component {
       }
     };
   }
+
   componentDidMount() {
     // typing listener
-    let last_val = null;
-    let throttleChanges = _.throttle((e) => {
+    let lastVal = null;
+    const throttleChanges = _.throttle((e) => {
       // ensure we don't trigger a search if the actual search val hasn't changed
-      if (last_val !== e.target.value) {
-        last_val = e.target.value;
+      if (lastVal !== e.target.value) {
+        lastVal = e.target.value;
         this.doSearch({q: e.target.value});
       }
     }, 250);
+
     this.state.$input.on('keyup change', throttleChanges);
 
     // close search button
@@ -61,7 +66,7 @@ export default class OmniSearch extends React.Component {
     this.interval = setInterval(() => {
       // update the "userTyping" state when necessary - check every 100ms
       const newUserTyping = moment().diff(this.state.lastSearch, 'milliseconds') < 1000;
-      if (this.state.userTyping != newUserTyping) {
+      if (this.state.userTyping !== newUserTyping) {
         this.setState({
           userTyping: newUserTyping
         });
@@ -69,19 +74,22 @@ export default class OmniSearch extends React.Component {
     }, 100); // every 100ms check if the user has not typed in a while or not
 
     // clickaway handler
-    $(document).on('keypress', function(e) {
-        if(e.which == 13) { // Checks for the enter key
-            // without this, hitting "enter" to view the search results page would count as a "click"
-            // outside of the ominsearch and clear the search
-            e.preventDefault();
-        }
+    $(document).on('keypress', function(event) {
+      if (event.which === 13) { // Checks for the enter key
+        // without this, hitting "enter" to view the search results page would count as a "click"
+        // outside of the ominsearch and clear the search
+        event.preventDefault();
+      }
     });
-    document.addEventListener("click", this.documentClickHandler.bind(this));
+
+    document.addEventListener('click', this.documentClickHandler.bind(this));
   }
+
   componentWillUnmount() {
     window.clearInterval(this.interval);
-    document.removeEventListener("click", this.documentClickHandler.bind(this));
+    document.removeEventListener('click', this.documentClickHandler.bind(this));
   }
+
   documentClickHandler(e) {
     const $target = $(e.target);
     // if we are clicking inside the search results its ok. but otherwise, we want to clear/close the search.
@@ -94,9 +102,10 @@ export default class OmniSearch extends React.Component {
       this.doSearch({ q: '' }); // reset/close search
     }
   }
-  doSearch(query_modifications) {
-    const last_query = this.state.search_query || {};
-    const search_query = {...last_query, ...query_modifications};
+
+  doSearch(queryModifications) {
+    const lastQuery = this.state.search_query || {};
+    const search_query = {...lastQuery, ...queryModifications};
     this.setState({
       lastSearch: moment(),
       search_query
@@ -122,18 +131,20 @@ export default class OmniSearch extends React.Component {
       }
     });
   }
+
   doResultsExist() {
     let grandTotal = 0;
-    _.forOwn(this.state.data, (type_results, type) => {
-      if ("results" in type_results) {
-        grandTotal += _.keys(type_results.results).length;
+    _.forOwn(this.state.data, (typeResults) => {
+      if ('results' in typeResults) {
+        grandTotal += _.keys(typeResults.results).length;
       }
     });
 
     return grandTotal > 0;
   }
+
   render() {
-    let data = this.state.data;
+    const data = this.state.data;
 
     if (this.state.search_query.q.length < 3) {
       return null;
@@ -144,7 +155,7 @@ export default class OmniSearch extends React.Component {
             ref="searchDropdown"
             className="expanded-search-results"
             style={{
-              display: this.state.search_query.q.length > 0 ? "block" : "none",
+              display: this.state.search_query.q.length > 0 ? 'block' : 'none',
               width: this.state.$input.closest('.search-form').width()
             }}>
           {
@@ -157,7 +168,7 @@ export default class OmniSearch extends React.Component {
                   name="Tickets"
                   nameApi="ticket"
                   nameIcon="fa fa-support"
-                  initialResult={"ticket" in data ? data.ticket : []}
+                  initialResult={'ticket' in data ? data.ticket : []}
                   q={this.state.search_query.q}
                   />
 
@@ -165,7 +176,7 @@ export default class OmniSearch extends React.Component {
                   name="Knowledge base"
                   nameApi="article"
                   nameIcon="fa fa-file-text-o"
-                  initialResult={"article" in data ? data.article : []}
+                  initialResult={'article' in data ? data.article : []}
                   q={this.state.search_query.q}
                   />
 
@@ -173,7 +184,7 @@ export default class OmniSearch extends React.Component {
                   name="Downloads"
                   nameApi="download"
                   nameIcon="fa fa-download"
-                  initialResult={"download" in data ? data.download : []}
+                  initialResult={'download' in data ? data.download : []}
                   q={this.state.search_query.q}
                   />
 
@@ -181,7 +192,7 @@ export default class OmniSearch extends React.Component {
                   name="News"
                   nameApi="news"
                   nameIcon="fa fa-file-text-o"
-                  initialResult={"news" in data ? data.news : []}
+                  initialResult={'news' in data ? data.news : []}
                   q={this.state.search_query.q}
                   />
 
@@ -189,7 +200,7 @@ export default class OmniSearch extends React.Component {
                   name="Feedback"
                   nameApi="feedback"
                   nameIcon="fa fa-comments"
-                  initialResult={"feedback" in data ? data.feedback : []}
+                  initialResult={'feedback' in data ? data.feedback : []}
                   q={this.state.search_query.q}
                   />
               </div>) :
@@ -213,7 +224,7 @@ export default class OmniSearch extends React.Component {
             </a>
           </div>
         </div>
-      );
-    }
+    );
+  }
 }
 
--- pub/src/DeskPRO/Bundle/PortalBundle/React/OmniSearch/OmniSearchResultSection.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/React/OmniSearch/OmniSearchResultSection.js
@@ -1,13 +1,15 @@
-import React from "react";
-import _ from "lodash";
-import moment from "moment";
-import PortalHttp from "DeskPRO/Bundle/PortalBundle/Http/PortalHttp";
+import React, { PropTypes } from 'react';
+import PortalHttp from 'DeskPRO/Bundle/PortalBundle/Http/PortalHttp';
+import _ from 'lodash';
+import moment from 'moment';
 
 class SearchResultCollection {
+
   constructor() {
     this.items = {};
     this.ordered_items = [];
   }
+
   addItem(item) {
     // only add unique
     const same = _.filter(this.items, (a) => a.id === item.id);
@@ -18,32 +20,50 @@ class SearchResultCollection {
     this.items[item.id] = item;
     this.ordered_items.push(item);
   }
+
   getNum(num) {
     return this.ordered_items.slice(0, num);
   }
+
   isEmpty() {
     return this.getTotal() === 0;
   }
+
   getTotal() {
     return this.ordered_items.length;
   }
 }
 
 class ListLink extends React.Component {
+
+  static propTypes = {
+    url: PropTypes.string,
+    text: PropTypes.string
+  };
+
   render() {
-    return (
-      <li><a href={this.props.url}>{this.props.text}</a></li>
-    );
+    const { url, text } = this.props;
+
+    return <li><a href={url}>{text}</a></li>;
   }
 }
 
 export default class OmniSearchResultSection extends React.Component {
+
+  static propTypes = {
+    name: PropTypes.string,
+    nameApi: PropTypes.string,
+    nameIcon: PropTypes.string,
+    initialResult: PropTypes.object
+  };
+
   constructor(props) {
     super(props);
+
     this.state = {
-      name: this.props.name,
-      nameApi: this.props.nameApi,
-      nameIcon: this.props.nameIcon,
+      name: props.name,
+      nameApi: props.nameApi,
+      nameIcon: props.nameIcon,
       display_amount: 10,
       currently_displaying: 10,
       page: 1,
@@ -51,15 +71,10 @@ export default class OmniSearchResultSection extends React.Component {
       q: props.q,
       doSpin: false
     };
+
     this.state.items = this.createsItemsFromProps(props);
   }
-  createsItemsFromProps(props) {
-    const theItems = new SearchResultCollection();
-    _.forEach(props.initialResult.results, (item) => {
-      theItems.addItem(item.object);
-    });
-    return theItems;
-  }
+
   componentWillReceiveProps(newProps) {
     this.setState({
       name: this.props.name,
@@ -74,49 +89,52 @@ export default class OmniSearchResultSection extends React.Component {
       items: this.createsItemsFromProps(newProps)
     });
   }
-  addItems(items) {
-    const theItems = this.state.items;
-    _.forEach(items, (val) => {
-      theItems.addItem(val.object);
-    });
-    this.setState({
-      items: theItems
-    });
-  }
-  grabFromApi() {
 
+  getShowMoreNum() {
+    const diff = _.parseInt(this.state.total_results) - _.parseInt(this.state.currently_displaying);
+    if (diff > 10) {
+      return 10;
+    } else if (diff > 0) {
+      return diff;
+    }
+
+    return null;
+  }
+
+  grabFromApi() {
     console.info('calling API...');
 
     this.setState({
       doSpin: true
     });
 
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       const newpage = this.state.page + 1;
       PortalHttp.sendGet('DP_URL/search/omni', {
         data: {
           q: this.state.q,
           page: newpage,
-          "types[]": this.state.nameApi
+          'types[]': this.state.nameApi
         }
       }).then((r) => {
         if (!r.isError()) {
-          const result_data = r.data.data[this.state.nameApi];
+          const resultData = r.data.data[this.state.nameApi];
           this.setState({
             doSpin: false,
             page: newpage,
-            total_results: _.parseInt(result_data.pageinfo.total_results)
+            total_results: _.parseInt(resultData.pageinfo.total_results)
           });
-          resolve(result_data);
+          resolve(resultData);
         }
       });
     });
   }
+
   showMore() {
-    const pending_total_display = this.state.currently_displaying + this.state.display_amount;
-    if (pending_total_display > this.state.items.getTotal()) {
+    const pendingTotalDisplay = this.state.currently_displaying + this.state.display_amount;
+    if (pendingTotalDisplay > this.state.items.getTotal()) {
       this.grabFromApi().then((items) => {
-        if ("results" in items) {
+        if ('results' in items) {
           this.addItems(items.results);
         }
       });
@@ -126,16 +144,25 @@ export default class OmniSearchResultSection extends React.Component {
       currently_displaying: this.state.currently_displaying + this.state.display_amount
     });
   }
-  getShowMoreNum() {
-    const diff = _.parseInt(this.state.total_results) - _.parseInt(this.state.currently_displaying);
-    if (diff > 10) {
-      return 10;
-    } else if (diff > 0) {
-      return diff;
-    } else {
-      return null;
-    }
+
+  addItems(items) {
+    const theItems = this.state.items;
+    _.forEach(items, (val) => {
+      theItems.addItem(val.object);
+    });
+    this.setState({
+      items: theItems
+    });
   }
+
+  createsItemsFromProps(props) {
+    const theItems = new SearchResultCollection();
+    _.forEach(props.initialResult.results, (item) => {
+      theItems.addItem(item.object);
+    });
+    return theItems;
+  }
+
   render() {
     if (this.state.items.isEmpty()) {
       return null;
@@ -149,7 +176,7 @@ export default class OmniSearchResultSection extends React.Component {
             if (this.state.nameApi === 'news') {
               t = (<span><span className="date-mark"><i className="fa fa-calendar-o"></i> {moment(item.date).fromNow()}</span><span className="item-name">{item.name}</span></span>);
             } else if (this.state.nameApi === 'feedback') {
-              let sign = item.rating < 0 ? '-' : '+';
+              const sign = item.rating < 0 ? '-' : '+';
               t = (<span><span className="feedback-mark"><i className="fa fa-thumbs-up"></i>{sign + item.rating}</span><span className="item-name">{item.name}</span></span>);
             } else if (this.state.nameApi === 'download') {
               t = (<span><span dangerouslySetInnerHTML={{__html: item.icon_html}}></span><span className="item-name">{item.name}</span></span>);
