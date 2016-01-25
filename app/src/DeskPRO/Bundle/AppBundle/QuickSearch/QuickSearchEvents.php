0c54e92196f680a443b1201917474d10d38375e2
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-25 17:52:23 +0300

    small changes

--- app/src/DeskPRO/Bundle/AppBundle/QuickSearch/QuickSearchEvents.php
+++ app/src/DeskPRO/Bundle/AppBundle/QuickSearch/QuickSearchEvents.php
@@ -37,17 +37,17 @@ namespace DeskPRO\Bundle\AppBundle\QuickSearch;
 final class QuickSearchEvents
 {
     /**
-     * Collects entities or entity ids from request query string or external sources (db, elastic search).
+     * Collects entities and entity ids from request query string or external sources (db, elastica search).
      */
     const SEARCH = 'quick_search.search';
 
     /**
-     * Elastic search is a primary service to search data but if it's disabled or some error happened then we can fetch from database.
+     * Fallback event if elastica search is disabled or some error was happened.
      */
     const SEARCH_FALLBACK = 'quick_search.search_fallback';
 
     /**
-     * Loads data for deferred ids, related entities and validates view permissions.
+     * Loads data for deferred ids, related entities and then validates view permissions.
      */
     const POST_SEARCH = 'quick_search.post_search';
 
