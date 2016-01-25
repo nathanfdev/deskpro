8a29cfb9395bab5fa55022bab306fc4cd03b2f0a
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-25 12:47:46 +0300

    quick search ticket by ref tests

--- app/src/DeskPRO/Bundle/AppBundle/QuickSearch/EventListener/TicketRefListener.php
+++ app/src/DeskPRO/Bundle/AppBundle/QuickSearch/EventListener/TicketRefListener.php
@@ -82,15 +82,17 @@ class TicketRefListener implements EventSubscriberInterface
             return;
         }
 
+        $query = $request->getQuery();
+
         /** @var \Application\DeskPRO\EntityRepository\Ticket $repository */
         $repository = $this->em->getRepository('DeskPRO:Ticket');
-        $ticket     = $repository->findTicketRef($request->getQuery());
+        $ticket     = $repository->findTicketRef($query);
 
         if ($ticket) {
             $context->ids->add((int) $ticket->getId());
             $context->entities->add($ticket);
-        } elseif (strlen($request->getQuery()) >= 3) {
-            $tickets = $repository->searchTicketRef($request->getQuery());
+        } elseif (strlen($query) >= 3) {
+            $tickets = $repository->searchTicketRef($query);
 
             foreach ($tickets as $ticket) {
                 $context->ids->add((int) $ticket->getId());
