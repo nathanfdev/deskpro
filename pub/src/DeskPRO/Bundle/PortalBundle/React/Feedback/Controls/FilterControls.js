1d42bfb54e045ed5da55a45373968ffa7a590bf7
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-26 19:18:16 +0300

    feedback filter control cleanup

--- pub/src/DeskPRO/Bundle/PortalBundle/React/Feedback/Controls/FilterControls.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/React/Feedback/Controls/FilterControls.js
@@ -1,50 +1,67 @@
-import React from "react"
-import _ from "lodash"
-import TabRow from "./TabRow"
-import TypeRow from "./TypeRow"
+import React, { PropTypes } from 'react';
+import TabRow from './TabRow';
+import TypeRow from './TypeRow';
 
 export default class FilterControls extends React.Component {
+
+  static propTypes = {
+    available: PropTypes.object,
+    updateFilter: PropTypes.func,
+    filterModel: PropTypes.object,
+    doSpin: PropTypes.any
+  };
+
+  setStatus(statusId) {
+    const { filterModel } = this.props;
+    filterModel.status_categories = [];
+    filterModel.setStatus(statusId);
+
+    this.updateFilter(filterModel);
+  }
+
+  setStatusCategory(statusCategoryId) {
+    const { filterModel } = this.props;
+    filterModel.toggleStatusCategory(statusCategoryId);
+
+    this.updateFilter(filterModel);
+  }
+
+  setSort(newSort) {
+    const { filterModel } = this.props;
+    filterModel.changeSort(newSort);
+
+    this.updateFilter(filterModel);
+  }
+
   updateFilter(filter) {
     this.props.updateFilter(filter);
   }
-  setStatus(status_id) {
-    let filter = this.props.filterModel;
-    filter.status_categories = [];
-    filter.setStatus(status_id);
-    this.updateFilter(filter);
-  }
-  setStatusCategory(status_category_id) {
-    let filter = this.props.filterModel;
-    filter.toggleStatusCategory(status_category_id);
-    this.updateFilter(filter);
+
+  toggleType(typeId) {
+    const { filterModel } = this.props;
+    filterModel.toggleType(typeId);
+
+    this.updateFilter(filterModel);
   }
 
-  setSort(new_sort) {
-    let filter = this.props.filterModel;
-    filter.changeSort(new_sort);
-    this.updateFilter(filter);
-  }
-  toggleType(type_id) {
-    let filter = this.props.filterModel;
-    filter.toggleType(type_id);
-    this.updateFilter(filter);
-  }
   render() {
+    const { available, filterModel, doSpin } = this.props;
+
     return (
       <div className="feedback-filter">
         <TabRow
-          available={this.props.available}
-          filter={this.props.filterModel}
+          available={available}
+          filter={filterModel}
           setStatus={this.setStatus.bind(this)}
           setSort={this.setSort.bind(this)}
           setStatusCategory={this.setStatusCategory.bind(this)}
           />
 
         <TypeRow
-          available={this.props.available.types}
-          selected={this.props.filterModel.types}
+          available={available.types}
+          selected={filterModel.types}
           toggleType={this.toggleType.bind(this)}
-          doSpin={this.props.doSpin}
+          doSpin={doSpin}
           />
       </div>
     );
