79913b0a99da94b7316849eed621e9a959b6c476
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-01-30 09:28:15 +0300

    portal rte adds blob hidden inputs if has blobPath

--- pub/src/DeskPRO/Bundle/PortalBundle/React/Form/PortalRte.js
+++ pub/src/DeskPRO/Bundle/PortalBundle/React/Form/PortalRte.js
@@ -74,8 +74,10 @@ export class PortalRte extends React.Component {
       $image.removeAttr('data-paste-id').attr('src', blob.url);
 
       const blobPath = $textarea.data('blob-path');
-      $(`<input type="hidden" name="${blobPath}[${blob.id}][blob_auth]" />`).val(blob.authcode).insertAfter($editor);
-      $(`<input type="hidden" name="${blobPath}[${blob.id}][is_inline]" />`).val(1).insertAfter($editor);
+      if (blobPath) {
+        $(`<input type="hidden" name="${blobPath}[${blob.id}][blob_auth]" />`).val(blob.authcode).insertAfter($editor);
+        $(`<input type="hidden" name="${blobPath}[${blob.id}][is_inline]" />`).val(1).insertAfter($editor);
+      }
     } else {
       $image.remove();
     }
