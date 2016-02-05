aae70869c18911a7f5bea321108993f6dcf93b92
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-02-05 13:27:10 +0300

    moved workflow type to app bundle

--- app/src/DeskPRO/Bundle/AppBundle/Form/Type/WorkflowType.php
+++ app/src/DeskPRO/Bundle/AppBundle/Form/Type/WorkflowType.php
@@ -29,38 +29,50 @@
 /**
  * DeskPRO.
  */
-namespace DeskPRO\Bundle\PortalBundle\Form\Form\Type;
+namespace DeskPRO\Bundle\AppBundle\Form\Type;
 
 use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+/**
+ * Class WorkflowType.
+ */
 class WorkflowType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'deskpro_workflow';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getParent()
     {
         return 'entity';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'class'         => 'Application\\DeskPRO\\Entity\\TicketWorkflow',
             'property'      => 'title',
             'empty_data'    => null,
             'required'      => true,
-            'query_builder' => function (EntityRepository $repo) {
-                    return $repo
-                        ->createQueryBuilder('w')
-                        ->select('w')
-                        ->addOrderBy('w.display_order')
-                    ;
-                },
-        ));
+            'query_builder' => function (EntityRepository $repository) {
+                return $repository
+                    ->createQueryBuilder('w')
+                    ->select('w')
+                    ->addOrderBy('w.display_order')
+                ;
+            },
+        ]);
     }
 }
