642bdcf0d53553692913aec029dff2fe51d9f17f
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-02-06 13:16:28 +0300

    removed ticket_message option from new ticket controller

--- app/src/DeskPRO/Bundle/PortalBundle/Controller/NewTicketController.php
+++ app/src/DeskPRO/Bundle/PortalBundle/Controller/NewTicketController.php
@@ -81,7 +81,6 @@ class NewTicketController extends AbstractController
 
         $form = $this->createForm('ticket', $ticket, [
             'person'                => $person,
-            'ticket_message'        => $ticket_message,
             'settings'              => $this->getBrandContainer()->getSettings(),
             'action'                => $this->generateUrl('portal_new_ticket'),
             'saved_form_subrequest' => $this->isSavedFormSubRequest($request),
