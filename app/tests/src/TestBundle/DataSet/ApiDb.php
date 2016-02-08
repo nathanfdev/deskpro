0cb2158cf966894b2fb6182fbceeddf2208c7df0
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-02-08 18:42:55 +0300

    api db set text/textarea fields on ticket layout

--- app/tests/src/TestBundle/DataSet/ApiDb.php
+++ app/tests/src/TestBundle/DataSet/ApiDb.php
@@ -128,6 +128,8 @@ class ApiDb extends AbstractDbSet
             INSERT INTO `custom_def_ticket` (`id`, `parent_id`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `is_agent_field`) VALUES ('3', '1', '', '0', '0', 'Medium', '', '?', '1', '1', '14', '0');
             INSERT INTO `custom_def_ticket` (`id`, `parent_id`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `is_agent_field`) VALUES ('4', '1', '', '0', '0', 'Large', '', '?', '1', '1', '15', '0');
             INSERT INTO `custom_def_ticket` (`id`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `handler_class`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `is_agent_field`) VALUES ('5', '', '0', '0', 'Delivery Time', 'A custom  field', 'Application\\\DeskPRO\\\CustomFields\\\Handler\\\DateTime', '?', '1', '1', '38', '0');
+            INSERT INTO `custom_def_ticket` (`id`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `handler_class`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `is_agent_field`) VALUES ('6', '', '0', '0', 'Widget Type', 'A custom  field', 'Application\\\DeskPRO\\\CustomFields\\\Handler\\\Text', '?', '1', '1', '10', '0');
+            INSERT INTO `custom_def_ticket` (`id`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `handler_class`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `is_agent_field`) VALUES ('7', '', '0', '0', 'Widget Description', 'A custom  field', 'Application\\\DeskPRO\\\CustomFields\\\Handler\\\Textarea', '?', '1', '1', '11', '0');
             "
         );
 
@@ -154,6 +156,8 @@ class ApiDb extends AbstractDbSet
             ->add(new LayoutField(FormFields::PRIORITY))
             ->add(new LayoutField('ticket_field', 1)) // Select box
             ->add(new LayoutField('ticket_field', 5)) // Datetime
+            ->add(new LayoutField('ticket_field', 6)) // Text
+            ->add(new LayoutField('ticket_field', 7)) // Textarea
         ;
 
         $ticket_layout2               = new TicketLayout($dep2);
