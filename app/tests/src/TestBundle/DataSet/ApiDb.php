a83ff19a108720b7718b82ef7757208e92a1e9e2
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-02-08 17:10:08 +0300

    api db set datetime custom def ticket

--- app/tests/src/TestBundle/DataSet/ApiDb.php
+++ app/tests/src/TestBundle/DataSet/ApiDb.php
@@ -127,6 +127,7 @@ class ApiDb extends AbstractDbSet
             INSERT INTO `custom_def_ticket` (`id`, `parent_id`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `is_agent_field`) VALUES ('2', '1', '', '0', '0', 'Small', '', '?', '1', '1', '13', '0');
             INSERT INTO `custom_def_ticket` (`id`, `parent_id`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `is_agent_field`) VALUES ('3', '1', '', '0', '0', 'Medium', '', '?', '1', '1', '14', '0');
             INSERT INTO `custom_def_ticket` (`id`, `parent_id`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `is_agent_field`) VALUES ('4', '1', '', '0', '0', 'Large', '', '?', '1', '1', '15', '0');
+            INSERT INTO `custom_def_ticket` (`id`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `handler_class`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `is_agent_field`) VALUES ('5', '', '0', '0', 'Delivery Time', 'A custom  field', 'Application\\\DeskPRO\\\CustomFields\\\Handler\\\DateTime', '?', '1', '1', '38', '0');
             "
         );
 
@@ -151,7 +152,8 @@ class ApiDb extends AbstractDbSet
             ->add(new LayoutField(FormFields::PRODUCT))
             ->add(new LayoutField(FormFields::CC))
             ->add(new LayoutField(FormFields::PRIORITY))
-            ->add(new LayoutField('ticket_field', 1))
+            ->add(new LayoutField('ticket_field', 1)) // Select box
+            ->add(new LayoutField('ticket_field', 5)) // Datetime
         ;
 
         $ticket_layout2               = new TicketLayout($dep2);
