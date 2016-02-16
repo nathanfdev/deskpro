824c95c59f3c54138fc19f5d90fdbbac11a96317
Christopher Nadeau <chris@nadeau.ws>
2016-02-16 09:01:19 +0000

    Hide options from isset op as well

--- web/javascripts/DeskPRO/Form/RuleBuilder.js
+++ web/javascripts/DeskPRO/Form/RuleBuilder.js
@@ -414,7 +414,7 @@ DeskPRO.Form.RuleBuilder = new Orb.Class({
 		var opSel = $('.builder-op select', row);
 		var updateOp = function() {
 			var val = opSel.val();
-			if (val == 'changed' || val == 'not_isset') {
+			if (val == 'changed' || val == 'not_isset' || val == 'isset') {
 				$('.builder-options', row).hide();
 			} else {
 				$('.builder-options', row).show();
