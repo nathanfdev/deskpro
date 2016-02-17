9755852b7962c9b185285406d0da89b2c504958b
ImmortaL <denis@ranneft.ru>
2016-02-17 16:07:58 +0300

    return default settings for api logging

--- app/BUILD/sys/config/settings.php
+++ app/BUILD/sys/config/settings.php
@@ -1076,4 +1076,15 @@ return array(
     'response.cache.global_version' => '7dedef53d7b6762e2ad984051e37638d',
 
     'response.cache.enabled' => false,
+
+    'api_log.enabled'     => false,
+    'api_log.modes'       => ['key'],
+    'api_log.dupe.modes'  => ['key'],
+    'api_log.writer.type' => 'db',
+    'api_log.writer.file' => [
+        'log_max_size'  => 5 * 1024 * 1024,
+        'log_max_files' => 5,
+        'log_name'      => 'api_log.log',
+    ],
+    'api_log.writer.file.serializer.type' => 'serialize',
 );
