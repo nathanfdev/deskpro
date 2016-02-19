e548f55dcb8b6961e6b97daef03e00de30f0aa99
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-02-19 17:33:05 +0000

    Tweak font styles, fix permissions in fixtures

---
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/DataFixtures/InstallFixtures/DefaultPermissionsFixture.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+ * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * a British company located in London, England.
+ *
+ * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ *
+ * The license agreement under which this software is released
+ * can be found at https://www.deskpro.com/eula/
+ *
+ * By using this software, you acknowledge having read the license
+ * and agree to be bound thereby.
+ *
+ * Please note that DeskPRO is not free software. We release the full
+ * source code for our software because we trust our users to pay us for
+ * the huge investment in time and energy that has gone into both creating
+ * this software and supporting our customers. By providing the source code
+ * we preserve our customers' ability to modify, audit and learn from our
+ * work. We have been developing DeskPRO since 2001, please help us make it
+ * another decade.
+ *
+ * Like the work you see? Think you could make it better? We are always
+ * looking for great developers to join us: http://www.deskpro.com/jobs/
+ *
+ * ~ Thanks, Everyone at Team DeskPRO
+ */
+
+/**
+ * DeskPRO.
+ */
+namespace DeskPRO\Bundle\AppBundle\DataFixtures\InstallFixtures;
+
+use Application\DeskPRO\People\UserPermissions\GroupsDbLoader;
+use Application\DeskPRO\People\UserPermissions\UserPermissions;
+use DeskPRO\Bundle\AppBundle\DataFixtures\DeskProAbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class DefaultPermissionsFixture extends DeskProAbstractFixture implements OrderedFixtureInterface
+{
+    private $permNames;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 90;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $insert_dep_perms = [];
+        $insert_perms     = [];
+
+        foreach ([
+            $this->getReference('usergroup.everyone'),
+            $this->getReference('usergroup.registered'),
+        ] as $ug) {
+            foreach (['support', 'sales'] as $id) {
+                $dep                = $this->getReference('department.'.$id);
+                $insert_dep_perms[] = [
+                    'usergroup_id'  => $ug->getId(),
+                    'department_id' => $dep->getId(),
+                    'name'          => 'full',
+                    'value'         => 1,
+                    'app'           => 'tickets',
+                ];
+            }
+            foreach (['support', 'sales'] as $id) {
+                $dep                = $this->getReference('chat_department.'.$id);
+                $insert_dep_perms[] = [
+                    'usergroup_id'  => $ug->getId(),
+                    'department_id' => $dep->getId(),
+                    'name'          => 'full',
+                    'value'         => 1,
+                    'app'           => 'chat',
+                ];
+            }
+
+            foreach ($this->getUsergroupPermNames() as $p) {
+                $insert_perms[] = [
+                    'usergroup_id' => $ug->getId(),
+                    'name'         => $p,
+                    'value'        => 1,
+                ];
+            }
+        }
+
+        /** @var \Application\DeskPRO\DBAL\Connection $db */
+        $db = $this->container->get('database_connection');
+        $db->batchInsert('department_permissions', $insert_dep_perms);
+        $db->batchInsert('permissions', $insert_perms);
+    }
+
+    private function getUsergroupPermNames()
+    {
+        if ($this->permNames !== null) {
+            return $this->permNames;
+        }
+
+        $perms = new UserPermissions();
+
+        $set_perms = array();
+        foreach (GroupsDbLoader::$prefix_map as $real_name => $coll_name) {
+            $obj = $perms->$coll_name;
+            foreach ($obj->getNames() as $prop) {
+                $set_perms[] = $real_name.'.'.$prop;
+            }
+        }
+
+        return $this->permNames = $set_perms;
+    }
+}
