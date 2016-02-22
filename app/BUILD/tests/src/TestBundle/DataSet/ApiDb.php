753186536749eb2c15e54bcf42562e5f779ffefa
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-02-22 13:46:33 +0300

    api db set handler class fix

--- app/BUILD/tests/src/TestBundle/DataSet/ApiDb.php
+++ app/BUILD/tests/src/TestBundle/DataSet/ApiDb.php
@@ -501,7 +501,7 @@ class ApiDb extends AbstractDbSet
             INSERT INTO `custom_def_feedback`
             (`id`, `parent_id`, `app_id`, `sys_name`, `js_class`, `has_form_template`, `has_display_template`, `title`, `description`, `handler_class`, `options`, `is_user_enabled`, `is_enabled`, `display_order`, `default_value`, `is_agent_field`)
             VALUES
-              (1, NULL, NULL, 'cat', '', 0, 0, 'Category', 'e.g., maybe Windows, Mac, Linux.', NULL, '', 1, 1, 0, NULL, 1)
+              (1, NULL, NULL, 'cat', '', 0, 0, 'Category', 'e.g., maybe Windows, Mac, Linux.', 'Application\\\DeskPRO\\\CustomFields\\\Handler\\\Text', '', 1, 1, 0, NULL, 1)
             ;
         "
         );
