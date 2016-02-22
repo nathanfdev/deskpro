5b9a02f19eeb96fbb54f528d37393d36df56e089
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-02-22 18:35:41 +0300

    little cleanup

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Organizations/OrganizationsController.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Organizations/OrganizationsController.php
@@ -29,7 +29,6 @@
 /**
  * DeskPRO.
  */
-
 namespace DeskPRO\Bundle\ApiBundle\Controller\Organizations;
 
 use Application\DeskPRO\Entity\Organization;
@@ -42,6 +41,7 @@ use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Route;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class OrganizationsController.
@@ -71,6 +71,11 @@ class OrganizationsController extends CrudController
      *      }
      * )
      * @Get("/{id}/tickets")
+     *
+     * @param Request $request
+     * @param int     $id
+     *
+     * @return Response
      */
     public function getTicketsAction(Request $request, $id)
     {
@@ -82,7 +87,8 @@ class OrganizationsController extends CrudController
      */
     protected function applyListFilters(QueryBuilder $qb, $alias, Request $request)
     {
-        if ($period = $request->get('period_created')) {
+        $period = $request->get('period_created');
+        if ($period) {
             $datePeriodCaseWhen = DatePeriods::getDatePeriodCaseWhenDql("$alias.date_created");
             $qb->andWhere("$datePeriodCaseWhen = :period_created");
             $qb->setParameter('period_created', $period);
