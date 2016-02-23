8e0b514fe24df1aa7426af72d769123b86483701
Christopher Nadeau <chris@nadeau.ws>
2016-02-23 12:17:14 +0000

    Use read server for query

--- app/src/Application/DeskPRO/EntityRepository/DepartmentPermission.php
+++ app/src/Application/DeskPRO/EntityRepository/DepartmentPermission.php
@@ -65,7 +65,7 @@ class DepartmentPermission extends AbstractEntityRepository
             $this->cache_by_agent = array();
             $this->cache_by_group = array();
 
-            $q = $this->_em->getConnection()->query('
+            $q = App::getContainer()->getDbRead()->query('
                 SELECT dp.app, dp.department_id, dp.usergroup_id, dp.person_id, dp.name, dp.value
                 FROM department_permissions dp
             ');
