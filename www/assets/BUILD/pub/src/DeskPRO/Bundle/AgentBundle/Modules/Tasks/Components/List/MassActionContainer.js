fddd1aa0bc2f86f4399aab6e392c4d9d7e2a51fa
A. Yakutskiy <a.yakutskiy@gmail.com>
2016-02-25 19:41:35 +0300

    mass action options for setting project, assign and status of tasks

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/MassActionContainer.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/List/MassActionContainer.js
@@ -1,18 +1,20 @@
 import React, {Component, PropTypes} from 'react';
 import { MassActionBarContainer }
   from '../../../../../AgentBundle/Modules/Common/Components/ListFrame/MassActionBar/MassActionBarContainer';
-import { selectedSelector } from '../../../Application/Selectors/massActions';
 import { massActionsSelector } from '../../Selectors/list';
 import { loadIndicator } from '../../Actions/listActions';
 import { initialLoad } from '../../Actions/navActions';
 
 import { connect } from 'react-redux';
 @connect(state => ({
-  selected: selectedSelector(state),
   actions: massActionsSelector(state)
 }))
 
 export class MassActionContainer extends Component {
+
+  static propTypes = {
+    actions: PropTypes.array.isRequired
+  };
 
   render() {
     const config = {
--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Selectors/list.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Selectors/list.js
@@ -1,6 +1,8 @@
 import { hashStateSelectorFactory } from 'DeskPRO/Bundle/AgentBundle/Modules/Application/Selectors/routing';
 import { createSelector } from 'reselect';
 import { constants } from 'DeskPRO/Bundle/AgentBundle/Constants/Constants';
+import { collectionSelectorFactory } from 'DeskPRO/Bundle/AppBundle/Modules/RecordsStore';
+import { agentsSelector } from 'DeskPRO/Bundle/AppBundle/Modules/RecordsStore/Shortcuts/agents';
 
 const stateSelector = state => state.Tasks.list;
 
@@ -52,25 +54,27 @@ export const isLoadedSelector = createSelector(
 /* ==================== Mass actions ===================== */
 
 export const massActionsSelector = createSelector(
-  [],
-  () => {
+  [collectionSelectorFactory('Project', 'all'), agentsSelector],
+  (projects, agents) => {
     const massActions = [];
     // Project options
+    const projectOptions = projects.toArray().map(type => ({ value: type.get('id'), label: type.get('title') }));
     massActions.push({
       label: 'Project',
       type: 'action',
       param: 'set_project',
       quickFilter: true,
-      options: []
+      options: projectOptions
     });
 
     // Assign options
+    const assignOptions = agents.toArray().map(type => ({ value: type.get('id'), label: type.get('name') }));
     massActions.push({
       label: 'Assign',
       type: 'action',
       param: 'assign',
       quickFilter: true,
-      options: []
+      options: assignOptions
     });
 
     // Due date options
@@ -85,7 +89,7 @@ export const massActionsSelector = createSelector(
     // Status options
     massActions.push({
       label: 'Status', type: 'action', param: 'set_status', quickFilter: true,
-      options: []
+      options: [{value: 1, label: 'Complete'}, {value:0, label: 'Incomplete'}]
     });
 
     // Other options
