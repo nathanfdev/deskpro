2839bd630e18ab44cbe519c49eea8257769de80d
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-02-25 16:26:04 +0300

    ticket filter transformer term fix

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/DataSerializer/DataTransformer/TicketFilterTransformer.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/DataSerializer/DataTransformer/TicketFilterTransformer.php
@@ -29,10 +29,10 @@
 /**
  * DeskPRO.
  */
-
 namespace DeskPRO\Bundle\AppBundle\DataSerializer\DataTransformer;
 
 use DeskPRO\Bundle\AppBundle\DataSerializer\DataTransformerRequest;
+use DeskPRO\Bundle\AppBundle\Entity\TicketFilter;
 
 /**
  * Class TicketFilterTransformer.
@@ -62,6 +62,11 @@ class TicketFilterTransformer extends AbstractDataSerializerTransformer
      */
     public function getCustomProperties(DataTransformerRequest $transformation_request)
     {
-        return [];
+        /** @var TicketFilter $data */
+        $data = $transformation_request->getDataToBeTransformed();
+
+        return [
+            'term' => $data->getTerm(),
+        ];
     }
 }
