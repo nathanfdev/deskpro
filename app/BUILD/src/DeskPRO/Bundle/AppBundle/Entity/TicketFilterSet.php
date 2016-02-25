15759fee66fce6f88ba8f584d66b96761ce0a4ad
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-02-25 18:33:04 +0300

    better methods/properties order in ticket filter set entity

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Entity/TicketFilterSet.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Entity/TicketFilterSet.php
@@ -29,7 +29,6 @@
 /**
  * DeskPRO.
  */
-
 namespace DeskPRO\Bundle\AppBundle\Entity;
 
 use Application\DeskPRO\Entity\Person;
@@ -55,18 +54,21 @@ class TicketFilterSet implements EntityInterface, NotifyPropertyChanged
 
     /**
      * @var string
+     *
      * @ORM\Column(name="title", type="string")
      */
     protected $title;
 
     /**
      * @var int
+     *
      * @ORM\Column(name="display_order", type="integer")
      */
     protected $display_order;
 
     /**
      * @var TicketFilter[]|ArrayCollection
+     *
      * @ORM\OneToMany(
      *     targetEntity="DeskPRO\Bundle\AppBundle\Entity\TicketFilter",
      *     mappedBy="filter_set",
@@ -78,35 +80,15 @@ class TicketFilterSet implements EntityInterface, NotifyPropertyChanged
     protected $filters;
 
     /**
-     * Returns the filters in the set as an array of filter ids.
-     *
-     * @return array the filter IDs attached.
-     *
-     * @Serializer\VirtualProperty
-     * @Serializer\SerializedName("filters")
-     */
-    public function getFiltersIds()
-    {
-        if (!$this->filters) {
-            return array();
-        }
-
-        $my_ids = array();
-        foreach ($this->filters as $filter) {
-            $my_ids[] = $filter->getId();
-        }
-
-        return $my_ids;
-    }
-
-    /**
      * @var bool
+     *
      * @ORM\Column(name="is_default", type="boolean")
      */
     protected $is_default = false;
 
     /**
      * @var Person
+     *
      * @ORM\ManyToOne(targetEntity="Application\DeskPRO\Entity\Person", cascade={"remove"})
      * @ORM\JoinColumn(name="person_id", onDelete="CASCADE")
      */
@@ -114,6 +96,7 @@ class TicketFilterSet implements EntityInterface, NotifyPropertyChanged
 
     /**
      * @var Person[]|ArrayCollection
+     *
      * @ORM\ManyToMany(targetEntity="Application\DeskPRO\Entity\Person")
      * @ORM\JoinTable(
      *      name="filter_set_agents",
@@ -127,6 +110,9 @@ class TicketFilterSet implements EntityInterface, NotifyPropertyChanged
      */
     protected $shared_agents;
 
+    /**
+     * Constructor.
+     */
     public function __construct()
     {
         $this->filters       = new ArrayCollection();
@@ -156,7 +142,6 @@ class TicketFilterSet implements EntityInterface, NotifyPropertyChanged
     public function addFilter(TicketFilter $filter)
     {
         $this->filters->add($filter);
-
         $this->setModelField('filter', $filter);
 
         $filter->setFilterSet($this);
@@ -194,14 +179,42 @@ class TicketFilterSet implements EntityInterface, NotifyPropertyChanged
         $this->setModelField('display_order', (int) $display_order);
     }
 
+    /**
+     * @return bool
+     */
     public function getIsDefault()
     {
         return $this->is_default === true;
     }
 
+    /**
+     * @param bool $default
+     */
     public function setIsDefault($default)
     {
         $this->setModelField('is_default', (bool) $default);
+    }
+
+    /**
+     * Returns the filters in the set as an array of filter ids.
+     *
+     * @return array the filter IDs attached.
+     *
+     * @Serializer\VirtualProperty
+     * @Serializer\SerializedName("filters")
+     */
+    public function getFiltersIds()
+    {
+        if (!$this->filters) {
+            return [];
+        }
+
+        $my_ids = [];
+        foreach ($this->filters as $filter) {
+            $my_ids[] = $filter->getId();
+        }
+
+        return $my_ids;
     }
 
     /**
@@ -238,6 +251,9 @@ class TicketFilterSet implements EntityInterface, NotifyPropertyChanged
         $this->setModelField('shared_agents', $this->shared_agents);
     }
 
+    /**
+     * @return bool
+     */
     public function isPrivate()
     {
         return null !== $this->private_agent;
