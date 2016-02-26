92317600ddbd33d8d3bf69ab39418485b0339315
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-02-26 08:18:40 +0300

    removed ticket filter views controller

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Tickets/NewFilters/TicketFiltersController.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Tickets/NewFilters/TicketFiltersController.php
@@ -36,7 +36,9 @@ use DeskPRO\Bundle\ApiBundle\Controller\Tickets\TicketsController;
 use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\ApiModes;
 use DeskPRO\Bundle\AppBundle\Entity\PersonSetting;
 use DeskPRO\Bundle\AppBundle\Entity\TicketFilter;
+use DeskPRO\Bundle\AppBundle\Form\Type\Tickets\TicketFilterType;
 use DeskPRO\Bundle\AppBundle\Settings\Model\Tickets\TicketsSettings;
+use DeskPRO\Bundle\AppBundle\TermEngine\Engine\TermEngineContext;
 use Doctrine\ORM\QueryBuilder;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Put;
@@ -56,6 +58,7 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 class TicketFiltersController extends CrudController
 {
     public static $entity    = TicketFilter::class;
+    public static $type      = TicketFilterType::class;
     public static $listOrder = 'asc';
 
     /**
@@ -125,6 +128,17 @@ class TicketFiltersController extends CrudController
 //        $this->getManager()->flush();
 //
 //        return new Response(null, Response::HTTP_NO_CONTENT);
+
+// from views controller
+//try {
+//$form->submit($submitted, $request->getMethod() !== 'PUT');
+//} catch (TermTypeDoesNotExistException $e) {
+//    throw new WrappedApiErrorException(
+//        new BadRequestHttpException(ApiErrors::TERM_TYPE_DOES_NOT_EXIST),
+//        [
+//            'type' => $e->getMessage(),
+//        ]
+//
 //    }
 
     /**
@@ -142,7 +156,7 @@ class TicketFiltersController extends CrudController
      *
      * @return Response
      */
-    public function getFilterTicketsAction(Request $request, TicketFilter $filter)
+    public function getTicketsAction(Request $request, TicketFilter $filter)
     {
         /** @var HttpKernelInterface $kernel */
         $kernel = $this->get('kernel');
@@ -150,6 +164,52 @@ class TicketFiltersController extends CrudController
         return TicketsController::subRequestSearch($kernel, $request, [
             'filter' => $filter->getId(),
         ]);
+    }
+
+    /**
+     * @ApiDoc(
+     *      description="Get ticket filter view counts",
+     *      input={"class"="filter","name"=""},
+     *      statusCodes={
+     *          200="Success"
+     *      },
+     *      output="DeskPRO\Bundle\AppBundle\Entity\TicketFilter"
+     * )
+     *
+     * @Get("/ticket_filter_views/{filter}/count")
+     *
+     * @param Request      $request
+     * @param TicketFilter $filter
+     *
+     * @return View
+     */
+    public function getTicketsCountAction(Request $request, TicketFilter $filter)
+    {
+        // Let's retrieve the tickets for this filter.
+        $engine = $this->get('term_engine.dbal_ticket_filter_views.engine');
+        $conn   = $this->get('database_connection');
+
+        $context = new TermEngineContext($this->getUser());
+        // Applying the group-by clauses.
+        $group_by = $request->query->get('group_by');
+        if ($group_by) {
+            $context->addGroupByFromString($group_by);
+        }
+
+        $tickets_query = $engine->evaluate($filter, $context);
+
+        if ($group_by) {
+            $view_factory = $this->get('api_view_representation_factory');
+
+            return View::create(
+                $view_factory->dataSerialize($tickets_query->fetchGroupedCount(), $view_factory::DATATYPE_GROUPED_COUNT),
+                Response::HTTP_OK
+            );
+        } else {
+            return View::create($this->dataSerialize([
+                'count' => $tickets_query->fetchCount(),
+            ]));
+        }
     }
 
     /**
