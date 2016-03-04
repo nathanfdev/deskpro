a6d33c85f0aa05ec2086f7b333a6c01893458cb6
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-03-04 12:25:20 +0300

    js tests fix

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Reducers/list.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Reducers/list.js
@@ -1,7 +1,6 @@
 import { createReducer } from 'Ampliflux';
 import Immutable from 'immutable';
-import { setFullPayload, setValue, async, togglePayloadInCollection, handleMassAction, pushPayloadToCollection }
-  from 'Ampliflux/reducers/handlers';
+import { setFullPayload, setValue, async, togglePayloadInCollection, handleMassAction, pushPayloadToCollection } from 'Ampliflux/reducers/handlers';
 import * as actions from '../Actions/listActions';
 import { constants } from 'DeskPRO/Bundle/AgentBundle/Constants/Constants';
 import invariant from 'invariant';
