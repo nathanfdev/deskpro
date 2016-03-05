8e81f9dcec48493b6925cd2bda58755d4f0e9961
ImmortaL <denis@ranneft.ru>
2016-03-05 22:20:28 +0300

    remove useless SWG comments (not annotation) since we can restore it from git

--- app/BUILD/src/Application/DeskPRO/Entity/Usergroup.php
+++ app/BUILD/src/Application/DeskPRO/Entity/Usergroup.php
@@ -31,6 +31,7 @@
  *
  * @category Entities
  */
+
 namespace Application\DeskPRO\Entity;
 
 use Application\DeskPRO\Domain\DomainObject;
@@ -51,7 +52,6 @@ use Symfony\Component\Validator\Mapping\ClassMetadata as ValidatorClassMetadata;
  * @property string $sys_name
  * @property bool $is_enabled
  * @property Permission[] $permissions
- * SWG\Model(id="Usergroup")
  */
 class Usergroup extends DomainObject
 {
@@ -59,7 +59,6 @@ class Usergroup extends DomainObject
      * The unique ID.
      *
      * @var int
-     *          SWG\Property(name="id", type="integer")
      */
     protected $id = null;
 
@@ -67,7 +66,6 @@ class Usergroup extends DomainObject
      * Title of the usergroup.
      *
      * @var string
-     *             SWG\Property(name="title", type="string")
      */
     protected $title;
 
@@ -75,7 +73,6 @@ class Usergroup extends DomainObject
      * A note or description about the usergroup.
      *
      * @var string
-     *             SWG\Property(name="note", type="string")
      */
     protected $note = '';
 
@@ -83,7 +80,6 @@ class Usergroup extends DomainObject
      * Is this an agent group?
      *
      * @var bool
-     *           SWG\Property(name="is_agent_group", type="boolean")
      */
     protected $is_agent_group = false;
 
@@ -91,7 +87,6 @@ class Usergroup extends DomainObject
      * When non-null, the group is a special system group (hidden from most interfaces).
      *
      * @var bool
-     *           SWG\Property(name="sys_name", type="string")
      */
     protected $sys_name = null;
 
@@ -99,13 +94,11 @@ class Usergroup extends DomainObject
      * Is the group enabled?
      *
      * @var bool
-     *           SWG\Property(name="is_enabled", type="boolean")
      */
     protected $is_enabled = true;
 
     /**
      * @var \Doctrine\Common\Collections\ArrayCollection
-     *                                                   SWG\Property(name="permissions", type="array", SWG\Items("Permission"))
      */
     protected $permissions;
 
