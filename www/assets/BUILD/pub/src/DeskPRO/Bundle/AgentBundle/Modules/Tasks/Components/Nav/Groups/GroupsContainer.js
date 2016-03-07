ffab6030f68fe64f4788dbb9b6035bdffe5e158b
n3b <neb802@gmail.com>
2016-03-08 00:51:06 +0100

    few updates to groups

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Groups/GroupsContainer.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Groups/GroupsContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Immutable from 'immutable';
 import { connect } from 'react-redux';
 import { Groups } from './Groups';
 import { groupsCountSelector } from '../../../Selectors/nav';
 
-const groupsOptions = {
+const options = {
   my: {filter: {assigned_agent: ['me']}, label: 'My Tasks'},
   team: {filter: {assigned_team: ['me']}, label: 'My Team Tasks'},
   department: {filter: {assigned_department: ['me']}, label: 'My Department Tasks'},
@@ -13,11 +14,27 @@ const groupsOptions = {
 };
 
 @connect(state => ({
-  groupsCount: groupsCountSelector(state)
+  count: groupsCountSelector(state)
 }))
 export class GroupsContainer extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: props.count
+    }
+  }
+
+  componentWillReceiveProps(props) {
+    console.info(props.count.toJS());
+    this.setState({count: props.count});
+  }
+
+  shouldComponentUpdate(props, state) {
+    return !Immutable.is(this.state.count, state.count);
+  }
+
   render() {
-    return <Groups {...this.props} groupsOptions={groupsOptions} />;
+    return <Groups {...this.props} options={options} />;
   }
 }
