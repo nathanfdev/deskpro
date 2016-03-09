fe431cac01638a308bf56f94dd1405c044bb361b
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-03-09 11:57:53 +0000

    Misc fixes
    * [www] htaccess was wrong
    * [Kernel] Legacy API was using the wrong kernel
    * [PortalBundle] Portal theme repos did not resolve parent when loading from cache

--- app/BUILD/sys/Boot/BootTask/HttpKernelBootTask.php
+++ app/BUILD/sys/Boot/BootTask/HttpKernelBootTask.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -139,7 +139,7 @@ class HttpKernelBootTask implements BootTaskInterface
             return 'apiv2';
         }
         if ($this->isUrlSegmentPrefix($path, '/api')) {
-            return 'apiv2';
+            return 'api';
         }
 
         return 'user';
