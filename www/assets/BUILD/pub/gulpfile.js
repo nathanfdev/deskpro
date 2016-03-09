6b0cf84f74888ee9eb698aa4dc414c79e745787c
ImmortaL <denis@ranneft.ru>
2016-03-10 00:48:20 +0300

    Fix slate docs:  * Fix typos  * Fix document generation

--- www/assets/BUILD/pub/gulpfile.js
+++ www/assets/BUILD/pub/gulpfile.js
@@ -218,37 +218,29 @@ var slate = require('gulp-slate');
 
 gulp.task('slate', function() {
 
-  return Promise.all(
-    [
-      new Promise(function(resolve, reject) {
-        var options = {
-          scss: '../../../../app/BUILD/src/DeskPRO/Bundle/ApiBundle/Resources/apidocs/slate.scss',
-          style: 'androidstudio',
-          logo: 'static/Common/deskpro-logo_2x.png',
-          template: '../../../../app/BUILD/src/DeskPRO/Bundle/ApiBundle/Resources/apidocs/layouts/layout.html',
-        };
-        gulp.src(
-          [
-            '../../../../app/BUILD/src/DeskPRO/Bundle/ApiBundle/Resources/apidocs/source/index.html.twig.md'
-          ]
-          )
-          .pipe(slate(options))
-          .on('erorr', reject)
-          .pipe(gulp.dest('build/apidocs'))
-          .on('end', resolve)
-        ;
-      }),
-
-      new Promise(function(resolve) {
+  return new Promise(function(resolve, reject) {
+    var options = {
+      scss: '../../../../app/BUILD/src/DeskPRO/Bundle/ApiBundle/Resources/apidocs/slate.scss',
+      style: 'androidstudio',
+      logo: 'static/Common/deskpro-logo_2x.png',
+      template: '../../../../app/BUILD/src/DeskPRO/Bundle/ApiBundle/Resources/apidocs/layouts/layout.html'
+    };
+    gulp.src(
+      [
+        '../../../../app/BUILD/src/DeskPRO/Bundle/ApiBundle/Resources/apidocs/source/index.html.twig.md'
+      ]
+      )
+      .pipe(slate(options))
+      .on('erorr', reject)
+      .pipe(gulp.dest('build/apidocs'))
+      .on('end', function() {
         gulp.src(['build/apidocs/index.html.twig'])
           .pipe(gulp.dest(
             '../../../../app/BUILD/src/DeskPRO/Bundle/ApiBundle/Resources/views/apidocs/'
           ))
-          .on('end', resolve)
-        ;
-      })
-    ]
-  );
+          .on('end', resolve);
+      });
+  });
 });
 
 // ######################################################################################################################
