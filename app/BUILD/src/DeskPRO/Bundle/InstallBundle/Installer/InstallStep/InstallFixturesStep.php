baa252985390bc261f03cc7ddcbe1b0caba0c620
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-03-09 19:56:18 +0300

    display fixtures order in dev mode, disabled extra fields in ticket form

--- app/BUILD/src/DeskPRO/Bundle/InstallBundle/Installer/InstallStep/InstallFixturesStep.php
+++ app/BUILD/src/DeskPRO/Bundle/InstallBundle/Installer/InstallStep/InstallFixturesStep.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -93,6 +93,10 @@ class InstallFixturesStep extends AbstractStep
             $this->getSession()->enableFlag('reset_db_details');
 
             return;
+        } elseif ($is_dev) {
+            // display fixtures order in dev mode
+            $this->writeln('<info>'.$proc->getCommandLine().'</info>');
+            $this->writeln($proc->getOutput());
         }
 
         $this->writeln('Done!');
