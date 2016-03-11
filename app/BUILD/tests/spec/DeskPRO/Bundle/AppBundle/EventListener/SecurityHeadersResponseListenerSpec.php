50304c0bfa9761591b93068ebaf486b3ce5d787d
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-03-11 23:09:18 +0300

    phpspec fix

--- app/BUILD/tests/spec/DeskPRO/Bundle/AppBundle/EventListener/SecurityHeadersResponseListenerSpec.php
+++ app/BUILD/tests/spec/DeskPRO/Bundle/AppBundle/EventListener/SecurityHeadersResponseListenerSpec.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -46,11 +46,9 @@ class SecurityHeadersResponseListenerSpec extends ObjectBehavior
 
     public function it_listens_to_kernel_response_events_with_64_priority()
     {
-        $this->getSubscribedEvents()->shouldBe(
-            array(
-                KernelEvents::RESPONSE => array('onResponse', 64),
-            )
-        );
+        $this->getSubscribedEvents()->shouldBe([
+            KernelEvents::RESPONSE => ['onResponse', 64],
+        ]);
     }
 
     public function it_adds_security_headers_to_every_response(
@@ -61,12 +59,9 @@ class SecurityHeadersResponseListenerSpec extends ObjectBehavior
         $response->headers = $headers;
         $event->getResponse()->willReturn($response);
 
-        $headers->add(
-            array(
-                'X-Content-Type-Options' => 'nosniff',
-                'X-FRAME-OPTIONS'        => 'DENY',
-            )
-        )->shouldBeCalled();
+        $headers->add([
+            'X-Content-Type-Options' => 'nosniff',
+        ])->shouldBeCalled();
 
         $this->onResponse($event);
     }
