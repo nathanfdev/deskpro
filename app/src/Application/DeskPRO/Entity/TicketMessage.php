0d14ea6dee2023f818adf4cad82804edd7b3b952
n3b <neb802@gmail.com>
2016-03-15 23:59:21 +0100

    [FIX] do not resize inline attachments

--- app/src/Application/DeskPRO/Entity/TicketMessage.php
+++ app/src/Application/DeskPRO/Entity/TicketMessage.php
@@ -351,9 +351,9 @@ class TicketMessage extends \Application\DeskPRO\Domain\DomainObject
         return $message;
     }
 
-    public function getMessageHtml()
+    public function getMessageHtml($resizeInlines = true)
     {
-        return $this->procInlineAttach($this->message);
+        return $this->procInlineAttach($this->message, $resizeInlines);
     }
 
     /**
@@ -361,11 +361,11 @@ class TicketMessage extends \Application\DeskPRO\Domain\DomainObject
      *
      * @return string
      */
-    public function procInlineAttach($message)
+    public function procInlineAttach($message, $resizeInlines = true)
     {
         // An email might have inline attachments and we tokenize them with these
         // codes so we can now turn them into inline images or attachment links
-        $fn = function ($m, $before = '') {
+        $fn = function ($m, $before = '') use ($resizeInlines) {
             $download_url = App::getSetting('core.deskpro_url');
             $download_url .= ltrim(
                 App::getRouter()->getGenerator()->generatePath(
@@ -409,10 +409,14 @@ class TicketMessage extends \Application\DeskPRO\Domain\DomainObject
                 $replace = sprintf('<img src="%s" title="%s" />', $url, $m[3]);
             } elseif ($m[1] == 'image') {
                 $url = App::getSetting('core.deskpro_url');
+                $_p  = array('blob_auth_id' => $m[2], 'filename' => $m[3], 'sc' => $sc_code);
+                if ($resizeInlines) {
+                    $_p['s'] = 350;
+                }
                 $url .= ltrim(
                     App::getRouter()->getGenerator()->generatePath(
                         'serve_blob',
-                        array('blob_auth_id' => $m[2], 'filename' => $m[3], 's' => 350, 'sc' => $sc_code),
+                        $_p,
                         false
                     ),
                     '/'
