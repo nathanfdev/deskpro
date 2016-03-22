58105bd35dfee264c7b4a8a6411c953ffdb01d5f
A. Yakutskiy <a.yakutskiy@gmail.com>
2016-03-22 17:49:52 +0300

    Tickets mass actions: assign

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/ActionEngine/Applicators/Tickets/ApplyAssignAction.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/ActionEngine/Applicators/Tickets/ApplyAssignAction.php
@@ -33,33 +33,49 @@
 namespace DeskPRO\Bundle\AppBundle\ActionEngine\Applicators\Tickets;
 
 use Application\DeskPRO\Entity\Ticket;
+use Application\DeskPRO\Tickets\TicketManager;
 use DeskPRO\Bundle\AppBundle\ActionEngine\Applicators\AbstractActionApplicator;
 use DeskPRO\Bundle\AppBundle\ActionEngine\Applicators\ActionApplicatorInterface;
+use Doctrine\ORM\EntityManager;
 
 class ApplyAssignAction extends AbstractActionApplicator implements ActionApplicatorInterface
 {
+    /** @var EntityManager  */
+    protected $em;
+    private $tm;
+
+    public function __construct(EntityManager $em, TicketManager $tm)
+    {
+        parent::__construct($em);
+        $this->tm = $tm;
+    }
+
     /**
      * @param Ticket[] $tickets
      */
     public function apply(array $tickets)
     {
         $collection = $this->init();
+        $context    = $this->tm->createAgentExecutorContext(null, 'set_status', 'mass_actions');
 
         foreach ($collection as $type => $value) {
             switch ($type) {
                 case 'agent':
                     foreach ($tickets as $ticket) {
                         $ticket->setAgent($value);
+                        $this->tm->saveTicket($ticket, $context);
                     }
                     break;
                 case 'team':
                     foreach ($tickets as $ticket) {
                         $ticket->setAgentTeam($value);
+                        $this->tm->saveTicket($ticket, $context);
                     }
                     break;
                 case 'department':
                     foreach ($tickets as $ticket) {
                         $ticket->setDepartment($value);
+                        $this->tm->saveTicket($ticket, $context);
                     }
                     break;
             }
@@ -76,13 +92,13 @@ class ApplyAssignAction extends AbstractActionApplicator implements ActionApplic
         foreach ($assign as $type => $id) {
             switch ($type) {
                 case 'agent':
-                    $collection['agent'] = $this->em->getRepository('DeskPRO:Person')->find($id);
+                    $collection['agent'] = $this->em->getRepository('DeskPRO:Person')->find($id[0]);
                     break;
                 case 'team':
-                    $collection['team'] = $this->em->getRepository('DeskPRO:AgentTeam')->find($id);
+                    $collection['team'] = $this->em->getRepository('DeskPRO:AgentTeam')->find($id[0]);
                     break;
                 case 'department':
-                    $collection['department'] = $this->em->getRepository('DeskPRO:Department')->find($id);
+                    $collection['department'] = $this->em->getRepository('DeskPRO:Department')->find($id[0]);
                     break;
             }
         }
