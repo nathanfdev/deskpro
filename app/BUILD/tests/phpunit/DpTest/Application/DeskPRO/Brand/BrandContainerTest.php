802bb1c7edd2db82df289ba1416d5715d8e6ab54
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-03-23 14:21:10 +0000

    Remove BrandAssetLoader that is no longer necessary

--- app/BUILD/tests/phpunit/DpTest/Application/DeskPRO/Brand/BrandContainerTest.php
+++ app/BUILD/tests/phpunit/DpTest/Application/DeskPRO/Brand/BrandContainerTest.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -29,6 +29,7 @@
 /**
  * DeskPRO.
  */
+
 namespace DpTest\DeskPRO\Application\Brand;
 
 use DeskPRO\Bundle\PortalBundle\Brand\BrandContainer;
@@ -40,8 +41,7 @@ class BrandContainerTest extends DeskProTestCase
     {
         $mockBrand    = \Mockery::mock('Application\DeskPRO\Entity\Brand');
         $mockSettings = \Mockery::mock('Application\DeskPRO\NewSettings\SettingsBag');
-        $assetLoader  = \Mockery::mock('DeskPRO\Bundle\PortalBundle\Brand\BrandAssetLoader');
-        $bc           = new BrandContainer($mockBrand, $mockSettings, $assetLoader);
+        $bc           = new BrandContainer($mockBrand, $mockSettings);
 
         $this->assertSame($mockBrand, $bc->getBrand());
         $this->assertSame($mockSettings, $bc->getSettings());
@@ -51,8 +51,7 @@ class BrandContainerTest extends DeskProTestCase
     {
         $mockBrand    = \Mockery::mock('Application\DeskPRO\Entity\Brand');
         $mockSettings = \Mockery::mock('Application\DeskPRO\NewSettings\SettingsBag');
-        $assetLoader  = \Mockery::mock('DeskPRO\Bundle\PortalBundle\Brand\BrandAssetLoader');
-        $bc           = new BrandContainer($mockBrand, $mockSettings, $assetLoader);
+        $bc           = new BrandContainer($mockBrand, $mockSettings);
 
         $mockSettings->shouldReceive('get')->with('setting_name', null)->andReturn('the val!')->once();
 
