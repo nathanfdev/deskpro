5dbaa625bf3823275df09f2638f65b366a1e8838
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-03-23 10:51:31 +0000

    Fix elastic search reset cmd

--- app/BUILD/src/Application/DeskPRO/NewSearch/Provider/Doctrine.php
+++ app/BUILD/src/Application/DeskPRO/NewSearch/Provider/Doctrine.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -93,7 +93,6 @@ class Doctrine extends Provider
 
             if ($this->options['clear_object_manager']) {
                 $this->managerRegistry->getManagerForClass($this->objectClass)->clear();
-                $this->managerRegistry->getManagerForClass($this->objectClass)->clearRepositoryCache();
 
                 $objects          = null;
                 $stepCount        = null;
