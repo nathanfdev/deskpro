a8fda949cd08f26c11416f73c26b32c9037fee8f
A. Yakutskiy <a.yakutskiy@gmail.com>
2016-03-24 18:57:40 +0300

    fix jest

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tickets/Reducers/list.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tickets/Reducers/list.js
@@ -14,6 +14,7 @@ export const ticketsListInitialState = {
   },
   pagination: {},
   elements: [], // array of filtered tickets IDs
+  selected: [],
 
   tableVisibleFields: ['id', 'urgency', 'person', 'agent', 'subject', 'status'],
   cardVisibleFields: ['id', 'urgency', 'person', 'agent', 'subject', 'status', 'date_created', 'labels']
