d44c9d045eb8fd7e54bcf3dbc00399d8e532f534
ImmortaL <denis@ranneft.ru>
2016-03-29 18:25:48 +0300

    replace dataSerialize with wrap to solve empty sideload

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Tickets/LegacyFilters/TicketFiltersController.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Tickets/LegacyFilters/TicketFiltersController.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -98,6 +98,6 @@ class TicketFiltersController extends CrudController
         $pager->setMaxPerPage($max_per_page);
         $pager->setCurrentPage($current_page);
 
-        return View::create($this->dataSerialize($pager));
+        return View::create($this->wrap($pager));
     }
 }
