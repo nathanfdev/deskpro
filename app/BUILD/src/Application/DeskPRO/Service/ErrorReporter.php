23aade12eee69f976629caf41c88c5f60ed73bfd
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-03-29 14:24:24 +0100

    Fix invalid constants

--- app/BUILD/src/Application/DeskPRO/Service/ErrorReporter.php
+++ app/BUILD/src/Application/DeskPRO/Service/ErrorReporter.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -29,6 +29,7 @@
 /**
  * DeskPRO.
  */
+
 namespace Application\DeskPRO\Service;
 
 use Application\DeskPRO\App;
@@ -394,6 +395,9 @@ class ErrorReporter
      */
     public static function sendHeartbeat()
     {
+        /* @var \DpRun\DpEnv */
+        global $DP_ENV;
+
         if (isset($GLOBALS['DP_DISABLE_SENDREPORTS'])) {
             return '';
         }
@@ -411,7 +415,7 @@ class ErrorReporter
 
         $data['setting_elastica_enabled'] = App::getSetting('elastica.enabled');
         $data['setting_core_deskpro_url'] = App::getSetting('core.deskpro_url');
-        $data['db_id_hash']               = md5(DP_DATABASE_HOST.DP_DATABASE_NAME.DP_DATABASE_USER);
+        $data['db_id_hash']               = md5($DP_ENV->getConfig('database.host').$DP_ENV->getConfig('database.dbnae').$DP_ENV->getConfig('database.user'));
         $data['license_code']             = App::getSetting('core.license');
 
         try {
