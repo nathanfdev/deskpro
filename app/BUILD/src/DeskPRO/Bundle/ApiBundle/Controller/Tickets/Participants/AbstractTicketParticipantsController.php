147a4a2e863e6ddd6449d6f31b1bdb51f014b1f0
ImmortaL <denis@ranneft.ru>
2016-03-29 22:25:56 +0300

    wrap instead of dataSerialize for TicketParticipants

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Tickets/Participants/AbstractTicketParticipantsController.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Tickets/Participants/AbstractTicketParticipantsController.php
@@ -48,6 +48,8 @@ abstract class AbstractTicketParticipantsController extends CrudSubController
     public static $parentProperty = 'ticket';
     public static $exposeOnly     = ['get', 'list', 'post', 'delete'];
 
+    public static $serializeMethod = 'wrap';
+
     /**
      * {@inheritdoc}
      */
