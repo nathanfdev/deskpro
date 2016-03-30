b6cd2cb0bbcf3e1400f641dbb5109587144869e3
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-03-30 17:37:19 +0100

    Fix link

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/Parts/WidgetFooter.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/Parts/WidgetFooter.js
@@ -5,7 +5,7 @@ export class WidgetFooter extends React.Component {
   render() {
     return (
       <div className="dpdesignportal-powered-by-deskpro">
-        <a href="http://deskpro.com/" target="_blank">
+        <a href="https://www.deskpro.com/" target="_blank">
           <hr/> Support powered by <span className="dpdesignportal-deskpro-mark-logo"></span> <hr/>
         </a>
       </div>
