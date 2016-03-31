f30667beb6b400f0ff64a24d1658bbee883a799c
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-03-31 16:52:23 +0100

    Changes to portal settings, fixes enable/disable toggle on apps

--- app/BUILD/src/Application/DeskPRO/Settings/GeneralPortalSettings.php
+++ app/BUILD/src/Application/DeskPRO/Settings/GeneralPortalSettings.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -29,6 +29,7 @@
 /**
  * DeskPRO.
  */
+
 namespace Application\DeskPRO\Settings;
 
 class GeneralPortalSettings
@@ -167,12 +168,6 @@ class GeneralPortalSettings
             if ($set_settings['portal_mode'] != 'publish') {
                 foreach (array('apps_feedback', 'apps_kb', 'apps_news', 'apps_downloads') as $n) {
                     $set_settings[$n] = 0;
-                }
-            } else {
-                if (!($this->apps_downloads || $this->apps_feedback || $this->apps_kb || $this->apps_news)) {
-                    foreach (array('apps_feedback', 'apps_kb', 'apps_news', 'apps_downloads') as $n) {
-                        $set_settings[$n] = 1;
-                    }
                 }
             }
         }
