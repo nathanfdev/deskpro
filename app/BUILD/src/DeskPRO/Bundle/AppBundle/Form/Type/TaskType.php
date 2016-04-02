185a503f2249189908d012b9accab699c8744f66
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-04-02 07:46:20 +0300

    refactor labels type, get entities via entity labels property (prevent multiple repository calls)

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/TaskType.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/TaskType.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -30,6 +30,7 @@ namespace DeskPRO\Bundle\AppBundle\Form\Type;
 
 use DeskPRO\Bundle\AppBundle\Entity\LabelTask;
 use DeskPRO\Bundle\AppBundle\Entity\Task;
+use DeskPRO\Bundle\AppBundle\Form\Type\Labels\LabelsCollectionType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
@@ -118,7 +119,7 @@ class TaskType extends AbstractType
                 'class'    => 'App:TaskList',
                 'property' => 'title',
             ])
-            ->add('labels', 'api_labels_collection', [
+            ->add('labels', LabelsCollectionType::class, [
                 'labels_class'   => LabelTask::class,
                 'labels_owner'   => $builder->getData(),
                 'owner_property' => 'task',
