ca94eb6a1b85fbf0fcc4f2edd0fed1c13a3acfb1
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-04-05 18:34:28 +0300

    class names conflict fix

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/TaskType.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/TaskType.php
@@ -39,7 +39,7 @@ use DeskPRO\Bundle\AppBundle\Form\Type\Labels\LabelsCollectionType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType as CoreDateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -76,21 +76,21 @@ class TaskType extends AbstractType
                     Task::TYPE_EVENT => 'Event',
                 ],
             ])
-            ->add('date_due', DateTimeType::class, [
+            ->add('date_due', CoreDateTimeType::class, [
                 'required'    => false,
                 'widget'      => 'single_text',
                 'description' => 'the task due date',
             ])
-            ->add('date_done', DateTimeType::class, [
+            ->add('date_done', CoreDateTimeType::class, [
                 'required'    => false,
                 'widget'      => 'single_text',
                 'description' => 'the task done date',
             ])
-            ->add('date_event_start', DateTimeType::class, [
+            ->add('date_event_start', CoreDateTimeType::class, [
                 'required'    => false,
                 'description' => 'the event start datetime',
             ])
-            ->add('date_event_end', DateTimeType::class, [
+            ->add('date_event_end', CoreDateTimeType::class, [
                 'required'    => false,
                 'description' => 'the event end datetime',
             ])
