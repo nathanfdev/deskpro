28974852e22537cd269ac075b1a80bd7246410c0
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-04-06 20:27:08 +0300

    proper name in the widget chat feedback block, hide fake descriptions

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Feedback/RateAgentComplete.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Feedback/RateAgentComplete.js
@@ -5,9 +5,8 @@ export class RateAgentComplete extends React.Component {
   render() {
     return (
       <div className="dpdesignportal-agent-rating dpdesignportal-agent-rating-complete">
-        <i className="fa fa-check-circle-o"></i>
+        <i className="fa fa-check-circle-o" />
         <h1>Thank you for your feedback!</h1>
-        <p className="grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
       </div>
     );
   }
