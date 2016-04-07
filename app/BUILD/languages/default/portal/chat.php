a2679beac805c60fcc3f9d6dbecd86162870013f
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-04-07 10:25:04 +0300

    portal chat logs desc

--- app/BUILD/languages/default/portal/chat.php
+++ app/BUILD/languages/default/portal/chat.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -28,7 +28,7 @@
 
 return array(
     'portal.chat.chat_logs_header' => 'Chat Logs',
-    'portal.chat.chat_logs_desc'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip',
+    'portal.chat.chat_logs_desc'   => '',
     'portal.chat.list_date'        => 'Date',
     'portal.chat.list_agent'       => 'Agent',
     'portal.chat.list_chat'        => 'Chat',
