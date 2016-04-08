b46567c7b9bcba3a5d3217173689fe112ed0272d
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-04-08 08:54:51 +0300

    widget ticket module with new code style

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Ticket/Actions/ticketActions.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Ticket/Actions/ticketActions.js
@@ -7,13 +7,13 @@ export const setNewTicketFormContent = createAction('WIDGET_SET_NEW_TICKET_FORM_
 // Api actions
 export const loadTicketDisplayFields = createAction(
   'WIDGET_LOAD_TICKET_DISPLAY_FIELDS',
-  () => widgetApi.sendGet(`DP_API/tickets/display.js`, {...ajaxOptions, dataType: 'script'})
+  () => widgetApi.sendGet('DP_API/tickets/display.js', { ...ajaxOptions, dataType: 'script' })
 );
 
 export const loadNewTicketForm = createAction(
   'WIDGET_LOAD_NEW_TICKET_FORM',
   () => dispatch => {
-    const promise = widgetApi.sendGet(`DP_API/tickets/new`, {...ajaxOptions});
+    const promise = widgetApi.sendGet('DP_API/tickets/new', { ...ajaxOptions });
     promise.success(response => dispatch(setNewTicketFormContent(response.data)));
 
     return promise;
@@ -23,7 +23,7 @@ export const loadNewTicketForm = createAction(
 export const saveNewTicketForm = createAction(
   'WIDGET_SAVE_NEW_TICKET_FORM',
   params => dispatch => {
-    const promise = widgetApi.sendPost(`DP_API/tickets/new`, params, {...ajaxOptions});
+    const promise = widgetApi.sendPost('DP_API/tickets/new', params, { ...ajaxOptions });
     promise.then(
       response => dispatch(setNewTicketFormContent(response.data)),
       response => dispatch(setNewTicketFormContent(response.data.data))
