1da824cf8679c11465a10bfd69eb0385e41e3be4
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-04-12 18:24:02 +0100

    Init server info auth code in upgrade script

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
@@ -1671,4 +1671,8 @@ return array(
         'file'      => '/src/Application/InstallBundle/Upgrade/Build/new-agent/BuildNewAgent_0200_oldtables2.php',
         'classname' => 'Application\InstallBundle\Upgrade\Build\BuildNewAgent_0200_oldtables2',
     ),
+    1460678417 => array(
+        'file'      => '/src/Application/InstallBundle/Upgrade/Build/new-agent/BuildNewAgent_0900_post.php',
+        'classname' => 'Application\InstallBundle\Upgrade\Build\BuildNewAgent_0900_post',
+    ),
 );
