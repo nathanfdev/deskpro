72d8b957fb7811dc6186120a597590c61764003f
Julien Ducro <julien.ducro@deskpro.com>
2016-04-13 13:07:15 +0100

    Use QueryBuilder instead of string query Remove tickets assign to agent and tickets where agent is participating from portal search in both MySql and ES

--- app/BUILD/src/Application/DeskPRO/NewSearch/SearchEngine/UserSearchProxy.php
+++ app/BUILD/src/Application/DeskPRO/NewSearch/SearchEngine/UserSearchProxy.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -122,6 +122,7 @@ class UserSearchProxy implements UserSearchInterface
 
         return $this->dbs = new Mysql\UserSearch(
             $this->c->getDbRead('search.searcher.content'),
+            $this->c->getEm(),
             new Mysql\MysqlResultsTransformer($this->c->getEm())
         );
     }
