bbdeeb6ca674b8204351dbc006fbb9b45984257d
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-04-13 14:37:23 +0100

    Install default chat widget on portal during upgrade

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/new-agent/BuildNewAgent_0900_post.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/new-agent/BuildNewAgent_0900_post.php
@@ -32,6 +32,8 @@
 
 namespace Application\InstallBundle\Upgrade\Build;
 
+use DeskPRO\Bundle\AppBundle\Settings\WidgetSettingsResolver;
+use DeskPRO\Component\Util\RandUtils;
 use Orb\Util\Strings;
 
 class BuildNewAgent_0900_post extends AbstractBuild
@@ -45,5 +47,28 @@ class BuildNewAgent_0900_post extends AbstractBuild
             $this->out('Init server info auth code');
             $DP_ENV->getDatManager()->writeTxtFile('server_info_auth', Strings::random(30, Strings::CHARS_ALPHANUM_IU));
         }
+
+        $this->out('Enable chat widget by default on portal');
+        $db = $this->container->get('database_connection');
+        $db->delete('datastore', ['name' => 'widget.portal_brand_settings']);
+
+        // This is a default serialized WidgetBrandSettings object to enable the widget
+        // It's normally applied from the admin interface when the widget is enabled
+        $widgetSettingsBlob = base64_decode(trim(file_get_contents(__DIR__.'/data/WidgetBrandSettings.dat')));
+
+        $db->insert('datastore', [
+            'name' => 'widget.portal_brand_settings',
+            'data' => $widgetSettingsBlob,
+            'auth' => RandUtils::randomStringFormat('%15An'),
+        ]);
+
+        $db->executeUpdate('
+            INSERT INTO settings
+                (name, value)
+            VALUES
+                (?, ?)
+            ON DUPLICATE KEY UPDATE
+                value = VALUES(value)
+        ', [WidgetSettingsResolver::ENABLED_ON_PORTAL, '1']);
     }
 }
