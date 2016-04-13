e80333301edb5e1a528071653105c618df672e3b
Julien Ducro <julien.ducro@deskpro.com>
2016-04-13 09:31:08 +0100

    Fix Organisation tickets users sort

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/DataService/TicketsDataService.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/DataService/TicketsDataService.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -29,6 +29,7 @@
 /**
  * DeskPRO.
  */
+
 namespace DeskPRO\Bundle\AppBundle\DataService;
 
 use Application\DeskPRO\Entity\Person;
@@ -153,7 +154,7 @@ class TicketsDataService extends AbstractDataService
                         break;
 
                     case TicketFilter::SORT_USER:
-                        $qb->addOrderBy('t.person', $filter->getSortDirection());
+                        $qb->addOrderBy('p.name', $filter->getSortDirection());
                         break;
 
                     case TicketFilter::SORT_AGENT:
