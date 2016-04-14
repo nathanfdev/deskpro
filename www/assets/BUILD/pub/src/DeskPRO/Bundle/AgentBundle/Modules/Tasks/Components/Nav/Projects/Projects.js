5672e79a58265758cb8bfb9f221f4cbd47674787
n3b <neb802@gmail.com>
2016-04-14 16:11:29 +0200

    fix projects list styles

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Projects/Projects.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Projects/Projects.js
@@ -2,15 +2,15 @@ import React, { PropTypes } from 'react';
 import { Detached } from 'DeskPRO/Component/Positioned/Detached';
 import { Section, SectionHeader, ListItem } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/NavFrame';
 import { ClickOut } from 'DeskPRO/Component/ClickOut';
-import { ProjectFormContainer } from './ProjectForm/ProjectFormContainer';
+import { ProjectForm } from './ProjectForm/ProjectForm';
 import { ListItemContainer } from '../ListItemContainer';
 import Immutable from 'immutable';
 
 export class Projects extends React.Component {
 
   static propTypes = {
-    projects:         PropTypes.object.isRequired,
-    projectsCountMap: PropTypes.array.isRequired
+    projects: PropTypes.object.isRequired,
+    projectsCount: PropTypes.object.isRequired
   };
 
   constructor(props) {
@@ -21,67 +21,86 @@ export class Projects extends React.Component {
     };
   }
 
-  onNewProject = event => {
-    event.preventDefault();
-    this.setState({
-      project: Immutable.fromJS({})
-    });
-  };
+  /**
+   * hide project edit form
+   * @param nextProps
+   */
+  componentWillReceiveProps(nextProps) {
+    if (!this.state.project) return;
+    const id = this.state.project.get('id');
 
-  onEdit = project => {
-    this.setState({ project });
-  };
+    if (id) {
+      // on edit
+      if (nextProps.projects.get(id)) return;
+    } else {
+      // on create
+      if (nextProps.projects.size === this.props.projects.size) return;
+    }
 
-  onCloseEditing = () => {
     this.setState({
       project: null
     });
+  }
+
+  onEdit = (project, event) => {
+    event && event.preventDefault();
+    this.setState({
+      project: project
+    });
   };
 
-  renderProjects() {
-    const { projects, projectsCountMap } = this.props;
-
+  renderProject(countMap, project, index) {
     return (
-      <ul>
-        {projects.map((project, index) =>
-          <ListItemContainer
-            key={index}
-            urlHash={`project-${project.get('id')}-${project.get('title')}`}
-            listOptions={{ project: [project.get('id')] }}
-            >
-            <ListItem count={projectsCountMap[project.get('id')] || 0} onEdit={() => this.onEdit(project)}>
-              <div part="label">
-                <i className="fa fa-book" /> {project.get('title')}
-              </div>
-            </ListItem>
-          </ListItemContainer>
-        )}
-      </ul>
+      <ListItemContainer key={index}
+                         urlHash={`project-${project.get('id')}-${project.get('title')}`}
+                         listOptions={{project: [project.get('id')]}}>
+
+        <ListItem count={countMap[project.get('id')] || 0}
+                  onEdit={this.onEdit.bind(this, project)}>
+
+          <div part="label" className="section-list-title">
+            <i className="fa fa-book" />
+            <div className="cutted" >
+              {project.get('title')}
+            </div>
+          </div>
+        </ListItem>
+      </ListItemContainer>
     );
   }
 
   render() {
+    const { projects, projectsCount = [] } = this.props;
     const { project } = this.state;
+
+    const countMap = [];
+    projectsCount.forEach(projectCount => {
+      countMap[projectCount.get('project_id')] = parseInt(projectCount.get('tasks_count'), 10);
+    });
+
+    const renderProject = this.renderProject.bind(this, countMap);
 
     return (
       <Section>
         <SectionHeader>
           Projects &nbsp;
-          <a href="#" onClick={this.onNewProject}>
-            <i className="fa fa-plus" />
+          <a href="#" onClick={this.onEdit.bind(this, Immutable.fromJS({}))}>
+            <i className="fa fa-plus"/>
           </a>
         </SectionHeader>
 
-        {this.renderProjects()}
+        <ul>
+          {projects.map(renderProject)}
+        </ul>
 
-        <Detached
-          isOpen={this.state.project}
-          positionTarget={this}
-          positionAt="right+5 top-6"
-          collision="fit"
-          >
-          <ClickOut onClickOut={this.onCloseEditing}>
-            <ProjectFormContainer project={project} />
+        <Detached isOpen={this.state.project}
+                  positionTarget={this}
+                  positionAt="right+5 top-6"
+                  collision="fit">
+
+          <ClickOut onClickOut={this.onEdit.bind(this, null)}>
+            <ProjectForm project={project}
+                         tasksCount={project && project.get('id') && countMap[project.get('id')] || 0}/>
           </ClickOut>
         </Detached>
       </Section>
