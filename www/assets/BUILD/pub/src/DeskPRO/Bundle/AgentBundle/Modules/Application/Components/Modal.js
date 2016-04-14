ae1672b3b3253ffbb47ff1f9ad19a8b4d77ce52b
n3b <neb802@gmail.com>
2016-04-14 16:12:02 +0200

    new notification type

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Modal.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Modal.js
@@ -80,9 +80,8 @@ export class Modal extends React.Component {
     this.cancelClick(event);
   };
 
-  render() {
-    const { title, content, confirmVisible, cancelVisible, isMini, children } = this.props;
-    const { isOpen } = this.state;
+  renderBody() {
+    const { title, confirmVisible, cancelVisible, isMini, children } = this.props;
 
     let className = 'dpw--modal';
     if (isMini) {
@@ -98,25 +97,31 @@ export class Modal extends React.Component {
     }
 
     return (
-      <Detached>
-        {isOpen &&
-        <div className="cover" onClick={this.coverClick} style={style}>
-          <section className={className}>
-            <header>
-              <h1>
-                {title}
-              </h1>
-              <div className="controls">
-                <a href="#" onClick={this.cancelClick}><i className="fa fa-times"></i></a>
-              </div>
-            </header>
-            <div className={(isMini && 'mini-') + 'popup-content'}>
-              {children}
+      <div className="dpw-site-cover" onClick={this.coverClick} style={style}>
+        <section className={className}>
+          <header>
+            <h1>
+              {title}
+            </h1>
+            <div className="controls">
+              <a href="#" onClick={this.cancelClick}><i className="fa fa-times"></i></a>
             </div>
-            {this.renderFooter()}
-          </section>
-        </div>
-        }
+          </header>
+          <div className={(isMini && 'mini-') + 'popup-content'}>
+            {children}
+          </div>
+          {this.renderFooter()}
+        </section>
+      </div>
+    );
+  }
+
+  render() {
+    const { isOpen } = this.state;
+
+    return (
+      <Detached>
+        {isOpen ? this.renderBody() : null}
       </Detached>
     );
   }
--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Projects/ProjectForm/ProjectForm.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tasks/Components/Nav/Projects/ProjectForm/ProjectForm.js
@@ -3,9 +3,8 @@ import { createProject, editProject, deleteProject } from '../../../../Actions/n
 import { FieldErrors } from 'DeskPRO/Component/Form/FormErrors';
 import Immutable from 'immutable';
 import Loader from 'react-loader';
-import { FieldGroup, Popup } from '../../../../../Common/Components/Popup/index';
-import { AgentsListContainer, TeamsListContainer, DepartmentsListContainer }
-  from '../../../../../Common/Components/Form/Lists/index';
+import { FieldGroup, Popup, CollectionField } from '../../../../../Common/Components/Popup';
+import { AssignAgentContainer } from '../../../../../Common/Components/Form/AssignAgentContainer';
 import {
   BaseForm,
   Header,
@@ -13,16 +12,16 @@ import {
   FloatField,
   ShowOnlySelected,
   Unassign
-} from '../../../Form/index';
+} from '../../../Form';
 import { QuickFilter } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Form/QuickFilter';
-import { Modal } from 'DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Modal';
+import { Notification } from 'DeskPRO/Bundle/AgentBundle/Modules/Application/Components/Notification';
 
 export class ProjectForm extends BaseForm {
 
   static propTypes = {
-    project:    PropTypes.object,
+    project: PropTypes.object,
     tasksCount: PropTypes.number,
-    dispatch:   PropTypes.func.isRequired
+    dispatch: PropTypes.func.isRequired
   };
 
   constructor(props) {
@@ -35,11 +34,11 @@ export class ProjectForm extends BaseForm {
     this.state = {
       ...localState,
 
-      title:            project.get('title'),
+      title: project.get('title'),
       showOnlySelected: false,
-      agents:           project.get('agents', emptyObject).toArray(),
-      agentTeams:       project.get('teams', emptyObject).toArray(),
-      departments:      project.get('departments', emptyObject).toArray()
+      agents: project.get('agents', emptyObject),
+      agentTeams: project.get('teams', emptyObject),
+      departments: project.get('departments', emptyObject)
     };
   }
 
@@ -68,10 +67,10 @@ export class ProjectForm extends BaseForm {
     const { project, dispatch } = this.props;
     const isNew = !project.get('id');
     const submitData = {
-      title:       this.state.title,
-      departments: this.state.departments,
-      teams:       this.state.agentTeams,
-      agents:      this.state.agents
+      title: this.state.title,
+      departments: this.state.departments.toArray(),
+      teams: this.state.agentTeams.toArray(),
+      agents: this.state.agents.toArray()
     };
 
     let promise;
@@ -83,17 +82,13 @@ export class ProjectForm extends BaseForm {
 
     promise.then(
       () => {
-        if (!this.unmounted) {
-          this.setState({ submit: false });
-        }
+        !this.unmounted && this.setState({ submit: false });
       },
-      result => {
-        if (!this.unmounted) {
-          this.setState({
-            errors: result.getData().errors,
-            submit: false
-          });
-        }
+        result => {
+        !this.unmounted && this.setState({
+          errors: result.getData().errors,
+          submit: false
+        });
       }
     );
   };
@@ -106,12 +101,10 @@ export class ProjectForm extends BaseForm {
     this.setState({ submit: true });
     const { project, dispatch } = this.props;
     dispatch(deleteProject(project.get('id'))).catch((result) => {
-      if (!this.unmounted) {
-        this.setState({
-          errors: result.getData().errors,
-          submit: false
-        });
-      }
+      !this.unmounted && this.setState({
+        errors: result.getData().errors,
+        submit: false
+      });
     });
   };
 
@@ -129,79 +122,63 @@ export class ProjectForm extends BaseForm {
           <div className="dpw--popup-content">
             <FieldGroup>
               <FullField title="Title">
-                <input
-                  name="title"
-                  type="text"
-                  placeholder="Title"
-                  value={this.state.title}
-                  onChange={this.onChangeTitle}
-                  />
+                <input name="title"
+                       type="text"
+                       placeholder="Title"
+                       value={this.state.title}
+                       onChange={this.onChangeTitle}/>
 
-                <FieldErrors errors={this.state.errors} name="title" />
+                <FieldErrors errors={this.state.errors} name="title"/>
               </FullField>
             </FieldGroup>
 
             <FieldGroup>
               <FloatField align="left">
-                <QuickFilter value={this.state.quickFilter} onChange={this.onChangeQuickFilter} />
+                <QuickFilter value={this.state.quickFilter}
+                             onChange={this.onChangeQuickFilter}/>
               </FloatField>
 
               <FloatField align="right">
-                <ShowOnlySelected value={this.state.showOnlySelected} onChange={this.onChangeFilterSelected} />
-                <Unassign onClick={this.onUnassignAll} />
+                <ShowOnlySelected value={this.state.showOnlySelected}
+                                  onChange={this.onChangeFilterSelected}/>
+                <Unassign onClick={this.onUnassignAll}/>
               </FloatField>
             </FieldGroup>
 
             <FieldGroup>
-              <AgentsListContainer
-                multiple
-                selfAssign={this.onAssignSelf}
-                selected={this.state.agents}
-                showOnlySelected={this.state.showOnlySelected}
-                filter={this.state.quickFilter}
-                onClick={value => this.onChange('agents', value)}
-                />
-              <TeamsListContainer
-                multiple
-                selected={this.state.agentTeams}
-                showOnlySelected={this.state.showOnlySelected}
-                filter={this.state.quickFilter}
-                onClick={value => this.onChange('agentTeams', value)}
-                />
-              <DepartmentsListContainer
-                multiple
-                selected={this.state.departments}
-                showOnlySelected={this.state.showOnlySelected}
-                filter={this.state.quickFilter}
-                onClick={value => this.onChange('departments', value)}
-                />
+              <AssignAgentContainer selected={this.state.agents.toSet()}
+                                    showOnlySelected={this.state.showOnlySelected}
+                                    filter={this.state.quickFilter}
+                                    onChange={this.onChange.bind(this, 'agents')} />
+
             </FieldGroup>
 
             <FieldGroup>
               <FullField>
-                {!this.state.submit &&
-                  <button type="submit" value="Save" className="dpw--popup-button" onClick={this.onSubmit}>
-                    Save
-                  </button>
+                {!this.state.submit && <button type="submit"
+                                               value="Save"
+                                               className="dpw--popup-button"
+                                               onClick={this.onSubmit}>
+                  Save
+                </button>
                 }
-                {!isNew && !this.state.submit &&
-                  <button type="button" className="dpw--popup-button" onClick={this.onDeletePrompt}>
-                    Delete
-                  </button>
+                {!isNew && !this.state.submit && <button type="button"
+                                                         className="dpw--popup-button"
+                                                         onClick={this.onDeletePrompt}>
+                  Delete
+                </button>
                 }
-                {!isNew &&
-                  <Modal
-                    ref="deleteModal"
-                    title="Delete project?"
-                    onConfirm={this.onDeleteConfirm}
-                    confirmTitle="Delete"
-                    >
-                    Are you sure you want to delete "{project.get('title')}"?
-                    <br />
-                    {tasksCount > 0 && `All (${tasksCount}) tasks will be deleted too!`}
-                  </Modal>
+                {!isNew && <Notification ref="deleteModal"
+                                  title="Delete this project?"
+                                  onConfirm={this.onDeleteConfirm}>
+                  Are you sure you want to delete "{project.get('title')}"?
+                  <br />
+                  {tasksCount > 0 && `All (${tasksCount}) tasks will be deleted too!`}
+                </Notification>
                 }
-                <Loader opacity={0} width={3} loaded={!this.state.submit} />
+                <Loader opacity={0}
+                        width={3}
+                        loaded={!this.state.submit}/>
               </FullField>
             </FieldGroup>
           </div>
