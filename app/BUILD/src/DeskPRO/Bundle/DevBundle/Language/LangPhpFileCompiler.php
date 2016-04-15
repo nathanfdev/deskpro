532a4c639d0fd073b1bd31cc8e95f32536316ce5
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-04-15 10:57:09 +0100

    Add dpdev:lang:onesky:download command

--- app/BUILD/src/DeskPRO/Bundle/DevBundle/Language/LangPhpFileCompiler.php
+++ app/BUILD/src/DeskPRO/Bundle/DevBundle/Language/LangPhpFileCompiler.php
@@ -33,6 +33,22 @@ use DeskPRO\Component\Util\MapUtils;
 class LangPhpFileCompiler
 {
     /**
+     * Given a phrase name, get the filename it should be included in.
+     *
+     * Example: foo.bar.baz -> foo/bar.php
+     *
+     * @param string $phraseName
+     *
+     * @return string
+     */
+    public function getFilenameFromPhraseName($phraseName)
+    {
+        $parts = explode('.', $phraseName, 3);
+
+        return $parts[0].'/'.$parts[1].'.php';
+    }
+
+    /**
      * @param array $phrases
      *
      * @return string
@@ -42,9 +58,22 @@ class LangPhpFileCompiler
         $phrases = MapUtils::filterOutValues($phrases, ['', false, null], true);
         ksort($phrases, \SORT_STRING);
 
-        $php = array("<?php return array(\n");
-        foreach ($phrases as $phrase_id => $string) {
-            $php[] = sprintf("\t%-70s => %s,\n", "'$phrase_id'", var_export($string, true));
+        $maxLen = 0;
+        foreach ($phrases as $phraseId => $string) {
+            $maxLen = max($maxLen, strlen($phraseId));
+        }
+
+        $maxLen += 2;
+
+        $php   = ["<?php\n\n"];
+        $php[] = $this->getFileHeader();
+        $php[] = "\n\n";
+        $php[] = "return array(\n";
+
+        ksort($phrases, \SORT_STRING);
+
+        foreach ($phrases as $phraseId => $string) {
+            $php[] = sprintf("    %-{$maxLen}s => %s,\n", "'$phraseId'", var_export($string, true));
         }
 
         $php[] = ");\n";
@@ -52,5 +81,36 @@ class LangPhpFileCompiler
         $php = implode('', $php);
 
         return $php;
+    }
+
+    private function getFileHeader()
+    {
+        return <<<'HEADER'
+/*
+ * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * a British company located in London, England.
+ *
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
+ *
+ * The license agreement under which this software is released
+ * can be found at https://www.deskpro.com/eula/
+ *
+ * By using this software, you acknowledge having read the license
+ * and agree to be bound thereby.
+ *
+ * Please note that DeskPRO is not free software. We release the full
+ * source code for our software because we trust our users to pay us for
+ * the huge investment in time and energy that has gone into both creating
+ * this software and supporting our customers. By providing the source code
+ * we preserve our customers' ability to modify, audit and learn from our
+ * work. We have been developing DeskPRO since 2001, please help us make it
+ * another decade.
+ *
+ * Like the work you see? Think you could make it better? We are always
+ * looking for great developers to join us: http://www.deskpro.com/jobs/
+ *
+ * ~ Thanks, Everyone at Team DeskPRO
+ */
+HEADER;
     }
 }
