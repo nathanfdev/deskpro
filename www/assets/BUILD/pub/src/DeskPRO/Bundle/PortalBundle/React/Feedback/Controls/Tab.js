a5b5bb5048f0f1c235cb48274a19752525e0da44
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-04-15 16:24:13 +0100

    Click/touch events on feedback nav

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/React/Feedback/Controls/Tab.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/React/Feedback/Controls/Tab.js
@@ -15,36 +15,47 @@ export class Tab extends React.Component {
   }
 
   componentDidMount() {
-    if ("ontouchstart" in document.documentElement) {
-      if (this.refs.quickJump) {
-        const $el = $(this.refs.quickJump);
+    if (this.refs.quickJump) {
+      const $el = $(this.refs.quickJump);
 
-        const widget = new TouchFocusWidget($el);
-        widget.render();
-      }
+      const widget = new TouchFocusWidget($el);
+      widget.render();
     }
   }
 
-  render() {
+  renderActive() {
     let dropdownCats = this.props.available.getStatusCategoriesForStatus(this.props.id);
     let canRenderDropdown = dropdownCats.length > 0;
     return (
+      <div className={"quick-jump" + (canRenderDropdown ? "" : " no-dropdown")} ref="quickJump">
+        <span className={this.props.active ? "link active" : "link"}>
+          {this.props.label}
+          {this.renderActiveCats()}
+          {canRenderDropdown ? <span><i className="fa fa-caret-down"></i></span> : null}
+        </span>
+        {canRenderDropdown ? this.renderDropdown(dropdownCats) : null}
+      </div>
+    );
+  }
+
+  renderInactive() {
+    return (
+      <div className={"quick-jump no-dropdown"} ref="quickJump">
+        <a href={'/feedback/browse/' + this.props.id}
+           className={this.props.active ? "active" : ""}
+           onClick={this.clickTab.bind(this)}
+           onTouchStart={this.clickTab.bind(this)}
+        >
+          {this.props.label}
+        </a>
+      </div>
+    );
+  }
+
+  render() {
+    return (
       <li>
-        <div className={"quick-jump" + (canRenderDropdown ? "" : " no-dropdown")} ref="quickJump">
-          <a href={'/feedback/browse/' + this.props.id}
-             className={this.props.active ? "active" : ""}
-             onClick={this.clickTab.bind(this)}
-             onTouchStart={this.clickTab.bind(this)}
-            >
-            {this.props.label}
-            {this.renderActiveCats()}
-            {canRenderDropdown
-              ? <span><i className="fa fa-caret-down"></i></span>
-              : null
-            }
-          </a>
-          {canRenderDropdown ? this.renderDropdown(dropdownCats) : null}
-        </div>
+          {this.props.active ? this.renderActive() : this.renderInactive()}
       </li>
     );
   }
@@ -62,7 +73,7 @@ export class Tab extends React.Component {
 
     let avail = this.props.available.getStatusCategoriesForStatus(this.props.id);
 
-    if (cat_titles.length > 0) {
+    if (cat_titles.length > 0 && cat_titles.length !== avail.length) {
       return (<small> ({cat_titles.length}/{avail.length})</small>);
     }
   }
