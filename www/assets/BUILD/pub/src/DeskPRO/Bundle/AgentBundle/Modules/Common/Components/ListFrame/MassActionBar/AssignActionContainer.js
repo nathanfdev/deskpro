ea961709f986546c65237a3ebda8ca863214b8ab
A. Yakutskiy <a.yakutskiy@gmail.com>
2016-04-18 20:20:11 +0300

    unassign tickets mass action

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/MassActionBar/AssignActionContainer.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/MassActionBar/AssignActionContainer.js
@@ -17,15 +17,26 @@ export class AssignActionContainer extends Component {
     currentParams:     PropTypes.object
   };
 
-  onClick(param, value) {
+  onClick = (param, value) => {
     const { setParams, dispatch, currentParams, resetSingleAction } = this.props;
 
     if (currentParams.get('assign') && currentParams.get('assign').get(param) === value) {
       dispatch(resetSingleAction('assign'));
     } else {
+      const setOfActions = currentParams.get('set_of_actions') ? currentParams.get('set_of_actions').toArray() : [];
+      setOfActions.splice(setOfActions.indexOf('unassign'), 1);
       dispatch(setParams({ assign: { [param]: value } }));
     }
-  }
+  };
+
+  unAssign = (e) => {
+    e.preventDefault();
+    const { setParams, dispatch, resetSingleAction, currentParams } = this.props;
+    const setOfActions = currentParams.get('set_of_actions') ? currentParams.get('set_of_actions').toArray() : [];
+    dispatch(resetSingleAction('assign'));
+    setOfActions.push('unassign');
+    dispatch(setParams({ set_of_actions: [... new Set(setOfActions)] }));
+  };
 
   render() {
     const { currentParams } = this.props;
@@ -36,14 +47,32 @@ export class AssignActionContainer extends Component {
         <Popup additionalClassNames="assign-form">
           <form>
             <div className="dpw--popup-content">
+              <div className="dpw--popup-content-line">
+                <div className="dpw--popup-content-right">
+                  <div className="dpw-popup-content-item">
+                    <div className="dpw-popup-content-item-unassign-all">
+                      <a href="#" className="checkbox-link" onClick={this.unAssign}>
+                        <span>Unassign All</span>
+                        <span className="unassign-all-icon"><span></span></span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="dpw--popup-item-collection">
                 <FieldGroup>
-                  <AgentsListContainer selected={assign && assign.get('agent')}
-                                       onClick={this.onClick.bind(this)}/>
-                  <TeamsListContainer selected={assign && assign.get('team')}
-                                      onClick={this.onClick.bind(this)}/>
-                  <DepartmentsListContainer selected={assign && assign.get('department')}
-                                            onClick={this.onClick.bind(this)}/>
+                  <AgentsListContainer
+                    selected={assign && assign.get('agent')}
+                    onClick={this.onClick}
+                    />
+                  <TeamsListContainer
+                    selected={assign && assign.get('team')}
+                    onClick={this.onClick}
+                    />
+                  <DepartmentsListContainer
+                    selected={assign && assign.get('department')}
+                    onClick={this.onClick}
+                    />
                 </FieldGroup>
               </div>
             </div>
