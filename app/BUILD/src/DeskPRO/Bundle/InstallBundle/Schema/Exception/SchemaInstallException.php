bffa27376c785d35b16da4ddba396d3893e56937
ImmortaL <denis@ranneft.ru>
2016-04-19 23:46:28 +0300

    Exception could accept only integer error codes

--- app/BUILD/src/DeskPRO/Bundle/InstallBundle/Schema/Exception/SchemaInstallException.php
+++ app/BUILD/src/DeskPRO/Bundle/InstallBundle/Schema/Exception/SchemaInstallException.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -37,7 +37,7 @@ class SchemaInstallException extends \Exception
 
     public function __construct($query, $message, $code, \Exception $previous = null)
     {
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, (int) $code, $previous);
         $this->query = $query;
     }
 
