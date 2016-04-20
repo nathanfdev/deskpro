7833a2001ba59a9f6724903f39884b633a30b54b
A. Yakutskiy <a.yakutskiy@gmail.com>
2016-04-20 13:50:35 +0300

    Ticket stars

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/DataFixtures/DevFixtures/TicketsFixture.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/DataFixtures/DevFixtures/TicketsFixture.php
@@ -33,6 +33,7 @@ namespace DeskPRO\Bundle\AppBundle\DataFixtures\DevFixtures;
 
 use Application\DeskPRO\Entity\LabelDef;
 use Application\DeskPRO\Entity\Ticket;
+use Application\DeskPRO\Entity\TicketFlagged;
 use Application\DeskPRO\Entity\TicketSla;
 use DeskPRO\Bundle\AppBundle\DataFixtures\DeskProAbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
@@ -153,6 +154,7 @@ class TicketsFixture extends DeskProAbstractFixture implements OrderedFixtureInt
         $this->loadTicketProps();
         $this->loadTicketSlas();
         $this->setParentTicket();
+        $this->ticketStars();
     }
 
     private function initIds()
@@ -590,5 +592,32 @@ class TicketsFixture extends DeskProAbstractFixture implements OrderedFixtureInt
                 );
             }
         }
+    }
+
+    private function ticketStars()
+    {
+        $batch  = [];
+        $colors = [
+            TicketFlagged::STAR_BLUE,
+            TicketFlagged::STAR_GREEN,
+            TicketFlagged::STAR_ORANGE,
+            TicketFlagged::STAR_PINK,
+            TicketFlagged::STAR_PURPLE,
+            TicketFlagged::STAR_RED,
+            TicketFlagged::STAR_YELLOW,
+        ];
+        foreach ($this->agent_ids as $agentId) {
+            foreach ($colors as $color) {
+                $numTickets = $this->faker->numberBetween(1, 30);
+                for ($x = 0; $x < $numTickets; ++$x) {
+                    $batch[] = [
+                        'person_id' => $agentId,
+                        'ticket_id' => $this->faker->randomElement($this->ticket_ids),
+                        'color'     => $color,
+                    ];
+                }
+            }
+        }
+        $this->db->batchInsert('tickets_flagged', $batch, true);
     }
 }
