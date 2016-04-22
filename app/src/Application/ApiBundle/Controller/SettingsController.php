414762970353b77e74f82b62228351741dfda463
n3b <neb802@gmail.com>
2016-04-22 14:55:59 +0200

    [FIX] update min_length in password policy

--- app/src/Application/ApiBundle/Controller/SettingsController.php
+++ app/src/Application/ApiBundle/Controller/SettingsController.php
@@ -29,6 +29,7 @@
 /**
  * DeskPRO.
  */
+
 namespace Application\ApiBundle\Controller;
 
 use Application\ApiBundle\Form\CustomField\Type\PersonStartType;
@@ -443,7 +444,7 @@ class SettingsController extends AbstractController implements ProtectedControll
         $rate_limit_settings->setArray($this->in->getArrayValue('rate_limit_settings'));
         $rate_limit_settings->saveSettings();
 
-        return $this->createSuccessResponse();
+        return $this->passwordSettingsAction();
     }
 
     ############################################################################
