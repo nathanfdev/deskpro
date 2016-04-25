20e6463eab9c349e5422a1bb8a4bd5e6cc461ee0
Julien Ducro <julien.ducro@deskpro.com>
2016-04-25 16:26:25 +0100

    Revert "Fix missing template value on feedback browse"
    This reverts commit 6d33bb175e302fb1f244f5d75afccb22bbac55e5.

--- app/BUILD/src/DeskPRO/Bundle/PortalBundle/Controller/FeedbackController.php
+++ app/BUILD/src/DeskPRO/Bundle/PortalBundle/Controller/FeedbackController.php
@@ -328,17 +328,6 @@ class FeedbackController extends AbstractController
         $breadcrumbs = $this->getBreadcrumbGenerator()->buildFeedback();
 
         //
-        // SUBSCRIPTION
-        //
-        $isSubscribed = false;
-        if (
-        $this->getBrandSetting('user.feedback_subscriptions', false)
-        ) {
-            // waiting info regarding article category subscriptions
-            $isSubscribed = $this->getSubscriptionsHelper()->isSubscribedRootCategory('feedback', $this->getUser());
-        }
-
-        //
         // FILTER CATEGORIES
         //
         $feedback_types = $this->get('data.feedback')->getFeedbackCategoriesForPerson($person);
@@ -358,7 +347,6 @@ class FeedbackController extends AbstractController
             'page_title'        => $this->createPageTitle()->feedback(),
             'filter_js'         => $filter_js,
             'rerendering_saved' => false, // wont happen here because we always rerender on index
-            'is_subscribed'     => $isSubscribed,
         ];
 
         if ($request->isXmlHttpRequest()) {
