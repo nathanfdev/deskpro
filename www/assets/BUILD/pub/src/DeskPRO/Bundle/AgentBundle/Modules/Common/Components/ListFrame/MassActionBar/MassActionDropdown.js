a5d33468a3c5831def65b44d16836f5aade187cf
A. Yakutskiy <a.yakutskiy@gmail.com>
2016-04-26 09:14:31 +0300

    cs-fix

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/MassActionBar/MassActionDropdown.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/MassActionBar/MassActionDropdown.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { Detached } from 'DeskPRO/Component/Positioned/Detached';
 import { Button } from './Button';
 import { ClickOut } from 'DeskPRO/Component/ClickOut';
-import { SingleChoicePanelContainer } from 'DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Form/SingleChoicePanelContainer';
+import { SingleChoicePanelContainer } from '../../../../Common/Components/Form/SingleChoicePanelContainer';
 import { setMassActionsParams, resetParam } from '../../../../Application/Actions/massActions';
 import { ActionMenuContainer } from './ActionMenuContainer';
 import { MultipleActionChoiceContainer } from './MultipleActionChoiceContainer';
@@ -11,10 +11,10 @@ import { SetDateAction } from './SetDateAction';
 
 export class MassActionDropdown extends Component {
   static propTypes = {
-    isActive: PropTypes.bool,
+    isActive:      PropTypes.bool,
     currentParams: PropTypes.object,
-    id: PropTypes.number.isRequired,
-    item: PropTypes.object.isRequired
+    id:            PropTypes.number.isRequired,
+    item:          PropTypes.object.isRequired
   };
 
   componentWillMount() {
@@ -27,49 +27,70 @@ export class MassActionDropdown extends Component {
     event.preventDefault();
     this.setState({ expanded: !this.state.expanded });
   };
+
   collapse = () => this.setState({ expanded: false });
 
   renderPanel = (item) => {
     const { currentParams } = this.props;
     if (item.type === 'set_action') {
       return (
-        <SingleChoicePanelContainer item={item}
-                                    currentParams={currentParams}
-                                    setParams={setMassActionsParams}
-                                    resetSingleAction={resetParam}/>
+        <SingleChoicePanelContainer
+          item={item}
+          currentParams={currentParams}
+          setParams={setMassActionsParams}
+          resetSingleAction={resetParam}
+          />
       );
     } else if (item.type === 'select_action') {
       return (
-        <MultipleActionChoiceContainer item={item}
-                                       setParams={setMassActionsParams}
-                                       currentParams={currentParams}
-                                       resetSingleAction={resetParam}/>
+        <MultipleActionChoiceContainer
+          item={item}
+          setParams={setMassActionsParams}
+          currentParams={currentParams}
+          resetSingleAction={resetParam}
+          />
       );
     } else if (item.type === 'assign_action') {
       return (
-        <AssignActionContainer setParams={setMassActionsParams}
-                               currentParams={currentParams}
-                               resetSingleAction={resetParam}/>
+        <AssignActionContainer
+          setParams={setMassActionsParams}
+          currentParams={currentParams}
+          resetSingleAction={resetParam}
+          />
       );
     } else if (item.type === 'menu') {
       return (
-        <ActionMenuContainer options={item.options}
-                             setParams={setMassActionsParams}
-                             currentParams={currentParams}
-                             resetSingleAction={resetParam}/>
+        <ActionMenuContainer
+          options={item.options}
+          setParams={setMassActionsParams}
+          currentParams={currentParams}
+          resetSingleAction={resetParam}
+          />
       );
     } else if (item.type === 'set_date') {
       return (
-        <SetDateAction param={item.param}
-                       setParams={setMassActionsParams}
-                       currentParams={currentParams}
-                       resetSingleAction={resetParam}/>
+        <SetDateAction
+          param={item.param}
+          setParams={setMassActionsParams}
+          currentParams={currentParams}
+          resetSingleAction={resetParam}
+          />
+      );
+    } else if (item.type === 'html_reply') {
+      return (
+        <HtmlReplyActionContainer
+          param={item.param}
+          setParams={setMassActionsParams}
+          currentParams={currentParams}
+          resetSingleAction={resetParam}
+          />
       );
     }
+    return null;
   };
 
   render() {
-    const {id, item, currentParams } = this.props;
+    const { id, item, currentParams } = this.props;
     const checkIfButtonHasValue = () => {
       if (!currentParams) {
         return false;
@@ -99,18 +120,24 @@ export class MassActionDropdown extends Component {
 
     return (
       <li>
-        <Button isActive={this.state.expanded}
-                hasValue={!this.state.expanded && checkIfButtonHasValue()}
-                ref={'button' + id}
-                label={item.label}
-                icon={item.icon}
-                onClick={this.toggleExpanded}/>
-        <Detached isOpen={this.state.expanded}
-                  positionAt="left bottom"
-                  positionTarget={this.refs['button' + id]}>
-          <ClickOut onClickOut={this.collapse}
-                    ignoreNodes={[this.refs.menuItem, '.dpw-navigation-dropdown-panel']}
-                    additionalNodes={['.dpw-navigation-dropdown-item-clear']}>
+        <Button
+          isActive={this.state.expanded}
+          hasValue={!this.state.expanded && checkIfButtonHasValue()}
+          ref={`button${id}`}
+          label={item.label}
+          icon={item.icon}
+          onClick={this.toggleExpanded}
+          />
+        <Detached
+          isOpen={this.state.expanded}
+          positionAt="left bottom"
+          positionTarget={this.refs[`button${id}`]}
+          >
+          <ClickOut
+            onClickOut={this.collapse}
+            ignoreNodes={[this.refs.menuItem, '.dpw-navigation-dropdown-panel']}
+            additionalNodes={['.dpw-navigation-dropdown-item-clear']}
+            >
             {this.renderPanel(item)}
           </ClickOut>
         </Detached>
--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/MassActionBar/SetDateAction.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/MassActionBar/SetDateAction.js
@@ -5,25 +5,28 @@ import { connect } from 'react-redux';
 @connect()
 export class SetDateAction extends Component {
   static propTypes = {
-    dispatch: PropTypes.func.isRequired,
+    dispatch:      PropTypes.func.isRequired,
     currentParams: PropTypes.object,
-    setParams: PropTypes.func.isRequired,
-    param: PropTypes.string.isRequired
+    setParams:     PropTypes.func.isRequired,
+    param:         PropTypes.string.isRequired
   };
 
   render() {
-    const {dispatch, currentParams, setParams, param} = this.props;
+    const { dispatch, currentParams, setParams, param } = this.props;
     return (
       <div
-        className="dpw-navigation-dropdown-panel dpw-navigation-date-picker-panel dpw-navigation-dropdown-panel-corner-left">
+        className="dpw-navigation-dropdown-panel dpw-navigation-date-picker-panel dpw-navigation-dropdown-panel-corner-left"
+        >
         <div className="dpw-date-picker">
 
           <div className="dpw-date-picker-panel-container">
             <form>
-              <DateTimePicker label="From"
-                              className="dpw-date-picker-left"
-                              value={currentParams.get(param)}
-                              onChange={value => dispatch(setParams({[param]: value}))}/>
+              <DateTimePicker
+                label="From"
+                className="dpw-date-picker-left"
+                value={currentParams.get(param)}
+                onChange={value => dispatch(setParams({ [param]: value }))}
+                />
             </form>
           </div>
         </div>
