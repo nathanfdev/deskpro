b01fe2bbc81f7a857ba9aa5e524fe55de072ce07
A. Yakutskiy <a.yakutskiy@gmail.com>
2016-04-27 10:49:14 +0300

    UI for mass-reply

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/MassActionBar/HtmlReplyActionContainer.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/MassActionBar/HtmlReplyActionContainer.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import classNames from 'classnames';
 import { RteEditor } from 'DeskPRO/Component/Rte/RteEditor';
-import { Popup } from '../../../../Common/Components/Popup';
 
 @connect()
 
@@ -13,27 +13,61 @@ export class HtmlReplyActionContainer extends Component {
     currentParams:     PropTypes.object
   };
 
-  render() {
+  componentWillMount() {
     const { currentParams } = this.props;
-    const message = currentParams ? currentParams.get('message') : '';
+    const message     = currentParams ? currentParams.get('message') : '';
+    const isAgentNote = currentParams ? currentParams.get('isAgentNote') : false;
+
+    this.state = { message, isAgentNote };
+  }
+
+  onChangeMessage = (text) => {
+    this.setState({ message: text });
+  };
+
+  onReset = (event) => {
+    event.preventDefault();
+    const { dispatch, resetSingleAction } = this.props;
+    dispatch(resetSingleAction('reply'));
+    this.setState({ message: '', isAgentNote: false });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    const { dispatch, setParams } = this.props;
+    dispatch(setParams({ reply: { message: this.state.message, isAgentNote: this.state.isAgentNote } }));
+  };
+
+  setIsAgentNote = (event) => {
+    event.preventDefault();
+    this.setState({ isAgentNote: !this.state.isAgentNote });
+  };
+
+  render() {
     return (
-      <div className="dpw-navigation-dropdown-panel">
-        <Popup>
-          <form>
-            <div className="dpw--popup-content">
-              <div className="dpw--popup-content-line">
+      <div className="dpw-navigation-dropdown-panel" style={{ width: '440px' }}>
+        <div className="dpw-navigation-dropdown-panel-content">
+          <div className="dpw-navigation-dropdown-panel-content-line">
+            <div className="dpw-navigation-dropdown-panel-content-full">
+              <div className="dpw-navigation-dropdown-reply-controls">
+                <a href="#" className="add-agent-note" onClick={this.setIsAgentNote}>
+                  <span className="dpw--checkbox-boxy">
+                    <i className={classNames('fa', { 'fa-check': this.state.isAgentNote })} />
+                    </span>
+                  <span className="control-text">Agent Note</span>
+                </a>
+              </div>
+              <div className="dpw-navigation-dropdown-reply-item-reply">
                 <RteEditor
                   inline
-                  ref="editor"
-                  value={message}
+                  value={this.state.message}
                   onChange={this.onChangeMessage}
-                  onSubmit={this.onSubmit}
                   className="textarea"
                   onPasteImage={this.onPasteImage}
                   options={{
                     autoLink:      true,
                     imageDragging: true,
-                    placeholder:   { text: 'Send a message' },
+                    placeholder:   { text: 'Type a message' },
                     toolbar:       {
                       buttons:                ['bold', 'italic', 'underline', 'anchor'],
                       updateOnEmptySelection: true
@@ -46,17 +80,16 @@ export class HtmlReplyActionContainer extends Component {
                   }}
                 />
               </div>
-              <div className="dpw--popup-content-line">
-                <div className="dpw--popup-content-left">
-                  <a href="#" className="dpw--popup-button">OK</a>
-                </div>
-                <div className="dpw--popup-content-left">
-                  <a href="#" className="dpw--popup-button">Reset</a>
-                </div>
+              {this.state.message &&
+              <div className="dpw-navigation-dropdown-reply-item-reply-footer">
+                <a href="#" className="dpw--panel-button" onClick={this.onSubmit}>OK</a>
+                &nbsp;
+                <a href="#" className="dpw--panel-button" onClick={this.onReset}>Reset</a>
               </div>
+              }
             </div>
-          </form>
-        </Popup>
+          </div>
+        </div>
       </div>
     );
   }
