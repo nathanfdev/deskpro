532cf1a25927389bd101501a0e15f2e459e54e9c
Sergey <laa513@gmail.com>
2016-04-28 23:10:25 +0300

    Admin UI around ticket embeddable form

---
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Settings/TicketFormWidgetController.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * a British company located in London, England.
+ *
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
+ *
+ * The license agreement under which this software is released
+ * can be found at https://www.deskpro.com/eula/
+ *
+ * By using this software, you acknowledge having read the license
+ * and agree to be bound thereby.
+ *
+ * Please note that DeskPRO is not free software. We release the full
+ * source code for our software because we trust our users to pay us for
+ * the huge investment in time and energy that has gone into both creating
+ * this software and supporting our customers. By providing the source code
+ * we preserve our customers' ability to modify, audit and learn from our
+ * work. We have been developing DeskPRO since 2001, please help us make it
+ * another decade.
+ *
+ * Like the work you see? Think you could make it better? We are always
+ * looking for great developers to join us: http://www.deskpro.com/jobs/
+ *
+ * ~ Thanks, Everyone at Team DeskPRO
+ */
+
+/**
+ * DeskPRO.
+ */
+
+namespace DeskPRO\Bundle\ApiBundle\Controller\Settings;
+
+use DeskPRO\Bundle\ApiBundle\ApiDoc\Annotation\ApiDoc;
+use DeskPRO\Bundle\ApiBundle\Controller\BaseController;
+use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\ApiModes;
+use DeskPRO\Bundle\AppBundle\AppEnv\AppEnvInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Class TicketFormWidgetController.
+ *
+ * @ApiModes("all")
+ */
+class TicketFormWidgetController extends BaseController
+{
+    /**
+     * @ApiDoc(
+     *     section="Ticket Form Widget",
+     *     description="Get ticket form widget JS code",
+     *     statusCodes={
+     *         200="OK"
+     *     }
+     * )
+     * @Rest\Get("/ticket-form-widget/code")
+     */
+    public function getJsAction(Request $request)
+    {
+        $language   = $request->get('language') ?: 'en';
+        $department = (int) $request->get('department') ?: 0;
+        $width      = $request->get('width', '500');
+
+        /** @var AppEnvInterface $env */
+        $env    = $this->get('deskpro.app_env');
+        $assets = $env->getAppWwwAssetDir();
+        $file   = "$assets/pub/build/embed_loader.js";
+        $js     = file_get_contents($file);
+        $js     = strtr($js, [
+            '__DP_URL__'     => '"'.$this->generateUrl('portal_home', [], UrlGeneratorInterface::ABSOLUTE_URL).'"',
+            '__DP_OPTIONS__' => "{language: '$language', department: $department, width: '$width'}",
+        ]);
+        $js     = str_replace("\n", "\n  ", $js); // JS 2 space padding
+        $script = <<<CODE
+<!--DESKPRO_TICKET_FORM_WIDGET::BEGIN-->
+<script type="text/javascript">
+  $js
+</script>
+<!--DESKPRO_TICKET_FORM_WIDGET::END-->
+CODE;
+
+        return new Response($script);
+    }
+}
---
+++ www/assets/BUILD/web/app/Admin/Portal/Ctrl/TicketFormWidget.coffee
@@ -0,0 +1,27 @@
+define ['Admin/Main/Ctrl/Base', 'DeskPRO/Util/Functions', 'jquery'], (Admin_Ctrl_Base) ->
+  class Admin_Portal_Ctrl_TicketFormWidget extends Admin_Ctrl_Base
+    @CTRL_ID = 'Admin_Portal_Ctrl_TicketFormWidget'
+    @CTRL_AS = 'Ctrl'
+    @DEPS    = ['$http']
+
+    init: ->
+      @$scope.code = ''
+      @$scope.language = ''
+      @$scope.department = ''
+      @$scope.width = 500
+      @$scope.departments = []
+      @$scope.languages = []
+
+    initialLoad: ->
+      @Api2.sendGet('/ticket_departments').then ({data}) =>
+        @$scope.departments = data.data
+      @Api2.sendGet('/languages').then ({data}) =>
+        @$scope.languages = data.data
+        @$scope.language = data.data[0].locale
+
+    getCode: ->
+      params = {language: @$scope.language, department: @$scope.department, width: @$scope.width}
+      @Api2.sendGet('/ticket-form-widget/code', params).then (response) =>
+        @$scope.code = response.data
+
+  Admin_Portal_Ctrl_TicketFormWidget.EXPORT_CTRL()
