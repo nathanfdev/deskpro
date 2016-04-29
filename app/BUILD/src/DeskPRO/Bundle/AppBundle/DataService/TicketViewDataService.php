d157c088c94a04a3007f009a3d70870e92e3537c
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-04-29 16:05:13 +0100

    Fix error causing ticketlist to be cut short (due to bad data where ticket missing a dep)

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/DataService/TicketViewDataService.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/DataService/TicketViewDataService.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -29,6 +29,7 @@
 /**
  * DeskPRO.
  */
+
 namespace DeskPRO\Bundle\AppBundle\DataService;
 
 use Application\DeskPRO\CustomFields\Handler\Date;
@@ -117,7 +118,7 @@ class TicketViewDataService extends AbstractDataService
                     $view->addProperty(
                         $field_id,
                         $this->translate->phrase('user.tickets.fields_department'),
-                        $ticket->getDepartment()->getUserTitle(),
+                        $ticket->getDepartment() ? $ticket->getDepartment()->getUserTitle() : '',
                         $layout_field->isVisibleOnViewAlways()
                     );
                     break;
