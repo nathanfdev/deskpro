165b785d0da1c7640dbe9e45b4205242b2f9e100
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-04-29 09:16:54 +0300

    feedback custom category fix

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Feedback/FeedbackController.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Feedback/FeedbackController.php
@@ -116,11 +116,12 @@ class FeedbackController extends AbstractFeedbackController
                 break;
             case 'custom_category':
                 $qb
-                    ->leftJoin("{$alias}.custom_data", 'customCat')
-                    ->leftJoin('customCat.field', 'def')
-                    ->addSelect('customCat.input as title')
-                    ->addSelect('customCat.id as group_name')
-                    ->andWhere('def.sys_name = :cat')
+                    ->join("{$alias}.custom_data", 'customCat')
+                    ->join('customCat.field', 'def')
+                    ->join('def.parent', 'parent')
+                    ->addSelect('def.title as title')
+                    ->addSelect('def.id as group_name')
+                    ->andWhere('parent.sys_name = :cat')
                     ->setParameter('cat', 'cat')
                     ->groupBy('group_name')
                 ;
@@ -137,7 +138,7 @@ class FeedbackController extends AbstractFeedbackController
                 break;
             case 'status_category':
                 $qb
-                    ->leftJoin("{$alias}.status_category", 'statusCategory')
+                    ->join("{$alias}.status_category", 'statusCategory')
                     ->addSelect('statusCategory.title as title')
                     ->addSelect('statusCategory.id as group_name')
                     ->groupBy('group_name')
