271508c1915a16318c22564c2970c15597347153
n3b <neb802@gmail.com>
2016-05-02 14:16:33 +0200

    [FIX] elastic search by ref prefix

--- app/src/Application/DeskPRO/NewSearch/Manager/Elasticsearch.php
+++ app/src/Application/DeskPRO/NewSearch/Manager/Elasticsearch.php
@@ -28,6 +28,7 @@
 
 namespace Application\DeskPRO\NewSearch\Manager;
 
+use Application\DeskPRO\EntityRepository\Ticket;
 use Orb\Util\Arrays;
 use Orb\Util\Numbers;
 use Orb\Validator\StringEmail;
@@ -108,9 +109,20 @@ class Elasticsearch extends ContainerAware implements SearchManagerInterface
             }
 
             if ($model == 'DeskPRO:Ticket' && preg_match('#^[0-9A-Z\-_\.]+$#', $q)) {
-                $result = $ent_repos->findTicketRef($q);
-                if ($result) {
-                    $this->handleResult($object, $result);
+                /** @var Ticket $ent_repos */
+                if ($ticket = $ent_repos->findTicketRef(strtoupper($q))) {
+                    if ($this->person->PermissionsManager->TicketChecker->canView($ticket)) {
+                        $this->handleResult($object, $ticket);
+                    }
+                } elseif (strlen($q) >= 3) {
+                    $tickets = $ent_repos->searchTicketRef($q);
+                    $results = array();
+                    foreach ($tickets as $ticket) {
+                        if ($this->person->PermissionsManager->TicketChecker->canView($ticket)) {
+                            $results[] = $ticket;
+                        }
+                    }
+                    $this->handleResult($object, $results);
                 }
             }
 
