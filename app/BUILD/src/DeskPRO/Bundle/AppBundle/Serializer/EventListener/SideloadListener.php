a8fefd1107fd84f1eec56dfafbb8c0f8e85f17c1
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-05-04 21:29:50 +0300

    serializer visitor data accessor

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Serializer/EventListener/SideloadListener.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Serializer/EventListener/SideloadListener.php
@@ -29,6 +29,7 @@
 /**
  * DeskPRO.
  */
+
 namespace DeskPRO\Bundle\AppBundle\Serializer\EventListener;
 
 use Application\DeskPRO\Domain\DomainObject;
@@ -91,15 +92,17 @@ class SideloadListener implements EventSubscriberInterface
 
         /** @var GenericSerializationVisitor $visitor */
         $visitor = $event->getVisitor();
+        $context = $event->getContext();
+        if (!$context instanceof SideloadSerializationContext) {
+            return;
+        }
 
-        /** @var SideloadSerializationContext $context */
-        $context   = $event->getContext();
         $sideloads = $context->getSideloadStore();
         $includes  = $context->getIncludes();
 
         $sideloads->setInterests($includes);
-
         $context->setExclusionEnabled(false);
+
         while ($includes && $sideloads->hasSideloads()) {
             foreach ($includes as $include) {
                 $fqcn = $sideloads->getFqcn($include);
