1d4c7db1f1dce057fc99c9d088e009664aa901dc
ImmortaL <denis@ranneft.ru>
2016-05-04 15:25:20 +0300

    Fix recent feedback

--- app/BUILD/src/DeskPRO/Bundle/PortalBundle/Controller/PortalController.php
+++ app/BUILD/src/DeskPRO/Bundle/PortalBundle/Controller/PortalController.php
@@ -29,7 +29,6 @@
 /**
  * DeskPRO.
  */
-
 namespace DeskPRO\Bundle\PortalBundle\Controller;
 
 use Application\DeskPRO\Entity\Blob;
@@ -106,9 +105,12 @@ class PortalController extends AbstractController
      */
     public function homeAction(Request $request)
     {
+        $allowedFeedbackTypes = $this->getPermissionBagForCurrentUser()->getAllowedFeedbackCategoryIds();
+
         return $this->renderThemeView('Theme:Portal:home.html.twig',
             array(
-                'page_title' => $this->createPageTitle()->homepage(),
+                'page_title'    => $this->createPageTitle()->homepage(),
+                'feedbackTypes' => $allowedFeedbackTypes,
             )
         );
     }
