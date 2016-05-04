8c51daa991f32ba79687ef5035ee79bd64a08f9a
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-05-04 15:04:54 +0300

    ticket with layuts custom field required option fix

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/Tickets/TicketWithLayouts/TicketWithLayoutsType.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/Tickets/TicketWithLayouts/TicketWithLayoutsType.php
@@ -760,35 +760,36 @@ class TicketWithLayoutsType extends AbstractType
 
     /**
      * @param TicketWithLayoutsContext $context
-     * @param string                   $property_path
-     * @param CustomDefAbstract        $field_def
-     * @param bool                     $ignore_validation
+     * @param string                   $propertyPath
+     * @param CustomDefAbstract        $def
+     * @param bool                     $ignoreValidation
      *
      * @return FormField
      */
-    private function createCustomField(TicketWithLayoutsContext $context, $property_path, CustomDefAbstract $field_def = null, $ignore_validation = false)
+    private function createCustomField(TicketWithLayoutsContext $context, $propertyPath, CustomDefAbstract $def = null, $ignoreValidation = false)
     {
-        if (!$field_def || !$field_def->isEnabled()) {
+        if (!$def || !$def->isEnabled()) {
             return false;
         }
 
+        $isAgent = $context->getViewContext() === TicketWithLayoutsContext::VIEW_AGENT;
         $options = [
-            'custom_def'      => $field_def,
-            'property_path'   => $property_path,
-            'agent_interface' => $context->getViewContext() === TicketWithLayoutsContext::VIEW_AGENT,
-            'label'           => $field_def->getTitle(),
-            'required'        => $field_def->isRequired(),
+            'custom_def'      => $def,
+            'property_path'   => $propertyPath,
+            'agent_interface' => $isAgent,
+            'label'           => $def->getTitle(),
+            'required'        => $def->isRequired($isAgent),
             'inline'          => $context->forApi(),
         ];
 
-        if (in_array($field_def->getHandlerClass(), [
+        if (in_array($def->getHandlerClass(), [
             'Application\DeskPRO\CustomFields\Handler\Hidden',
             'Application\DeskPRO\CustomFields\Handler\Display',
         ])) {
             $options['label'] = false;
         }
 
-        if ($ignore_validation) {
+        if ($ignoreValidation) {
             $options                      = $this->markNoValidation($options);
             $options['ignore_validation'] = true;
         }
