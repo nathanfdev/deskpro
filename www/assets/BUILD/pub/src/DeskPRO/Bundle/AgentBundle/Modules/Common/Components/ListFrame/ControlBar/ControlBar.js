846da918137138269cb979f154e4158e4e8d4ab9
n3b <neb802@gmail.com>
2016-05-12 23:14:56 +0300

    CS and sorting menu fix

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/ControlBar/ControlBar.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/ListFrame/ControlBar/ControlBar.js
@@ -8,50 +8,50 @@ import { connect } from 'react-redux';
 export class ControlBar extends Component {
 
   static propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    applyParams: PropTypes.func.isRequired,
+    dispatch:      PropTypes.func.isRequired,
+    applyParams:   PropTypes.func.isRequired,
     currentParams: PropTypes.object.isRequired,
-    sorting: PropTypes.objectOf(
+    sorting:       PropTypes.objectOf(
       PropTypes.shape({
         label: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
+        icon:  PropTypes.string.isRequired
       })
     ),
-    filters: PropTypes.arrayOf(PropTypes.oneOfType([
+    filters:       PropTypes.arrayOf(PropTypes.oneOfType([
       PropTypes.shape({
-        type: PropTypes.oneOf(['date']).isRequired,
-        label: PropTypes.string.isRequired,
+        type:      PropTypes.oneOf(['date']).isRequired,
+        label:     PropTypes.string.isRequired,
         fromParam: PropTypes.string.isRequired,
-        toParam: PropTypes.string.isRequired
+        toParam:   PropTypes.string.isRequired
       }),
       PropTypes.shape({
-        type: PropTypes.oneOf(['labels']).isRequired,
-        label: PropTypes.string.isRequired,
-        param: PropTypes.string.isRequired,
+        type:      PropTypes.oneOf(['labels']).isRequired,
+        label:     PropTypes.string.isRequired,
+        param:     PropTypes.string.isRequired,
         modeParam: PropTypes.string.isRequired,
-        labels: PropTypes.object.isRequired
+        labels:    PropTypes.object.isRequired
       }),
       PropTypes.shape({
-        type: PropTypes.oneOf(['select']).isRequired,
-        label: PropTypes.string.isRequired,
-        param: PropTypes.string.isRequired,
+        type:    PropTypes.oneOf(['select']).isRequired,
+        label:   PropTypes.string.isRequired,
+        param:   PropTypes.string.isRequired,
         options: PropTypes.arrayOf(PropTypes.shape({
-          label: PropTypes.string.isRequired,
-          value: PropTypes.any.isRequired,
+          label:  PropTypes.string.isRequired,
+          value:  PropTypes.any.isRequired,
           nested: PropTypes.array
         }))
       })
     ])),
-    view: PropTypes.shape({
-      options: PropTypes.objectOf(PropTypes.shape({
-        label: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired,
-        configurableFields: PropTypes.object.isRequired,
-        visibleFields: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+    view:          PropTypes.shape({
+      options:                 PropTypes.objectOf(PropTypes.shape({
+        label:                 PropTypes.string.isRequired,
+        icon:                  PropTypes.string.isRequired,
+        configurableFields:    PropTypes.object.isRequired,
+        visibleFields:         PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
         toggleFieldVisibility: PropTypes.func.isRequired
       })).isRequired,
-      viewMode: PropTypes.string.isRequired,
-      viewModeAction: PropTypes.func.isRequired,
+      viewMode:                PropTypes.string.isRequired,
+      viewModeAction:          PropTypes.func.isRequired,
       onViewFieldsMenuUnmount: PropTypes.func.isRequired
     })
   };
@@ -59,7 +59,7 @@ export class ControlBar extends Component {
   componentWillMount() {
     this.setState({
       changed: false,
-      params: this.props.currentParams.toJS()
+      params:  this.props.currentParams.toJS()
     });
   }
 
@@ -69,19 +69,19 @@ export class ControlBar extends Component {
     });
   }
 
-  setParam = (params)=> {
+  setParam = (params) => {
     const newParams = this.state.params;
     newParams[params.param] = params.value;
     this.setState({
       changed: true,
-      params: newParams
+      params:  newParams
     });
   };
 
-  unsetParam(param) {
+  unsetParam = (param) => {
     const newParams = this.state.params;
     if (Array.isArray(param)) {
-      param.forEach((item)=> {
+      param.forEach((item) => {
         newParams[item] = undefined;
       });
     } else {
@@ -89,11 +89,11 @@ export class ControlBar extends Component {
     }
     this.setState({
       changed: true,
-      params: newParams
+      params:  newParams
     });
-  }
+  };
 
-  reloadList() {
+  reloadList = () => {
     const { dispatch, applyParams } = this.props;
     if (this.state.changed) {
       dispatch(applyParams(this.state.params));
@@ -101,7 +101,7 @@ export class ControlBar extends Component {
     this.setState({
       changed: false
     });
-  }
+  };
 
   render() {
     const { sorting, filters, view } = this.props;
@@ -110,16 +110,18 @@ export class ControlBar extends Component {
       <ul className="dpwd-navigation-dropdown-top-row-main-list">
         {sorting &&
         <SortingMenu options={sorting}
-                     currentParams={this.state.params}
-                     setParam={this.setParam.bind(this)}
-                     onMenuUnmount={this.reloadList.bind(this)}/>}
+          currentParams={this.state.params}
+          setParam={this.setParam}
+          onMenuUnmount={this.reloadList}
+        />}
 
         {filters &&
         <FilteringMenuContainer filters={filters}
-                                currentParams={this.state.params}
-                                setParam={this.setParam.bind(this)}
-                                unsetParam={this.unsetParam.bind(this)}
-                                onMenuUnmount={this.reloadList.bind(this)}/>}
+          currentParams={this.state.params}
+          setParam={this.setParam}
+          unsetParam={this.unsetParam}
+          onMenuUnmount={this.reloadList}
+        />}
 
         {view && <ViewMenuContainer {...view} onViewFieldsMenuUnmount={view.onViewFieldsMenuUnmount}/>}
       </ul>
