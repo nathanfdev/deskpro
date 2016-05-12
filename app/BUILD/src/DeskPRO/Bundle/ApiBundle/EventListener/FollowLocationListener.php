3422e33792f40c6f1ce68cd7ec9552176c30ad04
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-05-12 22:08:42 +0300

    refactored follow_redirect listener

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/EventListener/FollowLocationListener.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/EventListener/FollowLocationListener.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -35,55 +35,42 @@ use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-class FollowRedirectListener implements EventSubscriberInterface
+/**
+ * Class FollowLocationListener.
+ */
+class FollowLocationListener implements EventSubscriberInterface
 {
-    const FOLLOW_REDIRECT_PARAMETER = 'follow_redirect';
-
+    /**
+     * {@inheritdoc}
+     */
     public static function getSubscribedEvents()
     {
-        return array(
-            KernelEvents::RESPONSE => array('onResponse', 1024),
-        );
+        return [
+            KernelEvents::RESPONSE => ['onResponse', 1024],
+        ];
     }
 
+    /**
+     * @param FilterResponseEvent $event
+     */
     public function onResponse(FilterResponseEvent $event)
     {
         $request  = $event->getRequest();
         $response = $event->getResponse();
-        if ($response->getStatusCode() === Response::HTTP_FOUND
-            && $request->get(self::FOLLOW_REDIRECT_PARAMETER, false) !== false) {
-            $response = $this->getNewResponse($event);
-            $event->setResponse($response);
-        }
-    }
 
-    private function getNewResponse(FilterResponseEvent $event)
-    {
-        $response = $event->getResponse();
-        $location = $response->headers->get('Location', false);
-        if (!$location) {
-            // TODO possibly we have to throw some kind of exception in this case
-            return $response;
+        if ($request->getMethod() !== Request::METHOD_PUT
+            || $response->getStatusCode() !== Response::HTTP_NO_CONTENT
+            || !$request->query->get('follow_location')
+            || !$response->headers->get('Location')) {
+            return;
         }
-        $request  = $this->createSubRequest($event->getRequest(), $location);
+
+        $response = $event->getResponse();
+        $location = $response->headers->get('Location');
+
+        $request  = Request::create($location);
         $response = $event->getKernel()->handle($request, HttpKernelInterface::SUB_REQUEST);
 
-        return $response;
-    }
-
-    private function createSubRequest(Request $request, $location)
-    {
-        $clone = clone $request;
-        $clone->server->set('REQUEST_URI', $location);
-        $clone->server->set('REQUEST_METHOD', Request::METHOD_GET);
-        $query = $clone->query;
-        $query->remove(self::FOLLOW_REDIRECT_PARAMETER);
-        $query   = $query->all();
-        $cookies = $clone->cookies->all();
-        $server  = $clone->server->all();
-        $content = $clone->getContent();
-        $clone->initialize($query, [], [], $cookies, [], $server, $content);
-
-        return $clone;
+        $event->setResponse($response);
     }
 }
---
+++ app/BUILD/tests/features/api/system_features/api_follow_location.feature
@@ -0,0 +1,15 @@
+Feature: Follow location
+
+  Background:
+    Given I install the api data set
+    And my request is authenticated
+
+  @reinstall
+  Scenario: I follow location
+    When I send a PUT request to "/api/v2/tickets/1"
+    Then the response status code should be 204
+    And the response should be empty
+
+    When I send a PUT request to "/api/v2/tickets/1?follow_location=1"
+    Then the response status code should be 200
+    And the JSON node "data.id" should be equal to 1
