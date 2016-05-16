f502b7f8bf91ca4f17e7a9c0ea9dd3d6bde8cbcb
Julien Ducro <julien.ducro@deskpro.com>
2016-05-16 10:41:50 +0100

    Fix chat waiting style

--- app/BUILD/languages/default/portal/chat.php
+++ app/BUILD/languages/default/portal/chat.php
@@ -110,7 +110,6 @@ return array(
     'portal.chat.reply_to'                   => 'Reply to {{firstName}}',
     'portal.chat.support_powered_by'         => 'Support powered by {{DeskPRO}}',
     'portal.chat.start'                      => 'Start a new chat',
-    'portal.chat.starting'                   => 'Your chat is starting...',
     'portal.chat.label-details'              => 'Your Details',
     'portal.chat.details-placeholder'        => 'First name, Last name',
     'portal.chat.screen_share'               => 'Screen Share',
--- www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Waiting/WaitingPreview.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Waiting/WaitingPreview.js
@@ -6,9 +6,6 @@ export class WaitingPreview extends React.Component {
   render() {
     return (
       <div className="dpdesignportal-collect-user-info-waiting">
-        <div>
-            {portalPhrases.get('portal.chat.message_wait-pending')}
-        </div>
         <div className="spinner">
           <i />
         </div>
