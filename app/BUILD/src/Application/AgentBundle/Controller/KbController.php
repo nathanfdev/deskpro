d26e5c53d998e1c6ef863800b9a6770a74e7268d
Julien Ducro <julien.ducro@deskpro.com>
2016-05-18 16:13:22 +0100

    Replace vendor_src mPdf by composer one Add PdfInterface and implementing in place of direct call to mPdf

--- app/BUILD/src/Application/AgentBundle/Controller/KbController.php
+++ app/BUILD/src/Application/AgentBundle/Controller/KbController.php
@@ -155,41 +155,19 @@ class KbController extends AbstractController
             'perms'               => $perms,
             'user_view_count'     => $user_view_count,
 
-            'glossary_words' => $glossary_words,
-            'word_defs'      => $word_defs,
+            'word_defs' => $word_defs,
         ];
 
         if ($is_pdf) {
             $content_html = $this->renderView('DeskPRO:pdf_agent:view_article.html.twig', $vars);
 
-            if (!defined('_MPDF_TEMP_PATH')) {
-                define('_MPDF_TEMP_PATH', dp_get_tmp_dir().'/pdf');
-                if (!is_dir(_MPDF_TEMP_PATH)) {
-                    @mkdir(_MPDF_TEMP_PATH, 0777, true);
-                }
-            }
-            $mpdf = new \mPDF_mPDF(
-                'utf-8', // Language/Character set
-                'A4', // Size
-                '8', // Default Font Size
-                '', // Default Font
-                20, // Margin Left
-                20, // Margin Right
-                40, // Margin Top
-                40, // Margin Bottom
-                10, // Margin Header
-                10, // Margin Footer
-                'P' // Orientation
-            );
-
-            $mpdf->SetBasePath($this->container->getSetting('core.deskpro_url').'/');
-            $mpdf->WriteHTML($content_html);
-
             if ($this->in->getBool('html')) {
                 $response = new Response();
                 $response->setContent($content_html);
             } else {
-                $mpdf->Output($article->title.'.pdf', 'D');
+                $pdf_renderer = $this->get('pdf_renderer');
+
+                $pdf_renderer->generateFile($content_html, $article->title.'.pdf');
                 exit;
             }
         }
