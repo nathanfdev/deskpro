3830bb06da24d184e3453012a0886554dbd59688
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-05-19 17:47:46 +0300

    api tokens usersource callback accept post requests

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Authentication/ApiTokensController.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Authentication/ApiTokensController.php
@@ -231,6 +231,7 @@ class ApiTokensController extends BaseController
      * )
      *
      * @Rest\Get("/user_sources/{usersource}/callback/{format}", requirements={"format": "(ios|default)"})
+     * @Rest\Post("/user_sources/{usersource}/callback/{format}", requirements={"format": "(ios|default)"})
      *
      * @param Request    $request
      * @param Usersource $usersource
