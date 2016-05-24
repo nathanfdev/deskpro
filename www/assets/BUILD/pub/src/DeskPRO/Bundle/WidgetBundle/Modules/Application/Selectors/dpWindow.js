481f96d1c06503e289af6256541f004f86d0145d
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-05-24 15:50:06 +0300

    widget help button and popup use new translation logic

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Selectors/dpWindow.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Selectors/dpWindow.js
@@ -1,6 +1,19 @@
 import { createSelector } from 'reselect';
 import { widgetLanguageSelector } from './bootstrap';
 
+export const translationsSelectorFactory = (property, defaultValue) => (options, language) => {
+  const filtered = options.get('translations').filter(translation => translation.get('language') === language);
+
+  if (filtered.size) {
+    return filtered.first().get(property);
+  }
+  if (options.get('translations').size) {
+    return options.get('translations').first().get(property);
+  }
+
+  return defaultValue;
+};
+
 const stateSelector = state => state.Application.dpWindow;
 
 export const widgetOpenedSelector = createSelector(
@@ -91,18 +104,7 @@ export const helpButtonSizeSelector = createSelector(
 export const helpButtonNameSelector = createSelector(
   helpButtonSelector,
   widgetLanguageSelector,
-  (options, language) => {
-    const filtered = options.get('translations').filter(translation => translation.get('language') === language);
-
-    if (filtered.size) {
-      return filtered.first().get('name');
-    }
-    if (options.get('translations').size) {
-      return options.get('translations').first().get('name');
-    }
-
-    return 'Help';
-  }
+  translationsSelectorFactory('name', 'Help')
 );
 
 export const helpButtonColorsSelector = createSelector(
@@ -143,12 +145,14 @@ export const helpPopupSelector = createSelector(
 
 export const helpPopupTitleSelector = createSelector(
   helpPopupSelector,
-  options => options.get('title')
+  widgetLanguageSelector,
+  translationsSelectorFactory('title', 'Customer Support')
 );
 
 export const helpPopupMessageSelector = createSelector(
   helpPopupSelector,
-  options => options.get('message')
+  widgetLanguageSelector,
+  translationsSelectorFactory('message', 'Need help? Just reply to start a live chat with one of our team.')
 );
 
 export const helpPopupReplyTypeSelector = createSelector(
