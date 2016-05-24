d26936a4392c029e29b32c76fe7dd1ce518622ae
ImmortaL <denis@ranneft.ru>
2016-05-24 15:07:59 +0300

    use our own JsonVisitor

---
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Serializer/Visitor/JsonSerializationVisitor.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * a British company located in London, England.
+ *
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
+ *
+ * The license agreement under which this software is released
+ * can be found at https://www.deskpro.com/eula/
+ *
+ * By using this software, you acknowledge having read the license
+ * and agree to be bound thereby.
+ *
+ * Please note that DeskPRO is not free software. We release the full
+ * source code for our software because we trust our users to pay us for
+ * the huge investment in time and energy that has gone into both creating
+ * this software and supporting our customers. By providing the source code
+ * we preserve our customers' ability to modify, audit and learn from our
+ * work. We have been developing DeskPRO since 2001, please help us make it
+ * another decade.
+ *
+ * Like the work you see? Think you could make it better? We are always
+ * looking for great developers to join us: http://www.deskpro.com/jobs/
+ *
+ * ~ Thanks, Everyone at Team DeskPRO
+ */
+
+namespace DeskPRO\Bundle\AppBundle\Serializer\Visitor;
+
+use DpSys\LowError\SystemErrorHandler;
+use JMS\Serializer\JsonSerializationVisitor as BaseVisitor;
+
+class JsonSerializationVisitor extends BaseVisitor
+{
+    private $options;
+
+    public function getResult()
+    {
+        $result = @json_encode($this->getRoot(), $this->options);
+
+        switch (json_last_error()) {
+            case JSON_ERROR_NONE:
+                return $result;
+
+            case JSON_ERROR_UTF8:
+                $root = $this->getRoot();
+                trigger_error('Failed to serialize value: '.SystemErrorHandler::varToString($root), E_USER_NOTICE);
+                array_walk_recursive($root, function (&$item) { $item = iconv('UTF-8', 'UTF-8//IGNORE', $item);});
+                $result = @json_encode($root, $this->options);
+                if (json_last_error() === JSON_ERROR_UTF8) {
+                    return '';
+                } else {
+                    return $result;
+                }
+            default:
+                throw new \RuntimeException(sprintf('An error occurred while encoding your data (error code %d).', json_last_error()));
+        }
+    }
+
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    public function setOptions($options)
+    {
+        $this->options = (integer) $options;
+    }
+}
