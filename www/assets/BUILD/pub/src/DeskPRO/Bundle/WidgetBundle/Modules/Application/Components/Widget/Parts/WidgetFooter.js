1e699e493a96fd7e1667f67d04beeeb9ff275b9b
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-05-25 15:46:21 +0300

    removed weird <hr/> from widget footer

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/Parts/WidgetFooter.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Application/Components/Widget/Parts/WidgetFooter.js
@@ -4,10 +4,12 @@ import { portalPhrases } from 'DeskPRO/Bundle/PortalBundle/PortalPhrases';
 export class WidgetFooter extends React.Component {
 
   render() {
+    const params = { '{DeskPRO}': '<span class="dpdesignportal-deskpro-mark-logo"></span>' };
+
     return (
       <div className="dpdesignportal-powered-by-deskpro">
         <a href="https://www.deskpro.com/" target="_blank">
-          <hr/><div dangerouslySetInnerHTML={portalPhrases.getHtml('portal.chat.support_powered_by', {}, {'{DeskPRO}': '<span class="dpdesignportal-deskpro-mark-logo"></span>'})} /><hr/>
+          <div dangerouslySetInnerHTML={portalPhrases.getHtml('portal.chat.support_powered_by', {}, params)}></div>
         </a>
       </div>
     );
