d17a4887c66cd4b23bea10bd16529a3199416678
Julien Ducro <julien.ducro@deskpro.com>
2016-05-26 12:13:05 +0100

    Fix tests Warning message

--- app/BUILD/src/Application/DeskPRO/Cache/Adapter/SimpleArrayCache.php
+++ app/BUILD/src/Application/DeskPRO/Cache/Adapter/SimpleArrayCache.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -29,6 +29,7 @@
 /**
  * DeskPRO.
  */
+
 namespace Application\DeskPRO\Cache\Adapter;
 
 use Application\DeskPRO\Cache\CacheAdapterInterface;
@@ -45,7 +46,7 @@ class SimpleArrayCache implements CacheAdapterInterface
 
     public function __construct()
     {
-        $this->cache = array();
+        $this->cache = [];
     }
 
     public function set($key, $val)
@@ -69,6 +70,10 @@ class SimpleArrayCache implements CacheAdapterInterface
 
     public function has($key)
     {
+        if (!$this->cache) {
+            return false;
+        }
+
         return array_key_exists($key, $this->cache);
     }
 }
