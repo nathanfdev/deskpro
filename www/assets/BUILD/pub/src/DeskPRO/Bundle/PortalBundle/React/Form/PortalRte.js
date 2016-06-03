d5b5b7f24fac04a09c5b12603f030245236424ce
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-06-03 13:17:35 +0300

    don't emit to from portal rte to attach widget

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/React/Form/PortalRte.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/React/Form/PortalRte.js
@@ -5,7 +5,6 @@ import { DropZone } from 'DeskPRO/Component/Uploader/DropZone';
 import { portalPhrases } from 'DeskPRO/Bundle/PortalBundle/PortalPhrases';
 import { DragOverlayListener } from 'DeskPRO/Component/Uploader/DragOverlayListener';
 import { portalUrlGenerator } from '../../Http/PortalUrlGenerator';
-import { pageWidgetEmitter } from 'DeskPRO/Component/PageWidget/PageWidgetEmitter';
 import uniqueId from 'lodash/utility/uniqueId';
 import $ from 'jquery';
 
@@ -47,10 +46,8 @@ export class PortalRte extends React.Component {
   };
 
   onUploadSubmit = (event, data) => {
-    const { $textarea } = this.props;
-    const file = data.files[0];
-    if (file.type.indexOf('image') === -1) {
-      pageWidgetEmitter.emit('rteFileUpload', file, $textarea);
+    if (data.files[0].type.indexOf('image') === -1) {
+      window.alert('Allowed images only.');
       return false;
     }
 
