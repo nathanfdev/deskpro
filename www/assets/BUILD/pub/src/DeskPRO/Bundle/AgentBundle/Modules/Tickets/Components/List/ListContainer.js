a8db4e3700cb6e06b8f3291f4cd6fa11b3b52049
A. Yakutskiy <a.yakutskiy@gmail.com>
2016-06-03 13:51:12 +0300

    view fields options on CSV export

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tickets/Components/List/ListContainer.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Tickets/Components/List/ListContainer.js
@@ -3,7 +3,7 @@ import { saveAs } from 'file-saver';
 import json2csv from 'json2csv';
 import { repository } from 'DeskPRO/Bundle/AppBundle/DAL';
 import { List } from './List';
-import { currentViewModeSelector, paginationSelector, listParamsSelector } from '../../Selectors/list';
+import { currentViewModeSelector, paginationSelector, listParamsSelector, fieldsSelector } from '../../Selectors/list';
 import {
   isLoadedCollectionSelectorFactory, releaseCollection, setCollection
 }
@@ -17,13 +17,16 @@ import { connect } from 'react-redux';
   currentListParams: listParamsSelector(state),
   selected:          selectedSelector(state),
   pagination:        paginationSelector(state),
+  fieldsConfig:      fieldsSelector(state),
   viewMode:          currentViewModeSelector(state)
 }))
 export class ListContainer extends Component {
   static propTypes = {
     dispatch:          PropTypes.func.isRequired,
     currentListParams: PropTypes.object.isRequired,
+    fieldsConfig:      PropTypes.object.isRequired,
     selected:          PropTypes.object.isRequired,
+    viewMode:          PropTypes.string.isRequired,
     pagination:        PropTypes.object
   };
 
@@ -37,10 +40,22 @@ export class ListContainer extends Component {
 
   saveAsCsv = (event) => {
     event.preventDefault();
-    repository('Ticket').search(Object.assign({}, this.props.currentListParams.toJS(), { count: 1000 }))
+    const { currentListParams, fieldsConfig, viewMode } = this.props;
+    repository('Ticket').search(Object.assign({}, currentListParams.toJS(), { count: 1000 }))
       .then(response => {
-        const fields = ['id', 'language'];
-        json2csv({ data: response.getData().data, fields }, (err, csv) => {
+        const visibleFields = fieldsConfig.get(viewMode).toArray();
+        console.log('visibleFields', visibleFields);
+        const fields     = [];
+        const fieldNames = [];
+        visibleFields.map(item => {
+          console.log('item', item.get('id'));
+          fields.push(item.get('id'));
+          fieldNames.push(item.get('title'));
+          return null;
+        });
+        console.log('Fields', fields);
+        console.log('fieldNames', fieldNames);
+        json2csv({ data: response.getData().data, fields, fieldNames }, (err, csv) => {
           if (err) console.log(err);
           const blob = new Blob([csv], { type: 'text/csv;charset=utf-8' });
           saveAs(blob, 'ticket_list.csv');
