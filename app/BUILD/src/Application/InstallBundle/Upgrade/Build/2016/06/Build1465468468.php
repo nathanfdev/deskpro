59043368274d9e3873749400230a0dc99b6c2234
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-06-10 11:03:17 +0100

    Fix bad upgrade query

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2016/06/Build1465468468.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2016/06/Build1465468468.php
@@ -33,7 +33,7 @@ class Build1465468468 extends AbstractBuild
     public function run()
     {
         $this->out('Update rate_limit_log ip');
-        $this->execDbQuery('TRUNCATE TABLE `rate_limit_log`');
+        $this->execDbQuery('default', 'TRUNCATE TABLE `rate_limit_log`');
         $this->execDbQuery('default', 'ALTER TABLE `rate_limit_log` CHANGE `ip` `ip` VARCHAR(255) NOT NULL');
     }
 }
