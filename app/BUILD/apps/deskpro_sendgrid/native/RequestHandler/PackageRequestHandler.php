e3e8acb643d5a8cb65c3274577adef83663c5ffa
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-06-20 17:39:24 +0100

    Fix sendgrid app install screen

--- app/BUILD/apps/deskpro_sendgrid/native/RequestHandler/PackageRequestHandler.php
+++ app/BUILD/apps/deskpro_sendgrid/native/RequestHandler/PackageRequestHandler.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -31,10 +31,12 @@
  *
  * @category Entities
  */
+
 namespace deskpro_sendgrid\RequestHandler;
 
 use Application\DeskPRO\App\Native\RequestHandler\ApiPackageRequestContext;
 use Application\DeskPRO\App\Native\RequestHandler\ApiPackageRequestHandlerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class PackageRequestHandler implements ApiPackageRequestHandlerInterface
 {
@@ -58,8 +60,8 @@ class PackageRequestHandler implements ApiPackageRequestHandlerInterface
      */
     public function getUrlAction(ApiPackageRequestContext $context)
     {
-        $router = $context->getContainer()->getRouter();
-        $url    = $router->generateUrl('email_callback');
+        $router = $context->getContainer()->get('router');
+        $url    = $router->generate('email_callback', [], UrlGeneratorInterface::ABSOLUTE_URL);
 
         return $context->createJsonResponse(array('url' => $url));
     }
