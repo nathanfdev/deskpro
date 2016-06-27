d574ad27358009741edf5bf7c5a43f3511b86b5c
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-06-27 17:53:53 +0100

    Phrase editor needs to expose new portal phrase files

--- app/BUILD/src/Application/LegacyApiBundle/Controller/LanguagesController.php
+++ app/BUILD/src/Application/LegacyApiBundle/Controller/LanguagesController.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -577,7 +577,7 @@ class LanguagesController extends AbstractController implements ProtectedControl
         return $this->createJsonResponse(array(
             'phrase_groups' => array(
                 'object' => $object_groups,
-                'user'   => $phrase_groups['user'],
+                'user'   => array_merge($phrase_groups['user'], $phrase_groups['portal']),
                 'agent'  => $phrase_groups['agent'],
                 'admin'  => $phrase_groups['admin'],
             ),
