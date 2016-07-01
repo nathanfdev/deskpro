5a77dfaf4efd12441cb9ac40faae76cd031d7089
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-07-01 15:35:04 +0100

    temp disable redirect protection

--- app/BUILD/src/DeskPRO/Bundle/PortalBundle/EventListener/RedirectProtectionListener.php
+++ app/BUILD/src/DeskPRO/Bundle/PortalBundle/EventListener/RedirectProtectionListener.php
@@ -72,6 +72,9 @@ class RedirectProtectionListener implements EventSubscriberInterface, SkipLowReq
 
     public function onResponse(FilterResponseEvent $event)
     {
+        // todo fixme https://trello.com/c/KRgESk80
+        return;
+
         $response = $event->getResponse();
         $request  = $event->getRequest();
 
