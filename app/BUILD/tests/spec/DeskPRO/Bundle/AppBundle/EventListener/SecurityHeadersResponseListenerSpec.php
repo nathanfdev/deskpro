2c98334a02798bb2838d6a98300146e4bc81c05e
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-07-01 18:09:48 +0100

    Fix CSP test

--- app/BUILD/tests/spec/DeskPRO/Bundle/AppBundle/EventListener/SecurityHeadersResponseListenerSpec.php
+++ app/BUILD/tests/spec/DeskPRO/Bundle/AppBundle/EventListener/SecurityHeadersResponseListenerSpec.php
@@ -67,7 +67,7 @@ class SecurityHeadersResponseListenerSpec extends ObjectBehavior
 
         $headers->add(['X-Content-Type-Options' => 'nosniff'])->shouldBeCalled();
         $headers->add(['X-Frame-Options' => 'sameorigin'])->shouldBeCalled();
-        $headers->add(['Content-Security-Policy' => 'default-src \'self\'; script-src * \'unsafe-inline\' \'unsafe-eval\'; style-src * \'unsafe-inline\'; img-src * data:; font-src * data:; connect-src *; media-src *; object-src *; child-src *; form-action *; referrer no-referrer-when-downgrade; frame-ancestors \'self\''])->shouldBeCalled();
+        $headers->add(['Content-Security-Policy' => 'default-src \'self\'; script-src * data: \'unsafe-inline\' \'unsafe-eval\'; style-src * data: \'unsafe-inline\'; img-src * data:; font-src * data:; connect-src *; media-src *; object-src *; child-src *; form-action *; referrer no-referrer-when-downgrade; frame-ancestors \'self\''])->shouldBeCalled();
 
         $this->onResponse($event);
     }
