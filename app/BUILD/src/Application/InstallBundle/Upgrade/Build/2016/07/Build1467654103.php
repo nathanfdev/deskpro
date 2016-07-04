42780384c797b9252f585e5be5b5e3caf3106142
ImmortaL <denis@ranneft.ru>
2016-07-04 20:51:54 +0300

    move builds

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2016/07/Build1467654103.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2016/07/Build1467654103.php
@@ -28,7 +28,7 @@
 
 namespace Application\InstallBundle\Upgrade\Build;
 
-class Build1467269813 extends AbstractBuild
+class Build1467654103 extends AbstractBuild
 {
     public function run()
     {
--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
@@ -1743,8 +1743,8 @@ return array(
         'file'      => '/src/Application/InstallBundle/Upgrade/Build/2016/07/Build1467639200.php',
         'classname' => 'Application\InstallBundle\Upgrade\Build\Build1467639200',
     ),
-    1467269813 => array(
-        'file'      => '/src/Application/InstallBundle/Upgrade/Build/2016/06/Build1467269813.php',
-        'classname' => 'Application\InstallBundle\Upgrade\Build\Build1467269813',
+    1467654103 => array(
+        'file'      => '/src/Application/InstallBundle/Upgrade/Build/2016/07/Build1467654103.php',
+        'classname' => 'Application\InstallBundle\Upgrade\Build\Build1467654103',
     ),
 );
