a73b5697a316ee4ba5c3ad202c743dd9a73c35c7
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-07-08 15:01:57 +0100

    Dont log error for 301s that happen during esi render

--- app/BUILD/src/DeskPRO/Bundle/PortalBundle/Theme/TagProcessor.php
+++ app/BUILD/src/DeskPRO/Bundle/PortalBundle/Theme/TagProcessor.php
@@ -30,6 +30,7 @@ namespace DeskPRO\Bundle\PortalBundle\Theme;
 
 use DeskPRO\Bundle\PortalBundle\Request\TagRequest;
 use DeskPRO\Bundle\SystemBundle\SystemAlerts\EventLogger;
+use DeskPRO\Component\Util\RegexUtils;
 use DpSys\LowError\SystemErrorHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -88,6 +89,25 @@ class TagProcessor
         } catch (HttpException $e) {
             // http errors like 404
             $response = '';
+        } catch (\RuntimeException $e) {
+            // See src/Symfony/Component/HttpKernel/HttpCache/Esi.php
+            // A redirect also causes an exception. We should blank these out.
+            // In portal they're typically auto-redirections based on permission
+            // (e.g. sidebar visible on login page, but no access to say, news, which is in the sidebar)
+            $statusCode = RegexUtils::getMatch(
+                '/Error when rendering ".*?" \(Status code is (?P<statusCode>\d+)\)./',
+                $e->getMessage(),
+                'statusCode'
+            );
+
+            $statusCode = $statusCode ? (int) $statusCode : null;
+
+            if ($statusCode && $statusCode >= 300 && $statusCode < 400) {
+                $response = '';
+            } else {
+                SystemErrorHandler::logException($e);
+                $response = '';
+            }
         } catch (\Exception $e) {
             // Any other exception is unexpected. Log it.
             SystemErrorHandler::logException($e);
