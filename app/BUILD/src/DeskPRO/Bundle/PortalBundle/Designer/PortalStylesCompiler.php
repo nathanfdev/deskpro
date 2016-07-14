b2d307aa3ea24c6a22c1b51d10c8f844ae110da2
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-07-14 17:34:06 +0100

    Dont delete old css blob right away, might be used in http cached responses

--- app/BUILD/src/DeskPRO/Bundle/PortalBundle/Designer/PortalStylesCompiler.php
+++ app/BUILD/src/DeskPRO/Bundle/PortalBundle/Designer/PortalStylesCompiler.php
@@ -193,11 +193,16 @@ class PortalStylesCompiler
         $asset->setBlob($blob);
 
         $this->em->persist($asset);
-        $this->em->flush();
 
+        // we dont delete the blob because it might still be required in old
+        // cached pages. Mark it as temp so it'll be auto-removed in 6 hours
         if ($oldBlob) {
-            $this->bs->deleteBlobRecord($oldBlob);
+            $oldBlob->is_temp      = true;
+            $oldBlob->date_created = new \DateTime();
+            $this->em->persist($oldBlob);
         }
+
+        $this->em->flush();
     }
 
     /**
