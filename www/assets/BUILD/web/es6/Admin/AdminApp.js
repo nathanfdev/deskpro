c3b1ad603f5e5a00384c82540774b754dfc3d72a
A. Yakutskiy <a.yakutskiy@gmail.com>
2016-07-29 11:19:28 +0700

    fix auth apps

--- www/assets/BUILD/web/es6/Admin/AdminApp.js
+++ www/assets/BUILD/web/es6/Admin/AdminApp.js
@@ -1,4 +1,5 @@
 import jQuery from '../../bower_components/jquery/dist/jquery.min';
 import { start } from './AdminLoad';
 
+__webpack_public_path__ = window.DP_ASSET_URL + 'app-build/';
 jQuery(document).on('ready', start());
