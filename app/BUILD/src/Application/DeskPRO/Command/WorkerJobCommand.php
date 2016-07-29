127c8110e4764f43d5b6cc737255637242ebc050
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-07-29 14:33:51 +0100

    Add --no-auto-updater and --auto-updater commands to updater

--- app/BUILD/src/Application/DeskPRO/Command/WorkerJobCommand.php
+++ app/BUILD/src/Application/DeskPRO/Command/WorkerJobCommand.php
@@ -58,6 +58,8 @@ class WorkerJobCommand extends \Symfony\Bundle\FrameworkBundle\Command\Container
             ->addOption('group', 'g', InputOption::VALUE_REQUIRED, 'Run only a specific group of jobs')
             ->addOption('ignore-interval', 'f', InputOption::VALUE_NONE, 'Always run job(s) even if the job interval has not ellapsed since last run')
             ->addOption('options', 'o', InputOption::VALUE_REQUIRED, 'Specify a JSON-encoded array of options to pass to worker jobs')
+            ->addOption('no-auto-updater', null, InputOption::VALUE_NONE, 'Do NOT start any auto-update process')
+            ->addOption('auto-updater', null, InputOption::VALUE_NONE, 'Start the auto-update process if it is scheduled. This will block/wait if another cron instance is still running and start the update after it finishes.')
             ->addOption('info', null, InputOption::VALUE_NONE, 'Don\'t execute anything, just list info about scheduled tasks');
     }
 
@@ -130,6 +132,16 @@ class WorkerJobCommand extends \Symfony\Bundle\FrameworkBundle\Command\Container
             return 0;
         }
 
+        $skipUpdater = $input->getOption('no-auto-updater');
+        $onlyUpdater = $input->getOption('auto-updater');
+
+        $cron_id = 'dp-cron';
+        if ($input->getOption('job')) {
+            $cron_id .= '-'.$input->getOption('job');
+        } elseif ($input->getOption('group')) {
+            $cron_id .= '-g-'.$input->getOption('group');
+        }
+
         #------------------------------
         # Clean up installer error detection
         #------------------------------
@@ -144,24 +156,64 @@ class WorkerJobCommand extends \Symfony\Bundle\FrameworkBundle\Command\Container
         # Auto-upgrader
         #------------------------------
 
-        $updaterSettings = $this->getContainer()->get('updater_settings_resolver')->getUpdaterSettings();
-        $updaterStatus   = $this->getContainer()->get('updater_settings_resolver')->getUpdaterStatus();
+        if (!$skipUpdater) {
+            $updaterSettings = $this->getContainer()->get('updater_settings_resolver')->getUpdaterSettings();
+            $updaterStatus   = $this->getContainer()->get('updater_settings_resolver')->getUpdaterStatus();
 
-        if ($updaterSettings->isEnabled() && $updaterStatus->getNextCheck() && $updaterStatus->getNextCheck() < (new \DateTime())) {
-            $cmd = $this->getContainer()->get('deskpro.app_env')->getConsolePhpCommand('dp:update --no-interaction');
-            if ($output->getVerbosity() > OutputInterface::VERBOSITY_NORMAL) {
-                $output->writeln("Running upgrade: $cmd");
-                $cb = function ($t, $l) use ($output) {
-                    $output->write($l);
-                };
-            } else {
-                $cb = null;
+            $check = App::getDb()->fetchColumn('SELECT value FROM settings WHERE name LIKE ?', ['core.croncheck.updater']);
+            if ($check && $check > (time() - 3600)) {
+                if ($output->getVerbosity() > OutputInterface::VERBOSITY_NORMAL) {
+                    $output->writeln('core.croncheck.updater already running');
+                }
+
+                return 0;
             }
 
-            $proc = new Process($cmd);
-            $proc->run($cb);
+            App::getDb()->replace('settings', ['name' => 'core.croncheck.updater', 'value' => time()]);
+            $db = App::getDb();
 
-            return 0;
+            register_shutdown_function(function () use ($db) {
+                $db->delete('settings', ['name' => 'core.croncheck.updater']);
+            });
+
+            if ($updaterSettings->isEnabled() && $updaterStatus->getNextCheck() && $updaterStatus->getNextCheck() < (new \DateTime())) {
+                do {
+                    $check = App::getDb()->fetchColumn('SELECT value FROM settings WHERE name LIKE ?', ['core.croncheck.%']);
+                    if ($check) {
+                        if ($check < time() - 3600) {
+                            return 0;
+                        }
+                        if ($output->getVerbosity() > OutputInterface::VERBOSITY_NORMAL) {
+                            $output->writeln('Waiting ...');
+                        }
+                        sleep(1);
+                    }
+                } while ($check);
+
+                $cmd = $this->getContainer()->get('deskpro.app_env')->getConsolePhpCommand('dp:update --no-interaction');
+                if ($output->getVerbosity() > OutputInterface::VERBOSITY_NORMAL) {
+                    $output->writeln("Running upgrade: $cmd");
+                    $cb = function ($t, $l) use ($output) {
+                        $output->write($l);
+                    };
+                } else {
+                    $cb = null;
+                }
+
+                $proc = new Process($cmd);
+                $proc->run($cb);
+
+                return 0;
+            }
+
+            // If we got here, there is nothing to do, so return
+            if ($onlyUpdater) {
+                if ($output->getVerbosity() > OutputInterface::VERBOSITY_NORMAL) {
+                    $output->writeln('No update is scheduled');
+                }
+
+                return 0;
+            }
         }
 
         #------------------------------
@@ -203,13 +255,6 @@ class WorkerJobCommand extends \Symfony\Bundle\FrameworkBundle\Command\Container
             ));
         }
         App::getDb()->replace('settings', array('name' => 'core.last_cron_start', 'value' => time()));
-
-        $cron_id = 'dp-cron';
-        if ($input->getOption('job')) {
-            $cron_id .= '-'.$input->getOption('job');
-        } elseif ($input->getOption('group')) {
-            $cron_id .= '-g-'.$input->getOption('group');
-        }
 
         $GLOBALS['DP_CRON_ID'] = $cron_id;
 
