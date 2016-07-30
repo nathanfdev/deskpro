f821bdb2192705339454e3a0088f3e4cefaeb546
Sergey <laa513@gmail.com>
2016-07-31 00:00:34 +0300

    CI E2E with crossbrowsertesting.com

--- www/assets/BUILD/pub/src/tests/E2E/tests/rough-agent-availability-test.js
+++ www/assets/BUILD/pub/src/tests/E2E/tests/rough-agent-availability-test.js
@@ -1,5 +1,9 @@
 import { url } from '../helpers.js';
 
+function menuItemSelector(num) {
+  return '//a[contains(@class, "dpw-app-bar-item-' + num + '")]/div/div[contains(@class, "icon")]';
+}
+
 module.exports = {
   'I go through the all apps in the agent interface and check navigation panes get loaded': function(client) {
     client.page.login().navigate().loginAsAdmin();
@@ -16,41 +20,37 @@ module.exports = {
       // Check we were redirected to /tasks
       .assert.urlEquals(url('/tasks'))
 
-      // Click on the link #1
-      .click('//a[contains(@class, "dpw-app-bar-item-1")]')
+      // Go through the all menu items
+      
+      .click(menuItemSelector(1))
       .assert.urlEquals(url('/tickets'))
       .waitForElementVisible('//h1[contains(., "Tickets")]')
       .waitForElementVisible('//div[contains(@class, "list-sidebar-title") and contains(., "Inbox")]')
       .waitForElementVisible('//div[contains(@class, "list-sidebar-title") and contains(., "All Tickets")]')
 
-      // Click on the link #2
-      .click('//a[contains(@class, "dpw-app-bar-item-2")]')
+      .click(menuItemSelector(2))
       .assert.urlEquals(url('/crm'))
       .waitForElementVisible('//h1[contains(., "CRM")]')
       .waitForElementVisible('//div[contains(@class, "list-sidebar-title") and contains(., "People")]')
       .waitForElementVisible('//div[contains(@class, "list-sidebar-title") and contains(., "Organizations")]')
 
-      // Click on the link #3
-      .click('//a[contains(@class, "dpw-app-bar-item-3")]')
+      .click(menuItemSelector(3))
       .assert.urlEquals(url('/chat'))
       .waitForElementVisible('//h1[contains(., "Chat")]')
       .waitForElementVisible('//div[contains(@class, "list-sidebar-title") and contains(., "My Chats")]')
       .waitForElementVisible('//div[contains(@class, "list-sidebar-title") and contains(., "All Chats")]')
 
-      // Click on the link #4
-      .click('//a[contains(@class, "dpw-app-bar-item-4")]')
+      .click(menuItemSelector(4))
       .assert.urlEquals(url('/feedback'))
       .waitForElementVisible('//h1[contains(., "Feedback")]')
       .waitForElementVisible('//div[contains(@class, "list-sidebar-title") and contains(., "Pending")]')
 
-      // Click on the link #5
-      .click('//a[contains(@class, "dpw-app-bar-item-5")]')
+      .click(menuItemSelector(5))
       .assert.urlEquals(url('/publish'))
       .waitForElementVisible('//h1[contains(., "Publish")]')
       .waitForElementVisible('//div[contains(@class, "list-sidebar-title") and contains(., "Articles")]')
 
-      // Click on the link #6
-      .click('//a[contains(@class, "dpw-app-bar-item-6")]')
+      .click(menuItemSelector(6))
       .assert.urlEquals(url('/tasks'))
       .waitForElementVisible('//h1[contains(., "Tasks")]')
       .waitForElementVisible('//div[contains(@class, "list-sidebar-title") and contains(., "Tasks")]')
