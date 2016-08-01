e11150c438c1f9031579d4d05021253ef32ebd05
ImmortaL <denis@ranneft.ru>
2016-08-01 23:36:02 +0300

    Update missing insertSnippetHtml call

--- www/assets/BUILD/web/javascripts/DeskPRO/Agent/ElementHandler/TicketReplyBox.js
+++ www/assets/BUILD/web/javascripts/DeskPRO/Agent/ElementHandler/TicketReplyBox.js
@@ -624,7 +624,7 @@ DeskPRO.Agent.ElementHandler.TicketReplyBox = new Orb.Class({
 					html = html.replace(/<\/p>\s*<p>/g, '<br/>');
 					html = html.replace(/^<p>/, '');
 					html = html.replace(/<\/p>$/, '');
-					textarea.data('redactor').insertHtml(html);
+					textarea.data('redactor').insertSnippetHtml(html);
 				} else {
 					self.page.insertTextInReply(result);
 				}
