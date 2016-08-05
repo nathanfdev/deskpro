d67997a53440e90f74875b8202a4cba23e16a444
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-08-05 18:44:40 +0300

    feedback vote widget prevent click if no permission, CS cleanup

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/FeedbackVoteWidget.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/FeedbackVoteWidget.js
@@ -1,33 +1,41 @@
-import React from 'react';
 import { PageWidget } from 'DeskPRO/Component/PageWidget/PageWidget';
 import $ from 'jquery';
+import _ from 'lodash';
 
 export class FeedbackVoteWidget extends PageWidget {
+
   renderWidget() {
-    let $iAgreeBox = this.$element;
-    $iAgreeBox.click(function (e) {
+    const $iAgreeBox = this.$element;
+
+    $iAgreeBox.click(e => {
       e.preventDefault();
+
       if ($iAgreeBox.hasClass('closed')) {
-        return; // rate closed
+        return false; // rate closed
+      }
+      if ($iAgreeBox.hasClass('rate_forbidden')) {
+        return false; // no permission
       }
       if ($iAgreeBox.hasClass('agreed')) {
-        return; // already agreed
+        return false; // already agreed
       }
-      let action = $iAgreeBox.attr('href');
-      let $counter = $iAgreeBox.find('span.counter');
+
+      const action = $iAgreeBox.attr('href');
+      const $counter = $iAgreeBox.find('span.counter');
       $counter.text(_.parseInt($counter.text()) + 1);
       $iAgreeBox.addClass('agreed');
+
       $.ajax({
         url:         action,
         method:      'POST',
         contentType: 'application/json'
-      }).success(function (data) {
+      }).success(data => {
         if (!data.success) {
           $iAgreeBox.find('div').text(data.error);
           $counter.text(_.parseInt($counter.text()) - 1);
           $iAgreeBox.removeClass('agreed');
         }
-      }).fail(function () {
+      }).fail(() => {
         $counter.text(_.parseInt($counter.text()) - 1);
         $iAgreeBox.removeClass('agreed');
       });
