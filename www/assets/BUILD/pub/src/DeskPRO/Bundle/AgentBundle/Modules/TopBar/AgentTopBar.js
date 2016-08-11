99184d3e0c56d22255ebcadfb6ab104170a77f7b
ImmortaL <denis@ranneft.ru>
2016-08-11 20:55:11 +0300

    add redux to agent app, little hack to map component to DOM node
    This reverts commit cd3996c07486d70a6f29ffb595cb853045b15c03.

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/TopBar/AgentTopBar.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/TopBar/AgentTopBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TopBar, TopBarItem, TopBarRightMenu, TopBarNotificationIcon } from 'DeskPRO/Component/Semantic/TopBar';
 import SearchBox from 'DeskPRO/Component/Semantic/SearchBox';
 import AddButton from './AddButton';
@@ -6,7 +6,8 @@ import Chat from './Chat';
 import User from './User';
 import Isvg from 'react-inlinesvg';
 
-class AgentTopBar extends React.Component {
+class AgentTopBar extends Component {
+
   constructor() {
     super();
     this.state = {
@@ -32,7 +33,6 @@ class AgentTopBar extends React.Component {
   onChatVolumeUpdate(newVal) {
     const volume = newVal / 10;
     window.DeskPRO_Window.volume = volume;
-
 
     window.$('audio').each(function changeVolume() {
       this.volume = volume;
