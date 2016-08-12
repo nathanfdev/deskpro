9f07465fddb99df3c95639598038c10cca091907
ImmortaL <denis@ranneft.ru>
2016-08-12 11:54:37 +0300

    Test agent list written with react, integrated to old agent interface

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/AgentLegacyApp.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/AgentLegacyApp.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AgentTopBar from './Modules/TopBar/AgentTopBar';
+import { AgentList } from './Modules/Agent/Components/AgentList';
 import { api, setApi, loadRepositoriesConfig } from 'DeskPRO/Bundle/AppBundle/DAL';
 import { repositoriesConfig } from 'DeskPRO/Bundle/AgentBundle/DAL/config';
 import { createStore, applyMiddleware, compose } from 'redux';
@@ -13,6 +14,9 @@ import { preloadData } from './Modules/Application/Actions/bootstrapActions';
 
 
 export class AgentLegacyApp {
+
+  rendered = [];
+
   run() {
     window.$(document).on('ready', () => this.start());
   }
@@ -24,17 +28,30 @@ export class AgentLegacyApp {
       this.store = AgentLegacyApp.createStore();
       this.store.dispatch(preloadData());
       this.renderPiece(AgentTopBar, AgentTopBar.getType());
+      this.renderPiece(AgentList, AgentList.getType());
     }
   }
 
   renderPiece(piece, piecePlace) {
+    if (this.rendered[piecePlace]) {
+      return;
+    }
+
     const element = React.createElement(piece, { store: this.store });
     const elementPlace = piecePlace.replace(/([A-Z])/g, ($1) => `_${$1.toLowerCase()}`);
     const node = document.getElementById(`react_dp${elementPlace}`);
 
     if (node) {
+      this.rendered[piecePlace] = true;
       ReactDOM.render(element, node);
+      return;
     }
+    // this is just a stub. Possibly we just need some event listener or so to handle element rendering when
+    // some page component was just loaded.
+    // maybe we could use something like angular directive or so.
+    // it's not good when there are elements for different pages.
+    // Or, we may just call this method when it's needed.
+    setTimeout(() => this.renderPiece(piece, piecePlace), 1000);
   }
 
   static createStore(initialState = {}) {
