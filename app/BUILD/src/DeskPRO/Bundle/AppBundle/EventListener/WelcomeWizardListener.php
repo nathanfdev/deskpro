70b73c986f23397a462ec8fc3a501fa233c55ec7
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-08-16 16:39:33 +0100

    Fix keyfile download on welcome wizard

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/EventListener/WelcomeWizardListener.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/EventListener/WelcomeWizardListener.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2015, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2016, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -29,6 +29,7 @@
 /**
  * DeskPRO.
  */
+
 namespace DeskPRO\Bundle\AppBundle\EventListener;
 
 use DeskPRO\Bundle\AppBundle\Request\RequestUtils;
@@ -73,6 +74,7 @@ final class WelcomeWizardListener implements EventSubscriberInterface
             && !RequestUtils::isLowRequest($event->getRequest())
             && $event->getRequest()->getMethod() === 'GET'
             && !$event->getRequest()->isXmlHttpRequest()
+            && strpos($event->getRequest()->getPathInfo(), '/api') === false
             && strpos($event->getRequest()->getPathInfo(), '/login') === false
             && strpos($event->getRequest()->getPathInfo(), '/admin/start') === false
         )
