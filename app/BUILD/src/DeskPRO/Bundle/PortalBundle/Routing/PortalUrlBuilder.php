a6c4f2bb36a88b1fe7b2d23502e3ddc65e07bd04
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-08-22 11:17:51 +0300

    portal url builder handles base url

--- app/BUILD/src/DeskPRO/Bundle/PortalBundle/Routing/PortalUrlBuilder.php
+++ app/BUILD/src/DeskPRO/Bundle/PortalBundle/Routing/PortalUrlBuilder.php
@@ -26,39 +26,67 @@
  * ~ Thanks, Everyone at Team DeskPRO
  */
 
-/**
- * DeskPRO.
- */
-
 namespace DeskPRO\Bundle\PortalBundle\Routing;
 
 use Application\DeskPRO\Entity\Language;
 use DeskPRO\Bundle\PortalBundle\Mode\PortalMode;
 
+/**
+ * Class PortalUrlBuilder.
+ */
 class PortalUrlBuilder
 {
+    /**
+     * @var string
+     */
     private $path;
+
     /**
      * @var Language
      */
     private $language;
+
     /**
      * @var PortalMode
      */
     private $mode;
 
-    public function __construct($path, Language $language = null, PortalMode $mode = null)
+    /**
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * Constructor.
+     *
+     * @param string          $path
+     * @param Language|null   $language
+     * @param PortalMode|null $mode
+     * @param string          $baseUrl
+     */
+    public function __construct($path, Language $language = null, PortalMode $mode = null, $baseUrl)
     {
         $this->path     = trim($path);
         $this->language = $language;
         $this->mode     = $mode;
+        $this->baseUrl  = $baseUrl;
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
-        $parts = array();
+        $parts = [];
+        $path  = $this->path;
 
-        if (!preg_match('#^/?(?:agent|admin|reports)(/|$)#', $this->path)) {
+        // remove base url from the url path to set it in the proper order
+        if ($this->baseUrl && strpos($path, $this->baseUrl) === 0) {
+            $path    = substr($path, strlen($this->baseUrl));
+            $parts[] = trim($this->baseUrl, '/');
+        }
+
+        if (!preg_match('#^/?(?:agent|admin|reports)(/|$)#', $path)) {
             if ($this->mode) {
                 $mode_path = trim($this->mode->getModePath(), '/');
                 if (strlen($mode_path) > 0) {
@@ -74,10 +102,10 @@ class PortalUrlBuilder
             }
         }
 
-        if ((string) $this->path !== '/') {
-            $path_part = trim($this->path, '/');
-            if (strlen($path_part) > 0) {
-                $parts[] = $path_part;
+        if ((string) $path !== '/') {
+            $pathPart = trim($path, '/');
+            if (strlen($pathPart) > 0) {
+                $parts[] = $pathPart;
             }
         }
 
