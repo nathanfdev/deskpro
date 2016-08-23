8a6ec0aea216c7dd14c85f01e6ebf6f316f62cfb
Chris Padfield <chris.padfield@deskpro.com>
2016-08-23 17:12:52 +0100

    language update

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Onboarding/Tours/topbar.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Onboarding/Tours/topbar.js
@@ -2,44 +2,44 @@ const topbar = {
   force: false,
   steps: [
     {
-      title: 'Search and recent tabs',
-      text: 'You can search the helpdesk and view recent tabs in the top-left corner of the screen.',
+      title: 'Search',
+      text: 'You can search the helpdesk for tickets, people, articles etc. in the top-left corner of the screen.',
       selector: '#react_dp_agent_top_bar .item.search-box',
       position: 'bottom'
     },
     {
-      title: 'Search and recent tabs',
-      text: 'You can search the helpdesk and view recent tabs in the top-left corner of the screen.',
+      title: 'History',
+      text: 'Quickly re-open items you have recently viewed.',
       selector: '#react_dp_agent_top_bar .item.recent',
       position: 'bottom'
     },
     {
       title: 'Create',
-      text: 'Add new tickets, articles, users (and more) to your helpdesk by clicking the \'create button\' in the header.',
+      text: 'Add new tickets, people, articles etc. to your helpdesk by clicking the \'create button\' in the header.',
       selector: '#react_dp_agent_top_bar .item.add',
       position: 'bottom'
     },
     {
       title: 'Manage views',
-      text: 'Click the views icon when you want to change the layout of your screen.',
+      text: 'Click the \'views icon\' when you want to change the layout of your screen.',
       selector: '#react_dp_agent_top_bar .item.view_mode',
       position: 'bottom'
     },
     {
       title: 'Notifications',
-      text: 'You can access notifications about activity in your helpdesk by clicking here.',
+      text: 'You will receive notifications about helpdesk activity (e.g. new tickets).',
       selector: '#react_dp_agent_top_bar .item.notifications',
       position: 'bottom'
     },
     {
       title: 'Your profile',
-      text: 'Manage your preferences, find help and log out of DeskPRO from here.',
+      text: 'Manage your preferences, find help and log out of DeskPRO.',
       selector: '#react_dp_agent_top_bar .user',
       position: 'bottom'
     },
     {
       title: 'Chat',
-      text: 'Change your chat status or settings and see who\'s online by clicking here.',
+      text: 'You can control whether you are online to answer user chats.',
       selector: '#react_dp_agent_top_bar .chat',
       position: 'bottom'
     },
