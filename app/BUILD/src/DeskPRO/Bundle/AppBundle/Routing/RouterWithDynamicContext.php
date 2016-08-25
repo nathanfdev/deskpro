cb0222da3135d15783e8e760061a6e45274fd56e
n3b <neb802@gmail.com>
2016-08-25 13:47:06 -0300

    new router reference type

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Routing/RouterWithDynamicContext.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Routing/RouterWithDynamicContext.php
@@ -28,6 +28,7 @@
 
 namespace DeskPRO\Bundle\AppBundle\Routing;
 
+use DeskPRO\Component\Routing\Generator\RouterInterface as DpRouterInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\CacheWarmer\WarmableInterface;
 use Symfony\Component\Routing\Matcher\RequestMatcherInterface;
@@ -132,6 +133,10 @@ class RouterWithDynamicContext implements RouterInterface, RouterDecorator, Requ
         }
 
         try {
+            if ($referenceType === DpRouterInterface::RELATIVE_URL) {
+                $referenceType = self::ABSOLUTE_URL;
+            }
+
             return $this->getBaseRouter()->generate($name, $parameters, $referenceType);
         } finally {
             if ($context !== $prevContext) {
