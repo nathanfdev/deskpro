50b77e013163d4bf133af7c33091e2542c1d8ef1
ImmortaL <denis@ranneft.ru>
2016-09-05 21:08:46 +0300

    add reexports

---
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/IM/Components/New/IMTabs/index.js
@@ -0,0 +1,7 @@
+export AbstractList from './AbstractList';
+export AgentList from './AgentList';
+export AgentTeamList from './AgentTeamList';
+export AvatarHelper from './AvatarHelper';
+export DepartmentList from './DepartmentList';
+export EveryoneSegment from './EveryoneSegment';
+export RecentList from './RecentList';
