ab35c0ecd8cd8ce0f2a04ad5c6dd24652e5260a2
n3b <neb802@gmail.com>
2016-09-09 13:33:52 -0300

    export default

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Disconnect/LostConnection.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/WidgetBundle/Modules/Chat/Components/Stage/Active/Disconnect/LostConnection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { portalPhrases } from 'DeskPRO/Bundle/PortalBundle/PortalPhrases';
 
-export class LostConnection extends React.Component {
+export default class LostConnection extends React.Component {
 
   render() {
     return (
