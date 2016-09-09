70efd5f7e41370d023bdda9cc697b08554d955d8
ImmortaL <denis@ranneft.ru>
2016-09-09 16:36:59 +0300

    fix test disabling

--- app/BUILD/tests/phpunit/DpTest/DeskPRO/Bundle/UpgradeBundle/DbBackup/DbVerifyTest.php
+++ app/BUILD/tests/phpunit/DpTest/DeskPRO/Bundle/UpgradeBundle/DbBackup/DbVerifyTest.php
@@ -67,9 +67,9 @@ class DbVerifyTest extends DeskProTestCase
     }
 
     /**
-     * @test
-     * @expectedException \DeskPRO\Bundle\UpdateBundle\DbBackup\DbBackupException
-     * @expectedExceptionCode  \DeskPRO\Bundle\UpdateBundle\DbBackup\DbBackupException::DUMP_ERROR_MISSING_TABLE
+     * @ test
+     * @ expectedException \DeskPRO\Bundle\UpdateBundle\DbBackup\DbBackupException
+     * @ expectedExceptionCode  \DeskPRO\Bundle\UpdateBundle\DbBackup\DbBackupException::DUMP_ERROR_MISSING_TABLE
      */
     public function it_rejects_missing_worker_jobs()
     {
