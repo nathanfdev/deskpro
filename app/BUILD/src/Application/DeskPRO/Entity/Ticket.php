65655a0ada65df838df9e4a30c611c18c2b24be1
Sergey Yastrebov <serg.yastrebov@gmail.com>
2016-09-16 17:20:46 +0300

    ticket access code CS cleanup

--- app/BUILD/src/Application/DeskPRO/Entity/Ticket.php
+++ app/BUILD/src/Application/DeskPRO/Entity/Ticket.php
@@ -82,7 +82,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @property Organization                        $organization
  * @property ChatConversation                    $linked_chat
  * @property TicketAttachment[]                  $attachments
- * @property TicketAccessCode[]                  $access_codes
+ * @property TicketAccessCode[]|ArrayCollection  $access_codes
  * @property TicketMessage[]|ArrayCollection     $messages
  * @property TicketSms[]                         $sms_messages
  * @property CustomDataTicket[]|ArrayCollection  $custom_data
@@ -3328,7 +3328,9 @@ class Ticket extends DomainObject implements HighlightableModelInterface, Labels
     /**
      * Add an access code for a person.
      *
-     * @param PersonEmail $email
+     * @param Person $person
+     *
+     * @return TicketAccessCode
      */
     public function addAccessCodeForPerson(Person $person)
     {
@@ -3336,10 +3338,12 @@ class Ticket extends DomainObject implements HighlightableModelInterface, Labels
             return $tac;
         }
 
-        $tac           = new TicketAccessCode();
-        $tac['ticket'] = $this;
-        $tac['person'] = $person;
+        $tac = new TicketAccessCode();
+        $tac->setTicket($this);
+        $tac->setPerson($person);
         $this->access_codes->add($tac);
+
+        return $tac;
     }
 
     /**
@@ -3426,6 +3430,14 @@ class Ticket extends DomainObject implements HighlightableModelInterface, Labels
         $str .= $this->auth;
 
         return $str;
+    }
+
+    /**
+     * @return TicketAccessCode[]|ArrayCollection
+     */
+    public function getAccessCodes()
+    {
+        return $this->access_codes;
     }
 
     public function forceSetAccessCode($code)
