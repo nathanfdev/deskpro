97abd374e21345b84396a6d869838e24b07ffbbc
Julien Ducro <julien@ducro.fr>
2016-09-19 18:13:49 +0100

    Fix Storybook structure

--- www/assets/BUILD/pub/src/tests/Visual/AgentBundle/Modules/IM/im.js
+++ www/assets/BUILD/pub/src/tests/Visual/AgentBundle/Modules/IM/im.js
@@ -6,6 +6,9 @@ import SearchBox from 'DeskPRO/Component/Semantic/SearchBox';
 import { Container } from 'DeskPRO/Bundle/AgentBundle/Modules/IM/Components/New/ChatWindow';
 import { IMOverlay, IMButton, TopBarRecentImList, GroupAddDrawer } from 'DeskPRO/Bundle/AgentBundle/Modules/IM/Components/New/TopBar';
 import { imState, messages } from 'DemoState/AgentBundle/Modules/IM/im';
+import recentSvg from 'DeskPRO/Bundle/AgentBundle/Resources/img/topbar/recent.svg';
+import viewsSvg from 'DeskPRO/Bundle/AgentBundle/Resources/img/topbar/views.svg';
+import notificationsSvg from 'DeskPRO/Bundle/AgentBundle/Resources/img/topbar/notifications.svg';
 import Immutable from 'immutable';
 
 const chatState = {
@@ -21,76 +24,85 @@ storiesOf('Agent: IM', module)
   )
   .add(
   'Agent: IM: TopBar intergrated with recent list',
-  () => <TopBar>
-    <div className="logo">
-      <img src="/assets/BUILD/web/images/dp-logo-48.png" alt="DeskPRO logo" />
+  () =>
+    <div id="react_dp_agent_top_bar">
+      <TopBar>
+        <TopBarItem classes={['search-box legacy-omnibox']}>
+          <SearchBox onUserInput={action('Search')} placeholder="Search ..." />
+        </TopBarItem>
+        <TopBarItem classes={['recent']}>
+          <Isvg src={recentSvg} />
+        </TopBarItem>
+        <TopBarItem childrenWrapper="im-list">
+          <TopBarRecentImList {...imState} onRecentClick={action('recent chat (top bar) click')}>
+            <IMOverlay
+              {...imState}
+              onRecentClick={action('recent chat (list) click')}
+              onParticipantClick={action('participant click')}
+              createNewGroup={action('create new group')}
+              toggleOverlay={action('toggle overlay')}
+              isOpen
+            >
+              <IMButton />
+            </IMOverlay>
+          </TopBarRecentImList>
+        </TopBarItem>
+      </TopBar>
     </div>
-    <SearchBox onUserInput={action('Search')} placeholder="Search ..." />
-    <TopBarItem>
-      <i className="icon wait" />
-    </TopBarItem>
-    <TopBarItem childrenWrapper="im-list">
-      <TopBarRecentImList {...imState} onRecentClick={action('recent chat (top bar) click')}>
-        <IMOverlay
-          {...imState}
-          onRecentClick={action('recent chat (list) click')}
-          onParticipantClick={action('participant click')}
-          createNewGroup={action('create new group')}
-          toggleOverlay={action('toggle overlay')}
-          isOpen
-        >
-          <IMButton />
-        </IMOverlay>
-      </TopBarRecentImList>
-    </TopBarItem>
-  </TopBar>)
+  )
   .add(
     'Agent: IM: Chat opened',
-    () => <TopBar>
-      <div className="logo">
-        <img src="/assets/BUILD/web/images/dp-logo-48.png" alt="DeskPRO logo" />
+    () =>
+      <div id="react_dp_agent_top_bar">
+        <TopBar>
+          <div className="logo">
+            <img src="/assets/BUILD/web/images/dp-logo-48.png" alt="DeskPRO logo" />
+          </div>
+          <SearchBox onUserInput={action('Search')} placeholder="Search ..." />
+          <TopBarItem>
+            <i className="icon wait" />
+          </TopBarItem>
+          <TopBarItem childrenWrapper="im-list">
+            <span>
+              <TopBarRecentImList {...imState} onRecentClick={action('recent chat (top bar) click')}>
+                <Container
+                  dispatch={() => console.log('www')}
+                  isOpen
+                  {...chatState}
+                  current={Immutable.fromJS({ id: '1', chat_type: 'agent', agents: [1, 6] })}
+                  onChange={action('reply')}
+                  onAttach={action('attach')}
+                  clickOut={action('clickout')}
+                />
+              </TopBarRecentImList>
+            </span>
+          </TopBarItem>
+        </TopBar>
       </div>
-      <SearchBox onUserInput={action('Search')} placeholder="Search ..." />
-      <TopBarItem>
-        <i className="icon wait" />
-      </TopBarItem>
-      <TopBarItem childrenWrapper="im-list">
-        <span>
-          <TopBarRecentImList {...imState} onRecentClick={action('recent chat (top bar) click')}>
-            <Container
-              dispatch={() => console.log('www')}
-              isOpen
-              {...chatState}
-              current={Immutable.fromJS({ id: '1', chat_type: 'agent', agents: [1, 6] })}
-              onChange={action('reply')}
-              onAttach={action('attach')}
-              clickOut={action('clickout')}
-            />
-          </TopBarRecentImList>
-        </span>
-      </TopBarItem>
-    </TopBar>
   )
   .add(
     'Agent: IM: Group creation',
-    () => <TopBar>
-      <div className="logo">
-        <img src="/assets/BUILD/web/images/dp-logo-48.png" alt="DeskPRO logo" />
+    () =>
+      <div id="react_dp_agent_top_bar">
+        <TopBar>
+          <div className="logo">
+            <img src="/assets/BUILD/web/images/dp-logo-48.png" alt="DeskPRO logo" />
+          </div>
+          <SearchBox onUserInput={action('Search')} placeholder="Search ..." />
+          <TopBarItem>
+            <i className="icon wait" />
+          </TopBarItem>
+          <TopBarItem>
+            <IMButton />
+            <GroupAddDrawer
+              isOpen
+              target={document.getElementById('im-button')}
+              {...imState}
+              checkedAgents={{ 2: true, 6: true }}
+              createGroup={action('creategroup')}
+            />
+          </TopBarItem>
+        </TopBar>
       </div>
-      <SearchBox onUserInput={action('Search')} placeholder="Search ..." />
-      <TopBarItem>
-        <i className="icon wait" />
-      </TopBarItem>
-      <TopBarItem>
-        <IMButton />
-        <GroupAddDrawer
-          isOpen
-          target={document.getElementById('im-button')}
-          {...imState}
-          checkedAgents={{ 2: true, 6: true }}
-          createGroup={action('creategroup')}
-        />
-      </TopBarItem>
-    </TopBar>
   )
 ;
