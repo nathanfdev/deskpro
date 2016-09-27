b54a2f8afb7278c533cc6744d6649b6775a768f8
Julien Ducro <julien@ducro.fr>
2016-09-27 10:49:53 +0100

    es-lint fixes, refactor classes into className

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/DemoBundle/Modules/Login/Components/ExtendTrial.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/DemoBundle/Modules/Login/Components/ExtendTrial.js
@@ -119,198 +119,230 @@ class ExtendTrial extends React.Component {
 
 
     return (
-      <Segment className="extend-trial">
-        <h3>
-          <FormattedMessage
-            id="cloud.demo_expired.extend_trial_title"
-            defaultMessage="Extend your trial"
-          />
-        </h3>
-        <p>
-          <FormattedMessage
-            id="cloud.demo_expired.extend_trial_desc"
-            defaultMessage="Get {period} by entering your details below."
-            values={{
-              period:
-                <span className="green">
+      <Segments className="extend-trial horizontal">
+        <Segment className="extend-trial-form">
+          <h3>
+            <FormattedMessage
+              id="cloud.demo_expired.extend_trial_title"
+              defaultMessage="Extend your trial"
+            />
+          </h3>
+          <p>
+            <FormattedMessage
+              id="cloud.demo_expired.extend_trial_desc"
+              defaultMessage="Get {period} by entering your details below."
+              values={{
+                period:
+                  <span className="green">
+                    <FormattedMessage
+                      id="cloud.demo_expired.extend_trial_period"
+                      defaultMessage="7 extra days"
+                    />
+                  </span>
+              }}
+            />
+          </p>
+          <Message className="positive">
+            <FormattedMessage
+              id="cloud.demo_expired.extend_trial_message"
+              defaultMessage="You won't pay anything whilst in your trial and you can still cancel at any time."
+            />
+          </Message>
+          <Segments className="horizontal">
+            <Segment className="address">
+              <Form>
+                <h4>
+                  <i className="icon home" />
                   <FormattedMessage
-                    id="cloud.demo_expired.extend_trial_period"
-                    defaultMessage="7 extra days"
+                    id="cloud.demo_expired.billing_address"
+                    defaultMessage="Billing address"
                   />
-                </span>
-            }}
-          />
-        </p>
-        <Message className="positive">
-          <FormattedMessage
-            id="cloud.demo_expired.extend_trial_message"
-            defaultMessage="You won't pay anything whilst in your trial and you can still cancel at any time."
-          />
-        </Message>
-        <Segments className="horizontal">
-          <Segment className="address">
-            <Form>
-              <h4>
-                <i className="icon home" />
-                <FormattedMessage
-                  id="cloud.demo_expired.billing_address"
-                  defaultMessage="Billing address"
-                />
-              </h4>
-              <Field field="address" errors={this.props.errors}>
-                <label htmlFor="address">
+                </h4>
+                <Field field="address" errors={this.props.errors}>
+                  <label htmlFor="address">
+                    <FormattedMessage
+                      id="cloud.demo_expired.address"
+                      defaultMessage="Address"
+                    />
+                  </label>
+                  <TextArea
+                    id="address"
+                    name="address"
+                    onChange={this.props.onChangeAddress}
+                    value={this.props.address}
+                    rows={2}
+                  />
+                </Field>
+                <Field field="city" errors={this.props.errors}>
+                  <label htmlFor="city">
+                    <FormattedMessage
+                      id="cloud.demo_expired.city"
+                      defaultMessage="City"
+                    />
+                  </label>
+                  <Input
+                    id="city"
+                    onChange={this.props.onChangeCity}
+                    value={this.props.city}
+                  />
+                </Field>
+                <Field field="postcode" errors={this.props.errors}>
+                  <label htmlFor="postcode">
+                    <FormattedMessage
+                      id="cloud.demo_expired.post_code"
+                      defaultMessage="Zip / Post Code"
+                    />
+                  </label>
+                  <Input
+                    id="postcode"
+                    onChange={this.props.onChangePostCode}
+                    value={this.props.postCode}
+                  />
+                </Field>
+                {this.getState()}
+                <Field field="country" errors={this.props.errors}>
+                  <label htmlFor="country">
+                    <FormattedMessage
+                      id="cloud.demo_expired.country"
+                      defaultMessage="Country"
+                    />
+                  </label>
+                  <Select
+                    options={countriesOptions}
+                    placeholder={formatMessage(messages.select_placeholder)}
+                    onChange={this.props.onChangeCountry}
+                    filter
+                  />
+                </Field>
+              </Form>
+            </Segment>
+            <Segment className="credit-card">
+              <Form>
+                <h4>
+                  <i className="icon lock" />
                   <FormattedMessage
-                    id="cloud.demo_expired.address"
-                    defaultMessage="Address"
+                    id="cloud.demo_expired.credit_card_title"
+                    defaultMessage="Credit card details"
                   />
-                </label>
-                <TextArea
-                  id="address"
-                  name="address"
-                  onChange={this.props.onChangeAddress}
-                  value={this.props.address}
-                  rows={2}
-                />
-              </Field>
-              <Field field="city" errors={this.props.errors}>
-                <label htmlFor="city">
+                </h4>
+                <Field field="card_name" errors={this.props.errors}>
+                  <label htmlFor="card_holder">
+                    <FormattedMessage
+                      id="cloud.demo_expired.card_holder"
+                      defaultMessage="Card holder name"
+                    />
+                  </label>
+                  <Input
+                    id="card_holder"
+                    onChange={this.props.onChangeCardName}
+                    value={this.props.cardName}
+                    placeholder={formatMessage(messages.card_holder_placeholder)}
+                  />
+                </Field>
+                <Field className="card-number" field="card_number" errors={this.props.errors}>
+                  <label htmlFor="card_number">
+                    <FormattedMessage
+                      id="cloud.demo_expired.card_number"
+                      defaultMessage="Card number"
+                    />
+                    <Isvg
+                      className={classNames({ faded: (card.isCardAmex(cardNumber) || card.isCardMasterCard(cardNumber)) })}
+                      src={`${window.DESKPRO_APP_ASSETS_URL}/DeskPRO/Bundle/DemoBundle/Resources/img/visa.svg`}
+                    />
+                    <Isvg
+                      className={classNames({ faded: (card.isCardVisa(cardNumber) || card.isCardAmex(cardNumber)) })}
+                      src={`${window.DESKPRO_APP_ASSETS_URL}/DeskPRO/Bundle/DemoBundle/Resources/img/mastercard.svg`}
+                    />
+                    <Isvg
+                      className={classNames({ faded: (card.isCardMasterCard(cardNumber) || card.isCardVisa(cardNumber)) })}
+                      src={`${window.DESKPRO_APP_ASSETS_URL}/DeskPRO/Bundle/DemoBundle/Resources/img/amex.svg`}
+                    />
+                  </label>
+                  <Input
+                    id="card_number"
+                    onChange={this.props.onChangeCardNumber}
+                    value={card.formatCardNumber(this.props.cardNumber)}
+                    type="text"
+                  />
+                </Field>
+                <Field className="expiry" field="card_expiry" errors={this.props.errors}>
+                  <label htmlFor="expiry_month">Expiry</label>
+                  <Input
+                    id="expiry_month"
+                    onChange={this.props.onChangeExpiryMonth}
+                    value={this.props.expiryMonth}
+                    placeholder={formatMessage(messages.expiry_month)}
+                    type="number"
+                  />
+                  <span> / </span>
+                  <Input
+                    id="expiry_year"
+                    onChange={this.props.onChangeExpiryYear}
+                    value={this.props.expiryYear}
+                    placeholder={formatMessage(messages.expiry_year)}
+                    type="number"
+                  />
+                </Field>
+                <Field className="security-code" field="security_code" errors={this.props.errors}>
+                  <label htmlFor="security_code">
+                    <FormattedMessage
+                      id="cloud.demo_expired.security_code"
+                      defaultMessage="Security code"
+                    />
+                  </label>
+                  <Input
+                    id="security_code"
+                    onChange={this.props.onChangeSecurityCode}
+                    value={this.props.securityCode}
+                    type="number"
+                  />
+                  <img
+                    src={`${window.DESKPRO_APP_ASSETS_URL}/DeskPRO/Bundle/DemoBundle/Resources/img/backofcard.png`}
+                    alt="Back of card"
+                  />
+                </Field>
+                <Button onClick={this.props.onResumeTrial} className="positive">
                   <FormattedMessage
-                    id="cloud.demo_expired.city"
-                    defaultMessage="City"
+                    id="cloud.demo_expired.extend_trial_resume"
+                    defaultMessage="Resume free trial"
                   />
-                </label>
-                <Input
-                  id="city"
-                  onChange={this.props.onChangeCity}
-                  value={this.props.city}
-                />
-              </Field>
-              <Field field="postcode" errors={this.props.errors}>
-                <label htmlFor="postcode">
-                  <FormattedMessage
-                    id="cloud.demo_expired.post_code"
-                    defaultMessage="Zip / Post Code"
-                  />
-                </label>
-                <Input
-                  id="postcode"
-                  onChange={this.props.onChangePostCode}
-                  value={this.props.postCode}
-                />
-              </Field>
-              {this.getState()}
-              <Field field="country" errors={this.props.errors}>
-                <label htmlFor="country">
-                  <FormattedMessage
-                    id="cloud.demo_expired.country"
-                    defaultMessage="Country"
-                  />
-                </label>
-                <Select
-                  options={countriesOptions}
-                  placeholder={formatMessage(messages.select_placeholder)}
-                  onChange={this.props.onChangeCountry}
-                  filter
-                />
-              </Field>
-            </Form>
-          </Segment>
-          <Segment className="credit-card">
-            <Form>
-              <h4>
-                <i className="icon lock" />
-                <FormattedMessage
-                  id="cloud.demo_expired.credit_card_title"
-                  defaultMessage="Credit card details"
-                />
-              </h4>
-              <Field field="card_name" errors={this.props.errors}>
-                <label htmlFor="card_holder">
-                  <FormattedMessage
-                    id="cloud.demo_expired.card_holder"
-                    defaultMessage="Card holder name"
-                  />
-                </label>
-                <Input
-                  id="card_holder"
-                  onChange={this.props.onChangeCardName}
-                  value={this.props.cardName}
-                  placeholder={formatMessage(messages.card_holder_placeholder)}
-                />
-              </Field>
-              <Field className="card-number" field="card_number" errors={this.props.errors}>
-                <label htmlFor="card_number">
-                  <FormattedMessage
-                    id="cloud.demo_expired.card_number"
-                    defaultMessage="Card number"
-                  />
-                  <Isvg
-                    className={classNames({ faded: (card.isCardAmex(cardNumber) || card.isCardMasterCard(cardNumber)) })}
-                    src={`${window.DESKPRO_APP_ASSETS_URL}/DeskPRO/Bundle/DemoBundle/Resources/img/visa.svg`}
-                  />
-                  <Isvg
-                    className={classNames({ faded: (card.isCardVisa(cardNumber) || card.isCardAmex(cardNumber)) })}
-                    src={`${window.DESKPRO_APP_ASSETS_URL}/DeskPRO/Bundle/DemoBundle/Resources/img/mastercard.svg`}
-                  />
-                  <Isvg
-                    className={classNames({ faded: (card.isCardMasterCard(cardNumber) || card.isCardVisa(cardNumber)) })}
-                    src={`${window.DESKPRO_APP_ASSETS_URL}/DeskPRO/Bundle/DemoBundle/Resources/img/amex.svg`}
-                  />
-                </label>
-                <Input
-                  id="card_number"
-                  onChange={this.props.onChangeCardNumber}
-                  value={card.formatCardNumber(this.props.cardNumber)}
-                  type="text"
-                />
-              </Field>
-              <Field className="expiry" field="card_expiry" errors={this.props.errors}>
-                <label htmlFor="expiry_month">Expiry</label>
-                <Input
-                  id="expiry_month"
-                  onChange={this.props.onChangeExpiryMonth}
-                  value={this.props.expiryMonth}
-                  placeholder={formatMessage(messages.expiry_month)}
-                  type="number"
-                />
-                <span> / </span>
-                <Input
-                  id="expiry_year"
-                  onChange={this.props.onChangeExpiryYear}
-                  value={this.props.expiryYear}
-                  placeholder={formatMessage(messages.expiry_year)}
-                  type="number"
-                />
-              </Field>
-              <Field className="security-code" field="security_code" errors={this.props.errors}>
-                <label htmlFor="security_code">
-                  <FormattedMessage
-                    id="cloud.demo_expired.security_code"
-                    defaultMessage="Security code"
-                  />
-                </label>
-                <Input
-                  id="security_code"
-                  onChange={this.props.onChangeSecurityCode}
-                  value={this.props.securityCode}
-                  type="number"
-                />
-                <img
-                  src={`${window.DESKPRO_APP_ASSETS_URL}/DeskPRO/Bundle/DemoBundle/Resources/img/backofcard.png`}
-                  alt="Back of card"
-                />
-              </Field>
-              <Button onClick={this.props.onResumeTrial} className="positive">
-                <FormattedMessage
-                  id="cloud.demo_expired.extend_trial_resume"
-                  defaultMessage="Resume free trial"
-                />
-                {this.props.submit && <span> <i className="fa fa-spinner fa-pulse fa-fw margin-bottom" /></span>}
-              </Button>
-            </Form>
-          </Segment>
-        </Segments>
-      </Segment>
+                  {this.props.submit && <span> <i className="fa fa-spinner fa-pulse fa-fw margin-bottom" /></span>}
+                </Button>
+              </Form>
+            </Segment>
+          </Segments>
+        </Segment>
+        <Segment className="extend-trial-side-bar">
+          <div className="offer">
+            <div className="intro">
+              <FormattedMessage
+                id="cloud.demo_expired.offer_text"
+                defaultMessage="One plan and one price, with all features for everyone."
+              />
+            </div>
+            <div className="price">$30</div>
+            <div className="unit">
+              <FormattedMessage
+                id="cloud.demo_expired.offer_unit"
+                defaultMessage="per agent/month"
+              />
+            </div>
+          </div>
+          <Button className="questions">
+            <i className="icon comments outline" />
+            <FormattedMessage
+              id="cloud.demo_expired.got_questions"
+              defaultMessage="Got questions? Just ask..."
+            />
+          </Button>
+          <Button className="delete primary" onClick={this.props.onDeleteAccount}>
+            <FormattedMessage
+              id="cloud.demo_expired.delete"
+              defaultMessage="Delete your account"
+            />
+          </Button>
+        </Segment>
+      </Segments>
     );
   }
 }
