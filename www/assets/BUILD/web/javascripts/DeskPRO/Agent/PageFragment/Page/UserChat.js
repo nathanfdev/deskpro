342927986dfa0b0dc934cd0550e174b6b9a6da1c
ImmortaL <denis@ranneft.ru>
2016-09-28 19:19:20 +0300

    use n3bs inserHtml by default, leave special insertSnippetHtml only for vars

--- www/assets/BUILD/web/javascripts/DeskPRO/Agent/PageFragment/Page/UserChat.js
+++ www/assets/BUILD/web/javascripts/DeskPRO/Agent/PageFragment/Page/UserChat.js
@@ -446,7 +446,7 @@ DeskPRO.Agent.PageFragment.Page.UserChat = new Orb.Class({
 					html = html.replace(/<\/p>\s*<p>/g, '<br/>');
 					html = html.replace(/^<p>/, '');
 					html = html.replace(/<\/p>$/, '');
-					messageTextarea.data('redactor').insertSnippetHtml(html);
+					messageTextarea.data('redactor').insertHtml(html);
 					messageTextarea.change();
 					window.setTimeout(function() {
 						var tmp = ed.height();
