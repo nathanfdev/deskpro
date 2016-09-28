d98c6a2071e96b29e23581f07d1c419895f73a92
Sergey <laa513@gmail.com>
2016-09-28 04:11:39 +0300

    DB upgrade file

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
@@ -1787,4 +1787,8 @@ return [
         'file'      => '/src/Application/InstallBundle/Upgrade/Build/2016/09/Build1473175621.php',
         'classname' => 'Application\InstallBundle\Upgrade\Build\Build1473175621',
     ],
+    1475024916 => [
+        'file'      => '/src/Application/InstallBundle/Upgrade/Build/2016/09/Build1475024916.php',
+        'classname' => 'Application\InstallBundle\Upgrade\Build\Build1475024916',
+    ],
 ];
