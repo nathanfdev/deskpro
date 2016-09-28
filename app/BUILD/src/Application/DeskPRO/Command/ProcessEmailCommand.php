53ef1f149ec9e06037e33fb93874c981486f9479
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-09-28 12:50:25 +0100

    Fix async email processing tools

--- app/BUILD/src/Application/DeskPRO/Command/ProcessEmailCommand.php
+++ app/BUILD/src/Application/DeskPRO/Command/ProcessEmailCommand.php
@@ -294,7 +294,7 @@ class ProcessEmailCommand extends ContainerAwareCommand
             /* @var \DpRun\DpEnv $DP_ENV */
             global $DP_ENV;
 
-            if ($DP_ENV->getConfig('adv_email_process')) {
+            if ($DP_ENV->getConfig('async_email_processing.process')) {
                 /** @var \Application\EmailBundle\Incoming\ProcQueue\ProcQueueInterface $proc */
                 $proc = App::getContainer()->get('in_email.proc_queue');
                 $proc->enqueueNewEmail($source);
