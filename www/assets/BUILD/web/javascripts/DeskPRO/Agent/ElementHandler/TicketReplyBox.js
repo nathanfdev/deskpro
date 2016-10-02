057250beb3fd43357ebaddd667b99ef304b323d2
n3b <neb802@gmail.com>
2016-10-02 06:47:21 -0300

    retry TicketReplyBox init to avoid race condition errors

--- www/assets/BUILD/web/javascripts/DeskPRO/Agent/ElementHandler/TicketReplyBox.js
+++ www/assets/BUILD/web/javascripts/DeskPRO/Agent/ElementHandler/TicketReplyBox.js
@@ -15,7 +15,7 @@ DeskPRO.Agent.ElementHandler.TicketReplyBox = new Orb.Class({
 		this.page = this.el.closest('.with-page-fragment').data('page-fragment');
 
 		if (!this.page && this.initRetries-- > 0) {
-			setTimeout(this.initPage.bind(this), this.initRetryTimeout);
+			return setTimeout(this.initPage.bind(this), this.initRetryTimeout);
 		}
 
 		this.lang = eval(this.el.data('dp-lang') || '{}');
