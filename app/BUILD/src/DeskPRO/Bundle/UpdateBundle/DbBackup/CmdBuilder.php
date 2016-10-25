25b2fe833e45a48c26745c13e016751df7e4143f
n3b <neb802@gmail.com>
2016-10-24 23:47:20 -0300

    wrong argument

--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/DbBackup/CmdBuilder.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/DbBackup/CmdBuilder.php
@@ -78,9 +78,9 @@ class CmdBuilder implements CmdBuilderInterface
 
         $cmd = array_merge($cmd, [
             '-u '.escapeshellarg($dbInfo['user']),
-            '-p'.escapeshellarg($dbInfo['password']),
+            '-p'.$password,
             '--opt', '-Q', '--hex-blob', '--lock-tables=false', '--single-transaction',
-            $password,
+            escapeshellarg($dbInfo['dbname']),
             '>', escapeshellarg($filename),
         ]);
 
