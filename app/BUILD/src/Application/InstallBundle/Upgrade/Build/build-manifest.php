76239c934ba0f0453c77510c55019223e842361c
n3b <neb802@gmail.com>
2016-10-31 11:00:10 -0300

    upgrade script

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/build-manifest.php
@@ -1811,4 +1811,8 @@ return [
         'file'      => '/src/Application/InstallBundle/Upgrade/Build/2016/10/Build1475840809.php',
         'classname' => 'Application\InstallBundle\Upgrade\Build\Build1475840809',
     ],
+    1477919298 => [
+        'file'      => '/src/Application/InstallBundle/Upgrade/Build/2016/10/Build1477919298.php',
+        'classname' => 'Application\InstallBundle\Upgrade\Build\Build1477919298',
+    ],
 ];
