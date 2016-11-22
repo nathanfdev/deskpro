e8291447078ec618fa1bb53066ba70952e81b5a3
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-11-22 15:01:35 +0000

    Remove 65 MB GeoIP-City database. We do not need that level of granularity.

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/DependencyInjection/SystemServices/EnvironmentService.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/DependencyInjection/SystemServices/EnvironmentService.php
@@ -57,7 +57,7 @@ class EnvironmentService
     public function getGeoIp()
     {
         if (!$this->geo_reader) {
-            $this->geo_reader = new Reader(DP_ROOT.'/vendor-src/geoip-db/GeoLite2-City.mmdb');
+            $this->geo_reader = new Reader(DP_ROOT.'/vendor-src/geoip-db/GeoLite2-Country.mmdb');
         }
 
         $result = [
@@ -71,7 +71,7 @@ class EnvironmentService
 
         $ip = $this->getUserIp();
         try {
-            $record = $this->geo_reader->city($ip);
+            $record = $this->geo_reader->country($ip);
         } catch (\Exception $e) {
             return $result;
         }
