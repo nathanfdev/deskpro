976b8b3181d5284a02b3903fcfc313222bcd6c7c
ImmortaL <denis@ranneft.ru>
2016-11-24 09:33:19 +0300

    add missing getter and setter

--- app/BUILD/src/Application/ImportBundle/Model/Person.php
+++ app/BUILD/src/Application/ImportBundle/Model/Person.php
@@ -413,6 +413,26 @@ class Person implements LabelAwareModelInterface, LanguageAwareInterface, Custom
     }
 
     /**
+     * @return string
+     */
+    public function getTitlePrefix()
+    {
+        return $this->titlePrefix;
+    }
+
+    /**
+     * @param string $titlePrefix
+     *
+     * @return $this
+     */
+    public function setTitlePrefix($titlePrefix)
+    {
+        $this->titlePrefix = $titlePrefix;
+
+        return $this;
+    }
+
+    /**
      * Returns custom display name.
      *
      * @return string
