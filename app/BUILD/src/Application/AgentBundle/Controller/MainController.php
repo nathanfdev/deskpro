fe7ead6f155796b7f86c7b9ffa1c9ce362d262ef
ImmortaL <denis@ranneft.ru>
2016-11-28 12:33:58 +0300

    use just AgentPermissions helper

--- app/BUILD/src/Application/AgentBundle/Controller/MainController.php
+++ app/BUILD/src/Application/AgentBundle/Controller/MainController.php
@@ -203,7 +203,6 @@ class MainController extends AbstractController
         $textSnippetCategoryRepository = $this->em->getRepository(TextSnippetCategory::class);
         $ticket_snippet_cats           = $textSnippetCategoryRepository->getCatsForAgent('tickets', $this->person);
         $chat_snippet_cats             = $textSnippetCategoryRepository->getCatsForAgent('chat', $this->person);
-        $permissionBag                 = $this->get('permissions_manager')->getPortalPermissionsBag($this->getUser());
 
         return $this->render('AgentBundle:Main:index.html.twig', [
             'has_raw_assets'      => $has_raw_assets,
@@ -216,7 +215,7 @@ class MainController extends AbstractController
             'agents'              => $agents,
             'agent_teams'         => $agent_teams,
             'agent_chat_depmap'   => $agent_chat_depmap,
-            'chat_dep_ids'        => $permissionBag->getAllowedChatDepartmentIds(),
+            'chat_dep_ids'        => $this->person->getHelper('AgentPermissions')->getAllowedDepartments('chat'),
             'ticket_snippet_cats' => $ticket_snippet_cats,
             'chat_snippet_cats'   => $chat_snippet_cats,
             'brand_app_settings'  => $this->getBrandAppSettings(),
