fd2ba49f6b003e74a19fc43ab6040fa2faa5eb97
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-11-29 10:39:01 +0000

    Add dp:utility:export-blob command to easily get blobs from cli

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/AppBundle.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/AppBundle.php
@@ -34,6 +34,7 @@ namespace DeskPRO\Bundle\AppBundle;
 
 use DeskPRO\Bundle\AppBundle\Command\Configure\ConfigElasticCommand;
 use DeskPRO\Bundle\AppBundle\Command\ServerInfo\WebServerInfoCommand;
+use DeskPRO\Bundle\AppBundle\Command\Utility\ExportBlobCommand;
 use DeskPRO\Bundle\AppBundle\Command\Utility\RecompileTemplatesCommand;
 use DeskPRO\Bundle\AppBundle\DependencyInjection\AppExtension;
 use DeskPRO\Bundle\AppBundle\DependencyInjection\Compiler\AppSecretPass;
@@ -95,6 +96,7 @@ class AppBundle extends Bundle
         $application->add(new WebServerInfoCommand());
         $application->add(new ConfigElasticCommand());
         $application->add(new RecompileTemplatesCommand());
+        $application->add(new ExportBlobCommand());
     }
 
     /**
