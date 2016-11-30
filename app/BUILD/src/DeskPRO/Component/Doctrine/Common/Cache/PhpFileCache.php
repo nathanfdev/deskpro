4555f8ec7eb9649e8c1a0444cd81933c1929eaa5
n3b <neb802@gmail.com>
2016-11-29 22:10:12 -0300

    this one also

--- app/BUILD/src/DeskPRO/Component/Doctrine/Common/Cache/PhpFileCache.php
+++ app/BUILD/src/DeskPRO/Component/Doctrine/Common/Cache/PhpFileCache.php
@@ -35,6 +35,14 @@ class PhpFileCache extends BasePhpFileCache
     /**
      * {@inheritdoc}
      */
+    public function __construct($directory, $extension = self::EXTENSION)
+    {
+        parent::__construct($directory, $extension, umask());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function getFilename($id)
     {
         return FileCacheUtil::getFilename($id, $this->getDirectory(), $this->getExtension());
