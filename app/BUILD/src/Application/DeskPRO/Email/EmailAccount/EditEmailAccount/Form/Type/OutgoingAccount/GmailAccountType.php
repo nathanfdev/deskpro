22b39aa8203d13fbbddd589010ac66f6f3332582
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-12-07 09:32:34 +0000

    Revert email account setup to use old gmail method

--- app/BUILD/src/Application/DeskPRO/Email/EmailAccount/EditEmailAccount/Form/Type/OutgoingAccount/GmailAccountType.php
+++ app/BUILD/src/Application/DeskPRO/Email/EmailAccount/EditEmailAccount/Form/Type/OutgoingAccount/GmailAccountType.php
@@ -42,6 +42,7 @@ class GmailAccountType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('user', 'email', ['required' => true]);
+        $builder->add('password', 'dp_enc_password', ['required' => false]);
         $builder->add('token', 'text', ['required' => true]);
         $builder->add('refreshToken', 'text', ['required' => true]);
         $builder->add('clientId', 'text', ['required' => true]);
