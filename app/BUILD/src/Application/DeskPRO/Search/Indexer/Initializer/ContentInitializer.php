31d1105af31786720e3b3b634ff46a31e3ecf877
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-12-07 12:56:17 +0000

    Fix namespaces

--- app/BUILD/src/Application/DeskPRO/Search/Indexer/Initializer/ContentInitializer.php
+++ app/BUILD/src/Application/DeskPRO/Search/Indexer/Initializer/ContentInitializer.php
@@ -30,11 +30,10 @@
  * DeskPRO.
  */
 
-namespace Application\DeskPRO\Search\IndexInitializer;
+namespace Application\DeskPRO\Search\Indexer\Initializer;
 
 use Application\DeskPRO\App;
 use Application\DeskPRO\Log\Logger;
-use Application\DeskPRO\Search\Indexer\Initializer\AbstractInitializer;
 
 abstract class ContentInitializer extends AbstractInitializer
 {
