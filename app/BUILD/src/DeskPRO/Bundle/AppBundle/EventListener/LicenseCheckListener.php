cb983e7891a620d239e07924597041a768ccdeef
Christopher Nadeau <chris.nadeau@deskpro.com>
2016-12-19 11:58:49 +0000

    redirect to single-page admin if helpdesk is offline

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/EventListener/LicenseCheckListener.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/EventListener/LicenseCheckListener.php
@@ -38,6 +38,7 @@ use DpSys\License;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -134,7 +135,7 @@ final class LicenseCheckListener implements EventSubscriberInterface
                 return;
             }
 
-            $event->setResponse($this->getLicErrorPageResponse('You have not entered a license code. Go to /admin and enter your license code now.'));
+            $event->setResponse($this->getLicErrorPageResponse($request, 'You have not entered a license code. Go to /admin and enter your license code now.'));
             $event->stopPropagation();
 
             return;
@@ -146,7 +147,7 @@ final class LicenseCheckListener implements EventSubscriberInterface
             $count = $db->fetchColumn('SELECT COUNT(*) FROM people WHERE is_agent = 1 AND is_deleted = 0');
 
             if ($count > $lic->getMaxAgents()) {
-                $event->setResponse($this->getLicErrorPageResponse("
+                $event->setResponse($this->getLicErrorPageResponse($request, "
                     Your helpdesk is using more agents than your license allows.<br/><br/>
                     - Number of agents: {$count}<br/><br/>
                     - Number of seats available: {$lic->getMaxAgents()}<br/><br/>
@@ -166,7 +167,7 @@ final class LicenseCheckListener implements EventSubscriberInterface
             || ($this->interfaceInfo->isUserInterface() && $lic->isPastExpireDate() >= 14)
         )) {
             $date = $lic->getExpireDate()->format('F jS');
-            $event->setResponse($this->getLicErrorPageResponse("
+            $event->setResponse($this->getLicErrorPageResponse($request, "
                 Your helpdesk license expired on {$date}. To continue using your helpdesk,
                 an administrator needs to renew the license.
             "));
@@ -188,7 +189,7 @@ final class LicenseCheckListener implements EventSubscriberInterface
 
         // Expired demos
         if ($lic->isDemo() && $lic->isPastExpireDate()) {
-            $event->setResponse($this->getCloudErrorPageResponse('cloud-error.demo-expired.html'));
+            $event->setResponse($this->getCloudErrorPageResponse($request, 'cloud-error.demo-expired.html'));
             $event->stopPropagation();
 
             return;
@@ -200,7 +201,7 @@ final class LicenseCheckListener implements EventSubscriberInterface
                 ($this->interfaceInfo->isAgentInterface() && defined('DPC_AGENT_OFF') && DPC_AGENT_OFF)
                 || ($this->interfaceInfo->isUserInterface() && defined('DPC_USER_OFF') && DPC_USER_OFF)
             ) {
-                $event->setResponse($this->getCloudErrorPageResponse('cloud-error.bill-failed.html'));
+                $event->setResponse($this->getCloudErrorPageResponse($request, 'cloud-error.bill-failed.html'));
                 $event->stopPropagation();
 
                 return;
@@ -213,7 +214,7 @@ final class LicenseCheckListener implements EventSubscriberInterface
             || ($this->interfaceInfo->isAgentInterface() && defined('DPC_AGENT_OFF') && DPC_AGENT_OFF)
             || ($this->interfaceInfo->isUserInterface() && defined('DPC_USER_OFF') && DPC_USER_OFF)
         ) {
-            $event->setResponse($this->getCloudErrorPageResponse('cloud-error.offline.html', defined('DPC_OFF_REASON') ? DPC_OFF_REASON : ''));
+            $event->setResponse($this->getCloudErrorPageResponse($request, 'cloud-error.offline.html', defined('DPC_OFF_REASON') ? DPC_OFF_REASON : ''));
             $event->stopPropagation();
 
             return;
@@ -221,7 +222,7 @@ final class LicenseCheckListener implements EventSubscriberInterface
 
         // The whole site might be offline
         if (defined('DPC_SYS_DISABLED') && DPC_SYS_DISABLED) {
-            $event->setResponse($this->getCloudErrorPageResponse('cloud-error.offline.html', ''));
+            $event->setResponse($this->getCloudErrorPageResponse($request, 'cloud-error.offline.html', ''));
             $event->stopPropagation();
 
             return;
@@ -229,24 +230,26 @@ final class LicenseCheckListener implements EventSubscriberInterface
     }
 
     /**
-     * @param string $message
+     * @param Request $request
+     * @param string  $message
      *
      * @return Response
      */
-    private function getLicErrorPageResponse($message)
+    private function getLicErrorPageResponse(Request $request, $message)
     {
-        $response = new Response($this->getLicErrorPage($message));
+        $response = new Response($this->getLicErrorPage($request, $message));
         $response->headers->set('X-DeskPRO-ErrorType', 'license');
 
         return $response;
     }
 
     /**
-     * @param string $message
+     * @param Request $request
+     * @param string  $message
      *
      * @return string
      */
-    private function getLicErrorPage($message)
+    private function getLicErrorPage(Request $request, $message)
     {
         $asset_url = $this->assetUrl;
         $tpl       = 'lic-error.html';
@@ -254,38 +257,71 @@ final class LicenseCheckListener implements EventSubscriberInterface
         $page_html = @file_get_contents(DP_ROOT.'/src/DeskPRO/Bundle/AppBundle/Resources/views/kernel/'.$tpl) ?: '{{ CONTENT }}';
         $page_html = str_replace('{{ ASSET_URL }}', $asset_url, $page_html);
         $page_html = str_replace('{{ CONTENT }}', $message, $page_html);
+        $page_html = $this->injectAdminRedirect($request, $page_html);
 
         return $page_html;
     }
 
     /**
-     * @param string $tpl
-     * @param string $message
+     * @param Request $request
+     * @param string  $tpl
+     * @param string  $message
      *
      * @return Response
      */
-    private function getCloudErrorPageResponse($tpl, $message = '')
+    private function getCloudErrorPageResponse(Request $request, $tpl, $message = '')
     {
-        $response = new Response($this->getCloudErrorPage($tpl, $message));
+        $response = new Response($this->getCloudErrorPage($request, $tpl, $message));
         $response->headers->set('X-DeskPRO-ErrorType', 'license');
 
         return $response;
     }
 
     /**
-     * @param string $tpl
-     * @param string $message
+     * @param Request $request
+     * @param string  $tpl
+     * @param string  $message
      *
      * @return string
      */
-    private function getCloudErrorPage($tpl, $message = '')
+    private function getCloudErrorPage(Request $request, $tpl, $message = '')
     {
         $asset_url = $this->assetUrl;
 
         $page_html = @file_get_contents(DP_ROOT.'/src/DeskPRO/Bundle/AppBundle/Resources/views/kernel/'.$tpl) ?: '{{ CONTENT }}';
         $page_html = str_replace('{{ ASSET_URL }}', $asset_url, $page_html);
         $page_html = str_replace('{{ CONTENT }}', $message, $page_html);
+        $page_html = $this->injectAdminRedirect($request, $page_html);
 
         return $page_html;
+    }
+
+    /**
+     * @param Request $request
+     * @param Request $request
+     * @param string  $html
+     *
+     * @return string
+     */
+    private function injectAdminRedirect(Request $request, $html)
+    {
+        $url          = addslashes($request->getUriForPath('/admin/admin-interface')).'#';
+        $redirectHtml = <<<HTML
+<script>
+(function() {
+  var hash = window.location.hash.substr(1);
+  var m    = hash.match(/^admin:(.*?)$/);
+  var path = window.location.pathname + '';
+  
+  if (m && !path.match(/admin\-interface\/?$/)) {
+    window.location = '{$url}' + m[1];
+  }
+})();
+</script>
+HTML;
+
+        $html = str_replace('</head>', "{$redirectHtml}</head>", $html);
+
+        return $html;
     }
 }
