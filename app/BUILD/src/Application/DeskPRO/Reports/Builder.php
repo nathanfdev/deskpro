5b433b5dfbd510760904b78623c3cf091ef90c20
ImmortaL <denis@ranneft.ru>
2016-12-19 14:17:58 +0300

    make reports interface work

--- app/BUILD/src/Application/DeskPRO/Reports/Builder.php
+++ app/BUILD/src/Application/DeskPRO/Reports/Builder.php
@@ -54,7 +54,7 @@ class Builder
     public function __construct(EntityManager $em)
     {
         $this->em         = $em;
-        $this->repository = $this->em->getRepository('DeskPRO:ReportWidget');
+        $this->repository = $this->em->getRepository('DeskPRO:ReportBuilder');
         $this->in         = App::getContainer()->getIn();
     }
 
