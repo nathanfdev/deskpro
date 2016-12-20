daedb15f2a7d3a0f16ca7c44ed870097514107db
n3b <neb802@gmail.com>
2016-12-20 19:39:33 -0300

    dependencies for legacy assets

--- app/BUILD/sys/config/config.assets.php
+++ app/BUILD/sys/config/config.assets.php
@@ -50,12 +50,12 @@ $CONFIG = [];
 
 $CONFIG['OPTIONS'] = [
     'java_path'      => defined('DP_JAVA_PATH') ? DP_JAVA_PATH : '/usr/bin/java',
-    'yui_compressor' => defined('DP_YUI_COMPRESSOR_PATH') ? DP_YUI_COMPRESSOR_PATH : '/usr/local/bin/yuicompressor.jar',
+    'yui_compressor' => defined('DP_YUI_COMPRESSOR_PATH') ? DP_YUI_COMPRESSOR_PATH : DP_ROOT.'/dev/vendor/yuicompressor.jar',
     'nodejs'         => defined('DP_NODEJS_PATH') ? DP_NODEJS_PATH : '/usr/bin/nodejs',
     'less'           => defined('DP_LESSC_PATH') ? DP_LESSC_PATH : '/usr/local/bin/lessc',
     'smartsprites'   => defined(
         'DP_SMARTSPRITES_PATH'
-    ) ? DP_SMARTSPRITES_PATH : '/opt/smartsprites-0.2.8/smartsprites.sh',
+    ) ? DP_SMARTSPRITES_PATH : DP_ROOT.'/dev/vendor/smartsprites/smartsprites.sh',
 ];
 
 //##############################################################################
