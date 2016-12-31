11f43607e4ff51365e413268a7f6c43dfa2e69b9
n3b <neb802@gmail.com>
2016-12-31 09:32:16 -0800

    htmlentities on error phrases with params

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Error/ErrorMessageFactory.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Error/ErrorMessageFactory.php
@@ -66,6 +66,10 @@ class ErrorMessageFactory
      */
     public function createMessage($errorCode, array $params = [])
     {
+        foreach ($params as &$param) {
+            $param = htmlentities($param);
+        }
+
         return $this->translate->phrase($this->prefix.$errorCode, $params) ?: $errorCode;
     }
 
