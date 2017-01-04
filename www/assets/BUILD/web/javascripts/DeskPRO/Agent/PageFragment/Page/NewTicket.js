40b59e914dc3479f6f52a85bed361a8a55973f11
n3b <neb802@gmail.com>
2017-01-04 13:16:13 -0800

    fix new ticket layout fields

--- www/assets/BUILD/web/javascripts/DeskPRO/Agent/PageFragment/Page/NewTicket.js
+++ www/assets/BUILD/web/javascripts/DeskPRO/Agent/PageFragment/Page/NewTicket.js
@@ -223,11 +223,15 @@ DeskPRO.Agent.PageFragment.Page.NewTicket = new Orb.Class({
 				if ($field.is(':checkbox')) {
 					return $field.is(':checked');
 				}
-				if ($field.is('select, input:not(:radio, :checkbox), textarea')) {
-					return $field.val();
+        if ($field.attr('type') === 'hidden') {
+					return $.trim($field.parent().text());
 				}
+        if ($field.is('input:not(:radio, :checkbox), textarea')) {
+          return $field.val();
+        }
 				$field = $('[name="' + name + '"], [name="' + name + '[]"]', $cont);
 				if ($field.hasClass('with-select2')) {
+          var val = $.trim($field.select2('val'));
 					return $field.select2('val');
 				}
 				return $field.filter(':checked').map(function(i, el) { return el.value; }).get();
