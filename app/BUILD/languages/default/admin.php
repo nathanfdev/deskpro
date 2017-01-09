a7bec20d8dc72010d9883cb2f6a1229f394db90b
Sergey Yastrebov <serg.yastrebov@gmail.com>
2017-01-09 16:43:01 +0300

    [widget setup] renamed require userinfo label

--- app/BUILD/languages/default/admin.php
+++ app/BUILD/languages/default/admin.php
@@ -2298,7 +2298,7 @@ return [
     'admin.widget.chat_permissions_desc'                                                   => 'Manage which usergroups have access to chat',
     'admin.widget.chat_permissions_title'                                                  => 'Who can use chat',
     'admin.widget.chat_request_person_desc'                                                => 'Require users to provide their name and email address as well as adding custom fields or require departments.',
-    'admin.widget.chat_request_person_for_comments'                                        => 'Require information before chat commences',
+    'admin.widget.chat_request_person_for_comments'                                        => 'Ask for information before chat commences',
     'admin.widget.chat_allow_department_selection'                                         => 'Require department to chat with',
     'admin.widget.chat_require_login'                                                      => 'Require login',
     'admin.widget.chat_settings'                                                           => 'Chat settings',
