83ecaa29b65f677decec6001c828033e630b46cb
Sergey Yastrebov <serg.yastrebov@gmail.com>
2017-01-10 19:10:28 +0300

    prevent edit mode on text select

--- www/assets/BUILD/web/javascripts/DeskPRO/Agent/PageHelper/TicketFields.js
+++ www/assets/BUILD/web/javascripts/DeskPRO/Agent/PageHelper/TicketFields.js
@@ -116,6 +116,26 @@ DeskPRO.Agent.PageHelper.TicketFields = new Orb.Class({
 		$scope.editField = function(field) {
 			if (!$scope.editables[field]) return;
 			if ($scope.isEditMode(field)) return;
+
+			var getSelected = function() {
+				if (window.getSelection) {
+					return window.getSelection().toString();
+				} else if (document.getSelection) {
+					return document.getSelection().toString();
+				} else {
+					var selection = document.selection && document.selection.createRange();
+					if (selection.text) {
+						return selection.text.toString();
+					}
+					return '';
+				}
+				return '';
+			};
+
+			if (getSelected()) {
+				return;
+			}
+
 			$scope.edit_fields.push(field);
 		};
 
