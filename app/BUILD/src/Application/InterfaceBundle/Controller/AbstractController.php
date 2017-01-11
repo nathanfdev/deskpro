b48b2e7419fc377201d68f553ac7d4537e80aa08
ImmortaL <denis@ranneft.ru>
2017-01-11 22:42:52 +0300

    do not use unexisting methods

--- app/BUILD/src/Application/InterfaceBundle/Controller/AbstractController.php
+++ app/BUILD/src/Application/InterfaceBundle/Controller/AbstractController.php
@@ -30,6 +30,7 @@ namespace Application\InterfaceBundle\Controller;
 
 use Application\DeskPRO\Service\CheckWhitelistedIP;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 abstract class AbstractController extends \Application\DeskPRO\Controller\AbstractController
 {
@@ -84,7 +85,7 @@ abstract class AbstractController extends \Application\DeskPRO\Controller\Abstra
 
                 return $this->createJsonResponse($data, 403);
             } else {
-                return $this->renderStandardPermissionError('The form you are trying to submit has expired. Please go back and try again.');
+                throw new BadRequestHttpException('The form you are trying to submit has expired. Please go back and try again.');
             }
         }
 
