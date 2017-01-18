2bccf6510966f3bf7b4bd53aae2e1b7da01f7e50
ImmortaL <denis@ranneft.ru>
2017-01-18 20:22:08 +0300

    preload 10 recent chats

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/TopBar/Components/AgentTopBar.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/TopBar/Components/AgentTopBar.js
@@ -171,7 +171,7 @@ export class AgentTopBarContainer extends SeparateComponent {
     if (window.DP_HAS_NEW_IM) {
       this.props.dispatch(loadFromApi(
         'AgentChat',
-        'DP_API/agent_chats?order_by=date_last_message&order_dir=desc&count=5',
+        'DP_API/agent_chats?order_by=date_last_message&order_dir=desc&count=10',
         'recent'
       ));
       this.props.dispatch(loadFromApi(
