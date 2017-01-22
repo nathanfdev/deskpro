d7b9df79aa55f1cdd5c1db3311ca0a3def89b9df
n3b <neb802@gmail.com>
2017-01-22 12:45:12 -0800

    cancel button handling

--- www/assets/BUILD/web/javascripts/DeskPRO/Agent/PageHelper/TicketFields.js
+++ www/assets/BUILD/web/javascripts/DeskPRO/Agent/PageHelper/TicketFields.js
@@ -51,7 +51,7 @@ DeskPRO.Agent.PageHelper.TicketFields = new Orb.Class({
 				$field = $('[name="' + name + '"], [name="' + name + '[]"]', $holders);
 				if ($field.hasClass('with-select2')) {
 					var val = $.trim($field.select2('val'));
-					return $.trim(val) ? val : null;
+					return val || null;
 				}
 				return $field.filter(':checked').map(function(i, el) { return el.value; }).get();
 			},
@@ -109,9 +109,40 @@ DeskPRO.Agent.PageHelper.TicketFields = new Orb.Class({
 			language: 1,
 			problem: 1
 		};
+		$scope.savedValues = {};
 
 		$scope.show_hidden = 0;
 		$scope.is_saving = false;
+
+		$scope.setFieldValue = function(id, value, allowDefaultValue) {
+      self.display.find('.item.'+id).each(function(i, el) {
+        var $el = $(el);
+        value = allowDefaultValue ? $el.data('default-value') : value;
+        $el.find('input[type=text], textarea, select').val(value);
+        $el.find('.with-select2').select2('val', value);
+        $el.find('input[type=radio]').each(function(i, field) {
+          var $field = $(field);
+          if ($field.val() === String(value)) {
+            $field.prop('checked', true);
+          } else {
+            $field.prop('checked', false);
+          }
+        });
+        $el.find('input[type=checkbox]').each(function(i, field) {
+          var $field = $(field);
+          if ($field.attr('name') && $field.attr('name').indexOf('[]') !== -1) {
+            var vals = value ? String(value).split(',') : [];
+            if (vals.indexOf(String($field.val())) !== -1) {
+              $field.prop('checked', true);
+            } else {
+              $field.prop('checked', false);
+            }
+          } else {
+            $field.prop('checked', value);
+          }
+        });
+      });
+		};
 
 		$scope.editField = function($event, field) {
 			if (!$scope.editables[field]) return;
@@ -137,6 +168,7 @@ DeskPRO.Agent.PageHelper.TicketFields = new Orb.Class({
 				return;
 			}
 
+      $scope.savedValues[field] = self.ticketReader.getTicketFieldValue(field.replace('ticket_field_', ''));
 			$scope.edit_fields.push(field);
 
 			// focus input field on open edit mode
@@ -159,7 +191,12 @@ DeskPRO.Agent.PageHelper.TicketFields = new Orb.Class({
 		};
 
 		$scope.cancelEdit = function() {
+      $scope.edit_fields.forEach(function(id){
+      	$scope.setFieldValue(id, $scope.savedValues[id]);
+      	delete $scope.savedValues[id];
+			});
       $scope.edit_fields.length = 0;
+      self.updateDisplay();
 		};
 
 		$scope.saveFields = function() {
@@ -234,33 +271,7 @@ DeskPRO.Agent.PageHelper.TicketFields = new Orb.Class({
 
 		var unsetField = function(name, allowDefaultValue) {
 			$scope.edit_fields.push(name);
-			self.display.find('.item.'+name).each(function(i, el) {
-				var $el = $(el);
-				var defaultValue = allowDefaultValue ? $el.data('default-value') : '';
-				$el.find('input[type=text], textarea, select').val(defaultValue);
-				$el.find('.with-select2').select2('val', defaultValue);
-				$el.find('input[type=radio]').each(function(i, field) {
-					var $field = $(field);
-					if ($field.val() === String(defaultValue)) {
-						$field.prop('checked', true);
-					} else {
-						$field.prop('checked', false);
-					}
-				});
-				$el.find('input[type=checkbox]').each(function(i, field) {
-					var $field = $(field);
-					if ($field.attr('name') && $field.attr('name').indexOf('[]') !== -1) {
-						var vals = defaultValue ? String(defaultValue).split(',') : [];
-						if (vals.indexOf(String($field.val())) !== -1) {
-							$field.prop('checked', true);
-						} else {
-							$field.prop('checked', false);
-						}
-					} else {
-						$field.prop('checked', defaultValue);
-					}
-				});
-			});
+			$scope.setFieldValue(name, '', allowDefaultValue);
 		};
 
 		$scope.hidden = this.no_value_fields.length;
