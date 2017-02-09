ab0a2a8f414dc54fe591d66b144655154f859ed1
Julien Ducro <julien.ducro@deskpro.com>
2017-02-09 16:15:17 +0000

    Fix cssmin warning preg_replace

--- app/BUILD/vendor-src/cssmin/cssmin.php
+++ app/BUILD/vendor-src/cssmin/cssmin.php
@@ -2924,7 +2924,7 @@ class CssConvertNamedColorsMinifierPlugin extends aCssMinifierPlugin
 	 */
 	public function __construct(CssMinifier $minifier, array $configuration = array())
 		{
-		$this->reMatch = "/(^|\s)+(" . implode("|", array_keys($this->transformation)) . ")(\s|$)+/eiS";
+		$this->reMatch = "/(^|\s)+(" . implode("|", array_keys($this->transformation)) . ")(\s|$)+/iS";
 		parent::__construct($minifier, $configuration);
 		}
 	/**
@@ -2944,7 +2944,7 @@ class CssConvertNamedColorsMinifierPlugin extends aCssMinifierPlugin
 		// Declaration value contains a value in the transformation table => regular expression replace
 		elseif (preg_match($this->reMatch, $token->Value))
 			{
-			$token->Value = preg_replace($this->reMatch, $this->reReplace, $token->Value);
+			$token->Value = preg_replace_callback($this->reMatch, array($this, 'replaceTokens'), $token->Value);
 			}
 		return false;
 		}
@@ -2962,6 +2962,11 @@ class CssConvertNamedColorsMinifierPlugin extends aCssMinifierPlugin
 			"CssRulesetDeclarationToken"
 			);
 		}
+
+    private function replaceTokens($m)
+        {
+            return $m[1] . $this->transformation[strtolower($m[2])] . $m[3];
+        }
 	}
 
 /**
