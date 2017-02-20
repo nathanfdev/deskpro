1575fe591592b09b3ea506390c247a6ac60d785a
n3b <neb802@gmail.com>
2017-02-19 18:59:56 -0800

    fix ticket executor event names

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Traits/Tickets/TicketSaveTrait.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Traits/Tickets/TicketSaveTrait.php
@@ -31,6 +31,7 @@ namespace DeskPRO\Bundle\ApiBundle\Traits\Tickets;
 use Application\DeskPRO\DependencyInjection\DeskproContainer;
 use Application\DeskPRO\Entity\Person;
 use Application\DeskPRO\Entity\Ticket;
+use Application\DeskPRO\Tickets\ExecutorContext;
 
 /**
  * Class SaveTicketTrait.
@@ -51,18 +52,18 @@ trait TicketSaveTrait
         $changes = $ticket->getStateChangeRecorder();
 
         if ($changes->isNewTicket()) {
-            $event = 'new';
+            $event = ExecutorContext::EVENT_NEW;
         } elseif ($changes->hasNewReply()) {
-            $event = 'newreply';
+            $event = ExecutorContext::EVENT_REPLY;
         } elseif ($changes->isDeleted()) {
-            $event = 'delete';
+            $event = ExecutorContext::EVENT_DELETE;
         } else {
-            $event = 'update';
+            $event = ExecutorContext::EVENT_UPDATE;
         }
 
-        $eventMethod = 'api';
+        $eventMethod = ExecutorContext::METHOD_API;
         if ($this->getContainer()->get('api_client_info')->isMobileClient()) {
-            $eventMethod = 'mobile';
+            $eventMethod = ExecutorContext::METHOD_MOBILE;
         }
 
         $manager->saveTicket($ticket, $manager->createAgentExecutorContext($this->getUser(), $event, $eventMethod));
