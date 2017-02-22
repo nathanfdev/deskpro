6fade08d7a4b81d9479b55bec6b73ecf3717b52d
ImmortaL <denis@ranneft.ru>
2017-02-22 09:41:06 +0300

    convert emoji and {{t-xxx}} in browser notifications

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Services/NotificationsHandler.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Services/NotificationsHandler.js
@@ -1,9 +1,10 @@
 import Notify from 'notifyjs';
 import striptags from 'striptags';
 import $ from 'jquery';
+import emojione from 'emojione';
+import Message from 'DeskPRO/Bundle/AgentBundle/Modules/IM/Components/New/ChatWindow/Message';
 
-class NotificationsHandler
-{
+class NotificationsHandler {
   constructor(props) {
     this.options = {};
     Object.assign(this.options, props);
@@ -14,10 +15,11 @@ class NotificationsHandler
     switch (payload.type) {
       case 'notification.agent_chat.new_message':
         if (!NotificationsHandler.active && !Notify.needsPermission) {
+          const summary = striptags(emojione.shortnameToUnicode(Message.formatMessage(data.summary)));
           const notification = new Notify(
             data.title,
             {
-              body:    striptags(data.summary),
+              body:    summary,
               timeout: 5,
               icon:    data.icon
             }
