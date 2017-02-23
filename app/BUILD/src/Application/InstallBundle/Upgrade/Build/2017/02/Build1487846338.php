e5b9340894dfec5978009a569a96088ef6b79ab4
Christopher Nadeau <chris.nadeau@deskpro.com>
2017-02-23 10:40:37 +0000

    Split extra alter from existing build script

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2017/02/Build1487846338.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2017/02/Build1487846338.php
@@ -28,11 +28,11 @@
 
 namespace Application\InstallBundle\Upgrade\Build;
 
-class Build1487765029 extends AbstractBuild
+class Build1487846338 extends AbstractBuild
 {
     public function run()
     {
-        $this->out('Fix phone_numbers.guessed_type');
-        $this->execDbQuery('default', 'ALTER TABLE phone_numbers CHANGE guessed_type guessed_type VARCHAR(255) NOT NULL');
+        $this->out('Alter voice_phone_calls');
+        $this->execDbQuery('default', 'ALTER TABLE voice_phone_calls CHANGE call_sid call_sid VARCHAR(50) DEFAULT NULL, CHANGE from_number external_number VARCHAR(50) NOT NULL');
     }
 }
