04a1984abae5eebf7eca721b4372db2f8fad9a9a
ImmortaL <denis@ranneft.ru>
2017-02-24 02:33:55 +0300

    assume gravatar is always response

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Avatar/Avatar.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Common/Components/Avatar/Avatar.js
@@ -28,13 +28,9 @@ export class Avatar extends React.Component {
     }
     if (gravatar) {
       return (
-        <TextAvatar {...this.props}>
-          <UserPhoto type="text" className={className}>
-            <Gravatar gravatar={gravatar} size={size}>
-              <UserPhoto type="gravatar" className={className} />
-            </Gravatar>
-          </UserPhoto>
-        </TextAvatar>
+        <Gravatar gravatar={gravatar} size={size}>
+          <UserPhoto type="gravatar" className={className} />
+        </Gravatar>
       );
     }
 
