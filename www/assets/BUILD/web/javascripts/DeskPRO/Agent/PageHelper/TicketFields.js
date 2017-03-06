a2d7b285e8dddb9f8eea73aa3aa77117b9562678
n3b <neb802@gmail.com>
2017-03-06 01:52:50 -0700

    fix https://trello.com/c/5VLxlhsb/9-95793-urgent-custom-field-bug

--- www/assets/BUILD/web/javascripts/DeskPRO/Agent/PageHelper/TicketFields.js
+++ www/assets/BUILD/web/javascripts/DeskPRO/Agent/PageHelper/TicketFields.js
@@ -55,6 +55,16 @@ DeskPRO.Agent.PageHelper.TicketFields = new Orb.Class({
 				return $field.filter(':checked').map(function(i, el) { return el.value; }).get();
 			},
 			getTicketFieldValue: function(fieldId) {
+				switch (fieldId) {
+					case 'category':
+						return this.getCategoryId();
+					case 'workflow':
+            return this.getWorkflowId();
+					case 'priority':
+						return this.getPriorityId();
+					case 'product':
+						return this.getProductId();
+				}
         fieldId = ((fieldId || '')+'').replace('ticket_field_');
 				return this.getFieldValue('custom_fields[field_' + fieldId + ']');
 			},
