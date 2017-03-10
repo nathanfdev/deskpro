46cf0766beb6b5a5efce3e02169e4cba239eea18
ImmortaL <denis@ranneft.ru>
2017-03-10 22:18:11 +0300

    fix crazy case when current chat is absent in chats RS

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/IM/Components/New/TopBar/TopBarRecentImList.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/IM/Components/New/TopBar/TopBarRecentImList.js
@@ -89,9 +89,11 @@ class TopBarRecentImList extends RecentList {
     let stateChats = this.state.chats.slice(0, 10);
     if (current.get('id') && !stateChats.has(current.get('id'))) {
       const chat = chats.get(current.get('id'));
-      stateChats = stateChats.set(current.get('id'), chat.set('added', Date.now()));
-      stateChats = stateChats.sort((a, b) => b.get('added') - a.get('added'));
-      stateChats = stateChats.slice(0, 10);
+      if (chat) { // I'm not sure why current chat could absent, but seems that Lauren somehow reached that
+        stateChats = stateChats.set(current.get('id'), chat.set('added', Date.now()));
+        stateChats = stateChats.sort((a, b) => b.get('added') - a.get('added'));
+        stateChats = stateChats.slice(0, 10);
+      }
     }
 
     return stateChats.map(chat => this.getItem(chat));
