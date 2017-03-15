9613fdb4de43abbabff62657fa9dc7e39563cf46
ImmortaL <denis@ranneft.ru>
2017-03-15 16:52:50 +0300

    add enable/disable chat mechanics

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Features/AgentChatFeature.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Features/AgentChatFeature.php
@@ -28,6 +28,13 @@
 
 namespace DeskPRO\Bundle\AppBundle\Features;
 
+use Application\DeskPRO\Entity\Setting;
+use Application\DeskPRO\EntityRepository\Setting as SettingRepository;
+use DeskPRO\Bundle\AppBundle\Entity\AgentChat;
+use DeskPRO\Bundle\AppBundle\Entity\AgentChatMessage;
+use DeskPRO\Bundle\AppBundle\Entity\AgentChatParticipant;
+use DeskPRO\Component\Util\RandUtils;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -90,7 +97,12 @@ HTML;
      */
     public function enable(ContainerInterface $container)
     {
-        // TODO: Implement enable() method.
+        $em = $container->get('doctrine.orm.default_entity_manager');
+        /** @var SettingRepository $settingsRepository */
+        $settingsRepository = $em->getRepository(Setting::class);
+        $this->copyIM($container->get('doctrine.orm.default_entity_manager'));
+        $key = sprintf('%s.%s', BetaFeatureInterface::BETA_FEATURES_KEY, $this->getId());
+        $settingsRepository->updateSetting($key, true);
     }
 
     /**
@@ -98,6 +110,131 @@ HTML;
      */
     public function disable(ContainerInterface $container)
     {
-        // TODO: Implement disable() method.
+        $em = $container->get('doctrine.orm.default_entity_manager');
+        /** @var SettingRepository $settingsRepository */
+        $settingsRepository = $em->getRepository(Setting::class);
+        $connection         = $em->getConnection();
+        $classes            = [
+            AgentChat::class,
+            AgentChatMessage::class,
+            AgentChatParticipant::class,
+        ];
+        $parts = [];
+        $em->beginTransaction();
+        try {
+            $em->getRepository(AgentChat::class)->createQueryBuilder('ac')->delete()->getQuery()->execute();
+            $em->commit();
+            foreach ($classes as $class) {
+                $meta    = $em->getClassMetadata($class);
+                $parts[] = "ALTER TABLE {$meta->getTableName()} AUTO_INCREMENT = 1";
+            }
+            $connection->query(implode(';', $parts));
+            $key = sprintf('%s.%s', BetaFeatureInterface::BETA_FEATURES_KEY, $this->getId());
+            $settingsRepository->updateSetting($key, false);
+        } catch (\Exception $e) {
+            $em->rollback();
+        }
+    }
+
+    private function copyIM(EntityManager $em)
+    {
+        $chats = $this->getChats($em);
+        foreach ($chats as $chat) {
+            $participants = $this->getParticipants($em, $chat);
+            if (count($participants) !== 2) {
+                continue;
+            }
+            $messages  = $this->getMessages($em, $chat);
+            $newChatId = $this->copyChat($em, $chat, $participants);
+            $this->copyMessages($em, $newChatId, $messages);
+        }
+    }
+
+    private function getChats(EntityManager $em)
+    {
+        $sql = <<<'SQL'
+SELECT `id`, `date_created` FROM `chat_conversations` WHERE `is_agent` = 1
+SQL;
+
+        return $em->getConnection()->fetchAll($sql);
+    }
+
+    private function getParticipants(EntityManager $em, $chat)
+    {
+        $sql = <<<'SQL'
+SELECT `person_id` FROM `chat_conversation_to_person` WHERE `conversation_id` = ?
+SQL;
+
+        return $em->getConnection()->fetchAllCol($sql, [$chat['id']]);
+    }
+
+    private function getMessages(EntityManager $em, $chat)
+    {
+        $sql = <<<'SQL'
+SELECT `author_id`, `person_name`, `content`, `date_created`, `date_received` 
+  FROM `chat_messages`
+ WHERE `conversation_id` = ?
+ ORDER BY `date_created` ASC
+SQL;
+
+        return $em->getConnection()->fetchAll($sql, [$chat['id']]);
+    }
+
+    private function copyChat(EntityManager $em, $chat, $participants)
+    {
+        $connection = $em->getConnection();
+        // try to find same chat before create new one
+        $find = <<<'FIND'
+SELECT `ac`.`id` FROM `agent_chat` AS `ac`
+INNER JOIN `agent_chat_participant` AS `acp` ON `ac`.`id` = `acp`.`agent_chat_id`
+INNER JOIN `agent_chat_participant` AS `acp2` ON `ac`.`id` = `acp2`.`agent_chat_id`
+WHERE `ac`.`type` = 'agent' AND `acp`.`person_id` = ? AND `acp2`.`person_id` = ?
+FIND;
+
+        if (!$newChatId = $connection->fetchColumn($find, [$participants[0], $participants[1]])) {
+            $connection->insert('agent_chat', ['type' => 'agent', 'date_created' => $chat['date_created']]);
+            $newChatId = $connection->lastInsertId();
+
+            $participantsInsert = <<<'INSERT'
+INSERT INTO `agent_chat_participant` (`agent_chat_id`, `person_id`) VALUES (:agent_chat_id, :person_id)
+INSERT;
+            $statement = $connection->prepare($participantsInsert);
+            foreach ($participants as $participant) {
+                $statement->execute(['agent_chat_id' => $newChatId, 'person_id' => $participant]);
+            }
+        }
+
+        return $newChatId;
+    }
+
+    private function copyMessages(EntityManager $em, $newChatId, $messages)
+    {
+        if (!$messages) {
+            return;
+        } // no messages
+        $connection  = $em->getConnection();
+        $newMessages = [];
+        foreach ($messages as $message) {
+            $data = [
+                'agent_chat_id' => $newChatId,
+                'person_id'     => $message['author_id'],
+                'uuid'          => RandUtils::uuidV4(),
+                'person_name'   => $message['person_name'],
+                'message'       => '<p>'.$message['content'].'</p>',
+                'date_created'  => $message['date_created'],
+                'status'        => 2,
+                'metadata'      => '[]',
+            ];
+            $newMessages[] = $data;
+        }
+        $connection->batchInsert('agent_chat_message', $newMessages);
+        // note here we are explicitly using that $message var contains last from $messages
+        $update = <<<UPDATE
+UPDATE `agent_chat` 
+   SET `date_last_message` = '{$message['date_created']}'
+ WHERE `id` = {$newChatId}
+UPDATE;
+
+        $connection->executeQuery($update);
     }
 }
--- www/assets/BUILD/web/app/Admin/Main/Ctrl/Features.coffee
+++ www/assets/BUILD/web/app/Admin/Main/Ctrl/Features.coffee
@@ -25,12 +25,12 @@ define [
 
     disableFeature: ->
       @Api2.sendPutJson('/features/' + @$stateParams.id + '/disable').then( =>
-        Growl.success('Feature is under disabling process');
+        @Growl.success('Feature is under disabling process');
       );
 
     enableFeature: ->
       @Api2.sendPutJson('/features/' + @$stateParams.id + '/enable').then( =>
-        Growl.success('Feature is under enabling process');
+        @Growl.success('Feature is under enabling process');
       );
 
   Admin_Main_Ctrl_Features.EXPORT_CTRL()
