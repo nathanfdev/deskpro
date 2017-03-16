defec1f8e3dd0e21f32ec7c63835976d0e2ffb52
yakut <a.yakutskiy@gmail.com>
2017-03-16 17:06:13 +0300

    fix registeres page view logging

--- app/BUILD/src/DeskPRO/Bundle/PortalBundle/Controller/DownloadsController.php
+++ app/BUILD/src/DeskPRO/Bundle/PortalBundle/Controller/DownloadsController.php
@@ -35,6 +35,7 @@ namespace DeskPRO\Bundle\PortalBundle\Controller;
 use Application\DeskPRO\Entity\Download;
 use Application\DeskPRO\Entity\DownloadCategory;
 use Application\DeskPRO\Entity\DownloadComment;
+use Application\DeskPRO\Entity\PageViewLog;
 use DeskPRO\Bundle\AppBundle\Annotation\AutoPostOnGetRequest;
 use DeskPRO\Bundle\AppBundle\AntiAbuse\Event\SubmitCommentAbuseCheck;
 use DeskPRO\Bundle\AppBundle\Security\Voter\Portal\ContentCommentVoter;
@@ -251,6 +252,11 @@ class DownloadsController extends AbstractController
         $check = new SubmitCommentAbuseCheck($this->getUser(), $request->getClientIp());
         $check->markAsCheckOnly();
         $this->get('anti_abuse')->check($check);
+
+        // REGISTERED PAGE VIEW LOG
+        if ($person = $this->getUser()) {
+            $this->container->get('content.page_view')->pageView($person, PageViewLog::TYPE_DOWNLOAD, $file->getId());
+        }
 
         // RENDER THEME
 
