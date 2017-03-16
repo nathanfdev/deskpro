efedc725a8c3009d5e79913e339fa6ff31484b87
ImmortaL <denis@ranneft.ru>
2017-03-16 22:49:53 +0300

    also copy groups

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Features/AgentChatFeature.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Features/AgentChatFeature.php
@@ -28,6 +28,7 @@
 
 namespace DeskPRO\Bundle\AppBundle\Features;
 
+use Application\DeskPRO\Entity\Person;
 use Application\DeskPRO\Entity\Setting;
 use Application\DeskPRO\Entity\TmpData;
 use Application\DeskPRO\EntityRepository\Setting as SettingRepository;
@@ -153,20 +154,36 @@ HTML;
         }
     }
 
+    /**
+     * @param EntityManager $em
+     */
     private function copyIM(EntityManager $em)
     {
-        $chats = $this->getChats($em);
+        $chats    = $this->getChats($em);
+        $setAdmin = false;
         foreach ($chats as $chat) {
             $participants = $this->getParticipants($em, $chat);
-            if (count($participants) !== 2) {
-                continue;
+            $messages     = $this->getMessages($em, $chat);
+
+            if (count($participants) == 2) {
+                $newChatId = $this->find121Chat($em, $chat, $participants);
+            } else {
+                $newChatId = $this->findGroupChat($em, $chat, $participants);
+                $setAdmin  = true;
             }
-            $messages  = $this->getMessages($em, $chat);
-            $newChatId = $this->copyChat($em, $chat, $participants);
+
             $this->copyMessages($em, $newChatId, $messages);
+            if ($setAdmin) {
+                $this->setAdmin($em, $newChatId);
+            }
         }
     }
 
+    /**
+     * @param EntityManager $em
+     *
+     * @return array
+     */
     private function getChats(EntityManager $em)
     {
         $sql = <<<'SQL'
@@ -176,6 +193,29 @@ SQL;
         return $em->getConnection()->fetchAll($sql);
     }
 
+    private function setAdmin(EntityManager $em, $chatId)
+    {
+        /** @var \Application\DeskPRO\DBAL\Connection $connection */
+        $connection = $em->getConnection();
+        $sql        = <<<'SQL'
+SELECT `person_id` FROM `agent_chat_message` WHERE `agent_chat_id` = ? ORDER BY `date_created` ASC LIMIT 1
+SQL;
+        $personId = $connection->fetchColumn($sql, [$chatId]);
+        if ($personId) {
+            $personId = $personId[0];
+        }
+        $sql = <<<'SQL'
+        UPDATE `agent_chat` SET `admin_id` = ? WHERE `id` = ?
+SQL;
+        $connection->executeQuery($sql, [$personId, $chatId]);
+    }
+
+    /**
+     * @param EntityManager $em
+     * @param array         $chat
+     *
+     * @return mixed
+     */
     private function getParticipants(EntityManager $em, $chat)
     {
         $sql = <<<'SQL'
@@ -185,6 +225,12 @@ SQL;
         return $em->getConnection()->fetchAllCol($sql, [$chat['id']]);
     }
 
+    /**
+     * @param EntityManager $em
+     * @param array         $chat
+     *
+     * @return array
+     */
     private function getMessages(EntityManager $em, $chat)
     {
         $sql = <<<'SQL'
@@ -197,7 +243,16 @@ SQL;
         return $em->getConnection()->fetchAll($sql, [$chat['id']]);
     }
 
-    private function copyChat(EntityManager $em, $chat, $participants)
+    /**
+     * @param EntityManager $em
+     * @param array         $chat
+     * @param array         $participants
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     *
+     * @return mixed|string
+     */
+    private function find121Chat(EntityManager $em, $chat, $participants)
     {
         $connection = $em->getConnection();
         // try to find same chat before create new one
@@ -212,18 +267,62 @@ FIND;
             $connection->insert('agent_chat', ['type' => 'agent', 'date_created' => $chat['date_created']]);
             $newChatId = $connection->lastInsertId();
 
-            $participantsInsert = <<<'INSERT'
-INSERT INTO `agent_chat_participant` (`agent_chat_id`, `person_id`) VALUES (:agent_chat_id, :person_id)
-INSERT;
-            $statement = $connection->prepare($participantsInsert);
-            foreach ($participants as $participant) {
-                $statement->execute(['agent_chat_id' => $newChatId, 'person_id' => $participant]);
-            }
+            $this->insertParticipants($em, $newChatId, $participants);
         }
 
         return $newChatId;
     }
 
+    /**
+     * @param EntityManager $em
+     * @param array         $chat
+     * @param array         $participants
+     *
+     * @return string
+     */
+    private function findGroupChat(EntityManager $em, $chat, $participants)
+    {
+        $connection = $em->getConnection();
+        if (!$newChat = $em->getRepository(AgentChat::class)->findGroupChat($participants)) {
+            $people    = $em->getRepository(Person::class)->findBy(['id' => $participants], null, 4);
+            $nameParts = [];
+            foreach ($people as $person) {
+                $nameParts[] = $person->getFirstName() ?: $person->getName();
+            }
+            $connection->insert('agent_chat', [
+                    'type'         => 'group',
+                    'date_created' => $chat['date_created'],
+                    'name'         => implode(', ', $nameParts).' Group',
+                ]
+            );
+            $newChatId = $connection->lastInsertId();
+            $this->insertParticipants($em, $newChatId, $participants);
+        } else {
+            $newChatId = $newChat->getId();
+        }
+
+        return $newChatId;
+    }
+
+    private function insertParticipants(EntityManager $em, $newChatId, $participants)
+    {
+        $connection         = $em->getConnection();
+        $participantsInsert = <<<'INSERT'
+INSERT INTO `agent_chat_participant` (`agent_chat_id`, `person_id`) VALUES (:agent_chat_id, :person_id)
+INSERT;
+        $statement = $connection->prepare($participantsInsert);
+        foreach ($participants as $participant) {
+            $statement->execute(['agent_chat_id' => $newChatId, 'person_id' => $participant]);
+        }
+    }
+
+    /**
+     * @param EntityManager $em
+     * @param int           $newChatId
+     * @param array         $messages
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     */
     private function copyMessages(EntityManager $em, $newChatId, $messages)
     {
         if (!$messages) {
@@ -248,7 +347,7 @@ INSERT;
         // note here we are explicitly using that $message var contains last from $messages
         $update = <<<UPDATE
 UPDATE `agent_chat` 
-   SET `date_last_message` = '{$message['date_created']}'
+   SET `date_last_message` = GREATEST(`date_last_message`, '{$message['date_created']}')
  WHERE `id` = {$newChatId}
 UPDATE;
 
