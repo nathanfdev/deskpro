4f1c513639518bd4cf9f048a4358fe6573d9140b
ImmortaL <denis@ranneft.ru>
2017-03-17 13:04:53 +0300

    fix css for markjs is missing

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/IM/Components/New/ChatWindow/Container.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/IM/Components/New/ChatWindow/Container.js
@@ -3,7 +3,6 @@ import React, { PropTypes } from 'react';
 import ReactTooltip from 'react-tooltip';
 import Isvg from 'react-inlinesvg';
 import $ from 'jquery';
-import 'mark.js';
 import FroalaEditor from 'react-froala-wysiwyg';
 import classNames from 'classnames';
 import { Detached } from 'DeskPRO/Component/Positioned/Detached';
