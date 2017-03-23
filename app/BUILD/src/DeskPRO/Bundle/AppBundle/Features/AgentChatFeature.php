e276e63459efaa54d206bd2632744988b036ddde
Christopher Nadeau <chris.nadeau@deskpro.com>
2017-03-23 09:45:10 +0000

    Change chat availability -- not quite beta ready yet

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Features/AgentChatFeature.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Features/AgentChatFeature.php
@@ -99,7 +99,7 @@ HTML;
      */
     public function getAvailability()
     {
-        return [BetaFeatureInterface::AVAILABLE_EVERYWHERE];
+        return [BetaFeatureInterface::AVAILABLE_AT_QA];
     }
 
     /**
