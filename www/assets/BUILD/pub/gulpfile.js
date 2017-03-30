f5d4f1ecc96ffdb2dbf84e084ebfe5fcaa669689
Julien Ducro <julien.ducro@deskpro.com>
2017-03-30 12:02:33 +0100

    Fix pdf generation errors

--- www/assets/BUILD/pub/gulpfile.js
+++ www/assets/BUILD/pub/gulpfile.js
@@ -269,6 +269,8 @@ function getWebpackConfig(mode, isProd) {
 
     config.entry.DeskPRO_PortalBundle_vendors_style = ['./src/DeskPRO/Bundle/PortalBundle/Resources/style/vendors-style.scss'];
 
+    config.entry.DeskPRO_PortalBundle_GuidePdf_style = ['./src/DeskPRO/Bundle/PortalBundle/Resources/style/guide_pdf.scss'];
+
     config.entry.DeskPRO_PortalBundle_iestyle  = ['./src/DeskPRO/Bundle/PortalBundle/Resources/style/ie-overrides.scss'];
     config.entry.DeskPRO_PortalBundle_ie8style = ['./src/DeskPRO/Bundle/PortalBundle/Resources/style/ie8-overrides.scss'];
     config.entry.DeskPRO_PortalBundle_ie9style = ['./src/DeskPRO/Bundle/PortalBundle/Resources/style/ie9-overrides.scss'];
