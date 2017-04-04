5141a8386f547c1fe81d6cc0dbd82b3305789a8a
Sergey Yastrebov <serg.yastrebov@gmail.com>
2017-04-04 20:02:58 +0300

    agent auto sso logout fix

--- app/BUILD/src/Application/AgentBundle/Controller/LoginController.php
+++ app/BUILD/src/Application/AgentBundle/Controller/LoginController.php
@@ -69,7 +69,12 @@ class LoginController extends \Application\UserBundle\Controller\LoginController
             }
         }
 
-        $has_logged_out = $this->in->checkIsset('o');
+        $has_logged_out = $request->cookies->has('dp-recent-logout');
+        if ($has_logged_out) {
+            // remove recent logout cookie on redirect login
+            $cookie = \Application\DeskPRO\HttpFoundation\Cookie::makeDeleteCookie('dp-recent-logout');
+            $cookie->send();
+        }
 
         // SSO Automatic Redirecting
 
