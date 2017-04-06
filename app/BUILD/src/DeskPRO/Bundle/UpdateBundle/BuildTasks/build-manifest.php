92a98a6d1f4c9f1c0434a0be2e9eaa75e75d7b3d
ImmortaL <denis@ranneft.ru>
2017-04-06 20:16:11 +0300

    remove buggy rebase lines, also remove already moved build script

--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -399,12 +399,6 @@ return [
         'skipPostBuild' => false,
         'isOnlineBuild' => false,
     ],
-    1491314589 => [
-        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2017/04/Build1491314589.php',
-        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1491314589',
-        'skipPostBuild' => false,
-        'isOnlineBuild' => false,
-    ],
     1491407808 => [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2017/04/Build1491407808.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1491407808',
