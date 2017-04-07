5f0c72ac93f15b37db14bcb9095078e083384aab
Christopher Nadeau <chris.nadeau@deskpro.com>
2017-04-07 09:26:03 +0100

    Fix weird syntax error on PHP 5.5

--- app/BUILD/src/DeskPRO/Bundle/InstallBundle/FileIntegrity/ProjectFileSet.php
+++ app/BUILD/src/DeskPRO/Bundle/InstallBundle/FileIntegrity/ProjectFileSet.php
@@ -206,7 +206,8 @@ class ProjectFileSet
             'storybook',
         ];
 
-        static $ignorePaths = [
+        // making this static causes a syntax error in PHP5.5 on the last element of the array for some reason (wtf?)
+        $ignorePaths = [
             '%DP_APP_DIR%/vendor/autoload.php',
             '%DP_APP_DIR%/vendor/behat/',
             '%DP_APP_DIR%/vendor/behatch/',
