8c282914f902fa91fd534e565f1f46c726427e71
Julien Ducro <julien.ducro@deskpro.com>
2017-04-19 13:25:44 +0100

    Fix ChatMessage serialization

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Serializer/Model/Chats/ChatMessage.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Serializer/Model/Chats/ChatMessage.php
@@ -55,6 +55,26 @@ class ChatMessage
     protected $author = null;
 
     /**
+     * Author id (legacy).
+     *
+     * @JMS\Expose()
+     * @JMS\Type("integer")
+     *
+     * @var int
+     */
+    protected $authorId;
+
+    /**
+     * Author type (legacy).
+     *
+     * @JMS\Expose()
+     * @JMS\Type("string")
+     *
+     * @return string
+     */
+    protected $authorType;
+
+    /**
      * The message.
      *
      * @JMS\Expose()
@@ -75,15 +95,102 @@ class ChatMessage
     protected $contentHtml;
 
     /**
+     * Conversation id (legacy).
+     *
+     * @JMS\Expose()
+     * @JMS\Type("integer")
+     *
+     * @var int
+     */
+    protected $conversationId;
+
+    /**
+     * Date message was created.
+     *
+     * @JMS\Expose()
+     * @JMS\Type("DateTime")
+     *
+     * @var \DateTime
+     */
+    protected $dateCreated;
+
+    /**
+     * Date message was received.
+     *
+     * @JMS\Expose()
+     * @JMS\Type("DateTime")
+     *
+     * @var \DateTime
+     */
+    protected $dateReceived = null;
+
+    /**
+     * Is this a system message? (ended, joined, etc).
+     *
+     * @JMS\Expose()
+     * @JMS\Type("boolean")
+     *
+     * @var bool
+     */
+    protected $isSys = false;
+
+    /**
+     * Is this an user's message? (send from the widget).
+     *
+     * @JMS\Expose()
+     * @JMS\Type("boolean")
+     *
+     * @var bool
+     */
+    protected $isUser = false;
+
+    /**
+     * Is the message hidden from the user?
+     *
+     * @var bool
+     */
+    protected $isUserHidden = false;
+
+    /**
+     * Is the content an HTML message?
+     *
+     * @JMS\Expose()
+     * @JMS\Type("boolean")
+     *
+     * @var bool
+     */
+    protected $isHtml = false;
+
+    /**
+     * Additional data.
+     *
+     * @JMS\Expose()
+     * @JMS\Type("array")
+     *
+     * @var array
+     */
+    protected $metadata = [];
+
+    /**
      * Constructor.
      *
      * @param ChatMessageEntity $chat
      */
     public function __construct(ChatMessageEntity $chat)
     {
-        $this->id          = $chat->getId();
-        $this->author      = $chat->getAuthor();
-        $this->content     = $chat->getContent();
-        $this->contentHtml = $chat->getContentHtml();
+        $this->author         = $chat->getAuthor();
+        $this->authorId       = $chat->getAuthorId();
+        $this->authorType     = $chat->getAuthorType();
+        $this->content        = $chat->getContent();
+        $this->contentHtml    = $chat->getContentHtml();
+        $this->conversationId = $chat->getConversationId();
+        $this->dateCreated    = $chat->getDateCreated();
+        $this->dateReceived   = $chat->getDateReceived();
+        $this->id             = $chat->getId();
+        $this->isHtml         = $chat->isHtml();
+        $this->isSys          = $chat->getIsSys();
+        $this->isUser         = $chat->getIsUser();
+        $this->isUserHidden   = $chat->getIsUserHidden();
+        $this->metadata       = $chat->getMetadata();
     }
 }
