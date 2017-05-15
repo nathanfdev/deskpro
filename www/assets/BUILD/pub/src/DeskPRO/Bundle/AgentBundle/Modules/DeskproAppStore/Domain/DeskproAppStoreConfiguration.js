25aedeac5c2d59af586756b17a716f6610dd2890
yakut <a.yakutskiy@gmail.com>
2017-05-15 14:26:49 +0300

    new tabs locations

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/DeskproAppStore/Domain/DeskproAppStoreConfiguration.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/DeskproAppStore/Domain/DeskproAppStoreConfiguration.js
@@ -5,17 +5,20 @@ class DeskproAppStoreConfiguration {
       'ticket-sidebar', 'ticket-header-top', 'ticket-header-bottom', 'ticket-header-after',
       'ticket-properties-before', 'ticket-properties-after', 'ticket-replybox-before', 'ticket-replybox-after',
       'ticket-messages-before', 'ticket-messages-after', 'ticket-footer',
+      'ticket-properties-new-tab', 'ticket-body-tabs-new-tab',
       'person-sidebar', 'person-header-top', 'person-header-bottom', 'person-header-after',
       'person-content-before', 'person-summary-before', 'person-summary-after', 'person-properties-before',
       'person-properties-after', 'person-notes-before', 'person-notes-after', 'person-content-after',
       'person-contact-before', 'person-contact-after', 'person-org-before', 'person-org-after',
       'person-usergroups-before', 'person-usergroups-after', 'person-footer',
+      'person-summary-new-tab', 'person-content-box-new-tab', 'person-notes-new-tab',
       'org-sidebar', 'org-header-top', 'org-header-bottom', 'org-header-after',
       'org-content-before', 'org-summary-before', 'org-summary-after', 'org-notes-before', 'org-notes-after',
       'org-content-after',
       'org-contact-before', 'org-contact-after', 'org-properties-before', 'org-properties-after',
       'org-email-assoc-before', 'org-email-assoc-after', 'org-usergroups-before', 'org-usergroups-after',
-      'org-hierarchy-before', 'org-hierarchy-after', 'org-footer'
+      'org-hierarchy-before', 'org-hierarchy-after', 'org-footer',
+      'org-summary-new-tab', 'org-content-box-new-tab', 'org-notes-new-tab'
     ];
   }
 
