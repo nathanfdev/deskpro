18a06ac10e9689ad66247c4f669bc5e05aec5c72
radu <radu.helstern@deskpro.com>
2017-05-17 13:10:04 +0100

    add missing id to sidebar content elements which prevented the content area from resizing

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/DeskproAppStore/Domain/DeskproAppStoreConfiguration.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/DeskproAppStore/Domain/DeskproAppStoreConfiguration.js
@@ -1,16 +1,25 @@
 class DeskproAppStoreConfiguration {
   static get validTargets() {
     return [
+
+      // ticket tab
+
       'ticket-sidebar', 'ticket-header-top', 'ticket-header-bottom', 'ticket-header-after',
       'ticket-properties-before', 'ticket-properties-after', 'ticket-replybox-before', 'ticket-replybox-after',
       'ticket-messages-before', 'ticket-messages-after', 'ticket-footer',
       'ticket-properties-new-tab', 'ticket-body-tabs-new-tab',
+
+      // person tab
+
       'person-sidebar', 'person-header-top', 'person-header-bottom', 'person-header-after',
       'person-content-before', 'person-summary-before', 'person-summary-after', 'person-properties-before',
       'person-properties-after', 'person-notes-before', 'person-notes-after', 'person-content-after',
       'person-contact-before', 'person-contact-after', 'person-org-before', 'person-org-after',
       'person-usergroups-before', 'person-usergroups-after', 'person-footer',
       'person-summary-new-tab', 'person-content-box-new-tab', 'person-notes-new-tab',
+
+      // org tab
+
       'org-sidebar', 'org-header-top', 'org-header-bottom', 'org-header-after',
       'org-content-before', 'org-summary-before', 'org-summary-after', 'org-notes-before', 'org-notes-after',
       'org-content-after',
@@ -18,6 +27,7 @@ class DeskproAppStoreConfiguration {
       'org-email-assoc-before', 'org-email-assoc-after', 'org-usergroups-before', 'org-usergroups-after',
       'org-hierarchy-before', 'org-hierarchy-after', 'org-footer',
       'org-summary-new-tab', 'org-content-box-new-tab', 'org-notes-new-tab'
+
     ];
   }
 
