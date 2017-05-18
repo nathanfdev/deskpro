b968b88a268c3cc1c72c259a48b11c4491e71b03
ImmortaL <denis@ranneft.ru>
2017-05-18 15:23:13 +0300

    turn back is_reply, make it noop in reply actions applicator

--- app/BUILD/src/Application/DeskPRO/EmailGateway/TicketGateway/AgentReplyCodes.php
+++ app/BUILD/src/Application/DeskPRO/EmailGateway/TicketGateway/AgentReplyCodes.php
@@ -297,6 +297,12 @@ class AgentReplyCodes implements Loggable
                 $this->props['is_note'] = true;
                 break;
 
+            case 'reply':
+            case 'isreply':
+                $this->getLogger()->logDebug('[AgentReplyCodes] Message is a reply');
+                $this->props['is_reply'] = true;
+                break;
+
             case 'assign':
             case 'agent':
                 $this->getLogger()->logDebug('[AgentReplyCodes] Assign agent -- finding param: '.$param);
