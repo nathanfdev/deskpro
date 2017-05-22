76d6a26beed39010fcd3d0fb68cb5002ffd54610
ImmortaL <denis@ranneft.ru>
2017-05-22 23:27:15 +0300

    make 'force' work

--- app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
+++ app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
@@ -3746,21 +3746,21 @@ class TicketController extends AbstractController
             throw $this->createNotFoundException('You cannot merge a ticket with itself');
         }
 
-        if ($ticket->getLockedByAgent() && $ticket->getLockedByAgent() !== $this->person) {
+        if ($ticket->getLockedByAgent() && $ticket->getLockedByAgent() !== $this->person && !$this->in->getBool('ticket_force')) {
             return $this->createJsonResponse(
                 [
-                    'success'     => false,
-                    'fail_reason' => 'This ticket is locked by '.$ticket->getLockedByAgent()->getDisplayName(),
+                    'success' => false,
+                    'html'    => $this->renderMergeOverlay($ticket_id, $other_ticket_id),
                 ],
                 400
             );
         }
 
-        if ($other_ticket->getLockedByAgent() && $other_ticket->getLockedByAgent() !== $this->person) {
+        if ($other_ticket->getLockedByAgent() && $other_ticket->getLockedByAgent() !== $this->person && !$this->in->getBool('other_ticket_force')) {
             return $this->createJsonResponse(
                 [
-                    'success'     => false,
-                    'fail_reason' => 'Other ticket is locked by '.$other_ticket->getLockedByAgent()->getDisplayName(),
+                    'success' => false,
+                    'html'    => $this->renderMergeOverlay($ticket_id, $other_ticket_id),
                 ],
                 400
             );
@@ -3786,6 +3786,35 @@ class TicketController extends AbstractController
                 'success' => true,
                 'id'      => $ticket['id'],
                 'old_id'  => $old_ticket_id,
+            ]
+        );
+    }
+
+    private function renderMergeOverlay($ticketId, $otherTicketId)
+    {
+        $ticket = $this->getTicketOr404($ticketId, 'modify_merge');
+
+        $fieldManager = $this->container->getSystemService('ticket_fields_manager');
+        $customFields = $fieldManager->getDisplayArrayForObject($ticket);
+
+        if ($otherTicketId) {
+            $otherTicket       = $this->getTicketOr404($otherTicketId, 'view');
+            $otherCustomFields = $fieldManager->getDisplayArrayForObject($otherTicket);
+            $canMerge          = $this->person->PermissionsManager->TicketChecker->canMerge($ticket, $otherTicket);
+        } else {
+            $canMerge          = null;
+            $otherTicket       = false;
+            $otherCustomFields = false;
+        }
+
+        return $this->container->get('twig')->render(
+            'AgentBundle:Ticket:merge-overlay.html.twig',
+            [
+                'ticket'              => $ticket,
+                'custom_fields'       => $customFields,
+                'other_ticket'        => $otherTicket,
+                'other_custom_fields' => $otherCustomFields,
+                'can_merge'           => $canMerge,
             ]
         );
     }
