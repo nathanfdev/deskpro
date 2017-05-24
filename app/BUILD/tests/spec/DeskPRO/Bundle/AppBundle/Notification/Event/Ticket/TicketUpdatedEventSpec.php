c9fc01fb5e80ddead15b50b9992f76bdf44baa2f
ImmortaL <denis@ranneft.ru>
2017-05-24 14:06:51 +0300

    fix tests

--- app/BUILD/tests/spec/DeskPRO/Bundle/AppBundle/Notification/Event/Ticket/TicketUpdatedEventSpec.php
+++ app/BUILD/tests/spec/DeskPRO/Bundle/AppBundle/Notification/Event/Ticket/TicketUpdatedEventSpec.php
@@ -48,11 +48,11 @@ class TicketUpdatedEventSpec extends ObjectBehavior
 
     public function it_can_return_data()
     {
-        $this->getData()->shouldBe(['test_data' => 2]);
+        $this->getData()->shouldBe(['test_data' => 2, 'eventType' => 'some.event', 'ticket_id' => 1]);
     }
 
     public function it_returns_propper_event_name()
     {
-        $this->getName()->shouldBe(TicketUpdatedEvent::EVENT_NAME);
+        $this->getName()->shouldBe('some.event');
     }
 }
