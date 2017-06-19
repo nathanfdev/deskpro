8228b12b9f236710d49148e5a2f621b9191ccb7c
Sergey Yastrebov <serg.yastrebov@gmail.com>
2017-06-19 15:01:44 +0300

    voice account 'is synced' icon

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AdminBundle/Modules/Voice/Actions/queueActions.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AdminBundle/Modules/Voice/Actions/queueActions.js
@@ -12,6 +12,7 @@ export const createQueue = createAction(
   'VOICE_CREATE_QUEUE',
   data => dispatch => repository('VoiceQueue').create(data).success((response) => {
     dispatch(addToCollection('VoiceQueue', 'all', Immutable.List([Immutable.fromJS(response.data)])));
+    dispatch(releaseCollection('VoiceAccount', 'all'));
   })
 );
 
@@ -19,6 +20,7 @@ export const updateQueue = createAction(
   'VOICE_UPDATE_QUEUE',
   (id, data) => dispatch => repository('VoiceQueue').update(data, id).success(() => {
     dispatch(updateCollection('VoiceQueue', Immutable.List([Immutable.fromJS({ ...data, id })]), 'merge'));
+    dispatch(releaseCollection('VoiceAccount', 'all'));
   })
 );
 
