8990eaba3cd7d3109fc0b77702f62fecadabcf6e
Julien Ducro <julien.ducro@deskpro.com>
2017-07-04 20:58:34 +0100

    Deal with parent / children in departments

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Snippets/Components/SnippetsModal.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Snippets/Components/SnippetsModal.js
@@ -321,6 +321,49 @@ export class SnippetsModalContainer extends React.Component {
   };
 
   handleDepartmentsChange = (departments) => {
+    const previous = this.state.departments;
+    const added = departments.filter(function(i) {return previous.indexOf(i) < 0;});
+    const removed = previous.filter(function(i) {return departments.indexOf(i) < 0;});
+    if (added.length === 1) {
+      if (this.state.types.find(type => type === 'ticket')) {
+        this.props.ticketDepartments.forEach((department) => {
+          if (department.get('parent') === parseInt(added[0])) {
+            if (departments.indexOf(department.get('id')) === -1) {
+              departments.push(`${department.get('id')}`);
+            }
+          }
+        });
+      }
+      if (this.state.types.find(type => type === 'chat')) {
+        this.props.chatDepartments.forEach((department) => {
+          if (department.get('parent') === parseInt(added[0])) {
+            if (departments.indexOf(department.get('id')) === -1) {
+              departments.push(`${department.get('id')}`);
+            }
+          }
+        });
+      }
+    }
+    if (removed.length === 1) {
+      if (this.state.types.find(type => type === 'ticket')) {
+        this.props.ticketDepartments.forEach((department) => {
+          if (department.get('parent') === parseInt(removed[0])) {
+            if (departments.indexOf(department.get('id')) === -1) {
+              departments = departments.filter(item => parseInt(item) !== department.get('id'));
+            }
+          }
+        });
+      }
+      if (this.state.types.find(type => type === 'chat')) {
+        this.props.chatDepartments.forEach((department) => {
+          if (department.get('parent') === parseInt(removed[0])) {
+            if (departments.indexOf(department.get('id')) === -1) {
+              departments = departments.filter(item => parseInt(item) !== department.get('id'));
+            }
+          }
+        });
+      }
+    }
     this.setState({
       departments
     });
@@ -530,10 +573,14 @@ export class SnippetsModal extends React.Component {
     if (types.find(type => type === 'ticket')) {
       ticketDepartments.forEach((department) => {
         if (ids.indexOf(department.get('id') === -1)) {
+          let label = department.get('title');
+          if (department.get('parent')) {
+            label = `-- ${label}`;
+          }
           departments.push({
             value:    `${department.get('id')}`,
-            label:    department.get('title'),
-            selected: this.props.snippetDepartments.find(d => parseInt(d, 10) === department.get('id')),
+            label,
+            selected: !!this.props.snippetDepartments.find(d => parseInt(d, 10) === department.get('id')),
           });
           ids.push(department.get('id'));
         }
@@ -545,12 +592,13 @@ export class SnippetsModal extends React.Component {
           departments.push({
             value:    `${department.get('id')}`,
             label:    department.get('title'),
-            selected: this.props.snippetDepartments.find(d => parseInt(d, 10) === department.get('id')),
+            selected: !!this.props.snippetDepartments.find(d => parseInt(d, 10) === department.get('id')),
           });
           ids.push(department.get('id'));
         }
       });
     }
+    console.log(departments);
     return departments;
   };
 
