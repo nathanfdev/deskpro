7ce17775dfc5acacccfc0e7b3395d75d324979be
Julien Ducro <julien.ducro@deskpro.com>
2017-07-06 09:56:35 +0100

    Close Snippets on app change

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/SideBar/Components/LeftDrawer.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/SideBar/Components/LeftDrawer.js
@@ -72,6 +72,9 @@ export class LeftDrawerContainer extends SeparateComponent {
     window.document.addEventListener('dpLeftDrawerClose', () => {
       this.closeDrawer();
     });
+    window.document.addEventListener('dpChangeSection', () => {
+      this.closeDrawer();
+    });
   };
 
   openDrawer = () => {
