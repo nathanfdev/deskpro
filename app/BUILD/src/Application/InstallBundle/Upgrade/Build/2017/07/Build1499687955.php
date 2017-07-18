cba45b34fd9a5949038bd6a6e153944d65cc0df9
Christopher Nadeau <chris.nadeau@deskpro.com>
2017-07-18 15:48:01 +0100

    Should be a blocking build

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2017/07/Build1499687955.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2017/07/Build1499687955.php
@@ -28,7 +28,7 @@
 
 namespace Application\InstallBundle\Upgrade\Build;
 
-class Build1499687955 extends AbstractBuild implements OnlineBuildInterface, SkipPostBuildInterface
+class Build1499687955 extends AbstractBuild implements BlockingBuildInterface, SkipPostBuildInterface
 {
     public function addNewTables()
     {
