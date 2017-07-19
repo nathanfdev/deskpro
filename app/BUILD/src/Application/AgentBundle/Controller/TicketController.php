83e9ee5b57161e49a38cf8c7335f762a9185e1a1
ImmortaL <denis@ranneft.ru>
2017-07-19 14:45:33 +0300

    don't show linked chat if have no perms

--- app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
+++ app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
@@ -442,7 +442,12 @@ class TicketController extends AbstractController
             }
         }
 
-        $brands = $this->em->getRepository(Brand::class)->findAll();
+        $brands      = $this->em->getRepository(Brand::class)->findAll();
+        $chatChecker = $this->getPerson()->getPermissionsManager()->ChatChecker;
+        $linkedChat  = null;
+        if ($ticket->linked_chat && $chatChecker->canView($ticket->linked_chat)) {
+            $linkedChat = $ticket->linked_chat;
+        }
 
         $vars = [
             'agent_teams' => $agent_teams,
@@ -454,6 +459,7 @@ class TicketController extends AbstractController
             'ticket_api'                 => $ticket_api,
             'ticket_attachments'         => $ticket_attachments,
             'ticket_message_attachments' => $ticket_message_attachments,
+            'linked_chat'                => $linkedChat,
 
             'validator_errors' => $validator_errors,
 
