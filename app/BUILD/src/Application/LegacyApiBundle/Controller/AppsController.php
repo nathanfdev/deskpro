6e4bc8b22b2775d8d96df2733e988838a16dad54
radu <radu.helstern@deskpro.com>
2017-07-19 19:40:13 +0100

    - normalize package name when installing to allow installation of packages with  slashes in their names

--- app/BUILD/src/Application/LegacyApiBundle/Controller/AppsController.php
+++ app/BUILD/src/Application/LegacyApiBundle/Controller/AppsController.php
@@ -141,17 +141,23 @@ class AppsController extends AbstractController
 
     public function getPackageAction($name)
     {
+        // we are expecting the client to double url encode $name
+        // in case it contains forward slashes, e.g @deskproapps/app-name
+        // the actual problem can be solved by just double encoding of '/', / => %2F => %252F
+        // but it is simpler on the client to double encode everything
+        $name = urldecode(urldecode($name));
         $manager = $this->container->getAppManager();
 
         if (!$manager->hasPackage($name)) {
             // app v2 package info
             $appArchive = $this->getAppV2ArchiveBundle($name);
+
             $app        = $this->em->getRepository(App::class)->findOneBy([
                 'name' => $name,
             ]);
-
             if ($appArchive) {
                 $manifestReader = new AppManifestReader();
+
                 $manifest       = $manifestReader->readManifestFromJson($appArchive->getManifestAsString());
                 $iconBlob       = $this->container->get('blob.storage')->createBlobRecordFromString(
                     $appArchive->getIcon(),
@@ -193,7 +199,7 @@ class AppsController extends AbstractController
                 'author_name'  => $manifest->getAuthor()->getName(),
                 'author_email' => $manifest->getAuthor()->getEmail(),
                 'author_link'  => $manifest->getAuthor()->getUrl(),
-                'version_name' => $manifest->getVersion(),
+                'version_name' => $manifest->getAppVersion(),
                 'apps'         => $apps,
                 'app_version'  => 2,
             ];
