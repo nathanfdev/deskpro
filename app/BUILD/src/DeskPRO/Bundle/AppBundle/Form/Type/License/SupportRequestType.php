d3452e9c66ebc3dc98d26336787c37449deafa03
Sergey Yastrebov <serg.yastrebov@gmail.com>
2017-08-15 20:26:15 +0300

    api support request action

---
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/License/SupportRequestType.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * a British company located in London, England.
+ *
+ * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ *
+ * The license agreement under which this software is released
+ * can be found at https://www.deskpro.com/eula/
+ *
+ * By using this software, you acknowledge having read the license
+ * and agree to be bound thereby.
+ *
+ * Please note that DeskPRO is not free software. We release the full
+ * source code for our software because we trust our users to pay us for
+ * the huge investment in time and energy that has gone into both creating
+ * this software and supporting our customers. By providing the source code
+ * we preserve our customers' ability to modify, audit and learn from our
+ * work. We have been developing DeskPRO since 2001, please help us make it
+ * another decade.
+ *
+ * Like the work you see? Think you could make it better? We are always
+ * looking for great developers to join us: http://www.deskpro.com/jobs/
+ *
+ * ~ Thanks, Everyone at Team DeskPRO
+ */
+
+namespace DeskPRO\Bundle\AppBundle\Form\Type\License;
+
+use Application\DeskPRO\Entity\Person;
+use DeskPRO\Bundle\AppBundle\Form\Type\HtmlTextareaType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class SupportRequestType.
+ */
+class SupportRequestType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        /** @var Person $person */
+        $person = $options['person'];
+        $builder
+            ->add('name', TextType::class, [
+                'empty_data' => $person->getDisplayName(),
+            ])
+            ->add('email', EmailType::class, [
+                'empty_data'  => $person->getPrimaryEmailAddress(),
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Email(['strict' => true]),
+                ],
+            ])
+            ->add('subject', TextType::class)
+            ->add('message', HtmlTextareaType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                ],
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setRequired('person')
+            ->setAllowedTypes('person', Person::class)
+        ;
+    }
+}
