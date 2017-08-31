1b796cbe183cccb9de9c5bba9693ecdd4b04cfce
radu <radu.helstern@deskpro.com>
2017-08-31 11:01:07 +0100

    add fixes to state variable for trello app

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2017/08/Build1504102498.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2017/08/Build1504102498.php
@@ -48,5 +48,105 @@ class Build1504102498 extends AbstractBuild implements BlockingBuildInterface
 
     public function run()
     {
+        $fixTrelloAuthSql = $this->getFixTrelloAuthSql();
+        $this->execDbQuery('default', $fixTrelloAuthSql);
+
+        $fixTrelloCardsSql = $this->getFixTrelloCardsPermissionsSQL();
+        $this->execDbQuery('default', $fixTrelloCardsSql);
+
+        $updateManifestSql = <<<'SQL'
+  UPDATE
+  `app2_app`
+  SET `manifest` = ?
+  WHERE `app2_app`.id =  ?;
+SQL;
+        $fixedManifests = $this->fixTrelloCardsPermissionsInManifest();
+        foreach ($fixedManifests as $key => $value) {
+            $this->getDbConnection('default')->executeQuery($updateManifestSql, [$value, $key]);
+        }
     }
+
+    public function getFixTrelloAuthSql()
+    {
+        $sql = <<<'SQL'
+  UPDATE
+  `app2_app_state_v2` 
+  INNER JOIN `app2_app_instance` ON `app2_app_state_v2`.app_instance_id = `app2_app_instance`.id
+  INNER JOIN `app2_app` ON `app2_app`.id = `app2_app_instance`.app_id
+  SET `app2_app_state_v2`.entity_id = CONCAT_WS(':', 'app', `app2_app`.id) 
+  WHERE `app2_app_state_v2`.name = 'auth' AND `app2_app_state_v2`.entity_id  LIKE 'person:%' 
+  AND `app2_app`.name IN ('deskpro-app-trello');
+SQL;
+        return $sql;
+    }
+
+    public function getFixTrelloCardsPermissionsSQL()
+    {
+
+        $sql = <<<'SQL'
+  UPDATE
+  `app2_app_state_v2` 
+  INNER JOIN `app2_app_instance` ON `app2_app_state_v2`.app_instance_id = `app2_app_instance`.id
+  INNER JOIN `app2_app` ON `app2_app`.id = `app2_app_instance`.app_id
+  SET `app2_app_state_v2`.perm_read = 'EVERYBODY', `app2_app_state_v2`.perm_write = 'EVERYBODY'  
+  WHERE `app2_app_state_v2`.name = 'cards' AND ( `app2_app_state_v2`.perm_read = 'OWNER' OR `app2_app_state_v2`.perm_write = 'OWNER' )   
+  AND `app2_app`.name IN ('deskpro-app-trello');
+SQL;
+        return $sql;
+    }
+
+    public function fixTrelloCardsPermissionsInManifest()
+    {
+        $sql = <<<'SQL'
+SELECT
+  `id`, `manifest` from 
+  `app2_app`    
+  WHERE `app2_app`.name IN ('deskpro-app-trello');
+SQL;
+
+        $fixedManifests = [];
+
+        $entries = $this->getDbConnection()->fetchAll($sql);
+        foreach ($entries as $trelloApp) {
+            $fixedManifest = $this->fixManifest($trelloApp['manifest']);
+            if ($fixedManifest) {
+                $id = $trelloApp['id'];
+                $fixedManifests[$id] = $fixedManifest;
+            }
+        }
+
+        return $fixedManifests;
+    }
+
+    private function fixManifest($manifest) {
+        $parsed = json_decode($manifest, $assoc = true);
+        if (! is_array($parsed)) {
+            return null;
+        }
+
+        $wasFixed = false;
+        if (array_key_exists('state', $parsed) && is_array($parsed['state'])) {
+            foreach ($parsed['state'] as $key => $state) {
+                if (
+                    is_array($state)
+                    && array_key_exists('name', $state)
+                    && is_string($state['name'])
+                    && array_key_exists('permRead', $state)
+                    && is_string($state['permRead'])
+                    && array_key_exists('permWrite', $state)
+                    && is_string($state['permWrite'])
+                    && $state['name'] === 'cards'
+                    && ( $state['permRead'] !== 'EVERYBODY' || $state['permWrite'] !== 'EVERYBODY' )
+                ) {
+                    $parsed['state'][$key]['permRead'] = 'EVERYBODY';
+                    $parsed['state'][$key]['permWrite'] = 'EVERYBODY';
+                    $wasFixed = true;
+                }
+            }
+        }
+
+        return $wasFixed ? json_encode($parsed) : null;
+    }
+
+
 }
