60068bc9a109894764f5ad87d5bbd0944db8e839
Julien Ducro <julien.ducro@deskpro.com>
2017-09-13 14:55:31 +0100

    Fix update of types on Snippets

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Entity/Snippet.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Entity/Snippet.php
@@ -331,6 +331,7 @@ class Snippet implements EntityInterface, NotifyPropertyChanged
         if (!in_array($type, $this->types, true)) {
             $this->types[] = $type;
         }
+        $this->setModelField('types', $this->types);
 
         return $this;
     }
@@ -346,6 +347,7 @@ class Snippet implements EntityInterface, NotifyPropertyChanged
             unset($this->types[$key]);
             $this->types = array_values($this->types);
         }
+        $this->setModelField('types', $this->types);
 
         return $this;
     }
