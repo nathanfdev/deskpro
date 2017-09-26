b7c169ba4064368b25de53fa2a38afc14b67a4a3
Sergey Yastrebov <serg.yastrebov@gmail.com>
2017-09-26 19:40:34 +0300

    cloud demo forgot password action

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Cloud/CloudApiController.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Cloud/CloudApiController.php
@@ -35,6 +35,9 @@ use DeskPRO\Bundle\ApiBundle\Controller\BaseController;
 use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\ApiModes;
 use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\ApiUserContext;
 use DeskPRO\Bundle\AppBundle\DependencyInjection\SystemServices\EnvironmentService;
+use DeskPRO\Bundle\AppBundle\Form\Error\Exception\InvalidFormException;
+use DeskPRO\Bundle\AppBundle\Form\Type\People\PersonPasswordResetRequestType;
+use DpSys\License;
 use DpSys\LowError\SystemErrorHandler;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
@@ -292,6 +295,53 @@ class CloudApiController extends BaseController
             'success'  => true,
             'agentUrl' => $this->get('router')->generate('agent', [], UrlGeneratorInterface::ABSOLUTE_URL),
         ]));
+    }
+
+    /**
+     * @Rest\Post("/forgot_password")
+     * @ApiUserContext("open")
+     * @ApiModes("all")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function forgotPasswordAction(Request $request)
+    {
+        $this->assertValidDemoCall();
+
+        $form = $this->createForm(PersonPasswordResetRequestType::class);
+        $form->submit($request->request->all());
+
+        if (!$form->isValid()) {
+            throw new InvalidFormException($form);
+        }
+
+        $email     = $form->get('email')->getData();
+        $person    = $this->get('data.person')->getPersonForEmail($email);
+        $interface = 'agent';
+        if (License::getLicense()->isPastExpireDate() || (defined('DPC_BILL_FAILED') && DPC_BILL_FAILED)) {
+            $interface = 'billing';
+        }
+
+        $code_data = TmpData::create('reset-password', ['person_id' => $person['id'], 'interface' => $interface], '+3 days');
+        $this->getManager()->persist($code_data);
+        $this->getManager()->flush();
+
+        $vars = [
+            'code'      => $code_data->getCode(),
+            'person'    => $person,
+            'email'     => $email,
+            'interface' => $interface,
+        ];
+
+        $message = $this->container->get('mailer')->createMessage();
+        $message->setTemplate('DeskPRO:emails_user:reset-password.html.twig', $vars);
+        $message->setTo($form->get('email')->getData(), $person->getDisplayName());
+
+        $this->container->get('mailer')->send($message);
+
+        return new View();
     }
 
     private function assertValidDemoCall()
---
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/People/PersonPasswordResetRequestType.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * a British company located in London, England.
+ *
+ * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ *
+ * The license agreement under which this software is released
+ * can be found at https://www.deskpro.com/eula/
+ *
+ * By using this software, you acknowledge having read the license
+ * and agree to be bound thereby.
+ *
+ * Please note that DeskPRO is not free software. We release the full
+ * source code for our software because we trust our users to pay us for
+ * the huge investment in time and energy that has gone into both creating
+ * this software and supporting our customers. By providing the source code
+ * we preserve our customers' ability to modify, audit and learn from our
+ * work. We have been developing DeskPRO since 2001, please help us make it
+ * another decade.
+ *
+ * Like the work you see? Think you could make it better? We are always
+ * looking for great developers to join us: http://www.deskpro.com/jobs/
+ *
+ * ~ Thanks, Everyone at Team DeskPRO
+ */
+
+namespace DeskPRO\Bundle\AppBundle\Form\Type\People;
+
+use DeskPRO\Bundle\AppBundle\Validator\Constraints as AppAssert;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class PersonPasswordResetRequestType.
+ */
+class PersonPasswordResetRequestType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('email', EmailType::class, [
+            'constraints' => [
+                new Assert\NotBlank(),
+                new Assert\Email(['strict' => true]),
+                new AppAssert\Person\Email\ExistEmail(),
+                new AppAssert\Person\PersonType(['type' => 'agent']),
+            ],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_protection'               => false,
+            'csrf_double_submit_protection' => false,
+        ]);
+    }
+}
