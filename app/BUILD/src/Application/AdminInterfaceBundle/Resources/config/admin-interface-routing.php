3155c85714fd8616ee1d5e765963ce8bc04fb63e
radu <radu.helstern@deskpro.com>
2017-10-04 13:02:04 +0100

    remove test admin page

--- app/BUILD/src/Application/AdminInterfaceBundle/Resources/config/admin-interface-routing.php
+++ app/BUILD/src/Application/AdminInterfaceBundle/Resources/config/admin-interface-routing.php
@@ -126,11 +126,4 @@ $collection->create('gmail_token', [
     'controller' => 'AdminInterfaceBundle:Gmail:requestAccessToken',
 ]);
 
-// Apps2 Admin
-
-$collection->create('apps-admin', [
-    'path'       => '/admin-apps',
-    'controller' => 'AdminInterfaceBundle:Index:appsInstaller',
-]);
-
 return $collection;
