91f52b91dd4bd7400b545b2e1c25fa8b9cf68dd5
Sergey Yastrebov <serg.yastrebov@gmail.com>
2017-10-16 17:16:58 +0300

    agent assets url fix

--- app/BUILD/src/Application/DeskPRO/Entity/AgentTeam.php
+++ app/BUILD/src/Application/DeskPRO/Entity/AgentTeam.php
@@ -143,7 +143,7 @@ class AgentTeam extends DomainObject implements PersonList, AvatarOwner
     public function getAvatarUrl($size = 50)
     {
         if (!$this->hasAvatar()) {
-            return App::get('router')->generate(
+            return App::get('router.default')->generate(
                 'serve_default_picture',
                 [
                     's'        => $size,
