1c40bb806b136e797bf644381a41ef37ac1f8871
Christopher Nadeau <chris.nadeau@deskpro.com>
2017-11-03 11:01:00 +0000

    Remove note comments

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2017/11/Build1509706440.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2017/11/Build1509706440.php
@@ -28,16 +28,6 @@
 
 namespace Application\InstallBundle\Upgrade\Build;
 
-// NOTE: I used the BlockingBuildInterface interface because
-//       it looks like your schema changes are NOT backwards compatible with the previous version.
-//       You should double-check this yourself though. If they are backwards compatible, use OnlineBuildInterface instead.
-
-// NOTE: I have added the SkipPostBuildInterface interface because
-//       it looks like you do not have any changes that require PostBuild to run.
-//       You should double-check this yourself though. Remove the SkipPostBuildInterface interface if necessary.
-
-// Please remove these NOTE comments after you have checked the code.
-
 class Build1509706440 extends AbstractBuild implements BlockingBuildInterface, SkipPostBuildInterface
 {
     public function addNewTables()
