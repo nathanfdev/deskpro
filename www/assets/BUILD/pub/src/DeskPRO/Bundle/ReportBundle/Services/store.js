e44ba99ba2989ed2c9f7ffdaffb19406d7413c1f
Christopher Nadeau <chris.nadeau@deskpro.com>
2017-11-22 17:11:50 +0000

    redux-form in reports

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/ReportBundle/Services/store.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/ReportBundle/Services/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import { combineReducerHierarchy } from 'Ampliflux';
 import * as ampMiddleware from 'Ampliflux/middleware';
 import ReportAppReducers from '../ReportApp_Reducers';
 import AppReducers from '../../AppBundle/AppApp_Reducers';
 
-const reducer = combineReducerHierarchy(Object.assign({}, ReportAppReducers, AppReducers));
+const reducer = combineReducerHierarchy(Object.assign({
+  form: formReducer
+}, ReportAppReducers, AppReducers));
 const middleware = applyMiddleware(
   ampMiddleware.timerMiddleware('startTime'),
   ampMiddleware.intervalMiddleware,
