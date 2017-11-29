86b506981e109ecdd9c9e8159241c532e7f2d2b2
Julien Ducro <julien.ducro@deskpro.com>
2017-11-29 15:27:24 +0000

    Restore new registration agent notifications

--- app/BUILD/src/Application/DeskPRO/Notifications/NewRegistrationNotification.php
+++ app/BUILD/src/Application/DeskPRO/Notifications/NewRegistrationNotification.php
@@ -49,9 +49,7 @@ class NewRegistrationNotification extends AbstractAgentNotification
 
     public function shouldSendBrowserNotification(Person $agent)
     {
-        if ($this->person->is_confirmed) {
-            return true;
-        } elseif ($this->person->is_confirmed && $agent->getPref('agent_notif.new_user.alert')) {
+        if ($this->person->is_confirmed && $agent->getPref('agent_notif.new_user.alert')) {
             return true;
         }
 
@@ -60,9 +58,7 @@ class NewRegistrationNotification extends AbstractAgentNotification
 
     public function shouldSendEmailNotification(Person $agent)
     {
-        if ($this->person->is_confirmed) {
-            return true;
-        } elseif ($this->person->is_confirmed && $agent->getPref('agent_notif.new_user.email')) {
+        if ($this->person->is_confirmed && $agent->getPref('agent_notif.new_user.email')) {
             return true;
         }
 
