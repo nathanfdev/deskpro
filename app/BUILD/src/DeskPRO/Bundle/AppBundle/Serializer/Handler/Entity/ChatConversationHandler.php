9d4b14a57110e223d96037a6ad3569d4b405288a
Sergey Yastrebov <serg.yastrebov@gmail.com>
2017-12-06 16:54:51 +0300

    api /user_chat filters

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Serializer/Handler/Entity/ChatConversationHandler.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Serializer/Handler/Entity/ChatConversationHandler.php
@@ -29,10 +29,13 @@
 namespace DeskPRO\Bundle\AppBundle\Serializer\Handler\Entity;
 
 use Application\DeskPRO\Entity\ChatConversation;
+use Application\DeskPRO\Entity\LabelChatConversation;
+use DeskPRO\Bundle\AppBundle\Serializer\Deferred\CallbackDeferredProperty;
 use DeskPRO\Bundle\AppBundle\Serializer\Model\Chats\Chat;
 use DeskPRO\Bundle\AppBundle\Serializer\Model\Chats\ChatCsv;
 use DeskPRO\Bundle\AppBundle\Serializer\Model\Chats\WidgetChat;
 use DeskPRO\Bundle\AppBundle\Serializer\Sideload\SideloadSerializationContext;
+use Doctrine\ORM\EntityManager;
 
 /**
  * Class ChatConversationHandler.
@@ -40,12 +43,38 @@ use DeskPRO\Bundle\AppBundle\Serializer\Sideload\SideloadSerializationContext;
 class ChatConversationHandler extends AbstractEntityHandler
 {
     /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @var array
+     */
+    private $chatIds = [];
+
+    /**
+     * @var array
+     */
+    private $labels;
+
+    /**
+     * Constructor.
+     *
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
      * {@inheritdoc}
      *
      * @param ChatConversation $entity
      */
     protected function createModel($entity, SideloadSerializationContext $context)
     {
+        $this->chatIds[] = $entity->getId();
         $serializerClass = $context->getMappedClass(ChatConversation::class);
 
         if ($serializerClass === ChatCsv::class) {
@@ -54,7 +83,10 @@ class ChatConversationHandler extends AbstractEntityHandler
             return new WidgetChat($entity);
         }
 
-        return new Chat($entity);
+        $model = new Chat($entity);
+        $model->setLabels(new CallbackDeferredProperty([$this, 'getLabels'], [$entity]));
+
+        return $model;
     }
 
     /**
@@ -63,5 +95,30 @@ class ChatConversationHandler extends AbstractEntityHandler
     public static function getClassNames()
     {
         return ChatConversation::class;
+    }
+
+    /**
+     * @param ChatConversation $entity
+     *
+     * @return LabelChatConversation[]
+     */
+    public function getLabels(ChatConversation $entity)
+    {
+        if (null === $this->labels) {
+            $result = $this->em->getRepository(LabelChatConversation::class)->findBy([
+                'chat' => $this->chatIds,
+            ]);
+
+            $this->labels = [];
+            foreach ($result as $label) {
+                $this->labels[$label->getChat()->getId()][] = $label;
+            }
+        }
+
+        if (isset($this->labels[$entity->getId()])) {
+            return $this->labels[$entity->getId()];
+        }
+
+        return [];
     }
 }
---
+++ app/BUILD/tests/features/api/chats/user/user_chat_filters.feature
@@ -0,0 +1,56 @@
+@new
+Feature: /user_chats endpoint
+  To check list filters
+
+  Background:
+    Given I'm authenticated as admin
+    And agent and user exist
+    And only the following Chat records exist:
+      | #  | Subject | Status | Agent   | Person |
+      | c1 | Chat1   | open   | {admin} | {user} |
+      | c2 | Chat2   | ended  | {admin} | NULL   |
+      | c3 | Chat3   | ended  | {agent} | NULL   |
+      | c4 | Chat4   | open   | NULL    | {user} |
+    And only the following LabelChatConversation records exist:
+      | Chat | Label   |
+      | {c1} | label_1 |
+      | {c1} | label_2 |
+      | {c2} | label_2 |
+      | {c2} | label_3 |
+      | {c4} | label_4 |
+      | {c4} | label_5 |
+
+  Scenario: I filter by admin
+    When I send a GET request to "/api/v2/user_chats?agent={admin}&order_by=id&order_dir=asc"
+    Then the JSON node "data" should have 2 elements
+    Then the JSON node "data[0].id" should be equal to "{c1}"
+    Then the JSON node "data[1].id" should be equal to "{c2}"
+
+  Scenario: I filter by agent
+    When I send a GET request to "/api/v2/user_chats?agent={agent}&order_by=id&order_dir=asc"
+    Then the JSON node "data" should have 1 element
+    Then the JSON node "data[0].id" should be equal to "{c3}"
+
+  Scenario: I filter by person
+    When I send a GET request to "/api/v2/user_chats?person={user}&order_by=id&order_dir=asc"
+    Then the JSON node "data" should have 2 elements
+    Then the JSON node "data[0].id" should be equal to "{c1}"
+    Then the JSON node "data[1].id" should be equal to "{c4}"
+
+  Scenario: I filter by labels
+    When I send a GET request to "/api/v2/user_chats?label=label_2&order_by=id&order_dir=asc"
+    Then the JSON node "data" should have 2 elements
+    Then the JSON node "data[0].id" should be equal to "{c1}"
+    Then the JSON node "data[1].id" should be equal to "{c2}"
+
+  Scenario: I filter by status 'open'
+    When I send a GET request to "/api/v2/user_chats?status=open&order_by=id&order_dir=asc"
+    Then the JSON node "data" should have 2 elements
+    Then the JSON node "data[0].id" should be equal to "{c1}"
+    Then the JSON node "data[1].id" should be equal to "{c4}"
+
+  Scenario: I filter by status 'open'
+    When I send a GET request to "/api/v2/user_chats?status=ended&order_by=id&order_dir=asc"
+    Then the JSON node "data" should have 2 elements
+    Then the JSON node "data[0].id" should be equal to "{c2}"
+    Then the JSON node "data[1].id" should be equal to "{c3}"
