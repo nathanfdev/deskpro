bdd510ef129ad65ec0940072d09c924a579df6fa
Sergey Yastrebov <serg.yastrebov@gmail.com>
2017-12-14 23:32:30 +0300

    outgoing default ring tone

---
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Voice/Components/VoiceMenu/OutgoingCall/OutgoingCallAudio.js
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import '../../../../../Resources/sounds/outgoing-ringtone.mp3';
+
+class OutgoingCallAudio extends React.Component {
+
+  static propTypes = {
+    loop:          PropTypes.bool,
+    ringingVolume: PropTypes.string,
+    onSoundEnded:  PropTypes.func
+  };
+
+  componentWillUnmount() {
+    this.stopSound();
+  }
+
+  onSoundEnded = () => {
+    const { onSoundEnded, loop } = this.props;
+    if (onSoundEnded) {
+      onSoundEnded();
+    }
+    if (loop) {
+      this.playSound();
+    }
+  };
+
+  playSound() {
+    const { ringingVolume } = this.props;
+
+    this.stopSound();
+
+    this.sound.addEventListener('ended', this.onSoundEnded);
+    this.sound.volume = ringingVolume / 100;
+    this.sound.play();
+  }
+
+  stopSound() {
+    if (this.sound) {
+      this.sound.removeEventListener('ended', this.onSoundEnded);
+      this.sound.pause();
+      this.sound.currentTime = 0;
+    }
+  }
+
+  render() {
+    const soundsPath = `${window.DESKPRO_APP_ASSETS_URL}/DeskPRO/Bundle/AgentBundle/Resources/sounds`;
+
+    return (
+      <audio ref={(c) => { this.sound = c; }} preload="preload">
+        <source src={`${soundsPath}/outgoing-ringtone.mp3`} />
+      </audio>
+    );
+  }
+}
+
+export default OutgoingCallAudio;
