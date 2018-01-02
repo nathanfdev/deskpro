8a69f4b4e8c3592cae9999c3ea5ae97df4f0d1fd
Julien Ducro <julien.ducro@deskpro.com>
2018-01-02 17:52:38 +0000

    Fix reset password

--- app/BUILD/src/Application/UserBundle/Controller/LoginController.php
+++ app/BUILD/src/Application/UserBundle/Controller/LoginController.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -1118,7 +1118,7 @@ class LoginController extends AbstractController
                 );
             } else {
                 $resetUrl = $this->container->get('router')->generate(
-                    'user_login_resetpass_newpass',
+                    'portal_reset_password_process',
                     ['code' => $tmpdata->getCode()],
                     UrlGeneratorInterface::ABSOLUTE_URL
                 );
