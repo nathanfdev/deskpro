1e3712b3daa639fe2343eb61065679c0f91baf94
Christopher Nadeau <chris.nadeau@deskpro.com>
2018-01-03 15:34:27 +0000

    Log exception dont exit

--- app/BUILD/sys/Features.php
+++ app/BUILD/sys/Features.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -125,7 +125,7 @@ final class Features
     {
         if (!$this->settingsResolver) {
             $e = new LogicException('There is no settings resolver set in DpSys\Features. Check the code!');
-            SystemErrorHandler::handleException($e);
+            SystemErrorHandler::logException($e);
 
             return false;
         }
