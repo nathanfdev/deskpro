99cc8579d778a510d10c06c65f18d86f3c922900
ruslanix <ruslanix.com@gmail.com>
2018-01-05 13:30:21 +0300

    #DP-191 100987: Browser notifications duration

--- app/BUILD/languages/default/agent.php
+++ app/BUILD/languages/default/agent.php
@@ -1809,6 +1809,7 @@ return [
     'agent.settings.desktop_notifs_enable'                     => 'Enable Desktop Notifications',
     'agent.settings.desktop_notifs_explicit_disabled'          => 'Desktop notifications have been explicitly disabled. They must be enabled for this domain via the browser itself.',
     'agent.settings.desktop_notifs_test'                       => 'Generate Test Notification',
+    'agent.settings.desktop_notifs_tip'                        => 'All browsers and platforms handle desktop notifications in slightly different ways. On some platforms, Deskpro cannot override notification behaviour and they may disappear faster than the time you select here.',
     'agent.settings.done_start_using'                          => 'Done, start using DeskPRO',
     'agent.settings.drop_here_insert_signature'                => 'Drop your <strong>image</strong> here to insert it into your signature',
     'agent.settings.edit_filter'                               => 'Edit Filter',
