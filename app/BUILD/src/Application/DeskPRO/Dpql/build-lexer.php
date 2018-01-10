b742043f116738e5e285b4fb4c437134241d65d0
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-01-10 14:55:45 +0300

    check if php lexer/parser generators deps are installed

--- app/BUILD/src/Application/DeskPRO/Dpql/build-lexer.php
+++ app/BUILD/src/Application/DeskPRO/Dpql/build-lexer.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -33,6 +33,11 @@ if (php_sapi_name() !== 'cli') {
 ini_set('display_errors', true);
 
 chdir(__DIR__);
+
+if (!file_exists('PHP/LexerGenerator.php')) {
+    echo 'LexerGenerator is not installed';
+    exit;
+}
 
 require_once 'PHP/LexerGenerator.php';
 $a = new PHP_LexerGenerator('Lexer.plex');
