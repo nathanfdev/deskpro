d47d09e82516c4e52bddaaf7ee77f795a478dedb
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-01-10 14:14:06 +0300

    undefined 'reports' index fix

--- app/BUILD/src/Application/DeskPRO/ResourceScanner/LanguagePhrases.php
+++ app/BUILD/src/Application/DeskPRO/ResourceScanner/LanguagePhrases.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -57,6 +57,7 @@ class LanguagePhrases
         $groupToReal = [
             'adm'     => 'admin',
             'admin'   => 'admin',
+            'reports' => 'admin',
             'api'     => 'api',
             'agent'   => 'agent',
             'general' => 'general',
