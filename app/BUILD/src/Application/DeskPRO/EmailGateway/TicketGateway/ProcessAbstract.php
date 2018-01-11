b61c443023777702859afa19b5615e1eb500260b
Julien Ducro <julien.ducro@deskpro.com>
2018-01-11 18:03:48 +0000

    DP-736 - Increase max CC limit on incoming emails

--- app/BUILD/src/Application/DeskPRO/EmailGateway/TicketGateway/ProcessAbstract.php
+++ app/BUILD/src/Application/DeskPRO/EmailGateway/TicketGateway/ProcessAbstract.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -180,8 +180,8 @@ abstract class ProcessAbstract
             $cc_email = $cc->getEmail();
             $this->logMessage("Checking cc: $cc_email");
 
-            // Max 10 CC's to prevent mass spamming
-            if ($count >= 10) {
+            // Max 100 CC's to prevent mass spamming
+            if ($count >= 100) {
                 $this->logMessage('CC limit reached, break');
                 break;
             }
