924295d040ee9230c2bf2b43c6a335817a73eff3
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-01-23 09:58:34 +0300

    typo fix

--- app/BUILD/src/Application/DeskPRO/CustomFields/Handler/Choice.php
+++ app/BUILD/src/Application/DeskPRO/CustomFields/Handler/Choice.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -313,17 +313,19 @@ class Choice extends HandlerAbstract
     }
 
     /**
-     * @param array $form_data
+     * @param array $formData
+     *
      * @return mixed|null
      */
-    private function findValue(array $form_data)
+    private function findValue(array $formData)
     {
         $names = $this->getAllFormFieldNames();
         foreach ($names as $name) {
             if (!empty($formData[$name])) {
-                return $form_data[$name];
+                return $formData[$name];
             }
         }
+
         return null;
     }
 
