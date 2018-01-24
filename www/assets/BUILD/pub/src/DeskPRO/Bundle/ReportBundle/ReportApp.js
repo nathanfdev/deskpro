072b169822cb79031ce26f545debbf62ca004fc7
ImmortaL <denis@ranneft.ru>
2018-01-24 13:28:25 +0300

    use licensed amcharts, #DP-874

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/ReportBundle/ReportApp.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/ReportBundle/ReportApp.js
@@ -1,9 +1,11 @@
 import 'babel-polyfill';
-import 'amcharts3/amcharts/amcharts';
-import 'amcharts3/amcharts/serial';
-import 'amcharts3/amcharts/pie';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../../../../vendor/amcharts3/amcharts';
+import '../../../../vendor/amcharts3/serial';
+import '../../../../vendor/amcharts3/pie';
+import '../../../../vendor/amcharts3/themes/light';
+import '../../../../vendor/amcharts3/themes/dark';
 import AppContainer from './Modules/Application/Components/AppContainer';
 
 class ReportApp {
