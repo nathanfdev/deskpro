c71059f3696b5c8e72cb29060d3461a6f1848b0a
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-01-25 17:48:11 +0300

    upgraded twilio/sdk with undefined index fixes

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Twilio/TwilioAdapter.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Twilio/TwilioAdapter.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -259,7 +259,7 @@ class TwilioAdapter
         ]);
 
         // add additional activity
-        $taskRouter->workspaces($workspace->sid)->activities->create('IdleDisabled', 'false');
+        $taskRouter->workspaces($workspace->sid)->activities->create('IdleDisabled', ['available' => false]);
 
         return $workspace;
     }
