2de49519c7c5b47e7fe85d0991b51a32b741db24
ruslanix <ruslanix.com@gmail.com>
2018-01-26 17:37:27 +0300

    #DP-765 Calculations of total integers in DPQL reports incorrect ignoring decimals

--- app/BUILD/src/Application/DeskPRO/Dpql/Renderer/Csv.php
+++ app/BUILD/src/Application/DeskPRO/Dpql/Renderer/Csv.php
@@ -160,7 +160,7 @@ class Csv extends AbstractRenderer
                     if (!isset($columnTotals[$id])) {
                         $columnTotals[$id] = 0;
                     }
-                    $columnTotals[$id] += (int) $this->getColumnValue($row, $id);
+                    $columnTotals[$id] += (float) $this->getColumnValue($row, $id);
                 }
             }
 
@@ -265,11 +265,11 @@ class Csv extends AbstractRenderer
                 $columns[] = $this->wrapCell($value);
 
                 if ($totalType) {
-                    $rowTotal += (int) str_replace(',', '', $value);
+                    $rowTotal += (float) str_replace(',', '', $value);
                     if (!isset($columnTotals[$xPath])) {
                         $columnTotals[$xPath] = 0;
                     }
-                    $columnTotals[$xPath] += (int) str_replace(',', '', $value);
+                    $columnTotals[$xPath] += (float) str_replace(',', '', $value);
                 }
             }
 
--- app/BUILD/src/Application/DeskPRO/Dpql/Renderer/Html.php
+++ app/BUILD/src/Application/DeskPRO/Dpql/Renderer/Html.php
@@ -312,7 +312,7 @@ class Html extends AbstractRenderer
                 if (!isset($columnTotals[$id])) {
                     $columnTotals[$id] = 0;
                 }
-                $columnTotals[$id] += (int) $this->getColumnValue($row, $id);
+                $columnTotals[$id] += (float) $this->getColumnValue($row, $id);
             }
         }
 
@@ -509,11 +509,11 @@ class Html extends AbstractRenderer
                 $cells[] = "<td>$value</td>";
 
                 if ($totalType) {
-                    $rowTotal += (int) str_replace(',', '', $value);
+                    $rowTotal += (float) str_replace(',', '', $value);
                     if (!isset($columnTotals[$xPath])) {
                         $columnTotals[$xPath] = 0;
                     }
-                    $columnTotals[$xPath] += (int) str_replace(',', '', $value);
+                    $columnTotals[$xPath] += (float) str_replace(',', '', $value);
                 }
             }
 
