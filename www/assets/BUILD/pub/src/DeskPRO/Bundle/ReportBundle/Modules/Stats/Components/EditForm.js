67700f06a7dab7d6afb39f553584bad6af37c7a8
ImmortaL <denis@ranneft.ru>
2018-01-30 22:27:41 +0300

    fixed labels for edit form, #DP-848

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/ReportBundle/Modules/Stats/Components/EditForm.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/ReportBundle/Modules/Stats/Components/EditForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Container, Tabs, TabLink, Section } from '@deskpro/react-components';
-import { Input, Checkbox, Textarea, Select, validators } from '@deskpro/react-components/lib/bindings/redux-form';
+import { Input, Checkbox, Textarea, Select, TagSet, validators } from '@deskpro/react-components/lib/bindings/redux-form';
 import { formValues, Field, FieldArray, FormSection } from 'redux-form';
 import classNames from 'classnames';
 import { varTypes } from './helper';
@@ -134,7 +134,32 @@ class VarsFieldComponent extends React.PureComponent {
   }
 }
 
+class LabelsFieldComponent extends React.PureComponent {
+  static defaultProps = {
+    labels: []
+  };
+
+  static propTypes = {
+    fields:  PropTypes.object.isRequired,
+    options: PropTypes.array.isRequired,
+  };
+
+  render() {
+    const { fields, options } = this.props;
+
+    const newOptions = options.map(label => label.label);
+
+    return (<TagSet
+      name="labels"
+      label="Labels"
+      tags={fields.getAll()}
+      options={newOptions}
+    />);
+  }
+}
+
 const VarsField = formValues('vars')(VarsFieldComponent);
+const LabelsField = formValues('labels')(LabelsFieldComponent);
 
 export class EditFormComponent extends React.PureComponent {
 
@@ -145,11 +170,13 @@ export class EditFormComponent extends React.PureComponent {
     change:       null,
     queryValues:  {},
     handleSubmit: null,
-    error:        null
+    error:        null,
+    labels:       []
   };
 
   static propTypes = {
     groupParams:  PropTypes.object.isRequired,
+    labels:       PropTypes.array.isRequired,
     select:       PropTypes.string,
     groupBy:      PropTypes.string,
     dpqlParser:   PropTypes.func,
@@ -242,10 +269,11 @@ export class EditFormComponent extends React.PureComponent {
   };
 
   render() {
-    const select  = this.props.select;
     const groupBy = this.props.groupBy || '';
+    const { select, groupParams, labels } = this.props;
 
-    const renderVars = field => <VarsField fields={field.fields} groupParams={this.props.groupParams || {}} />;
+    const renderVars = field => <VarsField fields={field.fields} groupParams={groupParams || {}} />;
+    const renderLabels = field => <LabelsField fields={field.fields} options={labels} />;
 
     return (
       <form onSubmit={this.props.handleSubmit}>
@@ -256,11 +284,7 @@ export class EditFormComponent extends React.PureComponent {
             name="title"
             validate={validators.required}
           />
-          <Input
-            label="Labels"
-            id="labels"
-            name="labels"
-          />
+          <FieldArray name="labels" component={renderLabels} />
           <Field component="input" type="hidden" name="query_input_mode" />
           <div className="query-builder-input">
             <Tabs active={this.state.queryInputMode} onChange={this.queryModeChange}>
