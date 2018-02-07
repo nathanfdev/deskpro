e2a5cd0575c8bcca3ad60971bd28f2264bc9aab0
ImmortaL <denis@ranneft.ru>
2018-02-07 16:57:15 +0300

    make saved reports ~~great~~ work again, #DP-958

--- app/BUILD/src/Application/ReportsInterfaceBundle/Controller/HeadlessController.php
+++ app/BUILD/src/Application/ReportsInterfaceBundle/Controller/HeadlessController.php
@@ -49,11 +49,12 @@ class HeadlessController extends \Application\DeskPRO\Controller\AbstractControl
         $widgetService = $this->get('dashboard.widget.service');
         $widgets       = [];
         foreach ($report->getSavedWidgets() as $savedWidget) {
-            $wdata         = $widgetService->getWidgetData($savedWidget);
-            $wdata['id']   = $savedWidget->getDashboardWidget()->getId();
-            $wdata['type'] = $widgetService->getWidgetType($savedWidget->getType());
-            $wdata['data'] = $savedWidget->getData();
-            $widgets[]     = $wdata;
+            $wdata                = $widgetService->getWidgetData($savedWidget);
+            $wdata['id']          = $savedWidget->getDashboardWidget()->getId();
+            $wdata['widget_type'] = $widgetService->getWidgetType($savedWidget->getType());
+            $wdata['type']        = $savedWidget->getType();
+            $wdata['data']        = $savedWidget->getData() ?: null;
+            $widgets[]            = $wdata;
         }
 
         return $this->render('ReportsInterfaceBundle:Headless:headless.html.twig', [
