2b2c97ad7d3190f64d36da4f6cc82c8120b7b68c
Christopher Nadeau <chris@nadeau.tv>
2018-02-09 16:37:23 +0000

    Fix typo in table name

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2018/02/Build1517911793.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2018/02/Build1517911793.php
@@ -39,12 +39,12 @@ class Build1517911793 extends AbstractBuild implements BlockingBuildInterface
         $this->execDbQuery('default', 'ALTER TABLE settings_brand MODIFY id INT NOT NULL');
         $this->execDbQuery('default', 'ALTER TABLE settings_brand DROP PRIMARY KEY');
         $this->execDbQuery('default', 'ALTER TABLE settings_brand DROP id');
-        $this->execDbQuery('default', 'ALTER TABLE settings_brand ADD PRIMARY KEY (name)');
+        $this->execDbQuery('default', 'ALTER TABLE settings_brand ADD PRIMARY KEY (name, brand_id)');
 
         $this->execDbQuery('default', 'ALTER TABLE settings MODIFY id INT NOT NULL');
         $this->execDbQuery('default', 'ALTER TABLE settings DROP PRIMARY KEY');
         $this->execDbQuery('default', 'ALTER TABLE settings DROP id');
-        $this->execDbQuery('default', 'ALTER TABLE settings ADD PRIMARY KEY (name, brand_id)');
+        $this->execDbQuery('default', 'ALTER TABLE settings ADD PRIMARY KEY (name)');
     }
 
     public function run()
