c172ee4adc3b81fcafaae92f6297fd1c1fd2c699
radu <radu.helstern@deskpro.com>
2018-02-12 17:40:41 +0000

    DP-981  allow webhook invocation through get requests

--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -885,6 +885,7 @@ return [
         'skipPostBuild' => false,
         'isOnlineBuild' => true,
     ],
+<<<<<<< HEAD
     1518434839 => [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2018/02/Build1518434839.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1518434839',
@@ -903,6 +904,11 @@ return [
     1519218919 => [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2018/02/Build1519218919.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1519218919',
+=======
+    1518452652 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2018/02/Build1518452652.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1518452652',
+>>>>>>> DP-981  allow webhook invocation through get requests
         'skipPostBuild' => false,
         'isOnlineBuild' => false,
     ],
