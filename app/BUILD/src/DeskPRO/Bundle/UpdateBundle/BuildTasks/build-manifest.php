6dcdd8816575162dd96af46fdaf32f01fceafd7b
radu <radu.helstern@deskpro.com>
2018-02-13 17:11:28 +0000

    synchronize migrations

--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -885,12 +885,6 @@ return [
         'skipPostBuild' => false,
         'isOnlineBuild' => true,
     ],
-    1518452652 => [
-        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2018/02/Build1518452652.php',
-        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1518452652',
-        'skipPostBuild' => false,
-        'isOnlineBuild' => false,
-    ],
     1518434839 => [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2018/02/Build1518434839.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1518434839',
@@ -902,5 +896,11 @@ return [
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1519215169',
         'skipPostBuild' => false,
         'isOnlineBuild' => true,
+    ],
+    1518452652 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2018/02/Build1518452652.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1518452652',
+        'skipPostBuild' => false,
+        'isOnlineBuild' => false,
     ],
 ];
