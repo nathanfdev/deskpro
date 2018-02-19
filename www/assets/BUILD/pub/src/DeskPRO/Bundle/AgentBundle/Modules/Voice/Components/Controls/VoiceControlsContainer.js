decc06e7a37ddf72dc905d7a490cf2f5703281e7
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-02-19 21:24:58 +0300

    set call state optimisation

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Voice/Components/Controls/VoiceControlsContainer.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Voice/Components/Controls/VoiceControlsContainer.js
@@ -74,11 +74,11 @@ class VoiceControlsContainer extends React.Component {
       const { status, participants } = this.state;
 
       if (connectionStatus === 'open') {
-        if (participants.contains(me.get('id')) || connection.message.Outbound) {
+        if (status !== 'active' && (participants.contains(me.get('id')) || connection.message.Outbound)) {
           this.setState({
             status: 'active'
           });
-        } else {
+        } else if (status !== 'connected' && status !== 'active') {
           this.setState({
             status: 'connected'
           });
@@ -261,7 +261,7 @@ class VoiceControlsContainer extends React.Component {
         {...this.props}
         {...this.state}
         onlineAgents={onlineAgents}
-        connection={this.getConnection()}
+        connection={connection}
         endCall={this.endCall}
         toggleMute={this.toggleMute}
         toggleHold={this.toggleHold}
