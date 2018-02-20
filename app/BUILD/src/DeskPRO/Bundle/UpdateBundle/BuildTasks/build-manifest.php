bd16c55f3c21b6671f3cd4099d7ed4be404bd24a
ruslanix <ruslanix.com@gmail.com>
2018-02-20 15:10:12 +0300

    DP-949 Link Feedback and Tickets

--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -889,12 +889,12 @@ return [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2018/02/Build1518434839.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1518434839',
         'skipPostBuild' => false,
-        'isOnlineBuild' => false,
+        'isOnlineBuild' => true,
     ],
     1519127633 => [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2018/02/Build1519127633.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1519127633',
         'skipPostBuild' => true,
         'isOnlineBuild' => true,
-    ]
+    ],
 ];
