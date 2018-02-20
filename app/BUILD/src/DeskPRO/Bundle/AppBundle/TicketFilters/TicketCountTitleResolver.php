bca73bf22ed5ba2ac1c3373c3bf8182f437843b0
Christopher Nadeau <chris.nadeau@deskpro.com>
2018-02-20 11:31:12 +0000

    Add title resolvers for other grouping fields

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/TicketFilters/TicketCountTitleResolver.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/TicketFilters/TicketCountTitleResolver.php
@@ -28,7 +28,14 @@
 
 namespace DeskPRO\Bundle\AppBundle\TicketFilters;
 
+use Application\DeskPRO\Entity\AgentTeam;
+use Application\DeskPRO\Entity\Department;
+use Application\DeskPRO\Entity\Language;
 use Application\DeskPRO\Entity\Person;
+use Application\DeskPRO\Entity\Product;
+use Application\DeskPRO\Entity\TicketCategory;
+use Application\DeskPRO\Entity\TicketPriority;
+use Application\DeskPRO\Entity\TicketWorkflow;
 use DeskPRO\Bundle\AppBundle\CountBadge\CountTitleResolver;
 use DeskPRO\Component\Util\MapUtils;
 use Symfony\Component\DependencyInjection\Container;
@@ -61,41 +68,99 @@ class TicketCountTitleResolver implements CountTitleResolver
                 break;
 
             case TicketSearchParams::GROUP_AGENT:
-                return MapUtils::rekeyByGetter(
+                return MapUtils::map(
                     $this->container
                         ->get('doctrine.orm.entity_manager')
                         ->getRepository(Person::class)
                         ->getByIds($values),
-                    'getId'
+                    function ($idx, Person $a) {
+                        return [$a->getId(), $a->getName()];
+                    }
                 );
                 break;
 
             case TicketSearchParams::GROUP_AGENT_TEAM:
-
+                return MapUtils::map(
+                    $this->container
+                        ->get('doctrine.orm.entity_manager')
+                        ->getRepository(AgentTeam::class)
+                        ->getByIds($values),
+                    function ($idx, AgentTeam $a) {
+                        return [$a->getId(), $a->getName()];
+                    }
+                );
                 break;
 
             case TicketSearchParams::GROUP_DEPARTMENT:
-
+                return MapUtils::map(
+                    $this->container
+                        ->get('doctrine.orm.entity_manager')
+                        ->getRepository(Department::class)
+                        ->getByIds($values),
+                    function ($idx, Department $a) {
+                        return [$a->getId(), $a->getFullTitle()];
+                    }
+                );
                 break;
 
             case TicketSearchParams::GROUP_WORKFLOW:
-
+                return MapUtils::map(
+                    $this->container
+                        ->get('doctrine.orm.entity_manager')
+                        ->getRepository(TicketWorkflow::class)
+                        ->getByIds($values),
+                    function ($idx, TicketWorkflow $a) {
+                        return [$a->getId(), $a->getTitle()];
+                    }
+                );
                 break;
 
             case TicketSearchParams::GROUP_PRIORITY:
-
+                return MapUtils::map(
+                    $this->container
+                        ->get('doctrine.orm.entity_manager')
+                        ->getRepository(TicketPriority::class)
+                        ->getByIds($values),
+                    function ($idx, TicketPriority $a) {
+                        return [$a->getId(), $a->getTitle()];
+                    }
+                );
                 break;
 
             case TicketSearchParams::GROUP_CATEGORY:
-
+                return MapUtils::map(
+                    $this->container
+                        ->get('doctrine.orm.entity_manager')
+                        ->getRepository(TicketCategory::class)
+                        ->getByIds($values),
+                    function ($idx, TicketCategory $a) {
+                        return [$a->getId(), $a->getTitle()];
+                    }
+                );
                 break;
 
             case TicketSearchParams::GROUP_PRODUCT:
-
+                return MapUtils::map(
+                    $this->container
+                        ->get('doctrine.orm.entity_manager')
+                        ->getRepository(Product::class)
+                        ->getByIds($values),
+                    function ($idx, Product $a) {
+                        return [$a->getId(), $a->getTitle()];
+                    }
+                );
                 break;
 
             case TicketSearchParams::GROUP_LANGUAGE:
-
+                return MapUtils::map(
+                    $this->container
+                        ->get('doctrine.orm.entity_manager')
+                        ->getRepository(Language::class)
+                        ->getByIds($values),
+                    function ($idx, Language $a) {
+                        return [$a->getId(), $a->getTitle()];
+                    }
+                );
                 break;
 
             default:
