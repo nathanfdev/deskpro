b925cd4327de2aab7f6cfbaf4b7579c912ea6371
ImmortaL <denis@ranneft.ru>
2018-03-01 16:58:34 +0300

    special action to check saml requirements, #DP-793

--- app/BUILD/src/Application/LegacyApiBundle/Controller/UsersourcesController.php
+++ app/BUILD/src/Application/LegacyApiBundle/Controller/UsersourcesController.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -374,6 +374,16 @@ class UsersourcesController extends AbstractController
             ],
             $adapter->getIframeTemplateParams(false))
         );
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return Response
+     */
+    public function checkRequirementsAction($id)
+    {
+        return $this->createApiSuccessResponse(['id' => $id]);
     }
 
     public function updateDisplayOrderAction()
