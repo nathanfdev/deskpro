0220dc757940ec70558ce5214986549b9907a740
ImmortaL <denis@ranneft.ru>
2018-03-01 17:10:08 +0300

    usersource_okta, check-req, #DP-793

--- app/BUILD/apps/deskpro_us_okta/native/native_config.php
+++ app/BUILD/apps/deskpro_us_okta/native/native_config.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -29,5 +29,8 @@
 return [
     'install' => [
         'handler' => 'deskpro_us_okta\\InstallerHandler',
+    ],
+    'api' => [
+        'package_request_handler' => 'deskpro_us_okta\\RequestHandler\\PackageRequestHandler',
     ],
 ];
