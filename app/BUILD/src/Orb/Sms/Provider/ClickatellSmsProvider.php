aec108e5423a6c318b14233c20ae2eb329002a2d
Julien Ducro <julien.ducro@deskpro.com>
2018-03-02 17:40:23 +0100

    Allow Clickatell to use http proxy

--- app/BUILD/src/Orb/Sms/Provider/ClickatellSmsProvider.php
+++ app/BUILD/src/Orb/Sms/Provider/ClickatellSmsProvider.php
@@ -4,7 +4,7 @@
  * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, DeskPRO Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -28,7 +28,7 @@
 
 namespace Orb\Sms\Provider;
 
-use Clickatell\Rest;
+use Orb\Sms\Provider\ClickatellRest as Rest;
 use Orb\Sms\SmsMessageChunk;
 use Orb\Sms\SmsProviderInterface;
 use Orb\Sms\SmsResult;
