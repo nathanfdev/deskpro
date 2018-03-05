508edd97149bcf8503187a79cab7c7f2f1c25a6b
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-03-05 22:54:09 +0300

    object alias validation

--- app/BUILD/src/Application/DeskPRO/CustomFields/Form/FormHelper.php
+++ app/BUILD/src/Application/DeskPRO/CustomFields/Form/FormHelper.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
- * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * Deskpro (r) has been developed by Deskpro Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, Deskpro Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -12,18 +12,18 @@
  * By using this software, you acknowledge having read the license
  * and agree to be bound thereby.
  *
- * Please note that DeskPRO is not free software. We release the full
+ * Please note that Deskpro is not free software. We release the full
  * source code for our software because we trust our users to pay us for
  * the huge investment in time and energy that has gone into both creating
  * this software and supporting our customers. By providing the source code
  * we preserve our customers' ability to modify, audit and learn from our
- * work. We have been developing DeskPRO since 2001, please help us make it
+ * work. We have been developing Deskpro since 2001, please help us make it
  * another decade.
  *
  * Like the work you see? Think you could make it better? We are always
  * looking for great developers to join us: http://www.deskpro.com/jobs/
  *
- * ~ Thanks, Everyone at Team DeskPRO
+ * ~ Thanks, Everyone at Team Deskpro
  */
 
 /**
@@ -47,17 +47,16 @@ class FormHelper
     private $formFactory;
 
     /**
-     * @var array
-     */
-    private $errors = [];
-
-    private $type;
-
-    /**
      * @var \Doctrine\ORM\EntityManager
      */
     private $em;
 
+    /**
+     * Constructor.
+     *
+     * @param EntityManager        $em
+     * @param FormFactoryInterface $formFactory
+     */
     public function __construct(EntityManager $em, FormFactoryInterface $formFactory)
     {
         $this->em          = $em;
@@ -66,34 +65,36 @@ class FormHelper
 
     /**
      * @param CustomDefAbstract $field
-     * @param $formData
+     * @param array             $formData
+     *
+     * @return \Symfony\Component\Form\FormInterface
+     */
+    public function buildForm(CustomDefAbstract $field, array $formData)
+    {
+        $handlerClass = $formData['handler_class'];
+        $baseType     = Util::getBaseClassname($handlerClass);
+        $modelClass   = 'Application\\DeskPRO\\CustomFields\\Form\\Model\\'.$baseType.'Field';
+        $typeClass    = 'Application\\DeskPRO\\CustomFields\\Form\\Type\\'.$baseType.'FieldType';
+
+        $editField = new $modelClass($field);
+        $formType  = new $typeClass();
+        $form      = $this->formFactory->create($formType, $editField);
+
+        return $form;
+    }
+
+    /**
+     * @param CustomDefAbstract $field
+     * @param array             $formData
      *
      * @throws \Exception
      */
     public function saveFormToField(CustomDefAbstract $field, array $formData)
     {
-        $this->errors = [];
-        $this->type   = null;
-        $handlerClass = empty($formData['handler_class']) ? $field['handler_class'] : $formData['handler_class'];
-        if (empty($handlerClass)) {
-            throw new \DomainException('missing handler_class');
-        }
-
-        if (empty($formData['handler_class'])) {
-            $formData['handler_class'] = $handlerClass;
-        }
-
-        $baseType   = Util::getBaseClassname($handlerClass);
-        $modelClass = 'Application\\DeskPRO\\CustomFields\\Form\\Model\\'.$baseType.'Field';
-        $typeClass  = 'Application\\DeskPRO\\CustomFields\\Form\\Type\\'.$baseType.'FieldType';
-
-        if (!isset($formData['choices_structure'])) {
-            $formData['choices_structure'] = [];
-        }
-
-        $editField = new $modelClass($field);
-        $formType  = new $typeClass();
-        $form      = $this->formFactory->create($formType, $editField);
+        $formData   = $this->prepareFormData($field, $formData);
+        $form       = $this->buildForm($field, $formData);
+        $editField  = $form->getData();
+        $modelClass = get_class($editField);
 
         $this->em->getConnection()->beginTransaction();
         try {
@@ -108,6 +109,15 @@ class FormHelper
             }
 
             $form->submit($formData);
+            if (!$form->isValid()) {
+                // check if the field's alias is valid
+                // e.g. to prevent unique key exceptions
+                $aliasErrors = $form->get('alias')->getErrors();
+                if (count($aliasErrors)) {
+                    throw new \RuntimeException($aliasErrors[0]->getMessage());
+                }
+            }
+
             $editField->save(); // sic! save anyway (as it was)
             $this->em->getConnection()->commit();
         } catch (\Exception $e) {
@@ -116,13 +126,28 @@ class FormHelper
         }
     }
 
-    public function getErrors()
+    /**
+     * @param CustomDefAbstract $field
+     * @param array             $formData
+     *
+     * @throws \DomainException
+     *
+     * @return array
+     */
+    private function prepareFormData(CustomDefAbstract $field, array $formData)
     {
-        return $this->errors;
-    }
+        $handlerClass = empty($formData['handler_class']) ? $field['handler_class'] : $formData['handler_class'];
+        if (empty($handlerClass)) {
+            throw new \DomainException('missing handler_class');
+        }
 
-    public function getType()
-    {
-        return $this->type;
+        if (empty($formData['handler_class'])) {
+            $formData['handler_class'] = $handlerClass;
+        }
+        if (!isset($formData['choices_structure'])) {
+            $formData['choices_structure'] = [];
+        }
+
+        return $formData;
     }
 }
