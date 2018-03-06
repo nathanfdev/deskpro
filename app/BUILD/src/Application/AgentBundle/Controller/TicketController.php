e814c00ff092baaf948ffe5cafb7aac57250dc09
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-03-06 13:15:48 +0300

    skip validation for realtime urgency changes on ticket view

--- app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
+++ app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
- * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * Deskpro (r) has been developed by Deskpro Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2018, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, Deskpro Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -12,18 +12,18 @@
  * By using this software, you acknowledge having read the license
  * and agree to be bound thereby.
  *
- * Please note that DeskPRO is not free software. We release the full
+ * Please note that Deskpro is not free software. We release the full
  * source code for our software because we trust our users to pay us for
  * the huge investment in time and energy that has gone into both creating
  * this software and supporting our customers. By providing the source code
  * we preserve our customers' ability to modify, audit and learn from our
- * work. We have been developing DeskPRO since 2001, please help us make it
+ * work. We have been developing Deskpro since 2001, please help us make it
  * another decade.
  *
  * Like the work you see? Think you could make it better? We are always
  * looking for great developers to join us: http://www.deskpro.com/jobs/
  *
- * ~ Thanks, Everyone at Team DeskPRO
+ * ~ Thanks, Everyone at Team Deskpro
  */
 
 namespace Application\AgentBundle\Controller;
@@ -2259,10 +2259,13 @@ class TicketController extends AbstractController
 
                 $actions = $this->in->getCleanValueArray('actions', 'raw', 'raw');
 
-                if (count($actions) == 1 && isset($actions['department_id'])) {
-                    // Validation not on dep changes,
-                    // because changing dep could change validation options
-                    $new_department_id = $actions['department_id'];
+                if (count($actions) == 1 && (isset($actions['department_id']) || isset($actions['urgency']))) {
+                    // skip validation for realtime updates
+                    if (isset($actions['department_id'])) {
+                        // Validation not on dep changes,
+                        // because changing dep could change validation options
+                        $new_department_id = $actions['department_id'];
+                    }
                 } elseif ($ticket->status == 'hidden' && count(
                         $actions
                     ) == 2 && isset($actions['status']) && isset($actions['hidden_status'])
