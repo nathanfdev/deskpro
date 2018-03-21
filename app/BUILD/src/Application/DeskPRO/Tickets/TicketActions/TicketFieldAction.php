637c180e96ea379537f140a0d9b50fc917df0d9e
ruslanix <ruslanix.com@gmail.com>
2018-03-21 12:05:27 +0300

    DP-1199 fix macro with unchecked checkbox

--- app/BUILD/src/Application/DeskPRO/Tickets/TicketActions/TicketFieldAction.php
+++ app/BUILD/src/Application/DeskPRO/Tickets/TicketActions/TicketFieldAction.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
- * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
+ * Deskpro (r) has been developed by Deskpro Ltd. https://www.deskpro.com/
  * a British company located in London, England.
  *
- * All source code and content Copyright (c) 2017, DeskPRO Ltd.
+ * All source code and content Copyright (c) 2018, Deskpro Ltd.
  *
  * The license agreement under which this software is released
  * can be found at https://www.deskpro.com/eula/
@@ -12,18 +12,18 @@
  * By using this software, you acknowledge having read the license
  * and agree to be bound thereby.
  *
- * Please note that DeskPRO is not free software. We release the full
+ * Please note that Deskpro is not free software. We release the full
  * source code for our software because we trust our users to pay us for
  * the huge investment in time and energy that has gone into both creating
  * this software and supporting our customers. By providing the source code
  * we preserve our customers' ability to modify, audit and learn from our
- * work. We have been developing DeskPRO since 2001, please help us make it
+ * work. We have been developing Deskpro since 2001, please help us make it
  * another decade.
  *
  * Like the work you see? Think you could make it better? We are always
  * looking for great developers to join us: http://www.deskpro.com/jobs/
  *
- * ~ Thanks, Everyone at Team DeskPRO
+ * ~ Thanks, Everyone at Team Deskpro
  */
 
 /**
@@ -60,6 +60,15 @@ class TicketFieldAction extends AbstractAction implements PermissionableAction
         $this->field_manager = $field_manager;
         $this->field_def     = $field_def;
         $this->set_value     = $set_value;
+
+        // for some of actions `custom_fields` might be empty
+        // for example for unchecked checkbox
+        // fill it manually to prevent errors
+        if (!array_key_exists('custom_fields', $this->set_value)) {
+            $this->set_value['custom_fields'] = [
+                'field_'.$field_def->getId() => null,
+            ];
+        }
     }
 
     /**
