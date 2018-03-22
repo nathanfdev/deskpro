5f2eb619229beb0c8396eda7829233f16034ff0f
Christopher Nadeau <chris.nadeau@deskpro.com>
2018-03-22 12:44:36 +0000

    Better exception message, better handling of empty vals

---
+++ app/BUILD/src/DeskPRO/Bundle/ReportBundle/Dpql2/DpqlCompileException.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * Deskpro (r) has been developed by Deskpro Ltd. https://www.deskpro.com/
+ * a British company located in London, England.
+ *
+ * All source code and content Copyright (c) 2018, Deskpro Ltd.
+ *
+ * The license agreement under which this software is released
+ * can be found at https://www.deskpro.com/eula/
+ *
+ * By using this software, you acknowledge having read the license
+ * and agree to be bound thereby.
+ *
+ * Please note that Deskpro is not free software. We release the full
+ * source code for our software because we trust our users to pay us for
+ * the huge investment in time and energy that has gone into both creating
+ * this software and supporting our customers. By providing the source code
+ * we preserve our customers' ability to modify, audit and learn from our
+ * work. We have been developing Deskpro since 2001, please help us make it
+ * another decade.
+ *
+ * Like the work you see? Think you could make it better? We are always
+ * looking for great developers to join us: http://www.deskpro.com/jobs/
+ *
+ * ~ Thanks, Everyone at Team Deskpro
+ */
+
+namespace DeskPRO\Bundle\ReportBundle\Dpql2;
+
+use DeskPRO\Bundle\AppBundle\Exception\HelpdeskInstanceExceptionInterface;
+use Throwable;
+
+/**
+ * The exception for an error that occurs when compiling.
+ */
+class DpqlCompileException extends \Exception implements HelpdeskInstanceExceptionInterface
+{
+    /**
+     * @var string
+     */
+    private $query;
+
+    /**
+     * @param DpqlException $e
+     * @param               $query
+     *
+     * @return DpqlCompileException
+     */
+    public static function createFromException(DpqlException $e, $query)
+    {
+        $message = $e->getMessage().' -- Query: '.$query;
+
+        return new self($message, $e->getCode(), $e, $query);
+    }
+
+    public function __construct($message = '', $code = 0, Throwable $previous = null, $query = '')
+    {
+        parent::__construct($message, $code, $previous);
+        $this->query = $query;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+}
