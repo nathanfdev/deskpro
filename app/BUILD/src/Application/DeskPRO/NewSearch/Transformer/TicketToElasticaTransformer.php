5fb8071377255ff0bccb713672eb2d470077fc27
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-03-26 19:02:45 +0300

    fix apache ticka

--- app/BUILD/src/Application/DeskPRO/NewSearch/Transformer/TicketToElasticaTransformer.php
+++ app/BUILD/src/Application/DeskPRO/NewSearch/Transformer/TicketToElasticaTransformer.php
@@ -135,7 +135,7 @@ class TicketToElasticaTransformer implements ModelToElasticaTransformerInterface
                     /** @var ApacheTikaManager $client */
                     $client = $this->getApacheTika()->getClient();
                     /** @var TicketAttachment $attachment */
-                    foreach ($attachments as $attachment) {
+                    foreach ($object->getAttachments() as $attachment) {
                         $blob = $attachment->getBlob();
                         if (!$blob->isImage()) {
                             $attachmentData[] = [
