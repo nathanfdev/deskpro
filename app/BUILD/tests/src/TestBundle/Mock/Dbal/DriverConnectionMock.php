e26104d7a021ec8029d1877ba85acdc6daa664c0
Julien Ducro <julien.ducro@deskpro.com>
2018-03-26 17:05:17 +0200

    fql - remove copyright

--- app/BUILD/tests/src/TestBundle/Mock/Dbal/DriverConnectionMock.php
+++ app/BUILD/tests/src/TestBundle/Mock/Dbal/DriverConnectionMock.php
@@ -1,43 +1,6 @@
 <?php
 
-<<<<<<< HEAD:app/BUILD/tests/src/TestBundle/Mock/Dbal/DriverConnectionMock.php
-/*
- * DeskPRO (r) has been developed by DeskPRO Ltd. https://www.deskpro.com/
- * a British company located in London, England.
- *
- * All source code and content Copyright (c) 2018, DeskPRO Ltd.
- *
- * The license agreement under which this software is released
- * can be found at https://www.deskpro.com/eula/
- *
- * By using this software, you acknowledge having read the license
- * and agree to be bound thereby.
- *
- * Please note that DeskPRO is not free software. We release the full
- * source code for our software because we trust our users to pay us for
- * the huge investment in time and energy that has gone into both creating
- * this software and supporting our customers. By providing the source code
- * we preserve our customers' ability to modify, audit and learn from our
- * work. We have been developing DeskPRO since 2001, please help us make it
- * another decade.
- *
- * Like the work you see? Think you could make it better? We are always
- * looking for great developers to join us: http://www.deskpro.com/jobs/
- *
- * ~ Thanks, Everyone at Team DeskPRO
- */
-
 namespace DpTestSrc\TestBundle\Mock\Dbal;
-=======
-/**
- * DeskPRO.
- */
-
-namespace DeskPRO\Bundle\AppBundle\TermEngine\Engine\Dbal\TermCompiler\Helper;
-
-use DeskPRO\Bundle\AppBundle\TermEngine\TermCompilerHelperInterface;
-use Psr\Log\LoggerInterface;
->>>>>>> origin/develop:app/BUILD/src/DeskPRO/Bundle/AppBundle/TermEngine/Engine/Dbal/TermCompiler/Helper/AbstractDbalHelper.php
 
 class DriverConnectionMock implements \Doctrine\DBAL\Driver\Connection
 {
