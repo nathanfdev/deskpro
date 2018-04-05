daf1fbe02ead88b77b0f7b495c14d37f639e3c82
ImmortaL <denis@ranneft.ru>
2018-04-05 13:12:44 +0300

    refresh position when scrolled, #DP-1339

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/ReportBundle/Modules/Stats/Components/TitleWithVars.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/ReportBundle/Modules/Stats/Components/TitleWithVars.js
@@ -30,15 +30,20 @@ class InlineSelectComp extends React.Component {
     const posLeft = pos.left;
     const posTop  = pos.top + 20;
 
+    document.addEventListener('scroll', this.refreshOnScroll, true);
+
     // eslint-disable-next-line react/no-did-mount-set-state
     this.setState({
       posTop, posLeft
     });
   }
 
-
   componentDidUpdate() {
     this.refreshSize();
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.refreshOnScroll, true);
   }
 
   onClick = (ev, value) => {
@@ -47,6 +52,20 @@ class InlineSelectComp extends React.Component {
       this.props.onChange(value);
       this.setState({ isOpen: false });
     }
+  };
+
+  refreshOnScroll = () => {
+    const pos  = this.$el.offset();
+    const posLeft = pos.left;
+    const posTop  = pos.top + 20;
+
+    this.setState({
+      posTop, posLeft
+    }, () => {
+      if (this.state.isOpen) {
+        this.refreshSize();
+      }
+    });
   };
 
   handleClickOutside = () => {
