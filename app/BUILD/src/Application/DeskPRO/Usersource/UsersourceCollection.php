6079e3ad1ffe030ee49e8bff5b8a5a53c03123ad
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-04-17 00:59:21 +0300

    split portal auth by brand

--- app/BUILD/src/Application/DeskPRO/Usersource/UsersourceCollection.php
+++ app/BUILD/src/Application/DeskPRO/Usersource/UsersourceCollection.php
@@ -6,6 +6,7 @@
 
 namespace Application\DeskPRO\Usersource;
 
+use Application\DeskPRO\Entity\Brand;
 use Application\DeskPRO\Entity\Usersource;
 
 /**
@@ -31,6 +32,22 @@ class UsersourceCollection extends \ArrayObject
         }
 
         throw new \InvalidArgumentException("Unknown interface '$interface'");
+    }
+
+    /**
+     * @param Brand $brand
+     *
+     * @return static
+     */
+    public function forBrand(Brand $brand)
+    {
+        $filtered = array_filter(
+            (array) $this, function (Usersource $us) use ($brand) {
+                return $us->hasBrand($brand) || $us->isAllBrands();
+            }
+        );
+
+        return new static($filtered);
     }
 
     /**
