77a150a8478e910718700e3d7fb0e0013a3dadc8
ruslanix <ruslanix.com@gmail.com>
2018-04-16 15:36:57 +0300

    DP-1390 usersource to brand model

--- app/BUILD/src/Application/DeskPRO/Entity/Usersource.php
+++ app/BUILD/src/Application/DeskPRO/Entity/Usersource.php
@@ -11,6 +11,7 @@ namespace Application\DeskPRO\Entity;
 use Application\DeskPRO\Usersource\ActionsCollection;
 use Application\DeskPRO\Usersource\Adapter as UsersourceAdapter;
 use deskpro_us_jwt\Usersource\Adapter\Jwt as JwtAdapter;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use Orb\Util\Strings;
@@ -32,6 +33,8 @@ use Orb\Util\Util;
  * @property $auto_agent_props
  * @property $app
  * @property $id
+ * @property Brand[]|ArrayCollection      $brands
+ * @property $is_all_brands
  */
 class Usersource extends \Application\DeskPRO\Domain\DomainObject
 {
@@ -145,6 +148,18 @@ class Usersource extends \Application\DeskPRO\Domain\DomainObject
     protected $actions;
 
     /**
+     * @var \Doctrine\Common\Collections\ArrayCollection
+     */
+    protected $brands;
+
+    /**
+     * True if this usersource enabled for all brands.
+     *
+     * @var bool
+     */
+    protected $is_all_brands = true;
+
+    /**
      * @var array
      */
     public static $callbackAdapters = [
@@ -159,6 +174,7 @@ class Usersource extends \Application\DeskPRO\Domain\DomainObject
     public function __construct()
     {
         $this->actions = new ActionsCollection();
+        $this->brands  = new ArrayCollection();
     }
 
     /**
@@ -386,6 +402,68 @@ class Usersource extends \Application\DeskPRO\Domain\DomainObject
         return $this->actions ?: new ActionsCollection();
     }
 
+    /**
+     * @return ArrayCollection|Brand[]
+     */
+    public function getBrands()
+    {
+        return $this->brands;
+    }
+
+    /**
+     * @param Brand $brand
+     *
+     * @return self
+     */
+    public function addBrand(Brand $brand)
+    {
+        if (!$this->brands->contains($brand)) {
+            $this->brands->add($brand);
+            $this->_onPropertyChanged('brands', $this->brands, $this->brands);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Brand $brand
+     *
+     * @return self
+     */
+    public function removeBrand(Brand $brand)
+    {
+        $this->brands->removeElement($brand);
+        $this->_onPropertyChanged('brands', $this->brands, $this->brands);
+
+        return $this;
+    }
+
+    /**
+     * @param Brand $brand
+     *
+     * @return bool
+     */
+    public function hasBrand(Brand $brand)
+    {
+        return $this->brands->contains($brand);
+    }
+
+    /**
+     * @param bool $is_enabled
+     */
+    public function setIsAllBrands($is_all_brands)
+    {
+        $this->setModelField('is_all_brands', $is_all_brands);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAllBrands()
+    {
+        return (bool) $this->is_all_brands;
+    }
+
     //###########################################################################
     // Doctrine Metadata
     //###########################################################################
@@ -506,6 +584,47 @@ class Usersource extends \Application\DeskPRO\Domain\DomainObject
                     'columnDefinition'     => null,
                 ],
             ],
+        ]);
+
+        $metadata->mapManyToMany(
+            [
+                'fieldName'    => 'brands',
+                'targetEntity' => Brand::class,
+                'cascade'      => ['persist', 'merge'],
+                'fetch'        => ClassMetadataInfo::FETCH_EXTRA_LAZY,
+                'joinTable'    => [
+                    'name'        => 'usersource_to_brand',
+                    'schema'      => null,
+                    'joinColumns' => [
+                        0 => [
+                            'name'                 => 'usersource_id',
+                            'referencedColumnName' => 'id',
+                            'nullable'             => false,
+                            'onDelete'             => 'cascade',
+                            'columnDefinition'     => null,
+                        ],
+                    ],
+                    'inverseJoinColumns' => [
+                        0 => [
+                            'name'                 => 'brand_id',
+                            'referencedColumnName' => 'id',
+                            'nullable'             => false,
+                            'onDelete'             => 'cascade',
+                            'columnDefinition'     => null,
+                        ],
+                    ],
+                ],
+                'dpApi' => true,
+            ]
+        );
+
+        $metadata->mapField([
+            'fieldName'  => 'is_all_brands',
+            'type'       => 'boolean',
+            'precision'  => 0,
+            'scale'      => 0,
+            'nullable'   => false,
+            'columnName' => 'is_all_brands',
         ]);
 
         $metadata->mapField([
--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -919,4 +919,10 @@ return [
         'skipPostBuild' => true,
         'isOnlineBuild' => true,
     ],
+    1523875487 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2018/04/Build1523875487.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1523875487',
+        'skipPostBuild' => true,
+        'isOnlineBuild' => false,
+    ],
 ];
