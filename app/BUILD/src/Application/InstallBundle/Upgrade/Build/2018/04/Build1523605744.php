db1f84324288bd68c1d62e694998d4f1099d65ec
ruslanix <ruslanix.com@gmail.com>
2018-04-16 11:47:43 +0300

    DP-1381 build script fix

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2018/04/Build1523605744.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2018/04/Build1523605744.php
@@ -6,9 +6,9 @@ class Build1523605744 extends AbstractBuild implements OnlineBuildInterface, Ski
 {
     public function addNewTables()
     {
-        $this->execDbQuery('default', 'CREATE TABLE person_to_brand (person_id INT NOT NULL, brand_id INT NOT NULL, INDEX IDX_83286D5F217BBB47 (person_id), INDEX IDX_83286D5F44F5D008 (brand_id), PRIMARY KEY(person_id, brand_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->execDbQuery('default', 'ALTER TABLE person_to_brand ADD CONSTRAINT FK_83286D5F217BBB47 FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE');
-        $this->execDbQuery('default', 'ALTER TABLE person_to_brand ADD CONSTRAINT FK_83286D5F44F5D008 FOREIGN KEY (brand_id) REFERENCES brands (id) ON DELETE CASCADE');
+        //        $this->execDbQuery('default', 'CREATE TABLE person_to_brand (person_id INT NOT NULL, brand_id INT NOT NULL, INDEX IDX_83286D5F217BBB47 (person_id), INDEX IDX_83286D5F44F5D008 (brand_id), PRIMARY KEY(person_id, brand_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+//        $this->execDbQuery('default', 'ALTER TABLE person_to_brand ADD CONSTRAINT FK_83286D5F217BBB47 FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE');
+//        $this->execDbQuery('default', 'ALTER TABLE person_to_brand ADD CONSTRAINT FK_83286D5F44F5D008 FOREIGN KEY (brand_id) REFERENCES brands (id) ON DELETE CASCADE');
     }
 
     public function runAlters()
@@ -19,8 +19,20 @@ class Build1523605744 extends AbstractBuild implements OnlineBuildInterface, Ski
     {
         $this->out('Bind persons to default brand');
 
-        $brands = $this->getDbConnection('default')->fetchAll('SELECT id FROM `brands` ORDER BY id ASC limit 1');
-        $brand  = current($brands);
+        $brand          = null;
+        $defaultBrandId = $this->container
+            ->get('settings_resolver')
+            ->getGlobalSettings()
+            ->get('portal.default_brand');
+        if ($defaultBrandId) {
+            $brands = $this->getDbConnection('default')->fetchAll('SELECT id FROM `brands` WHERE id = ?', [$defaultBrandId]);
+            $brand  = current($brands);
+        }
+
+        if (!$brand) {
+            $brands = $this->getDbConnection('default')->fetchAll('SELECT id FROM `brands` ORDER BY id ASC limit 1');
+            $brand  = current($brands);
+        }
 
         $statement = $this->getDbConnection('default')->prepare('
             INSERT INTO `person_to_brand` (`person_id`, `brand_id`)
