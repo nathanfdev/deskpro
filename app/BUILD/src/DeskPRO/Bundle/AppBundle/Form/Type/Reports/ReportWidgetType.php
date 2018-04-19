72ef2fe20f67fa13bff921124b02922bc98ab7a0
ImmortaL <denis@ranneft.ru>
2018-04-19 11:36:55 +0300

    handle unexisting variables in form

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/Reports/ReportWidgetType.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/Reports/ReportWidgetType.php
@@ -138,7 +138,7 @@ class ReportWidgetType extends AbstractType
                     'property_path'  => 'query',
                     'required'       => true,
                     'error_bubbling' => false,
-                    'variables'      => $data['variables'],
+                    'variables'      => isset($data['variables']) ? $data['variables'] : [],
                 ]);
             }
         }
