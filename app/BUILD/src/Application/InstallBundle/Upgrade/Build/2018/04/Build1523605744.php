7881be3e964361120e6f373cc61cbdef7598f988
ruslanix <ruslanix.com@gmail.com>
2018-04-20 19:36:22 +0300

    DP-1381 build task fix

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2018/04/Build1523605744.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2018/04/Build1523605744.php
@@ -6,9 +6,9 @@ class Build1523605744 extends AbstractBuild implements OnlineBuildInterface, Ski
 {
     public function addNewTables()
     {
-        //        $this->execDbQuery('default', 'CREATE TABLE person_to_brand (person_id INT NOT NULL, brand_id INT NOT NULL, INDEX IDX_83286D5F217BBB47 (person_id), INDEX IDX_83286D5F44F5D008 (brand_id), PRIMARY KEY(person_id, brand_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-//        $this->execDbQuery('default', 'ALTER TABLE person_to_brand ADD CONSTRAINT FK_83286D5F217BBB47 FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE');
-//        $this->execDbQuery('default', 'ALTER TABLE person_to_brand ADD CONSTRAINT FK_83286D5F44F5D008 FOREIGN KEY (brand_id) REFERENCES brands (id) ON DELETE CASCADE');
+        $this->execDbQuery('default', 'CREATE TABLE person_to_brand (person_id INT NOT NULL, brand_id INT NOT NULL, INDEX IDX_83286D5F217BBB47 (person_id), INDEX IDX_83286D5F44F5D008 (brand_id), PRIMARY KEY(person_id, brand_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->execDbQuery('default', 'ALTER TABLE person_to_brand ADD CONSTRAINT FK_83286D5F217BBB47 FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE CASCADE');
+        $this->execDbQuery('default', 'ALTER TABLE person_to_brand ADD CONSTRAINT FK_83286D5F44F5D008 FOREIGN KEY (brand_id) REFERENCES brands (id) ON DELETE CASCADE');
     }
 
     public function runAlters()
