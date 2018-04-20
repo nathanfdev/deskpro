3ee926a072bdf06099fcc6c255a5461e023170c5
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-04-20 09:48:25 +0300

    group by multiple DPQL_HIERARCHY columns fixes

--- app/BUILD/src/DeskPRO/Bundle/ReportBundle/Dpql2/Plugin/Hierarchy/HierarchyDepth.php
+++ app/BUILD/src/DeskPRO/Bundle/ReportBundle/Dpql2/Plugin/Hierarchy/HierarchyDepth.php
@@ -48,7 +48,7 @@ class HierarchyDepth
         });
 
         $groupColumns = $metadata->getGroupYColumns();
-        $groupColumn  = array_shift($groupColumns); // unset first hierarchy one
+        $groupColumn  = $groupColumns[0];
 
         foreach ($results as &$result) {
             // try to merge children
@@ -58,19 +58,107 @@ class HierarchyDepth
         }
 
         $mergeResults = $results;
+
+        // populate root nodes with actual group field values
+        // if we have group columns like IF('', '', '') then we can't get such data for missing root nodes
+        // and we need to iterate the children nodes to get the all possible group values
+
+        // e. g. IF(UNIX_TIMESTAMP(tickets.date_resolved) <= tickets.custom_data[target_complete_date], 'On Time', 'Overdue') AS 'Status'
+
+        // and we got data like:
+
+        //      root node, '-'
+        //         -- child 1, 'On Time'
+        //         -- child 2, 'Overdue'
+
+        // so we replace it to:
+
+        //      root node, 'On Time'
+        //      root node, 'Overdue'
+        //         -- child 1, 'On Time'
+        //         -- child 2, 'Overdue'
+        $uniqueHashes = [];
+        foreach ($results as $i => $result) {
+            if ($result['hierarchy_depth'] > $max) {
+                continue;
+            }
+
+            foreach ($mergeResults as $mergeResult) {
+                if ($mergeResult['hierarchy_depth'] <= $max || $mergeResult['hierarchy_root_title'] !== $result['hierarchy_root_title']) {
+                    continue;
+                }
+
+                $newResult = $result;
+
+                foreach ($groupColumns as $groupColumn) {
+                    $expected = $result[$groupColumn['groupResultId'] - 1];
+                    $actual   = $mergeResult[$groupColumn['groupResultId'] - 1];
+
+                    if ($expected === '-' && $actual !== $expected) {
+                        // replace group column
+                        $newResult[$groupColumn['groupResultId'] - 1] = $actual;
+                    }
+
+                    // replace select column
+                    if (isset($groupColumn['resultId']) && $result[$groupColumn['resultId'] - 1] === '-') {
+                        $newResult[$groupColumn['resultId'] - 1] = $mergeResult[$groupColumn['resultId'] - 1];
+                    }
+                }
+
+                $hash = md5(serialize($newResult));
+                if (!isset($uniqueHashes[$hash])) {
+                    $results[]           = $newResult;
+                    $uniqueHashes[$hash] = true;
+                }
+            }
+        }
+
+        foreach ($results as $i => $result) {
+            if ($result['hierarchy_depth'] > $max) {
+                continue;
+            }
+
+            foreach ($groupColumns as $groupColumn) {
+                if ($result[$groupColumn['groupResultId'] - 1] === '-') {
+                    unset($results[$i]);
+                    break;
+                }
+            }
+        }
+
+        // merge countable select columns
+        // sum countable fields from children nodes, e.g.
+
+        //      root node, 'On Time', 0
+        //      root node, 'Overdue', 0
+        //         -- child 1, 'On Time', 10
+        //         -- child 2, 'On Time', 15
+        //         -- child 3, 'Overdue', 20
+
+        // so we will get:
+
+        //      root node, 'On Time', 25
+        //      root node, 'Overdue', 20
+
+        $groupColumns = $metadata->getGroupYColumns();
+        array_shift($groupColumns); // unshift DPQL_HIERARCHY columns from compare
+
         foreach ($results as &$result) {
             if ($result['hierarchy_depth'] > $max) {
                 continue;
             }
 
-            foreach ($mergeResults as $i => &$mergeResult) {
+            foreach ($mergeResults as $i => $mergeResult) {
                 if ($mergeResult['hierarchy_depth'] <= $max || $mergeResult['hierarchy_root_title'] !== $result['hierarchy_root_title']) {
                     continue;
                 }
 
                 $found = true;
                 foreach ($groupColumns as $groupColumn) {
-                    if ($mergeResult[$groupColumn['groupResultId']] !== $result[$groupColumn['groupResultId']]) {
+                    $expected = $result[$groupColumn['groupResultId'] - 1];
+                    $actual   = $mergeResult[$groupColumn['groupResultId'] - 1];
+
+                    if ($expected !== $actual) {
                         $found = false;
                     }
                 }
@@ -85,6 +173,7 @@ class HierarchyDepth
             }
         }
 
+        // remove children nodes
         $results = array_filter($results, function ($result) use ($max) {
             return $result['hierarchy_depth'] <= $max;
         });
@@ -94,6 +183,6 @@ class HierarchyDepth
             $result['hierarchy_depth'] = max(0, $result['hierarchy_depth'] - $min);
         }
 
-        return $results;
+        return array_values($results);
     }
 }
