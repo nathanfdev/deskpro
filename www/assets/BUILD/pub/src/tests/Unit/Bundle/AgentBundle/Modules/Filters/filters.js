dd7e6553daaad211d85e8362e24e7141828e98b8
Julien Ducro <julien.ducro@deskpro.com>
2018-04-24 15:25:59 +0200

    Fix Jest tests

--- www/assets/BUILD/pub/src/tests/Unit/Bundle/AgentBundle/Modules/Filters/filters.js
+++ www/assets/BUILD/pub/src/tests/Unit/Bundle/AgentBundle/Modules/Filters/filters.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { IntlProvider } from 'react-intl';
 import AgentFilters from 'DeskPRO/Bundle/AgentBundle/Modules/Filters/Components/AgentFilters';
 import { filterSets, filters, stars } from 'DemoState/AgentBundle/Modules/Filters/filters';
 
 describe('>>> Filters --- Snapshot', () => {
-  it('+++capturing Snapshot of Filters', () => {
+  it('+++capturing Snapshot of Filters', (props = { locale: 'en' }) => {
     const renderedValue = renderer.create(
-      <AgentFilters
-        filterSets={filterSets}
-        filters={filters}
-        stars={stars}
-      />
+      <IntlProvider {...props}>
+        <AgentFilters
+          filterSets={filterSets}
+          filters={filters}
+          filtersCounts={[]}
+          labels={[]}
+          stars={stars}
+          starsCounts={[]}
+        />
+      </IntlProvider>
     ).toJSON();
     expect(renderedValue).toMatchSnapshot();
   });
