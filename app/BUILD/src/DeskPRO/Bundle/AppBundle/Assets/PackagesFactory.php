f12360a86e591082623c6016b4bde9226ae029db
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-04-24 17:05:43 +0300

    fix default asset pack url

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Assets/PackagesFactory.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Assets/PackagesFactory.php
@@ -124,14 +124,12 @@ class PackagesFactory
      */
     public function createPackages()
     {
-        $asset_packs = [];
+        $assetPacks = [];
         foreach ($this->asset_paths as $id => $p) {
-            $asset_packs[$id] = $this->getAssetPackage($p);
+            $assetPacks[$id] = $this->getAssetPackage($p);
         }
 
-        $default = $this->getAssetPackage(PathMapInfo::create()->setDeskproPath('/assets/%DP_ACTIVE_BUILD%/web'));
-
-        return new Packages($default, $asset_packs);
+        return new Packages($assetPacks['legacy_web'], $assetPacks);
     }
 
     /**
