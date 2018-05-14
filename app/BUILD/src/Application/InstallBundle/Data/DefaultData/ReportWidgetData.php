fe16c1c9107c625d4c2141ea509838d50eeb3636
Christopher Nadeau <chris.nadeau@deskpro.com>
2018-05-14 15:50:30 +0100

    fixes from notion, and DP-1507, DP-1659

--- app/BUILD/src/Application/InstallBundle/Data/DefaultData/ReportWidgetData.php
+++ app/BUILD/src/Application/InstallBundle/Data/DefaultData/ReportWidgetData.php
@@ -134,25 +134,19 @@ ORDER BY DPQL_COUNT() DESC
             ORDER BY @\'Created Hour\'',
             'variables' => '[{"name":"date","type":"dates","default":"past_24_hours"}]',
         ],
-        'daily-activity' => [
-            'title'         => 'Daily activity ${date}',
+        'agent-replies-by-hour' => [
+            'title'         => 'Agent Replies by Hour',
             'labels'        => 'agents,tickets',
             'description'   => '',
             'display_types' => 'simple_bars',
             'display_order' => 40,
-//            'query'         => 'SELECT SUM(count_open), SUM(count_resolve), hr
-//FROM (
-//	(SELECT DPQL_COUNT() AS count_open, 0 AS count_resolve, DPQL_HOUR(tickets.date_created) AS hr FROM tickets WHERE tickets.date_created = ${date} GROUP BY DPQL_HOUR(tickets.date_created))
-//	UNION
-//	(SELECT 0 AS count_open, DPQL_COUNT()*-1 AS count_resolve, DPQL_HOUR(tickets.date_resolved) AS hr FROM tickets WHERE tickets.date_resolved = ${date} GROUP BY DPQL_HOUR(tickets.date_resolved))
-//) AS dat
-//GROUP BY hr',
-            'query' => 'SELECT DPQL_COUNT() AS \'Replies\', DPQL_HOUR(tickets_messages.date_created) as \'Reply Hour\' 
+            'query'         => 'SELECT DPQL_COUNT() AS \'Replies\' 
             FROM  tickets_messages 
-            WHERE tickets_messages.date_created = ${date}
-            GROUP BY @\'Reply Hour\'
-            ORDER BY @\'Reply Hour\'',
-            'variables' => '[{"name":"date","type":"dates","default":"yesterday"}]',
+            WHERE
+                tickets_messages.date_created = %PAST_24_HOURS%
+                AND tickets_messages.person.is_agent = 1
+            GROUP BY DPQL_HOUR(tickets_messages.date_created, 0, 23) AS \'Hour\'',
+            'variables' => '[]',
         ],
         'incomplete-sla' => [
             'title'         => 'SLA status of non completed SLAs',
@@ -634,7 +628,7 @@ GROUP BY DPQL_MATRIX(${ticket}, ${ticket_2})',
                 'display_types' => 'table,simple_bars,pie,simple_area,simple_lines',
                 'display_order' => 20,
                 'query'         => '
-SELECT DPQL_COUNT() AS \'Total Tickets\'
+SELECT DPQL_COUNT() AS \'Tickets\'
 FROM tickets
 WHERE ${status}
 GROUP BY DPQL_MATRIX(${ticket}, ${ticket_2})',
--- app/BUILD/src/DeskPRO/Bundle/ReportBundle/Dpql2/Func/DpqlHour.php
+++ app/BUILD/src/DeskPRO/Bundle/ReportBundle/Dpql2/Func/DpqlHour.php
@@ -4,8 +4,12 @@ namespace DeskPRO\Bundle\ReportBundle\Dpql2\Func;
 
 use DeskPRO\Bundle\ReportBundle\Dpql2\DpqlException;
 use DeskPRO\Bundle\ReportBundle\Dpql2\SqlSelect;
+use DeskPRO\Bundle\ReportBundle\Dpql2\Statement\Part\Number;
 use DeskPRO\Bundle\ReportBundle\Dpql2\Statement\Part\Prepared;
+use DeskPRO\Bundle\ReportBundle\Dpql2\Statement\Part\StringPart;
 use DeskPRO\Bundle\ReportBundle\Dpql2\Statement\SelectPart;
+use DeskPRO\Bundle\ReportBundle\Reports\Renderer\AbstractRenderer;
+use DeskPRO\Bundle\ReportBundle\Reports\Renderer\AbstractValueRenderer;
 use DeskPRO\Bundle\ReportBundle\Reports\ResultMetadata;
 
 /**
@@ -18,23 +22,50 @@ class DpqlHour extends AbstractDpqlFunc
      */
     public function prepare(array $arguments, SelectPart $statement, $section, array $stack, SqlSelect $select, ResultMetadata $metadata)
     {
-        if (count($arguments) != 1) {
-            throw new DpqlException('DPQL_HOUR() can only accept 1 argument.');
+        if (count($arguments) > 3) {
+            throw new DpqlException('DPQL_HOUR() can only accept 1, 2 or 3 arguments.');
         }
 
         $expression = reset($arguments);
         $prepped    = $expression->prepare($statement, $section, $stack, $select, $metadata);
 
-        $sql = 'HOUR('.$prepped->sql().')';
-        $res = new Prepared($sql, 'DPQL_HOUR('.$prepped->name().')', false, 'numberraw');
+        $tzOffsetSeconds = $statement->getTimezoneOffsetForFunction($stack);
+        $sql             = "HOUR({$prepped->sql()} + INTERVAL $tzOffsetSeconds SECOND)";
 
-        $res->setGroupFill(function ($min, $max) {
-            if ($min == $max) {
-                return [];
+        $renderer = function (AbstractValueRenderer $valueRenderer, $value, array $row, AbstractRenderer $renderer) {
+            $ampm = $value >= 12 ? 'pm' : 'am';
+            $hour = $value % 12 ?: 12;
+
+            return "{$hour}{$ampm}";
+        };
+
+        $res = new Prepared($sql, 'DPQL_HOUR('.$prepped->name().')', false, $renderer);
+
+        $userMin = $userMax = null;
+        if (isset($arguments[1]) && ($arguments[1] instanceof Number || $arguments[1] instanceof StringPart)) {
+            $userMin = $arguments[1]->getValue();
+        }
+        if (isset($arguments[2]) && ($arguments[2] instanceof Number || $arguments[2] instanceof StringPart)) {
+            $userMax = $arguments[2]->getValue();
+        }
+
+        $res->setGroupFill(function ($min, $max) use ($userMin, $userMax) {
+            if (!$min) {
+                $min = 0;
+            }
+            if (!$max) {
+                $max = 23;
+            }
+
+            if ($userMin !== null) {
+                $min = $userMin;
+            }
+            if ($userMax !== null) {
+                $max = $userMax;
             }
 
             $fills = [];
-            for ($i = $min; $i <= $max; ++$i) {
+            for ($i = $min; $i < $max; ++$i) {
                 $fills[] = [$i, $i, $i];
             }
 
