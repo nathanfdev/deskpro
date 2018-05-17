eadd823e35810786c7d68454e841a851980319cb
Christopher Nadeau <chris.nadeau@deskpro.com>
2018-05-17 14:28:49 +0100

    Admin > Setup -- custom hostname provisioning

--- app/BUILD/src/Cloud/LegacyApiBundle/Helper/CloudBrandHelper.php
+++ app/BUILD/src/Cloud/LegacyApiBundle/Helper/CloudBrandHelper.php
@@ -45,15 +45,57 @@ class CloudBrandHelper
 
         $url = DP_MA_SERVER_SECURE.'/cloud/call/'.DPC_SITE_ID.'/'.$tmpdata->getCode();
 
-        \DpShutdown::add(function () use ($url) {
-            try {
-                $client = new \Zend\Http\Client(null, ['timeout' => 15, 'sslverifypeer' => false]);
-                $client->setMethod(\Zend\Http\Request::METHOD_GET);
-                $client->setUri($url);
-                $client->send();
-            } catch (\Exception $e) {
-                SystemErrorHandler::logException($e);
-            }
-        });
+        try {
+            $client = new \Zend\Http\Client(null, ['timeout' => 15, 'sslverifypeer' => false]);
+            $client->setMethod(\Zend\Http\Request::METHOD_GET);
+            $client->setUri($url);
+            $client->send();
+        } catch (\Exception $e) {
+            SystemErrorHandler::logException($e);
+        }
+    }
+
+    /**
+     * This requests a custom domain to get set up.
+     *
+     * @param $domain
+     *
+     * @return array
+     */
+    public static function provisionCustomDomain($domain)
+    {
+        $em = App::$container->getEm();
+
+        $tmpdata = new TmpData();
+        $tmpdata->setType('dpc_provision_custom_domain');
+        $tmpdata->setData('domain', $domain);
+        $tmpdata->date_expire = new \DateTime('+10 minutes');
+
+        $em->persist($tmpdata);
+        $em->flush();
+
+        $url = DP_MA_SERVER_SECURE.'/cloud/call/'.DPC_SITE_ID.'/'.$tmpdata->getCode();
+
+        try {
+            $client = new \Zend\Http\Client(null, ['timeout' => 15, 'sslverifypeer' => false]);
+            $client->setMethod(\Zend\Http\Request::METHOD_GET);
+            $client->setUri($url);
+            $client->send();
+
+            $response = $client->send();
+            $data     = json_decode($response->getBody(), true);
+
+            return $data;
+        } catch (\Exception $e) {
+            SystemErrorHandler::logException($e);
+
+            return [
+                'error'            => true,
+                'type'             => 'server_error',
+                'code'             => 'server_error',
+                'message'          => 'An error occurred while setting up your custom domain. Please try again later.',
+                'internal_message' => $e->getMessage(),
+            ];
+        }
     }
 }
