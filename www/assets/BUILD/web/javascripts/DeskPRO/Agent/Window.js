f3f8619284ef85b2b0745685a8a6f74a4a5ad373
Christopher Nadeau <chris.nadeau@deskpro.com>
2018-05-24 16:04:39 +0100

    Tweak loading order a bit so interface shows faster

--- www/assets/BUILD/web/javascripts/DeskPRO/Agent/Window.js
+++ www/assets/BUILD/web/javascripts/DeskPRO/Agent/Window.js
@@ -990,6 +990,11 @@ DeskPRO.Agent.Window = new Orb.Class({
 		// actually clicking stuff will instant
     DeskPRO_Window.TabBar.enableInitOnRender();
     window.DP_PAGE_PART_HAS_INIT('DeskPRO_Window');
+    window.DP_ADD_PAGE_INIT_FN(function() {
+    	window.setTimeout(function() {
+        self.getSectionDataSendQueued();
+			}, 500);
+		});
 	},
 
 	initScope: function() {
@@ -3041,8 +3046,6 @@ DeskPRO.Agent.Window = new Orb.Class({
 				el.on('click', function() { self.switchToSection(el.attr('id')) });
 			}
 		});
-
-		this.getSectionDataSendQueued();
 	},
 
 	switchToSection: function(section_id, no_load_list) {
@@ -3433,7 +3436,7 @@ DeskPRO.Agent.Window = new Orb.Class({
 		this.loadingSections[section_id] = true;
 
 		// If we're in queued mode, then dont send anything yet
-		if (this._getSectionDataQueued) {
+		if (this._getSectionDataQueued && section_id != 'tickets_section') {
 			this._getSectionDataQueued.push([section_id, callback]);
 			return;
 		}
@@ -3495,7 +3498,13 @@ DeskPRO.Agent.Window = new Orb.Class({
 					return;
 				}
 
-				callback(data);
+        if (window.requestAnimationFrame) {
+          window.requestAnimationFrame(function() {
+            callback(data);
+          });
+        } else {
+          callback(data);
+        }
 			},
 			tryCount : 0,
 		    retryLimit: 3,
@@ -3567,7 +3576,13 @@ DeskPRO.Agent.Window = new Orb.Class({
 						self.getSectionData(sectionId);
 					}
 					if (callback_map[sectionId]) {
-						callback_map[sectionId](sectionData);
+						if (window.requestAnimationFrame) {
+              window.requestAnimationFrame(function() {
+                callback_map[sectionId](sectionData);
+							});
+						} else {
+              callback_map[sectionId](sectionData);
+            }
 					}
 				});
 			}
