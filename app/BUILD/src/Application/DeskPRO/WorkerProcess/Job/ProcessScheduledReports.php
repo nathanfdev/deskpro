15d69701d580b8c7578bdee8b189605289138e54
ImmortaL <denis@ranneft.ru>
2018-05-28 14:00:51 +0300

    try to find a user or set up email as user name (try to find name from email)

--- app/BUILD/src/Application/DeskPRO/WorkerProcess/Job/ProcessScheduledReports.php
+++ app/BUILD/src/Application/DeskPRO/WorkerProcess/Job/ProcessScheduledReports.php
@@ -7,7 +7,9 @@
 namespace Application\DeskPRO\WorkerProcess\Job;
 
 use Application\DeskPRO\Entity\SavedDashboardReport;
+use Application\DeskPRO\EntityRepository\Person as PersonRepository;
 use DeskPRO\Bundle\AppBundle\Entity\Report\ScheduledReport;
+use Orb\Util\Strings;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
@@ -68,24 +70,31 @@ class ProcessScheduledReports extends AbstractJob
     protected function sendProcessedReport(ScheduledReport $scheduledReport, SavedDashboardReport $savedReport)
     {
         $message = $this->getContainer()->getMailer()->createMessage();
-        $message->setTemplate(
-            'DeskPRO:emails_user:scheduled-report.html.twig',
-            [
-                'person'      => $scheduledReport->getPerson(),
-                'frequency'   => $scheduledReport->getFrequency(),
-                'reportTitle' => $scheduledReport->getReport()->getTitle(),
-                'link'        => $this->getContainer()->get('router')->generate(
-                    'reports-interface-headless-view',
-                    [
-                        'id'       => $savedReport->getId(),
-                        'authcode' => $savedReport->getAuthcode(),
-                    ],
-                    UrlGeneratorInterface::ABSOLUTE_URL
-                ),
-            ]
-        );
+
+        /** @var PersonRepository $personRepository */
+        $personRepository = $this->getContainer()->get('doctrine.orm.default_entity_manager');
 
         foreach ($scheduledReport->getSendTo() as $to) {
+            $person = $personRepository->findOneByEmail($to);
+
+            $message->setTemplate(
+                'DeskPRO:emails_user:scheduled-report.html.twig',
+                [
+                    'person_title' => $person ? $person->getDisplayName() : Strings::getNameFromEmail($to),
+                    'scheduler'    => $scheduledReport->getPerson(),
+                    'frequency'    => $scheduledReport->getFrequency(),
+                    'reportTitle'  => $scheduledReport->getReport()->getTitle(),
+                    'link'         => $this->getContainer()->get('router')->generate(
+                        'reports-interface-headless-view',
+                        [
+                            'id'       => $savedReport->getId(),
+                            'authcode' => $savedReport->getAuthcode(),
+                        ],
+                        UrlGeneratorInterface::ABSOLUTE_URL
+                    ),
+                ]
+            );
+
             $message->setTo($to);
             $this->getContainer()->getMailer()->send($message);
         }
