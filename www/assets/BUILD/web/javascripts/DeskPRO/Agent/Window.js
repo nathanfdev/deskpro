65a4fa8b1a0a12a06fc56f5c898fe468e1134365
ruslanix <ruslanix.com@gmail.com>
2018-05-30 16:24:10 +0300

    DP-1498 Fix mentions

--- www/assets/BUILD/web/javascripts/DeskPRO/Agent/Window.js
+++ www/assets/BUILD/web/javascripts/DeskPRO/Agent/Window.js
@@ -3685,254 +3685,259 @@ DeskPRO.Agent.Window = new Orb.Class({
 		return DeskPRO.Agent.RteEditor.initRteAgentReply(textarea, options);
 	},
 
-	initAgentNotifierForRte: function(obj, textarea, alwaysAvailable) {
-		var api = textarea.data('redactor');
-		if (!api) {
-			return;
-		}
+  initAgentNotifierForRte: function(obj, textarea, alwaysAvailable) {
+    var self = this;
 
-		var ed = textarea.getEditor();
-		var self = this;
+    // Notifier depends from window.notifyAgentMap initialized during bootstrapActions
+    // add below function to waiting pull
+    window.DP_ADD_PAGE_INIT_FN(function() {
+      var api = textarea.data('redactor');
+      if (!api) {
+        return;
+      }
 
-		var agentMapLower = {}, hasAgents = false;
-		var notifyAgentMap = window.notifyAgentMap || [];
-		Object.each(notifyAgentMap, function(data, agentId) {
-			hasAgents = true;
-			agentMapLower[agentId] = data.name.toLowerCase();
+      var ed = textarea.getEditor();
+
+      var agentMapLower = {}, hasAgents = false;
+      var notifyAgentMap = window.notifyAgentMap || [];
+      Object.each(notifyAgentMap, function(data, agentId) {
+        hasAgents = true;
+        agentMapLower[agentId] = data.name.toLowerCase();
+      });
+
+      if (!hasAgents) {
+        return;
+      }
+
+      obj.agentNotifyList = $('<ul />').addClass('message-agent-notify-list').hide().appendTo(document.body);
+
+      var insertAgentNotify = function(agentId) {
+        if (typeof notifyAgentMap[agentId] === 'undefined') {
+          return;
+        }
+
+        self.hideAgentNotifyList(obj);
+
+        var focus = api.getFocus(),
+          focusNode = $(focus[0]),
+          testText;
+
+        if (!focus || !focus[0]) {
+          return;
+        }
+
+        if (focus[0].nodeType == 3) {
+          testText = focusNode.text().substring(0, focus[1]);
+        } else {
+          focus[0] = focusNode.contents().get(focus[1] - 1);
+          focusNode = $(focus[0]);
+          testText = focusNode.text();
+          focus[1] = testText.length;
+        }
+
+        var	lastAt = testText.lastIndexOf('@');
+
+        if (lastAt != -1) {
+          api.setSelection(focus[0], lastAt, focus[0], focus[1]);
+        }
+
+        // web kit handles content editable without an issue. this prevents the span
+        // from being extended unnecessarily
+        var editable = $.browser.webkit ? ' contenteditable="false"' : '';
+        api.insertHtml('<span' + editable + ' data-notify-agent-id="' + agentId + '">@' + Orb.escapeHtml(notifyAgentMap[agentId].name) + '</span>&nbsp;');
+      };
+
+      obj.agentNotifyList.on('mousedown', 'li', function(e) {
+        e.preventDefault();
+        insertAgentNotify($(this).data('agent-id'));
+      });
+
+      ed.on('click blur', function() {
+        if (obj.isNote || alwaysAvailable) {
+          self.hideAgentNotifyList(obj);
+        }
+      });
+
+      ed.on('keydown', function(e) {
+        if (!obj.isNote && !alwaysAvailable) {
+          self.hideAgentNotifyList(obj);
+          return;
+        }
+
+        switch (e.keyCode) {
+          case 38: // up
+          case 40: // down
+          case 13: // enter
+            if (!obj.agentNotifyList.is(':visible')) {
+              return;
+            }
+            break;
+
+          default:
+            return;
+        }
+
+        e.preventDefault();
+
+        if (e.keyCode == 13) { // enter - inserting the selected
+          var li = obj.agentNotifyList.find('li.selected');
+          if (!li.length) {
+            li = obj.agentNotifyList.find('li:first');
+          }
+
+          insertAgentNotify(li.data('agent-id'));
+        } else if (e.keyCode == 40) { // down - moves down the list
+          var li = obj.agentNotifyList.find('li.selected');
+          if (!li.length) {
+            obj.agentNotifyList.find('li:first').addClass('selected');
+          } else {
+            li.removeClass('selected');
+            var next = li.next('li');
+            if (next.length) {
+              next.addClass('selected');
+            } else {
+              obj.agentNotifyList.find('li:first').addClass('selected');
+            }
+          }
+        } else if (e.keyCode == 38) { // up - moves up the list
+          var li = obj.agentNotifyList.find('li.selected');
+          if (!li.length) {
+            obj.agentNotifyList.find('li:last').addClass('selected');
+          } else {
+            li.removeClass('selected');
+            var prev = li.prev('li');
+            if (prev.length) {
+              prev.addClass('selected');
+            } else {
+              obj.agentNotifyList.find('li:last').addClass('selected');
+            }
+          }
+        }
+      });
+
+      ed.on('keyup', function(e) {
+        if (!obj.isNote && !alwaysAvailable) {
+          return;
+        }
+
+        if (e.ctrlKey || e.metaKey) {
+          return;
+        }
+
+        switch (e.keyCode) {
+          case 16: // shift
+          case 17: // ctrl
+          case 18: // alt
+          case 19: // pause/break
+          case 20: // caps lock
+          case 91: // left windows
+          case 92: // right windows
+          case 93: // select
+          case 224: // apple key
+            return;
+
+          case 13: // enter
+          case 38: // up
+          case 40: // down
+            // these don't hide as that messes up the keydown handler
+            e.stopImmediatePropagation();
+            e.preventDefault();
+            return;
+
+          case 9: // tab
+          case 27: // esc
+          case 33: // page up
+          case 34: // page down
+          case 35: // end
+          case 36: // home
+          case 37: // left
+          case 39: // right
+            self.hideAgentNotifyList(obj);
+            return;
+
+          default:
+            // function keys and other special ones
+            if (e.keyCode >= 112 && e.keyCode <= 145) {
+              self.hideAgentNotifyList(obj);
+              return;
+            }
+        }
+
+        var focus = api.getFocus(),
+          origin = api.getOrigin(),
+          selection = api.getSelection();
+
+        if (focus[0] != origin[0] || focus[1] != origin[1]) {
+          // selected multiple points, don't show
+          self.hideAgentNotifyList(obj);
+          return;
+        }
+
+        var	focusNode = $(focus[0]),
+          testText = focus[0].nodeType == 3 ? focusNode.text().substring(0, focus[1]) : $(focusNode.contents().get(focus[1] - 1)).text(),
+          lastAt = testText.lastIndexOf('@'),
+          matches = [];
+
+        if (lastAt != -1 && (lastAt == 0 || testText[lastAt - 1].match(/^(\s|[\.!?:;,()<>|/-])$/))) {
+          var afterAt = testText.substring(lastAt + 1, testText.length).toLowerCase();
+
+          if (afterAt.length >= 2 && afterAt.length < 75) {
+            Object.each(notifyAgentMap, function(data, agentId) {
+              if (agentMapLower[agentId].indexOf(afterAt) == 0) {
+                matches.push(agentId);
+              }
+            });
+          }
+        }
+
+        if (matches.length) {
+          var selectedId = obj.agentNotifyList.find('li:selected').data('agent-id');
+
+          obj.agentNotifyList.empty();
+          for (var i = 0; i < matches.length; i++) {
+            var li = $('<li>')
+              .text(notifyAgentMap[matches[i]].name)
+              .css('background-image', 'url('+notifyAgentMap[matches[i]].picture_url+')')
+              .data('agent-id', matches[i]);
+            if (matches[i] === selectedId) {
+              li.addClass('selected');
+            }
+            obj.agentNotifyList.append(li);
+          }
+
+          if (!obj.agentNotifyList.find('li:selected').length) {
+            obj.agentNotifyList.find('li:first').addClass('selected');
+          }
+
+          var containingNode = focus[0].nodeType == 3 ? focusNode.parent() : focusNode;
+          if (!containingNode.is('div, p, li, ul, ol, blockquote, table, body')) {
+            containingNode = containingNode.closest('div, p, li, ul, ol, blockquote, table, body');
+          }
+          var offset = containingNode.offset();
+
+          if (selection) {
+            var selOffset = Orb.getSelectionCoords(selection);
+            if (selOffset) {
+              offset = selOffset;
+            }
+          }
+
+          obj.agentNotifyList.css({
+            top: offset.top - obj.agentNotifyList.outerHeight() - 1,
+            left: offset.left
+          });
+
+          obj.agentNotifyList.show();
+          obj.agentNotifyListShown = true;
+        } else {
+          self.hideAgentNotifyList(obj);
+        }
+      });
+
+      // this is important as I need this keyup handler to run before redactor's own because of new line handling
+      ed.data('events').keyup.reverse();
 		});
-
-		if (!hasAgents) {
-			return;
-		}
-
-		obj.agentNotifyList = $('<ul />').addClass('message-agent-notify-list').hide().appendTo(document.body);
-
-		var insertAgentNotify = function(agentId) {
-			if (typeof notifyAgentMap[agentId] === 'undefined') {
-				return;
-			}
-
-			self.hideAgentNotifyList(obj);
-
-			var focus = api.getFocus(),
-				focusNode = $(focus[0]),
-				testText;
-
-			if (!focus || !focus[0]) {
-				return;
-			}
-
-			if (focus[0].nodeType == 3) {
-				testText = focusNode.text().substring(0, focus[1]);
-			} else {
-				focus[0] = focusNode.contents().get(focus[1] - 1);
-				focusNode = $(focus[0]);
-				testText = focusNode.text();
-				focus[1] = testText.length;
-			}
-
-			var	lastAt = testText.lastIndexOf('@');
-
-			if (lastAt != -1) {
-				api.setSelection(focus[0], lastAt, focus[0], focus[1]);
-			}
-
-			// web kit handles content editable without an issue. this prevents the span
-			// from being extended unnecessarily
-			var editable = $.browser.webkit ? ' contenteditable="false"' : '';
-			api.insertHtml('<span' + editable + ' data-notify-agent-id="' + agentId + '">@' + Orb.escapeHtml(notifyAgentMap[agentId].name) + '</span>&nbsp;');
-		};
-
-		obj.agentNotifyList.on('mousedown', 'li', function(e) {
-			e.preventDefault();
-			insertAgentNotify($(this).data('agent-id'));
-		});
-
-		ed.on('click blur', function() {
-			if (obj.isNote || alwaysAvailable) {
-				self.hideAgentNotifyList(obj);
-			}
-		});
-
-		ed.on('keydown', function(e) {
-			if (!obj.isNote && !alwaysAvailable) {
-				self.hideAgentNotifyList(obj);
-				return;
-			}
-
-			switch (e.keyCode) {
-				case 38: // up
-				case 40: // down
-				case 13: // enter
-					if (!obj.agentNotifyList.is(':visible')) {
-						return;
-					}
-					break;
-
-				default:
-					return;
-			}
-
-			e.preventDefault();
-
-			if (e.keyCode == 13) { // enter - inserting the selected
-				var li = obj.agentNotifyList.find('li.selected');
-				if (!li.length) {
-					li = obj.agentNotifyList.find('li:first');
-				}
-
-				insertAgentNotify(li.data('agent-id'));
-			} else if (e.keyCode == 40) { // down - moves down the list
-				var li = obj.agentNotifyList.find('li.selected');
-				if (!li.length) {
-					obj.agentNotifyList.find('li:first').addClass('selected');
-				} else {
-					li.removeClass('selected');
-					var next = li.next('li');
-					if (next.length) {
-						next.addClass('selected');
-					} else {
-						obj.agentNotifyList.find('li:first').addClass('selected');
-					}
-				}
-			} else if (e.keyCode == 38) { // up - moves up the list
-				var li = obj.agentNotifyList.find('li.selected');
-				if (!li.length) {
-					obj.agentNotifyList.find('li:last').addClass('selected');
-				} else {
-					li.removeClass('selected');
-					var prev = li.prev('li');
-					if (prev.length) {
-						prev.addClass('selected');
-					} else {
-						obj.agentNotifyList.find('li:last').addClass('selected');
-					}
-				}
-			}
-		});
-
-		ed.on('keyup', function(e) {
-			if (!obj.isNote && !alwaysAvailable) {
-				return;
-			}
-
-			if (e.ctrlKey || e.metaKey) {
-				return;
-			}
-
-			switch (e.keyCode) {
-				case 16: // shift
-				case 17: // ctrl
-				case 18: // alt
-				case 19: // pause/break
-				case 20: // caps lock
-				case 91: // left windows
-				case 92: // right windows
-				case 93: // select
-				case 224: // apple key
-					return;
-
-				case 13: // enter
-				case 38: // up
-				case 40: // down
-					// these don't hide as that messes up the keydown handler
-					e.stopImmediatePropagation();
-					e.preventDefault();
-					return;
-
-				case 9: // tab
-				case 27: // esc
-				case 33: // page up
-				case 34: // page down
-				case 35: // end
-				case 36: // home
-				case 37: // left
-				case 39: // right
-					self.hideAgentNotifyList(obj);
-					return;
-
-				default:
-					// function keys and other special ones
-					if (e.keyCode >= 112 && e.keyCode <= 145) {
-						self.hideAgentNotifyList(obj);
-						return;
-					}
-			}
-
-			var focus = api.getFocus(),
-				origin = api.getOrigin(),
-				selection = api.getSelection();
-
-			if (focus[0] != origin[0] || focus[1] != origin[1]) {
-				// selected multiple points, don't show
-				self.hideAgentNotifyList(obj);
-				return;
-			}
-
-			var	focusNode = $(focus[0]),
-				testText = focus[0].nodeType == 3 ? focusNode.text().substring(0, focus[1]) : $(focusNode.contents().get(focus[1] - 1)).text(),
-				lastAt = testText.lastIndexOf('@'),
-				matches = [];
-
-			if (lastAt != -1 && (lastAt == 0 || testText[lastAt - 1].match(/^(\s|[\.!?:;,()<>|/-])$/))) {
-				var afterAt = testText.substring(lastAt + 1, testText.length).toLowerCase();
-
-				if (afterAt.length >= 2 && afterAt.length < 75) {
-					Object.each(notifyAgentMap, function(data, agentId) {
-						if (agentMapLower[agentId].indexOf(afterAt) == 0) {
-							matches.push(agentId);
-						}
-					});
-				}
-			}
-
-			if (matches.length) {
-				var selectedId = obj.agentNotifyList.find('li:selected').data('agent-id');
-
-				obj.agentNotifyList.empty();
-				for (var i = 0; i < matches.length; i++) {
-					var li = $('<li>')
-						.text(notifyAgentMap[matches[i]].name)
-						.css('background-image', 'url('+notifyAgentMap[matches[i]].picture_url+')')
-						.data('agent-id', matches[i]);
-					if (matches[i] === selectedId) {
-						li.addClass('selected');
-					}
-					obj.agentNotifyList.append(li);
-				}
-
-				if (!obj.agentNotifyList.find('li:selected').length) {
-					obj.agentNotifyList.find('li:first').addClass('selected');
-				}
-
-				var containingNode = focus[0].nodeType == 3 ? focusNode.parent() : focusNode;
-				if (!containingNode.is('div, p, li, ul, ol, blockquote, table, body')) {
-					containingNode = containingNode.closest('div, p, li, ul, ol, blockquote, table, body');
-				}
-				var offset = containingNode.offset();
-
-				if (selection) {
-					var selOffset = Orb.getSelectionCoords(selection);
-					if (selOffset) {
-						offset = selOffset;
-					}
-				}
-
-				obj.agentNotifyList.css({
-					top: offset.top - obj.agentNotifyList.outerHeight() - 1,
-					left: offset.left
-				});
-
-				obj.agentNotifyList.show();
-				obj.agentNotifyListShown = true;
-			} else {
-				self.hideAgentNotifyList(obj);
-			}
-		});
-
-		// this is important as I need this keyup handler to run before redactor's own because of new line handling
-		ed.data('events').keyup.reverse();
-	},
+  },
 
 	hideAgentNotifyList: function(obj) {
 		if (obj.agentNotifyList && obj.agentNotifyListShown) {
