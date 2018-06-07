77a4bdbb7c35aecc7422d1edbcfaa17765e56bd9
Julien Ducro <julien.ducro@deskpro.com>
2018-06-07 13:17:51 +0200

    DP-1837 - Fix footer

--- app/BUILD/apps/deskpro_zapier/js/AdminInterface/Install/SettingsCtrl.js
+++ app/BUILD/apps/deskpro_zapier/js/AdminInterface/Install/SettingsCtrl.js
@@ -1,3 +1,5 @@
 define(['DeskPRO/Util/Strings'], function(Strings) {
-	return ['$scope', 'Api', '$q', '$modal', function($scope, Api, $q, $modal) {}];
+	return ['$scope', 'Api', '$q', '$modal', function($scope, Api, $q, $modal) {
+    $scope.enableCustomFooter();
+	}];
 });
