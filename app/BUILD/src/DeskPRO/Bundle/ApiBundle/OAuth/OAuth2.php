139a9868cb511824a93ea6bab4c0a987b3e13a29
ruslanix <ruslanix.com@gmail.com>
2018-06-15 19:49:34 +0300

    DP-1883 Api OAuth Bug

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/OAuth/OAuth2.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/OAuth/OAuth2.php
@@ -2,6 +2,7 @@
 
 namespace DeskPRO\Bundle\ApiBundle\OAuth;
 
+use Application\DeskPRO\Entity\ApiToken;
 use Doctrine\ORM\EntityManager;
 use OAuth2\Model\IOAuth2Client;
 use OAuth2\OAuth2 as BaseOAuth2;
@@ -25,24 +26,32 @@ class OAuth2 extends BaseOAuth2
     }
 
     /**
-     * Return AccessToken in the format of Deskpro API token.
+     * {@inheritdoc}
+     */
+    protected function genAccessToken()
+    {
+        $token = new ApiToken();
+
+        return $token->getToken();
+    }
+
+    /**
+     * Return AccessToken in the format of Deskpro API token id:tokenstring.
      *
      * {@inheritdoc}
      */
     public function createAccessToken(IOAuth2Client $client, $data, $scope = null, $access_token_lifetime = null, $issue_refresh_token = true, $refresh_token_lifetime = null)
     {
-        $token = parent::createAccessToken($client, $data, $scope, $access_token_lifetime, $issue_refresh_token, $refresh_token_lifetime);
-
-        // length of api_token.token field
-        $accessToken = substr($token['access_token'], 0, 25);
+        $tokenData   = parent::createAccessToken($client, $data, $scope, $access_token_lifetime, $issue_refresh_token, $refresh_token_lifetime);
+        $accessToken = $tokenData['access_token'];
 
         $apiTokenEntity = $this->em->getRepository('DeskPRO:ApiToken')->findOneByToken($accessToken);
         if (!$apiTokenEntity) {
             throw new \Exception(sprintf('[OAuth2] Can\'t find ApiToken by token `%s`', $accessToken));
         }
 
-        $token['access_token'] = sprintf('%s:%s', $apiTokenEntity->getId(), $accessToken);
+        $tokenData['access_token'] = sprintf('%s:%s', $apiTokenEntity->getId(), $accessToken);
 
-        return $token;
+        return $tokenData;
     }
 }
