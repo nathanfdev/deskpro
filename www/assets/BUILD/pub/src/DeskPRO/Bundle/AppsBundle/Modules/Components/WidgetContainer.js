9e3cfc17f267c672e2ca48a673be4b446bc84555
radu <radu.helstern@deskpro.com>
2018-06-20 14:44:40 +0100

    first steps in refactoring the DeskproAppContainer isolate post-robot details in a WidgetContainer which wraps the widget iframe

---
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AppsBundle/Modules/Components/WidgetContainer.js
@@ -0,0 +1,116 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import * as postRobot from 'post-robot';
+
+import { WidgetConfiguration } from '../Domain';
+import { WidgetIframe } from './WidgetIframe';
+
+export class WidgetContainer extends React.PureComponent {
+  static propTypes = {
+    configuration:     PropTypes.instanceOf(WidgetConfiguration),
+    getEvent:          PropTypes.func,
+    getEventProviders: PropTypes.func,
+    unregister:        PropTypes.func
+  };
+
+  componentDidUpdate(prevProps)  { // eslint-disable-line no-unused-vars
+    const { getEvent }      = this.props;
+    const { name, message } = getEvent(this.props.configuration);
+
+    if (name && message && this.window) {
+      postRobot.send(this.window, name, message.toJS());
+    }
+
+    if (name && message && !this.window) {
+      console.log('message arrived before window is ready');
+    }
+  }
+
+  componentWillUnmount()  {
+    this.listeners.forEach(listener => listener.cancel());
+    this.listeners = [];
+    this.props.unregister(this.props.configuration);
+  }
+
+  onWindowReady = (wnd) =>  {
+    this.window = wnd;
+    this.releaseAll();
+    this.registerAll();
+  };
+
+  onReady = (event) => {
+    const { onReady } = this.props.getEventProviders();
+    return this.handle(onReady, event);
+  };
+
+
+  onEvent = (event) => {
+    const { onEvent } = this.props.getEventProviders();
+    return this.handle(onEvent, event);
+  };
+
+  releaseAll()  {
+    for (const listener of this.listeners) {
+      listener.cancel();
+    }
+    this.listeners = [];
+  }
+
+  registerAll()  {
+    const { /** @type {function} */ getEventProviders } = this.props;
+
+    const { onReady, onEvent } = getEventProviders();
+    this.register(onReady, this.onReady);
+    this.register(onEvent, this.onEvent);
+  }
+
+  /**
+   * @param {EventProvider} eventProvider
+   * @param {function} handler
+   */
+  register(eventProvider, handler)  {
+    const { /** @type {WidgetConfiguration} */ configuration } = this.props;
+    const eventName = eventProvider.getName(configuration);
+    const options = this.window ? { window: this.window } :  {};
+
+    const listener = postRobot.on(eventName, options, handler);
+    this.listeners.push(listener);
+  }
+
+  /**
+   * @param {EventProvider} eventProvider
+   * @param {*} event
+   */
+  handle(eventProvider, event)  {
+    const { /** @type {WidgetConfiguration} */ configuration } = this.props;
+    const handler = eventProvider.getHandler(configuration);
+    return handler(configuration, event);
+  }
+
+  preRender()  {
+    if (!this.registerAllRan) {
+      this.registerAllRan = true;
+      this.registerAll();
+    }
+  }
+
+  window = null;
+
+  listeners = [];
+
+  registerAllRan = false;
+
+  render()  {
+    this.preRender();
+
+    const { /** @type {WidgetConfiguration} */ configuration } = this.props;
+    return (<WidgetIframe
+      id={configuration.canonicId}
+      url={configuration.getUrl()}
+      onWindowReady={this.onWindowReady}
+
+    />);
+  }
+
+}
--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AppsBundle/Modules/Components/WidgetIframe.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AppsBundle/Modules/Components/WidgetIframe.js
@@ -3,27 +3,27 @@ import React from 'react';
 
 class WidgetIframe extends React.PureComponent {
   static propTypes = {
-    url: PropTypes.string.isRequired,
-    id:  PropTypes.string.isRequired
+    url:           PropTypes.string.isRequired,
+    id:            PropTypes.string.isRequired,
+    onWindowReady: PropTypes.func
   };
 
-  /**
-   * @param {WidgetConfiguration} config
-   * @return {*}
-   */
-  static fromConfiguration(config)  {
-    return (<WidgetIframe id={config.canonicId} url={config.getUrl()} />);
-  }
+  onWindowReady = (iframe) => {
+    if (iframe) {
+      this.props.onWindowReady(iframe.contentWindow);
+    } else {
+      this.props.onWindowReady(null);
+    }
+  };
 
   render()  {
-    const { url, id } = this.props;
-
     return (
-      <div id={id}>
+      <div id={this.props.id}>
         <iframe
+          ref={this.onWindowReady}
           scrolling={'no'}
           frameBorder={'0'}
-          src={url}
+          src={this.props.url}
           style={{ width: '100%', height: '100%', backgroundColor: 'transparent' }}
         />
       </div>
