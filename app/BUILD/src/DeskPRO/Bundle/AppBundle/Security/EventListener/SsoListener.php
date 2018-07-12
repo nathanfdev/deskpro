7908254035ffec12d21388321d155ecd9464bfa6
Sergey Yastrebov <serg.yastrebov@gmail.com>
2018-07-12 23:48:35 +0300

    redirect to last page with automatic sso

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Security/EventListener/SsoListener.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Security/EventListener/SsoListener.php
@@ -120,8 +120,9 @@ class SsoListener implements EventSubscriberInterface
             }
         }
 
-        if ($request->get('return')) {
-            $session->set('_security.portal.target_path', $request->get('return'));
+        $returnUrl = $request->get('return') ?: $request->getUri();
+        if ($returnUrl) {
+            $session->set('_security.portal.target_path', $returnUrl);
         }
 
         $ssoResult = $this->handleAutomaticSso($authInterfaceSettings);
