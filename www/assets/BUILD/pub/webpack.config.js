47cec7296e61b885e68639ff32c952bead7ca8c7
radu <radu.helstern@deskpro.com>
2018-08-20 17:27:25 +0100

    remove un-needed css loader

--- www/assets/BUILD/pub/webpack.config.js
+++ www/assets/BUILD/pub/webpack.config.js
@@ -127,14 +127,6 @@ const config = {
         ]
       },
       {
-        test:    /\.css$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            'css-loader'
-          ]
-        })
-      },
-      {
         test:    /\.scss$/,
         include: [
           path.resolve(__dirname, 'src/DeskPRO/Bundle/AdminBundle/Resources/style'),
