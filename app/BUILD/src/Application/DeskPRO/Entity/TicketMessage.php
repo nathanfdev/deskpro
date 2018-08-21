81705f4b1a0bd97c9ea4747fdae41a38422db895
ImmortaL <denis@ranneft.ru>
2018-08-21 19:18:21 +0300

    fix attachments delete when message deleted, delete ALL blobs

--- app/BUILD/src/Application/DeskPRO/Entity/TicketMessage.php
+++ app/BUILD/src/Application/DeskPRO/Entity/TicketMessage.php
@@ -16,6 +16,7 @@ use DeskPRO\Component\Util\RegexUtils;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Doctrine\ORM\PersistentCollection;
 use Orb\Html\Html2Text;
 use Orb\Util\Strings;
 use Orb\Util\Util;
@@ -841,7 +842,7 @@ class TicketMessage extends DomainObject
     }
 
     /**
-     * @return TicketAttachment[]
+     * @return TicketAttachment[]|PersistentCollection
      */
     public function getAttachments()
     {
