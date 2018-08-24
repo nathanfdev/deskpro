80f1d73274a4c12c24aa08bf34b8f2ca9f6e20db
ruslanix <ruslanix.com@gmail.com>
2018-08-24 22:04:54 +0300

    DP-2317 Fix lack of error logs during Ticket creation bug

--- app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
+++ app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
@@ -5174,7 +5174,8 @@ class TicketController extends AbstractController
                 );
             } catch (\Exception $e) {
                 $this->db->rollback();
-                SystemErrorHandler::logException($e);
+                // pass $noShowErrors = true to not `echo` error in browser
+                SystemErrorHandler::logException($e, false, null, true);
 
                 return $this->createJsonResponse(
                     [
