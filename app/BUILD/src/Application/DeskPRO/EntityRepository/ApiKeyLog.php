4df2165c3688d6c0bb34fff4e3d3778159f1739d
ImmortaL <denis@ranneft.ru>
2018-08-28 18:59:15 +0300

    remove cleanup from repo

--- app/BUILD/src/Application/DeskPRO/EntityRepository/ApiKeyLog.php
+++ app/BUILD/src/Application/DeskPRO/EntityRepository/ApiKeyLog.php
@@ -8,49 +8,10 @@
 
 namespace Application\DeskPRO\EntityRepository;
 
-use Application\DeskPRO\App;
 use Application\DeskPRO\Entity;
 
 class ApiKeyLog extends AbstractEntityRepository
 {
-    const LIMIT = 50;
-
-    /**
-     * clean old records.
-     */
-    public function cleanup()
-    {
-        $limit = self::LIMIT;
-
-        $key_ids = App::$container->getDb()->fetchAllCol('
-            SELECT key_id
-            FROM api_key_log
-            GROUP BY key_id
-            HAVING COUNT(*) > ?
-        ', [$limit], [\PDO::PARAM_INT]);
-
-        if (!$key_ids) {
-            return 0;
-        }
-
-        foreach ($key_ids as $key_id) {
-            $lid = App::$container->getDb()->fetchColumn("
-                SELECT id
-                FROM api_key_log
-                WHERE key_id = ?
-                ORDER BY id DESC
-                LIMIT $limit, 1
-            ", [$key_id]);
-
-            if ($lid) {
-                App::$container->getDb()->executeUpdate('
-                    DELETE FROM api_key_log
-                    WHERE key_id = ? AND id <= ?
-                ', [$key_id, $lid]);
-            }
-        }
-    }
-
     /**
      * @param Entity\ApiKey $api_key
      * @param int           $limit
