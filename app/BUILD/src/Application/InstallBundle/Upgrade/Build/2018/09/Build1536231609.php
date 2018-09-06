14dcfd39cd67e735c81e58512378646e50535415
Christopher Nadeau <chris.nadeau@deskpro.com>
2018-09-06 12:22:22 +0100

    Cleanup upgrade script a bit

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2018/09/Build1536231609.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2018/09/Build1536231609.php
@@ -2,6 +2,8 @@
 
 namespace Application\InstallBundle\Upgrade\Build;
 
+use Doctrine\DBAL\Connection;
+
 class Build1536231609 extends AbstractBuild implements OnlineBuildInterface
 {
     public function addNewTables()
@@ -14,26 +16,56 @@ class Build1536231609 extends AbstractBuild implements OnlineBuildInterface
 
     public function run()
     {
-        $connection      = $this->getDbConnection('default');
-        $chatDepartments = $connection->fetchAll('SELECT id FROM `departments` WHERE `is_chat_enabled` = 1');
-        if (count($chatDepartments) > 0) {
-            $departments = $connection->fetchAll(
-                'SELECT `department_id`, COUNT(*) AS cnt FROM `tickets` WHERE department_id IN (?) GROUP BY department_id HAVING cnt > 0',
-                [implode(', ', array_map(function ($dep) {
-                    return $dep['id'];
-                }, $chatDepartments))]
-            );
-            foreach ($departments as $department) {
-                $chatDepartment = $connection->fetchAssoc('SELECT d.*, dtb.`brand_id` as brand_id FROM `departments` d LEFT JOIN `department_to_brand` dtb ON d.id = dtb.department_id WHERE id = ?', [$department['department_id']]);
-                $connection->executeQuery('INSERT IGNORE INTO `departments` (`title`, `user_title`, `is_tickets_enabled`, `is_chat_enabled`, `display_order`) VALUES (?, ?, 1, 0, 0)',
-                    [$chatDepartment['title'], $chatDepartment['user_title']]
-                );
-                $newId = $connection->lastInsertId();
-                $connection->executeQuery('INSERT IGNORE INTO `department_to_brand` (`brand_id`, `department_id`) VALUES (?, ?)', [$chatDepartment['brand_id'], $newId]);
-                $connection->executeQuery('UPDATE tickets SET department_id = ? WHERE department_id = ?', [$newId, $chatDepartment['id']]);
-                $connection->executeQuery('UPDATE tickets_search_active SET department_id = ? WHERE department_id = ?', [$newId, $chatDepartment['id']]);
+        $this->out('Scanning for incorrectly set ticket departments');
+
+        $db = $this->getDbConnection('default');
+
+        $chatDepsIds = $db->fetchAllCol('
+            SELECT id FROM
+            departments
+            WHERE is_chat_enabled = 1
+        ');
+
+        if (count($chatDepsIds) > 0) {
+            $depsWithCount = $db->fetchAllCol('
+                SELECT department_id, COUNT(*) AS cnt
+                FROM tickets
+                WHERE department_id IN (?)
+                GROUP BY department_id HAVING cnt > 0
+            ', [$chatDepsIds], [Connection::PARAM_INT_ARRAY]);
+
+            foreach ($depsWithCount as $depId) {
+                $this->out("Fixing tickets using chat department {$depId}");
+                $oldDep = $db->fetchAssoc('
+                    SELECT d.*, dtb.brand_id as brand_id
+                    FROM departments d
+                    LEFT JOIN department_to_brand dtb ON d.id = dtb.department_id
+                    WHERE id = ?
+                ', [$depId]);
+
+                $db->insert('departments', [
+                    'title'              => $oldDep['title'],
+                    'user_title'         => $oldDep['user_title'],
+                    'is_tickets_enabled' => 1,
+                    'is_chat_enabled'    => 0,
+                    'display_order'      => 0,
+                ]);
+
+                $newId = $db->lastInsertId();
+                $this->out("\tNew department is $newId");
+
+                $db->insertIgnore('department_to_brand', [
+                    'brand_id'      => $oldDep['brand_id'],
+                    'department_id' => $newId,
+                ]);
+
+                $this->out("\tUpdating tickets...");
+                $count = $db->update('tickets', ['department_id' => $newId], ['department_id' => $oldDep['id']]);
+                $db->update('tickets_search_active', ['department_id' => $newId], ['department_id' => $oldDep['id']]);
+                $this->out("\tDone: $count tickets updated");
             }
-            $connection->executeQuery('UPDATE departments SET is_tickets_enabled = 0 WHERE is_chat_enabled = 1');
         }
+
+        $db->update('departments', ['is_tickets_enabled' => 0], ['is_chat_enabled' => 1]);
     }
 }
