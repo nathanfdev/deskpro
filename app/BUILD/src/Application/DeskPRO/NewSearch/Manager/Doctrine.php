696972b58e810382d7679bbf028657f8c381fcdf
Shad <cspshadx@gmail.com>
2018-09-10 23:14:16 +0300

    Ability perform quick search providing URL as a search query

--- app/BUILD/src/Application/DeskPRO/NewSearch/Manager/Doctrine.php
+++ app/BUILD/src/Application/DeskPRO/NewSearch/Manager/Doctrine.php
@@ -3,6 +3,8 @@
 namespace Application\DeskPRO\NewSearch\Manager;
 
 use Application\DeskPRO\App;
+use function is_int;
+use League\Url\Url;
 use Orb\Util\Arrays;
 use Orb\Util\Numbers;
 use Orb\Util\Strings;
@@ -80,6 +82,40 @@ class Doctrine extends ContainerAware implements SearchManagerInterface
 
         if (preg_match('#^\[(.*?)\]$#', $q, $m)) {
             $is_label = $m[1];
+        }
+
+        // Try to parse an URL, in case it's provided as a search query
+        try {
+            // all cases: we do not care about the host, we just need to check if could get ticket number
+            // so just attempt to parse URL. Unfortunately, the old League\Url is not smart enough and
+            // can't validate the URL, so just catch \RuntimeException in case of parse failure.
+            $url = Url::createFromUrl($q);
+
+            // case 1: searh by ticket number in framgment
+            // eg.: https://support.deskpro.com/agent/#app.tickets,inbox:agent,t:109346,t:108966,t:108169,p.o:65264,vis:7
+            $fragment = $url->getFragment()->get();
+            if (!empty($fragment)) {
+                preg_match_all('/[a-zA-Z]\.o:(\d*)/', $fragment, $matches);
+                foreach ($matches[1] as $ticketId) {
+                    $ticket = $this->em->getRepository('DeskPRO:Ticket')->findTicketId((int) $ticketId);
+                    if ($ticket && $this->person->PermissionsManager->TicketChecker->canView($ticket)) {
+                        $results['ticket'][] = $ticket;
+                    }
+                }
+            }
+
+            // case 2: search ticket number in path,
+            // eg.: https://support.deskpro.com/agent/go/ticket/109346
+            $path = $url->getPath()->toArray();
+            $index = array_search('ticket', $path);
+            if (is_int($index) && array_key_exists($index + 1, $path)) {
+                $ticket = $this->em->getRepository('DeskPRO:Ticket')->findTicketId((int)$path[$index + 1]);
+                if ($ticket && $this->person->PermissionsManager->TicketChecker->canView($ticket)) {
+                    $results['ticket'][] = $ticket;
+                }
+            }
+        } catch (\RuntimeException $exception) {
+            // do nothing, and just let it go further
         }
 
         // We dont know about past ref formats, so just always try to find
