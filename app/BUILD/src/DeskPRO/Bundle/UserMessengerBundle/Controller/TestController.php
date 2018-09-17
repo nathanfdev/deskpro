9792d4e51a35de0dd414ec8cdb5ab3e3a9c0dabf
ImmortaL <denis@ranneft.ru>
2018-09-17 15:58:47 +0300

    new kernel, bundle, authenticator, configs and so on

---
+++ app/BUILD/src/DeskPRO/Bundle/UserMessengerBundle/Controller/TestController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DeskPRO\Bundle\UserMessengerBundle\Controller;
+
+use DeskPRO\Bundle\ApiBundle\ApiDoc\Annotation\ApiDoc;
+use DeskPRO\Bundle\ApiBundle\Controller\BaseController;
+use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\ApiModes;
+use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\ApiUserContext;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+
+/**
+ * Class TestController.
+ *
+ * @ApiModes("all")
+ * @ApiUserContext("open")
+ */
+class TestController extends BaseController
+{
+    /**
+     * You can use this endpoint to gather information about clients you need to obtain notifications and alerts.
+     *
+     * @ApiDoc(
+     *     section="Testing new Bundle and Kernel",
+     *     resourceDescription="Testing new Bundle and Kernel",
+     *     statusCodes={
+     *         200="Returned if everything is ok"
+     *     },
+     *     noOutput=true
+     * )
+     *
+     * @return View
+     * @Rest\Get("/ping")
+     */
+    public function pingAction()
+    {
+        return View::create($this->wrap(['response' => 'pong']));
+    }
+}
---
+++ app/BUILD/sys/Kernel/MessengerKernel.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace DpSys\Kernel;
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+/**
+ * Class MessengerKernel.
+ */
+class MessengerKernel extends BaseKernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        $bundles = [
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
+            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new \WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            new \FOS\RestBundle\FOSRestBundle(),
+            new \FOS\ElasticaBundle\FOSElasticaBundle(),
+            new \Nelmio\CorsBundle\NelmioCorsBundle(),
+            new \JMS\SerializerBundle\JMSSerializerBundle(),
+
+            new \Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new \RMS\PushNotificationsBundle\RMSPushNotificationsBundle(),
+            new \Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
+
+            new \Application\DeskPRO\DeskPROBundle(),
+            new \Application\EmailBundle\EmailBundle(),
+            new \DeskPRO\Bundle\SendmailBundle\SendmailBundle(),
+            new \DeskPRO\Bundle\ApiBundle\ApiBundle(),
+            new \DeskPRO\Bundle\AppBundle\AppBundle(),
+            new \DeskPRO\Bundle\SystemBundle\SystemBundle(),
+            new \DeskPRO\Bundle\AuditBundle\AuditBundle(),
+            new \DeskPRO\Bundle\VoiceBundle\VoiceBundle(),
+            new \DeskPRO\Bundle\UserMessengerBundle\UserMessengerBundle(),
+        ];
+
+        if ('dev' === $this->getEnvironment()
+            or
+            'test' === $this->getEnvironment()) {
+            $bundles[] = new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new \Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        if ('test' === $this->getEnvironment()) {
+            $bundles[] = new \DpTestSrc\TestBundle\TestBundle();
+        }
+
+        return $bundles;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(DP_ROOT.'/sys/config/messenger/messenger_config_'.$this->getEnvironment().'.yml');
+    }
+}
