512ed6e7e87d12ea44ea41248078634c7022bb71
Julien Ducro <julien.ducro@deskpro.com>
2018-10-01 10:53:52 +0200

    DP-2591 - Fix API Log Options save

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/Logs/ApiLogsOptionsType.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/Logs/ApiLogsOptionsType.php
@@ -5,6 +5,7 @@ namespace DeskPRO\Bundle\AppBundle\Form\Type\Logs;
 use DeskPRO\Bundle\AppBundle\Form\Type\ApiBooleanType;
 use DeskPRO\Bundle\AppBundle\Serializer\Model\Logs\OptionsModel;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -22,6 +23,14 @@ class ApiLogsOptionsType extends AbstractType
         $builder
             ->add('enabled', ApiBooleanType::class, [
                 'required' => false,
+            ])
+            ->add('modes', ChoiceType::class, [
+                'required'          => false,
+                'multiple'          => true,
+                'choices_as_values' => true,
+                'choices'           => [
+                    'key',
+                ],
             ])
             ->add('request_length', IntegerType::class, [
                 'required'      => false,
