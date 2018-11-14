4b5deea4091c48fa4e4bd9b13a6a015950316cd3
Christopher Nadeau <chris.nadeau@deskpro.com>
2018-11-14 14:13:44 +0000

    Fix resolving dashboard perms by team

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Reports/DashboardsController.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Reports/DashboardsController.php
@@ -123,8 +123,8 @@ class DashboardsController extends CrudController
         }
 
         $qb
-            ->join("$alias.permissions", 'p')
-            ->join('p.person', 'per')
+            ->leftJoin("$alias.permissions", 'p')
+            ->leftJoin('p.person', 'per')
             ->andWhere("p.person IN (:person) OR p.team IN (:teams) OR $alias.person IN (:person)")
             ->orWhere('p.person IS NULL AND p.team IS NULL AND p.department IS NULL')
             ->setParameter('person', $this->getUser())
