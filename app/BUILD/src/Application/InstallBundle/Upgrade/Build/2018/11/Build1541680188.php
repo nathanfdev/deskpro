560883d35808ae4c3bef7e4f8721e51182553b6b
Christopher Nadeau <chris.nadeau@deskpro.com>
2018-11-15 12:34:59 +0000

    Fix upgrade script using default that breaks on older mysql

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2018/11/Build1541680188.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2018/11/Build1541680188.php
@@ -10,7 +10,10 @@ class Build1541680188 extends AbstractBuild implements OnlineBuildInterface
 
     public function runAlters()
     {
-        $this->execDbQuery('default', 'ALTER TABLE app2_app ADD `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP, ADD `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP, ADD `bundle_updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP');
+        $this->execDbQuery('default', 'ALTER TABLE app2_app ADD `created_at` DATETIME DEFAULT NULL, ADD `updated_at` DATETIME DEFAULT NULL, ADD `bundle_updated_at` DATETIME DEFAULT NULL');
+
+        $d = '2018-11-08 12:29:48';
+        $this->execDbQuery('default', "UPDATE app2_app SET created_at = '$d', updated_at = '$d', bundle_updated_at = '$d'");
     }
 
     public function run()
