94fe6333b0b131d84552dd665bb92477b2111156
Julien Ducro <julien.ducro@deskpro.com>
2018-11-29 16:32:13 +0100

    DP-2392 - Fix messenger-setup import

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AdminBundle/Modules/Messenger/Components/MessengerSetupContainer.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AdminBundle/Modules/Messenger/Components/MessengerSetupContainer.js
@@ -1,51 +1,52 @@
 import React from 'react';
 import MessengerSetup from '@deskpro/messenger-setup';
-import Immutable from "immutable";
+import Immutable from 'immutable';
 
-const settings = {
-  styles: {
-    backgroundColor: '#aaa',
-    primaryColor: '#3d88f3'
-  },
-  embed: {
-    showOnPortal: false,
-    authorizeDomains: ''
-  },
-  chat: {
-    enabled: true,
-    prompt: 'What can we help you with today?',
-    timeout: 90,
-    noAnswerBehavior: 'save_ticket',
-    busyMessage:
-      'It looks like all of our agents are busy at the moment. You can still send us a ticket below and we will get back to you as soon as possible',
-    department: 3,
-    ticketSubject: 'Missed chat from {name}'
-  },
-  tickets: {
-    enabled: false
-  },
-  messenger: {
-    autoStart: false,
-    title: 'Get In Touch',
-    subtext: '',
-    chat: {
-      title: 'Start a conversation',
-      description: 'Start a chat with one of our agents',
-      buttonText: 'Start a new conversation',
-      showAgentPhotos: false
-    },
-    tickets: {
-      title: 'Contact Us',
-      description: 'Send us a message and we will reply by email',
-      buttonText: 'Send a message'
-    }
+class MessengerSetupContainer extends React.Component {
+  static getInitialSettings() {
+    return {
+      styles: {
+        backgroundColor: '#aaa',
+        primaryColor:    '#3d88f3'
+      },
+      embed: {
+        showOnPortal:     false,
+        authorizeDomains: ''
+      },
+      chat: {
+        enabled:          true,
+        prompt:           'What can we help you with today?',
+        timeout:          90,
+        noAnswerBehavior: 'save_ticket',
+        busyMessage:
+                          'It looks like all of our agents are busy at the moment. You can still send us a ticket below and we will get back to you as soon as possible',
+        department:    3,
+        ticketSubject: 'Missed chat from {name}'
+      },
+      tickets: {
+        enabled: false
+      },
+      messenger: {
+        autoStart: false,
+        title:     'Get In Touch',
+        subtext:   '',
+        chat:      {
+          title:           'Start a conversation',
+          description:     'Start a chat with one of our agents',
+          buttonText:      'Start a new conversation',
+          showAgentPhotos: false
+        },
+        tickets: {
+          title:       'Contact Us',
+          description: 'Send us a message and we will reply by email',
+          buttonText:  'Send a message'
+        }
+      }
+    };
   }
-};
 
-class MessengerSetupContainer extends React.Component
-{
   state = {
-    settings: Immutable.fromJS(settings)
+    settings: Immutable.fromJS(MessengerSetupContainer.getInitialSettings())
   };
 
   onChange = (value, name) => {
