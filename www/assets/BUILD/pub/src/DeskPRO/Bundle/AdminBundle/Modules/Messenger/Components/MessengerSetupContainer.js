4173b96d2b8cc688fd13cddb1ecf33e935d2c1cb
ImmortaL <denis@ranneft.ru>
2018-12-10 20:26:26 +0300

    use api data only

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AdminBundle/Modules/Messenger/Components/MessengerSetupContainer.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AdminBundle/Modules/Messenger/Components/MessengerSetupContainer.js
@@ -13,55 +13,13 @@ class MessengerSetupContainer extends React.Component {
     params:   PropTypes.object.isRequired,
   };
 
-  static getInitialSettings() {
-    return {
-      styles: {
-        backgroundColor: '#aaa',
-        primaryColor:    '#3d88f3'
-      },
-      embed: {
-        showOnPortal:     false,
-        authorizeDomains: ''
-      },
-      chat: {
-        enabled:          true,
-        prompt:           'What can we help you with today?',
-        timeout:          90,
-        noAnswerBehavior: 'save_ticket',
-        busyMessage:
-                          'It looks like all of our agents are busy at the moment. You can still send us a ticket below and we will get back to you as soon as possible',
-        department:    3,
-        ticketSubject: 'Missed chat from {name}'
-      },
-      tickets: {
-        enabled: false
-      },
-      messenger: {
-        autoStart: false,
-        title:     'Get In Touch',
-        subtext:   '',
-        chat:      {
-          title:           'Start a conversation',
-          description:     'Start a chat with one of our agents',
-          buttonText:      'Start a new conversation',
-          showAgentPhotos: false
-        },
-        tickets: {
-          title:       'Contact Us',
-          description: 'Send us a message and we will reply by email',
-          buttonText:  'Send a message'
-        }
-      }
-    };
-  }
-
   state = {
-    settings: Immutable.fromJS(MessengerSetupContainer.getInitialSettings())
+    settings: Immutable.fromJS({})
   };
 
   componentDidMount() {
-    this.props.dispatch(getSettings(this.props.params.brandId)).then((data) => {
-      const newSettings = this.state.settings.merge(data.data);
+    this.props.dispatch(getSettings(this.props.params.brandId)).then((response) => {
+      const newSettings = this.state.settings.merge(response.data.data);
       this.setState({ settings: newSettings });
     });
   }
