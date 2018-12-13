c971fa234282374086f9d5ff635cbb4f2eaefa67
ImmortaL <denis@ranneft.ru>
2018-12-13 18:35:07 +0300

    messenger config endpoint

---
+++ app/BUILD/src/DeskPRO/Bundle/MessengerBundle/Controller/ServiceController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace DeskPRO\Bundle\MessengerBundle\Controller;
+
+use DeskPRO\Bundle\ApiBundle\ApiDoc\Annotation\ApiDoc;
+use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\ApiModes;
+use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\ApiUserContext;
+use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\Feature;
+use DeskPRO\Bundle\AppBundle\Serializer\Sideload\SideloadSerializationContext;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class ServiceController.
+ *
+ * @ApiModes("all")
+ * @ApiUserContext("open")
+ *
+ * @Rest\Route("/service")
+ * @Feature("messenger")
+ */
+class ServiceController extends AbstractMessengerController
+{
+    /**
+     * You can use this endpoint to gather information about clients you need to obtain notifications and alerts.
+     *
+     * @ApiDoc(
+     *     section="Messenger",
+     *     resourceDescription="Service actions",
+     *     statusCodes={
+     *         200="Returned if everything is ok"
+     *     },
+     *     requirements={
+     *          {
+     *              "name"="visitorId",
+     *              "requirement"="[a-zA-Z0-9\\.\\-_]+",
+     *              "description"="id of the visitor to look for",
+     *              "dataType"="string"
+     *          }
+     *      },
+     *     output="DeskPRO\Bundle\MessengerBundle\Settings\Model\MessengerSettings"
+     * )
+     * @Rest\Get("/setup")
+     *
+     * @return View
+     */
+    public function messengerSetupAction()
+    {
+        $brand    = $this->get('brand_stack')->getActive()->getBrand();
+        $settings = $this->get('messenger.service.settings_resolver')->getMessengerSettings($brand);
+        $data     = $this->get('serializer')->toArray($settings,  new SideloadSerializationContext());
+
+        return View::create($data, Response::HTTP_OK);
+    }
+}
