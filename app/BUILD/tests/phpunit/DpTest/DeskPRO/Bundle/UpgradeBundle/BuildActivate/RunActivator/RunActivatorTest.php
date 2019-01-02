26173a68757096290f9073839bdc0aa341046728
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-01-02 17:16:27 +0300

    removed outdated test

--- app/BUILD/tests/phpunit/DpTest/DeskPRO/Bundle/UpgradeBundle/BuildActivate/RunActivator/RunActivatorTest.php
+++ app/BUILD/tests/phpunit/DpTest/DeskPRO/Bundle/UpgradeBundle/BuildActivate/RunActivator/RunActivatorTest.php
@@ -62,23 +62,4 @@ class RunActivatorTest extends DeskProTestCase
         $this->assertFalse(file_exists($this->root->url().'/app/run/run_15740'));
         $this->assertTrue(file_exists($this->root->url().'/app/run/file.txt'));
     }
-
-    /**
-     * @test
-     * @expectedException \Symfony\Component\Filesystem\Exception\IOException
-     */
-    public function it_excepts_when_cannot_write()
-    {
-        $build = new BuildInstance(
-            '15741.0',
-            $this->root->url().'/app/15741.0',
-            $this->root->url().'/www/assets/15741.0',
-            $this->root->url().'/var/kernel_cache/15741.0'
-        );
-
-        chmod($this->root->url().'/app', 0555);
-
-        $runActivator = new RunActivator(Zippy::load(), sys_get_temp_dir());
-        $runActivator->activateRunDir($build);
-    }
 }
