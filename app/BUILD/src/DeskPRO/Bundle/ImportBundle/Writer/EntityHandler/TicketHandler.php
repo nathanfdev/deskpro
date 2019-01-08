eec38e90e55f73b088b9b465ca7c43efce9c3863
ruslanix <ruslanix.com@gmail.com>
2019-01-08 22:54:57 +0300

    DP-2791 Ticket Statuses. Fix tests.

--- app/BUILD/src/DeskPRO/Bundle/ImportBundle/Writer/EntityHandler/TicketHandler.php
+++ app/BUILD/src/DeskPRO/Bundle/ImportBundle/Writer/EntityHandler/TicketHandler.php
@@ -2,7 +2,6 @@
 
 namespace DeskPRO\Bundle\ImportBundle\Writer\EntityHandler;
 
-use Application\DeskPRO\App;
 use Application\DeskPRO\Entity;
 use DeskPRO\Bundle\ImportBundle\Model;
 
@@ -41,17 +40,9 @@ class TicketHandler extends AbstractEntityHandler
         }
 
         $entity
-            ->disableAutoTicketProcess();
-
-        if ($model->getStatus()) {
-            $entity->setTicketStatus(App::getContainer()->getTicketStatuses()->findStatusOrException($model->getStatus()));
-        } else {
-            $entity->setStatus($model->getStatus());
-        }
-
-        $entity
+            ->disableAutoTicketProcess()
             ->setSubject($model->getSubject())
-            //->setStatus($model->getStatus())
+            ->setStatus($model->getStatus())
             ->setLanguage($this->helpers->getLanguageHelper()->findOrCreateLanguage($model->getLanguage()))
             ->setDateResolved($model->getDateResolved())
             ->setDateArchived($model->getDateArchived())
