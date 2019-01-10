45e32ef45fd13d84177aac62ce4ad83a5df0b9c1
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-01-11 00:10:43 +0300

    fix typo

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AdminBundle/Modules/Voice/Components/Queues/Form/QueueForm.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AdminBundle/Modules/Voice/Components/Queues/Form/QueueForm.js
@@ -157,7 +157,7 @@ class RoutingModel extends React.Component {
     const choices = [
       { value: 'automatic', label: 'Automatic', help: 'Call will be routed amongst all agents evenly. The system will automatically balance calls so that no single agent handles more or less than any other agent. For example, given three agents online, if AgentA and AgentB have both accepted a call, then they won\'t receive another call until AgentC has also accepted a call.' },
       { value: 'least_utilized', label: 'Least Utilized', help: 'Calls will be routed towards agents who have handled the fewest calls.' },
-      { value: 'simulring', label: 'Simulring', help: 'Any incoming call will ring ALL agents at the same time. The first to answer wil handle the call.' }
+      { value: 'simulring', label: 'Simulring', help: 'Any incoming call will ring ALL agents at the same time. The first to answer will handle the call.' }
     ];
 
     const help = {};
