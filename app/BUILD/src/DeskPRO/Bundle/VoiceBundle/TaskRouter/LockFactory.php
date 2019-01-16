4ff2477055d0693fbc5937a3aa489ccac96db740
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-01-16 19:35:55 +0300

    flock fallback

--- app/BUILD/src/DeskPRO/Bundle/VoiceBundle/TaskRouter/LockFactory.php
+++ app/BUILD/src/DeskPRO/Bundle/VoiceBundle/TaskRouter/LockFactory.php
@@ -2,7 +2,9 @@
 
 namespace DeskPRO\Bundle\VoiceBundle\TaskRouter;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Lock\Factory;
+use Symfony\Component\Lock\Store\FlockStore;
 use Symfony\Component\Lock\Store\SemaphoreStore;
 
 /**
@@ -11,11 +13,18 @@ use Symfony\Component\Lock\Store\SemaphoreStore;
 class LockFactory
 {
     /**
+     * @param ContainerInterface $container
+     *
      * @return \Symfony\Component\Lock\Lock
      */
-    public static function createTaskRouterLock()
+    public static function createTaskRouterLock(ContainerInterface $container)
     {
-        $store   = new SemaphoreStore();
+        if (SemaphoreStore::isSupported()) {
+            $store = new SemaphoreStore();
+        } else {
+            $store = new FlockStore($container->get('deskpro.app_env')->getUserTmpDir());
+        }
+
         $factory = new Factory($store);
 
         return $factory->createLock('voice-task-router', 30);
