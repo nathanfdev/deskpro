c24f2542cda3e14a731877ce39246aec6b782261
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-01-16 19:35:55 +0300

    pdo lock store

--- app/BUILD/src/DeskPRO/Bundle/VoiceBundle/TaskRouter/LockFactory.php
+++ app/BUILD/src/DeskPRO/Bundle/VoiceBundle/TaskRouter/LockFactory.php
@@ -2,10 +2,10 @@
 
 namespace DeskPRO\Bundle\VoiceBundle\TaskRouter;
 
+use DeskPRO\Component\Lock\PdoStore;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Lock\Factory;
-use Symfony\Component\Lock\Store\FlockStore;
-use Symfony\Component\Lock\Store\SemaphoreStore;
+use Symfony\Component\Lock\Store\RetryTillSaveStore;
 
 /**
  * Class LockFactory.
@@ -19,12 +19,7 @@ class LockFactory
      */
     public static function createTaskRouterLock(ContainerInterface $container)
     {
-        if (SemaphoreStore::isSupported()) {
-            $store = new SemaphoreStore();
-        } else {
-            $store = new FlockStore($container->get('deskpro.app_env')->getUserTmpDir());
-        }
-
+        $store   = new RetryTillSaveStore(new PdoStore($container->get('doctrine.dbal.default_connection')));
         $factory = new Factory($store);
 
         return $factory->createLock('voice-task-router', 30);
