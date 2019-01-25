d2e2bf10ae635177b0591f2c19f396defff35618
Christopher Nadeau <chris.nadeau@deskpro.com>
2019-01-25 10:09:40 +0000

    remove manual create of lock_keys during tests

--- app/BUILD/tests/src/TestBundle/DataSet/AbstractDbSet.php
+++ app/BUILD/tests/src/TestBundle/DataSet/AbstractDbSet.php
@@ -8,7 +8,6 @@ namespace DpTestSrc\TestBundle\DataSet;
 
 use Application\DeskPRO\DependencyInjection\DeskproContainer;
 use Application\DeskPRO\Entity\Brand;
-use DeskPRO\Component\Lock\PdoStore;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\DBAL\Connection;
 use Orb\Util\Util;
@@ -306,9 +305,6 @@ abstract class AbstractDbSet implements DataSetInterface
             $this->installDatabase('system');
             $this->installDatabase('audit');
             $this->installDatabase('voice');
-
-            $store = new PdoStore($this->getEm()->getConnection());
-            $store->createTable();
 
             self::$isStructureCreated = true;
         } else {
