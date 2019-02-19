b44b946dbdf82f9d6f39fb6d11be45ac6960513b
Julien Ducro <julien.ducro@deskpro.com>
2019-02-19 11:55:08 +0100

    DP-3003 - Prevent Upload menu for leaving screen

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Publish/Components/Editor/ImageMenu.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Publish/Components/Editor/ImageMenu.js
@@ -5,6 +5,7 @@ import DropZone from 'DeskPRO/Component/Uploader/DropZone';
 import { UploadButton } from 'DeskPRO/Component/Uploader/UploadButton';
 import { Input } from 'DeskPRO/Component/Semantic/Form';
 import { Button } from 'DeskPRO/Component/Semantic/Button';
+import { Scrollbar } from '@deskpro/react-components';
 
 class ImageMenu extends React.Component {
   static propTypes = {
@@ -91,46 +92,51 @@ class ImageMenu extends React.Component {
       <div>
         <div className="header"><FormattedMessage id="agent.guides.add_image" /></div>
         <div className="description">
-          <DropZone
-            getExternalInput={() => this.uploadButton.input}
-            uploadUrl={this.getUploadUrl()}
-            onSend={this.onSend}
-            onSuccess={this.handleSuccess}
-            onFail={this.onFail}
-            ref={(c) => { this.node = c; }}
+          <Scrollbar
+            autoHide
+            autoHeightMax={470}
           >
-            <div className="drop-zone">
-              <UploadButton
-                id={'upload_topic_image'}
-                ref={(c) => { this.uploadButton = c; }}
-                className="upload_topic_image"
-                name="file"
-                onSend={this.onSend}
-                onSuccess={this.handleSuccess}
-                onFail={this.onFail}
-                uploadUrl={this.getUploadUrl()}
-              />
-              <label className="ui button small basic" htmlFor={'upload_topic_image'}>
-                <FormattedMessage id="agent.general.upload_image" />
-              </label>
-            </div>
-          </DropZone>
-          {this.renderImage()}
-          <br />
-          <label htmlFor="image_title"><FormattedMessage id="agent.general.title" />: </label><br />
-          <Input type="text" id="image_title" value={this.state.title} onChange={this.setTitle} /><br />
-          <label htmlFor="image_width"><FormattedMessage id="agent.general.width" />: </label><br />
-          <Input type="text" id="image_width" value={this.state.width} onChange={this.setWidth} /><br />
-          <label htmlFor="image_height"><FormattedMessage id="agent.general.height" />: </label><br />
-          <Input type="text" id="image_height" value={this.state.height} onChange={this.setHeight} /><br />
-          <label htmlFor="image_alt"><FormattedMessage id="agent.guides.alternative_text" />: </label><br />
-          <Input type="text" id="image_alt" value={this.state.alt} onChange={this.setAlt} /><br />
-          <br />
-          <Button onClick={this.insert} disabled={!(this.state.image)}>
-            <FormattedMessage id="agent.general.insert" />
-          </Button>
-          <br />
-          <div className="drop-info"><FormattedMessage id="agent.guides.images_drop_info" /></div>
+            <DropZone
+              getExternalInput={() => this.uploadButton.input}
+              uploadUrl={this.getUploadUrl()}
+              onSend={this.onSend}
+              onSuccess={this.handleSuccess}
+              onFail={this.onFail}
+              ref={(c) => { this.node = c; }}
+            >
+              <div className="drop-zone">
+                <UploadButton
+                  id={'upload_topic_image'}
+                  ref={(c) => { this.uploadButton = c; }}
+                  className="upload_topic_image"
+                  name="file"
+                  onSend={this.onSend}
+                  onSuccess={this.handleSuccess}
+                  onFail={this.onFail}
+                  uploadUrl={this.getUploadUrl()}
+                />
+                <label className="ui button small basic" htmlFor={'upload_topic_image'}>
+                  <FormattedMessage id="agent.general.upload_image" />
+                </label>
+              </div>
+            </DropZone>
+            {this.renderImage()}
+            <br />
+            <label htmlFor="image_title"><FormattedMessage id="agent.general.title" />: </label><br />
+            <Input type="text" id="image_title" value={this.state.title} onChange={this.setTitle} /><br />
+            <label htmlFor="image_width"><FormattedMessage id="agent.general.width" />: </label><br />
+            <Input type="text" id="image_width" value={this.state.width} onChange={this.setWidth} /><br />
+            <label htmlFor="image_height"><FormattedMessage id="agent.general.height" />: </label><br />
+            <Input type="text" id="image_height" value={this.state.height} onChange={this.setHeight} /><br />
+            <label htmlFor="image_alt"><FormattedMessage id="agent.guides.alternative_text" />: </label><br />
+            <Input type="text" id="image_alt" value={this.state.alt} onChange={this.setAlt} /><br />
+            <br />
+            <Button onClick={this.insert} disabled={!(this.state.image)}>
+              <FormattedMessage id="agent.general.insert" />
+            </Button>
+            <br />
+            <div className="drop-info"><FormattedMessage id="agent.guides.images_drop_info" /></div>
+          </Scrollbar>
         </div>
       </div>
     );
