3e4ce8288659747d596d12e3d6aa787710f8aea1
ruslanix <ruslanix.com@gmail.com>
2019-02-21 15:19:47 +0300

    DP-3054 Tix TicketCharges constraints

--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -1213,10 +1213,10 @@ return [
         'skipPostBuild' => true,
         'isOnlineBuild' => true,
     ],
-    1550675133 => [
-        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/02/Build1550675133.php',
-        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1550675133',
-        'skipPostBuild' => false,
-        'isOnlineBuild' => true,
+    1550750781 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/02/Build1550750781.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1550750781',
+        'skipPostBuild' => true,
+        'isOnlineBuild' => false,
     ],
 ];
