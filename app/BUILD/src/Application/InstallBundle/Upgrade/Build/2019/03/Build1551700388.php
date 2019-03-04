5f060bee34de69ea2bed0b2c2ee7c6baf6a237b4
ImmortaL <denis@ranneft.ru>
2019-03-04 20:52:03 +0300

    update mapping with new field, also update config and serach and transformer

--- app/BUILD/src/Application/InstallBundle/Upgrade/Build/2019/03/Build1551700388.php
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2019/03/Build1551700388.php
@@ -2,6 +2,8 @@
 
 namespace Application\InstallBundle\Upgrade\Build;
 
+use Elastica\Request;
+
 class Build1551700388 extends AbstractBuild implements OnlineBuildInterface
 {
     public function addNewTables()
@@ -14,7 +16,28 @@ class Build1551700388 extends AbstractBuild implements OnlineBuildInterface
 
     public function run()
     {
-        $this->execDbQuery('default', 'INSERT INTO `settings` (`name`, `value`) VALUES (\'elastica.requires_reset\', 1) ON DUPLICATE KEY UPDATE `value` = 1;');
-        $this->execDbQuery('default', 'DELETE FROM `datastore` WHERE `name` = \'sys.es_indexer\';');
+        $enabled = $this->container->getDb()->fetchColumn("SELECT `value` FROM `settings` WHERE `name` = 'elastica.enabled'");
+        if ($enabled) {
+            $this->out('Elasticsearch is enabled. Updating schema');
+            $index = $this->container->get('fos_elastica.index.deskpro');
+            $paths = [
+                'ticket',
+                'person',
+                'organization',
+                'feedback',
+                'chat_conversation',
+                'article',
+            ];
+
+            foreach ($paths as $path) {
+                $index->getClient()->request('deskpro/_mapping/'.$path, Request::PUT, [
+                    'properties' => [
+                    'custom_data2' => ['type' => 'nested', 'properties' => [
+                        'id'    => ['type' => 'integer'],
+                        'value' => ['type' => 'string', 'analyzer' => 'text_content_analyzer'],
+                    ]],
+                ], ]);
+            }
+        }
     }
 }
