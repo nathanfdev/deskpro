95377eea9ed43d327b54b5ccc26f59e40e050ef4
Christopher Nadeau <chris.nadeau@deskpro.com>
2019-03-11 15:48:54 +0000

    Add pending status info for existing agents

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Onboarding/Tours/index.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/Onboarding/Tours/index.js
@@ -1,4 +1,5 @@
 export testOnboarding from './testOnboarding';
 export topbar from './topbar';
 export newIm from './newIm';
+export newPendingStatus from './newPendingStatus';
 export topbarChanges from './topbarChanges';
