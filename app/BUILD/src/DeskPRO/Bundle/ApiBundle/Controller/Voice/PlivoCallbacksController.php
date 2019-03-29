15b13b8000e1b0f7c57bed3bf185e7e3f5588a0a
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-03-29 12:51:16 +0300

    skip two fake rings if no greet asset

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Voice/PlivoCallbacksController.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Voice/PlivoCallbacksController.php
@@ -1107,9 +1107,11 @@ class PlivoCallbacksController extends BaseController
         }
 
         if ($target instanceof VoiceQueueTarget) {
-            $plivoXml->addPlay($this->get('dp.voice.assets_helper')->getDefaultRingAssetUrl());
-            $plivoXml->addPlay($this->get('dp.voice.assets_helper')->getDefaultRingAssetUrl());
-            $this->playAsset($plivoXml, $target->getQueue()->getGreetAsset());
+            if ($target->getQueue()->getGreetAsset()) {
+                $plivoXml->addPlay($this->get('dp.voice.assets_helper')->getDefaultRingAssetUrl());
+                $plivoXml->addPlay($this->get('dp.voice.assets_helper')->getDefaultRingAssetUrl());
+                $this->playAsset($plivoXml, $target->getQueue()->getGreetAsset());
+            }
         } elseif ($target instanceof VoiceAutoAttendantTarget) {
             $autoAttendant = $target->getAutoAttendant();
             $dialNumbers   = $autoAttendant->getOrderedDialNumbers();
--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Voice/TwilioCallbacksController.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Controller/Voice/TwilioCallbacksController.php
@@ -967,7 +967,11 @@ class TwilioCallbacksController extends BaseController
         }
 
         if ($target instanceof VoiceQueueTarget) {
-            $this->playGreetAsset($twiml, $target->getQueue()->getGreetAsset());
+            if ($target->getQueue()->getGreetAsset()) {
+                $twiml->play($this->get('dp.voice.assets_helper')->getDefaultRingAssetUrl());
+                $twiml->play($this->get('dp.voice.assets_helper')->getDefaultRingAssetUrl());
+                $this->playGreetAsset($twiml, $target->getQueue()->getGreetAsset());
+            }
         } elseif ($target instanceof VoiceAutoAttendantTarget) {
             $autoAttendant = $target->getAutoAttendant();
             $dialNumbers   = $autoAttendant->getOrderedDialNumbers();
