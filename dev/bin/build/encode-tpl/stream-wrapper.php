3c3cbdf0ea4a6ca80f1ce1b91c80f5c80e14788c
Christopher Nadeau <chris.nadeau@deskpro.com>
2019-04-30 13:06:48 +0100

    simplify path compares, only allow loading this one specific file

--- dev/bin/build/encode-tpl/stream-wrapper.php
+++ dev/bin/build/encode-tpl/stream-wrapper.php
@@ -1,6 +1,6 @@
 <?php
 
-class __STREAM_ID__
+final class __STREAM_ID__
 {
     private $position = 0;
     private $content = '';
@@ -8,22 +8,11 @@ class __STREAM_ID__
 
     public function stream_open($path)
     {
-        $pos = strpos($path, '://');
-        if ($pos === false) {
+        if ($path !== self::p1()) {
             return false;
         }
 
-        $realPath = substr($path, $pos + 3);
-        if ($realPath[0] !== '/' && $realPath[0] != '\\') {
-            return false;
-        }
-
-        $raw = file_get_contents($realPath);
-        if ($raw === false) {
-            return false;
-        }
-
-        $this->content = @gzuncompress(strrev(base64_decode($raw)));
+        $this->content = @gzuncompress(strrev(base64_decode(file_get_contents(self::p2()))));
         if (!$this->content) {
             $this->content = '';
             return false;
@@ -51,6 +40,19 @@ class __STREAM_ID__
     {
         return ['mode' => 0444, 'size' => $this->size, 'atime' => __STREAM_TIME__, 'mtime' => __STREAM_TIME__, 'ctime' => __STREAM_TIME__];
     }
+
+    private static function p1() { return __DAT_BIN_PATH__; }
+    private static function p2() { return __DAT_BIN_PATH_REAL__; }
+    public static function a($c) {
+        switch ($c) {
+            case __STREAM_AUTOLOAD__:
+                if (file_exists(self::p2())) {
+                    return require_once self::p1();
+                }
+        }
+        return false;
+    }
 }
 
-stream_wrapper_register("__STREAM_ID__", "__STREAM_ID__");
+stream_wrapper_register('__STREAM_ID__', '__STREAM_ID__');
+spl_autoload_register(['__STREAM_ID__', 'a'], false, true);
