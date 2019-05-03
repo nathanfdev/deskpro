6872fc0e8f3d5fa9e55f6c926b304ac21f6a78c2
ImmortaL <denis@ranneft.ru>
2019-05-03 13:05:45 +0300

    CS update, remove handlebars from vendor

--- app/BUILD/sys/config/config.assets.php
+++ app/BUILD/sys/config/config.assets.php
@@ -56,7 +56,7 @@ $CONFIG['agent'] = [
 $CONFIG['agent_vendors'] = [
     'out'   => 'js/agent-vendors.js',
     'files' => [
-        'vendor/handlebars.runtime-v4.1.2.js',
+        'node_modules/handlebars/dist/handlebars.runtime.js',
         'vendor/modernizr.min.js',
         'javascripts/Orb/modernizr-ext.js',
         'node_modules/custom-event-polyfill/custom-event-polyfill.js',
