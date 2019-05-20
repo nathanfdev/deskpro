994d4e375c1f864dfff9f33674cd40b5c51c6a07
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-05-20 22:11:20 +0300

    voice queue with a single brand fixes

--- app/BUILD/src/DeskPRO/Bundle/VoiceBundle/Form/Type/VoiceQueueType.php
+++ app/BUILD/src/DeskPRO/Bundle/VoiceBundle/Form/Type/VoiceQueueType.php
@@ -9,7 +9,6 @@ use Application\DeskPRO\Entity\Person;
 use DeskPRO\Bundle\AppBundle\Entity\AgentData;
 use DeskPRO\Bundle\AppBundle\Entity\VoiceQueue;
 use DeskPRO\Bundle\AppBundle\Form\Type\ApiBooleanType;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
@@ -27,21 +26,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class VoiceQueueType extends AbstractType
 {
     /**
-     * @var EntityManager
-     */
-    private $em;
-
-    /**
-     * Constructor.
-     *
-     * @param EntityManager $em
-     */
-    public function __construct(EntityManager $em)
-    {
-        $this->em = $em;
-    }
-
-    /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -57,6 +41,10 @@ class VoiceQueueType extends AbstractType
                 'query_builder' => function (EntityRepository $er) {
                     return $er->createQueryBuilder('u')->where('u.is_tickets_enabled = 1');
                 },
+            ])
+            ->add('brand', EntityType::class, [
+                'class'    => Brand::class,
+                'required' => false,
             ])
             ->add('agents', VoiceQueueAgentCollectionType::class, [
                 'queue'    => $builder->getData(),
@@ -116,14 +104,6 @@ class VoiceQueueType extends AbstractType
                 'property_path' => 'recordingEnabled',
             ])
         ;
-
-        $brands = $this->em->getRepository(Brand::class)->findAll();
-        if (count($brands) > 1) {
-            $builder->add('brand', EntityType::class, [
-                'class'    => Brand::class,
-                'required' => false,
-            ]);
-        }
 
         $builder->addEventListener(FormEvents::PRE_SUBMIT, [$this, 'onPreSubmit']);
         $builder->addEventListener(FormEvents::POST_SUBMIT, [$this, 'onPostSubmit']);
