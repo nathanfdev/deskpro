e4df388fa7dfd2c84ada8afc3122c9fc44c770bc
ruslanix <ruslanix.com@gmail.com>
2019-06-17 18:28:09 +0300

    DP-3487 Email body (text) is being cut off (8bit charset)

--- app/BUILD/src/Application/DeskPRO/EmailGateway/Reader/EzcReader.php
+++ app/BUILD/src/Application/DeskPRO/EmailGateway/Reader/EzcReader.php
@@ -760,7 +760,7 @@ class EzcReader extends AbstractReader
          * mapping this on UNKNOWN-8BIT charset doesn't help
          * as a fix - just ignore this charset
          */
-        if ($originalCharset == '8bit') {
+        if (strtoupper(trim($originalCharset)) == '8BIT') {
             $originalCharset = null;
         }
         if (!$originalCharset) {
