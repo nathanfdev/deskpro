a3636d6ff9a379db0c9e6cd7d6e3a13c49fe2431
ruslanix <ruslanix.com@gmail.com>
2019-06-25 15:17:06 +0300

    261 Remove old code

--- app/BUILD/src/Application/EmailBundle/SourceMapper/DeskproSourceMapperFactory.php
+++ app/BUILD/src/Application/EmailBundle/SourceMapper/DeskproSourceMapperFactory.php
@@ -6,10 +6,7 @@
 
 namespace Application\EmailBundle\SourceMapper;
 
-use Application\DeskPRO\NewSettings\SettingsResolver;
 use Application\EmailBundle\SourceMapper\EmailRateLimit\EmailRateLimitFactory;
-use Application\EmailBundle\SourceMapper\PendingQueuer\CloudEmailPendingQueuer;
-use Application\EmailBundle\SourceMapper\PendingQueuer\CloudEmailPendingSQSQueuer;
 use Application\EmailBundle\SourceMapper\PendingQueuer\RedisPendingQueuer;
 use Application\EmailBundle\SourceMapper\PendingQueuer\SQSPendingQueuer;
 use Predis;
@@ -23,30 +20,12 @@ use Symfony\Component\DependencyInjection\Container;
  *
  * The default is to queue messages to the DB which get sent on cron.
  *
- * === CLOUD :: Redis Queue ===
+ * === SQS Queue ===
  *
- * The configuration for the redis queue strategy of delivering outgoing email.
- *
- * Most likely you'll want to like the `key` parameter set to the default value below. If you change it, make sure the
- * same value is changed/configured for cloud email consumers.
+ * The url to an sqs queue
  *
  * <code>
- * $SETTINGS['cloudemail_outgoing_redis_queue'] = [
- *     'connection' => [
- *         'scheme' => 'tcp',
- *         'host'   => '10.0.0.1',
- *         'port'   => 6379,
- *     ],
- *     'key' => 'outgoing-queue'
- * ];
- * </code>
- *
- * === CLOUD :: SQS Queue ===
- *
- * The url to an sqs queue which replaces the redis queue.
- *
- * <code>
- * $SETTINGS['cloudemail_outgoing_sqs_queue'] = "https://sqs.<region>.amazonaws.com/<aws_account_id>/outgoing.fifo";
+ * $SETTINGS['settings.sendmail_sqs_queue'] = "https://sqs.<region>.amazonaws.com/<aws_account_id>/outgoing.fifo";
  * </code>
  *
  * === Generic Redis Queue ===
@@ -81,21 +60,7 @@ class DeskproSourceMapperFactory
 
         $env = $container->get('deskpro.app_env');
 
-        if ($redis = $env->getConfig('settings.cloudemail_outgoing_redis_queue')) {
-            $queuer   = CloudEmailPendingQueuer::create($redis['connection'], $redis['key']);
-            $external = new ExternalPendingQueue($source_mapper, $queuer);
-
-            return $external;
-        } elseif ($queueUrl = $env->getConfig('settings.cloudemail_outgoing_sqs_queue')) {
-            /** @var SettingsResolver $resolver */
-            $resolver = $container->get('settings_resolver');
-            $apiKey   = $resolver->getGlobalSettings()->get('api_auth.master_key', '');
-            $queuer   = CloudEmailPendingSQSQueuer::create($queueUrl, $apiKey);
-
-            $external = new ExternalPendingQueue($source_mapper, $queuer);
-
-            return $external;
-        } elseif ($queueUrl = $env->getConfig('settings.sendmail_sqs_queue')) {
+        if ($queueUrl = $env->getConfig('settings.sendmail_sqs_queue')) {
             $queuer = SQSPendingQueuer::create($queueUrl);
 
             $external = new ExternalPendingQueue($source_mapper, $queuer);
