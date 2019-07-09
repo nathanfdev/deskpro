e401728d03b61a7eb8d9148ec9ee136e856eba8e
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-07-09 11:59:04 +0300

    hide voice tickets in the portal

--- app/BUILD/src/Application/DeskPRO/Entity/Ticket.php
+++ app/BUILD/src/Application/DeskPRO/Entity/Ticket.php
@@ -1737,19 +1737,21 @@ class Ticket extends DomainObject implements HighlightableModelInterface, Labels
 
         $now = new \DateTime();
         if ($message->person['is_agent'] && !(defined('DP_INTERFACE') && DP_INTERFACE == 'user')) {
-            if (!$message->is_agent_note && !$this->_is_new) {
+            if (!$message->isAgentNote() && !$this->_is_new) {
                 if (!$this->date_last_agent_reply || $this->date_last_agent_reply < $now) {
                     $this['date_last_agent_reply'] = $now;
                 }
 
-                if (!$this->date_first_agent_reply && !$message->is_agent_note) {
+                if (!$this->date_first_agent_reply && !$message->isAgentNote()) {
                     $this['date_first_agent_reply'] = $now;
                     $this['total_to_first_reply']   = $this->date_first_agent_reply->getTimestamp() - $this->date_created->getTimestamp();
                 }
             }
         } else {
-            if (!$this->date_last_user_reply || $this->date_last_user_reply < $now) {
-                $this['date_last_user_reply'] = $now;
+            if (!$message->isAgentNote()) {
+                if (!$this->date_last_user_reply || $this->date_last_user_reply < $now) {
+                    $this['date_last_user_reply'] = $now;
+                }
             }
         }
 
