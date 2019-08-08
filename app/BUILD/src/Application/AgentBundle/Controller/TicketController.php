e1fab7635c784a1ac1468420b9b2cac25a9f705e
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-08-08 23:27:05 +0300

    fix missing creation system

--- app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
+++ app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
@@ -4574,7 +4574,9 @@ class TicketController extends AbstractController
             $this->em->rollback();
             throw $e;
         }
+
         $newTicket->setParentTicket($ticket);
+        $newTicket->setCreationSystem(Entity\TicketMessage::CREATED_WEB_AGENT_PORTAL);
 
         /** @var Ticket $oldTicket */
         $oldTicket = $ticket;
