a40fd0ad326e0ac65cca584d4f38a338edb7eb88
Vladimir Kirejko <vladimir.kirejko@deskpro.com>
2019-08-10 16:35:34 +0300

    [ch1164] Copy attachment with blob.

--- app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
+++ app/BUILD/src/Application/AgentBundle/Controller/TicketController.php
@@ -4640,9 +4640,9 @@ class TicketController extends AbstractController
             $ticket->setAgentTeam(null);
         }
 
-        $account = $this->getAccount($ticket);
-
-        $context = $ticketManager->createAgentExecutorContext($this->person, 'forward', 'web');
+        $account     = $this->getAccount($ticket);
+        $context     = $ticketManager->createAgentExecutorContext($this->person, 'forward', 'web');
+        $blobStorage = $this->get('blob.storage');
 
         foreach ($messages as $message) {
             /** @var TicketMessage $messageCopy */
@@ -4651,7 +4651,28 @@ class TicketController extends AbstractController
             $messageCopy->setTicket($ticket);
 
             foreach ($message->getAttachments() as $attachment) {
-                $messageCopy->addAttachment($attachment);
+                /** @var TicketAttachment $attachmentCopy */
+                /** @var Blob $blob */
+
+                $attachmentCopy = clone $attachment;
+                $blob           = $attachment->getBlob();
+                $rawFile        = $blobStorage->copyBlobRecordToString($blob);
+                $blobCopy       = $blobStorage
+                    ->createBlobRecordFromString(
+                        $rawFile,
+                        $blob->getFileName(),
+                        $blob->getContentType(),
+                        ['tag' => DeskproBlobStorage::TAG_TICKET_ATTACHMENT]
+                    )
+                    ->setIsTemp(false);
+
+                $attachmentCopy
+                    ->setId(null)
+                    ->setMessage($messageCopy)
+                    ->setBlob($blobCopy)
+                    ->setTicket($ticket);
+
+                $messageCopy->addAttachment($attachmentCopy);
             }
 
             $this->em->persist($messageCopy);
