c757edea05ab1ac1bb690b47c95dbba93951195d
Christopher Nadeau <chris.nadeau@deskpro.com>
2019-08-13 10:30:59 +0100

    regen build manifest

--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -1393,6 +1393,18 @@ return [
         'skipPostBuild' => false,
         'isOnlineBuild' => true,
     ],
+    1565605219 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/08/Build1565605219.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1565605219',
+        'skipPostBuild' => true,
+        'isOnlineBuild' => false,
+    ],
+    1565605220 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/08/Build1565605220.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1565605220',
+        'skipPostBuild' => true,
+        'isOnlineBuild' => true,
+    ],
     1565606453 => [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/08/Build1565606453.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1565606453',
@@ -1450,18 +1462,6 @@ return [
     1565606462 => [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/08/Build1565606462.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1565606462',
-        'skipPostBuild' => true,
-        'isOnlineBuild' => true,
-    ],
-    1565605219 => [
-        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/08/Build1565605219.php',
-        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1565605219',
-        'skipPostBuild' => true,
-        'isOnlineBuild' => false,
-    ],
-    1565605220 => [
-        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/08/Build1565605220.php',
-        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1565605220',
         'skipPostBuild' => true,
         'isOnlineBuild' => true,
     ],
