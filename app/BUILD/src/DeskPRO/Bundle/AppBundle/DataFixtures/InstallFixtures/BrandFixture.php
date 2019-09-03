b17976d55ad0afae01ab477b6b26c037ecbd25d7
ImmortaL <denis@ranneft.ru>
2019-09-04 00:02:12 +0300

    ch2455 remove debug

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/DataFixtures/InstallFixtures/BrandFixture.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/DataFixtures/InstallFixtures/BrandFixture.php
@@ -63,11 +63,6 @@ class BrandFixture extends AbstractDpFixture implements OrderedFixtureInterface
 
     public function getDefaultThemeId()
     {
-        $var  = $this->container->get('settings_resolver')->getGlobalSettings(true)->get('install.with_theme', 'standard');
-        $var2 = $this->container->get('deskpro.app_env')->getConfig('settings.install.with_theme', 'standard');
-        var_dump(file_get_contents($this->container->get('deskpro.app_env')->getDpRoot().'/config/advanced/config.settings.php'));
-        var_dump('INSTALL_WITH_THEME '.$var, $var2);
-
-        return $var;
+        return $this->container->get('settings_resolver')->getGlobalSettings(true)->get('install.with_theme', 'standard');
     }
 }
