a6b3c7f47b2d878e4205f47fb46921e6bd29f3c0
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-09-17 13:43:12 +0300

    missing upgrade script

---
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2019/09/Build1568646529.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Application\InstallBundle\Upgrade\Build;
+
+class Build1568646529 extends AbstractBuild implements OnlineBuildInterface, SkipPostBuildInterface
+{
+    public function addNewTables()
+    {
+    }
+
+    public function runAlters()
+    {
+        $this->execDbQuery('default', 'ALTER TABLE voice_phone_call_participants ADD data LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\'');
+    }
+
+    public function run()
+    {
+    }
+}
