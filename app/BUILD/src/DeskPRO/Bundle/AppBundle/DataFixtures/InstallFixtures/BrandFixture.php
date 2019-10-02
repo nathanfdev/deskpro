aeca8b7d39e802f096c35b7a51735fc4dfe01755
ImmortaL <denis@ranneft.ru>
2019-10-02 12:41:41 +0300

    typo in theme

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/DataFixtures/InstallFixtures/BrandFixture.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/DataFixtures/InstallFixtures/BrandFixture.php
@@ -64,6 +64,6 @@ class BrandFixture extends AbstractDpFixture implements OrderedFixtureInterface
     public function getDefaultThemeId()
     {
         // For the first approach it should work only until we have ch1043 branch not merged
-        return $this->container->get('settings_resolver')->getGlobalSettings(true)->get('install.with_theme', 'help_center');
+        return $this->container->get('settings_resolver')->getGlobalSettings(true)->get('install.with_theme', 'helpcenter');
     }
 }
