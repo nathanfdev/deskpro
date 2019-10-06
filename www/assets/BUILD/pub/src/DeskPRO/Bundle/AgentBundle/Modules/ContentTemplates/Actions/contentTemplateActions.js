bf0b452e84f1f3056751591db19d7f4c8b284849
Vladimir Kirejko <vladimir.kirejko@deskpro.com>
2019-10-06 14:09:25 +0300

    [ch2921] Fix content setting.

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/ContentTemplates/Actions/contentTemplateActions.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/AgentBundle/Modules/ContentTemplates/Actions/contentTemplateActions.js
@@ -14,7 +14,7 @@ const setFormFields = contentTemplate => (page) => {
       if (name === 'newarticle[labels][]') {
         labels.push(value);
         $('.article-tags input').val(labels.join(','));
-      } else if (name === 'newarticle[content]') {
+      } else if (name === 'newarticle[content]' && page.rte.current) {
         page.rte.current.editor.current.reactEditor.current.editor.setContent(value);
       } else {
         const $el = $(page.form).find(`[name="${name}"]`);
