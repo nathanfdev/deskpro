09b00d4349faff1d4c553b2670418f3c71435924
Ashley Dawson <ashley.dawson@deskpro.com>
2019-10-08 12:18:16 +0100

    Draft blocking and unblocking functionality for DMs

---
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2019/10/Build1570463314.php
@@ -0,0 +1,21 @@
+<?php
+namespace Application\InstallBundle\Upgrade\Build;
+
+class Build1570463314 extends AbstractBuild implements OnlineBuildInterface, SkipPostBuildInterface
+{
+    public function addNewTables()
+    {
+        $this->execDbQuery('default', 'CREATE TABLE direct_message_blocks (id INT AUTO_INCREMENT NOT NULL, during_thread_id INT DEFAULT NULL, person_id INT DEFAULT NULL, by_person_id INT DEFAULT NULL, date_created DATETIME NOT NULL, INDEX IDX_316F1F8B427DFCF3 (during_thread_id), INDEX IDX_316F1F8B217BBB47 (person_id), INDEX IDX_316F1F8BB5BE2AA2 (by_person_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->execDbQuery('default', 'ALTER TABLE direct_message_blocks ADD CONSTRAINT FK_316F1F8B427DFCF3 FOREIGN KEY (during_thread_id) REFERENCES direct_message_threads (id) ON DELETE SET NULL');
+        $this->execDbQuery('default', 'ALTER TABLE direct_message_blocks ADD CONSTRAINT FK_316F1F8B217BBB47 FOREIGN KEY (person_id) REFERENCES people (id) ON DELETE SET NULL');
+        $this->execDbQuery('default', 'ALTER TABLE direct_message_blocks ADD CONSTRAINT FK_316F1F8BB5BE2AA2 FOREIGN KEY (by_person_id) REFERENCES people (id) ON DELETE SET NULL');
+    }
+
+    public function runAlters()
+    {
+    }
+
+    public function run()
+    {
+    }
+}
