939f92e14eb6bfa2bcf7365d6750fc3768e1683e
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-10-15 22:29:45 +0300

    cs cleanup

--- www/assets/BUILD/web/app/Admin/TicketApprovals/Ctrl/TypeEdit.js
+++ www/assets/BUILD/web/app/Admin/TicketApprovals/Ctrl/TypeEdit.js
@@ -10,11 +10,11 @@ define(['Admin/Main/Ctrl/Base'], function(Admin_Ctrl_Base) {
       this.dataService = this.DataService.get('TicketApprovals');
 
       this.typeId = (this.$stateParams.id)
-        ? parseInt(this.$stateParams.id.replace(/^type-(\d+)$/, '$1'))
+        ? parseInt(this.$stateParams.id.replace(/^type-(\d+)$/, '$1'), 10)
         : null;
 
       this.form = {
-        name: '',
+        name:        '',
         description: ''
       };
 
@@ -27,11 +27,11 @@ define(['Admin/Main/Ctrl/Base'], function(Admin_Ctrl_Base) {
     }
 
     initialLoad() {
-      let promises = [];
+      const promises = [];
 
       if (this.typeId) {
-        let promise = this.dataService.loadApprovalTypes(this.typeId)
-          .then(data => {
+        const promise = this.dataService.loadApprovalTypes(this.typeId)
+          .then((data) => {
             this.form.name = data.name;
             this.form.description = data.description;
           });
@@ -47,7 +47,7 @@ define(['Admin/Main/Ctrl/Base'], function(Admin_Ctrl_Base) {
      */
     saveForm() {
       // Growl messages
-      let msgSuccess = this.getRegisteredMessage('approval_type_save_success');
+      const msgSuccess = this.getRegisteredMessage('approval_type_save_success');
       let msgFailure = this.getRegisteredMessage('approval_type_save_failure');
 
       // start spinner
@@ -57,16 +57,13 @@ define(['Admin/Main/Ctrl/Base'], function(Admin_Ctrl_Base) {
       this.dataService.saveApprovalType(this.form, this.typeId)
         .then((response) => {
           // get List controller
-          const listController = this.$scope['TicketApprovalsList'];
+          const listController = this.$scope.TicketApprovalsList;
 
           // add or update approval type
-          switch (true) {
-            case response.status === 201:
-              listController.addType(response.data.data);
-              break;
-            case response.status === 204:
-              listController.updateTypeById(this.typeId, this.form);
-              break;
+          if (response.status === 201) {
+            listController.addType(response.data.data);
+          } else if (response.status === 204) {
+            listController.updateTypeById(this.typeId, this.form);
           }
 
           // stop spinner and show growl message
@@ -105,8 +102,8 @@ define(['Admin/Main/Ctrl/Base'], function(Admin_Ctrl_Base) {
      */
     deleteType(id) {
       // Growl messages
-      let msgSuccess = this.getRegisteredMessage('approval_type_delete_success');
-      let msgFailure = this.getRegisteredMessage('approval_type_delete_failure');
+      const msgSuccess = this.getRegisteredMessage('approval_type_delete_success');
+      const msgFailure = this.getRegisteredMessage('approval_type_delete_failure');
 
       // start spinner
       this.startSpinner('deleting');
@@ -115,7 +112,7 @@ define(['Admin/Main/Ctrl/Base'], function(Admin_Ctrl_Base) {
       this.dataService.deleteApprovalType(id)
         .then(() => {
           // get List controller
-          const listController = this.$scope['TicketApprovalsList'];
+          const listController = this.$scope.TicketApprovalsList;
 
           // remove type from list
           listController.removeTypeById(id);
