9e5fb8d91f4154c35e216369f9c2c8bb52d275e6
Ashley Dawson <ashley.dawson@deskpro.com>
2019-10-23 12:43:46 +0100

    Added command to copy similar phrases that already exist in other language translations and prefix them with new id prefix

---
+++ app/BUILD/src/DeskPRO/Bundle/DevBundle/Command/Lang/CopyIdenticalPhrasesCommand.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace DeskPRO\Bundle\DevBundle\Command\Lang;
+
+use DeskPRO\Bundle\DevBundle\Language\CopyIdenticalPhrases;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class CopyIdenticalPhrasesCommand
+ *
+ * @package DeskPRO\Bundle\DevBundle\Command\Lang
+ */
+class CopyIdenticalPhrasesCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('dpdev:lang:copy-identical-phrases')
+            ->setDescription('Copy any unchanged phrases over to another phrase ID')
+            ->addOption(
+                'locales-dir',
+                'd',
+                InputOption::VALUE_OPTIONAL,
+                'Location of the locales directory',
+                null
+            )
+            ->addOption(
+                'root-phrase-id-prefix',
+                'i',
+                InputOption::VALUE_OPTIONAL,
+                'The root phrase ID prefix, e.g. "helpcenter"',
+                'helpcenter'
+            )
+            ->addOption(
+                'root-language',
+                'l',
+                InputOption::VALUE_OPTIONAL,
+                'The root language, e.g. "en-US" (corresponds to the locale subdirectory)',
+                'en-US'
+            )
+            ->addOption(
+                'language-files',
+                'f',
+                InputOption::VALUE_OPTIONAL,
+                'A comma separated list of target language files, e.g. "user.yml,backend.yml"',
+                'user.yml'
+            )
+            ->addOption(
+                'prepend',
+                'p',
+                InputOption::VALUE_NONE,
+                'Prepends the new ID prefixed translations to the other language files'
+            )
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $kernelRootDir = $this->getContainer()->getParameter('kernel.root_dir');
+
+        $isPrepend     = $input->getOption('prepend');
+        $idPrefix      = $input->getOption('root-phrase-id-prefix');
+        $localesDir    = $input->getOption('locales-dir') ?: "{$kernelRootDir}/../locales";
+        $rootLanguage  = $input->getOption('root-language');
+        $languageFiles = array_map('trim', explode(',', $input->getOption('language-files')));
+
+        $output->writeln("Analysing...");
+
+        $same = $this->getCopyIdenticalPhrasesService()->analyze(
+            $localesDir,
+            $rootLanguage,
+            $languageFiles,
+            $idPrefix
+        );
+
+        foreach ($same as $languageFile => $replacementTuple) {
+            $output->writeln("Replacements for <info>{$languageFile}</info>:");
+
+            /** @var \Symfony\Component\Console\Helper\TableHelper $table */
+            $table = $this->getHelper('table');
+            $table->setHeaders(['Replacement', 'Value', 'Hash']);
+            foreach ($replacementTuple as $hash => $replacement) {
+                list ($_, $addId, $value) = $replacement;
+                $table->addRow([
+                    $addId,
+                    is_string($value) ? $this->truncateString($value) : print_r($value, true),
+                    $hash,
+                ]);
+            }
+            $table->render($output);
+        }
+
+        $output->writeln("Building replacements...");
+
+        $additions = $this->getCopyIdenticalPhrasesService()->buildAdditions(
+            $same,
+            $localesDir,
+            $rootLanguage
+        );
+
+        foreach ($additions as $languageFile => $phrases) {
+            $output->writeln("Additions for <info>{$languageFile}</info>:");
+            $output->writeln("---------");
+            foreach ($phrases as $id => $phrase) {
+                $output->writeln(sprintf(
+                    '%s: %s',
+                    $id,
+                    is_string($phrase) ? $phrase : print_r($phrase, true)
+                ));
+            }
+            $output->writeln("---------");
+        }
+
+        if ($isPrepend) {
+            $output->writeln("Prepending to language files...");
+            $output->writeln("---------");
+
+            $this->getCopyIdenticalPhrasesService()->prependAdditions(
+                $additions,
+                function ($languageFile) use ($output) {
+                    $output->writeln(" + Prepended to <info>{$languageFile}</info>");
+                }
+            );
+
+            $output->writeln("---------");
+        }
+
+        $output->writeln("Done.");
+
+    }
+
+    /**
+     * @return CopyIdenticalPhrases
+     */
+    private function getCopyIdenticalPhrasesService()
+    {
+        return $this->getContainer()->get('dpdev.language.copy_identical_phrases');
+    }
+
+    /**
+     * @param string $string
+     * @param int $length
+     * @return string
+     */
+    private function truncateString($string, $length = 30)
+    {
+        if (strlen($string) <= $length) {
+            return $string;
+        }
+
+        return substr($string, 0, $length).'...';
+    }
+}
---
+++ app/BUILD/src/DeskPRO/Bundle/DevBundle/Language/CopyIdenticalPhrases.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace DeskPRO\Bundle\DevBundle\Language;
+
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class CopyIdenticalPhrases
+ *
+ * @package DeskPRO\Bundle\DevBundle\Language
+ */
+class CopyIdenticalPhrases
+{
+    /**
+     * Group keys
+     */
+    const GROUP_PREFIX = 'prefix';
+    const GROUP_NON_PREFIX = 'non_prefix';
+
+    /**
+     * Find the same phrases within the root language (ID prefix vs. non ID prefix)
+     *
+     * @param string $localesDir
+     * @param string $rootLanguage
+     * @param string[] $languageFiles
+     * @param string $idPrefix
+     * @return array
+     */
+    public function analyze($localesDir, $rootLanguage, array $languageFiles, $idPrefix)
+    {
+        $same = [];
+        $rootLanguageDirPath = $this->buildRootLanguageDirPath($localesDir, $rootLanguage);
+
+        foreach ($languageFiles as $languageFile) {
+            $languageFilepath = "{$rootLanguageDirPath}/{$languageFile}";
+
+            if (!is_readable($languageFilepath)) {
+                throw new \RuntimeException("Language file path {$languageFilepath} is not readable");
+            }
+
+            $phrases = Yaml::parse(file_get_contents($languageFilepath));
+
+            // Hash the values for comparison
+            $values = array_map(
+                [$this, 'buildHashValueTuple'],
+                $phrases
+            );
+
+            // Split into ID prefix/non ID prefix groups
+            $groups = [
+                self::GROUP_PREFIX => [],
+                self::GROUP_NON_PREFIX => [],
+            ];
+            foreach ($values as $key => $value) {
+                if (preg_match("/^{$idPrefix}/", $key)) {
+                    $groups[self::GROUP_PREFIX][$key] = $value;
+                } else {
+                    $groups[self::GROUP_NON_PREFIX][$key] = $value;
+                }
+            }
+
+            // Find the same phrases in non ID prefix group (Big O goes out the window here...)
+            foreach ($groups[self::GROUP_PREFIX] as $prefixKey => $prefixValue) {
+                foreach ($groups[self::GROUP_NON_PREFIX] as $nonPrefixKey => $nonPrefixValue) {
+                    if ($prefixValue[1] === $nonPrefixValue[1]) {
+                        $same[$languageFile][$prefixValue[1]] = [$nonPrefixKey, $prefixKey, $nonPrefixValue[0]];
+                    }
+                }
+            }
+        }
+
+        return $same;
+    }
+
+    /**
+     * @param array $same
+     * @param $localesDir
+     * @param $rootLanguage
+     * @return array
+     */
+    public function buildAdditions(array $same, $localesDir, $rootLanguage)
+    {
+        $additions = [];
+        $languageDirs = $this->getOtherLanguageDirPaths($localesDir, $rootLanguage);
+
+        foreach ($same as $languageFile => $replacementTuples) {
+            foreach ($languageDirs as $languageDir) {
+                $languageFilepath = "{$languageDir}/{$languageFile}";
+
+                if (!is_readable($languageFilepath)) {
+                    continue;
+                }
+
+                $phrases = Yaml::parse(file_get_contents($languageFilepath));
+
+                if (!is_array($phrases)) {
+                    continue;
+                }
+
+                foreach ($replacementTuples as $hash => $replacement) {
+                    foreach ($phrases as $id => $phrase) {
+                        if ($replacement[0] === $id) {
+                            $additions[$languageFilepath][$replacement[1]] = $phrase;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $additions;
+    }
+
+    /**
+     * @param array $additions
+     * @param callable|null $afterPrepend
+     */
+    public function prependAdditions(array $additions, callable $afterPrepend = null)
+    {
+        foreach ($additions as $languageFile => $phrases) {
+            if (!is_writable($languageFile)) {
+                throw new \RuntimeException("The language file \"{$languageFile}\" is not writable.");
+            }
+
+            file_put_contents(
+                $languageFile,
+                Yaml::dump($phrases).PHP_EOL.file_get_contents($languageFile)
+            );
+
+            if ($afterPrepend) {
+                $afterPrepend($languageFile);
+            }
+        }
+    }
+
+    /**
+     * @param mixed $value
+     * @return array
+     */
+    private function buildHashValueTuple($value)
+    {
+        return [$value, md5(serialize($value))];
+    }
+
+    /**
+     * @param string $localesDir
+     * @param string $rootLanguage
+     * @return array
+     */
+    private function getOtherLanguageDirPaths($localesDir, $rootLanguage)
+    {
+        $finder = (new Finder())
+            ->in($localesDir)
+        ;
+
+        $paths = array_map(function (SplFileInfo $dir) {
+            return $dir->getFilename();
+        }, iterator_to_array($finder->directories()));
+
+        $paths = array_filter($paths, function ($name) use ($rootLanguage) {
+            return ($name != $rootLanguage);
+        });
+
+        return array_keys($paths);
+    }
+
+    /**
+     * @param string $localesDir
+     * @param string $rootLanguage
+     * @return string
+     */
+    private function buildRootLanguageDirPath($localesDir, $rootLanguage)
+    {
+        return "{$localesDir}/{$rootLanguage}";
+    }
+}
