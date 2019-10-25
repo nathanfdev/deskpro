1d2f5c2ebfb2a30ed6f922222a908d57d9d78d8f
Ashley Dawson <ashley.dawson@deskpro.com>
2019-10-25 08:55:46 +0100

    Swapped diff colours

--- app/BUILD/src/DeskPRO/Bundle/DevBundle/Command/Lang/PhraseDiffCommand.php
+++ app/BUILD/src/DeskPRO/Bundle/DevBundle/Command/Lang/PhraseDiffCommand.php
@@ -107,7 +107,7 @@ class PhraseDiffCommand extends ContainerAwareCommand
         if (count($phraseDiff)) {
             foreach ($phraseDiff as $id => $diff) {
                 $output->writeln(sprintf(
-                    '<fg=cyan;options=bold> > </><options=bold>%s:</> <fg=green>%s</> -> <fg=red>%s</>',
+                    '<fg=cyan;options=bold> > </><options=bold>%s:</> <fg=red>%s</> -> <fg=green>%s</>',
                     $id,
                     print_r($diff[0], true),
                     print_r($diff[1], true)
