08240fa499a52d28080f7a3301344e2bc9040396
Ashley Dawson <ashley.dawson@deskpro.com>
2019-10-29 14:59:43 +0000

    Refactored log and removed reference to approval template

--- app/BUILD/src/Application/DeskPRO/People/PermissionChecker/TicketChecker.php
+++ app/BUILD/src/Application/DeskPRO/People/PermissionChecker/TicketChecker.php
@@ -91,13 +91,11 @@ class TicketChecker extends AbstractChecker
         foreach ($ticket->getApprovals() as $approval) {
             // If the current user is the ticket owner AND the approval contains the ticket owner as an approver
             // then view access is always allowed irrespective of canApproversViewSubject
-            if ($this->person->isEqualTo($ticket->getPerson())) {
-                if ($approval->hasApprover($this->person)) {
-                    return true;
-                }
+            if ($this->person->isEqualTo($ticket->getPerson()) && $approval->hasApprover($this->person)) {
+                return true;
             }
 
-            if ($approval->getTemplate()->canApproversViewSubject()) {
+            if ($approval->canApproversViewSubject()) {
                 if ($approval->hasApprover($this->person)) {
                     return true;
                 }
