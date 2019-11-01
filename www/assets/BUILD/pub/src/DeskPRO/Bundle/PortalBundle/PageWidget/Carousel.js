b692e8e5e37fbcd5877f5d5a8c22cc4a7eabe20b
Jimmy Ahuja <jimahuja@gmail.com>
2019-11-01 14:51:24 +0530

    Carousel fix - Jimmy

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/Carousel.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/Carousel.js
@@ -5,12 +5,13 @@ import { PageWidget } from 'DeskPRO/Component/PageWidget/PageWidget';
 export class Carousel extends PageWidget {
 
   renderWidget() {
-    $(this.$element).slick({
+    $('.dp-po-news-carousel-list').slick({
       dots:           true,
       infinite:       false,
       speed:          300,
       slidesToShow:   4,
       slidesToScroll: 4,
+      rows: 0,
       nextArrow:      '<button class="dp-po-news-carousel-arrow dp-po-news-carousel-arrow-right"><i class="dp-po-icon far fa-angle-right"></i></button>',
       prevArrow:      '<button class="dp-po-news-carousel-arrow dp-po-news-carousel-arrow-left"><i class="dp-po-icon far fa-angle-left"></i></button>',
       responsive:     [{
