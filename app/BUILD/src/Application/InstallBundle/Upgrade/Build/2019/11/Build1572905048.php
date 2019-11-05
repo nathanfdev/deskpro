8035fd7af7d28bbc7a9488681c1d82b1e791a586
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-11-05 10:55:33 +0300

    missing PT-br migration

---
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2019/11/Build1572905048.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Application\InstallBundle\Upgrade\Build;
+
+class Build1572905048 extends AbstractBuild implements OnlineBuildInterface
+{
+    public function addNewTables()
+    {
+    }
+
+    public function runAlters()
+    {
+    }
+
+    public function run()
+    {
+        $oldLanguage = $this->getDbConnection()->fetchAll("SELECT id FROM `languages` WHERE sys_name = 'portuguese'");
+        if (!$oldLanguage) {
+            return;
+        }
+
+        $newLanguage = $this->getDbConnection()->fetchAll("SELECT id FROM `languages` WHERE sys_name ='portuguese_pt'");
+        if ($newLanguage) {
+            // we need to update relations and remove them
+            $oldLangId = $oldLanguage[0]['id'];
+            $newLangId = $newLanguage[0]['id'];
+
+            $tables = [
+                'people',
+                'phrases',
+                'news',
+                'articles',
+                'downloads',
+                'topics',
+                'community_topics',
+                'object_lang',
+                'tickets',
+                'tickets_search_active',
+                'snippet_changelog',
+                'snippet_use_log',
+                'snippet_translations',
+            ];
+
+            // fix all recent content to use the same language, set the old lang
+            foreach ($tables as $table) {
+                $this->execDbQuery('default', "UPDATE IGNORE `$table` SET language_id = $oldLangId WHERE language_id = $newLangId");
+            }
+
+            $this->execDbQuery('default', "DELETE FROM `languages` WHERE sys_name = 'portuguese_pt'");
+        }
+
+        // replace the old language with the new format
+        $this->execDbQuery('default', "UPDATE `languages` SET sys_name = 'portuguese_pt', title = 'Português (Europeu)', base_filepath = '%DP_ROOT%/locales/pt-PT', locale = 'pt-PT', plural_categories = 'one,other', plural_formula = 'n != 1', flag_image = 'locale_pt-PT.png' WHERE sys_name = 'portuguese'");
+    }
+}
--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -1573,4 +1573,10 @@ return [
         'skipPostBuild' => false,
         'isOnlineBuild' => true,
     ],
+    1572905048 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/11/Build1572905048.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1572905048',
+        'skipPostBuild' => false,
+        'isOnlineBuild' => true,
+    ],
 ];
