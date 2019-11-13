c2a56f2f97ee096cb56241f8574e5d4d77ccb292
Julien Ducro <julien.ducro@deskpro.com>
2019-11-13 16:41:36 +0100

    [ch4750] Default sort of tickets in table view should be Last Action DESC

--- app/BUILD/sys/config/config.php
+++ app/BUILD/sys/config/config.php
@@ -203,6 +203,7 @@ $container->loadFromExtension(
                     'dql' => [
                         'string_functions' => [
                             'group_concat' => 'DoctrineExtensions\Query\Mysql\GroupConcat',
+                            'greatest'     => 'DoctrineExtensions\Query\Mysql\Greatest',
                         ],
                     ],
                     'mappings' => [
