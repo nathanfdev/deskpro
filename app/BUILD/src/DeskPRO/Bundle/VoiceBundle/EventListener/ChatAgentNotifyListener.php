568cef842bac7ba4cb401fbb57950111945735fa
Sergey Yastrebov <serg.yastrebov@gmail.com>
2019-11-29 14:48:14 +0300

    auto-assign chat when round robin is set

--- app/BUILD/src/DeskPRO/Bundle/VoiceBundle/EventListener/ChatAgentNotifyListener.php
+++ app/BUILD/src/DeskPRO/Bundle/VoiceBundle/EventListener/ChatAgentNotifyListener.php
@@ -2,6 +2,8 @@
 
 namespace DeskPRO\Bundle\VoiceBundle\EventListener;
 
+use Application\DeskPRO\Entity\Person;
+use DeskPRO\Bundle\AppBundle\Entity\UserChatQueue;
 use DeskPRO\Bundle\AppBundle\Notification\Event\UserChat\UserChatEvent as ChatNotificationEvent;
 use DeskPRO\Bundle\AppBundle\Serializer\Sideload\SideloadSerializationContext;
 use DeskPRO\Bundle\AppBundle\UserChat\UserChatEvent;
@@ -9,6 +11,7 @@ use DeskPRO\Bundle\VoiceBundle\Event\TaskRouterEvent;
 use DeskPRO\Bundle\VoiceBundle\Helper\ChatTaskHelper;
 use DeskPRO\Bundle\VoiceBundle\TaskRouter\Model\Worker;
 use DeskPRO\Bundle\VoiceBundle\TaskRouter\StorageAdapter\StorageAdapterInterface;
+use Doctrine\ORM\EntityManager;
 use JMS\Serializer\Serializer;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -18,6 +21,11 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class ChatAgentNotifyListener implements EventSubscriberInterface
 {
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
     /**
      * @var ChatTaskHelper
      */
@@ -41,17 +49,20 @@ class ChatAgentNotifyListener implements EventSubscriberInterface
     /**
      * Constructor.
      *
+     * @param EntityManager            $em
      * @param ChatTaskHelper           $taskHelper
      * @param Serializer               $serializer
      * @param StorageAdapterInterface  $storage
      * @param EventDispatcherInterface $dispatcher
      */
     public function __construct(
+        EntityManager            $em,
         ChatTaskHelper           $taskHelper,
         Serializer               $serializer,
         StorageAdapterInterface  $storage,
         EventDispatcherInterface $dispatcher
     ) {
+        $this->em         = $em;
         $this->taskHelper = $taskHelper;
         $this->serializer = $serializer;
         $this->storage    = $storage;
@@ -85,6 +96,26 @@ class ChatAgentNotifyListener implements EventSubscriberInterface
         $chat = $this->taskHelper->getChat($task);
         if (!$chat) {
             return;
+        }
+
+        $chatQueue = $this->taskHelper->getChatQueue($task);
+        if ($chatQueue->getRoutingModel() === UserChatQueue::ROUTING_MODEL_ROUND_ROBIN) {
+            // if it's round robin then assign the chat directly to the agent
+            if ($task->getWorkerIds()) {
+                $worker = $this->storage->getWorker($task->getWorkerIds()[0]);
+                if ($worker) {
+                    $agent = $this->em->getRepository(Person::class)->find($worker->getTypeId());
+                    if ($agent) {
+                        $chat->setAgent($agent);
+                        $this->em->flush();
+
+                        $this->dispatcher->dispatch(UserChatEvent::STARTED, new UserChatEvent($chat));
+                        $this->dispatcher->dispatch(UserChatEvent::ASSIGNED, new UserChatEvent($chat, [
+                            'name' => $chat->getAgent()->getDisplayName(),
+                        ]));
+                    }
+                }
+            }
         }
 
         $data = array_merge(
