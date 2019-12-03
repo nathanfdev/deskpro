d2123b308aa65b0ce6c57abb5d60575774b562e4
Christopher Nadeau <chris@nadeau.tv>
2019-12-03 10:46:37 +0000

    fix some build issues

--- app/BUILD/src/DeskPRO/Bundle/ReportBundle/Dpql2/build-parser.php
+++ app/BUILD/src/DeskPRO/Bundle/ReportBundle/Dpql2/build-parser.php
@@ -8,31 +8,30 @@ ini_set('display_errors', true);
 
 chdir(__DIR__);
 
-register_shutdown_function('shutdown_function');
-
 $_SERVER['argv'] = [basename(__FILE__), 'Parser.y'];
 $_SERVER['argc'] = 2;
 
 if (!file_exists('PHP/ParserGenerator.php')) {
-    echo 'ParserGenerator is not installed';
-    exit;
-}
+    echo "ParserGenerator is not installed\n";
+} else {
+    register_shutdown_function('shutdown_function');
 
-require_once 'PHP/ParserGenerator.php';
-$me = new PHP_ParserGenerator();
-$me->main(); // this calls exit so need to hack around that with a shutdown function
+    require_once 'PHP/ParserGenerator.php';
+    $me = new PHP_ParserGenerator();
+    $me->main(); // this calls exit so need to hack around that with a shutdown function
 
-function shutdown_function()
-{
-    $prefixCode = 'namespace DeskPRO\Bundle\ReportBundle\Dpql2;
+    function shutdown_function()
+    {
+        $prefixCode = 'namespace DeskPRO\Bundle\ReportBundle\Dpql2;
 
-use DeskPRO\Bundle\ReportBundle\Dpql2\Statement\DpqlStatementFactory;
-';
+    use DeskPRO\Bundle\ReportBundle\Dpql2\Statement\DpqlStatementFactory;
+    ';
 
-    $contents = file_get_contents('Parser.php');
-    $contents = preg_replace('#^(<\?php)#i', '$1'."\n$prefixCode", $contents);
-    $contents = preg_replace('#(implements\s+)(ArrayAccess)#i', '$1\\\\$2', $contents);
-    file_put_contents('Parser.php', $contents);
+        $contents = file_get_contents('Parser.php');
+        $contents = preg_replace('#^(<\?php)#i', '$1' . "\n$prefixCode", $contents);
+        $contents = preg_replace('#(implements\s+)(ArrayAccess)#i', '$1\\\\$2', $contents);
+        file_put_contents('Parser.php', $contents);
 
-    echo 'Parser build complete.'.PHP_EOL;
+        echo 'Parser build complete.' . PHP_EOL;
+    }
 }
