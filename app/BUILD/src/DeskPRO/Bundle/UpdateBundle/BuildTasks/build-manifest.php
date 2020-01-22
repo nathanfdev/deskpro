a5124c9788b681de2a11684d524ac782b5e93275
ruslanix <ruslanix.com@gmail.com>
2020-01-22 10:05:58 +0300

    ch4246 merge

--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -1699,6 +1699,12 @@ return [
         'skipPostBuild' => true,
         'isOnlineBuild' => true,
     ],
+    1577707394 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/12/Build1577707394.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1577707394',
+        'skipPostBuild' => true,
+        'isOnlineBuild' => true,
+    ],
     1578475520 => [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2020/01/Build1578475520.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1578475520',
@@ -1720,12 +1726,6 @@ return [
     1579079410 => [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2020/01/Build1579079410.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1579079410',
-        'skipPostBuild' => true,
-        'isOnlineBuild' => true,
-    ],
-    1577707394 => [
-        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2019/12/Build1577707394.php',
-        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1577707394',
         'skipPostBuild' => true,
         'isOnlineBuild' => true,
     ],
