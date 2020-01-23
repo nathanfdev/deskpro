b1b9b620ac6e78378095183fe5763afdb673673d
Julien Ducro <julien.ducro@deskpro.com>
2020-01-23 11:08:00 +0000

    Add finally polyfill

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/DeskPRO_HelpcenterBundle.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/DeskPRO_HelpcenterBundle.js
@@ -4,6 +4,38 @@ import { helpcenterApp } from './HelpcenterApp';
 import $ from 'jquery';
 import { addLocaleData } from 'react-intl';
 
+(function () {
+  // based on https://github.com/matthew-andrews/Promise.prototype.finally
+
+  // Get a handle on the global object
+  let globalObject;
+  if (typeof global !== 'undefined') {
+    globalObject = global;
+  } else if (typeof window !== 'undefined' && window.document) {
+    globalObject = window;
+  }
+
+  // check if the implementation is available
+  if (typeof Promise.prototype['finally'] === 'function') {
+    return;
+  }
+
+  // implementation
+  globalObject.Promise.prototype['finally'] = function (callback) {
+    const constructor = this.constructor;
+
+    return this.then(function (value) {
+      return constructor.resolve(callback()).then(function () {
+        return value;
+      });
+    }, function (reason) {
+      return constructor.resolve(callback()).then(function () {
+        throw reason;
+      });
+    });
+  };
+}());
+
 import('./hc-lazy-inc').then(function() {
   $(document).ready(function() {
     const toolOptions = {
