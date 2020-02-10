1f585123b58b605eef1beb8088d184ba8f9e6814
ImmortaL <denis@ranneft.ru>
2020-02-10 17:19:17 +0200

    [ch8933] new ticket service for messenger bundle to handle new tickets correctly

--- app/BUILD/src/DeskPRO/Bundle/MessengerBundle/Controller/TicketController.php
+++ app/BUILD/src/DeskPRO/Bundle/MessengerBundle/Controller/TicketController.php
@@ -4,18 +4,18 @@ namespace DeskPRO\Bundle\MessengerBundle\Controller;
 
 use Application\DeskPRO\Entity\Person;
 use Application\DeskPRO\Entity\Ticket;
-use Application\DeskPRO\EntityRepository\Person as PersonRepository;
-use Application\DeskPRO\Tickets\ExecutorContext;
+use Application\DeskPRO\People\PersonGuest;
 use DeskPRO\Bundle\ApiBundle\ApiDoc\Annotation\ApiDoc;
 use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\ApiModes;
 use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\ApiUserContext;
 use DeskPRO\Bundle\AppBundle\Annotation\ActionPermissions\Annotation\Feature;
 use DeskPRO\Bundle\AppBundle\Form\Error\Exception\InvalidFormException;
-use DeskPRO\Bundle\AppBundle\Form\Type\Tickets\TicketWithLayouts\TicketWithLayoutsApiFullType;
+use DeskPRO\Bundle\AppBundle\Form\Type\Tickets\TicketWithLayouts\TicketWithLayoutsApiType;
 use DeskPRO\Bundle\AppBundle\Form\Type\Tickets\TicketWithLayouts\TicketWithLayoutsContext;
 use DeskPRO\Bundle\AppBundle\Serializer\ApiWrapper;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -52,48 +52,64 @@ class TicketController extends AbstractMessengerController
      */
     public function createTicketAction(Request $request)
     {
-        $ticket = new Ticket();
-
-        $requestData = $request->request->all();
-        $formOptions = [
-            'attachAuth'          => true,
+        $newTicketService = $this->get('tickets.new_ticket');
+        $ticket           = $newTicketService->createNewTicket(
+            $request,
+            $this->getVisitorId($request),
+            $this->getUser(),
+            $this->get('brand_stack')->getActive()->getBrand(),
+            Ticket::CREATED_WEB_PERSON_WIDGET
+        );
+        $person           = $ticket->getPerson();
+        $formOptions      = [
+            'person'              => $person,
             'csrf_protection'     => false,
             'ticket_view_context' => TicketWithLayoutsContext::VIEW_USER,
             'ticket_visibility'   => TicketWithLayoutsContext::VISIBILITY_NEW,
         ];
 
-        // try to find person
-        /** @var PersonRepository $personRepository */
-        $personRepository = $this
-            ->get('doctrine.orm.default_entity_manager')
-            ->getRepository(Person::class);
-        $person = $this->getUser();
-
-        if (!$person && isset($requestData['person']) && isset($requestData['person']['email'])) {
-            $person = $personRepository->findOneByEmail($requestData['person']['email']);
-        }
-
-        $requestData['message'] = ['message' => $requestData['message'], 'format' => 'html'];
-
-        $person                = $person ?: new Person();
-        $formOptions['person'] = $person;
-
         $form = $this->container->get('form.factory')->create(
-            TicketWithLayoutsApiFullType::class,
+            TicketWithLayoutsApiType::class,
             $ticket,
             $formOptions
         );
 
-        $form->submit($requestData, false);
+        $form->handleRequest($request);
         if (!$form->isValid()) {
             throw new InvalidFormException($form);
         }
+        $person = $ticket->getPerson();
 
-        $manager = $this->getContainer()->getTicketManager();
-        $context = $manager->createUserExecutorContext($person, ExecutorContext::EVENT_NEW, ExecutorContext::METHOD_API, ['api_v2' => true]);
-        $this->get('doctrine.orm.default_entity_manager')->persist($person);
-        $manager->saveTicket($ticket, $context);
+        $email     = $person->getPrimaryEmail();
+        $person    = $this->get('data.person')->getPersonForEmail($email->getEmail());
+        $guestForm = $this->createForm(TicketWithLayoutsApiType::class, $ticket, $formOptions);
 
-        return View::create(new ApiWrapper($ticket));
+        if ($person) {
+            $ticket->setPerson($person);
+            $guestForm->handleRequest($request);
+
+            if (!$this->getUser() || $this->getUser() instanceof PersonGuest) {
+                // if the user is not authorized then don't allow to change person entity
+                $this->getManager()->getUnitOfWork()->clearEntityChangeSet(spl_object_hash($ticket->getPerson()));
+                // if the user is not authorized then don't allow to change person entity email
+                // form configured to set email to `primary_email` field
+                if ($person->getPrimaryEmail()) {
+                    $this->getManager()->getUnitOfWork()->clearEntityChangeSet(spl_object_hash($person->getPrimaryEmail()));
+                }
+            }
+
+            $newTicketService->acceptNewTicket($ticket, $request, 'widget');
+        } else {
+            $newTicketService->acceptNewTicketForGuest($ticket, $request, $guestForm, 'widget');
+        }
+
+        // check if ticket was created and then return success response
+        if ($ticket->getId()) {
+            return View::create(new ApiWrapper($ticket));
+        } else {
+            $form->addError(new FormError('Unable to save ticket.'));
+
+            throw new InvalidFormException($form);
+        }
     }
 }
