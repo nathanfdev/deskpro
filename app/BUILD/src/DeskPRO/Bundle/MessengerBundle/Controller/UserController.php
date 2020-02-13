c8be92b11248f4f9fd5ebb5ec5f38cb3d247f9c5
ImmortaL <denis@ranneft.ru>
2020-02-13 07:24:40 +0200

    fix jwt user only disallows edit an email

--- app/BUILD/src/DeskPRO/Bundle/MessengerBundle/Controller/UserController.php
+++ app/BUILD/src/DeskPRO/Bundle/MessengerBundle/Controller/UserController.php
@@ -64,7 +64,7 @@ class UserController extends AbstractMessengerController
         $chatConversationRepo = $em->getRepository(ChatConversation::class);
         $chats                = $chatConversationRepo->findBy(['visitor_id' => $visitorId], ['date_created' => 'DESC'], 25);
 
-        $userInfo = new UserInfo($visitorId, $this->getJwtUser($request));
+        $userInfo = new UserInfo($visitorId, $this->getUser());
 
         return View::create($this->wrap($userInfo->addChats($chats)), Response::HTTP_OK);
     }
