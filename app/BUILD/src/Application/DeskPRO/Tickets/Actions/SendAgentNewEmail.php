e973114944dd8d5250728659130d85bdc9569690
Julien Ducro <julien.ducro@deskpro.com>
2020-02-28 18:03:35 +0000

    [ch9598] Fix embedded images for new tickets

--- app/BUILD/src/Application/DeskPRO/Tickets/Actions/SendAgentNewEmail.php
+++ app/BUILD/src/Application/DeskPRO/Tickets/Actions/SendAgentNewEmail.php
@@ -218,25 +218,28 @@ class SendAgentNewEmail extends AbstractEmailAction implements ActionInterface, 
         $changedParticipants = $state->hasChangedField('participants');
         $changedStatus       = $state->hasChangedField('status');
 
+        /** @var \Application\DeskPRO\EntityRepository\TicketMessage $messageRepo */
+        $messageRepo = $this->getContainer()->getEm()->getRepository(TicketMessage::class);
+        $messages    = $messageRepo->getTicketMessages(
+            $ticket,
+            [
+                'with_notes'       => false,
+                'with_attachments' => true,
+                'limit'            => 15,
+                'order'            => 'DESC',
+            ]
+        );
         switch ($context->getEventType()) {
             case TicketTrigger::EVENT_TYPE_NEWTICKET:
+                if ($messages) {
+                    $lastMessage = array_shift($messages);
+                }
             case TicketTrigger::EVENT_TYPE_UPDATE:
             case 'system':
                 $arguments = [$ticket];
 
                 break;
             case TicketTrigger::EVENT_TYPE_NEWREPLY:
-                /** @var \Application\DeskPRO\EntityRepository\TicketMessage $messageRepo */
-                $messageRepo = $this->getContainer()->getEm()->getRepository(TicketMessage::class);
-                $messages    = $messageRepo->getTicketMessages(
-                    $ticket,
-                    [
-                        'with_notes'       => false,
-                        'with_attachments' => true,
-                        'limit'            => 15,
-                        'order'            => 'DESC',
-                    ]
-                );
                 if ($messages) {
                     $lastMessage = array_shift($messages);
                     $arguments   = [$ticket, $lastMessage];
@@ -308,7 +311,7 @@ class SendAgentNewEmail extends AbstractEmailAction implements ActionInterface, 
             $vars = [];
 
             if (isset($lastMessage)) {
-                $vars['attached_blobs'] = $this->getLastMessageAttachments($ticket, $lastMessage, $context, $isAuto);
+                $vars['attached_blobs'] = $this->getLastMessageAttachments($ticket, $lastMessage, $context);
             }
 
             $message = $ticketEmail->prepareMailerMessage($vars, false);
