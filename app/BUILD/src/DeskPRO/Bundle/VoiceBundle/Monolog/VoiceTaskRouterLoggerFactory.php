81840721d937c28a0c27dbd1de0705b3da3c37eb
Sergey Yastrebov <serg.yastrebov@gmail.com>
2020-03-20 01:50:52 +0300

    [ch10647] syslog support in voice logs

--- app/BUILD/src/DeskPRO/Bundle/VoiceBundle/Monolog/VoiceTaskRouterLoggerFactory.php
+++ app/BUILD/src/DeskPRO/Bundle/VoiceBundle/Monolog/VoiceTaskRouterLoggerFactory.php
@@ -2,8 +2,10 @@
 
 namespace DeskPRO\Bundle\VoiceBundle\Monolog;
 
+use DpSys\LowError\SystemErrorHandler;
 use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogHandler;
 use Monolog\Logger;
 use Monolog\Processor\UidProcessor;
 use Psr\Log\LoggerInterface;
@@ -26,7 +28,12 @@ class VoiceTaskRouterLoggerFactory
 
         $env = $container->get('deskpro.app_env');
         if ($env->isQa() || $env->getConfig('logs.enable_voice_log')) {
-            $handler = new StreamHandler($env->getUserLogsDir().'/task_router.log');
+            if (SystemErrorHandler::useSyslog()) {
+                $handler = new SyslogHandler('deskpro-task-router');
+            } else {
+                $handler = new StreamHandler($env->getUserLogsDir().'/task_router.log');
+            }
+
             $handler->setFormatter(new LineFormatter(null, 'Y-m-d H:i:s.u'));
 
             $logger->pushHandler($handler);
