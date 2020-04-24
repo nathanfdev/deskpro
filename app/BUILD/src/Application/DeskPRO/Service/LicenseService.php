3953da16c8843ff63cee84d98c2b34b6ea657de9
ruslanix <ruslanix.com@gmail.com>
2020-04-24 12:12:50 +0300

    11960 Display news posts from 'Release Announcements' category in Admin Dashboard news feed

--- app/BUILD/src/Application/DeskPRO/Service/LicenseService.php
+++ app/BUILD/src/Application/DeskPRO/Service/LicenseService.php
@@ -92,34 +92,41 @@ class LicenseService
                 RequestOptions::VERIFY => false,
             ]);
 
-            try {
-                $response = $client->get('/news/2-product.rss');
-            } catch (ClientException $e) {
-                return;
-            }
+            foreach (['/news/2-product.rss', '/news/release-announcements.rss'] as $url) {
+                try {
+                    $response = $client->get($url);
+                } catch (ClientException $e) {
+                    return;
+                }
 
-            $rss = @simplexml_load_string((string) $response->getBody());
-            unset($r);
+                $rss = @simplexml_load_string((string) $response->getBody());
 
-            if (!$rss || empty($rss) || empty($rss->channel->item)) {
-                return;
-            }
-
-            $x = 0;
-            foreach ($rss->channel->item as $item) {
-                $news[] = [
-                    'title' => (string) $item->title,
-                    'link'  => (string) $item->link,
-                ];
-                if ($x++ > 5) {
+                if (!$rss || empty($rss) || empty($rss->channel->item)) {
                     break;
+                }
+
+                $x = 0;
+                foreach ($rss->channel->item as $item) {
+                    $news[] = [
+                        'title' => (string) $item->title,
+                        'link'  => (string) $item->link,
+                        'pubDate' => @strtotime($item->pubDate),
+                        'date' => (string) $item->pubDate
+                    ];
+                    if ($x++ > 5) {
+                        break;
+                    }
                 }
             }
         } catch (\Exception $e) {
             return;
         }
 
-        return $news;
+        usort($news, function($a, $b)  {
+            return ($a['pubDate'] >= $b['pubDate']) ? -1 : 1;
+        });
+
+        return array_slice($news, 0, 7);
     }
 
     /**
