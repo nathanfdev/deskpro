a8cf2bdd6dbd5814be118877e3cdfb9261d658eb
ImmortaL <denis@ranneft.ru>
2020-05-01 00:05:30 +0300

    [ch11614] remove temp

--- app/BUILD/src/DeskPRO/Bundle/MessengerBundle/Admin/Controller/AdminController.php
+++ app/BUILD/src/DeskPRO/Bundle/MessengerBundle/Admin/Controller/AdminController.php
@@ -299,6 +299,7 @@ CODE;
         ) {
             $messengerChat->setNoAnswerBehavior(MessengerChat::NO_ANSWER_SAVE_TICKET);
         }
+
         $this
             ->getSettingRepository()
 
@@ -307,7 +308,7 @@ CODE;
             ->updateSetting(MSR::WIDGET_BG_COLOR, $messengerWidget->getBackgroundColor(), $brand)
             ->updateSetting(MSR::WIDGET_TEXT_COLOR, $messengerWidget->getTextColor(), $brand)
             ->updateSetting(MSR::WIDGET_POSITION, $messengerWidget->getPosition(), $brand)
-            ->updateSetting(MSR::WIDGET_ICON, $messengerWidget->getIcon()->getId(), $brand)
+            ->updateSetting(MSR::WIDGET_ICON, $messengerWidget->getIcon()->setIsTemp(false)->getId(), $brand)
 
             // Chat settings
             ->updateSetting(MSR::CHAT_ENABLED, $messengerChat->isEnabled(), $brand)
@@ -350,7 +351,8 @@ CODE;
             ->updateSetting(MSR::EMBED_AUTHORIZE_DOMAINS, $messengerEmbed->getAuthorizeDomains(), $brand)
             ->updateSetting(MSR::EMBED_ENABLED_ON_PORTAL, $messengerEmbed->isShowOnPortal(), $brand)
             ->updateSetting(MSR::JWT_SECRET, $messengerEmbed->getJwtSecret(), $brand)
-
         ;
+
+        $this->container->get('doctrine.orm.default_entity_manager')->flush();
     }
 }
