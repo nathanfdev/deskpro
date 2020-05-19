6e180e5e9e1c82a1d0161305f3cec7936710662c
Julien Ducro <julien.ducro@deskpro.com>
2020-05-19 11:00:42 +0200

    Phrases updates

--- app/BUILD/src/DeskPRO/Bundle/PortalBundle/Controller/CommunityTopicsController.php
+++ app/BUILD/src/DeskPRO/Bundle/PortalBundle/Controller/CommunityTopicsController.php
@@ -643,7 +643,7 @@ class CommunityTopicsController extends AbstractPublishController
     public function communityRateAction(Request $request, CommunityTopic $topic, $visitor_id, $up_or_down)
     {
         if (!$this->isGranted('USE_COMMUNITY')) {
-            throw $this->createAccessDeniedException($this->phrase(['portal.community.module_forbidden', 'helpcenter.community.module_forbidden']));
+            throw $this->createNotFoundException();
         }
         if (!$this->isGranted('RATE_COMMUNITY', $topic)) {
             if ($this->getUser()) {
@@ -652,7 +652,7 @@ class CommunityTopicsController extends AbstractPublishController
             if ($request->getContentType() == 'json') {
                 return new JsonResponse(
                     [
-                        'error'    => $this->phrase(['portal.community.error_login', 'helpcenter.community.error_login']),
+                        'error'    => 'need login',
                         'redirect' => $this->generateUrl('portal_login', [
                             '_destination' => $this->generateUrl('portal_community_topic_view', ['slug' => $topic->getSlug()]),
                         ]),
@@ -665,7 +665,7 @@ class CommunityTopicsController extends AbstractPublishController
             }
         }
         if (!$topic->isVisibleOnPortal()) {
-            throw $this->createNotFoundException($this->phrase(['portal.community.error_hidden', 'helpcenter.community.error_hidden']));
+            throw $this->createNotFoundException();
         }
 
         $person = $this->isGranted('ROLE_USER') ? $this->getUser() : null;
