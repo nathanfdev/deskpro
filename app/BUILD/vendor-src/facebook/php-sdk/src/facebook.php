b61e7812404a6af42ab498fab245e67cdfa2b64d
Sergey Yastrebov <serg.yastrebov@gmail.com>
2020-06-17 15:28:47 +0300

    [ch14886] fix FB login

--- app/BUILD/vendor-src/facebook/php-sdk/src/facebook.php
+++ app/BUILD/vendor-src/facebook/php-sdk/src/facebook.php
@@ -23,95 +23,120 @@ require_once "base_facebook.php";
  */
 class Facebook extends BaseFacebook
 {
-  const FBSS_COOKIE_NAME = 'fbss';
+    const FBSS_COOKIE_NAME = 'fbss';
 
-  // We can set this to a high number because the main session
-  // expiration will trump this.
-  const FBSS_COOKIE_EXPIRE = 31556926; // 1 year
+    // We can set this to a high number because the main session
+    // expiration will trump this.
+    const FBSS_COOKIE_EXPIRE = 31556926; // 1 year
 
-  // Stores the shared session ID if one is set.
-  protected $sharedSessionID;
+    // Stores the shared session ID if one is set.
+    protected $sharedSessionID;
 
-	/**
-	 * @var \Orb\Auth\StateHandler\StateHandlerInterface
-	 */
-	protected $state_handler;
+    /**
+     * @var \Orb\Auth\StateHandler\StateHandlerInterface
+     */
+    protected $state_handler;
 
-  /**
-   * Identical to the parent constructor, except that
-   * we start a PHP session to store the user ID and
-   * access token if during the course of execution
-   * we discover them.
-   *
-   * @param Array $config the application configuration. Additionally
-   * accepts "sharedSession" as a boolean to turn on a secondary
-   * cookie for environments with a shared session (that is, your app
-   * shares the domain with other apps).
-   * @see BaseFacebook::__construct in facebook.php
-   */
-  public function __construct($config, \Orb\Auth\StateHandler\StateHandlerInterface $state) {
-	  $this->state_handler = $state;
-      parent::__construct($config);
-  }
-
-  protected static $kSupportedKeys =
-    array('state', 'code', 'access_token', 'user_id');
-
-  /**
-   * Provides the implementations of the inherited abstract
-   * methods.  The implementation uses PHP sessions to maintain
-   * a store for authorization codes, user ids, CSRF states, and
-   * access tokens.
-   */
-  protected function setPersistentData($key, $value) {
-    if (!in_array($key, self::$kSupportedKeys)) {
-      self::errorLog('Unsupported key passed to setPersistentData.');
-      return;
+    /**
+     * Identical to the parent constructor, except that
+     * we start a PHP session to store the user ID and
+     * access token if during the course of execution
+     * we discover them.
+     *
+     * @param array $config the application configuration. Additionally
+     * accepts "sharedSession" as a boolean to turn on a secondary
+     * cookie for environments with a shared session (that is, your app
+     * shares the domain with other apps).
+     * @see BaseFacebook::__construct in facebook.php
+     */
+    public function __construct($config, \Orb\Auth\StateHandler\StateHandlerInterface $state)
+    {
+        $this->state_handler = $state;
+        parent::__construct($config);
     }
 
-    $session_var_name = $this->constructSessionVariableName($key);
-	  $this->state_handler[$session_var_name] = $value;
-  }
+    protected static $kSupportedKeys =
+        array('state', 'code', 'access_token', 'user_id');
 
-  protected function getPersistentData($key, $default = false) {
-    if (!in_array($key, self::$kSupportedKeys)) {
-      self::errorLog('Unsupported key passed to getPersistentData.');
-      return $default;
+    /**
+     * Provides the implementations of the inherited abstract
+     * methods.  The implementation uses PHP sessions to maintain
+     * a store for authorization codes, user ids, CSRF states, and
+     * access tokens.
+     */
+    protected function setPersistentData($key, $value)
+    {
+        if (!in_array($key, self::$kSupportedKeys)) {
+            self::errorLog('Unsupported key passed to setPersistentData.');
+            return;
+        }
+
+        $session_var_name = $this->constructSessionVariableName($key);
+        $this->state_handler[$session_var_name] = $value;
     }
 
-    $session_var_name = $this->constructSessionVariableName($key);
-	  $val = $this->state_handler[$session_var_name];
-    return isset($this->state_handler[$session_var_name]) ?
-	    $this->state_handler[$session_var_name] : $default;
-  }
+    protected function getPersistentData($key, $default = false)
+    {
+        if (!in_array($key, self::$kSupportedKeys)) {
+            self::errorLog('Unsupported key passed to getPersistentData.');
+            return $default;
+        }
 
-  protected function clearPersistentData($key) {
-    if (!in_array($key, self::$kSupportedKeys)) {
-      self::errorLog('Unsupported key passed to clearPersistentData.');
-      return;
+        $session_var_name = $this->constructSessionVariableName($key);
+
+        return isset($this->state_handler[$session_var_name]) ?
+            $this->state_handler[$session_var_name] : $default;
     }
 
-    $session_var_name = $this->constructSessionVariableName($key);
-    if (isset($this->state_handler[$session_var_name])) {
-      unset($this->state_handler[$session_var_name]);
+    protected function clearPersistentData($key)
+    {
+        if (!in_array($key, self::$kSupportedKeys)) {
+            self::errorLog('Unsupported key passed to clearPersistentData.');
+            return;
+        }
+
+        $session_var_name = $this->constructSessionVariableName($key);
+        if (isset($this->state_handler[$session_var_name])) {
+            unset($this->state_handler[$session_var_name]);
+        }
     }
-  }
 
-  protected function clearAllPersistentData() {
-    foreach (self::$kSupportedKeys as $key) {
-      $this->clearPersistentData($key);
+    protected function clearAllPersistentData()
+    {
+        foreach (self::$kSupportedKeys as $key) {
+            $this->clearPersistentData($key);
+        }
     }
-  }
 
-  protected function deleteSharedSessionCookie() {
-  }
+    protected function deleteSharedSessionCookie()
+    {
+    }
 
-  protected function getSharedSessionCookieName() {
-    return self::FBSS_COOKIE_NAME . '_' . $this->getAppId();
-  }
+    protected function getSharedSessionCookieName()
+    {
+        return self::FBSS_COOKIE_NAME . '_' . $this->getAppId();
+    }
 
-  protected function constructSessionVariableName($key) {
-    $parts = array('fb', $this->getAppId(), $key);
-    return implode('_', $parts);
-  }
+    protected function constructSessionVariableName($key)
+    {
+        $parts = array('fb', $this->getAppId(), $key);
+        return implode('_', $parts);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getHttpProtocol()
+    {
+        // auto-correct redirect url based on the helpdesk url
+        $container   = \Application\DeskPRO\App::$container;
+        $brand       = $container->get('brand_stack')->getActive()->getBrand();
+        $helpdeskUrl = $container->get('settings_resolver')->getSetting('core.deskpro_url', $brand);
+
+        if (strpos($helpdeskUrl, 'https://') !== false) {
+            return 'https';
+        }
+
+        return parent::getHttpProtocol();
+    }
 }
