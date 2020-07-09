742820e0a2628a08dfc6dc043dda068b523bce28
Sergey Yastrebov <serg.yastrebov@gmail.com>
2020-07-09 23:11:56 +0300

    [ch16055] missing unique index

---
+++ app/BUILD/src/Application/InstallBundle/Upgrade/Build/2020/06/Build1594322886.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Application\InstallBundle\Upgrade\Build;
+
+class Build1594322886 extends AbstractBuild implements OnlineBuildInterface
+{
+    public function addNewTables()
+    {
+    }
+
+    public function runAlters()
+    {
+        $this->execDbQuery('default', 'CREATE UNIQUE INDEX queue_agent_idx ON user_chat_queue_targets (queue_id, agent_id)');
+    }
+
+    public function run()
+    {
+    }
+}
--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -1769,6 +1769,12 @@ return [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2020/06/Build1592308935.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1592308935',
         'skipPostBuild' => false,
+        'isOnlineBuild' => false,
+    ],
+    1594322886 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2020/06/Build1594322886.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1594322886',
+        'skipPostBuild' => false,
         'isOnlineBuild' => true,
     ],
 ];
