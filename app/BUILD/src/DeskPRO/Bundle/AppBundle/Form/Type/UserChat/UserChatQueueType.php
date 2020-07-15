932089592eb5567d80be44891789bb5d739937af
ImmortaL <denis@ranneft.ru>
2020-07-15 18:34:30 +0300

    [ch16302] fix queues update

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/UserChat/UserChatQueueType.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Form/Type/UserChat/UserChatQueueType.php
@@ -59,8 +59,50 @@ class UserChatQueueType extends AbstractType
                 'allow_delete'   => true,
                 'by_reference'   => false,
                 'error_bubbling' => false,
+                'mapped'         => false,
             ])
         ;
+
+        $builder->addEventListener(FormEvents::POST_SUBMIT, [$this, 'onPostSubmit']);
+    }
+
+    /**
+     * @internal
+     *
+     * @param FormEvent $event
+     */
+    public function onPostSubmit(FormEvent $event)
+    {
+        $addedTargets = $event->getForm()->get('targets')->getData();
+        $queue        = $event->getData();
+        $existingKeys = [];
+        $addingKeys   = [];
+
+        //note: sorting is not affected here as it not updated at all and always is 10
+
+        // collect unique keys which exist
+        foreach ($queue->getTargets() as $existingTarget) {
+            $key                = sprintf('%d-%d', $existingTarget->getAgent()->getId(), $existingTarget->getQueue()->getId());
+            $existingKeys[$key] = true;
+        }
+
+        // add only those targets which don't exist and write down all keys we're trying to add
+        foreach ($addedTargets as $target) {
+            $target->setQueue($queue);
+            $key              = sprintf('%d-%d', $target->getAgent()->getId(), $target->getQueue()->getId());
+            $addingKeys[$key] = true;
+            if (!isset($existingKeys[$key])) {
+                $queue->getTargets()->add($target);
+            }
+        }
+
+        // if existing targets are not in those we wanted to add - remove it from collection
+        foreach ($queue->getTargets() as $target) {
+            $key = sprintf('%d-%d', $target->getAgent()->getId(), $target->getQueue()->getId());
+            if (!isset($addingKeys[$key])) {
+                $queue->getTargets()->removeElement($target);
+            }
+        }
     }
 
     /**
