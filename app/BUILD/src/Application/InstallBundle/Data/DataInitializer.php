2ed6fdeac262609f6d75222a93cdbf9e618e4721
ruslanix <ruslanix.com@gmail.com>
2020-07-21 18:00:40 +0300

    ch16517 fixes

--- app/BUILD/src/Application/InstallBundle/Data/DataInitializer.php
+++ app/BUILD/src/Application/InstallBundle/Data/DataInitializer.php
@@ -225,7 +225,7 @@ class DataInitializer
         $ticket->agent           = $for_agent;
         $ticket->department      = $department;
         $ticket->subject         = 'Welcome to DeskPRO';
-        $ticket->setTicketStatus(App::$container->getTicketStatuses()->find(TicketStatus::STATUS_TYPE_AWAITING_AGENT));
+        $ticket->setTicketStatus($this->container->getTicketStatuses()->find(TicketStatus::STATUS_TYPE_AWAITING_AGENT));
         $ticket->setProperty('send_reply_service', 'https://support.deskpro.com/api/open/tickets/new-ticket-message');
         $ticket->setProperty('allow_send_reply_service', true);
 
