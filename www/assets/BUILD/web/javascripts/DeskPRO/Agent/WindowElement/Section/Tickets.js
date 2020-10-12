b140064a97ed3645a36fc3842b3cf9a24e8ec704
Vladimir Kirejko <vladimir.kirejko@deskpro.com>
2020-10-12 15:56:57 +0300

    Call update only when $scope is defined.

--- www/assets/BUILD/web/javascripts/DeskPRO/Agent/WindowElement/Section/Tickets.js
+++ www/assets/BUILD/web/javascripts/DeskPRO/Agent/WindowElement/Section/Tickets.js
@@ -50,7 +50,7 @@ DeskPRO.Agent.WindowElement.Section.Tickets = new Orb.Class({
 		}, 11000);
 
 		this.limited_getUpdatedSlaCounts = _.throttle(function() {
-      if (self.listPage.$scope.realtime) {
+      if (_.get(self, 'listPage.$scope.realtime', false)) {
         self.getUpdatedSlaCounts();
       }
 		}, 35000);
