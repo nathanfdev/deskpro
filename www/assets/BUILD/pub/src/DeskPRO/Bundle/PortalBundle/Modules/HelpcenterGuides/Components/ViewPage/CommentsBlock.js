4b5168213b7f5b12912bd2590f075ad2e6ab564b
Thanipro <me@thani.pro>
2020-10-26 17:42:40 +0100

    remove redundant regex matcher

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/Modules/HelpcenterGuides/Components/ViewPage/CommentsBlock.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/Modules/HelpcenterGuides/Components/ViewPage/CommentsBlock.js
@@ -35,10 +35,6 @@ class Comment extends React.Component {
       const  replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
       var replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
-      // Change email addresses to mailto:: links.
-      const replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-      var replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
-
       return replacedText;
     }
 
