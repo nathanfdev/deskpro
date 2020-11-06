11fd7bdce2b552610236931c5d27b8bea9927834
Thanipro <me@thani.pro>
2020-11-06 16:15:30 +0100

    Add methods to set and trigger splash image download

--- app/BUILD/src/DeskPRO/Bundle/ApiBundle/Services/SplashImageService.php
+++ app/BUILD/src/DeskPRO/Bundle/ApiBundle/Services/SplashImageService.php
@@ -3,9 +3,13 @@
 namespace DeskPRO\Bundle\ApiBundle\Services;
 
 use Application\DeskPRO\BlobStorage\DeskproBlobStorage;
+use Application\DeskPRO\NewSettings\SettingsResolver;
 use DeskPRO\Bundle\AppBundle\Entity\SplashImageProperty;
+use DeskPRO\Component\Util\IpUtils;
 use Doctrine\ORM\EntityManager;
 use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Promise\PromiseInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
@@ -21,17 +25,23 @@ class SplashImageService
      * @var EntityManager
      */
     private EntityManager $em;
+    /**
+     * @var SettingsResolver
+     */
+    private SettingsResolver $settingResolver;
 
     /**
      * SplashImageService constructor.
      *
      * @param EntityManager $em
      * @param DeskproBlobStorage $blobStorage
+     * @param SettingsResolver $settingResolver
      */
-    public function __construct(EntityManager $em, DeskproBlobStorage $blobStorage)
+    public function __construct(EntityManager $em, DeskproBlobStorage $blobStorage, SettingsResolver $settingResolver)
     {
-        $this->em          = $em;
-        $this->blobStorage = $blobStorage;
+        $this->em              = $em;
+        $this->blobStorage     = $blobStorage;
+        $this->settingResolver = $settingResolver;
     }
 
     /**
@@ -54,6 +64,52 @@ class SplashImageService
             $this->em->flush();
 
             return $splashImage;
+        } catch (Exception $e) {
+            return $e;
+        }
+    }
+
+
+    /**
+     * @param $image
+     * @return SplashImageProperty|Exception
+     */
+    public function setSplashImage($image)
+    {
+        try {
+            $splashImage = new SplashImageProperty();
+            $splashImage->setUrn($splashImage::$unsplashNs . ':' . $image->id);
+            $splashImage->setOptions(['url' => $image->urls->raw]);
+            $this->em->persist($splashImage);
+            $this->em->flush();
+
+            $this->triggerSplashImageDownload($image);
+
+            return $splashImage;
+        } catch (Exception $e) {
+            return $e;
+        }
+    }
+
+    /**
+     * @param $image
+     * @return PromiseInterface|Exception
+     */
+    public function triggerSplashImageDownload($image)
+    {
+        try {
+            $client = new Client();
+            if (!IpUtils::isUrlUserCallable($image->links->download_location)) {
+                throw new \InvalidArgumentException("URL is not user callable");
+            }
+
+            return $client->requestAsync('GET', $image->links->download_location, [
+                'headers' => [
+                    'Authorization' => 'Client-ID ' . $this->settingResolver->getGlobalSettings()->get('services.unsplash_access_key',
+                            null),
+                ],
+            ]);
+
         } catch (Exception $e) {
             return $e;
         }
