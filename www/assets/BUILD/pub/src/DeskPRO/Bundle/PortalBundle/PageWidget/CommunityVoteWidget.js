68017c98023f1f21d00c278142e081fe00189544
Thanipro <me@thani.pro>
2020-12-13 11:36:34 +0100

    Update dom only after voting

--- www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/CommunityVoteWidget.js
+++ www/assets/BUILD/pub/src/DeskPRO/Bundle/PortalBundle/PageWidget/CommunityVoteWidget.js
@@ -6,34 +6,29 @@ export class CommunityVoteWidget extends PageWidget {
   renderWidget() {
     const $iAgreeBox = this.$element;
 
-    $iAgreeBox.click((event) => {
-      event.preventDefault();
-
-      if ($iAgreeBox.hasClass('closed')) {
-        return false; // rate closed
-      }
-      if ($iAgreeBox.hasClass('rate_forbidden')) {
-        return false; // no permission
-      }
-
-      const agreed = $iAgreeBox.hasClass('agreed');
-      const voteUpUrl = $iAgreeBox.data('vote-up-url');
-      const voteDownUrl = $iAgreeBox.data('vote-down-url');
+    const onSuccess = (agreed) => {
       const $counter = $iAgreeBox.find('span.counter');
-      const onFail = () => {
-        $counter.text(parseInt($counter.text(), 10) - (agreed ? -1 : 1));
-        if (agreed) {
-          $iAgreeBox.addClass('agreed');
-        } else {
-          $iAgreeBox.removeClass('agreed');
-        }
-      };
-
-      $counter.text(parseInt($counter.text(), 10) + (agreed ? -1 : 1));
       if (agreed) {
         $iAgreeBox.removeClass('agreed');
       } else {
         $iAgreeBox.addClass('agreed');
+      }
+      $counter.text(parseInt($counter.text(), 10) + (agreed ? -1 : 1));
+    };
+
+    $iAgreeBox.click((event) => {
+      event.preventDefault();
+
+      const agreed = $iAgreeBox.hasClass('agreed');
+      const voteUpUrl = $iAgreeBox.data('vote-up-url');
+      const voteDownUrl = $iAgreeBox.data('vote-down-url');
+
+      if ($iAgreeBox.hasClass('closed')) {
+        return false; // rate closed
+      }
+
+      if ($iAgreeBox.hasClass('rate_forbidden')) {
+        return false; // no permission
       }
 
       $.ajax({
@@ -42,14 +37,17 @@ export class CommunityVoteWidget extends PageWidget {
         contentType: 'application/json',
         dataType:    'json',
       }).success((data) => {
+        $iAgreeBox.find('div').text(data.error);
         if (!data.success) {
           if ('redirect' in data) {
             window.location.href = data.redirect;
           }
-          $iAgreeBox.find('div').text(data.error);
-          onFail();
+          return;
         }
-      }).fail(onFail);
+        onSuccess(agreed);
+      }).fail((data) => {
+        $iAgreeBox.find('div').text(data.error);
+      });
 
       return false;
     });
