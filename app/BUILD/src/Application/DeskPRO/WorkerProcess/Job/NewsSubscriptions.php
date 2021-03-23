530b3fc562576b63d5456623c2933939f7cf84cf
Thanipro <me@thani.pro>
2021-03-23 14:42:30 +0100

    replace mysql NOW with doctrine CURRENT_TIMESTAMP

--- app/BUILD/src/Application/DeskPRO/WorkerProcess/Job/NewsSubscriptions.php
+++ app/BUILD/src/Application/DeskPRO/WorkerProcess/Job/NewsSubscriptions.php
@@ -202,7 +202,7 @@ class NewsSubscriptions extends AbstractJob
                 JOIN n.category c
                 WHERE n.status = 'published'
                   AND n.date_published > :date
-                  AND n.date_published < NOW()
+                  AND n.date_published < CURRENT_TIMESTAMP()
                   AND c.brand = :brand
                 ORDER BY n.date_published DESC
             ")->setMaxResults(250)->execute(['date' => $lastDate, 'brand' => $brand]);
