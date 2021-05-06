5d10ddab61044e88ac62e0117e2a699d39983717
ImmortaL <denis@ranneft.ru>
2021-05-06 15:29:12 +0300

    Fix brand url for cli is always default

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Command/Articles/ArticlesCopyCategoriesCommand.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Command/Articles/ArticlesCopyCategoriesCommand.php
@@ -198,10 +198,17 @@ EOT
                 $this->em->persist($newArticle);
                 $this->em->flush();
 
+                $newArticleUrl = $this->getContainer()->get('brand_stack')->pushTemporary(
+                    $toBrand,
+                    function () use ($objectRouter, $newArticle) {
+                        return $objectRouter->getPortalUrl($newArticle);
+                    }
+                );
+
                 $output->writeln([
                     '<comment>[NEW ARTICLE]</comment>',
                     sprintf('#%d %s', $oldArticle->getId(), $objectRouter->getPortalUrl($oldArticle)),
-                    sprintf('=> #%d %s', $newArticle->getId(), $objectRouter->getPortalUrl($newArticle)),
+                    sprintf('=> #%d %s', $newArticle->getId(), $newArticleUrl),
                 ]);
             }
         }
