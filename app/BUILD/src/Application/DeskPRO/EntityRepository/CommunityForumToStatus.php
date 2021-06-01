e0c18b4fdcd44eb1b56b416fa9b3d04bc9a80c36
Thanipro <me@thani.pro>
2021-06-01 13:32:59 +0100

    Fix community topic selection

--- app/BUILD/src/Application/DeskPRO/EntityRepository/CommunityForumToStatus.php
+++ app/BUILD/src/Application/DeskPRO/EntityRepository/CommunityForumToStatus.php
@@ -4,9 +4,34 @@ namespace Application\DeskPRO\EntityRepository;
 
 /**
  * Class CommunityForumToStatus
- *
- * @package Application\DeskPRO\EntityRepository
  */
 class CommunityForumToStatus extends AbstractEntityRepository
 {
+    /**
+     * @param array $statuses
+     * @param null $forum
+     *
+     * @return array
+     */
+    public function getForumStatus($statuses = [], $forum = null)
+    {
+        $forumStatus = [];
+
+        if (empty($statuses) || !$forum) {
+            return $forumStatus;
+        }
+
+        foreach ($statuses as $status) {
+            $foundStatus = $this->findOneBy([
+                'forum'  => $forum,
+                'status' => $status->getId(),
+            ]);
+
+            if ($foundStatus) {
+                $forumStatus[] = $status;
+            }
+        }
+
+        return $forumStatus;
+    }
 }
