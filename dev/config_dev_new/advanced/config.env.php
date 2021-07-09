06aa9bdf4fc035fa678e621e0cb5e86395213391
Ashley Dawson <ashley.dawson@deskpro.com>
2021-07-09 15:38:01 +0100

    Commented out config template env params for proxy config

--- dev/config_dev_new/advanced/config.env.php
+++ dev/config_dev_new/advanced/config.env.php
@@ -10,5 +10,5 @@ $ENV_CONFIG['debug_mode'] = true;
 $ENV_CONFIG['init_scripts'] = [];
 
 // Deskpro http and token exchange proxy URLs used in cloud mode
-$ENV_CONFIG['proxy_token_exchange_url'] = getenv('DP_PROXY_TOKEN_EXCHANGE_URL') ?: "http://localhost:6317";
-$ENV_CONFIG['http_proxy_url'] = getenv('DP_HTTP_PROXY_URL') ?: "http://localhost:3000";
+//$ENV_CONFIG['proxy_token_exchange_url'] = "http://localhost:6317";
+//$ENV_CONFIG['http_proxy_url'] = "http://localhost:3000";
