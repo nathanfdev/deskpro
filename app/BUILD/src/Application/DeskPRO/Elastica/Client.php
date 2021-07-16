af7d3a1cd61dc3705d046c409e1a819a9a93f85c
Ashley Dawson <ashley.dawson@deskpro.com>
2021-07-16 17:33:38 +0100

    Added outbound proxy to ES client

--- app/BUILD/src/Application/DeskPRO/Elastica/Client.php
+++ app/BUILD/src/Application/DeskPRO/Elastica/Client.php
@@ -8,10 +8,25 @@ namespace Application\DeskPRO\Elastica;
  * @package DeskPRO
  */
 
+use Application\DeskPRO\Proxy\OutboundHttpProxy;
+use Elastica\Request;
 use FOS\ElasticaBundle\Elastica\Client as BaseClient;
 
 class Client extends BaseClient
 {
+    /**
+     * @var OutboundHttpProxy
+     */
+    private $proxy;
+
+    /**
+     * @param OutboundHttpProxy $proxy
+     */
+    public function setOutboundProxy(OutboundHttpProxy $proxy)
+    {
+        $this->proxy = $proxy;
+    }
+
     protected function _initConnections()
     {
         // doubleslashes aren't accepted anymore since ES 5.*
@@ -29,5 +44,37 @@ class Client extends BaseClient
                 $conn->addConfig('headers', []);
             }
         }
+    }
+
+    public function request($path, $method = Request::GET, $data = array(), array $query = array())
+    {
+        global $DP_ENV;
+
+        if (OutboundHttpProxy::isUsingProxy() && $this->proxy) {
+            $proxyUrlParts = parse_url($DP_ENV->getConfig('env.http_proxy_url'));
+
+            $conn = $this->getConnection();
+
+            $serviceToken = $this->proxy->getElasticsearchServiceToken(
+                DPC_SITE_ID,
+                strtolower($conn->getTransport()),
+                $path,
+                $conn->getPort(),
+                $method
+            );
+
+            $conn->setHost($proxyUrlParts['host']);
+            $conn->setTransport(ucfirst($proxyUrlParts['scheme']));
+            $conn->setPort((int) $proxyUrlParts['port']);
+            $conn->addConfig('headers', [
+                'Proxy-Authorization' => 'Bearer '.$serviceToken,
+                'X-Forward-To' => $conn->getHost(),
+            ]);
+
+            $method = "POST"; // API Gateway excludes a GET body, so change it to a post @see https://github.com/elastic/elasticsearch/issues/16024#issuecomment-172491501
+            $path   = "elasticsearch/{$path}";
+        }
+
+        return parent::request($path, $method, $data, $query);
     }
 }
