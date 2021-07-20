bb1afd3ef3dbbcc75a4536f524929268e046b51d
Julien Ducro <julien.ducro@deskpro.com>
2021-07-20 18:50:25 +0200

    Out of Office replies still marking users [ch29847]

--- app/BUILD/src/Application/DeskPRO/EmailGateway/TicketGatewayProcessor.php
+++ app/BUILD/src/Application/DeskPRO/EmailGateway/TicketGatewayProcessor.php
@@ -553,7 +553,7 @@ class TicketGatewayProcessor extends AbstractGatewayProcessor
 
         if (!$person->disable_autoresponses) {
             if ($returnPath = $this->reader->getHeader('Return-Path')) {
-                if ($returnPath->getHeader() == '<>' && $this->container->getSetting('core_email.mark_null_returnpath_as_autoresponder', true)) {
+                if ($returnPath->getHeader() == '<>' && $this->container->getSetting('core_email.mark_null_returnpath_as_autoresponder')) {
                     $this->logMessage('Null return path, disabling auto-responses for this user');
                     $person->setDisableAutoresponses(
                         true,
