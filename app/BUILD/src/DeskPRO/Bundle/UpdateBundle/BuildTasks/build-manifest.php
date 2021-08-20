9a5474991d3c21b246747c77af37a33230dd3c6c
Christopher Nadeau <chris.nadeau@deskpro.com>
2021-08-20 14:43:49 +0100

    change unused permission_cache to use array type instead of object

--- app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
+++ app/BUILD/src/DeskPRO/Bundle/UpdateBundle/BuildTasks/build-manifest.php
@@ -1849,15 +1849,21 @@ return [
         'skipPostBuild' => false,
         'isOnlineBuild' => true,
     ],
+    1620727256 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2021/05/Build1620727256.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1620727256',
+        'skipPostBuild' => false,
+        'isOnlineBuild' => true,
+    ],
     1620824976 => [
         'file'          => '/src/Application/InstallBundle/Upgrade/Build/2021/05/Build1620824976.php',
         'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1620824976',
         'skipPostBuild' => false,
         'isOnlineBuild' => true,
     ],
-    1620727256 => [
-        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2021/05/Build1620727256.php',
-        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1620727256',
+    1629466470 => [
+        'file'          => '/src/Application/InstallBundle/Upgrade/Build/2021/08/Build1629466470.php',
+        'classname'     => 'Application\InstallBundle\Upgrade\Build\Build1629466470',
         'skipPostBuild' => false,
         'isOnlineBuild' => true,
     ],
