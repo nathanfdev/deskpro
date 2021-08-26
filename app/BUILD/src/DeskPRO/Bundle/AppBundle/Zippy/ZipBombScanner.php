ed7093e75042495fe4bc162732d1cef63893eb23
Ashley Dawson <ashley.dawson@deskpro.com>
2021-08-26 14:58:57 +0100

    Set max allowable archive contents

--- app/BUILD/src/DeskPRO/Bundle/AppBundle/Zippy/ZipBombScanner.php
+++ app/BUILD/src/DeskPRO/Bundle/AppBundle/Zippy/ZipBombScanner.php
@@ -5,6 +5,11 @@ namespace DeskPRO\Bundle\AppBundle\Zippy;
 class ZipBombScanner
 {
     /**
+     * Max allowable items in archive
+     */
+    const MAX_ARCHIVE_CONTENTS = 4096;
+
+    /**
      * Returns TRUE of the "true" size of the zip is within a limit
      *
      * @param string $filepath
@@ -20,6 +25,10 @@ class ZipBombScanner
         $size = 0;
 
         while ($idx = $zip->statIndex($i++)) {
+            if ($i > self::MAX_ARCHIVE_CONTENTS) {
+                return false;
+            }
+
             $size += $idx['size'];
         }
 
