d3604d39b7469a6efee5db8e0eb5f4dd08df25a3
Ashley Dawson <ashley.dawson@deskpro.com>
2021-09-27 16:37:23 +0100

    Removed dev toolbar whitelist for twig sandbox

--- app/BUILD/src/Application/DeskPRO/Templating/Sandbox/whitelists/base_paths.php
+++ app/BUILD/src/Application/DeskPRO/Templating/Sandbox/whitelists/base_paths.php
@@ -3,7 +3,4 @@
 // A list of allowed URL base paths. Note that all templates under this base URL
 // path will have sandboxed twig disabled
 
-return [
-    '/_wdt',
-    '/_profiler',
-];
+return [];
