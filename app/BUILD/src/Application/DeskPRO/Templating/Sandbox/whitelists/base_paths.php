ecfad5b9c8976e7039e95d7c7be4834d5c968ba7
Christopher Nadeau <chris.nadeau@deskpro.com>
2021-10-01 12:32:13 +0100

    enable paths when in dev/debug mode

--- app/BUILD/src/Application/DeskPRO/Templating/Sandbox/whitelists/base_paths.php
+++ app/BUILD/src/Application/DeskPRO/Templating/Sandbox/whitelists/base_paths.php
@@ -3,4 +3,11 @@
 // A list of allowed URL base paths. Note that all templates under this base URL
 // path will have sandboxed twig disabled
 
+if (isset($GLOBALS['DP_ENV']) && $GLOBALS['DP_ENV']->isDebug()) {
+    return [
+        '/_wdt',
+        '/_profiler',
+    ];
+}
+
 return [];
