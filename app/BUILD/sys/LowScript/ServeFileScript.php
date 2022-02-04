8350bfa022d5a6a57e88a7de7e9b86cfe4fb9762
Christopher Nadeau <chris.nadeau@deskpro.com>
2022-02-04 17:51:19 +0000

    Add source_ref to blobs.
    We have imperfect handling of is_temp blobs and the cleanup of is_temp blobs
    was commented-out to fix bugs where people's attachments were randomly disappearing.
    
    I've added a new source_ref field to blobs for two reasons:
    
    a) We currently don't have any map of a blob back to its owning object. It makes
    sense to try and save at least some sort of id when we know it, at least for auditing.
    
    b) We can audit each place in code where blobs are created, and one by one, we can
    confirm if is_temp is being handled properly. If it is, then we ammend the code
    to set a source_ref.
    
    c) This means the cleanup script can look for is_temp *and* non-null source_ref.
    If both are set, it indicates the blob was uploaded in a place we've confirmed
    is handling is_temp properly.
    
    d) Every place where is_temp is NOT validated remains in-place for us to look
    at on a case-by-case.
    
    e) The ServeFileScript has been chagned here to stop serving is_temp blobs
    after a few hours. If a customer complains of missing images, we have
    a fallback where we can just toggle is_temp=0. They won't actually be deleted,
    so no data loss.

--- app/BUILD/sys/LowScript/ServeFileScript.php
+++ app/BUILD/sys/LowScript/ServeFileScript.php
@@ -743,10 +743,13 @@ class ServeFileScript extends LowScriptAbstract
     {
         $authcode = $blob_id.$authseg;
 
+        $skipTmpCheck = false;
         if (substr($authcode, -1) === Blob::SUFFIX_TICKET_ATTACHMENT) {
             $this->checkTicketAttachmentAccessTokenOrRedirect($authcode);
+            $skipTmpCheck = true;
         } elseif (substr($authcode, -2) === Blob::SUFFIX_DOWNLOAD_ATTACHMENT) {
             $this->checkDownloadAttachmentAccessTokenOrRedirect($authcode);
+            $skipTmpCheck = true;
         }
 
         $size = null;
@@ -754,7 +757,7 @@ class ServeFileScript extends LowScriptAbstract
             $size = $_GET['s'];
         }
 
-        $this->showBlob($blob_id, $size, $authcode);
+        $this->showBlob($blob_id, $size, $authcode, $skipTmpCheck);
     }
 
     /**
@@ -766,7 +769,7 @@ class ServeFileScript extends LowScriptAbstract
      *
      * @throws \Exception
      */
-    protected function showBlob($blob, $size = null, $blobAuth = null)
+    protected function showBlob($blob, $size = null, $blobAuth = null, $skipTmpCheck = true)
     {
         if ($this->userDoesAcceptGzip() && isset($_GET['g'])) {
             if (preg_match('#^([0-9]+)([A-Z]+0)$#', $_GET['g'], $m)) {
@@ -835,6 +838,25 @@ class ServeFileScript extends LowScriptAbstract
             echo 'File not found. (3.1)';
 
             return;
+        }
+
+        // is_temp abuse check
+        // if the file is too old then we wont render it
+        if (!$skipTmpCheck
+            && $blob['is_temp']
+            && !$blob['source_ref']
+        ) {
+            $ts = strtotime($blob['date_created']);
+            $tsCut = time() - (60 * 60 * 10); //10hrs
+            if ($ts < $tsCut) {
+                if ($this->errorMode == 'exception') {
+                    throw new \Exception('File not found. (3.2)', 404);
+                }
+                header('HTTP/1.0 404 Not Found');
+                echo 'File not found. (3.2)';
+
+                return;
+            }
         }
 
         $blobId = $blob['id'];
